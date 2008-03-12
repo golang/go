74083dc60fb9ea97599a424a4996aacc55ee0bcd
Robert Griesemer <gri@golang.org>
2008-03-11 18:07:22 -0700

    - added 'nil' keyword to lang doc   (did not add 'true' and 'false' because I think these don't need to be   keywords, they can be recreated from whole cloth) - added 'new' syntax to expressions   (this is needed because of the type as 1st argument - the expression   list is probably not yet implemented, but I think we may want it   eventually) - removed optional ';'s in sieve example - make it more concise - fixed several bugs in go.atg, no parses sieve - added sieve as test case
    SVN=112135

---
+++ tests/sieve.go
@@ -0,0 +1,42 @@
+// $G $F.go && $L $F.$A  # don't run it - goes forever
+
+// Copyright 2009 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package Main
+
+// Send the sequence 2, 3, 4, ... to channel 'ch'.
+func Generate(ch *chan> int) {
+  for i := 2; ; i++ {
+    >ch = i  // Send 'i' to channel 'ch'.
+  }
+}
+
+// Copy the values from channel 'in' to channel 'out',
+// removing those divisible by 'prime'.
+func Filter(in *chan< int, out *chan> int, prime int) {
+  for {
+    i := <in;  // Receive value of new variable 'i' from 'in'.
+    if i % prime != 0 {
+      >out = i  // Send 'i' to channel 'out'.
+    }
+  }
+}
+
+// The prime sieve: Daisy-chain Filter processes together.
+func Sieve() {
+  ch := new(chan int);  // Create a new channel.
+  go Generate(ch);  // Start Generate() as a subprocess.
+  for {
+    prime := <ch;
+    printf("%d\n",  prime);
+    ch1 := new(chan int);
+    go Filter(ch, ch1, prime);
+    ch = ch1
+  }
+}
+
+func Main() {
+  Sieve()
+}
