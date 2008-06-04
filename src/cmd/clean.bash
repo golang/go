f4ccabdea6ef7266605a44ac5b1c8f2737c95047
Ken Thompson <ken@golang.org>
2008-06-04 16:44:48 -0700

    stuff
    SVN=121190

---
+++ src/cmd/clean.bash
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Copyright 2009 The Go Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style
+# license that can be found in the LICENSE file.
+
+
+for i in 6l 6a 6c 6g gc cc
+do
+	cd $i
+	mk clean
+	cd ..
+done
