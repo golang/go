e92b7538102ed9ea1402fb8ce4d948da27ffd456
Robert Griesemer <gri@golang.org>
2008-06-06 15:53:14 -0700

    - fixed a few tests and added 3 incorrectly succeeding tests - updated go_lang.txt to be more uniform and match the implementation - made makehtml work on Mac - fixed a couple of bugs in go.atg
    SVN=121520

---
+++ test/func1.go
@@ -0,0 +1,16 @@
+// errchk $G $F.go
+
+// Copyright 2009 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+func f1(a int) (int, float) {  // BUG multiple return values must have names
+	return 7, 7.0;
+}
+
+
+func f2(a int) (a int, b float) {  // BUG return value names must be different from parameter names
+	return 8, 8.0;
+}
