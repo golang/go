8b253a1f55765e07b98d1f32e3f6d704fd69f956
Rob Pike <r@golang.org>
2008-06-06 12:57:00 -0700

    first cut at test suite
    SVN=121454

--- test/hashmap.go
+++ test/hashmap.go
@@ -4,8 +4,6 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// To compile: go hashmap.go && gcc -o g.out main.go.c gort0.c && g.out
-
 package main
 
 // ----------------------------------------------------------------------------
