1b120ba2de4dffe2e379a5c813c53049ede4dd6e
Robert Griesemer <gri@golang.org>
2008-06-06 17:05:53 -0700

    - fixed test commands
    SVN=121551

--- test/bugs/bug032.go
+++ test/bugs/bug032.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// $G $F.go && echo BUG: compilation succeeds incorrectly
+// $G $D/$F.go && echo BUG: compilation succeeds incorrectly
 
 package main
 
