1b44a41c63288faece05df08d4d96e6d389cb958
Rob Pike <r@golang.org>
2008-06-08 19:21:16 -0700

    update lots of tests
    SVN=121624

--- test/ken/robliteral.go
+++ test/ken/robliteral.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// $G $D/$F.go && $L $F.$A && ! ./$A.out && echo BUG: known to fail incorrectly
+// $G $D/$F.go || echo BUG: known to fail incorrectly
 
 package main
 
