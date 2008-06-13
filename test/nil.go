dedd1736f03d2b792347ffe7edad05079b1c39bb
Rob Pike <r@golang.org>
2008-06-13 09:09:22 -0700

    Add nil test, with bug
    SVN=122644

---
+++ test/nil.go
@@ -0,0 +1,35 @@
+// $G $F.go && $L $F.$A && ./$A.out
+
+// Copyright 2009 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+type T struct {
+	i int
+}
+
+type IN interface {
+}
+
+func main() {
+	var i *int;
+	var f *float;
+	var s *string;
+	var m *map[float] *int;
+	var c *chan int;
+	var t *T;
+	var in IN;
+	var ta *[]T;
+
+	i = nil;
+	f = nil;
+	s = nil;
+	m = nil;
+	c = nil;
+	t = nil;
+	i = nil;
+	ta = new([1]T);
+	ta[0] = nil;  //BUG (see bugs/bug045.go) 
+}
