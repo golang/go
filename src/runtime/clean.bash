8d621493e8c11efaf2d25dab27b53abf69c8eaac
Rob Pike <r@golang.org>
2008-06-25 21:19:05 -0700

    put a makefile into runtime make the runtime library an archive (rt0 is still separate) update 6l to use the archive add fakeobj.c, missing from prior change
    SVN=124762

--- src/runtime/clean.bash
+++ src/runtime/clean.bash
@@ -2,4 +2,10 @@
 # Use of this source code is governed by a BSD-style
 # license that can be found in the LICENSE file.
 
-echo nothing to clean here
+set -ex
+
+for GOOS in linux darwin
+do
+	make clean
+done
+
