bcfd31f0e92b82a1b3e48b6e8146da4623b38315
Rob Pike <r@golang.org>
2008-06-26 15:35:43 -0700

    repair struct definition
    SVN=124985

--- src/runtime/amd64_linux.h
+++ src/runtime/amd64_linux.h
@@ -25,16 +25,16 @@ struct timespec {
 struct stat {
 	dev_t	st_dev;     /* ID of device containing file */
 	ino_t	st_ino;     /* inode number */
-	mode_t	st_mode;    /* protection */
 	nlink_t	st_nlink;   /* number of hard links */
+	mode_t	st_mode;    /* protection */
 	uid_t	st_uid;     /* user ID of owner */
 	gid_t	st_gid;     /* group ID of owner */
-	dev_t	st_rdev;
+	int32	pad0;
 	dev_t	st_rdev;    /* device ID (if special file) */
+	off_t	st_size;    /* total size, in bytes */
+	blksize_t st_blksize; /* blocksize for filesystem I/O */
+	blkcnt_t	st_blocks;  /* number of blocks allocated */
 	struct timespec	st_atime;   /* time of last access */
 	struct timespec	st_mtime;   /* time of last modification */
 	struct timespec	st_ctime;   /* time of last status change */
-	off_t	st_size;    /* total size, in bytes */
-	blkcnt_t	st_blocks;  /* number of blocks allocated */
-	blksize_t st_blksize; /* blocksize for filesystem I/O */
 };
