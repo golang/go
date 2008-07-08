f86c6f673e6de1fd57c8b0d79a833e0880a92aa3
Rob Pike <r@golang.org>
2008-07-07 17:52:44 -0700

    k,l,m := 0,0,0 doesn't compile
    SVN=126247

---
+++ test/bugs/bug065.go
@@ -0,0 +1,11 @@
+// $G $D/$F.go && $L $F.$A && ./$A.out
+
+// Copyright 2009 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+func main() {
+	k, l, m := 0,0,0;
+}
