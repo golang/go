92bb676290cd129969e9c8b9ce2033cd49894214
Rob Pike <r@golang.org>
2008-07-09 16:09:48 -0700

    build packages automatically
    SVN=126577

---
+++ src/lib/make.bash
@@ -0,0 +1,10 @@
+# Copyright 2009 The Go Authors.  All rights reserved.
+# Use of this source code is governed by a BSD-style
+# license that can be found in the LICENSE file.
+
+#!/bin/bash
+
+rm -f *.6
+6g fmt.go 
+6g container/vector.go
+mv *.6 $GOROOT/pkg
