526200345cd6b1e1449f7adb62f05410ff01a44a
Ken Thompson <ken@golang.org>
2008-07-14 14:33:39 -0700

    synch chan
    SVN=127055

--- src/runtime/chan.c
+++ src/runtime/chan.c
@@ -4,16 +4,29 @@
 
 #include "runtime.h"
 
-static	int32	debug	= 1;
+static	int32	debug	= 0;
 
 typedef	struct	Hchan	Hchan;
+typedef	struct	Link	Link;
 
 struct	Hchan
 {
 	uint32	elemsize;
-	uint32	hint;
-	uint32	eo;
-	Alg*	elemalg;
+	uint32	dataqsiz;		// size of the circular q
+	uint32	qcount;			// total data in the q
+	uint32	eo;			// vararg of element
+	uint32	po;			// vararg of present bool
+	Alg*	elemalg;		// interface for element type
+	Link*	senddataq;		// pointer for sender
+	Link*	recvdataq;		// pointer for receiver
+	WaitQ	recvq;			// list of recv waiters
+	WaitQ	sendq;			// list of send waiters
+};
+
+struct	Link
+{
+	Link*	link;
+	byte	data[8];
 };
 
 // newchan(elemsize uint32, elemalg uint32, hint uint32) (hchan *chan any);
@@ -22,6 +35,7 @@ sys·newchan(uint32 elemsize, uint32 elemalg, uint32 hint,
 	Hchan* ret)
 {
 	Hchan *c;
+	int32 i;
 
 	if(elemalg >= nelem(algarray)) {
 		prints("0<=");
@@ -37,10 +51,30 @@ sys·newchan(uint32 elemsize, uint32 elemalg, uint32 hint,
 
 	c->elemsize = elemsize;
 	c->elemalg = &algarray[elemalg];
-	c->hint = hint;
+
+	if(hint > 0) {
+		Link *d, *b, *e;
+
+		// make a circular q
+		b = nil;
+		e = nil;
+		for(i=0; i<hint; i++) {
+			d = mal(sizeof(*d));
+			if(e == nil)
+				e = d;
+			d->link = b;
+			b = d;
+		}
+		e->link = b;
+		c->recvdataq = b;
+		c->senddataq = b;
+		c->qcount = 0;
+		c->dataqsiz = hint;
+	}
 
 	// these calculations are compiler dependent
 	c->eo = rnd(sizeof(c), elemsize);
+	c->po = rnd(c->eo+elemsize, 1);
 
 	ret = c;
 	FLUSH(&ret);
@@ -52,10 +86,11 @@ sys·newchan(uint32 elemsize, uint32 elemalg, uint32 hint,
 		sys·printint(elemsize);
 		prints("; elemalg=");
 		sys·printint(elemalg);
-		prints("; hint=");
-		sys·printint(hint);
+		prints("; dataqsiz=");
+		sys·printint(c->dataqsiz);
 		prints("\n");
 	}
+
 }
 
 // chansend(hchan *chan any, elem any);
@@ -63,6 +98,7 @@ void
 sys·chansend(Hchan* c, ...)
 {
 	byte *ae;
+	G *gr;
 
 	ae = (byte*)&c + c->eo;
 	if(debug) {
@@ -72,6 +108,23 @@ sys·chansend(Hchan* c, ...)
 		c->elemalg->print(c->elemsize, ae);
 		prints("\n");
 	}
+	if(c->dataqsiz > 0)
+		goto asynch;
+
+	gr = dequeue(&c->recvq);
+	if(gr != nil) {
+		c->elemalg->copy(c->elemsize, gr->elem, ae);
+		gr->status = Grunnable;
+		return;
+	}
+	c->elemalg->copy(c->elemsize, g->elem, ae);
+	g->status = Gwaiting;
+	enqueue(&c->sendq, g);
+	sys·gosched();
+	return;
+
+asynch:
+	throw("sys·chansend: asynch not yet");
 }
 
 // chanrecv1(hchan *chan any) (elem any);
@@ -79,6 +132,7 @@ void
 sys·chanrecv1(Hchan* c, ...)
 {
 	byte *ae;
+	G *gs;
 
 	ae = (byte*)&c + c->eo;
 	if(debug) {
@@ -86,18 +140,42 @@ sys·chanrecv1(Hchan* c, ...)
 		sys·printpointer(c);
 		prints("\n");
 	}
+	if(c->dataqsiz > 0)
+		goto asynch;
+
+	gs = dequeue(&c->sendq);
+	if(gs != nil) {
+		c->elemalg->copy(c->elemsize, ae, gs->elem);
+		gs->status = Grunnable;
+		return;
+	}
+	g->status = Gwaiting;
+	enqueue(&c->recvq, g);
+	sys·gosched();
+	c->elemalg->copy(c->elemsize, ae, g->elem);
+	return;
+
+asynch:
+	throw("sys·chanrecv1: asynch not yet");
 }
 
 // chanrecv2(hchan *chan any) (elem any, pres bool);
 void
 sys·chanrecv2(Hchan* c, ...)
 {
-	byte *ae;
+	byte *ae, *ap;
 
 	ae = (byte*)&c + c->eo;
+	ap = (byte*)&c + c->po;
 	if(debug) {
 		prints("chanrecv2: chan=");
 		sys·printpointer(c);
 		prints("\n");
 	}
+	if(c->dataqsiz > 0)
+		goto asynch;
+	throw("sys·chanrecv2: synch not yet");
+
+asynch:
+	throw("sys·chanrecv2: asynch not yet");
 }
