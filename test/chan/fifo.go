7cece16d865a842cad159e780a00610574c6e6cb
Rob Pike <r@golang.org>
2008-07-15 20:52:07 -0700

    channel tests with new syntax
    SVN=127436

--- test/chan/fifo.go
+++ test/chan/fifo.go
@@ -13,22 +13,22 @@ const N = 10
 func AsynchFifo() {
 	ch := new(chan int, N);
 	for i := 0; i < N; i++ {
-		>ch = i
+		ch -< i
 	}
 	for i := 0; i < N; i++ {
-		if <ch != i {
+		if <-ch != i {
 			print "bad receive\n";
 			sys.exit(1);
 		}
 	}
 }
 
-func Chain(ch *chan< int, val int, in *chan< int, out *chan> int) {
-	BUG := <in;
-	if <ch != val {
-		panic val;
+func Chain(ch *chan<- int, val int, in *chan<- int, out *chan-< int) {
+	<-in;
+	if <-ch != val {
+		panic val
 	}
-	>out = 1;
+	out -< 1
 }
 
 // thread together a daisy chain to read the elements in sequence
@@ -41,9 +41,9 @@ func SynchFifo() {
 		go Chain(ch, i, in, out);
 		in = out;
 	}
-	>start = 0;
+	start -< 0;
 	for i := 0; i < N; i++ {
-		>ch = i
+		ch -< i
 	}
 }
 
--- test/sieve.go
+++ test/sieve.go
@@ -7,34 +7,34 @@
 package main
 
 // Send the sequence 2, 3, 4, ... to channel 'ch'.
-func Generate(ch *chan> int) {
-  for i := 2; ; i++ {
-    >ch = i  // Send 'i' to channel 'ch'.
-  }
+func Generate(ch *chan-< int) {
+	for i := 2; ; i++ {
+		ch -< i  // Send 'i' to channel 'ch'.
+	}
 }
 
 // Copy the values from channel 'in' to channel 'out',
 // removing those divisible by 'prime'.
-func Filter(in *chan< int, out *chan> int, prime int) {
-  for {
-    i := <in  // Receive value of new variable 'i' from 'in'.
-    if i % prime != 0 {
-      >out = i  // Send 'i' to channel 'out'.
-    }
-  }
+func Filter(in *chan<- int, out *chan-< int, prime int) {
+	for {
+		i := <-in  // Receive value of new variable 'i' from 'in'.
+		if i % prime != 0 {
+			out -< i  // Send 'i' to channel 'out'.
+		}
+	}
 }
 
 // The prime sieve: Daisy-chain Filter processes together.
 func Sieve() {
-  ch := new(chan int);  // Create a new channel.
-  go Generate(ch);  // Start Generate() as a subprocess.
-  for {
-    prime := <ch;
-    print prime, "\n";
-    ch1 := new(chan int);
-    go Filter(ch, ch1, prime);
-    ch = ch1
-  }
+	ch := new(chan int);  // Create a new channel.
+	go Generate(ch);  // Start Generate() as a subprocess.
+	for {
+		prime := <-ch;
+		print prime, "\n";
+		ch1 := new(chan int);
+		go Filter(ch, ch1, prime);
+		ch = ch1
+	}
 }
 
 func main() {
