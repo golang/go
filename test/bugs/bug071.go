30a39e8f29323912b6a67239483b00f3408f43cd
Rob Pike <r@golang.org>
2008-07-17 11:04:44 -0700

    compiler bug: infinite recursion in subtype()
    SVN=127822

---
+++ test/bugs/bug071.go
@@ -0,0 +1,22 @@
+// $G $D/$F.go || echo BUG: compiler crashes
+
+// Copyright 2009 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+type rat struct  {
+	den  int;
+}
+
+func (u *rat) pr() {
+}
+
+type dch struct {
+	dat *chan  *rat;
+}
+
+func dosplit(in *dch){
+	dat := <-in.dat;
+}
