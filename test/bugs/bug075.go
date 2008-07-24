97184bc4573f19f223da144b06fd5a8bf1803b4e
Rob Pike <r@golang.org>
2008-07-23 18:11:17 -0700

    new bug: x, ok = t.m[i] doesn't compile
    R=ken
    OCL=13395
    CL=13395

---
+++ test/bugs/bug075.go
@@ -0,0 +1,16 @@
+// $G $D/$F.go && $L $F.$A && ./$A.out
+
+// Copyright 2009 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+type T struct { m *map[int]int }
+func main() {
+	t := new(T);
+	t.m = new(map[int]int);
+	var x int;
+	var ok bool;
+	x, ok = t.m[0];  //bug075.go:11: bad shape across assignment - cr=1 cl=2
+}
