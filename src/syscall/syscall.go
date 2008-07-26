4fe776ac43991af527c76f921e040e76d34f9fe0
Rob Pike <r@golang.org>
2008-07-26 14:49:21 -0700

    beginnings of a low-level syscall library
    R=ken
    OCL=13483
    CL=13496

---
+++ src/syscall/syscall.go
@@ -0,0 +1,15 @@
+// Copyright 2009 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package syscall
+
+// for simplicity of addressing in assembler, all integers are 64 bits
+// in these calling sequences.
+
+func open(*byte, int64) (ret int64, errno int64);
+func close(int64) (ret int64, errno int64);
+func read(int64, *byte, int64) (ret int64, errno int64);
+func write(int64, *byte, int64) (ret int64, errno int64);
+
+export open, close, read, write
