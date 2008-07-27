6bff372594a0c9c30f3bc2a14a551b2600db0066
Rob Pike <r@golang.org>
2008-07-27 13:50:54 -0700

    update tests
    R=ken
    OCL=13507
    CL=13507

--- test/fixedbugs/bug025.go
+++ test/fixedbugs/bug025.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// $G $D/$F.go && echo BUG: bad exit status - should be 1 - plus extra newline in message
+// ! $G $D/$F.go
 
 package main
 
