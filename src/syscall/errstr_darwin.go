4a08aeb4d93fbb527663cd03c7e89025b70fef0f
Rob Pike <r@golang.org>
2008-07-29 13:16:42 -0700

    fix type error caused by recent change
    R=gri
    OCL=13545
    CL=13545

--- src/syscall/errstr_darwin.go
+++ src/syscall/errstr_darwin.go
@@ -336,11 +336,11 @@ func str(val int64) string {  // do it here rather than with fmt to avoid depend
 	var buf [32]byte;  // big enough for int64
 	i := len(buf)-1;
 	for val >= 10 {
-		buf[i] = val%10 + '0';
+		buf[i] = byte(val%10 + '0');
 		i--;
 		val /= 10;
 	}
-	buf[i] = val + '0';
+	buf[i] = byte(val + '0');
 	return string(buf)[i:len(buf)];
 }
 
