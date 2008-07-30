488a27b6eb3404ebcf5ae8c879bbf12b66f8bf6d
Robert Griesemer <gri@golang.org>
2008-07-30 16:09:29 -0700

    - filed 2 bugs: unary ^ not implemented, compiler crash with illegal const decl
    R=r
    OCL=13666
    CL=13666

---
+++ test/bugs/bug081.go
@@ -0,0 +1,14 @@
+// ! $G $D/$F.go
+
+// Copyright 2009 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main	
+ 	
+const x x = 2;
+
+/*
+bug081.go:3: first constant must evaluate an expression
+Bus error
+*/
