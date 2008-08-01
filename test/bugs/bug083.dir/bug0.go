d001123b058998e8719fde078b0fedb545347c45
Robert Griesemer <gri@golang.org>
2008-08-01 15:22:41 -0700

    bug: non-exported type is accessible in client
    R=r
    OCL=13736
    CL=13738

---
+++ test/bugs/bug083.dir/bug0.go
@@ -0,0 +1,11 @@
+// Copyright 2009 The Go Authors.  All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package bug0
+
+type T0 struct {
+}
+
+export v0
+var v0 T0
