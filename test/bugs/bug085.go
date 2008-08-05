044a3b1a5e87b2750ca1ec553cc60c12b5601890
Robert Griesemer <gri@golang.org>
2008-08-05 16:09:49 -0700

    bug: package name not visible
    R=r
    OCL=13898
    CL=13898

---
+++ test/bugs/bug085.go
@@ -0,0 +1,27 @@
+// errchk $G $D/$F.go
+
+// Copyright 2009 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package P
+
+var x int
+
+func foo() {
+	print P.x;  // P should be defined between the outermost "universe" scope and the global scope
+}
+
+/*
+uetli:~/Source/go1/test/bugs gri$ 6g bug085.go 
+bug085.go:6: P: undefined
+Bus error
+*/
+
+/* expected scope hierarchy (outermost to innermost)
+
+universe scope (contains predeclared identifiers int, float, int32, len, etc.)
+"solar" scope (just holds the package name P so it can be found but doesn't conflict)
+global scope (the package global scope)
+local scopes (function scopes)
+*/
