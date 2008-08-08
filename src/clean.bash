83e7926827ebff5662155c8cc4066e8b5bf34cfb
Rob Pike <r@golang.org>
2008-08-07 17:36:50 -0700

    include syscall in default build fix an issue with autolib names by compiling to target location print a bit more when compiling
    R=gri
    OCL=13988
    CL=13988

--- src/clean.bash
+++ src/clean.bash
@@ -3,7 +3,7 @@
 # Use of this source code is governed by a BSD-style
 # license that can be found in the LICENSE file.
 
-for i in lib9 libbio libmach_amd64
+for i in lib9 libbio libmach_amd64 syscall
 do
 	cd $i
 	make clean
