7924b3862d8abc20f11315e039ada34736e8bbbe
Robert Griesemer <gri@golang.org>
2008-08-08 16:12:34 -0700

    - removed misleading comment in bug041.go - removed bug042.go - not a bug according to updated spec
    R=r
    DELTA=43  (5 added, 36 deleted, 2 changed)
    OCL=14008
    CL=14010

--- test/bugs/bug041.go
+++ test/bugs/bug041.go
@@ -13,11 +13,3 @@ type S struct {
 func main() {
   var s S;
 }
-
-/*
-Another problem with implicit forward declarations (as in this program on line 6)
-is that it is not clear in which scope the type (here "T") should be declared.
-This is the main reason why we should not allow implicit forward declarations at all,
-and instead have an explicit type forward declaration. For more on this subject
-see bug042.go.
-*/
