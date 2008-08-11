1e0b17b24c682cd856d2bb74062f3857ee5787c2
Robert Griesemer <gri@golang.org>
2008-08-11 11:40:19 -0700

    - added test case for compiler bug re: exported variables of function pointer type
    R=r
    OCL=14086
    CL=14086

---
+++ test/bugs/bug088.go
@@ -0,0 +1,6 @@
+// $G $D/$F.dir/bug0.go && $G $D/$F.dir/bug1.go || echo BUG: fails incorrectly
+
+// Copyright 2009 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
