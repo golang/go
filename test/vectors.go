4169d6185acceb4226cd34c0f26afa94c605862e
Robert Griesemer <gri@golang.org>
2008-08-29 11:10:23 -0700

    - updated and cleaned up vector.go to use new array instructions - added initial test cases (needs to be expanded)
    R=r
    DELTA=135  (84 added, 30 deleted, 21 changed)
    OCL=14654
    CL=14654

---
+++ test/vectors.go
@@ -0,0 +1,61 @@
+// $G $F.go && $L $F.$A && ./$A.out
+
+// Copyright 2009 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+import vector "vector"
+
+
+type S struct {
+	val int
+}
+
+
+func (p *S) Init(val int) *S {
+	p.val = val;
+	return p;
+}
+
+
+func test0() {
+	v := vector.New();
+	if v.Len() != 0 {
+		panic("len = ", v.Len(), "\n");
+	}
+}
+
+
+func test1() {
+	var a [1000] *S;
+	for i := 0; i < len(a); i++ {
+		a[i] = new(S).Init(i);
+	}
+
+	v := vector.New();
+	for i := 0; i < len(a); i++ {
+		v.Insert(0, a[i]);
+		if v.Len() != i + 1 {
+			panic("len = ", v.Len(), "\n");
+		}
+	}
+
+	for i := 0; i < v.Len(); i++ {
+		x := convert(*S, v.At(i));
+		if x.val != v.Len() - i - 1 {
+			panic("expected ", i, ", found ", x.val, "\n");
+		}
+	}
+	
+	for v.Len() > 10 {
+		v.Delete(10);
+	}
+}
+
+
+func main() {
+	test0();
+	test1();
+}
