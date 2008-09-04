2bb5fbbf92a02d2457edd40d10ddd1d2594e74d6
Rob Pike <r@golang.org>
2008-09-04 16:48:19 -0700

    resolve that all unnecessary backslashes are rejected. bug068 is not a bug.
    R=gri
    OCL=14838
    CL=14838

--- test/fixedbugs/bug068.go
+++ test/fixedbugs/bug068.go
@@ -4,6 +4,8 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
+// RESOLUTION: This program is illegal.  We should reject all unnecessary backslashes.
+
 package main
 
 const c = '\'';  // this works
@@ -12,6 +14,6 @@ const s = "\'";  // this doesn't
 /*
 There is no reason why the escapes need to be different inside strings and chars.
 
-uetli:~/go/test/bugs gri$ 6g bug065.go
-bug065.go:6: unknown escape sequence: '
+uetli:~/go/test/bugs gri$ 6g bug068.go
+bug068.go:6: unknown escape sequence: '
 */
