7713599f95dd93274b779f0f74fe987ceb0e99c2
Rob Pike <r@golang.org>
2008-09-04 17:36:57 -0700

    update tests
    R=gri
    OCL=14847
    CL=14847

--- test/fixedbugs/bug029.go
+++ test/fixedbugs/bug029.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// $G $D/$F.go && echo BUG: known to succeed incorrectly
+// ! $G $D/$F.go
 
 package main
 
