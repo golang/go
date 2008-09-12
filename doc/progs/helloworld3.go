6693730e774177c8f3c069af3c890e93f16e119e
Rob Pike <r@golang.org>
2008-09-12 16:03:16 -0700

    next section: i/o package that has structs, methods
    R=gri
    DELTA=137  (134 added, 0 deleted, 3 changed)
    OCL=15251
    CL=15259

---
+++ doc/progs/helloworld3.go
@@ -0,0 +1,17 @@
+// Copyright 2009 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+import FD "fd"
+
+func main() {
+	hello := []byte{'h', 'e', 'l', 'l', 'o', ', ', ' ', 'w', 'o', 'r', 'l', 'd', '\n'};
+	FD.Stdout.Write(&hello);
+	fd,  errno := FD.Open("/does/not/exist",  0,  0);
+	if fd == nil {
+		print("can't open file; errno=",  errno,  "\n");
+		sys.exit(1);
+	}
+}
