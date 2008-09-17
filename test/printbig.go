68209ed5e323d48a63d7bbb718a277c6fb426b47
Russ Cox <rsc@golang.org>
2008-09-17 14:08:52 -0700

    fix printing of -(1<<63)
    R=r
    OCL=15441
    CL=15445

---
+++ test/printbig.go
@@ -0,0 +1,12 @@
+// $G $F.go && $L $F.$A && ./$A.out
+
+// Copyright 2009 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+func main() {
+	print(-(1<<63), "\n");
+	print((1<<63)-1, "\n")
+}
