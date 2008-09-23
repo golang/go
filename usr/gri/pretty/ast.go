c13c03c261ddd2c21a47b7b36754573b4e1f7c44
Robert Griesemer <gri@golang.org>
2008-09-22 18:26:12 -0700

    - first cut at building and printing AST
    R=r
    OCL=15675
    CL=15675

--- usr/gri/pretty/ast.go
+++ usr/gri/pretty/ast.go
@@ -4,36 +4,114 @@
 
 package AST;
 
+// ----------------------------------------------------------------------------
+// Lists
+
+export type Element interface {}
+
+
+export type List struct {
+	a *[] Element
+}
+
+
+func (p *List) len() int {
+	return len(p.a);
+}
+
+
+func (p *List) at(i int) Element {
+	return p.a[i];
+}
+
+
+func (p *List) Add (x Element) {
+	a := p.a;
+	n := len(a);
+
+	if n == cap(a) {
+		b := new([] interface {}, 2*n);
+		for i := 0; i < n; i++ {
+			b[i] = a[i];
+		}
+		a = b;
+	}
+
+	a = a[0 : n + 1];
+	a[n] = x;
+	p.a = a;
+}
+
+
+export func NewList() *List {
+	p := new(List);
+	p.a = new([] interface {}, 10);
+	return p;
+}
+
+
+// ----------------------------------------------------------------------------
+// Expressions
 
 export type Expr interface {
-       pos() int;
-       print();
 }
 
 
-export type Stat interface {
-       pos() int;
-       print();
+export type Selector struct {
+	pos int;
+	x Expr;
+	field string;
 }
 
 
-// ---------------------------------------------------------------------
-// Concrete nodes
-
-export type Ident struct {
-       pos_ int;
-       val_ string;
+export type Index struct {
+	pos int;
+	x Expr;
+	index Expr;
 }
 
 
-func (p *Ident) pos() int {
-     return p.pos_;
+export type Pair struct {
+	pos int;
+	x, y Expr;
 }
 
 
-func (p *Ident) print() {
-     print("x");  // TODO fix this
+export type Binary struct {
+	pos int;
+	tok int;
+	x, y Expr;
 }
 
 
-// TODO: complete this
+export type Unary struct {
+	pos int;
+	tok int;
+	x Expr;
+}
+
+
+export type Literal struct {
+	pos int;
+	tok int;
+	val string;
+}
+
+
+// ----------------------------------------------------------------------------
+// Statements
+
+
+// ----------------------------------------------------------------------------
+// Visitor
+
+export type Visitor interface {
+  DoBinary(x *Binary);
+  //DoUnary(x *Unary);
+  //DoLiteral(x *Literal);
+}
+
+
+func (x *Binary)  Visit(v Visitor)  { v.DoBinary(x); }
+//func (x *Unary)   Visit(v Visitor)  { v.DoUnary(x); }
+//func (x *Literal) Visit(v Visitor)  { v.DoLiteral(x); }
