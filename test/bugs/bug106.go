eeeaf6b931eb1f9a98619dadff3c92e84e972c67
Russ Cox <rsc@golang.org>
2008-09-25 16:32:39 -0700

    6g misparses negative constants in imports
    R=r
    DELTA=8  (8 added, 0 deleted, 0 changed)
    OCL=15893
    CL=15898

---
+++ test/bugs/bug106.go
@@ -0,0 +1,7 @@
+// $G $D/$F.dir/bug0.go && $G $D/$F.dir/bug1.go || echo BUG: failed to compile
+
+// Copyright 2009 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+ignored
