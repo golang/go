0eec78cdc50585206320f7f653317b0d6daaf60a
Rob Pike <r@golang.org>
2008-09-30 12:31:47 -0700

    104, 107, 108: they are known to fail, so mark them appropriately. update golden add the "goroutines" test
    R=iant
    DELTA=74  (36 added, 23 deleted, 15 changed)
    OCL=16194
    CL=16206

---
+++ test/chan/goroutines.go
@@ -0,0 +1,40 @@
+// $G $D/$F.go && $L $F.$A && ./$A.out
+
+// Copyright 2009 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// make a lot of goroutines, threaded together.
+// tear them down cleanly.
+
+package main
+
+import (
+	"strings";
+)
+
+func f(left, right *chan int) {
+	left <- <-right;
+}
+
+func main() {
+	var n = 10000;
+	if sys.argc() > 1 {
+		var ok bool;
+		n, ok = strings.atoi(sys.argv(1));
+		if !ok {
+			print("bad arg\n");
+			sys.exit(1);
+		}
+	}
+	leftmost := new(chan int);
+	right := leftmost;
+	left := leftmost;
+	for i := 0; i < n; i++ {
+		right = new(chan int);
+		go f(left, right);
+		left = right;
+	}
+	go func(c *chan int) { c <- 1 }(right);
+	<-leftmost;
+}
