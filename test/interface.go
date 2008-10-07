eb452f4b6ef68980e4f68e9c1a0c175334f659e5
Russ Cox <rsc@golang.org>
2008-10-07 12:36:39 -0700

    test that mutually recursive interfaces compile
    R=r
    DELTA=18  (18 added, 0 deleted, 0 changed)
    OCL=16639
    CL=16647

---
+++ test/interface.go
@@ -0,0 +1,22 @@
+// Copyright 2009 The Go Authors.  All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// $G $D/$F.go || echo BUG: should compile
+
+package main
+
+type I2 interface
+
+type I1 interface {
+	foo() I2
+}
+
+type I2 interface {
+	bar() I1
+}
+
+type T int
+func (t T) bar() I1;
+func (t T) foo() I2 { return t }
+func (t T) bar() I1 { return t }
