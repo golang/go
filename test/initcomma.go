3f9f0d637b367c08ea008d68dfd454ea0738263b
Russ Cox <rsc@golang.org>
2008-10-14 17:10:39 -0700

    allow trailing comma in braced initialized list
    R=ken
    OCL=17141
    CL=17143

---
+++ test/initcomma.go
@@ -0,0 +1,17 @@
+// $G $F.go && $L $F.$A && ./$A.out
+
+// Copyright 2009 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+var a = []int { 1, 2, }
+var b = [5]int { }
+var c = []int { 1 }
+
+func main() {
+	if len(a) != 2 { panicln("len a", len(a)) }
+	if len(b) != 5 { panicln("len b", len(b)) }
+	if len(c) != 1 { panicln("len a", len(a)) }
+}
