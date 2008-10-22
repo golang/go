dbabeb1d7adb51466b51cbe4a0d2474fe1d7af78
Russ Cox <rsc@golang.org>
2008-10-21 18:03:25 -0700

    reject new of function type
    R=ken
    OCL=17598
    CL=17598

---
+++ test/newfn.go
@@ -0,0 +1,14 @@
+// Copyright 2009 The Go Authors.  All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// errchk $G $D/$F.go
+
+package main
+
+func main()
+{
+	f := new(());	// ERROR "new"
+	g := new((x int, f float) string);	// ERROR "new"
+	h := new(*());	// ok
+}
