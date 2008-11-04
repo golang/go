7eddacee312699b534f6eb55ceb1907103608f1e
Russ Cox <rsc@golang.org>
2008-11-04 09:45:27 -0800

    move invalid method uses to new test
    R=iant
    DELTA=24  (13 added, 10 deleted, 1 changed)
    OCL=18424
    CL=18439

--- test/method.go
+++ test/method.go
@@ -10,8 +10,6 @@ type S string
 type S1 string
 type I int
 type I1 int
-type P *int
-type P1 *int
 type T struct { x int }
 type T1 T
 
@@ -19,8 +17,6 @@ func (s S) val() int { return 1 }
 func (s *S1) val() int { return 2 }
 func (i I) val() int { return 3 }
 func (i *I1) val() int { return 4 }
-func (p P) val() int { return 5 }
-func (p *P1) val() int { return 6 }
 //func (t T) val() int { return 7 }
 func (t *T1) val() int { return 8 }
 
@@ -37,8 +33,6 @@ func main() {
 	var ps *S1;
 	var i I;
 	var pi *I1;
-	var p P;
-	var pp *P1;
 	var t T;
 	var pt *T1;
 
@@ -46,8 +40,6 @@ func main() {
 	if ps.val() != 2 { panicln("ps.val:", ps.val()) }
 	if i.val() != 3 { panicln("i.val:", i.val()) }
 	if pi.val() != 4 { panicln("pi.val:", pi.val()) }
-	if p.val() != 5 { panicln("p.val:", p.val()) }
-	if pp.val() != 6 { panicln("pp.val:", pp.val()) }
 //	if t.val() != 7 { panicln("t.val:", t.val()) }
 	if pt.val() != 8 { panicln("pt.val:", pt.val()) }
 
@@ -55,8 +47,6 @@ func main() {
 	if val(ps) != 2 { panicln("ps.val:", val(ps)) }
 	if val(i) != 3 { panicln("i.val:", val(i)) }
 	if val(pi) != 4 { panicln("pi.val:", val(pi)) }
-	if val(p) != 5 { panicln("p.val:", val(p)) }
-	if val(pp) != 6 { panicln("pp.val:", val(pp)) }
 //	if val(t) != 7 { panicln("t.val:", val(t)) }
 	if val(pt) != 8 { panicln("pt.val:", val(pt)) }
 
