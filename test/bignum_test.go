e2eccf3bd07762ecbcae046a2e41e748f949f176
Robert Griesemer <gri@golang.org>
2008-11-04 13:55:18 -0800

    install bignum as library
    R=r
    DELTA=3501  (1752 added, 1749 deleted, 0 changed)
    OCL=18460
    CL=18471

--- test/bignum_test.go
+++ test/bignum_test.go
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
+// $G $D/$F.go && $L $F.$A && ./$A.out
+
 package main
 
 import Big "bignum"
