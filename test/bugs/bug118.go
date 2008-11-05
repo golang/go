1fbd9a1567b3820848cedcc45d43bca8d9573624
Ian Lance Taylor <iant@golang.org>
2008-11-05 15:27:40 -0800

    6g dumps core on this input file.
    R=ken,rsc
    DELTA=14  (14 added, 0 deleted, 0 changed)
    OCL=18555
    CL=18598

---
+++ test/bugs/bug118.go
@@ -0,0 +1,15 @@
+// $G $D/$F.go || echo BUG should compile
+
+// Copyright 2009 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+export func Send(c *chan int) int {
+  select {
+  default:
+    return 1;
+  }
+  return 2;
+}
