b1e8b5f5b715c9a4727bbfb1d32c852c7c8e9122
Ian Lance Taylor <iant@golang.org>
2008-11-17 12:19:02 -0800

    The scope rules have been clarified to indicate that a variable may only be named after the complete declaration, including the initialization statements.
    R=gri
    DELTA=61  (16 added, 45 deleted, 0 changed)
    OCL=19343
    CL=19376

---
+++ test/varinit.go
@@ -0,0 +1,20 @@
+// $G $D/$F.go && $L $F.$A && ./$A.out || echo BUG wrong result
+
+// Copyright 2009 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+func main() {
+   var x int = 1;
+   if x != 1 { panic("found ", x, ", expected 1\n"); }
+   {
+	   var x int = x + 1;
+	   if x != 2 { panic("found ", x, ", expected 2\n"); }
+   }
+   {
+	   x := x + 1;
+	   if x != 2 { panic("found ", x, ", expected 2\n"); }
+   }
+}
