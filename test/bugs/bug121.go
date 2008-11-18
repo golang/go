75fe9bda381b2818317b415a6d8888ba807c3456
Robert Griesemer <gri@golang.org>
2008-11-17 16:37:13 -0800

    - interface methods must have full function type
    R=r
    OCL=19410
    CL=19410

---
+++ test/bugs/bug121.go
@@ -0,0 +1,25 @@
+// errchk $G $D/$F.go
+
+// Copyright 2009 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+type T ()
+
+type I interface {
+	f, g ();
+	h T;  // should only allow FunctionType here
+}
+
+type S struct {
+}
+
+func (s *S) f() {}
+func (s *S) g() {}
+func (s *S) h() {}  // here we can't write (s *S) T either
+
+func main() {
+	var i I = new(S);
+}
