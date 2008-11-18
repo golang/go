1ed5c644e2930beea483489c67d881f817c13f1d
Russ Cox <rsc@golang.org>
2008-11-17 16:59:04 -0800

    add gobuild to build
    R=r
    DELTA=2  (0 added, 0 deleted, 2 changed)
    OCL=19404
    CL=19415

--- src/cmd/clean.bash
+++ src/cmd/clean.bash
@@ -3,7 +3,7 @@
 # Use of this source code is governed by a BSD-style
 # license that can be found in the LICENSE file.
 
-for i in 6l 6a 6c 6g gc cc ar db nm blyacc acid cov prof
+for i in cc 6l 6a 6c gc 6g ar db nm blyacc acid cov gobuild prof
 do
 	cd $i
 	make clean
