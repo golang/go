6b7dd4c0207f92b1398f3a1f3436424c182501e0
Russ Cox <rsc@golang.org>
2008-11-18 10:08:46 -0800

    add build step to install debuggers setgid procmod. they have to go in /usr/local/bin because the setgid bit is ignored in /Users.
    R=r
    DELTA=54  (49 added, 0 deleted, 5 changed)
    OCL=19424
    CL=19463

---
+++ src/sudo.bash
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Copyright 2009 The Go Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style
+# license that can be found in the LICENSE file.
+
+case "`uname`" in
+Darwin)
+	;;
+*)
+	exit 0
+esac
+
+for i in acid db prof cov
+do
+	sudo cp $GOROOT/src/cmd/$i/6$i /usr/local/bin/6$i
+	sudo chgrp procmod /usr/local/bin/6$i
+	sudo chmod g+s /usr/local/bin/6$i
+done
