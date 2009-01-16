9813be7b9a1b3fa88c2f4c93ebd56c7bc3513355
Rob Pike <r@golang.org>
2009-01-16 13:55:55 -0800

    s/split/Split/ to get compiling again
    R=rsc
    OCL=22967
    CL=22967

--- src/lib/strconv/fp_test.go
+++ src/lib/strconv/fp_test.go
@@ -27,7 +27,7 @@ func pow2(i int) float64 {
 // Wrapper around strconv.Atof64.  Handles dddddp+ddd (binary exponent)
 // itself, passes the rest on to strconv.Atof64.
 func myatof64(s string) (f float64, ok bool) {
-	a := strings.split(s, "p");
+	a := strings.Split(s, "p");
 	if len(a) == 2 {
 		n, err := strconv.Atoi64(a[0]);
 		if err != nil {
@@ -71,7 +71,7 @@ func myatof64(s string) (f float64, ok bool) {
 // Wrapper around strconv.Atof32.  Handles dddddp+ddd (binary exponent)
 // itself, passes the rest on to strconv.Atof32.
 func myatof32(s string) (f float32, ok bool) {
-	a := strings.split(s, "p");
+	a := strings.Split(s, "p");
 	if len(a) == 2 {
 		n, err := strconv.Atoi(a[0]);
 		if err != nil {
@@ -116,7 +116,7 @@ export func TestFp(t *testing.T) {
 		if len(line) == 0 || line[0] == '#' {
 			continue
 		}
-		a := strings.split(line, " ");
+		a := strings.Split(line, " ");
 		if len(a) != 4 {
 			t.Error("testfp.txt:", lineno, ": wrong field count\n");
 			continue;
