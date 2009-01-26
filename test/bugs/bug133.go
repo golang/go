ef94feed61d00a21bbd12503916f6fe9dcb942d4
Ian Lance Taylor <iant@golang.org>
2009-01-26 09:59:59 -0800

    Add a test for a case where 6g thinks that a field is visible when it should not be.  I couldn't get this any simpler; the error seems to have to do with the order of the imports in bug2.go.
    R=rsc
    DELTA=26  (26 added, 0 deleted, 0 changed)
    OCL=23450
    CL=23482

---
+++ test/bugs/bug133.go
@@ -0,0 +1,7 @@
+// $G $D/$F.dir/bug0.go && $G $D/$F.dir/bug1.go && errchk $G $D/$F.dir/bug2.go
+
+// Copyright 2009 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+ignored
