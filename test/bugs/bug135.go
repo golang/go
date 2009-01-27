fa615a3b303fdf10e9e3dcd21d372c0ed8e7351a
Rob Pike <r@golang.org>
2009-01-26 18:35:18 -0800

    f, ok := i.(Foo) does not compile if i already is equivalent to Foo
    R=rsc
    DELTA=18  (18 added, 0 deleted, 0 changed)
    OCL=23544
    CL=23547

---
+++ test/bugs/bug135.go
@@ -0,0 +1,18 @@
+// $G $D/$F.go || echo BUG: should compile
+
+// Copyright 2009 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+type Foo interface { }
+
+type T struct {}
+func (t *T) foo() {}
+
+func main() {
+  t := new(T);
+  var i interface {};
+  f, ok := i.(Foo);
+}
