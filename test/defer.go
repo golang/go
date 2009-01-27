b92285c3d6077db90519f4bf780198c54e9e87e2
Rob Pike <r@golang.org>
2009-01-27 15:08:08 -0800

    test for defer
    R=rsc
    DELTA=48  (48 added, 0 deleted, 0 changed)
    OCL=23624
    CL=23626

---
+++ test/defer.go
@@ -0,0 +1,52 @@
+// $G $F.go && $L $F.$A && ./$A.out
+
+// Copyright 2009 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+import "fmt"
+
+var result string
+
+func addInt(i int) {
+	result += fmt.Sprint(i)
+}
+
+func test1helper() {
+	for i := 0; i < 10; i++ {
+		defer addInt(i)
+	}
+}
+
+func test1() {
+	result = "";
+	test1helper();
+	if result != "9876543210" {
+		fmt.Printf("test1: bad defer result (should be 9876543210): %q\n", result);
+	}
+}
+
+func addDotDotDot(v ...) {
+	result += fmt.Sprint(v)
+}
+
+func test2helper() {
+	for i := 0; i < 10; i++ {
+		defer addDotDotDot(i)
+	}
+}
+
+func test2() {
+	result = "";
+	test2helper();
+	if result != "9876543210" {
+		fmt.Printf("test2: bad defer result (should be 9876543210): %q\n", result);
+	}
+}
+
+func main() {
+	test1();
+	test2();
+}
