4f49b88dda80a80ec1272447e3a2ca219b6c0436
Ken Thompson <ken@golang.org>
2009-01-29 15:13:36 -0800

    optimizer bug w STOstring
    R=r
    OCL=23820
    CL=23820

--- src/cmd/6c/peep.c
+++ src/cmd/6c/peep.c
@@ -377,6 +377,10 @@ subprop(Reg *r0)
 		case ACDQ:
 		case ACQO:
 
+		case ASTOSB:
+		case ASTOSL:
+		case ASTOSQ:
+		case AMOVSB:
 		case AMOVSL:
 		case AMOVSQ:
 			return 0;
@@ -755,11 +759,23 @@ copyu(Prog *p, Adr *v, Adr *s)
 			return 2;
 		goto caseread;
 
-	case AMOVSL:
-	case AMOVSQ:
 	case AREP:
 	case AREPN:
-		if(v->type == D_CX || v->type == D_DI || v->type == D_SI)
+		if(v->type == D_CX)
+			return 2;
+		goto caseread;
+
+	case AMOVSB:
+	case AMOVSL:
+	case AMOVSQ:
+		if(v->type == D_DI || v->type == D_SI)
+			return 2;
+		goto caseread;
+
+	case ASTOSB:
+	case ASTOSL:
+	case ASTOSQ:
+		if(v->type == D_AX || v->type == D_DI)
 			return 2;
 		goto caseread;
 
--- src/cmd/8c/peep.c
+++ src/cmd/8c/peep.c
@@ -305,6 +305,9 @@ subprop(Reg *r0)
 		case ACWD:
 		case ACDQ:
 
+		case ASTOSB:
+		case ASTOSL:
+		case AMOVSB:
 		case AMOVSL:
 		case AFSTSW:
 			return 0;
@@ -669,10 +672,21 @@ copyu(Prog *p, Adr *v, Adr *s)
 			return 2;
 		goto caseread;
 
-	case AMOVSL:
 	case AREP:
 	case AREPN:
-		if(v->type == D_CX || v->type == D_DI || v->type == D_SI)
+		if(v->type == D_CX)
+			return 2;
+		goto caseread;
+
+	case AMOVSB:
+	case AMOVSL:
+		if(v->type == D_DI || v->type == D_SI)
+			return 2;
+		goto caseread;
+
+	case ASTOSB:
+	case ASTOSL:
+		if(v->type == D_AX || v->type == D_DI)
 			return 2;
 		goto caseread;
 
