c5171ad929af895c31e1ec1dff06c302b53a62be
Rob Pike <r@golang.org>
2009-03-06 17:20:53 -0800

    document syscall.  all we do is redirect elsewhere.
    R=rsc
    DELTA=7  (7 added, 0 deleted, 0 changed)
    OCL=25859
    CL=25871

--- src/lib/syscall/syscall.go
+++ src/lib/syscall/syscall.go
@@ -2,6 +2,13 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
+// This package contains an interface to the low-level operating system
+// primitives.  The details vary depending on the underlying system.
+// Its primary use is inside other packages that provide a more portable
+// interface to the system, such as "os", "time" and "net".  Use those
+// packages rather than this one if you can.
+// For details of the functions and data types in this package consult
+// the manuals for the appropriate operating system.
 package syscall
 
 /*
