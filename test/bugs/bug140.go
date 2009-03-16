2427c89e69f1e6fd62be0331694036d2d3186b50
Robert Griesemer <gri@golang.org>
2009-03-16 11:21:58 -0700

    spec and implementation disagree with respect to label declarations
    R=rsc
    DELTA=19  (19 added, 0 deleted, 0 changed)
    OCL=26284
    CL=26336

---
+++ test/bugs/bug140.go
@@ -0,0 +1,18 @@
+// $G $D/$F.go || echo BUG should compile
+
+// Copyright 2009 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+func main() {
+	if {} else L: ;
+	if {} else L: main() ;
+}
+
+/*
+These should be legal according to the spec.
+bug140.go:6: syntax error near L
+bug140.go:7: syntax error near L
+*/
