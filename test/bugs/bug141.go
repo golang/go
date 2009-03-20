90943c8ee4cf2ccef11b72ba01ea729edd8b3c66
Ian Lance Taylor <iant@golang.org>
2009-03-20 16:30:54 -0700

    Test that interfaces work in type switches.
    R=ken,rsc
    DELTA=30  (30 added, 0 deleted, 0 changed)
    OCL=26599
    CL=26604

---
+++ test/bugs/bug141.go
@@ -0,0 +1,30 @@
+// $G $D/$F.go && $L $F.$A && ./$A.out || echo BUG: should run
+
+// Copyright 2009 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+type S struct { i int }
+func (p *S) Get() int { return p.i }
+
+type Empty interface {
+}
+
+type Getter interface {
+	Get() int;
+}
+
+func f1(p Empty) {
+	switch x := p.(type) {
+	default: println("failed to match interface"); sys.Exit(1);
+	case Getter: break;
+	}
+
+}
+
+func main() {
+	var s S;
+	f1(&s);
+}
