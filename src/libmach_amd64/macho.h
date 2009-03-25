a577a0ef33053713fbafaa3abe3172bedd7b6f25
Russ Cox <rsc@golang.org>
2009-03-24 18:04:50 -0700

    add .8 support to libmach_amd64 [sic]. add code to handle 32-bit Mach-O and ELF binaries.
    R=r
    DELTA=452  (365 added, 29 deleted, 58 changed)
    OCL=26696
    CL=26712

--- src/libmach_amd64/macho.h
+++ src/libmach_amd64/macho.h
@@ -25,6 +25,19 @@ typedef struct {
 typedef struct  {
 	MachCmd	cmd;
 	char		segname[16];	/* segment name */
+	uint32	vmaddr;		/* memory address of this segment */
+	uint32	vmsize;		/* memory size of this segment */
+	uint32	fileoff;	/* file offset of this segment */
+	uint32	filesize;	/* amount to map from the file */
+	uint32	maxprot;	/* maximum VM protection */
+	uint32	initprot;	/* initial VM protection */
+	uint32	nsects;		/* number of sections in segment */
+	uint32	flags;		/* flags */
+} MachSeg32; /* for 32-bit architectures */
+
+typedef struct  {
+	MachCmd	cmd;
+	char		segname[16];	/* segment name */
 	uvlong	vmaddr;		/* memory address of this segment */
 	uvlong	vmsize;		/* memory size of this segment */
 	uvlong	fileoff;	/* file offset of this segment */
@@ -44,6 +57,20 @@ typedef struct  {
 typedef struct  {
 	char		sectname[16];	/* name of this section */
 	char		segname[16];	/* segment this section goes in */
+	uint32	addr;		/* memory address of this section */
+	uint32	size;		/* size in bytes of this section */
+	uint32	offset;		/* file offset of this section */
+	uint32	align;		/* section alignment (power of 2) */
+	uint32	reloff;		/* file offset of relocation entries */
+	uint32	nreloc;		/* number of relocation entries */
+	uint32	flags;		/* flags (section type and attributes)*/
+	uint32	reserved1;	/* reserved (for offset or index) */
+	uint32	reserved2;	/* reserved (for count or sizeof) */
+} MachSect32; /* for 32-bit architectures */
+
+typedef struct  {
+	char		sectname[16];	/* name of this section */
+	char		segname[16];	/* segment this section goes in */
 	uvlong	addr;		/* memory address of this section */
 	uvlong	size;		/* size in bytes of this section */
 	uint32	offset;		/* file offset of this section */
@@ -58,12 +85,15 @@ typedef struct  {
 
 enum {
 	MACH_CPU_TYPE_X86_64 = (1<<24)|7,
+	MACH_CPU_TYPE_X86 = 7,
 	MACH_CPU_SUBTYPE_X86 = 3,
 	MACH_EXECUTABLE_TYPE = 2,
+	MACH_SEGMENT_32 = 1,	/* 32-bit mapped segment */
 	MACH_SEGMENT_64 = 0x19,	/* 64-bit mapped segment */
 	MACH_SYMSEG = 3,	/* obsolete gdb symtab, reused by go */
 	MACH_UNIXTHREAD = 0x5,	/* thread (for stack) */
 };
 
 
-#define	MACH_MAG		((0xcf<<24) | (0xfa<<16) | (0xed<<8) | 0xfe)
+#define	MACH64_MAG		((0xcf<<24) | (0xfa<<16) | (0xed<<8) | 0xfe)
+#define	MACH32_MAG		((0xce<<24) | (0xfa<<16) | (0xed<<8) | 0xfe)
