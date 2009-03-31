fa0a38bb7ce2fdb27370c703fd8bf07cba5f7a5d
Robert Griesemer <gri@golang.org>
2009-03-31 16:53:37 -0700

    1) Move parser.go into src/lib/go    - minor adjustments as suggested by rsc 2) Added parser_test fragment 3) Renamed some types in AST.go per rsc request
    R=rsc
    DELTA=2053  (2027 added, 0 deleted, 26 changed)
    OCL=26963
    CL=26971

---
+++ src/lib/go/parser_test.go
@@ -0,0 +1,47 @@
+// Copyright 2009 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package parser
+
+import (
+	"ast";
+	"os";
+	"parser";
+	"testing";
+)
+
+
+func TestParse0(t *testing.T) {
+	// test nil []bytes source
+	var src []byte;
+	prog, ok := Parse(src, nil, 0);
+	if ok {
+		t.Errorf("parse should have failed");
+	}
+}
+
+
+func TestParse1(t *testing.T) {
+	// test string source
+	src := `package main import "fmt" func main() { fmt.Println("Hello, World!") }`;
+	prog, ok := Parse(src, nil, 0);
+	if !ok {
+		t.Errorf("parse failed");
+	}
+}
+
+func TestParse2(t *testing.T) {
+	// test io.Read source
+	filename := "parser_test.go";
+	src, err := os.Open(filename, os.O_RDONLY, 0);
+	defer src.Close();
+	if err != nil {
+		t.Errorf("cannot open %s (%s)\n", filename, err.String());
+	}
+
+	prog, ok := Parse(src, nil, 0);
+	if !ok {
+		t.Errorf("parse failed");
+	}
+}
