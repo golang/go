f2f98b58e3b2fce70494c709421bc49dfd738642
Robert Griesemer <gri@golang.org>
2009-04-25 13:41:56 -0700

    cannot declare a variable in new scope with same name as type in outer scope (same as bug144.go but for types instead of constants)
    TBR=ken
    DELTA=17  (17 added, 0 deleted, 0 changed)
    OCL=27855
    CL=27855

---
+++ test/bugs/bug145.go
@@ -0,0 +1,17 @@
+// $G $D/$F.go || echo BUG should compile
+
+// Copyright 2009 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+type t int
+
+func main() {
+	t := 0
+}
+
+/*
+bug145.go:8: t is type, not var
+*/
