6fec52185a12715c711ef257086c6a7eb2ad1be7
Russ Cox <rsc@golang.org>
2009-05-08 16:40:55 -0700

    check for redeclaration of package name
    R=r
    DELTA=10  (10 added, 0 deleted, 0 changed)
    OCL=28585
    CL=28591

---
+++ test/import1.go
@@ -0,0 +1,14 @@
+// errchk $G -e $D/$F.go
+
+// Copyright 2009 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// check for import conflicts
+
+package main
+
+import (
+	"bufio";
+	bufio "os";	// ERROR "redeclaration"
+)
