12ea0da0dfcff4d601e29bb77467f5a8a06024c7
Russ Cox <rsc@golang.org>
2009-05-15 13:17:07 -0700

    bug152: literal []slice{ } as range expression
    R=ken
    OCL=28918
    CL=28918

---
+++ test/bugs/bug152.go
@@ -0,0 +1,17 @@
+// $G $D/$F.go && $L $F.$A && ./$A.out
+
+// Copyright 2009 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+func main() {
+	s := 0;
+	for i, v := range []int{1} {
+		s += v;
+	}
+	if s != 1 {
+		println("BUG: s =", s);
+	}
+}
