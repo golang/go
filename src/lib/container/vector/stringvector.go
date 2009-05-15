13fbb1d82e7e284d5545466358a7e470a5316c3b
Rob Pike <r@golang.org>
2009-05-15 11:52:58 -0700

    StringVector specialization of Vector
    R=gri,rsc
    DELTA=197  (194 added, 0 deleted, 3 changed)
    OCL=28900
    CL=28911

--- src/lib/container/vector/stringvector.go
+++ src/lib/container/vector/stringvector.go
@@ -6,50 +6,50 @@ package vector
 
 import "container/vector"
 
-// IntVector is a specialization of Vector that hides the wrapping of Elements around ints.
-type IntVector struct {
+// StringVector is a specialization of Vector that hides the wrapping of Elements around strings.
+type StringVector struct {
 	vector.Vector;
 }
 
 
 // Init initializes a new or resized vector.  The initial length may be <= 0 to
 // request a default length.  If initial_len is shorter than the current
-// length of the IntVector, trailing elements of the IntVector will be cleared.
-func (p *IntVector) Init(len int) *IntVector {
+// length of the StringVector, trailing elements of the StringVector will be cleared.
+func (p *StringVector) Init(len int) *StringVector {
 	p.Vector.Init(len);
 	return p;
 }
 
 
-// NewIntVector returns an initialized new IntVector with length at least len.
-func NewIntVector(len int) *IntVector {
-	return new(IntVector).Init(len)
+// NewStringVector returns an initialized new StringVector with length at least len.
+func NewStringVector(len int) *StringVector {
+	return new(StringVector).Init(len)
 }
 
 
 // At returns the i'th element of the vector.
-func (p *IntVector) At(i int) int {
-	return p.Vector.At(i).(int)
+func (p *StringVector) At(i int) string {
+	return p.Vector.At(i).(string)
 }
 
 
 // Set sets the i'th element of the vector to value x.
-func (p *IntVector) Set(i int, x int) {
+func (p *StringVector) Set(i int, x string) {
 	p.a[i] = x
 }
 
 
 // Last returns the element in the vector of highest index.
-func (p *IntVector) Last() int {
-	return p.Vector.Last().(int)
+func (p *StringVector) Last() string {
+	return p.Vector.Last().(string)
 }
 
 
 // Data returns all the elements as a slice.
-func (p *IntVector) Data() []int {
-	arr := make([]int, p.Len());
+func (p *StringVector) Data() []string {
+	arr := make([]string, p.Len());
 	for i, v := range p.a {
-		arr[i] = v.(int)
+		arr[i] = v.(string)
 	}
 	return arr
 }
@@ -57,62 +57,62 @@ func (p *IntVector) Data() []int {
 
 // Insert inserts into the vector an element of value x before
 // the current element at index i.
-func (p *IntVector) Insert(i int, x int) {
+func (p *StringVector) Insert(i int, x string) {
 	p.Vector.Insert(i, x)
 }
 
 
 // InsertVector inserts into the vector the contents of the Vector
 // x such that the 0th element of x appears at index i after insertion.
-func (p *IntVector) InsertVector(i int, x *IntVector) {
+func (p *StringVector) InsertVector(i int, x *StringVector) {
 	p.Vector.InsertVector(i, &x.Vector)
 }
 
 
-// Slice returns a new IntVector by slicing the old one to extract slice [i:j].
+// Slice returns a new StringVector by slicing the old one to extract slice [i:j].
 // The elements are copied. The original vector is unchanged.
-func (p *IntVector) Slice(i, j int) *IntVector {
-	return &IntVector{ *p.Vector.Slice(i, j) };
+func (p *StringVector) Slice(i, j int) *StringVector {
+	return &StringVector{ *p.Vector.Slice(i, j) };
 }
 
 
 // Push appends x to the end of the vector.
-func (p *IntVector) Push(x int) {
+func (p *StringVector) Push(x string) {
 	p.Vector.Push(x)
 }
 
 
 // Pop deletes and returns the last element of the vector.
-func (p *IntVector) Pop() int {
-	return p.Vector.Pop().(int)
+func (p *StringVector) Pop() string {
+	return p.Vector.Pop().(string)
 }
 
 
-// AppendVector appends the entire IntVector x to the end of this vector.
-func (p *IntVector) AppendVector(x *IntVector) {
+// AppendVector appends the entire StringVector x to the end of this vector.
+func (p *StringVector) AppendVector(x *StringVector) {
 	p.Vector.InsertVector(len(p.a), &x.Vector);
 }
 
 
 // SortInterface support
 // Less returns a boolean denoting whether the i'th element is less than the j'th element.
-func (p *IntVector) Less(i, j int) bool {
+func (p *StringVector) Less(i, j int) bool {
 	return p.At(i) < p.At(j)
 }
 
 
 // Iterate over all elements; driver for range
-func (p *IntVector) iterate(c chan int) {
+func (p *StringVector) iterate(c chan string) {
 	for i, v := range p.a {
-		c <- v.(int)
+		c <- v.(string)
 	}
 	close(c);
 }
 
 
 // Channel iterator for range.
-func (p *IntVector) Iter() chan int {
-	c := make(chan int);
+func (p *StringVector) Iter() chan string {
+	c := make(chan string);
 	go p.iterate(c);
 	return c;
 }
