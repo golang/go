6b3350baabeceee52f5b5db0ab29e1b56864af4a
David Symonds <dsymonds@golang.org>
2009-05-18 18:42:47 -0700

    Create dependencies automatically for top-level lib directory.
    It caught a few missing dependencies (bufio/fmt -> utf8, fmt -> os, http -> strconv, etc.).
    
    R=r,rsc
    APPROVED=r
    DELTA=126  (79 added, 45 deleted, 2 changed)
    OCL=28983
    CL=29014

---
+++ src/lib/deps.bash
@@ -0,0 +1,31 @@
+#!/bin/bash
+# Copyright 2009 The Go Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style
+# license that can be found in the LICENSE file.
+
+OUT="Make.deps"
+TMP="Make.deps.tmp"
+
+# Get list of directories from Makefile
+dirs=$(sed '1,/^DIRS=/d; /^$/,$d; s/\\//g' Makefile)
+dirpat=$(echo $dirs | sed 's/ /|/g; s/.*/^(&)$/')
+
+for dir in $dirs; do (
+	cd $dir || exit 1
+
+	sources=$(sed -n 's/\.\$O\\/.go/p' Makefile | sort | uniq)
+	sources=$(ls $sources 2> /dev/null)  # remove .s, .c, etc.
+
+	deps=$(
+		sed -n '/^import.*"/p; /^import[ \t]*(/,/^)/p' $sources /dev/null |
+		cut -d '"' -f2 |
+		egrep "$dirpat" |
+		grep -v "^$dir\$" |
+		sed 's/$/.install/' |
+		sort -u
+	)
+
+	echo $dir.install: $deps
+) done > $TMP
+
+mv $TMP $OUT
