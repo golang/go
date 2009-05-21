8b6b3806059aa7d74322cd0f59c9a3fb4f9919dd
Russ Cox <rsc@golang.org>
2009-05-21 14:06:24 -0700

    stricter rules for assignment. when assigning a multifield object (structs or arrays of structs) they must not contain any fields that could not be assigned individually.
    R=ken
    OCL=29192
    CL=29194

---
+++ test/assign.go
@@ -0,0 +1,33 @@
+// errchk $G $D/$F.go
+
+// Copyright 2009 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+import "sync"
+
+type T struct {
+	int;
+	sync.Mutex;
+}
+
+func main() {
+	{
+		var x, y sync.Mutex;
+		x = y;	// ERROR "assignment.*Mutex"
+	}
+	{
+		var x, y T;
+		x = y;	// ERROR "assignment.*Mutex"
+	}
+	{
+		var x, y [2]sync.Mutex;
+		x = y;	// ERROR "assignment.*Mutex"
+	}
+	{
+		var x, y [2]T;
+		x = y;	// ERROR "assignment.*Mutex"
+	}
+}
