afeb4230af7ce5dce910b58f09029d621055e1b1
Russ Cox <rsc@golang.org>
2009-05-21 13:46:20 -0700

    add test for yesterday's interface rule change (interface/convert1.go). move interface tests to subdirectory.
    R=r
    DELTA=1632  (827 added, 804 deleted, 1 changed)
    OCL=29181
    CL=29191

--- test/interface/receiver1.go
+++ test/interface/receiver1.go
@@ -1,8 +1,8 @@
-// Copyright 2009 The Go Authors.  All rights reserved.
+// errchk $G $D/$F.go
+
+// Copyright 2009 The Go Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
-
-// errchk $G $F.go
 
 // Error messages about missing implicit methods.
 
