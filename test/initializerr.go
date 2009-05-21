a03b39f481aa68c110619123f7f15c9915f03f35
Russ Cox <rsc@golang.org>
2009-05-21 14:46:13 -0700

    tests for new initializer syntax
    R=r
    DELTA=79  (79 added, 0 deleted, 0 changed)
    OCL=29196
    CL=29202

---
+++ test/initializerr.go
@@ -0,0 +1,25 @@
+// errchk $G -e $D/$F.go
+
+// Copyright 2009 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+type S struct {
+	A, B, C, X, Y, Z int;
+}
+
+type T struct {
+	S;
+}
+
+var x = 1
+var a1 = S { 0, X: 1 };	// ERROR "mixture"
+var a2 = S { Y: 3, Z: 2, Y: 3 }; // ERROR "duplicate"
+var a3 = T { 1, 2, 3, 4, 5, 6 };	// ERROR "convert"
+var a4 = [5]byte{ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 }	// ERROR "index"
+var a5 = []byte { x: 2 }	// ERROR "index"
+
+var ok1 = S { }	// should be ok
+var ok2 = T { S: a4 }	// should be ok
