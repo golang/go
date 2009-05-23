224b89cafabbaa993b6883ab4db39e6e07a06beb
Russ Cox <rsc@golang.org>
2009-05-22 23:30:31 -0700

    typo in hmac comment
    R=r
    DELTA=1  (0 added, 0 deleted, 1 changed)
    OCL=29307
    CL=29307

--- src/lib/crypto/hmac/hmac.go
+++ src/lib/crypto/hmac/hmac.go
@@ -4,7 +4,7 @@
 
 // The hmac package implements the Keyed-Hash Message Authentication Code (HMAC)
 // as defined in U.S. Federal Information Processing Standards Publication 198.
-// An HMAC is a cryptographic hash attesting that uses a key to sign a message.
+// An HMAC is a cryptographic hash that uses a key to sign a message.
 // The receiver verifies the hash by recomputing it using the same key.
 package hmac
 
