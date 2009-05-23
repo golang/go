b112d42ad60551a075d0bf4c06667f11e7aa976d
Russ Cox <rsc@golang.org>
2009-05-22 22:40:26 -0700

    add hmac.NewSHA1 and hmac.NewMD5. fix Reset bug in sha1. add hmac, sha1, md5 tests. document hmac.
    R=r
    DELTA=146  (111 added, 6 deleted, 29 changed)
    OCL=29294
    CL=29299

--- src/lib/crypto/hmac/hmac.go
+++ src/lib/crypto/hmac/hmac.go
@@ -2,22 +2,34 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// TODO(rsc): comments
-
+// The hmac package implements the Keyed-Hash Message Authentication Code (HMAC)
+// as defined in U.S. Federal Information Processing Standards Publication 198.
+// An HMAC is a cryptographic hash attesting that uses a key to sign a message.
+// The receiver verifies the hash by recomputing it using the same key.
 package hmac
 
 import (
+	"crypto/md5";
+	"crypto/sha1";
 	"hash";
 	"os";
 )
 
-// k0 = key
-// ipad = 0x36 byte repeated to key len
-// opad = 0x5c byte repeated to key len
-// hmac = 
-//	H((k0 ^ opad) || H((k0 ^ ipad) || text))
+// FIPS 198:
+// http://csrc.nist.gov/publications/fips/fips198/fips-198a.pdf
+
+// key is zero padded to 64 bytes
+// ipad = 0x36 byte repeated to 64 bytes
+// opad = 0x5c byte repeated to 64 bytes
+// hmac = H([key ^ opad] H([key ^ ipad] text))
 
 const (
+	// NOTE(rsc): This constant is actually the
+	// underlying hash function's block size.
+	// HMAC is only conventionally used with
+	// MD5 and SHA1, and both use 64-byte blocks.
+	// The hash.Hash interface doesn't provide a
+	// way to find out the block size.
 	padSize = 64;
 )
 
@@ -35,11 +47,6 @@ func (h *hmac) tmpPad(xor byte) {
 	for i := len(h.key); i < padSize; i++ {
 		h.tmp[i] = xor;
 	}
-}
-
-func (h *hmac) init() {
-	h.tmpPad(0x36);
-	h.inner.Write(h.tmp[0:padSize]);
 }
 
 func (h *hmac) Sum() []byte {
@@ -63,10 +70,17 @@ func (h *hmac) Size() int {
 
 func (h *hmac) Reset() {
 	h.inner.Reset();
-	h.init();
+	h.tmpPad(0x36);
+	h.inner.Write(h.tmp[0:padSize]);
 }
 
-func HMAC(h hash.Hash, key []byte) hash.Hash {
+// New returns a new HMAC hash using the given hash and key.
+func New(h hash.Hash, key []byte) hash.Hash {
+	if len(key) > padSize {
+		// If key is too big, hash it.
+		h.Write(key);
+		key = h.Sum();
+	}
 	hm := new(hmac);
 	hm.inner = h;
 	hm.size = h.Size();
@@ -75,6 +89,16 @@ func HMAC(h hash.Hash, key []byte) hash.Hash {
 		hm.key[i] = k;
 	}
 	hm.tmp = make([]byte, padSize + hm.size);
-	hm.init();
+	hm.Reset();
 	return hm;
+}
+
+// NewMD5 returns a new HMAC-MD5 hash using the given key.
+func NewMD5(key []byte) hash.Hash {
+	return New(md5.New(), key);
+}
+
+// NewSHA1 returns a new HMAC-SHA1 hash using the given key.
+func NewSHA1(key []byte) hash.Hash {
+	return New(sha1.New(), key);
 }
