cc7c31baa4fb6ccc074287bb24e98cbb8b219a6d
Russ Cox <rsc@golang.org>
2009-05-29 13:46:43 -0700

    64-bit integer math bugs
    R=r
    DELTA=46  (46 added, 0 deleted, 0 changed)
    OCL=29569
    CL=29614

---
+++ test/bugs/bug155.go
@@ -0,0 +1,23 @@
+// $G $D/$F.go && $L $F.$A || echo BUG: bug155
+
+// Copyright 2009 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+const big uint64 = 1<<63
+
+func f(a uint64) uint64 {
+	return a << big
+}
+
+func main() {
+	f(1)
+}
+
+/*
+main·f: doasm: notfound from=75 to=13 (82)    SHLQ    $-9223372036854775808,BX
+main·f: doasm: notfound from=75 to=13 (82)    SHLQ    $-9223372036854775808,BX
+main·f: doasm: notfound from=75 to=13 (82)    SHLQ    $-9223372036854775808,BX
+*/
