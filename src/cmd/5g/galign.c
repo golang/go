fe71909380de24d00b857cba01e80662d114075b
Kai Backman <kaib@golang.org>
2009-06-04 21:11:55 -0700

    Rolling galign back to 32 bit.
    R=rsc
    APPROVED=rsc
    DELTA=3  (0 added, 0 deleted, 3 changed)
    OCL=29532
    CL=29917

--- src/cmd/5g/galign.c
+++ src/cmd/5g/galign.c
@@ -16,7 +16,7 @@ Typedef	typedefs[] =
 {
 	"int",		TINT,		TINT32,
 	"uint",		TUINT,		TUINT32,
-	"uintptr",	TUINTPTR,	TUINT64,
+	"uintptr",	TUINTPTR,	TUINT32,
 	"float",	TFLOAT,		TFLOAT32,
 	0
 };
@@ -24,8 +24,8 @@ Typedef	typedefs[] =
 void
 betypeinit(void)
 {
-	maxround = 8;
-	widthptr = 8;
+	maxround = 4;
+	widthptr = 4;
 
 	zprog.link = P;
 	zprog.as = AGOK;
