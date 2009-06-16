d45442ed65d8306cc6d8faddc931f03fd32fd52a
Robert Griesemer <gri@golang.org>
2009-06-16 09:39:57 -0700

    fix build
    R=rsc
    DELTA=5  (0 added, 1 deleted, 4 changed)
    OCL=30343
    CL=30343

--- src/make.bash
+++ src/make.bash
@@ -18,8 +18,8 @@ rm -f $HOME/bin/quietgcc
 cp quietgcc.bash $HOME/bin/quietgcc
 chmod +x $HOME/bin/quietgcc
 
-for i in lib9 libbio libmach_amd64 libregexp cmd pkg cmd/gobuild
-do
+for i in lib9 libbio libmach_amd64 libregexp cmd pkg cmd/gobuild cmd/godoc
+do (
 	echo; echo; echo %%%% making $i %%%%; echo
 	cd $i
 	case $i in
@@ -29,8 +29,7 @@ do
 	*)
 		make install
 	esac
-	cd ..
-done
+) done
 
 case "`uname`" in
 Darwin)
