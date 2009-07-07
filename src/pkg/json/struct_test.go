2f81890e4e97176b82d345c0ae210ccd9f2a6233
Russ Cox <rsc@golang.org>
2009-07-07 11:04:18 -0700

    json: use new reflect interface (CL 31107)
    R=r
    DELTA=192  (52 added, 43 deleted, 97 changed)
    OCL=31116
    CL=31286

--- src/pkg/json/struct_test.go
+++ src/pkg/json/struct_test.go
@@ -10,24 +10,24 @@ import (
 )
 
 type _MyStruct struct {
-	t bool;
-	f bool;
-	s string;
-	i8 int8;
-	i16 int16;
-	i32 int32;
-	i64 int64;
-	u8 uint8;
-	u16 uint16;
-	u32 uint32;
-	u64 uint64;
-	i int;
-	u uint;
-	fl float;
-	fl32 float32;
-	fl64 float64;
-	a []string;
-	my *_MyStruct;
+	T bool;
+	F bool;
+	S string;
+	I8 int8;
+	I16 int16;
+	I32 int32;
+	I64 int64;
+	U8 uint8;
+	U16 uint16;
+	U32 uint32;
+	U64 uint64;
+	I int;
+	U uint;
+	Fl float;
+	Fl32 float32;
+	Fl64 float64;
+	A []string;
+	My *_MyStruct;
 };
 
 const _Encoded =
@@ -48,35 +48,35 @@ func _Check(t *testing.T, ok bool, name string, v interface{}) {
 
 func TestUnmarshal(t *testing.T) {
 	var m _MyStruct;
-	m.f = true;
+	m.F = true;
 	ok, errtok := Unmarshal(_Encoded, &m);
 	if !ok {
 		t.Fatalf("Unmarshal failed near %s", errtok);
 	}
-	_Check(t, m.t==true, "t", m.t);
-	_Check(t, m.f==false, "f", m.f);
-	_Check(t, m.s=="abc", "s", m.s);
-	_Check(t, m.i8==1, "i8", m.i8);
-	_Check(t, m.i16==2, "i16", m.i16);
-	_Check(t, m.i32==3, "i32", m.i32);
-	_Check(t, m.i64==4, "i64", m.i64);
-	_Check(t, m.u8==5, "u8", m.u8);
-	_Check(t, m.u16==6, "u16", m.u16);
-	_Check(t, m.u32==7, "u32", m.u32);
-	_Check(t, m.u64==8, "u64", m.u64);
-	_Check(t, m.i==-9, "i", m.i);
-	_Check(t, m.u==10, "u", m.u);
-	_Check(t, m.fl==11.5, "fl", m.fl);
-	_Check(t, m.fl32==12.25, "fl32", m.fl32);
-	_Check(t, m.fl64==13.75, "fl64", m.fl64);
-	_Check(t, m.a!=nil, "a", m.a);
-	if m.a != nil {
-		_Check(t, m.a[0]=="x", "a[0]", m.a[0]);
-		_Check(t, m.a[1]=="y", "a[1]", m.a[1]);
-		_Check(t, m.a[2]=="z", "a[2]", m.a[2]);
+	_Check(t, m.T==true, "t", m.T);
+	_Check(t, m.F==false, "f", m.F);
+	_Check(t, m.S=="abc", "s", m.S);
+	_Check(t, m.I8==1, "i8", m.I8);
+	_Check(t, m.I16==2, "i16", m.I16);
+	_Check(t, m.I32==3, "i32", m.I32);
+	_Check(t, m.I64==4, "i64", m.I64);
+	_Check(t, m.U8==5, "u8", m.U8);
+	_Check(t, m.U16==6, "u16", m.U16);
+	_Check(t, m.U32==7, "u32", m.U32);
+	_Check(t, m.U64==8, "u64", m.U64);
+	_Check(t, m.I==-9, "i", m.I);
+	_Check(t, m.U==10, "u", m.U);
+	_Check(t, m.Fl==11.5, "fl", m.Fl);
+	_Check(t, m.Fl32==12.25, "fl32", m.Fl32);
+	_Check(t, m.Fl64==13.75, "fl64", m.Fl64);
+	_Check(t, m.A!=nil, "a", m.A);
+	if m.A != nil {
+		_Check(t, m.A[0]=="x", "a[0]", m.A[0]);
+		_Check(t, m.A[1]=="y", "a[1]", m.A[1]);
+		_Check(t, m.A[2]=="z", "a[2]", m.A[2]);
 	}
-	_Check(t, m.my!=nil, "my", m.my);
-	if m.my != nil {
-		_Check(t, m.my.s=="subguy", "my.s", m.my.s);
+	_Check(t, m.My!=nil, "my", m.My);
+	if m.My != nil {
+		_Check(t, m.My.S=="subguy", "my.s", m.My.S);
 	}
 }
