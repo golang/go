ff9e657fb33bb7fe6f8b5e8922925d2ac5026372
Robert Griesemer <gri@golang.org>
2009-07-08 14:48:09 -0700

    WriteFile util function
    R=rsc
    DELTA=41  (41 added, 0 deleted, 0 changed)
    OCL=31349
    CL=31358

--- src/pkg/io/utils_test.go
+++ src/pkg/io/utils_test.go
@@ -7,6 +7,7 @@ package io
 import (
 	"io";
 	"os";
+	"strings";
 	"testing";
 )
 
@@ -34,4 +35,28 @@ func TestReadFile(t *testing.T) {
 	}
 
 	checkSize(t, filename, uint64(len(contents)));
+}
+
+func TestWriteFile(t *testing.T) {
+	filename := "_obj/rumpelstilzchen";
+	data :=
+		"Programming today is a race between software engineers striving to "
+		"build bigger and better idiot-proof programs, and the Universe trying "
+		"to produce bigger and better idiots. So far, the Universe is winning.";
+
+	if err := WriteFile(filename, strings.Bytes(data), 0644); err != nil {
+		t.Fatalf("WriteFile %s: %v", filename, err);
+	}
+
+	contents, err := ReadFile(filename);
+	if err != nil {
+		t.Fatalf("ReadFile %s: %v", filename, err);
+	}
+
+	if string(contents) != data {
+		t.Fatalf("contents = %q\nexpected = %q", string(contents), data);
+	}
+
+	// cleanup
+	os.Remove(filename);  // ignore error
 }
