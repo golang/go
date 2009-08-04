5b1fb14371ea5d863301ee87207c8d19335b553e
Rob Pike <r@golang.org>
2009-08-04 14:27:46 -0700

    nbody benchmark timing.sh improvements
    R=rsc
    DELTA=334  (319 added, 0 deleted, 15 changed)
    OCL=32734
    CL=32736

--- test/bench/timing.sh
+++ test/bench/timing.sh
@@ -21,18 +21,42 @@ run() {
 	(/home/r/plan9/bin/time $* 2>&1 >/dev/null) |  sed 's/r.*/r/'
 }
 
-echo 'fasta -n 25000000'
-run 'gcc -O2 fasta.c' a.out 25000000
-#run 'gccgo -O2 fasta.go' a.out -n 25000000	#commented out until WriteString is in bufio
-run 'gc fasta' $O.out -n 25000000
-run 'gc_B fasta' $O.out -n 25000000
+fasta() {
+	echo 'fasta -n 25000000'
+	run 'gcc -O2 fasta.c' a.out 25000000
+	#run 'gccgo -O2 fasta.go' a.out -n 25000000	#commented out until WriteString is in bufio
+	run 'gc fasta' $O.out -n 25000000
+	run 'gc_B fasta' $O.out -n 25000000
+}
 
-echo
-6.out -n 25000000 > x
-echo 'reverse-complement < output-of-fasta-25000000'
-run 'gcc -O2 reverse-complement.c' a.out 25000000 < x
-run 'gccgo -O2 reverse-complement.go' a.out -n 25000000 < x
-run 'gc reverse-complement' $O.out -n 25000000 < x
-run 'gc_B reverse-complement' $O.out -n 25000000 < x
-rm x
+revcomp() {
+	6.out -n 25000000 > x
+	echo 'reverse-complement < output-of-fasta-25000000'
+	run 'gcc -O2 reverse-complement.c' a.out 25000000 < x
+	run 'gccgo -O2 reverse-complement.go' a.out -n 25000000 < x
+	run 'gc reverse-complement' $O.out -n 25000000 < x
+	run 'gc_B reverse-complement' $O.out -n 25000000 < x
+	rm x
+}
 
+nbody() {
+	echo 'nbody -n 50000000'
+	run 'gcc -O2 nbody.c' a.out 50000000
+	run 'gccgo -O2 nbody.go' a.out -n 50000000
+	run 'gc nbody' $O.out -n 50000000
+	run 'gc_B nbody' $O.out -n 50000000
+}
+
+case $# in
+0)
+	run="fasta revcom nbody"
+	;;
+*)
+	run=$*
+esac
+
+for i in $run
+do
+	$i
+	echo
+done
