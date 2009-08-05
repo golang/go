15cd0fd71130a0b5befef0a6fb5b5d7ff0b76b8d
Russ Cox <rsc@golang.org>
2009-08-05 00:11:24 -0700

    fix amd64 build
    R=ken
    OCL=32771
    CL=32771

--- src/cmd/6g/gobj.c
+++ src/cmd/6g/gobj.c
@@ -360,9 +360,9 @@ datastring(char *s, int len, Addr *a)
 	*a = ao;
 
 	// only generate data the first time.
-	if(ao.sym->uniq)
+	if(ao.sym->flags & SymUniq)
 		return;
-	ao.sym->uniq = 1;
+	ao.sym->flags |= SymUniq;
 
 	data();
 	for(w=0; w<len; w+=8) {
@@ -440,9 +440,9 @@ datagostring(Strlit *sval, Addr *a)
 	}
 
 	*a = ao;
-	if(ao.sym->uniq)
+	if(ao.sym->flags & SymUniq)
 		return;
-	ao.sym->uniq = 1;
+	ao.sym->flags |= SymUniq;
 
 	data();
 	// DATA gostring, wp, $cstring
