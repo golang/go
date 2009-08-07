aa7bd483893d7520c7265133baf45612a4672066
Russ Cox <rsc@golang.org>
2009-08-07 13:23:09 -0700

    bug183 - embedded vs non-embedded struct field in eqtype
    R=ken
    OCL=32888
    CL=32888

---
+++ test/fixedbugs/bug183.go
@@ -0,0 +1,25 @@
+//errchk $G $D/$F.go
+
+// Copyright 2009 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+type T int
+
+func f() {
+	var x struct { T };
+	var y struct { T T };
+	x = y	// ERROR "cannot"
+}
+
+type T1 struct { T }
+type T2 struct { T T }
+
+func g() {
+	var x T1;
+	var y T2;
+	x = y	// ERROR "cannot"
+}
+
