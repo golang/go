b5b8748987230e984cfa3f57103dd5be42de363d
Russ Cox <rsc@golang.org>
2009-08-12 14:41:02 -0700

    delete heuristic left over from incremental compilation
    R=r
    DELTA=31  (2 added, 28 deleted, 1 changed)
    OCL=33064
    CL=33112

--- src/cmd/6l/obj.c
+++ src/cmd/6l/obj.c
@@ -692,22 +692,9 @@ addlib(char *src, char *obj)
 			snprint(pname, sizeof pname, "%s/pkg/%s_%s/%s", goroot, goos, goarch, name);
 		strcpy(name, pname);
 	}
+	cleanname(name);
 	if(debug['v'])
 		Bprint(&bso, "%5.2f addlib: %s %s pulls in %s\n", cputime(), obj, src, name);
-
-	p = strrchr(src, '/');
-	q = strrchr(name, '/');
-	if(p != nil && q != nil && p - src == q - name && memcmp(src, name, p - src) == 0) {
-		// leading paths are the same.
-		// if the source file refers to an object in its own directory
-		// and we are inside an archive, ignore the reference, in the hope
-		// that the archive contains that object too.
-		if(strchr(obj, '(')) {
-			if(debug['v'])
-				Bprint(&bso, "%5.2f ignored srcdir object %s\n", cputime(), name);
-			return;
-		}
-	}
 
 	for(i=0; i<libraryp; i++)
 		if(strcmp(name, library[i]) == 0)
