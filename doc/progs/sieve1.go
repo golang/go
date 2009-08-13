61028773b1333166ca3ae7161ca6291a94213d95
Rob Pike <r@golang.org>
2009-08-13 08:54:26 -0700

    update tutorial
    R=rsc
    DELTA=30  (5 added, 0 deleted, 25 changed)
    OCL=33138
    CL=33170

--- doc/progs/sieve1.go
+++ doc/progs/sieve1.go
@@ -9,37 +9,37 @@ import "fmt"
 // Send the sequence 2, 3, 4, ... to returned channel 
 func generate() chan int {
 	ch := make(chan int);
-	go func(ch chan int){
+	go func(){
 		for i := 2; ; i++ {
 			ch <- i
 		}
-	}(ch);
+	}();
 	return ch;
 }
 
 // Filter out input values divisible by 'prime', send rest to returned channel
 func filter(in chan int, prime int) chan int {
 	out := make(chan int);
-	go func(in chan int, out chan int, prime int) {
+	go func() {
 		for {
 			if i := <-in; i % prime != 0 {
 				out <- i
 			}
 		}
-	}(in, out, prime);
+	}();
 	return out;
 }
 
 func sieve() chan int {
 	out := make(chan int);
-	go func(out chan int) {
+	go func() {
 		ch := generate();
 		for {
 			prime := <-ch;
 			out <- prime;
 			ch = filter(ch, prime);
 		}
-	}(out);
+	}();
 	return out;
 }
 
