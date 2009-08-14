6f911ccaa5a431bd4f06e759037e22046704df8b
Robert Griesemer <gri@golang.org>
2009-08-14 15:13:32 -0700

    6g bug: struct initializers must provide all field values unless the fields are named
    R=rsc,r
    DELTA=16  (16 added, 0 deleted, 0 changed)
    OCL=33292
    CL=33303

---
+++ test/bugs/bug189.go
@@ -0,0 +1,17 @@
+// errchk $G $D/$F.go
+
+// Copyright 2009 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+type S struct {
+	a, b int
+}
+
+func main() {
+	s1 := S{a: 7};	// ok - field is named
+	s3 := S{7, 11};	// ok - all fields have values
+	s2 := S{7};	// bug - should not compile since not all fields have values
+}
