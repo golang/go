250e21808c33f728bfdf59bc11ba0ceaf37ca1ae
Ian Lance Taylor <iant@golang.org>
2009-08-17 19:23:17 -0700

    Test that a type can refer to itself recursively as long as it does so in ways that permit its size to be known.
    R=rsc
    DELTA=35  (35 added, 0 deleted, 0 changed)
    OCL=33427
    CL=33433

---
+++ test/bugs/bug190.go
@@ -0,0 +1,26 @@
+// $G $D/$F.go || echo BUG: should compile
+
+// Copyright 2009 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+type S struct {
+	p *S;
+	s []S;
+	m map[int] S;
+	c chan S;
+	i interface { f(S); };
+	f func(S) S;
+}
+
+func main() {
+	var s S;
+	s.p = &s;
+	s.s = make([]S, 1);
+	s.s[0] = s;
+	s.m[0] = s;
+	s.c <- s;
+	s.i.f(s);
+}
