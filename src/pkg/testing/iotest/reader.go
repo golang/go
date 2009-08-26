02cdbb62c88f6a10107ef8e5f630aba73452b83e
Russ Cox <rsc@golang.org>
2009-08-26 16:23:34 -0700

    fix bufio bugs involving readers that return os.EOF with the last bit of data.
    add a new iotest.DataErrReader for tests
    
    R=r
    DELTA=93  (41 added, 31 deleted, 21 changed)
    OCL=33897
    CL=33915

--- src/pkg/testing/iotest/reader.go
+++ src/pkg/testing/iotest/reader.go
@@ -9,7 +9,14 @@ package iotest
 import (
 	"io";
 	"os";
+	"bytes";
 )
+
+// OneByteReader returns a Reader that implements
+// each non-empty Read by reading one byte from r.
+func OneByteReader(r io.Reader) io.Reader {
+	return &oneByteReader{r};
+}
 
 type oneByteReader struct {
 	r io.Reader;
@@ -22,10 +29,10 @@ func (r *oneByteReader) Read(p []byte) (int, os.Error) {
 	return r.r.Read(p[0:1]);
 }
 
-// OneByteReader returns a Reader that implements
-// each non-empty Read by reading one byte from r.
-func OneByteReader(r io.Reader) io.Reader {
-	return &oneByteReader{r};
+// HalfReader returns a Reader that implements Read
+// by reading half as many requested bytes from r.
+func HalfReader(r io.Reader) io.Reader {
+	return &halfReader{r};
 }
 
 type halfReader struct {
@@ -36,9 +43,35 @@ func (r *halfReader) Read(p []byte) (int, os.Error) {
 	return r.r.Read(p[0:(len(p)+1)/2]);
 }
 
-// HalfReader returns a Reader that implements Read
-// by reading half as many requested bytes from r.
-func HalfReader(r io.Reader) io.Reader {
-	return &halfReader{r};
+
+// DataErrReader returns a Reader that returns the final
+// error with the last data read, instead of by itself with
+// zero bytes of data.
+func DataErrReader(r io.Reader) io.Reader {
+	return &dataErrReader{r, nil, make([]byte, 1024)};
+}
+
+type dataErrReader struct {
+	r io.Reader;
+	unread []byte;
+	data []byte;
+}
+
+func (r *dataErrReader) Read(p []byte) (n int, err os.Error) {
+	// loop because first call needs two reads:
+	// one to get data and a second to look for an error.
+	for {
+		if len(r.unread) == 0 {
+			n1, err1 := r.r.Read(r.data);
+			r.unread = r.data[0:n1];
+			err = err1;
+		}
+		if n > 0 {
+			break;
+		}
+		n = bytes.Copy(p, r.unread);
+		r.unread = r.unread[n:len(r.unread)];
+	}
+	return;
 }
 
