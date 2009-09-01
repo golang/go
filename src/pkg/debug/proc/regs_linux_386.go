dd87082ab80059a0c4d04ca6605f15ce19ac1a89
Russ Cox <rsc@golang.org>
2009-09-01 11:51:05 -0700

    import debug/proc from usr/austin/ptrace
    R=austin
    DELTA=1892  (1892 added, 0 deleted, 0 changed)
    OCL=34183
    CL=34197

---
+++ src/pkg/debug/proc/regs_linux_386.go
@@ -0,0 +1,11 @@
+// Copyright 2009 The Go Authors.  All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package proc
+
+import "syscall"
+
+func newRegs(regs *syscall.PtraceRegs, setter func (*syscall.PtraceRegs) os.Error) Regs {
+	panic("newRegs unimplemented on linux/386");
+}
