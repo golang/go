249f20c68baf7d7b36649847a710a57024dbbef8
Russ Cox <rsc@golang.org>
2009-09-22 07:49:49 -0700

    nacl syscall package - machine generated files
    R=r
    DELTA=515  (515 added, 0 deleted, 0 changed)
    OCL=34881
    CL=34891

---
+++ src/pkg/syscall/ztypes_nacl.go
@@ -0,0 +1,9 @@
+// godefs -gsyscall -f-m32 -f-I/home/rsc/pub/nacl/native_client/src/third_party/nacl_sdk/linux/sdk/nacl-sdk/nacl/include -f-I/home/rsc/pub/nacl/native_client/src/trusted/service_runtime/include types_nacl.c
+
+// MACHINE GENERATED - DO NOT EDIT.
+
+package syscall
+
+// Constants
+
+// Types
