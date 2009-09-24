86b119f7659d948de8471f0dc4bd9e1c0a4a12b0
Russ Cox <rsc@golang.org>
2009-09-24 14:27:52 -0700

    install assembly math.Sqrt on amd64
    R=r
    DELTA=33  (32 added, 0 deleted, 1 changed)
    OCL=34983
    CL=34986

---
+++ src/pkg/math/sqrt_amd64.s
@@ -0,0 +1,10 @@
+// Copyright 2009 The Go Authors.  All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// func Sqrt(x float64) float64
+TEXT math·Sqrt(SB),7,$0
+	MOVSD x+0(FP), X0
+	SQRTSD X0, X0
+	MOVSD X0, r+8(FP)
+	RET
