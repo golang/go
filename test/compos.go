f2f970d8e42dcb973cd1926d2a5c1bb39be53d57
Russ Cox <rsc@golang.org>
2009-09-24 13:38:18 -0700

    missing files; cleaned up g4 nothave output. fixes 386 build.
    R=r
    DELTA=56  (56 added, 0 deleted, 0 changed)
    OCL=34979
    CL=34981

---
+++ test/compos.go
@@ -0,0 +1,23 @@
+// $G $D/$F.go && $L $F.$A && ./$A.out || echo BUG: compos
+
+// Copyright 2009 The Go Authors.  All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+type T struct {
+	int;
+}
+
+func f() *T {
+	return &T{1}
+}
+
+func main() {
+	x := f();
+	y := f();
+	if x == y {
+		panic("not allocating & composite literals");
+	}
+}
