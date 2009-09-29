d9662340a061c10864098fb93dfc296101fcc100
Ivan Krasin <krasin@golang.org>
2009-09-29 13:16:21 -0700

    Deflate encoder
    APPROVED=rsc
    OCL=34514
    CL=35093

---
+++ src/pkg/compress/flate/deflate.go
@@ -0,0 +1,443 @@
+// Copyright 2009 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package flate
+
+import (
+	"bytes";
+	"io";
+	"math";
+	"os";
+)
+
+const (
+	NoCompression = 0;
+	BestSpeed = 1;
+	fastCompression = 3;
+	BestCompression = 9;
+	DefaultCompression = -1;
+
+	logMaxOffsetSize = 15; // Standard DEFLATE
+	wideLogMaxOffsetSize = 22; // Wide DEFLATE
+	minMatchLength = 3; // The smallest match that the deflater looks for
+	maxMatchLength = 258; // The longest match for the deflater
+	minOffsetSize = 1; // The shortest offset that makes any sence
+
+	// The maximum number of tokens we put into a single flat block, just too
+	// stop things from getting too large.
+	maxFlateBlockTokens = 1 << 14;
+	maxStoreBlockSize = 65535;
+	hashBits = 15;
+	hashSize = 1 << hashBits;
+	hashMask = (1 << hashBits) - 1;
+	hashShift = (hashBits + minMatchLength - 1) / minMatchLength;
+)
+
+type syncPipeReader struct {
+	*io.PipeReader;
+	closeChan chan bool;
+}
+
+func (sr *syncPipeReader) CloseWithError(err os.Error) os.Error {
+	retErr := sr.PipeReader.CloseWithError(err);
+	sr.closeChan <- true; // finish writer close
+	return retErr;
+}
+
+type syncPipeWriter struct {
+	*io.PipeWriter;
+	closeChan chan bool;
+}
+
+type compressionLevel struct {
+	good, lazy, nice, chain, fastSkipHashing int;
+}
+
+var levels = [] compressionLevel {
+	compressionLevel {}, // 0
+	// For levels 1-3 we don't bother trying with lazy matches
+	compressionLevel { 3, 0, 8, 4, 4, },
+	compressionLevel { 3, 0, 16, 8, 5, },
+	compressionLevel { 3, 0, 32, 32, 6 },
+	// Levels 4-9 use increasingly more lazy matching
+	// and increasingly stringent conditions for "good enough".
+	compressionLevel { 4, 4, 16, 16, math.MaxInt32 },
+	compressionLevel { 8, 16, 32, 32, math.MaxInt32 },
+	compressionLevel { 8, 16, 128, 128, math.MaxInt32 },
+	compressionLevel { 8, 32, 128, 256, math.MaxInt32 },
+	compressionLevel { 32, 128, 258, 1024, math.MaxInt32 },
+	compressionLevel { 32, 258, 258, 4096, math.MaxInt32 },
+}
+
+func (sw *syncPipeWriter) Close() os.Error {
+	err := sw.PipeWriter.Close();
+	<-sw.closeChan; // wait for reader close
+	return err;
+}
+
+func syncPipe() (*syncPipeReader, *syncPipeWriter) {
+	r, w := io.Pipe();
+	sr := &syncPipeReader{r, make(chan bool, 1)};
+	sw := &syncPipeWriter{w, sr.closeChan};
+	return sr, sw;
+}
+
+type deflater struct {
+	level int;
+	logWindowSize uint;
+	w *huffmanBitWriter;
+	r io.Reader;
+	// (1 << logWindowSize) - 1.
+	windowMask int;
+
+	// hashHead[hashValue] contains the largest inputIndex with the specified hash value
+	hashHead []int;
+
+	// If hashHead[hashValue] is within the current window, then
+	// hashPrev[hashHead[hashValue] & windowMask] contains the previous index
+	// with the same hash value.
+	hashPrev []int;
+
+	// If we find a match of length >= niceMatch, then we don't bother searching
+	// any further.
+	niceMatch int;
+
+	// If we find a match of length >= goodMatch, we only do a half-hearted
+	// effort at doing lazy matching starting at the next character
+	goodMatch int;
+
+	// The maximum number of chains we look at when finding a match
+	maxChainLength int;
+
+	// The sliding window we use for matching
+	window []byte;
+
+	// The index just past the last valid character
+	windowEnd int;
+
+	// index in "window" at which current block starts
+	blockStart int;
+}
+
+func (d *deflater) flush() os.Error {
+	d.w.flush();
+	return d.w.err;
+}
+
+func (d *deflater) fillWindow(index int) (int, os.Error) {
+	wSize := d.windowMask + 1;
+	if index >= wSize + wSize - (minMatchLength + maxMatchLength) {
+		// shift the window by wSize
+		bytes.Copy(d.window, d.window[wSize:2*wSize]);
+		index -= wSize;
+		d.windowEnd -= wSize;
+		if d.blockStart >= wSize {
+			d.blockStart -= wSize;
+		} else {
+			d.blockStart = math.MaxInt32;
+		}
+		for i, h := range d.hashHead {
+			d.hashHead[i] = max(h - wSize, -1);
+		}
+		for i, h := range d.hashPrev {
+			d.hashPrev[i] = max(h - wSize, -1);
+		}
+	}
+	var count int;
+	var err os.Error;
+	count, err = io.ReadAtLeast(d.r, d.window[d.windowEnd : len(d.window)], 1);
+	d.windowEnd += count;
+	if err == os.EOF {
+		return index, nil;
+	}
+	return index, err;
+}
+
+func (d *deflater) writeBlock(tokens []token, index int, eof bool) os.Error {
+	if index > 0 || eof {
+		var window []byte;
+		if d.blockStart <= index {
+			window = d.window[d.blockStart:index];
+		}
+		d.blockStart = index;
+		d.w.writeBlock(tokens, eof, window);
+		return d.w.err;
+	}
+	return nil;
+}
+
+// Try to find a match starting at index whose length is greater than prevSize.
+// We only look at chainCount possibilities before giving up.
+func (d *deflater) findMatch(pos int, prevHead int, prevLength int, lookahead int) (length, offset int, ok bool) {
+	win := d.window[0:pos+min(maxMatchLength, lookahead)];
+
+	// We quit when we get a match that's at least nice long
+	nice := min(d.niceMatch, len(win) - pos);
+
+	// If we've got a match that's good enough, only look in 1/4 the chain.
+	tries := d.maxChainLength;
+	length = prevLength;
+	if length >= d.goodMatch {
+		tries >>= 2;
+	}
+
+	w0 := win[pos];
+	w1 := win[pos + 1];
+	wEnd := win[pos + length];
+	minIndex := pos - (d.windowMask + 1);
+
+	for i := prevHead; tries > 0; tries-- {
+		if w0 == win[i] && w1 == win[i+1] && wEnd == win[i+length] {
+			// The hash function ensures that if win[i] and win[i+1] match, win[i+2] matches
+
+			n := 3;
+			for pos + n < len(win) && win[i+n] == win[pos+n] {
+				n++;
+			}
+			if n > length && (n > 3 || pos-i <= 4096) {
+				length = n;
+				offset = pos - i;
+				ok = true;
+				if n >= nice {
+					// The match is good enough that we don't try to find a better one.
+					break;
+				}
+				wEnd = win[pos+n];
+			}
+		}
+		if i == minIndex {
+			// hashPrev[i & windowMask] has already been overwritten, so stop now.
+			break;
+		}
+		if i = d.hashPrev[i & d.windowMask]; i < minIndex || i < 0 {
+			break;
+		}
+	}
+	return;
+}
+
+func (d *deflater) writeStoredBlock(buf []byte) os.Error {
+	if d.w.writeStoredHeader(len(buf), false); d.w.err != nil {
+		return d.w.err;
+	}
+	d.w.writeBytes(buf);
+	return d.w.err;
+}
+
+func (d *deflater) storedDeflate() os.Error {
+	buf := make([]byte, maxStoreBlockSize);
+	for {
+		n, err := d.r.Read(buf);
+		if n > 0 {
+			if err := d.writeStoredBlock(buf[0:n]); err != nil {
+				return err;
+			}
+		}
+		if err != nil {
+			if err == os.EOF {
+				break;
+			}
+			return err;
+		}
+	}
+	return nil;
+}
+
+func (d *deflater) doDeflate() (err os.Error) {
+	// init
+	d.windowMask = 1<<d.logWindowSize - 1;
+	d.hashHead = make([]int, hashSize);
+	d.hashPrev = make([]int, 1 << d.logWindowSize);
+	d.window = make([]byte, 2 << d.logWindowSize);
+	fillInts(d.hashHead, -1);
+	tokens := make([]token, maxFlateBlockTokens, maxFlateBlockTokens + 1);
+	l := levels[d.level];
+	d.goodMatch = l.good;
+	d.niceMatch = l.nice;
+	d.maxChainLength = l.chain;
+	lazyMatch := l.lazy;
+	length := minMatchLength - 1;
+	offset := 0;
+	byteAvailable := false;
+	isFastDeflate := l.fastSkipHashing != 0;
+	index := 0;
+	// run
+	if index, err = d.fillWindow(index); err != nil {
+		return;
+	}
+	maxOffset := d.windowMask + 1; // (1 << logWindowSize);
+	// only need to change when you refill the window
+	windowEnd := d.windowEnd;
+	maxInsertIndex := windowEnd - (minMatchLength - 1);
+	ti := 0;
+
+	hash := int(0);
+	if index < maxInsertIndex {
+		hash = int(d.window[index])<<hashShift + int(d.window[index+1]);
+	}
+	chainHead := -1;
+	for {
+		if index > windowEnd {
+			panic("index > windowEnd");
+		}
+		lookahead := windowEnd - index;
+		if lookahead < minMatchLength + maxMatchLength {
+			if index, err = d.fillWindow(index); err != nil {
+				return;
+			}
+			windowEnd = d.windowEnd;
+			if index > windowEnd {
+				panic("index > windowEnd");
+			}
+			maxInsertIndex = windowEnd - (minMatchLength - 1);
+			lookahead = windowEnd - index;
+			if lookahead == 0 {
+				break;
+			}
+		}
+		if index < maxInsertIndex {
+			// Update the hash
+			hash = (hash<<hashShift + int(d.window[index+2])) & hashMask;
+			chainHead = d.hashHead[hash];
+			d.hashPrev[index & d.windowMask] = chainHead;
+			d.hashHead[hash] = index;
+		}
+		prevLength := length;
+		prevOffset := offset;
+		minIndex := max(index - maxOffset, 0);
+		length = minMatchLength - 1;
+		offset = 0;
+
+		if chainHead >= minIndex &&
+			(isFastDeflate && lookahead > minMatchLength - 1 ||
+			!isFastDeflate && lookahead > prevLength && prevLength < lazyMatch) {
+			if newLength, newOffset, ok := d.findMatch(index, chainHead, minMatchLength -1 , lookahead); ok {
+				length = newLength;
+				offset = newOffset;
+			}
+		}
+		if isFastDeflate && length >= minMatchLength ||
+			!isFastDeflate && prevLength >= minMatchLength && length <= prevLength {
+			// There was a match at the previous step, and the current match is
+			// not better. Output the previous match.
+			if isFastDeflate {
+				tokens[ti] = matchToken(uint32(length - minMatchLength), uint32(offset - minOffsetSize));
+			} else {
+				tokens[ti] = matchToken(uint32(prevLength - minMatchLength), uint32(prevOffset - minOffsetSize));
+			}
+			ti++;
+			// Insert in the hash table all strings up to the end of the match.
+			// index and index-1 are already inserted. If there is not enough
+			// lookahead, the last two strings are not inserted into the hash
+			// table.
+			if length <= l.fastSkipHashing {
+				var newIndex int;
+				if isFastDeflate {
+					newIndex = index + length;
+				} else {
+					newIndex = prevLength - 1;
+				}
+				for index++; index < newIndex; index++ {
+					if index < maxInsertIndex {
+						hash = (hash<<hashShift + int(d.window[index+2])) & hashMask;
+						// Get previous value with the same hash.
+						// Our chain should point to the previous value.
+						d.hashPrev[index & d.windowMask] = d.hashHead[hash];
+						// Set the head of the hash chain to us.
+						d.hashHead[hash] = index;
+					}
+				}
+				if !isFastDeflate {
+					byteAvailable = false;
+					length = minMatchLength - 1;
+				}
+			} else {
+				// For matches this long, we don't bother inserting each individual
+				// item into the table.
+				index += length;
+				hash = (int(d.window[index])<<hashShift + int(d.window[index+1]));
+			}
+			if ti == maxFlateBlockTokens {
+				// The block includes the current character
+				if err = d.writeBlock(tokens, index, false); err != nil {
+					return;
+				}
+				ti = 0;
+			}
+		} else {
+			if isFastDeflate || byteAvailable {
+				i := index - 1;
+				if isFastDeflate {
+					i = index;
+				}
+				tokens[ti] = literalToken(uint32(d.window[i]) & 0xFF);
+				ti++;
+				if ti == maxFlateBlockTokens {
+					if err = d.writeBlock(tokens, i+1, false); err != nil {
+						return;
+					}
+					ti = 0;
+				}
+			}
+			index++;
+			if !isFastDeflate {
+				byteAvailable = true;
+			}
+		}
+
+	}
+	if byteAvailable {
+		// There is still one pending token that needs to be flushed
+		tokens[ti] = literalToken(uint32(d.window[index - 1]) & 0xFF);
+		ti++;
+	}
+
+	if ti > 0 {
+		if err = d.writeBlock(tokens[0:ti], index, false); err != nil {
+			return;
+		}
+	}
+	return;
+}
+
+func (d *deflater) deflater(r io.Reader, w io.Writer, level int, logWindowSize uint) (err os.Error) {
+	d.r = r;
+	d.w = newHuffmanBitWriter(w);
+	d.level = level;
+	d.logWindowSize = logWindowSize;
+
+	switch {
+	case level == NoCompression:
+		err = d.storedDeflate();
+	case level == DefaultCompression:
+		d.level = 4;
+		fallthrough;
+	case 1 <= level && level <= 9:
+		err = d.doDeflate();
+	default:
+		return WrongValueError { "level", 0, 9, int32(level) };
+	}
+
+	if err != nil {
+		return err;
+	}
+	if d.w.writeStoredHeader(0, true); d.w.err != nil {
+		return d.w.err;
+	}
+	return d.flush();
+}
+
+func newDeflater(w io.Writer, level int, logWindowSize uint) io.WriteCloser {
+	var d deflater;
+	pr, pw := syncPipe();
+	go func() {
+		err := d.deflater(pr, w, level, logWindowSize);
+		pr.CloseWithError(err);
+	}();
+	return pw;
+}
+
+func NewDeflater(w io.Writer, level int) io.WriteCloser {
+	return newDeflater(w, level, logMaxOffsetSize);
+}
---
+++ src/pkg/compress/flate/huffman_bit_writer.go
@@ -0,0 +1,510 @@
+// Copyright 2009 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package flate
+
+import (
+	"io";
+	"math";
+	"os";
+	"strconv";
+)
+
+const (
+	// The largest offset code.
+	offsetCodeCount = 30;
+
+	// The largest offset code in the extensions.
+	extendedOffsetCodeCount = 42;
+
+	// The special code used to mark the end of a block.
+	endBlockMarker = 256;
+
+	// The first length code.
+	lengthCodesStart = 257;
+
+	// The number of codegen codes.
+	codegenCodeCount = 19;
+
+	badCode = 255;
+)
+
+// The number of extra bits needed by length code X - LENGTH_CODES_START.
+var lengthExtraBits = []int8 {
+	/* 257 */       0, 0, 0,
+	/* 260 */       0, 0, 0, 0, 0, 1, 1, 1, 1, 2,
+	/* 270 */       2, 2, 2, 3, 3, 3, 3, 4, 4, 4,
+	/* 280 */       4, 5, 5, 5, 5, 0,
+}
+
+// The length indicated by length code X - LENGTH_CODES_START.
+var lengthBase = []uint32 {
+	0,  1,  2,  3,   4,   5,   6,   7,   8,  10,
+	12, 14, 16, 20,  24,  28,  32,  40,  48, 56,
+	64, 80, 96, 112, 128, 160, 192, 224, 255
+}
+
+// offset code word extra bits.
+var offsetExtraBits = []int8 {
+	0,  0,  0,  0,  1,  1,  2,  2,  3,  3,
+	4,  4,  5,  5,  6,  6,  7,  7,  8,  8,
+	9,  9,  10, 10, 11, 11, 12, 12, 13, 13,
+	/* extended window */
+	14, 14, 15, 15, 16, 16, 17, 17, 18, 18, 19, 19, 20, 20,
+}
+
+var offsetBase = []uint32 {
+	/* normal deflate */
+	0x000000, 0x000001, 0x000002, 0x000003, 0x000004,
+	0x000006, 0x000008, 0x00000c, 0x000010, 0x000018,
+	0x000020, 0x000030, 0x000040, 0x000060, 0x000080,
+	0x0000c0, 0x000100, 0x000180, 0x000200, 0x000300,
+	0x000400, 0x000600, 0x000800, 0x000c00, 0x001000,
+	0x001800, 0x002000, 0x003000, 0x004000, 0x006000,
+
+	/* extended window */
+	0x008000, 0x00c000, 0x010000, 0x018000, 0x020000,
+	0x030000, 0x040000, 0x060000, 0x080000, 0x0c0000,
+	0x100000, 0x180000, 0x200000, 0x300000
+}
+
+// The odd order in which the codegen code sizes are written.
+var codegenOrder = []uint32 {
+	16, 17, 18, 0, 8, 7, 9, 6, 10, 5, 11, 4, 12, 3, 13, 2, 14, 1, 15
+}
+
+type huffmanBitWriter struct {
+	w io.Writer;
+	// Data waiting to be written is bytes[0:nbytes]
+	// and then the low nbits of bits.
+	bits uint32;
+	nbits uint32;
+	bytes [64]byte;
+	nbytes int;
+	literalFreq []int32;
+	offsetFreq []int32;
+	codegen []uint8;
+	codegenFreq []int32;
+	literalEncoding *huffmanEncoder;
+	offsetEncoding *huffmanEncoder;
+	codegenEncoding *huffmanEncoder;
+	err os.Error;
+}
+
+type WrongValueError struct {
+	name string;
+	from int32;
+	to int32;
+	value int32;
+}
+
+func newHuffmanBitWriter(w io.Writer) *huffmanBitWriter {
+	return &huffmanBitWriter{
+		w: w,
+		literalFreq: make([]int32, maxLit),
+		offsetFreq: make([]int32, extendedOffsetCodeCount),
+		codegen: make([]uint8, maxLit + extendedOffsetCodeCount + 1),
+		codegenFreq: make([]int32, codegenCodeCount),
+		literalEncoding: newHuffmanEncoder(maxLit),
+		offsetEncoding: newHuffmanEncoder(extendedOffsetCodeCount),
+		codegenEncoding: newHuffmanEncoder(codegenCodeCount),
+	};
+}
+
+func (err WrongValueError) String() string {
+	return "huffmanBitWriter: " + err.name + " should belong to [" + strconv.Itoa64(int64(err.from)) + ";" +
+		strconv.Itoa64(int64(err.to)) + "] but actual value is " + strconv.Itoa64(int64(err.value));
+}
+
+func (w *huffmanBitWriter) flushBits() {
+	if w.err != nil {
+		w.nbits = 0;
+		return;
+	}
+	bits := w.bits;
+	w.bits >>= 16;
+	w.nbits -= 16;
+	n := w.nbytes;
+	w.bytes[n] = byte(bits);
+	w.bytes[n+1] = byte(bits>>8);
+	if n += 2; n >= len(w.bytes) {
+		_, w.err = w.w.Write(&w.bytes);
+		n = 0;
+	}
+	w.nbytes = n;
+}
+
+func (w *huffmanBitWriter) flush() {
+	if w.err != nil {
+		w.nbits = 0;
+		return;
+	}
+	n := w.nbytes;
+	if w.nbits > 8 {
+		w.bytes[n] = byte(w.bits);
+		w.bits >>= 8;
+		w.nbits -= 8;
+		n++;
+	}
+	if w.nbits > 0 {
+		w.bytes[n] = byte(w.bits);
+		w.nbits = 0;
+		n++;
+	}
+	w.bits = 0;
+	_, w.err = w.w.Write(w.bytes[0:n]);
+	w.nbytes = 0;
+}
+
+func (w *huffmanBitWriter) writeBits(b, nb int32) {
+	w.bits |= uint32(b) << w.nbits;
+	if w.nbits += uint32(nb); w.nbits >= 16 {
+		w.flushBits();
+	}
+}
+
+func (w *huffmanBitWriter) writeBytes(bytes []byte) {
+	if w.err != nil {
+		return;
+	}
+	n := w.nbytes;
+	if w.nbits == 8 {
+		w.bytes[n] = byte(w.bits);
+		w.nbits = 0;
+		n++;
+	}
+	if w.nbits != 0 {
+        	w.err = InternalError("writeBytes with unfinished bits");
+		return;
+	}
+	if n != 0 {
+		_, w.err = w.w.Write(w.bytes[0:n]);
+		if w.err != nil {
+			return;
+		}
+	}
+	w.nbytes = 0;
+	_, w.err = w.w.Write(bytes);
+}
+
+// RFC 1951 3.2.7 specifies a special run-length encoding for specifiying
+// the literal and offset lengths arrays (which are concatenated into a single
+// array).  This method generates that run-length encoding.
+//
+// The result is written into the codegen array, and the frequencies
+// of each code is written into the codegenFreq array.
+// Codes 0-15 are single byte codes. Codes 16-18 are followed by additional
+// information.  Code badCode is an end marker
+//
+//  numLiterals      The number of literals in literalEncoding
+//  numOffsets       The number of offsets in offsetEncoding
+func (w *huffmanBitWriter) generateCodegen(numLiterals int, numOffsets int) {
+	fillInt32s(w.codegenFreq, 0);
+	// Note that we are using codegen both as a temporary variable for holding
+	// a copy of the frequencies, and as the place where we put the result.
+	// This is fine because the output is always shorter than the input used
+	// so far.
+	codegen := w.codegen; // cache
+	// Copy the concatenated code sizes to codegen.  Put a marker at the end.
+	copyUint8s(codegen[0 : numLiterals], w.literalEncoding.codeBits);
+	copyUint8s(codegen[numLiterals : numLiterals + numOffsets], w.offsetEncoding.codeBits);
+	codegen[numLiterals + numOffsets] = badCode;
+
+	size := codegen[0];
+	count := 1;
+	outIndex := 0;
+	for inIndex := 1; size != badCode; inIndex++ {
+		// INVARIANT: We have seen "count" copies of size that have not yet
+		// had output generated for them.
+		nextSize := codegen[inIndex];
+		if nextSize == size {
+			count++;
+			continue;
+		}
+		// We need to generate codegen indicating "count" of size.
+		if size != 0 {
+			codegen[outIndex] = size;
+			outIndex++;
+			w.codegenFreq[size]++;
+			count--;
+			for count >= 3 {
+				n := min(count, 6);
+				codegen[outIndex] = 16;
+				outIndex++;
+				codegen[outIndex] = uint8(n - 3);
+				outIndex++;
+				w.codegenFreq[16]++;
+				count -= n;
+			}
+		} else {
+			for count >= 11 {
+				n := min(count, 138);
+				codegen[outIndex] = 18;
+				outIndex++;
+				codegen[outIndex] = uint8(n - 11);
+				outIndex++;
+				w.codegenFreq[18]++;
+				count -= n;
+			}
+			if count >= 3 {
+				// count >= 3 && count <= 10
+				codegen[outIndex] = 17;
+				outIndex++;
+				codegen[outIndex] = uint8(count - 3);
+				outIndex++;
+				w.codegenFreq[17]++;
+				count = 0;
+			}
+		}
+		count--;
+		for ; count >= 0; count-- {
+			codegen[outIndex] = size;
+			outIndex++;
+			w.codegenFreq[size]++;
+		}
+		// Set up invariant for next time through the loop.
+		size = nextSize;
+		count = 1;
+	}
+	// Marker indicating the end of the codegen.
+	codegen[outIndex] = badCode;
+}
+
+func (w *huffmanBitWriter) writeCode(code *huffmanEncoder, literal uint32) {
+	if w.err != nil {
+		return;
+	}
+	w.writeBits(int32(code.code[literal]), int32(code.codeBits[literal]));
+}
+
+// Write the header of a dynamic Huffman block to the output stream.
+//
+//  numLiterals  The number of literals specified in codegen
+//  numOffsets   The number of offsets specified in codegen
+//  numCodegens  Tne number of codegens used in codegen
+func (w *huffmanBitWriter) writeDynamicHeader(numLiterals int, numOffsets int, numCodegens int, isEof bool) {
+	if w.err != nil {
+		return;
+	}
+	var firstBits int32 = 4;
+	if isEof {
+		firstBits = 5;
+	}
+	w.writeBits(firstBits, 3);
+	w.writeBits(int32(numLiterals - 257), 5);
+	if numOffsets > offsetCodeCount {
+		// Extended version of deflater
+		w.writeBits(int32(offsetCodeCount + ((numOffsets - (1 + offsetCodeCount)) >> 3)), 5);
+		w.writeBits(int32((numOffsets - (1 + offsetCodeCount)) & 0x7), 3);
+	} else {
+		w.writeBits(int32(numOffsets - 1), 5);
+	}
+	w.writeBits(int32(numCodegens - 4), 4);
+
+	for i := 0; i < numCodegens; i++ {
+		value := w.codegenEncoding.codeBits[codegenOrder[i]];
+		w.writeBits(int32(value), 3);
+	}
+
+	i := 0;
+	for {
+		var codeWord int = int(w.codegen[i]);
+		i++;
+		if codeWord == badCode {
+			break;
+		}
+		// The low byte contains the actual code to generate.
+		w.writeCode(w.codegenEncoding, uint32(codeWord));
+
+		switch codeWord {
+		case 16:
+			w.writeBits(int32(w.codegen[i]), 2);
+			i++;
+			break;
+		case 17:
+			w.writeBits(int32(w.codegen[i]), 3);
+			i++;
+			break;
+		case 18:
+			w.writeBits(int32(w.codegen[i]), 7);
+			i++;
+			break;
+		}
+	}
+}
+
+func (w *huffmanBitWriter) writeStoredHeader(length int, isEof bool) {
+	if w.err != nil {
+		return;
+	}
+	var flag int32;
+	if isEof {
+		flag = 1;
+	}
+	w.writeBits(flag, 3);
+	w.flush();
+	w.writeBits(int32(length), 16);
+	w.writeBits(int32(^uint16(length)), 16);
+}
+
+func (w *huffmanBitWriter) writeFixedHeader(isEof bool) {
+	if w.err != nil {
+		return;
+	}
+	// Indicate that we are a fixed Huffman block
+	var value int32 = 2;
+	if isEof {
+		value = 3;
+	}
+	w.writeBits(value, 3);
+}
+
+func (w *huffmanBitWriter) writeBlock(tokens []token, eof bool, input []byte) {
+	if w.err != nil {
+		return;
+	}
+	fillInt32s(w.literalFreq, 0);
+	fillInt32s(w.offsetFreq, 0);
+
+	n := len(tokens);
+	tokens = tokens[0:n+1];
+	tokens[n] = endBlockMarker;
+
+	totalLength := -1; // Subtract 1 for endBlock.
+	for _, t := range tokens {
+		switch t.typ() {
+		case literalType:
+			w.literalFreq[t.literal()]++;
+			totalLength++;
+			break;
+		case matchType:
+			length := t.length();
+			offset := t.offset();
+			totalLength += int(length + 3);
+			w.literalFreq[lengthCodesStart + lengthCode(length)]++;
+			w.offsetFreq[offsetCode(offset)]++;
+			break;
+		}
+	}
+	w.literalEncoding.generate(w.literalFreq, 15);
+	w.offsetEncoding.generate(w.offsetFreq, 15);
+
+	// get the number of literals
+	numLiterals := len(w.literalFreq);
+	for w.literalFreq[numLiterals - 1] == 0 {
+		numLiterals--;
+	}
+	// get the number of offsets
+	numOffsets := len(w.offsetFreq);
+	for numOffsets > 1 && w.offsetFreq[numOffsets - 1] == 0 {
+		numOffsets--;
+	}
+	storedBytes := 0;
+	if input != nil {
+		storedBytes = len(input);
+	}
+	var extraBits int64;
+	var storedSize int64;
+	if storedBytes <= maxStoreBlockSize && input != nil {
+		storedSize = int64((storedBytes + 5) * 8);
+		// We only bother calculating the costs of the extra bits required by
+		// the length of offset fields (which will be the same for both fixed
+		// and dynamic encoding), if we need to compare those two encodings
+		// against stored encoding.
+		for lengthCode := lengthCodesStart + 8; lengthCode < numLiterals; lengthCode++ {
+			// First eight length codes have extra size = 0.
+			extraBits += int64(w.literalFreq[lengthCode]) * int64(lengthExtraBits[lengthCode - lengthCodesStart]);
+		}
+		for offsetCode := 4; offsetCode < numOffsets; offsetCode++ {
+			// First four offset codes have extra size = 0.
+			extraBits += int64(w.offsetFreq[offsetCode]) * int64(offsetExtraBits[offsetCode]);
+		}
+	} else {
+		storedSize = math.MaxInt32;
+	}
+
+	// Figure out which generates smaller code, fixed Huffman, dynamic
+	// Huffman, or just storing the data.
+	var fixedSize int64 = math.MaxInt64;
+	if numOffsets <= offsetCodeCount {
+		fixedSize = int64(3) +
+			fixedLiteralEncoding.bitLength(w.literalFreq) +
+			fixedOffsetEncoding.bitLength(w.offsetFreq) +
+			extraBits;
+	}
+	// Generate codegen and codegenFrequencies, which indicates how to encode
+	// the literalEncoding and the offsetEncoding.
+	w.generateCodegen(numLiterals, numOffsets);
+	w.codegenEncoding.generate(w.codegenFreq, 7);
+	numCodegens := len(w.codegenFreq);
+	for numCodegens > 4 && w.codegenFreq[codegenOrder[numCodegens - 1]] == 0 {
+		numCodegens--;
+	}
+	extensionSummand := 0;
+	if numOffsets > offsetCodeCount {
+		extensionSummand = 3;
+	}
+	dynamicHeader := int64(3 + 5 + 5 + 4 + (3 * numCodegens)) +
+		// Following line is an extension.
+		int64(extensionSummand) +
+		w.codegenEncoding.bitLength(w.codegenFreq) +
+		int64(extraBits) +
+		int64(w.codegenFreq[16] * 2) +
+		int64(w.codegenFreq[17] * 3) +
+		int64(w.codegenFreq[18] * 7);
+	dynamicSize := dynamicHeader +
+		w.literalEncoding.bitLength(w.literalFreq) +
+		w.offsetEncoding.bitLength(w.offsetFreq);
+
+	if storedSize < fixedSize && storedSize < dynamicSize {
+		w.writeStoredHeader(storedBytes, eof);
+		w.writeBytes(input[0:storedBytes]);
+		return;
+	}
+	var literalEncoding *huffmanEncoder;
+	var offsetEncoding *huffmanEncoder;
+
+	if fixedSize <= dynamicSize {
+		w.writeFixedHeader(eof);
+		literalEncoding = fixedLiteralEncoding;
+		offsetEncoding = fixedOffsetEncoding;
+	} else {
+		// Write the header.
+		w.writeDynamicHeader(numLiterals, numOffsets, numCodegens, eof);
+		literalEncoding = w.literalEncoding;
+		offsetEncoding = w.offsetEncoding;
+	}
+
+	// Write the tokens.
+	for _, t := range tokens {
+		switch t.typ() {
+		case literalType:
+			w.writeCode(literalEncoding, t.literal());
+			break;
+		case matchType:
+			// Write the length
+			length := t.length();
+			lengthCode := lengthCode(length);
+			w.writeCode(literalEncoding, lengthCode + lengthCodesStart);
+			extraLengthBits := int32(lengthExtraBits[lengthCode]);
+			if extraLengthBits > 0 {
+				extraLength := int32(length - lengthBase[lengthCode]);
+				w.writeBits(extraLength, extraLengthBits);
+			}
+			// Write the offset
+			offset := t.offset();
+			offsetCode := offsetCode(offset);
+			w.writeCode(offsetEncoding, offsetCode);
+			extraOffsetBits := int32(offsetExtraBits[offsetCode]);
+			if extraOffsetBits > 0 {
+				extraOffset := int32(offset - offsetBase[offsetCode]);
+				w.writeBits(extraOffset, extraOffsetBits);
+			}
+			break;
+		default:
+			panic("unknown token type: " + string(t));
+		}
+	}
+}
+
