7839521335349ce208818bdc2bbb7c4f07bdfb4c
Rob Pike <r@golang.org>
2009-10-13 12:37:04 -0700

    Automated g4 rollback of changelist 35383.
    *** Reason for rollback ***
    
    roll back the changes to the tutorial programs (only) since they
    break the automated processing used to create the tutorial.
    
    *** Original change description ***
    
    apply gofmt to the LGTM-marked files from 34501
    that have not changed since I applied gofmt.
    
    R=rsc
    DELTA=139  (0 added, 44 deleted, 95 changed)
    OCL=35670
    CL=35670

--- doc/progs/print_string.go
+++ doc/progs/print_string.go
@@ -6,16 +6,13 @@ package main
 
 import "fmt"
 
-type testType struct {
-	a	int;
-	b	string;
-}
+type testType struct { a int; b string }
 
 func (t *testType) String() string {
-	return fmt.Sprint(t.a) + " " + t.b;
+	return fmt.Sprint(t.a) + " " + t.b
 }
 
 func main() {
 	t := &testType{77, "Sunset Strip"};
-	fmt.Println(t);
+	fmt.Println(t)
 }
--- doc/progs/sortmain.go
+++ doc/progs/sortmain.go
@@ -14,7 +14,7 @@ func ints() {
 	a := sort.IntArray(data);
 	sort.Sort(a);
 	if !sort.IsSorted(a) {
-		panic();
+		panic()
 	}
 }
 
@@ -23,48 +23,42 @@ func strings() {
 	a := sort.StringArray(data);
 	sort.Sort(a);
 	if !sort.IsSorted(a) {
-		panic();
+		panic()
 	}
 }
 
 type day struct {
-	num		int;
-	short_name	string;
-	long_name	string;
+	num        int;
+	short_name string;
+	long_name  string;
 }
 
 type dayArray struct {
 	data []*day;
 }
 
-func (p *dayArray) Len() int {
-	return len(p.data);
-}
-func (p *dayArray) Less(i, j int) bool {
-	return p.data[i].num < p.data[j].num;
-}
-func (p *dayArray) Swap(i, j int) {
-	p.data[i], p.data[j] = p.data[j], p.data[i];
-}
+func (p *dayArray) Len() int            { return len(p.data); }
+func (p *dayArray) Less(i, j int) bool  { return p.data[i].num < p.data[j].num; }
+func (p *dayArray) Swap(i, j int)       { p.data[i], p.data[j] = p.data[j], p.data[i]; }
 
 func days() {
-	Sunday := day{0, "SUN", "Sunday"};
-	Monday := day{1, "MON", "Monday"};
-	Tuesday := day{2, "TUE", "Tuesday"};
-	Wednesday := day{3, "WED", "Wednesday"};
-	Thursday := day{4, "THU", "Thursday"};
-	Friday := day{5, "FRI", "Friday"};
-	Saturday := day{6, "SAT", "Saturday"};
+	Sunday :=    day{ 0, "SUN", "Sunday" };
+	Monday :=    day{ 1, "MON", "Monday" };
+	Tuesday :=   day{ 2, "TUE", "Tuesday" };
+	Wednesday := day{ 3, "WED", "Wednesday" };
+	Thursday :=  day{ 4, "THU", "Thursday" };
+	Friday :=    day{ 5, "FRI", "Friday" };
+	Saturday :=  day{ 6, "SAT", "Saturday" };
 	data := []*day{&Tuesday, &Thursday, &Wednesday, &Sunday, &Monday, &Friday, &Saturday};
 	a := dayArray{data};
 	sort.Sort(&a);
 	if !sort.IsSorted(&a) {
-		panic();
+		panic()
 	}
 	for _, d := range data {
-		fmt.Printf("%s ", d.long_name);
+		fmt.Printf("%s ", d.long_name)
 	}
-	fmt.Printf("\n");
+	fmt.Printf("\n")
 }
 
 
