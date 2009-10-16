222a15c8405bf080a8bcdb2c4405c6742058248f
Kai Backman <kaib@golang.org>
2009-10-15 19:41:51 -0700

    test/64bit.go passes but doesn't generate properly yet.
    R=rsc
    APPROVED=rsc
    DELTA=235  (98 added, 38 deleted, 99 changed)
    OCL=35789
    CL=35813

--- src/pkg/runtime/arm/vlrt.c
+++ src/pkg/runtime/arm/vlrt.c
@@ -291,9 +291,15 @@ _divv(Vlong *q, Vlong n, Vlong d)
         long nneg, dneg;
 
         if(n.hi == (((long)n.lo)>>31) && d.hi == (((long)d.lo)>>31)) {
-                q->lo = (long)n.lo / (long)d.lo;
-                q->hi = ((long)q->lo) >> 31;
-                return;
+		if((long)n.lo == -0x80000000 && (long)d.lo == -1) {
+			// special case: 32-bit -0x80000000 / -1 causes wrong sign
+			q->lo = 0x80000000;
+			q->hi = 0;
+			return;
+		}
+		q->lo = (long)n.lo / (long)d.lo;
+		q->hi = ((long)q->lo) >> 31;
+		return;
         }
         nneg = n.hi >> 31;
         if(nneg)
