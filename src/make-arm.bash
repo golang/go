d435967c2d7f5c5079147fccc4a8c30254337405
Kai Backman <kaib@golang.org>
2009-10-23 12:43:01 -0700

    fix build issue cause by transition to hg
    R=rsc
    http://go/go-review/1013012

--- src/make-arm.bash
+++ src/make-arm.bash
@@ -20,8 +20,8 @@ cp quietgcc.bash $GOBIN/quietgcc
 chmod +x $GOBIN/quietgcc
 
 # TODO(kaib): converge with normal build
-#for i in lib9 libbio libmach libregexp cmd pkg libcgo cmd/cgo cmd/ebnflint cmd/godoc cmd/gofmt
-for i in lib9 libbio libmach libregexp cmd pkg cmd/cgo cmd/ebnflint cmd/godoc cmd/gofmt
+#for i in lib9 libbio libmach cmd pkg libcgo cmd/cgo cmd/ebnflint cmd/godoc cmd/gofmt
+for i in lib9 libbio libmach cmd pkg cmd/cgo cmd/ebnflint cmd/godoc cmd/gofmt
 do
 	# The ( ) here are to preserve the current directory
 	# for the next round despite the cd $i below.
