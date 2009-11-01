6b77bc7ab9f326198872fb45bec3fc60e8caa388
Russ Cox <rsc@golang.org>
2009-11-01 09:37:13 -0800

    os cleanup.
    dir_* and stat_* are just os specific,
    not os+arch-specific.
    
    R=r
    http://go/go-review/1018010

--- src/pkg/os/stat_darwin.go
+++ src/pkg/os/stat_darwin.go
@@ -2,12 +2,6 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// TODO(rsc): Once the porting dust settles, consider
-// whether this file should be stat_linux.go (and similarly
-// stat_darwin.go) instead of having one copy per architecture.
-
-// 386, Linux
-
 package os
 
 import "syscall"
@@ -17,19 +11,19 @@ func isSymlink(stat *syscall.Stat_t) bool {
 }
 
 func dirFromStat(name string, dir *Dir, lstat, stat *syscall.Stat_t) *Dir {
-	dir.Dev = stat.Dev;
-	dir.Ino = uint64(stat.Ino);
+	dir.Dev = uint64(stat.Dev);
+	dir.Ino = stat.Ino;
 	dir.Nlink = uint64(stat.Nlink);
-	dir.Mode = stat.Mode;
+	dir.Mode = uint32(stat.Mode);
 	dir.Uid = stat.Uid;
 	dir.Gid = stat.Gid;
-	dir.Rdev = stat.Rdev;
+	dir.Rdev = uint64(stat.Rdev);
 	dir.Size = uint64(stat.Size);
 	dir.Blksize = uint64(stat.Blksize);
 	dir.Blocks = uint64(stat.Blocks);
-	dir.Atime_ns = uint64(syscall.TimespecToNsec(stat.Atim));
-	dir.Mtime_ns = uint64(syscall.TimespecToNsec(stat.Mtim));
-	dir.Ctime_ns = uint64(syscall.TimespecToNsec(stat.Ctim));
+	dir.Atime_ns = uint64(syscall.TimespecToNsec(stat.Atimespec));
+	dir.Mtime_ns = uint64(syscall.TimespecToNsec(stat.Mtimespec));
+	dir.Ctime_ns = uint64(syscall.TimespecToNsec(stat.Ctimespec));
 	for i := len(name)-1; i >= 0; i-- {
 		if name[i] == '/' {
 			name = name[i+1 : len(name)];
--- src/pkg/os/stat_nacl.go
+++ src/pkg/os/stat_nacl.go
@@ -2,12 +2,6 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// TODO(rsc): Once the porting dust settles, consider
-// whether this file should be stat_linux.go (and similarly
-// stat_darwin.go) instead of having one copy per architecture.
-
-// 386, Linux
-
 package os
 
 import "syscall"
@@ -17,19 +11,19 @@ func isSymlink(stat *syscall.Stat_t) bool {
 }
 
 func dirFromStat(name string, dir *Dir, lstat, stat *syscall.Stat_t) *Dir {
-	dir.Dev = stat.Dev;
+	dir.Dev = uint64(stat.Dev);
 	dir.Ino = uint64(stat.Ino);
 	dir.Nlink = uint64(stat.Nlink);
 	dir.Mode = stat.Mode;
 	dir.Uid = stat.Uid;
 	dir.Gid = stat.Gid;
-	dir.Rdev = stat.Rdev;
+	dir.Rdev = uint64(stat.Rdev);
 	dir.Size = uint64(stat.Size);
 	dir.Blksize = uint64(stat.Blksize);
 	dir.Blocks = uint64(stat.Blocks);
-	dir.Atime_ns = uint64(syscall.TimespecToNsec(stat.Atim));
-	dir.Mtime_ns = uint64(syscall.TimespecToNsec(stat.Mtim));
-	dir.Ctime_ns = uint64(syscall.TimespecToNsec(stat.Ctim));
+	dir.Atime_ns = uint64(stat.Atime)*1e9;
+	dir.Mtime_ns = uint64(stat.Mtime)*1e9;
+	dir.Ctime_ns = uint64(stat.Ctime)*1e9;
 	for i := len(name)-1; i >= 0; i-- {
 		if name[i] == '/' {
 			name = name[i+1 : len(name)];
