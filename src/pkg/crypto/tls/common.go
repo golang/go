6e8184d8cd53e76e7f9f9366c06f0441c5b5aa4c
Adam Langley <agl@golang.org>
2009-11-02 18:25:20 -0800

    crypto/tls (part 1)
    Rather than drop everything into a single, huge review, I've included
    some simple bits of code here.
    
    R=rsc
    CC=go-dev
    http://go/go-review/1016029

---
+++ src/pkg/crypto/tls/common.go
@@ -0,0 +1,123 @@
+// Copyright 2009 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package tls
+
+import (
+	"crypto/rsa";
+	"io";
+	"os";
+)
+
+const (
+	// maxTLSCiphertext is the maximum length of a plaintext payload.
+	maxTLSPlaintext	= 16384;
+	// maxTLSCiphertext is the maximum length payload after compression and encryption.
+	maxTLSCiphertext	= 16384+2048;
+	// maxHandshakeMsg is the largest single handshake message that we'll buffer.
+	maxHandshakeMsg	= 65536;
+)
+
+
+// TLS record types.
+type recordType uint8
+
+const (
+	recordTypeChangeCipherSpec	recordType	= 20;
+	recordTypeAlert			recordType	= 21;
+	recordTypeHandshake		recordType	= 22;
+	recordTypeApplicationData	recordType	= 23;
+)
+
+// TLS handshake message types.
+const (
+	typeClientHello		uint8	= 1;
+	typeServerHello		uint8	= 2;
+	typeCertificate		uint8	= 11;
+	typeServerHelloDone	uint8	= 14;
+	typeClientKeyExchange	uint8	= 16;
+	typeFinished		uint8	= 20;
+)
+
+// TLS cipher suites.
+var (
+	TLS_RSA_WITH_RC4_128_SHA uint16 = 5;
+)
+
+// TLS compression types.
+var (
+	compressionNone uint8 = 0;
+)
+
+type ConnectionState struct {
+	HandshakeComplete	bool;
+	CipherSuite		string;
+	Error			alertType;
+}
+
+// A Config structure is used to configure a TLS client or server. After one
+// has been passed to a TLS function it must not be modified.
+type Config struct {
+	// Rand provides the source of entropy for nonces and RSA blinding.
+	Rand	io.Reader;
+	// Time returns the current time as the number of seconds since the epoch.
+	Time		func() int64;
+	Certificates	[]Certificate;
+}
+
+type Certificate struct {
+	Certificate	[][]byte;
+	PrivateKey	*rsa.PrivateKey;
+}
+
+// A TLS record.
+type record struct {
+	contentType	recordType;
+	major, minor	uint8;
+	payload		[]byte;
+}
+
+type handshakeMessage interface {
+	marshal() []byte;
+}
+
+type encryptor interface {
+	// XORKeyStream xors the contents of the slice with bytes from the key stream.
+	XORKeyStream(buf []byte);
+}
+
+// mutualVersion returns the protocol version to use given the advertised
+// version of the peer.
+func mutualVersion(theirMajor, theirMinor uint8) (major, minor uint8, ok bool) {
+	// We don't deal with peers < TLS 1.0 (aka version 3.1).
+	if theirMajor < 3 || theirMajor == 3 && theirMinor < 1 {
+		return 0, 0, false;
+	}
+	major = 3;
+	minor = 2;
+	if theirMinor < minor {
+		minor = theirMinor;
+	}
+	ok = true;
+	return;
+}
+
+// A nop implements the NULL encryption and MAC algorithms.
+type nop struct{}
+
+func (nop) XORKeyStream(buf []byte) {}
+
+func (nop) Write(buf []byte) (int, os.Error) {
+	return len(buf), nil;
+}
+
+func (nop) Sum() []byte {
+	return nil;
+}
+
+func (nop) Reset() {}
+
+func (nop) Size() int {
+	return 0;
+}
---
+++ src/pkg/crypto/tls/prf.go
@@ -0,0 +1,148 @@
+// Copyright 2009 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package tls
+
+import (
+	"bytes";
+	"crypto/hmac";
+	"crypto/md5";
+	"crypto/sha1";
+	"hash";
+	"os";
+	"strings";
+)
+
+// Split a premaster secret in two as specified in RFC 4346, section 5.
+func splitPreMasterSecret(secret []byte) (s1, s2 []byte) {
+	s1 = secret[0 : (len(secret)+1)/2];
+	s2 = secret[len(secret)/2 : len(secret)];
+	return;
+}
+
+// pHash implements the P_hash function, as defined in RFC 4346, section 5.
+func pHash(result, secret, seed []byte, hash hash.Hash) {
+	h := hmac.New(hash, secret);
+	h.Write(seed);
+	a := h.Sum();
+
+	j := 0;
+	for j < len(result) {
+		h.Reset();
+		h.Write(a);
+		h.Write(seed);
+		b := h.Sum();
+		todo := len(b);
+		if j+todo > len(result) {
+			todo = len(result)-j;
+		}
+		bytes.Copy(result[j : j+todo], b);
+		j += todo;
+
+		h.Reset();
+		h.Write(a);
+		a = h.Sum();
+	}
+}
+
+// pRF11 implements the TLS 1.1 pseudo-random function, as defined in RFC 4346, section 5.
+func pRF11(result, secret, label, seed []byte) {
+	hashSHA1 := sha1.New();
+	hashMD5 := md5.New();
+
+	labelAndSeed := make([]byte, len(label)+len(seed));
+	bytes.Copy(labelAndSeed, label);
+	bytes.Copy(labelAndSeed[len(label):len(labelAndSeed)], seed);
+
+	s1, s2 := splitPreMasterSecret(secret);
+	pHash(result, s1, labelAndSeed, hashMD5);
+	result2 := make([]byte, len(result));
+	pHash(result2, s2, labelAndSeed, hashSHA1);
+
+	for i, b := range result2 {
+		result[i] ^= b;
+	}
+}
+
+const (
+	tlsRandomLength		= 32;	// Length of a random nonce in TLS 1.1.
+	masterSecretLength	= 48;	// Length of a master secret in TLS 1.1.
+	finishedVerifyLength	= 12;	// Length of verify_data in a Finished message.
+)
+
+var masterSecretLabel = strings.Bytes("master secret")
+var keyExpansionLabel = strings.Bytes("key expansion")
+var clientFinishedLabel = strings.Bytes("client finished")
+var serverFinishedLabel = strings.Bytes("server finished")
+
+// keysFromPreMasterSecret generates the connection keys from the pre master
+// secret, given the lengths of the MAC and cipher keys, as defined in RFC
+// 4346, section 6.3.
+func keysFromPreMasterSecret11(preMasterSecret, clientRandom, serverRandom []byte, macLen, keyLen int) (masterSecret, clientMAC, serverMAC, clientKey, serverKey []byte) {
+	var seed [tlsRandomLength * 2]byte;
+	bytes.Copy(seed[0:len(clientRandom)], clientRandom);
+	bytes.Copy(seed[len(clientRandom):len(seed)], serverRandom);
+	masterSecret = make([]byte, masterSecretLength);
+	pRF11(masterSecret, preMasterSecret, masterSecretLabel, seed[0:len(seed)]);
+
+	bytes.Copy(seed[0:len(clientRandom)], serverRandom);
+	bytes.Copy(seed[len(serverRandom):len(seed)], clientRandom);
+
+	n := 2*macLen + 2*keyLen;
+	keyMaterial := make([]byte, n);
+	pRF11(keyMaterial, masterSecret, keyExpansionLabel, seed[0:len(seed)]);
+	clientMAC = keyMaterial[0:macLen];
+	serverMAC = keyMaterial[macLen : macLen*2];
+	clientKey = keyMaterial[macLen*2 : macLen*2 + keyLen];
+	serverKey = keyMaterial[macLen*2 + keyLen : len(keyMaterial)];
+	return;
+}
+
+// A finishedHash calculates the hash of a set of handshake messages suitable
+// for including in a Finished message.
+type finishedHash struct {
+	clientMD5	hash.Hash;
+	clientSHA1	hash.Hash;
+	serverMD5	hash.Hash;
+	serverSHA1	hash.Hash;
+}
+
+func newFinishedHash() finishedHash {
+	return finishedHash{md5.New(), sha1.New(), md5.New(), sha1.New()};
+}
+
+func (h finishedHash) Write(msg []byte) (n int, err os.Error) {
+	h.clientMD5.Write(msg);
+	h.clientSHA1.Write(msg);
+	h.serverMD5.Write(msg);
+	h.serverSHA1.Write(msg);
+	return len(msg), nil;
+}
+
+// finishedSum calculates the contents of the verify_data member of a Finished
+// message given the MD5 and SHA1 hashes of a set of handshake messages.
+func finishedSum(md5, sha1, label, masterSecret []byte) []byte {
+	seed := make([]byte, len(md5)+len(sha1));
+	bytes.Copy(seed, md5);
+	bytes.Copy(seed[len(md5):len(seed)], sha1);
+	out := make([]byte, finishedVerifyLength);
+	pRF11(out, masterSecret, label, seed);
+	return out;
+}
+
+// clientSum returns the contents of the verify_data member of a client's
+// Finished message.
+func (h finishedHash) clientSum(masterSecret []byte) []byte {
+	md5 := h.clientMD5.Sum();
+	sha1 := h.clientSHA1.Sum();
+	return finishedSum(md5, sha1, clientFinishedLabel, masterSecret);
+}
+
+// serverSum returns the contents of the verify_data member of a server's
+// Finished message.
+func (h finishedHash) serverSum(masterSecret []byte) []byte {
+	md5 := h.serverMD5.Sum();
+	sha1 := h.serverSHA1.Sum();
+	return finishedSum(md5, sha1, serverFinishedLabel, masterSecret);
+}
