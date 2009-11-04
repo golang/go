ce5c2b4b5d5fc474ca9473fc55090255566d6a4d
Rob Pike <r@golang.org>
2009-11-03 16:05:47 -0800

    First steps at command documentation: C compilers and linkers. Each command gets a doc.go file for godoc to analyze. Its main element is a package comment.
    R=rsc
    CC=go-dev
    http://go/go-review/1018029

---
+++ src/cmd/5l/doc.go
@@ -0,0 +1,26 @@
+// Copyright 2009 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+/*
+
+5l is a modified version of the Plan 9 loader documented at
+
+	http://plan9.bell-labs.com/magic/man2html/1/2l
+
+Its target architecture is the ARM, referred to by these tools as arm.
+
+Major changes include:
+	- support for segmented stacks (this feature is implemented here, not in the compilers).
+
+
+Original options are listed in the link above.
+
+Options new in this version:
+-L dir1,dir2,..
+	Search for libraries (package files) in the comma-separated list of directories.
+	The default is the single location $GOROOT/pkg/$GOOS_arm.
+
+
+*/
+package documentation
---
+++ src/cmd/8c/doc.go
@@ -0,0 +1,14 @@
+// Copyright 2009 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+/*
+
+8c is a version of the Plan 9 C compiler documented at
+
+	http://plan9.bell-labs.com/magic/man2html/1/2c
+
+Its target architecture is the x86, referred to by these tools for historical reasons as 386.
+
+*/
+package documentation
