e3accc883cd3a64e4f1980839b66ba13668482fb
Rob Pike <r@golang.org>
2009-11-03 17:46:35 -0800

    more command documentation, including nm and prof
    R=rsc
    http://go/go-review/1017022

---
+++ src/cmd/cc/doc.go
@@ -0,0 +1,11 @@
+// Copyright 2009 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+/*
+
+This directory contains the portable section of the Plan 9 C compilers as
+represented here.  See ../6c, ../8c, and ../5c for more information.
+
+*/
+package documentation
