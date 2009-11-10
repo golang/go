589a045122733ea5ca13d0a27ace7b21aca53870
Russ Cox <rsc@golang.org>
2009-11-09 23:11:36 -0800

    drop output from nilptr tests; too hard to make it the same everywhere. still checking for non-zero exit status though.
    disable core files while running tests
    
    R=r
    http://go/go-review/1026037

--- test/nilptr/structfield2.go
+++ test/nilptr/structfield2.go
@@ -1,4 +1,5 @@
-// $G $D/$F.go && $L $F.$A && (! ./$A.out || echo BUG: should fail)
+// $G $D/$F.go && $L $F.$A &&
+//	((! sh -c ./$A.out) >/dev/null 2>&1 || echo BUG: should fail)
 
 // Copyright 2009 The Go Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
