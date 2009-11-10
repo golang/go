b27f05f63bcf6ac0bf9381630eb207caea3ff1f5
Robert Griesemer <gri@golang.org>
2009-11-10 10:55:53 -0800

    minor adjustments to package comments for better synopsis.
    R=rsc
    CC=r
    http://go/go-review/1026038

--- src/pkg/asn1/asn1.go
+++ src/pkg/asn1/asn1.go
@@ -2,8 +2,8 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// This package implements parsing of DER-encoded ASN.1 data structures,
-// as defined in ITU-T Rec. X.690.
+// The asn1 package implements parsing of DER-encoded ASN.1 data structures,
+// as defined in ITU-T Rec X.690.
 //
 // See also ``A Layman's Guide to a Subset of ASN.1, BER, and DER,''
 // http://luca.ntop.org/Teaching/Appunti/asn1.html.
