391e082ca9176f05be96a8853cc2bf9d93771756
Adam Langley <agl@golang.org>
2009-11-15 14:00:46 -0800

    crypto/md5: fix comment typo.
    Fixes #210.
    
    R=rsc
    CC=golang-dev
    https://golang.org/cl/155057

--- src/pkg/crypto/md5/md5.go
+++ src/pkg/crypto/md5/md5.go
@@ -38,7 +38,7 @@ func (d *digest) Reset() {
 	d.len = 0;
 }
 
-// New returns a hash.Hash computing the SHA1 checksum.
+// New returns a new hash.Hash computing the MD5 checksum.
 func New() hash.Hash {
 	d := new(digest);
 	d.Reset();
