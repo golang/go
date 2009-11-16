affcfe5a750db21c40d6fb62c7f2ac9add818564
David Symonds <dsymonds@golang.org>
2009-11-16 13:39:59 -0800

    Add some primitive type aliases to exp/iterable and define Iter on them.
    R=rsc
    https://golang.org/cl/155065

---
+++ src/pkg/exp/iterable/array.go
@@ -0,0 +1,59 @@
+// Copyright 2009 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package iterable
+
+// This file implements the Iterable interface on some primitive types.
+
+type ByteArray []byte
+
+func (a ByteArray) Iter() <-chan interface{} {
+	ch := make(chan interface{});
+	go func() {
+		for _, e := range a {
+			ch <- e
+		}
+		close(ch);
+	}();
+	return ch;
+}
+
+type IntArray []int
+
+func (a IntArray) Iter() <-chan interface{} {
+	ch := make(chan interface{});
+	go func() {
+		for _, e := range a {
+			ch <- e
+		}
+		close(ch);
+	}();
+	return ch;
+}
+
+type FloatArray []float
+
+func (a FloatArray) Iter() <-chan interface{} {
+	ch := make(chan interface{});
+	go func() {
+		for _, e := range a {
+			ch <- e
+		}
+		close(ch);
+	}();
+	return ch;
+}
+
+type StringArray []string
+
+func (a StringArray) Iter() <-chan interface{} {
+	ch := make(chan interface{});
+	go func() {
+		for _, e := range a {
+			ch <- e
+		}
+		close(ch);
+	}();
+	return ch;
+}
