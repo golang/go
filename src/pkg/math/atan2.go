61424574116b70f2d22843e282657bff17b306d1
Russ Cox <rsc@golang.org>
2009-11-17 08:39:56 -0800

    math: fix argument names in Atan2 (error introduced converting from arg1, arg2)
    Fixes #220.
    
    R=r
    https://golang.org/cl/156041

--- src/pkg/math/atan2.go
+++ src/pkg/math/atan2.go
@@ -4,20 +4,19 @@
 
 package math
 
-
-// Atan returns the arc tangent of y/x, using
+// Atan2 returns the arc tangent of y/x, using
 // the signs of the two to determine the quadrant
 // of the return value.
-func Atan2(x, y float64) float64 {
+func Atan2(y, x float64) float64 {
 	// Determine the quadrant and call atan.
-	if x+y == x {
-		if x >= 0 {
+	if y+x == y {
+		if y >= 0 {
 			return Pi / 2
 		}
 		return -Pi / 2;
 	}
-	q := Atan(x / y);
-	if y < 0 {
+	q := Atan(y / x);
+	if x < 0 {
 		if q <= 0 {
 			return q + Pi
 		}
