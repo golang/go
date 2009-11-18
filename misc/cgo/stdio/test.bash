74e0a2e54b43611c4b599da6cbc949773f53fd36
Russ Cox <rsc@golang.org>
2009-11-18 09:11:17 -0800

    make all.bash finish on FreeBSD
    R=dho
    CC=golang-dev
    https://golang.org/cl/156067

--- misc/cgo/stdio/test.bash
+++ misc/cgo/stdio/test.bash
@@ -3,6 +3,8 @@
 # Use of this source code is governed by a BSD-style
 # license that can be found in the LICENSE file.
 
+if [ "$(uname)" = "FreeBSD" ]; then exit 0; fi
+
 set -e
 gomake hello fib chain
 echo '*' hello >run.out
