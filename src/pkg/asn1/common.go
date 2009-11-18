16320655e89de4e312f77bffe8e60003239a35eb
Adam Langley <agl@golang.org>
2009-11-17 18:09:41 -0800

    asn1:   * add Marshal   * add BitString.RightAlign   * change to using a *time.Time (from time.Time) since that's what     the time package uses.   * return the unparsed data from Unmarshal.
    R=rsc
    CC=golang-dev
    https://golang.org/cl/156047

---
+++ src/pkg/asn1/common.go
@@ -0,0 +1,140 @@
+// Copyright 2009 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package asn1
+
+import (
+	"reflect";
+	"strconv";
+	"strings";
+)
+
+// ASN.1 objects have metadata preceeding them:
+//   the tag: the type of the object
+//   a flag denoting if this object is compound or not
+//   the class type: the namespace of the tag
+//   the length of the object, in bytes
+
+// Here are some standard tags and classes
+
+const (
+	tagBoolean		= 1;
+	tagInteger		= 2;
+	tagBitString		= 3;
+	tagOctetString		= 4;
+	tagOID			= 6;
+	tagSequence		= 16;
+	tagSet			= 17;
+	tagPrintableString	= 19;
+	tagIA5String		= 22;
+	tagUTCTime		= 23;
+)
+
+const (
+	classUniversal		= 0;
+	classApplication	= 1;
+	classContextSpecific	= 2;
+	classPrivate		= 3;
+)
+
+type tagAndLength struct {
+	class, tag, length	int;
+	isCompound		bool;
+}
+
+// ASN.1 has IMPLICIT and EXPLICIT tags, which can be translated as "instead
+// of" and "in addition to". When not specified, every primitive type has a
+// default tag in the UNIVERSAL class.
+//
+// For example: a BIT STRING is tagged [UNIVERSAL 3] by default (although ASN.1
+// doesn't actually have a UNIVERSAL keyword). However, by saying [IMPLICIT
+// CONTEXT-SPECIFIC 42], that means that the tag is replaced by another.
+//
+// On the other hand, if it said [EXPLICIT CONTEXT-SPECIFIC 10], then an
+// /additional/ tag would wrap the default tag. This explicit tag will have the
+// compound flag set.
+//
+// (This is used in order to remove ambiguity with optional elements.)
+//
+// You can layer EXPLICIT and IMPLICIT tags to an arbitrary depth, however we
+// don't support that here. We support a single layer of EXPLICIT or IMPLICIT
+// tagging with tag strings on the fields of a structure.
+
+// fieldParameters is the parsed representation of tag string from a structure field.
+type fieldParameters struct {
+	optional	bool;	// true iff the field is OPTIONAL
+	explicit	bool;	// true iff and EXPLICIT tag is in use.
+	defaultValue	*int64;	// a default value for INTEGER typed fields (maybe nil).
+	tag		*int;	// the EXPLICIT or IMPLICIT tag (maybe nil).
+	stringType	int;	// the string tag to use when marshaling.
+
+	// Invariants:
+	//   if explicit is set, tag is non-nil.
+}
+
+// Given a tag string with the format specified in the package comment,
+// parseFieldParameters will parse it into a fieldParameters structure,
+// ignoring unknown parts of the string.
+func parseFieldParameters(str string) (ret fieldParameters) {
+	for _, part := range strings.Split(str, ",", 0) {
+		switch {
+		case part == "optional":
+			ret.optional = true
+		case part == "explicit":
+			ret.explicit = true;
+			if ret.tag == nil {
+				ret.tag = new(int);
+				*ret.tag = 0;
+			}
+		case part == "ia5":
+			ret.stringType = tagIA5String
+		case part == "printable":
+			ret.stringType = tagPrintableString
+		case strings.HasPrefix(part, "default:"):
+			i, err := strconv.Atoi64(part[8:len(part)]);
+			if err == nil {
+				ret.defaultValue = new(int64);
+				*ret.defaultValue = i;
+			}
+		case strings.HasPrefix(part, "tag:"):
+			i, err := strconv.Atoi(part[4:len(part)]);
+			if err == nil {
+				ret.tag = new(int);
+				*ret.tag = i;
+			}
+		}
+	}
+	return;
+}
+
+// Given a reflected Go type, getUniversalType returns the default tag number
+// and expected compound flag.
+func getUniversalType(t reflect.Type) (tagNumber int, isCompound, ok bool) {
+	switch t {
+	case objectIdentifierType:
+		return tagOID, false, true
+	case bitStringType:
+		return tagBitString, false, true
+	case timeType:
+		return tagUTCTime, false, true
+	}
+	switch i := t.(type) {
+	case *reflect.BoolType:
+		return tagBoolean, false, true
+	case *reflect.IntType:
+		return tagInteger, false, true
+	case *reflect.Int64Type:
+		return tagInteger, false, true
+	case *reflect.StructType:
+		return tagSequence, true, true
+	case *reflect.SliceType:
+		if _, ok := t.(*reflect.SliceType).Elem().(*reflect.Uint8Type); ok {
+			return tagOctetString, false, true
+		}
+		return tagSequence, true, true;
+	case *reflect.StringType:
+		return tagPrintableString, false, true
+	}
+	return 0, false, false;
+}
---
+++ src/pkg/asn1/marshal_test.go
@@ -0,0 +1,78 @@
+// Copyright 2009 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package asn1
+
+import (
+	"bytes";
+	"encoding/hex";
+	"testing";
+	"time";
+)
+
+type intStruct struct {
+	A int;
+}
+
+type nestedStruct struct {
+	A intStruct;
+}
+
+type marshalTest struct {
+	in	interface{};
+	out	string;	// hex encoded
+}
+
+type implicitTagTest struct {
+	A int "implicit,tag:5";
+}
+
+type explicitTagTest struct {
+	A int "explicit,tag:5";
+}
+
+type ia5StringTest struct {
+	A string "ia5";
+}
+
+type printableStringTest struct {
+	A string "printable";
+}
+
+func setPST(t *time.Time) *time.Time {
+	t.ZoneOffset = -28800;
+	return t;
+}
+
+var marshalTests = []marshalTest{
+	marshalTest{10, "02010a"},
+	marshalTest{intStruct{64}, "3003020140"},
+	marshalTest{nestedStruct{intStruct{127}}, "3005300302017f"},
+	marshalTest{[]byte{1, 2, 3}, "0403010203"},
+	marshalTest{implicitTagTest{64}, "3003850140"},
+	marshalTest{explicitTagTest{64}, "3005a503020140"},
+	marshalTest{time.SecondsToUTC(0), "170d3730303130313030303030305a"},
+	marshalTest{time.SecondsToUTC(1258325776), "170d3039313131353232353631365a"},
+	marshalTest{setPST(time.SecondsToUTC(1258325776)), "17113039313131353232353631362d30383030"},
+	marshalTest{BitString{[]byte{0x80}, 1}, "03020780"},
+	marshalTest{BitString{[]byte{0x81, 0xf0}, 12}, "03030481f0"},
+	marshalTest{ObjectIdentifier([]int{1, 2, 3, 4}), "06032a0304"},
+	marshalTest{"test", "130474657374"},
+	marshalTest{ia5StringTest{"test"}, "3006160474657374"},
+	marshalTest{printableStringTest{"test"}, "3006130474657374"},
+}
+
+func TestMarshal(t *testing.T) {
+	for i, test := range marshalTests {
+		buf := bytes.NewBuffer(nil);
+		err := Marshal(buf, test.in);
+		if err != nil {
+			t.Errorf("#%d failed: %s", i, err)
+		}
+		out, _ := hex.DecodeString(test.out);
+		if bytes.Compare(out, buf.Bytes()) != 0 {
+			t.Errorf("#%d got: %x want %x", i, buf.Bytes(), out)
+		}
+	}
+}
