5fd4f768029cd8f84063d08e4a7787689ab6c09b
Robert Griesemer <gri@golang.org>
2009-11-19 00:04:30 -0800

    bug219: parsing difficulties with 6g
    R=r, rsc
    https://golang.org/cl/157085

---
+++ test/bugs/bug219.go
@@ -0,0 +1,35 @@
+// $G $D/$F.go || echo BUG: bug219
+
+// Copyright 2009 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+func f(func()) int { return 0 }
+
+// this doesn't work:
+// bug219.go:16: syntax error near if
+func g1() {
+	if x := f(func() {
+		if {}
+	}); {
+	}
+}
+
+// this works
+func g2() {
+	if x := f(func() {
+		//if {}
+	}); {
+	}
+}
+
+// this works
+func g3() {
+	x := f(func() {
+		if {}
+	});
+	if {
+	}
+}
