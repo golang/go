ed216711bb3274ad1bcf301605ad1f7b7aaf95a1
Russ Cox <rsc@golang.org>
2009-11-23 15:44:27 -0800

    gofmt -r: documentation and minor fixes fix a few paren insertion bugs in the printer too.
    R=gri, r
    CC=golang-dev
    https://golang.org/cl/157119

--- src/cmd/gofmt/doc.go
+++ src/cmd/gofmt/doc.go
@@ -18,6 +18,8 @@ The flags are:
 	-l
 		just list files whose formatting differs from gofmt's; generate no other output
 		unless -w is also set.
+	-r rule
+		apply the rewrite rule to the source before reformatting.
 	-w
 		if set, overwrite each input file with its output.
 	-spaces
@@ -32,5 +34,30 @@ Debugging flags:
 	-comments=true
 		print comments; if false, all comments are elided from the output.
 
+The rewrite rule specified with the -r flag must be a string of the form:
+
+	pattern -> replacement
+
+Both pattern and replacement must be valid Go expressions.
+In the pattern, single-character lowercase identifers serve as
+wildcards matching arbitrary subexpressions; those expressions
+will be substituted for the same identifiers in the replacement.
+
+
+Examples
+
+To check files for unnecessary parentheses:
+
+	gofmt -r '(a) -> a' -l *.go
+
+To remove the parentheses:
+
+	gofmt -r '(a) -> a' -w *.go
+
+To convert the package tree from explicit slice upper bounds to implicit ones:
+
+	gofmt -r 'α[β:len(α)] -> α[β:]' -w $GOROOT/src/pkg
 */
 package documentation
+
+// BUG(rsc): The implementation of -r is a bit slow.
