3c324707784882a36922e19e836d1f93b3fdc591
Chris Lennert <calennert@gmail.com>
2009-11-23 17:44:44 -0800

    Provides implementation of MD4 hashing algorithm.
    See IETF RFC 1320, http://tools.ietf.org/html/rfc1320.
    Fixes #279.
    
    R=rsc
    https://golang.org/cl/159051

---
+++ src/pkg/crypto/md4/md4block.go
@@ -0,0 +1,85 @@
+// Copyright 2009 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// MD4 block step.
+// In its own file so that a faster assembly or C version
+// can be substituted easily.
+
+package md4
+
+var shift1 = []uint{3, 7, 11, 19}
+var shift2 = []uint{3, 5, 9, 13}
+var shift3 = []uint{3, 9, 11, 15}
+
+var xIndex2 = []uint{0, 4, 8, 12, 1, 5, 9, 13, 2, 6, 10, 14, 3, 7, 11, 15}
+var xIndex3 = []uint{0, 8, 4, 12, 2, 10, 6, 14, 1, 9, 5, 13, 3, 11, 7, 15}
+
+func _Block(dig *digest, p []byte) int {
+	a := dig.s[0];
+	b := dig.s[1];
+	c := dig.s[2];
+	d := dig.s[3];
+	n := 0;
+	var X [16]uint32;
+	for len(p) >= _Chunk {
+		aa, bb, cc, dd := a, b, c, d;
+
+		for i := 0; i < 16; i++ {
+			j := i * 4;
+			X[i] = uint32(p[j]) | uint32(p[j+1])<<8 | uint32(p[j+2])<<16 | uint32(p[j+3])<<24;
+		}
+
+		// If this needs to be made faster in the future,
+		// the usual trick is to unroll each of these
+		// loops by a factor of 4; that lets you replace
+		// the shift[] lookups with constants and,
+		// with suitable variable renaming in each
+		// unrolled body, delete the a, b, c, d = d, a, b, c
+		// (or you can let the optimizer do the renaming).
+
+		// Round 1.
+		for i := 0; i < 16; i++ {
+			x := i;
+			s := shift1[i%4];
+			f := ((c ^ d) & b) ^ d;
+			a += f + X[x];
+			a = a<<s | a>>(32-s);
+			a, b, c, d = d, a, b, c;
+		}
+
+		// Round 2.
+		for i := 0; i < 16; i++ {
+			x := xIndex2[i];
+			s := shift2[i%4];
+			g := (b & c) | (b & d) | (c & d);
+			a += g + X[x] + 0x5a827999;
+			a = a<<s | a>>(32-s);
+			a, b, c, d = d, a, b, c;
+		}
+
+		// Round 3.
+		for i := 0; i < 16; i++ {
+			x := xIndex3[i];
+			s := shift3[i%4];
+			h := b ^ c ^ d;
+			a += h + X[x] + 0x6ed9eba1;
+			a = a<<s | a>>(32-s);
+			a, b, c, d = d, a, b, c;
+		}
+
+		a += aa;
+		b += bb;
+		c += cc;
+		d += dd;
+
+		p = p[_Chunk:];
+		n += _Chunk;
+	}
+
+	dig.s[0] = a;
+	dig.s[1] = b;
+	dig.s[2] = c;
+	dig.s[3] = d;
+	return n;
+}
