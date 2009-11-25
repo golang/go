b7c4314ecb87ee18d6dd8bc12f75bc1716f5ce88
Russ Cox <rsc@golang.org>
2009-11-24 16:11:48 -0800

    gc: correct type check for x, ok map assignment
    Fixes #288.
    
    R=ken2
    https://golang.org/cl/157162

---
+++ test/fixedbugs/bug220.go
@@ -0,0 +1,14 @@
+// $G $D/$F.go || echo BUG: bug220
+
+// Copyright 2009 The Go Authors.  All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+func main() {
+	m := make(map[int]map[uint]float);
+	
+	m[0] = make(map[uint]float), false;	// 6g used to reject this
+	m[1] = nil;
+}
