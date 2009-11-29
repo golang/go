5a013cee6e1908fcb067e55205d33f72b5657c77
Fumitoshi Ukai <ukai@google.com>
2009-11-29 14:22:44 -0800

    Add WebSocket server framework hooked into http.
    R=r, rsc
    https://golang.org/cl/156071

---
+++ src/pkg/websocket/server.go
@@ -0,0 +1,73 @@
+// Copyright 2009 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package websocket
+
+import (
+	"http";
+	"io";
+)
+
+// Handler is a interface that use a WebSocket.
+//
+// A trivial example server is:
+//
+//  package main
+//
+//  import (
+//     "http"
+//     "io"
+//     "websocket"
+//  )
+//
+//  // echo back the websocket.
+//  func EchoServer(ws *websocket.Conn) {
+//       io.Copy(ws, ws);
+//  }
+//
+//  func main() {
+//    http.Handle("/echo", websocket.Handler(EchoServer));
+//    err := http.ListenAndServe(":12345", nil);
+//    if err != nil {
+//        panic("ListenAndServe: ", err.String())
+//    }
+//  }
+type Handler func(*Conn)
+
+func (f Handler) ServeHTTP(c *http.Conn, req *http.Request) {
+	if req.Method != "GET" || req.Proto != "HTTP/1.1" ||
+		req.Header["Upgrade"] != "WebSocket" ||
+		req.Header["Connection"] != "Upgrade" {
+		c.WriteHeader(http.StatusNotFound);
+		io.WriteString(c, "must use websocket to connect here");
+		return;
+	}
+	rwc, buf, err := c.Hijack();
+	if err != nil {
+		panic("Hijack failed: ", err.String());
+		return;
+	}
+	defer rwc.Close();
+	origin := req.Header["Origin"];
+	location := "ws://" + req.Host + req.URL.Path;
+
+	// TODO(ukai): verify origin,location,protocol.
+
+	buf.WriteString("HTTP/1.1 101 Web Socket Protocol Handshake\r\n");
+	buf.WriteString("Upgrade: WebSocket\r\n");
+	buf.WriteString("Connection: Upgrade\r\n");
+	buf.WriteString("WebSocket-Origin: " + origin + "\r\n");
+	buf.WriteString("WebSocket-Location: " + location + "\r\n");
+	protocol := "";
+	// canonical header key of WebSocket-Protocol.
+	if protocol, found := req.Header["Websocket-Protocol"]; found {
+		buf.WriteString("WebSocket-Protocol: " + protocol + "\r\n")
+	}
+	buf.WriteString("\r\n");
+	if err := buf.Flush(); err != nil {
+		return
+	}
+	ws := newConn(origin, location, protocol, buf, rwc);
+	f(ws);
+}
---
+++ src/pkg/websocket/websocket.go
@@ -0,0 +1,138 @@
+// Copyright 2009 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// The websocket package implements Web Socket protocol server.
+package websocket
+
+// References:
+//   The Web Socket protocol: http://tools.ietf.org/html/draft-hixie-thewebsocketprotocol
+
+// TODO(ukai):
+//   better logging.
+
+import (
+	"bufio";
+	"io";
+	"net";
+	"os";
+)
+
+type WebSocketAddr string
+
+func (addr WebSocketAddr) Network() string	{ return "websocket" }
+
+func (addr WebSocketAddr) String() string	{ return string(addr) }
+
+// Conn is an channels to communicate over Web Socket.
+type Conn struct {
+	// An origin URI of the Web Socket.
+	Origin	string;
+	// A location URI of the Web Socket.
+	Location	string;
+	// A subprotocol of the Web Socket.
+	Protocol	string;
+
+	buf	*bufio.ReadWriter;
+	rwc	io.ReadWriteCloser;
+}
+
+// newConn creates a new Web Socket.
+func newConn(origin, location, protocol string, buf *bufio.ReadWriter, rwc io.ReadWriteCloser) *Conn {
+	if buf == nil {
+		br := bufio.NewReader(rwc);
+		bw := bufio.NewWriter(rwc);
+		buf = bufio.NewReadWriter(br, bw);
+	}
+	ws := &Conn{origin, location, protocol, buf, rwc};
+	return ws;
+}
+
+func (ws *Conn) Read(msg []byte) (n int, err os.Error) {
+	for {
+		frameByte, err := ws.buf.ReadByte();
+		if err != nil {
+			return
+		}
+		if (frameByte & 0x80) == 0x80 {
+			length := 0;
+			for {
+				c, err := ws.buf.ReadByte();
+				if err != nil {
+					return
+				}
+				if (c & 0x80) == 0x80 {
+					length = length*128 + int(c&0x7f)
+				} else {
+					break
+				}
+			}
+			for length > 0 {
+				_, err := ws.buf.ReadByte();
+				if err != nil {
+					return
+				}
+				length--;
+			}
+		} else {
+			for {
+				c, err := ws.buf.ReadByte();
+				if err != nil {
+					return
+				}
+				if c == '\xff' {
+					return
+				}
+				if frameByte == 0 {
+					if n+1 <= cap(msg) {
+						msg = msg[0 : n+1]
+					}
+					msg[n] = c;
+					n++;
+				}
+				if n >= cap(msg) {
+					err = os.E2BIG;
+					return;
+				}
+			}
+		}
+	}
+	return;
+}
+
+func (ws *Conn) Write(msg []byte) (n int, err os.Error) {
+	ws.buf.WriteByte(0);
+	ws.buf.Write(msg);
+	ws.buf.WriteByte(0xff);
+	err = ws.buf.Flush();
+	return len(msg), err;
+}
+
+func (ws *Conn) Close() os.Error	{ return ws.rwc.Close() }
+
+func (ws *Conn) LocalAddr() net.Addr	{ return WebSocketAddr(ws.Origin) }
+
+func (ws *Conn) RemoteAddr() net.Addr	{ return WebSocketAddr(ws.Location) }
+
+func (ws *Conn) SetTimeout(nsec int64) os.Error {
+	if conn, ok := ws.rwc.(net.Conn); ok {
+		return conn.SetTimeout(nsec)
+	}
+	return os.EINVAL;
+}
+
+func (ws *Conn) SetReadTimeout(nsec int64) os.Error {
+	if conn, ok := ws.rwc.(net.Conn); ok {
+		return conn.SetReadTimeout(nsec)
+	}
+	return os.EINVAL;
+}
+
+func (ws *Conn) SetWriteTimeout(nsec int64) os.Error {
+	if conn, ok := ws.rwc.(net.Conn); ok {
+		return conn.SetWriteTimeout(nsec)
+	}
+	return os.EINVAL;
+}
+
+var _ net.Conn = (*Conn)(nil)	// compile-time check that *Conn implements net.Conn.
