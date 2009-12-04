eae6a5fe29de36dd72cc8fbf31d51321c75354f4
Russ Cox <rsc@golang.org>
2009-12-03 17:23:11 -0800

    codereview: do not gofmt deleted files
    R=r
    https://golang.org/cl/164083

--- lib/codereview/codereview.py
+++ lib/codereview/codereview.py
@@ -622,6 +622,7 @@ def CheckGofmt(ui, repo, files, just_warn=False):
 		return
 	cwd = os.getcwd()
 	files = [RelativePath(repo.root + '/' + f, cwd) for f in files]
+	files = [f for f in files if os.access(f, 0)]
 	try:
 		cmd = subprocess.Popen(["gofmt", "-l"] + files, shell=False, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, close_fds=True)
 		cmd.stdin.close()
