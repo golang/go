ddde7f6625318ce22b27ff729d8bdc527fcc16ff
Devon H. O'Dell <devon.odell@gmail.com>
2009-12-11 12:48:55 -0800

    Add a Makefile in place of all.bash. Update docs to reflect this change.
    R=rsc
    CC=golang-dev
    https://golang.org/cl/171048

--- src/all.bash
+++ src/all.bash
@@ -1,8 +1,3 @@
-#!/usr/bin/env bash
-# Copyright 2009 The Go Authors. All rights reserved.
-# Use of this source code is governed by a BSD-style
-# license that can be found in the LICENSE file.
+#!/bin/sh
+exec make all
 
-set -e
-bash make.bash
-bash run.bash
