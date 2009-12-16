8d65873ce5382d540fd3898726007da7ebdc3ba4
Russ Cox <rsc@golang.org>
2009-12-15 16:22:04 -0800

    gc: bug fixes.   * better error for lookup of unexported field   * do not assign "ideal string" type to typed string literal   * do not confuse methods and fields during interface check
    Fixes #410.
    Fixes #411.
    Fixes #426.
    
    R=ken2
    https://golang.org/cl/179069

---
+++ test/fixedbugs/bug231.go
@@ -0,0 +1,22 @@
+// errchk $G -e $D/$F.go
+
+// Copyright 2009 The Go Authors.  All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+type I interface { m() }
+type T struct { m func() }
+type M struct {}
+func (M) m() {}
+
+func main() {
+	var t T
+	var m M
+	var i I
+	
+	i = m
+	i = t	// ERROR "not a method"
+	_ = i
+}
