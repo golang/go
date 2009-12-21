b15fefd7d7a0d94e1f014d3ba7f0c7639fb91d62
Jan Mercl <befelemepeseveze@gmail.com>
2009-12-21 14:34:54 -0800

    Experimental alternative implementation of the vector package
    R=gri
    CC=rsc
    https://golang.org/cl/178048

---
+++ src/pkg/exp/vector/defs.go
@@ -0,0 +1,78 @@
+// Copyright 2009 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// The vector package implements containers for managing sequences
+// of elements. Vectors grow and shrink dynamically as necessary.
+package vector
+
+
+// Vector is a container for numbered sequences of elements of type interface{}.
+// A vector's length and capacity adjusts automatically as necessary.
+// The zero value for Vector is an empty vector ready to use.
+type Vector []interface{}
+
+
+// IntVector is a container for numbered sequences of elements of type int.
+// A vector's length and capacity adjusts automatically as necessary.
+// The zero value for IntVector is an empty vector ready to use.
+type IntVector []int
+
+
+// StringVector is a container for numbered sequences of elements of type string.
+// A vector's length and capacity adjusts automatically as necessary.
+// The zero value for StringVector is an empty vector ready to use.
+type StringVector []string
+
+
+// Initial underlying array size
+const initialSize = 8
+
+
+// Partial sort.Interface support
+
+// LessInterface provides partial support of the sort.Interface.
+type LessInterface interface {
+	Less(y interface{}) bool
+}
+
+
+// Less returns a boolean denoting whether the i'th element is less than the j'th element.
+func (p *Vector) Less(i, j int) bool { return (*p)[i].(LessInterface).Less((*p)[j]) }
+
+
+// sort.Interface support
+
+// Less returns a boolean denoting whether the i'th element is less than the j'th element.
+func (p *IntVector) Less(i, j int) bool { return (*p)[i] < (*p)[j] }
+
+
+// Less returns a boolean denoting whether the i'th element is less than the j'th element.
+func (p *StringVector) Less(i, j int) bool { return (*p)[i] < (*p)[j] }
+
+
+// Do calls function f for each element of the vector, in order.
+// The behavior of Do is undefined if f changes *p.
+func (p *Vector) Do(f func(elem interface{})) {
+	for _, e := range *p {
+		f(e)
+	}
+}
+
+
+// Do calls function f for each element of the vector, in order.
+// The behavior of Do is undefined if f changes *p.
+func (p *IntVector) Do(f func(elem interface{})) {
+	for _, e := range *p {
+		f(e)
+	}
+}
+
+
+// Do calls function f for each element of the vector, in order.
+// The behavior of Do is undefined if f changes *p.
+func (p *StringVector) Do(f func(elem interface{})) {
+	for _, e := range *p {
+		f(e)
+	}
+}
---
+++ src/pkg/exp/vector/nogen_test.go
@@ -0,0 +1,76 @@
+// Copyright 2009 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package vector
+
+
+import (
+	"fmt"
+	"sort"
+	"testing"
+)
+
+var (
+	zero    interface{}
+	intzero int
+	strzero string
+)
+
+
+func int2Value(x int) int       { return x }
+func int2IntValue(x int) int    { return x }
+func int2StrValue(x int) string { return string(x) }
+
+
+func elem2Value(x interface{}) int  { return x.(int) }
+func elem2IntValue(x int) int       { return x }
+func elem2StrValue(x string) string { return x }
+
+
+func intf2Value(x interface{}) int       { return x.(int) }
+func intf2IntValue(x interface{}) int    { return x.(int) }
+func intf2StrValue(x interface{}) string { return x.(string) }
+
+
+type VectorInterfaceExp interface {
+	Len() int
+	Cap() int
+}
+
+
+func checkSizeExp(t *testing.T, v VectorInterfaceExp, len, cap int) {
+	if v.Len() != len {
+		t.Errorf("%T expected len = %d; found %d", v, len, v.Len())
+	}
+	if v.Cap() < cap {
+		t.Errorf("%T expected cap >= %d; found %d", v, cap, v.Cap())
+	}
+}
+
+
+func valExp(i int) int { return i*991 - 1234 }
+
+
+func TestSortingExp(t *testing.T) {
+	const n = 100
+
+	a := new(IntVector).Resize(n, 0)
+	for i := n - 1; i >= 0; i-- {
+		a.Set(i, n-1-i)
+	}
+	if sort.IsSorted(a) {
+		t.Error("int vector not sorted")
+	}
+
+	b := new(StringVector).Resize(n, 0)
+	for i := n - 1; i >= 0; i-- {
+		b.Set(i, fmt.Sprint(n-1-i))
+	}
+	if sort.IsSorted(b) {
+		t.Error("string vector not sorted")
+	}
+}
+
+
+func tname(x interface{}) string { return fmt.Sprintf("%T: ", x) }
