c2c940e0dfb98f2454736549e2a9318944a91507
Devon H. O'Dell <devon.odell@gmail.com>
2009-12-25 08:24:00 +1100

    Update documentation around time.Sleep to specify its precision, and suggest a possible interface for more granular sleep times.
    Fixes issue #260.
    
    R=rsc, r
    CC=golang-dev
    https://golang.org/cl/181058

--- src/pkg/time/sleep.go
+++ src/pkg/time/sleep.go
@@ -9,6 +9,7 @@ import (
 	"syscall"
 )
 
-// Sleep pauses the current goroutine for ns nanoseconds.
-// It returns os.EINTR if interrupted.
+// Sleep pauses the current goroutine for at least ns nanoseconds. Higher resolution
+// sleeping may be provided by syscall.Nanosleep on some operating systems.
+// Sleep returns os.EINTR if interrupted.
 func Sleep(ns int64) os.Error { return os.NewSyscallError("sleep", syscall.Sleep(ns)) }
