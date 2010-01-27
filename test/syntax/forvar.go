2a01d7287851e988d1e093dbe5788d3631e770b5
Russ Cox <rsc@golang.org>
2010-01-26 23:13:22 -0800

    gc: improved syntax errors
    * example-based syntax errors (go.errors)
    
    * enable bison's more specific errors
      and translate grammar token names into
      tokens like ++
    
    * test cases
    
    R=ken2, r, ken3
    CC=golang-dev
    https://golang.org/cl/194085

---
+++ test/syntax/forvar.go
@@ -0,0 +1,10 @@
+// errchk $G -e $D/$F.go
+
+// Copyright 2010 The Go Authors.  All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+func main() {
+	for var x = 0; x < 10; x++ {	// ERROR "var declaration not allowed in for initializer"
