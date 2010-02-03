ec7f1e6711634b30c354d63d1121ac5b9da45dbf
Andrey Mirtchovski <mirtchovski@gmail.com>
2010-02-02 23:01:21 -0800

    sort: fix comment typo
    R=rsc
    CC=golang-dev
    https://golang.org/cl/198084

--- src/pkg/sort/sort.go
+++ src/pkg/sort/sort.go
@@ -12,7 +12,7 @@ package sort
 type Interface interface {
 	// Len is the number of elements in the collection.
 	Len() int
-	// Less returns whether the element with index i is should sort
+	// Less returns whether the element with index i should sort
 	// before the element with index j.
 	Less(i, j int) bool
 	// Swap swaps the elements with indexes i and j.
