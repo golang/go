6e3853e2945277e9ca741e372f64be601e8fd536
Rob Pike <r@golang.org>
2010-03-04 15:26:15 -0800

    add a test that structures pack.  6g fails.
    R=rsc
    CC=golang-dev
    https://golang.org/cl/237041

---
+++ test/bugs/bug260.go
@@ -0,0 +1,41 @@
+// $G $D/$F.go && $L $F.$A && ./$A.out
+
+// Test that structures pack densely, according to the alignment of the largest field.
+
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
+
+type T1 struct { x uint8 }
+type T2 struct { x uint16 }
+type T4 struct { x uint32 }
+
+func main() {
+	status := 0
+	var b1 [10]T1
+	a0, _ := strconv.Btoui64(fmt.Sprintf("%p", &b1[0])[2:], 16)
+	a1, _ := strconv.Btoui64(fmt.Sprintf("%p", &b1[1])[2:], 16)
+	if a1 != a0 + 1 {
+		fmt.Println("FAIL: alignment should be 1, is", a1-a0)
+		status = 1
+	}
+	var b2 [10]T2
+	a0, _ = strconv.Btoui64(fmt.Sprintf("%p", &b2[0])[2:], 16)
+	a1, _ = strconv.Btoui64(fmt.Sprintf("%p", &b2[1])[2:], 16)
+	if a1 != a0 + 2 {
+		fmt.Println("FAIL: alignment should be 2, is", a1-a0)
+		status = 1
+	}
+	var b4 [10]T4
+	a0, _ = strconv.Btoui64(fmt.Sprintf("%p", &b4[0])[2:], 16)
+	a1, _ = strconv.Btoui64(fmt.Sprintf("%p", &b4[1])[2:], 16)
+	if a1 != a0 + 4 {
+		fmt.Println("FAIL: alignment should be 4, is", a1-a0)
+		status = 1
+	}
+	os.Exit(status)
+}
