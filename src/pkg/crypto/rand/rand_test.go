3cec8c7ab57eeffe52320a3cf92949cbf4c83a4b
Russ Cox <rsc@golang.org>
2010-03-16 23:12:20 -0700

    crypto/rand: new package
    Until proven insufficient, rely on the operating system
    to give us good random bytes (/dev/urandom).
    
    R=agl1
    CC=golang-dev
    https://golang.org/cl/569044

---
+++ src/pkg/crypto/rand/rand_test.go
@@ -0,0 +1,27 @@
+// Copyright 2010 The Go Authors.  All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package rand
+
+import (
+	"bytes"
+	"compress/flate"
+	"testing"
+)
+
+func TestRead(t *testing.T) {
+	b := make([]byte, 4e6)
+	n, err := Read(b)
+	if n != len(b) || err != nil {
+		t.Fatalf("Read(buf) = %d, %s", n, err)
+	}
+
+	var z bytes.Buffer
+	f := flate.NewDeflater(&z, 5)
+	f.Write(b)
+	f.Close()
+	if z.Len() < len(b)*99/100 {
+		t.Fatalf("Compressed %d -> %d", len(b), z.Len())
+	}
+}
