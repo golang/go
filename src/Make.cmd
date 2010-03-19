d0f9300049e20397a39e6264d4588f2c3fb31d20
Rob Pike <r@golang.org>
2010-03-19 15:14:46 -0700

    Factor the Makefiles to make writing outside Makefiles easier.
    R=rsc
    CC=golang-dev
    https://golang.org/cl/650042

--- src/Make.cmd
+++ src/Make.cmd
@@ -2,22 +2,22 @@
 # Use of this source code is governed by a BSD-style
 # license that can be found in the LICENSE file.
 
-# GNU Make syntax:
-ifndef GOBIN
-GOBIN=$(HOME)/bin
-endif
 
-# ugly hack to deal with whitespaces in $GOBIN
+# ugly hack to deal with whitespaces in $GOROOT
 nullstring :=
 space := $(nullstring) # a space at the end
-QUOTED_GOBIN=$(subst $(space),\ ,$(GOBIN))
+QUOTED_GOROOT:=$(subst $(space),\ ,$(GOROOT))
+
+include $(QUOTED_GOROOT)/src/Make.common
+
+PREREQ+=$(patsubst %,%.make,$(DEPS))
 
 all: $(TARG)
 
 $(TARG): _go_.$O $(OFILES)
 	$(QUOTED_GOBIN)/$(LD) -o $@ _go_.$O $(OFILES)
 
-_go_.$O: $(GOFILES)
+_go_.$O: $(GOFILES) $(PREREQ)
 	$(QUOTED_GOBIN)/$(GC) -o $@ $(GOFILES)
 
 install: $(QUOTED_GOBIN)/$(TARG)
@@ -25,8 +25,7 @@ install: $(QUOTED_GOBIN)/$(TARG)
 $(QUOTED_GOBIN)/$(TARG): $(TARG)
 	cp -f $(TARG) $(QUOTED_GOBIN)
 
-clean:
-	rm -f *.[$(OS)] $(TARG) $(CLEANFILES)
+CLEANFILES+=$(TARG)
 
-nuke:
-	rm -f *.[$(OS)] $(TARG) $(CLEANFILES) $(QUOTED_GOBIN)/$(TARG)
+nuke: clean
+	rm -f $(QUOTED_GOBIN)/$(TARG)
