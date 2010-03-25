a291e998c1c629af5a17ff431bd9380cb1530ba0
Robert Griesemer <gri@golang.org>
2010-03-25 10:01:51 -0700

    bug264: test case for issue 692
    R=r
    CC=golang-dev
    https://golang.org/cl/715042

---
+++ test/bugs/bug264.go
@@ -0,0 +1,49 @@
+// $G $D/$F.go && $L $F.$A && ./$A.out
+
+// Copyright 2010 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Test case for http://code.google.com/p/go/issues/detail?id=692
+
+package main
+
+import "fmt"
+
+var fooCount = 0
+var barCount = 0
+var balCount = 0
+
+func foo() (int, int) {
+	fooCount++
+	fmt.Println("foo")
+	return 0, 0
+}
+
+func bar() (int, int) {
+	barCount++
+	fmt.Println("bar")
+	return 0, 0
+}
+
+func bal() (int, int) {
+	balCount++
+	fmt.Println("bal")
+	return 0, 0
+}
+
+var a, b = foo() // foo is called once
+var c, _ = bar() // bar is called twice
+var _, _ = bal() // bal is called twice
+
+func main() {
+	if fooCount != 1 {
+		panic("BUG: fooCount != 1")
+	}
+	if barCount != 1 {
+		panic("BUG: barCount != 1")
+	}
+	if balCount != 1 {
+		panic("BUG: balCount != 1")
+	}
+}
