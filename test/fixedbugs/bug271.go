253fd30e667efc34f25be4e65ac0f451b712da5a
Russ Cox <rsc@golang.org>
2010-04-30 13:27:08 -0700

    gc: bug271
    Fixes #662.
    
    R=ken2
    CC=golang-dev
    https://golang.org/cl/978043

---
+++ test/fixedbugs/bug271.go
@@ -0,0 +1,20 @@
+// $G $D/$F.go && $L $F.$A && ./$A.out
+
+// Copyright 2010 The Go Authors.  All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// http://code.google.com/p/go/issues/detail?id=662
+
+package main
+
+import "fmt"
+
+func f() (int, int) { return 1, 2 }
+
+func main() {
+	s := fmt.Sprint(f())
+	if s != "1 2" {	// with bug, was "{1 2}"
+		println("BUG")
+	}
+}
