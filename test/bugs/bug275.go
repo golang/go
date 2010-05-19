a2636eea06471687d47ff8ed8255063d91acb756
Robert Griesemer <gri@golang.org>
2010-05-19 13:10:00 -0700

    bug275: cannot apply real() to a complex array element
    R=r, ken2
    CC=golang-dev
    https://golang.org/cl/1260041

---
+++ test/bugs/bug275.go
@@ -0,0 +1,20 @@
+// $G $D/$F.go || echo BUG should compile
+
+// Copyright 2010 The Go Authors.  All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// This is a test case for issue 788.
+
+package main
+
+func main() {
+	var a [1]complex64
+
+	t := a[0]
+	_ = real(t) // this works
+
+	_ = real(a[0]) // this doesn't
+}
+
+// bug275.go:17: internal compiler error: subnode not addable
