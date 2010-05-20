ce52cb19c1ba23999e61a88a067d869c925293f9
Nigel Tao <nigeltao@golang.org>
2010-05-20 13:57:18 -0700

    Fast-ish path for drawing onto an image.RGBA destination.
    Time to draw.Draw a 200x200 image fell from 18.4ms (and 1 malloc) to
    5.6ms (and 0 mallocs). It's still relatively slow since it assumes
    nothing about the src or mask images, but it does remove the malloc.
    There are existing faster, more specialized paths for copies, fills
    and image glyph masks.
    
    Also added a "compare to a slow but obviously correct implementation"
    check to draw_test.go.
    
    R=rsc, r
    CC=golang-dev
    https://golang.org/cl/1223044

--- src/pkg/exp/draw/draw_test.go
+++ src/pkg/exp/draw/draw_test.go
@@ -94,12 +94,66 @@ var drawTests = []drawTest{
 	drawTest{"genericSrc", fillBlue(255), vgradAlpha(192), Src, image.RGBAColor{0, 0, 102, 102}},
 }
 
+func makeGolden(dst image.Image, t drawTest) image.Image {
+	// Since golden is a newly allocated image, we don't have to check if the
+	// input source and mask images and the output golden image overlap.
+	golden := image.NewRGBA(dst.Width(), dst.Height())
+	for y := 0; y < golden.Height(); y++ {
+		my, sy := y, y
+		for x := 0; x < golden.Width(); x++ {
+			mx, sx := x, x
+			const M = 1<<16 - 1
+			var dr, dg, db, da uint32
+			if t.op == Over {
+				dr, dg, db, da = dst.At(x, y).RGBA()
+				dr >>= 16
+				dg >>= 16
+				db >>= 16
+				da >>= 16
+			}
+			sr, sg, sb, sa := t.src.At(sx, sy).RGBA()
+			sr >>= 16
+			sg >>= 16
+			sb >>= 16
+			sa >>= 16
+			ma := uint32(M)
+			if t.mask != nil {
+				_, _, _, ma = t.mask.At(mx, my).RGBA()
+				ma >>= 16
+			}
+			a := M - (sa * ma / M)
+			golden.Set(x, y, image.RGBA64Color{
+				uint16((dr*a + sr*ma) / M),
+				uint16((dg*a + sg*ma) / M),
+				uint16((db*a + sb*ma) / M),
+				uint16((da*a + sa*ma) / M),
+			})
+		}
+	}
+	return golden
+}
+
 func TestDraw(t *testing.T) {
-	for _, test := range drawTests {
+loop: for _, test := range drawTests {
 		dst := hgradRed(255)
-		DrawMask(dst, Rect(0, 0, 16, 16), test.src, ZP, test.mask, ZP, test.op)
+		// Draw the (src, mask, op) onto a copy of dst using a slow but obviously correct implementation.
+		golden := makeGolden(dst, test)
+		// Draw the same combination onto the actual dst using the optimized DrawMask implementation.
+		DrawMask(dst, Rect(0, 0, dst.Width(), dst.Height()), test.src, ZP, test.mask, ZP, test.op)
+		// Check that the resultant pixel at (8, 8) matches what we expect
+		// (the expected value can be verified by hand).
 		if !eq(dst.At(8, 8), test.expected) {
-			t.Errorf("draw %s: %v versus %v", test.desc, dst.At(8, 8), test.expected)
+			t.Errorf("draw %s: at (8, 8) %v versus %v", test.desc, dst.At(8, 8), test.expected)
+			continue
+		}
+		// Check that the resultant dst image matches the golden output.
+		for y := 0; y < golden.Height(); y++ {
+			for x := 0; x < golden.Width(); x++ {
+				if !eq(dst.At(x, y), golden.At(x, y)) {
+					t.Errorf("draw %s: at (%d, %d), %v versus golden %v", test.desc, x, y, dst.At(x, y), golden.At(x, y))
+					continue loop
+				}
+			}
 		}
 	}
 }
