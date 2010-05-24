43c0a46ac6a68d931cc7e067ba9156fb08d5de43
Russ Cox <rsc@golang.org>
2010-05-24 15:25:09 -0700

    gc: bug280
    Fixes #808.
    
    R=ken2
    CC=golang-dev
    https://golang.org/cl/1273042

---
+++ test/fixedbugs/bug280.go
@@ -0,0 +1,13 @@
+// errchk $G $D/$F.go
+
+// Copyright 2010 The Go Authors.  All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// http://code.google.com/p/go/issues/detail?id=808
+
+package main
+
+type A [...]int	// ERROR "outside of array literal"
+
+
