129c6c14d1426d0f75aca82e642434ea348ea153
Daniel Theophanes <kardianos@gmail.com>
2010-06-07 14:18:42 -0700

    runtime: finish pchw -> tiny, added gettime for tiny
    R=rsc
    CC=golang-dev
    https://golang.org/cl/1514041

--- src/pkg/runtime/tiny/386/signal.c
+++ src/pkg/runtime/tiny/386/signal.c
@@ -1,4 +1,8 @@
-// just the write function
+// Copyright 2010 The Go Authors.  All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file. 
+
+#include "runtime.h"
 
 extern void ·write(int32 fd, void *v, int32 len, int32 cap);	// slice, spelled out
 
@@ -7,4 +11,9 @@ write(int32 fd, void *v, int32 len)
 {
 	·write(fd, v, len, len);
 	return len;
+}
+
+void
+gettime(int64*, int32*) 
+{
 }
