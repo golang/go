2c59107d68bea9cd11c72ca0da08c7fc8eeb60df
Ian Lance Taylor <iant@golang.org>
2010-07-12 06:34:36 -0700

    Test case that gccgo fails (crashes rather than printing error).
    R=rsc
    CC=Raj_, golang-dev
    https://golang.org/cl/1740044

---
+++ test/fixedbugs/bug289.go
@@ -0,0 +1,26 @@
+// errchk $G $D/$F.go
+
+// Copyright 2010 The Go Authors.  All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// https://code.google.com/p/gofrontend/issues/detail?id=1
+
+package main
+
+func f1() {
+	a, b := f()	// ERROR "mismatch|does not match"
+	_ = a
+	_ = b
+}
+
+func f2() {
+	var a, b int
+	a, b = f()	// ERROR "mismatch|does not match"
+	_ = a
+	_ = b
+}
+
+func f() int {
+	return 1;
+}
