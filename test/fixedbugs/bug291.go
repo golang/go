08a263a991c2e5b38eebe6955ecc96372822661a
Russ Cox <rsc@golang.org>
2010-07-15 15:17:42 -0700

    gc: bug291
    Fixes #915.
    
    R=ken2
    CC=golang-dev
    https://golang.org/cl/1856042

---
+++ test/fixedbugs/bug291.go
@@ -0,0 +1,23 @@
+// $G $D/$F.go && $L $F.$A && ./$A.out
+
+// Copyright 2010 The Go Authors.  All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// http://code.google.com/p/go/issues/detail?id=915
+
+package main
+
+type T struct {
+	x int
+}
+
+var t = &T{42}
+var i interface{} = t
+var tt, ok = i.(*T)
+
+func main() {
+	if tt == nil || tt.x != 42 {
+		println("BUG")
+	}
+}
