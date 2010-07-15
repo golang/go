9b64fef71acf71175bc74e2d04ea7fc0a011b03b
Brad Fitzpatrick <brad@danga.com>
2010-07-14 17:26:14 -0700

    mime/multipart and HTTP multipart/form-data support
    Somewhat of a work-in-progress (in that MIME is a large spec), but this is
    functional and enough for discussion and/or code review.
    
    In addition to the unit tests, I've tested with curl and Chrome with
    a variety of test files, making sure the digests of files are unaltered
    when read via a multipart Part.
    
    R=rsc, adg, dsymonds1, agl1
    CC=golang-dev
    https://golang.org/cl/1681049

---
+++ src/pkg/mime/grammar.go
@@ -0,0 +1,36 @@
+// Copyright 2010 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package mime
+
+import (
+	"strings"
+)
+
+// isTSpecial returns true if rune is in 'tspecials' as defined by RFC
+// 1531 and RFC 2045.
+func isTSpecial(rune int) bool {
+	return strings.IndexRune(`()<>@,;:\"/[]?=`, rune) != -1
+}
+
+// IsTokenChar returns true if rune is in 'token' as defined by RFC
+// 1531 and RFC 2045.
+func IsTokenChar(rune int) bool {
+	// token := 1*<any (US-ASCII) CHAR except SPACE, CTLs,
+	//             or tspecials>
+	return rune > 0x20 && rune < 0x7f && !isTSpecial(rune)
+}
+
+// IsQText returns true if rune is in 'qtext' as defined by RFC 822.
+func IsQText(rune int) bool {
+	// CHAR        =  <any ASCII character>        ; (  0-177,  0.-127.)
+	// qtext       =  <any CHAR excepting <">,     ; => may be folded
+	//                "\" & CR, and including
+	//                linear-white-space>
+	switch rune {
+	case int('"'), int('\\'), int('\r'):
+		return false
+	}
+	return rune < 0x80
+}
