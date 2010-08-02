3f5966dcc0246b05604f14441566b67e10ab6614
Rob Pike <r@golang.org>
2010-08-03 08:04:33 +1000

    io: consolidate multi_reader and multi_writer into a single file, multi.go
    R=rsc
    CC=golang-dev
    https://golang.org/cl/1860046

--- src/pkg/io/multi.go
+++ src/pkg/io/multi.go
@@ -34,3 +34,27 @@ func (mr *multiReader) Read(p []byte) (n int, err os.Error) {
 func MultiReader(readers ...Reader) Reader {
 	return &multiReader{readers}
 }
+
+type multiWriter struct {
+	writers []Writer
+}
+
+func (t *multiWriter) Write(p []byte) (n int, err os.Error) {
+	for _, w := range t.writers {
+		n, err = w.Write(p)
+		if err != nil {
+			return
+		}
+		if n != len(p) {
+			err = ErrShortWrite
+			return
+		}
+	}
+	return len(p), nil
+}
+
+// MultiWriter creates a writer that duplicates its writes to all the
+// provided writers, similar to the Unix tee(1) command.
+func MultiWriter(writers ...Writer) Writer {
+	return &multiWriter{writers}
+}
