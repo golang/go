e0ea2049ad6dd9154e33617c815235178814dd58
Ian Lance Taylor <iant@golang.org>
2010-08-31 15:48:04 -0700

    test: Match gccgo error messages.
    Another case where gccgo and gc report similar but not
    identical errors for a recursive interface.
    
    bug251.go:11: error: invalid recursive interface
    
    R=rsc
    CC=golang-dev
    https://golang.org/cl/2094041

--- test/fixedbugs/bug251.go
+++ test/fixedbugs/bug251.go
@@ -8,14 +8,14 @@ package main
 
 type I1 interface {
 	m() I2
-	I2
+	I2 // GCCGO_ERROR "loop|interface"
 }
 
 type I2 interface {
-	I1 // ERROR "loop|interface"
+	I1 // GC_ERROR "loop|interface"
 }
 
 
-var i1 I1 = i2 // ERROR "missing m method|need type assertion"
+var i1 I1 = i2 // GC_ERROR "missing m method|need type assertion"
 var i2 I2
 var i2a I2 = i1
