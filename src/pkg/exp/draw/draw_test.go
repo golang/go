bd66de56c525ef5d243ed6b71c37a2dc6a74a249
Nigel Tao <nigeltao@golang.org>
2010-09-10 20:09:56 +1000

    exp/draw: fast path for drawing overlapping image.RGBAs.
    R=r, nigeltao
    CC=golang-dev
    https://golang.org/cl/2145045

--- src/pkg/exp/draw/draw_test.go
+++ src/pkg/exp/draw/draw_test.go
@@ -53,6 +53,16 @@ func hgradRed(alpha int) Image {
 	return m
 }
 
+func gradYellow(alpha int) Image {
+	m := image.NewRGBA(16, 16)
+	for y := 0; y < 16; y++ {
+		for x := 0; x < 16; x++ {
+			m.Set(x, y, image.RGBAColor{uint8(x * alpha / 15), uint8(y * alpha / 15), 0, uint8(alpha)})
+		}
+	}
+	return m
+}
+
 type drawTest struct {
 	desc     string
 	src      image.Image
@@ -94,24 +104,31 @@ var drawTests = []drawTest{
 	drawTest{"genericSrc", fillBlue(255), vgradAlpha(192), Src, image.RGBAColor{0, 0, 102, 102}},
 }
 
-func makeGolden(dst image.Image, t drawTest) image.Image {
+func makeGolden(dst, src, mask image.Image, op Op) image.Image {
 	// Since golden is a newly allocated image, we don't have to check if the
 	// input source and mask images and the output golden image overlap.
 	b := dst.Bounds()
-	golden := image.NewRGBA(b.Dx(), b.Dy())
+	sx0 := src.Bounds().Min.X - b.Min.X
+	sy0 := src.Bounds().Min.Y - b.Min.Y
+	var mx0, my0 int
+	if mask != nil {
+		mx0 = mask.Bounds().Min.X - b.Min.X
+		my0 = mask.Bounds().Min.Y - b.Min.Y
+	}
+	golden := image.NewRGBA(b.Max.X, b.Max.Y)
 	for y := b.Min.Y; y < b.Max.Y; y++ {
-		my, sy := y, y
+		my, sy := my0+y, sy0+y
 		for x := b.Min.X; x < b.Max.X; x++ {
-			mx, sx := x, x
+			mx, sx := mx0+x, sx0+x
 			const M = 1<<16 - 1
 			var dr, dg, db, da uint32
-			if t.op == Over {
+			if op == Over {
 				dr, dg, db, da = dst.At(x, y).RGBA()
 			}
-			sr, sg, sb, sa := t.src.At(sx, sy).RGBA()
+			sr, sg, sb, sa := src.At(sx, sy).RGBA()
 			ma := uint32(M)
-			if t.mask != nil {
-				_, _, _, ma = t.mask.At(mx, my).RGBA()
+			if mask != nil {
+				_, _, _, ma = mask.At(mx, my).RGBA()
 			}
 			a := M - (sa * ma / M)
 			golden.Set(x, y, image.RGBA64Color{
@@ -122,6 +139,7 @@ func makeGolden(dst image.Image, t drawTest) image.Image {
 			})
 		}
 	}
+	golden.Rect = b
 	return golden
 }
 
@@ -130,14 +148,14 @@ loop:
 	for _, test := range drawTests {
 		dst := hgradRed(255)
 		// Draw the (src, mask, op) onto a copy of dst using a slow but obviously correct implementation.
+		golden := makeGolden(dst, test.src, test.mask, test.op)
 		b := dst.Bounds()
-		golden := makeGolden(dst, test)
 		if !b.Eq(golden.Bounds()) {
 			t.Errorf("draw %s: bounds %v versus %v", test.desc, dst.Bounds(), golden.Bounds())
 			continue
 		}
 		// Draw the same combination onto the actual dst using the optimized DrawMask implementation.
-		DrawMask(dst, image.Rect(b.Min.X, b.Min.Y, b.Max.X, b.Max.Y), test.src, image.ZP, test.mask, image.ZP, test.op)
+		DrawMask(dst, b, test.src, image.ZP, test.mask, image.ZP, test.op)
 		// Check that the resultant pixel at (8, 8) matches what we expect
 		// (the expected value can be verified by hand).
 		if !eq(dst.At(8, 8), test.expected) {
@@ -150,6 +168,45 @@ loop:
 				if !eq(dst.At(x, y), golden.At(x, y)) {
 					t.Errorf("draw %s: at (%d, %d), %v versus golden %v", test.desc, x, y, dst.At(x, y), golden.At(x, y))
 					continue loop
+				}
+			}
+		}
+	}
+}
+
+func TestDrawOverlap(t *testing.T) {
+	for _, op := range []Op{Over, Src} {
+		for yoff := -2; yoff <= 2; yoff++ {
+		loop:
+			for xoff := -2; xoff <= 2; xoff++ {
+				m := gradYellow(127).(*image.RGBA)
+				dst := &image.RGBA{
+					Pix:    m.Pix,
+					Stride: m.Stride,
+					Rect:   image.Rect(5, 5, 10, 10),
+				}
+				src := &image.RGBA{
+					Pix:    m.Pix,
+					Stride: m.Stride,
+					Rect:   image.Rect(5+xoff, 5+yoff, 10+xoff, 10+yoff),
+				}
+				// Draw the (src, mask, op) onto a copy of dst using a slow but obviously correct implementation.
+				golden := makeGolden(dst, src, nil, op)
+				b := dst.Bounds()
+				if !b.Eq(golden.Bounds()) {
+					t.Errorf("drawOverlap xoff=%d,yoff=%d: bounds %v versus %v", xoff, yoff, dst.Bounds(), golden.Bounds())
+					continue
+				}
+				// Draw the same combination onto the actual dst using the optimized DrawMask implementation.
+				DrawMask(dst, b, src, src.Bounds().Min, nil, image.ZP, op)
+				// Check that the resultant dst image matches the golden output.
+				for y := b.Min.Y; y < b.Max.Y; y++ {
+					for x := b.Min.X; x < b.Max.X; x++ {
+						if !eq(dst.At(x, y), golden.At(x, y)) {
+							t.Errorf("drawOverlap xoff=%d,yoff=%d: at (%d, %d), %v versus golden %v", xoff, yoff, x, y, dst.At(x, y), golden.At(x, y))
+							continue loop
+						}
+					}
 				}
 			}
 		}
