b155a76a407f8f3f8b48aeeb74d98b25ba27bab4
Joe Poirier <jdpoirier@gmail.com>
2010-09-12 17:38:36 +1000

    exec.LookPath() unix/windows separation
    R=brainman, rsc, vcc, rsc1
    CC=golang-dev
    https://golang.org/cl/2068041

---
+++ src/pkg/exec/lp_unix.go
@@ -0,0 +1,45 @@
+// Copyright 2010 The Go Authors.  All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package exec
+
+import (
+	"os"
+	"strings"
+)
+
+func canExec(file string) bool {
+	d, err := os.Stat(file)
+	if err != nil {
+		return false
+	}
+	return d.IsRegular() && d.Permission()&0111 != 0
+}
+
+// LookPath searches for an executable binary named file
+// in the directories named by the PATH environment variable.
+// If file contains a slash, it is tried directly and the PATH is not consulted.
+func LookPath(file string) (string, os.Error) {
+	// NOTE(rsc): I wish we could use the Plan 9 behavior here
+	// (only bypass the path if file begins with / or ./ or ../)
+	// but that would not match all the Unix shells.
+
+	if strings.Index(file, "/") >= 0 {
+		if canExec(file) {
+			return file, nil
+		}
+		return "", os.ENOENT
+	}
+	pathenv := os.Getenv("PATH")
+	for _, dir := range strings.Split(pathenv, ":", -1) {
+		if dir == "" {
+			// Unix shell semantics: path element "" means "."
+			dir = "."
+		}
+		if canExec(dir + "/" + file) {
+			return dir + "/" + file, nil
+		}
+	}
+	return "", os.ENOENT
+}
---
+++ src/pkg/exec/lp_windows.go
@@ -0,0 +1,66 @@
+// Copyright 2010 The Go Authors.  All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package exec
+
+import (
+	"os"
+	"strings"
+)
+
+func chkStat(file string) bool {
+	d, err := os.Stat(file)
+	if err != nil {
+		return false
+	}
+	return d.IsRegular()
+}
+
+func canExec(file string, exts []string) (string, bool) {
+	if len(exts) == 0 {
+		return file, chkStat(file)
+	}
+	f := strings.ToLower(file)
+	for _, e := range exts {
+		if strings.HasSuffix(f, e) {
+			return file, chkStat(file)
+		}
+	}
+	for _, e := range exts {
+		if f := file + e; chkStat(f) {
+			return f, true
+		}
+	}
+	return ``, false
+}
+
+func LookPath(file string) (string, os.Error) {
+	exts := []string{}
+	if x := os.Getenv(`PATHEXT`); x != `` {
+		exts = strings.Split(strings.ToLower(x), `;`, -1)
+		for i, e := range exts {
+			if e == `` || e[0] != '.' {
+				exts[i] = `.` + e
+			}
+		}
+	}
+	if strings.Index(file, `\`) >= 0 || strings.Index(file, `/`) >= 0 {
+		if f, ok := canExec(file, exts); ok {
+			return f, nil
+		}
+		return ``, os.ENOENT
+	}
+	if pathenv := os.Getenv(`PATH`); pathenv == `` {
+		if f, ok := canExec(`.\`+file, exts); ok {
+			return f, nil
+		}
+	} else {
+		for _, dir := range strings.Split(pathenv, `;`, -1) {
+			if f, ok := canExec(dir+`\`+file, exts); ok {
+				return f, nil
+			}
+		}
+	}
+	return ``, os.ENOENT
+}
