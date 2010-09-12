eddddf042dcdc211658589dd294040ac5b585794
Russ Cox <rsc@golang.org>
2010-09-11 23:36:06 -0400

    arm: make libcgo build during OS X cross-compile
    The gcc on OS X seems pickier about noticing
    symbols that are referred to but not found during
    the link of a .so file.
    
    R=r
    CC=golang-dev
    https://golang.org/cl/2098047

--- src/libcgo/linux_arm.c
+++ src/libcgo/linux_arm.c
@@ -1,1 +1,17 @@
-/* unimplemented */
+// Copyright 2010 The Go Authors.  All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+#include "libcgo.h"
+
+void
+initcgo(void)
+{
+}
+
+void
+libcgo_sys_thread_start(ThreadStart *ts)
+{
+	// unimplemented
+	*(int*)0 = 0;
+}
