7283b08a1b7f54cf7d668f2a2fb830483419dc2d
Ian Lance Taylor <iant@golang.org>
2010-09-24 17:07:42 -0700

    test: Add test which causes incorrect error from gccgo.
    R=gri
    CC=golang-dev
    https://golang.org/cl/2220048

---
+++ test/fixedbugs/bug307.go
@@ -0,0 +1,15 @@
+// $G $D/$F.go
+
+// Copyright 2010 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Valid program, gccgo reported an error.
+// bug307.go:14:6: error: cmplx arguments must have identical types
+
+package main
+
+func main() {
+	var f float64
+	_ = cmplx(1 / f, 0)
+}
