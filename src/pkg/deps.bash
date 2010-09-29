45164a1f466866c7aa1f68d8ac6015a4f60c04c7
Russ Cox <rsc@golang.org>
2010-09-28 23:51:55 -0400

    build: clear custom variables like GREP_OPTIONS
    Fixes #946.
    
    R=r, r2
    CC=golang-dev
    https://golang.org/cl/2137048

--- src/pkg/deps.bash
+++ src/pkg/deps.bash
@@ -3,6 +3,8 @@
 # Use of this source code is governed by a BSD-style
 # license that can be found in the LICENSE file.
 
+eval $(gomake --no-print-directory -f ../Make.inc go-env)
+
 OUT="Make.deps"
 TMP="Make.deps.tmp"
 
