8a484555e76d50f4510c8ca6a4dc7340808bf177
Andrew Gerrand <adg@golang.org>
2010-10-11 13:45:26 +1100

    time: add After
    Permits one to easily put a timeout in a select:
    
    select {
    case <-ch:
    	// foo
    case <-time.After(1e6):
    	// bar
    }
    
    R=r, rog, rsc, sameer1, PeterGo, iant, nigeltao_gnome
    CC=golang-dev
    https://golang.org/cl/2321043

--- src/pkg/time/sleep.go
+++ src/pkg/time/sleep.go
@@ -9,18 +9,38 @@ import (
 	"syscall"
 )
 
-// Sleep pauses the current goroutine for at least ns nanoseconds. Higher resolution
-// sleeping may be provided by syscall.Nanosleep on some operating systems.
+// Sleep pauses the current goroutine for at least ns nanoseconds.
+// Higher resolution sleeping may be provided by syscall.Nanosleep 
+// on some operating systems.
 func Sleep(ns int64) os.Error {
-	// TODO(cw): use monotonic-time once it's available
+	_, err := sleep(Nanoseconds(), ns)
+	return err
+}
+
+// After waits at least ns nanoseconds before sending the current time
+// on the returned channel.
+func After(ns int64) <-chan int64 {
 	t := Nanoseconds()
+	ch := make(chan int64, 1)
+	go func() {
+		t, _ = sleep(t, ns)
+		ch <- t
+	}()
+	return ch
+}
+
+// sleep takes the current time and a duration,
+// pauses for at least ns nanoseconds, and
+// returns the current time and an error.
+func sleep(t, ns int64) (int64, os.Error) {
+	// TODO(cw): use monotonic-time once it's available
 	end := t + ns
 	for t < end {
 		errno := syscall.Sleep(end - t)
 		if errno != 0 && errno != syscall.EINTR {
-			return os.NewSyscallError("sleep", errno)
+			return 0, os.NewSyscallError("sleep", errno)
 		}
 		t = Nanoseconds()
 	}
-	return nil
+	return t, nil
 }
