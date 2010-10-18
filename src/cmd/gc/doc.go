c7d4e01ef1699288f29dd85d7bdc14e2a2801814
Andrew Gerrand <adg@golang.org>
2010-10-19 09:26:11 +1100

    gc: update usage string in doc.go
    R=rsc, r2, r
    CC=golang-dev
    https://golang.org/cl/2550041

--- src/cmd/gc/doc.go
+++ src/cmd/gc/doc.go
@@ -25,7 +25,10 @@ other packages. It is therefore not necessary when compiling client C of
 package P to read the files of P's dependencies, only the compiled output
 of P.
 
-Usage: 6g [flags] *.go  (or 8g or 5g)
+Usage:
+	6g [flags] file...
+The specified files must be Go source files and all part of the same package.
+Substitute 6g with 8g or 5g where appropriate.
 
 Flags:
 	-o file
