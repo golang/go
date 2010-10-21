833c06581a38393faaff26a1a4f5b847e333ea2b
Russ Cox <rsc@golang.org>
2010-10-21 12:50:23 -0400

    fix build
    R=ken2
    CC=golang-dev
    https://golang.org/cl/2621042

--- src/pkg/debug/gosym/pclinetest.s
+++ src/pkg/debug/gosym/pclinetest.s
@@ -51,37 +51,8 @@ TEXT main(SB),7,$0
 	CALL pcfromline(SB)
 
 // Keep the linker happy
-TEXT runtime·morestack(SB),7,$0
+TEXT main·main(SB),7,$0
 	RET
 
-TEXT runtime·morestack00(SB),7,$0
+TEXT main·init(SB),7,$0
 	RET
-
-TEXT runtime·morestack10(SB),7,$0
-	RET
-
-TEXT runtime·morestack01(SB),7,$0
-	RET
-
-TEXT runtime·morestack11(SB),7,$0
-	RET
-
-TEXT runtime·morestack8(SB),7,$0
-	RET
-
-TEXT runtime·morestack16(SB),7,$0
-	RET
-
-TEXT runtime·morestack24(SB),7,$0
-	RET
-
-TEXT runtime·morestack32(SB),7,$0
-	RET
-
-TEXT runtime·morestack40(SB),7,$0
-	RET
-
-TEXT runtime·morestack48(SB),7,$0
-	RET
-
-
