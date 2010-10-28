421cabb0898b680f608fdabf6bcd2fdbc320cae7
Andrew Gerrand <adg@golang.org>
2010-10-29 08:48:16 +1100

    container/list: document iteration
    R=rsc
    CC=golang-dev
    https://golang.org/cl/2768042

--- src/pkg/container/list/list.go
+++ src/pkg/container/list/list.go
@@ -3,6 +3,12 @@
 // license that can be found in the LICENSE file.
 
 // The list package implements a doubly linked list.
+//
+// To iterate over a list (where l is a *List):
+//	for e := l.Front(); e != nil; e = e.Next() {
+//		// do something with e.Value
+//	}
+//
 package list
 
 // Element is an element in the linked list.
