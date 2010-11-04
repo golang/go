85a388af9a7740d045ef998976dfabdea47fba54
Russ Cox <rsc@golang.org>
2010-11-04 16:04:04 -0400

    gc: line comments may end in EOF
    R=ken2
    CC=golang-dev
    https://golang.org/cl/2907041

---
+++ test/eof1.go
@@ -0,0 +1,9 @@
+// $G $D/$F.go
+
+// Copyright 2010 The Go Authors.  All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+// No newline at the end of this comment.
