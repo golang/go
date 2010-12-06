e2d1595c819ea6baeec42bae2107c141eb935b9e
Roger Peppe <rogpeppe@gmail.com>
2010-12-06 14:19:30 -0500

    time: make After use fewer goroutines and host processes. With credit to Gustavo Niemeyer, who hinted at this approach in #go-nuts.
    R=adg, rsc, niemeyer, r
    CC=golang-dev
    https://golang.org/cl/3416043

--- src/pkg/time/sleep.go
+++ src/pkg/time/sleep.go
@@ -7,7 +7,25 @@ package time
 import (
 	"os"
 	"syscall"
+	"sync"
+	"container/heap"
 )
+
+// The event type represents a single After event.
+type event struct {
+	t        int64        // The absolute time that the event should fire.
+	c        chan<- int64 // The channel to send on.
+	sleeping bool         // A sleeper is sleeping for this event.
+}
+
+type eventHeap []*event
+
+var events eventHeap
+var eventMutex sync.Mutex
+
+func init() {
+	events.Push(&event{1 << 62, nil, true}) // sentinel
+}
 
 // Sleep pauses the current goroutine for at least ns nanoseconds.
 // Higher resolution sleeping may be provided by syscall.Nanosleep 
@@ -15,18 +33,6 @@ import (
 func Sleep(ns int64) os.Error {
 	_, err := sleep(Nanoseconds(), ns)
 	return err
-}
-
-// After waits at least ns nanoseconds before sending the current time
-// on the returned channel.
-func After(ns int64) <-chan int64 {
-	t := Nanoseconds()
-	ch := make(chan int64, 1)
-	go func() {
-		t, _ = sleep(t, ns)
-		ch <- t
-	}()
-	return ch
 }
 
 // sleep takes the current time and a duration,
@@ -43,4 +49,88 @@ func sleep(t, ns int64) (int64, os.Error) {
 		t = Nanoseconds()
 	}
 	return t, nil
+}
+
+// After waits at least ns nanoseconds before sending the current time
+// on the returned channel.
+func After(ns int64) <-chan int64 {
+	c := make(chan int64, 1)
+	t := ns + Nanoseconds()
+	eventMutex.Lock()
+	t0 := events[0].t
+	heap.Push(events, &event{t, c, false})
+	if t < t0 {
+		go sleeper()
+	}
+	eventMutex.Unlock()
+	return c
+}
+
+// sleeper continually looks at the earliest event in the queue, marks it
+// as sleeping, waits until it happens, then removes any events
+// in the queue that are due. It stops when it finds an event that is
+// already marked as sleeping. When an event is inserted before the first item,
+// a new sleeper is started.
+//
+// Scheduling vagaries mean that sleepers may not wake up in
+// exactly the order of the events that they are waiting for,
+// but this does not matter as long as there are at least as
+// many sleepers as events marked sleeping (invariant). This ensures that
+// there is always a sleeper to service the remaining events.
+//
+// A sleeper will remove at least the event it has been waiting for
+// unless the event has already been removed by another sleeper.  Both
+// cases preserve the invariant described above.
+func sleeper() {
+	eventMutex.Lock()
+	e := events[0]
+	for !e.sleeping {
+		t := Nanoseconds()
+		if dt := e.t - t; dt > 0 {
+			e.sleeping = true
+			eventMutex.Unlock()
+			if nt, err := sleep(t, dt); err != nil {
+				// If sleep has encountered an error,
+				// there's not much we can do. We pretend
+				// that time really has advanced by the required
+				// amount and lie to the rest of the system.
+				t = e.t
+			} else {
+				t = nt
+			}
+			eventMutex.Lock()
+			e = events[0]
+		}
+		for t >= e.t {
+			e.c <- t
+			heap.Pop(events)
+			e = events[0]
+		}
+	}
+	eventMutex.Unlock()
+}
+
+func (eventHeap) Len() int {
+	return len(events)
+}
+
+func (eventHeap) Less(i, j int) bool {
+	return events[i].t < events[j].t
+}
+
+func (eventHeap) Swap(i, j int) {
+	events[i], events[j] = events[j], events[i]
+}
+
+func (eventHeap) Push(x interface{}) {
+	events = append(events, x.(*event))
+}
+
+func (eventHeap) Pop() interface{} {
+	// TODO: possibly shrink array.
+	n := len(events) - 1
+	e := events[n]
+	events[n] = nil
+	events = events[0:n]
+	return e
 }
