76d675219f5e94f60f1e4266dd1309b6880094cb
Russ Cox <rsc@golang.org>
2010-12-08 14:10:00 -0500

    runtime/cgo: take 2
    This is a second attempt at submitting
    https://golang.org/cl/3420043
    
    A Mercurial problem lost the new files
    in that submit.
    
    TBR=r
    CC=golang-dev
    https://golang.org/cl/3511043

---
+++ src/pkg/runtime/cgo/cgo.go
@@ -0,0 +1,10 @@
+// Copyright 2010 The Go Authors.  All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+/*
+Package cgo contains runtime support for code generated
+by the cgo tool.  See the documentation for the cgo command
+for details on using cgo.
+*/
+package cgo
