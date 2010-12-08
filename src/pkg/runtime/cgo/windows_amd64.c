98b2d7062e64bd68973d104500d3a21880b23789
Russ Cox <rsc@golang.org>
2010-12-08 14:33:17 -0500

    libcgo: delete (replaced by runtime/cgo)
    Move unported files (freebsd*, windows*, nacl*) to runtime/cgo.
    
    Step toward fixing FreeBSD build.
    
    R=r
    TBR=r
    CC=golang-dev
    https://golang.org/cl/3497042

--- src/pkg/runtime/cgo/windows_amd64.c
+++ src/pkg/runtime/cgo/windows_amd64.c
@@ -20,26 +20,26 @@ initcgo(void)
 void
 libcgo_sys_thread_start(ThreadStart *ts)
 {
-    ts->g->stackguard = STACKSIZE;
-    _beginthread(threadentry, STACKSIZE, ts);
+	ts->g->stackguard = STACKSIZE;
+	_beginthread(threadentry, STACKSIZE, ts);
 }
 
 static void*
 threadentry(void *v)
 {
-    ThreadStart ts;
+	ThreadStart ts;
 
-    ts = *(ThreadStart*)v;
-    free(v);
+	ts = *(ThreadStart*)v;
+	free(v);
 
-    ts.g->stackbase = (uintptr)&ts;
+	ts.g->stackbase = (uintptr)&ts;
 
-    /*
-     * libcgo_sys_thread_start set stackguard to stack size;
-     * change to actual guard pointer.
-     */
-    ts.g->stackguard = (uintptr)&ts - ts.g->stackguard + 4096;
+	/*
+	* libcgo_sys_thread_start set stackguard to stack size;
+	* change to actual guard pointer.
+	*/
+	ts.g->stackguard = (uintptr)&ts - ts.g->stackguard + 4096;
 
-    crosscall_386(ts.fn);
-    return nil;
+	crosscall_386(ts.fn);
+	return nil;
 }
