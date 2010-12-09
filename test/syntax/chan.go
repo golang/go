f980577064181bf8d24fe514c739305fce052968
Ryan Hitchman <hitchmanr@gmail.com>
2010-12-09 13:41:24 -0500

    gc: syntax error for incomplete chan type
    Fixes #932.
    
    R=rsc
    CC=golang-dev
    https://golang.org/cl/3515041

---
+++ test/syntax/chan.go
@@ -0,0 +1,17 @@
+// errchk $G -e $D/$F.go
+
+// Copyright 2010 The Go Authors.  All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+type xyz struct {
+    ch chan
+} // ERROR "unexpected } in channel type"
+
+func Foo(y chan) { // ERROR "unexpected \) in channel type"
+}
+
+func Bar(x chan, y int) { // ERROR "unexpected comma in channel type"
+}
