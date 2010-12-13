14122df3e17fbdd5a2363e483b4ca6ae335a6a2f
Russ Cox <rsc@golang.org>
2010-12-13 15:51:11 -0500

    json: check for invalid UTF-8
    Fixes #1250.
    
    R=r
    CC=golang-dev
    https://golang.org/cl/3562042

--- src/pkg/json/decode_test.go
+++ src/pkg/json/decode_test.go
@@ -102,6 +102,20 @@ func TestMarshal(t *testing.T) {
 	}
 }
 
+func TestMarshalBadUTF8(t *testing.T) {
+	s := "hello\xffworld"
+	b, err := Marshal(s)
+	if err == nil {
+		t.Fatal("Marshal bad UTF8: no error")
+	}
+	if len(b) != 0 {
+		t.Fatal("Marshal returned data")
+	}
+	if _, ok := err.(*InvalidUTF8Error); !ok {
+		t.Fatal("Marshal did not return InvalidUTF8Error: %T %v", err, err)
+	}
+}
+
 func TestUnmarshal(t *testing.T) {
 	var scan scanner
 	for i, tt := range unmarshalTests {
