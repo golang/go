a988078971c0437b370e2ff84749d10089c76dc3
Robert Griesemer <gri@golang.org>
2011-01-13 17:26:38 -0800

    fix build (submitted a modified parser.go file by mistake)
    R=r
    CC=golang-dev
    https://golang.org/cl/4005041

--- src/pkg/go/parser/parser.go
+++ src/pkg/go/parser/parser.go
@@ -69,8 +69,8 @@ func scannerMode(mode uint) uint {
 }
 
 
-func (p *parser) init(file *token.File, src []byte, mode uint) {
-	p.file = file
+func (p *parser) init(fset *token.FileSet, filename string, src []byte, mode uint) {
+	p.file = fset.AddFile(filename, fset.Base(), len(src))
 	p.scanner.Init(p.file, src, p, scannerMode(mode))
 	p.mode = mode
 	p.trace = mode&Trace != 0 // for convenience (p.trace is used frequently)
