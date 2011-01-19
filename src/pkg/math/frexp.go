ee0d5752d93d4badbe7866f09b30ff467e31f3be
Eoghan Sherry <ejsherry@gmail.com>
2011-01-19 14:23:59 -0500

    math: handle denormals in Frexp, Ilogb, Ldexp, and Logb
    Also:
    * document special cases for Frexp and Ldexp
    * handle ±Inf in Ldexp
    * correctly return -0 on underflow in Ldexp
    * test special cases for Ldexp
    * test boundary cases for Frexp, Ilogb, Ldexp, and Logb
    
    R=rsc
    CC=golang-dev
    https://golang.org/cl/3676041

--- src/pkg/math/frexp.go
+++ src/pkg/math/frexp.go
@@ -8,6 +8,11 @@ package math
 // and an integral power of two.
 // It returns frac and exp satisfying f == frac × 2**exp,
 // with the absolute value of frac in the interval [½, 1).
+//
+// Special cases are:
+//	Frexp(±0) = ±0, 0
+//	Frexp(±Inf) = ±Inf, 0
+//	Frexp(NaN) = NaN, 0
 func Frexp(f float64) (frac float64, exp int) {
 	// TODO(rsc): Remove manual inlining of IsNaN, IsInf
 	// when compiler does it for us
@@ -18,8 +23,9 @@ func Frexp(f float64) (frac float64, exp int) {
 	case f < -MaxFloat64 || f > MaxFloat64 || f != f: // IsInf(f, 0) || IsNaN(f):
 		return f, 0
 	}
+	f, exp = normalize(f)
 	x := Float64bits(f)
-	exp = int((x>>shift)&mask) - bias + 1
+	exp += int((x>>shift)&mask) - bias + 1
 	x &^= mask << shift
 	x |= (-1 + bias) << shift
 	frac = Float64frombits(x)
