9ec785af2f97b3208c694302aad72e4c1ef4782d
Andrew Gerrand <adg@golang.org>
2011-01-27 10:32:41 +1000

    doc/codelab/wiki: replace curl with a Go program
    R=rsc, bradfitzgo
    CC=golang-dev
    https://golang.org/cl/4087043

--- doc/codelab/wiki/test.sh
+++ doc/codelab/wiki/test.sh
@@ -12,11 +12,11 @@ trap cleanup INT
 
 sleep 1
 
-curl -s -o test_edit.out http://localhost:8080/edit/Test 
+./get.bin http://localhost:8080/edit/Test > test_edit.out
 diff -u test_edit.out test_edit.good || cleanup 1
-curl -s -o /dev/null -d body=some%20content http://localhost:8080/save/Test
+./get.bin -post=body=some%20content http://localhost:8080/save/Test
 diff -u Test.txt test_Test.txt.good || cleanup 1
-curl -s -o test_view.out http://localhost:8080/view/Test
+./get.bin http://localhost:8080/view/Test > test_view.out
 diff -u test_view.out test_view.good || cleanup 1
 
 echo "Passed"
