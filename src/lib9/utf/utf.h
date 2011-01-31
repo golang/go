bf751f088b1750cc5ca59a9369b0c384c833888d
Russ Cox <rsc@golang.org>
2011-01-31 12:38:54 -0500

    lib9: update to Unicode 6.0.0
    R=r
    CC=golang-dev
    https://golang.org/cl/4121042

--- src/lib9/utf/utf.h
+++ src/lib9/utf/utf.h
@@ -224,12 +224,6 @@ int isalpharune(Rune r);
 int isdigitrune(Rune r);
 
 
-// isideographicrune tests for ideographic characters and numbers, as
-// defined by the Unicode standard.
-
-int isideographicrune(Rune r);
-
-
 // isspacerune tests for whitespace characters, including "C" locale
 // whitespace, Unicode defined whitespace, and the "zero-width
 // non-break space" character.
