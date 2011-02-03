8bf0bc2754a4e168268a4d90a18f10063ab8c8c8
Rob Pike <r@golang.org>
2011-02-03 11:55:33 -0800

    fmt: document %%
    R=rsc, gri
    CC=golang-dev
    https://golang.org/cl/4130044

--- src/pkg/fmt/doc.go
+++ src/pkg/fmt/doc.go
@@ -16,6 +16,7 @@
 			when printing structs, the plus flag (%+v) adds field names
 		%#v	a Go-syntax representation of the value
 		%T	a Go-syntax representation of the type of the value
+		%%	a literal percent sign; consumes no value
 
 	Boolean:
 		%t	the word true or false
