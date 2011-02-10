bbb815ca7d3ef53efce5095d5ca0723f2a9f3fcd
Andrew Gerrand <adg@golang.org>
2011-02-11 06:56:31 +1100

    encoding/binary: update package doc string to be more comprehensive
    R=r
    CC=golang-dev
    https://golang.org/cl/4128060

--- src/pkg/encoding/binary/binary.go
+++ src/pkg/encoding/binary/binary.go
@@ -2,8 +2,9 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// This package implements translation between
-// unsigned integer values and byte sequences.
+// Package binary implements translation between
+// unsigned integer values and byte sequences
+// and the reading and writing of fixed-size values.
 package binary
 
 import (
