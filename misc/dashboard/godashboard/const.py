00d8d0052a9be4979bbfa929bc343530a980cd76
Andrew Gerrand <adg@golang.org>
2011-02-18 03:34:22 +1100

    misc/dashboard: notify golang-dev on build failure
    Fixes #1229.
    
    R=rsc
    CC=golang-dev
    https://golang.org/cl/4178048

---
+++ misc/dashboard/godashboard/const.py
@@ -0,0 +1,12 @@
+# Copyright 2011 The Go Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style
+# license that can be found in the LICENSE file.
+
+mail_from           = "Go Dashboard <builder@golang.org>"
+
+mail_submit_to      = "adg@golang.org"
+mail_submit_subject = "New Project Submitted"
+
+mail_fail_to       = "golang-dev@googlegroups.com"
+mail_fail_subject  = "%s broken by %s"
+
