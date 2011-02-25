ad8b0d915b2e19ec74982611ea0a2e10b3c6cd1b
Robert Griesemer <gri@golang.org>
2011-02-24 19:29:25 -0800

    bug324.go: test case for issue 1550
    Also: renamed fixedbugs/bug322.go to fixedbugs/bug323.go
    because we already have a bugs/bug322.go and bug322.dir.
    
    R=rsc
    CC=golang-dev
    https://golang.org/cl/4219044

---
+++ test/bugs/bug324.go
@@ -0,0 +1,8 @@
+// $G $D/$F.dir/p.go && errchk $G $D/$F.dir/main.go
+
+// Copyright 2011 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Test case for issue 1550
+ignored
