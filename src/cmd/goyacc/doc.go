78d0a110e4509e083be6f7f64f39e580299d0577
Rob Pike <r@golang.org>
2011-02-28 20:47:52 -0800

    goyacc: clarify it's package, not binary, that has conflict and explain that you could put the grammars in separate packages instead.
    I could be talked into rolling back the previous change
    and just making a doc change instead, but I'm content
    to let it stand.
    
    R=rsc
    CC=golang-dev
    https://golang.org/cl/4240053

--- src/cmd/goyacc/doc.go
+++ src/cmd/goyacc/doc.go
@@ -35,11 +35,12 @@ Error is equivalent to yyerror in the original yacc.
 Code inside the parser may refer to the variable yylex,
 which holds the yyLexer passed to Parse.
 
-The "-p prefix" flag to goyacc sets the prefix, by default yy, that
-begins the names of symbols, including types, the parser, and the
-lexer, generated and referenced by goyacc's generated code.  Setting
-it to distinct values allows multiple grammars to be used in a
-single binary.
+Multiple grammars compiled into a single program should be placed in
+distinct packages.  If that is impossible, the "-p prefix" flag to
+goyacc sets the prefix, by default yy, that begins the names of
+symbols, including types, the parser, and the lexer, generated and
+referenced by goyacc's generated code.  Setting it to distinct values
+allows multiple grammars to be placed in a single package.
 
 */
 package documentation
