60983130461880ad3be6e8a70629d039b815766a
Gustavo Niemeyer <gustavo@niemeyer.net>
2011-03-07 10:54:53 -0500

    goinstall: handle $(GOOS) and $(GOARCH) in filenames
    This enables goinstall to handle .go and .c files (for cgo)
    which are named after the following patterns:
    
        name_$(GOOS).*
        name_$(GOARCH).*
        name_$(GOOS)_$(GOARCH).*
    
    Files with those names are only included if the $(GOOS) and
    $(GOARCH) match the current system.
    
    R=rsc
    CC=golang-dev
    https://golang.org/cl/4172055

--- src/cmd/goinstall/parse.go
+++ src/cmd/goinstall/parse.go
@@ -14,6 +14,7 @@ import (
 	"path/filepath"
 	"strconv"
 	"strings"
+	"runtime"
 )
 
 
@@ -55,6 +56,9 @@ func scanDir(dir string, allowMain bool) (info *dirInfo, err os.Error) {
 	for i := range dirs {
 		d := &dirs[i]
 		if strings.HasPrefix(d.Name, "_") || strings.Index(d.Name, ".cgo") != -1 {
+			continue
+		}
+		if !goodOSArch(d.Name) {
 			continue
 		}
 		if strings.HasSuffix(d.Name, ".c") {
@@ -107,4 +111,48 @@ func scanDir(dir string, allowMain bool) (info *dirInfo, err os.Error) {
 		i++
 	}
 	return &dirInfo{goFiles, cgoFiles, cFiles, imports, pkgName}, nil
+}
+
+// goodOSArch returns false if the filename contains a $GOOS or $GOARCH
+// suffix which does not match the current system.
+// The recognized filename formats are:
+//
+//     name_$(GOOS).*
+//     name_$(GOARCH).*
+//     name_$(GOOS)_$(GOARCH).*
+//
+func goodOSArch(filename string) bool {
+	if dot := strings.Index(filename, "."); dot != -1 {
+		filename = filename[:dot]
+	}
+	l := strings.Split(filename, "_", -1)
+	n := len(l)
+	if n == 0 {
+		return true
+	}
+	if good, known := goodOS[l[n-1]]; known {
+		return good
+	}
+	if good, known := goodArch[l[n-1]]; known {
+		if !good || n < 2 {
+			return false
+		}
+		good, known = goodOS[l[n-2]]
+		return good || !known
+	}
+	return true
+}
+
+var goodOS = make(map[string]bool)
+var goodArch = make(map[string]bool)
+
+func init() {
+	goodOS = make(map[string]bool)
+	goodArch = make(map[string]bool)
+	for _, v := range strings.Fields(goosList) {
+		goodOS[v] = v == runtime.GOOS
+	}
+	for _, v := range strings.Fields(goarchList) {
+		goodArch[v] = v == runtime.GOARCH
+	}
 }
