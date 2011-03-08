87aa93457ed62b55b90ee9c52662b68df12fafd6
Peter Mundy <go.peter.90@gmail.com>
2011-03-08 12:33:49 -0500

    time: fix Time.ZoneOffset documentation
    R=rsc
    CC=golang-dev
    https://golang.org/cl/4240090

--- src/pkg/time/time.go
+++ src/pkg/time/time.go
@@ -23,7 +23,7 @@ type Time struct {
 	Month, Day           int    // Jan-2 is 1, 2
 	Hour, Minute, Second int    // 15:04:05 is 15, 4, 5.
 	Weekday              int    // Sunday, Monday, ...
-	ZoneOffset           int    // seconds east of UTC, e.g. -7*60 for -0700
+	ZoneOffset           int    // seconds east of UTC, e.g. -7*60*60 for -0700
 	Zone                 string // e.g., "MST"
 }
 
