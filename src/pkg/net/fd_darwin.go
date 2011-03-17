b5178602bd231c9c368fb15a16feddffd3f1cd10
Ian Lance Taylor <iant@golang.org>
2011-03-17 13:42:40 -0700

    net: Don't force epoll/kqueue to wake up in order to add new events.
    In conjunction with the non-blocking system call CL, this
    gives about an 8% performance improvement on a client/server
    test running on my local machine.
    
    R=rsc, iant2
    CC=golang-dev
    https://golang.org/cl/4272057

--- src/pkg/net/fd_darwin.go
+++ src/pkg/net/fd_darwin.go
@@ -75,7 +75,7 @@ func (p *pollster) DelFD(fd int, mode int) {
 	syscall.Kevent(p.kq, events[0:], events[0:], nil)
 }
 
-func (p *pollster) WaitFD(nsec int64) (fd int, mode int, err os.Error) {
+func (p *pollster) WaitFD(s *pollServer, nsec int64) (fd int, mode int, err os.Error) {
 	var t *syscall.Timespec
 	for len(p.events) == 0 {
 		if nsec > 0 {
@@ -84,7 +84,11 @@ func (p *pollster) WaitFD(nsec int64) (fd int, mode int, err os.Error) {
 			}
 			*t = syscall.NsecToTimespec(nsec)
 		}
+
+		s.Unlock()
 		nn, e := syscall.Kevent(p.kq, nil, p.eventbuf[0:], t)
+		s.Lock()
+
 		if e != 0 {
 			if e == syscall.EINTR {
 				continue
