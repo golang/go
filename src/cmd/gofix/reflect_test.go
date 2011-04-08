de56a2693379f7e0a17fe81408f128a0ea677ced
Russ Cox <rsc@golang.org>
2011-04-08 12:27:24 -0400

    gofix: reflect changes
    R=gri
    CC=golang-dev
    https://golang.org/cl/4343047

---
+++ src/cmd/gofix/reflect_test.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"io/ioutil"
+	"log"
+	"path/filepath"
+)
+
+func init() {
+	addTestCases(reflectTests())
+}
+
+func reflectTests() []testCase {
+	var tests []testCase
+
+	names, _ := filepath.Glob("testdata/reflect.*.in")
+	for _, in := range names {
+		out := in[:len(in)-len(".in")] + ".out"
+		inb, err := ioutil.ReadFile(in)
+		if err != nil {
+			log.Fatal(err)
+		}
+		outb, err := ioutil.ReadFile(out)
+		if err != nil {
+			log.Fatal(err)
+		}
+		tests = append(tests, testCase{Name: in, In: string(inb), Out: string(outb)})
+	}
+
+	return tests
+}
