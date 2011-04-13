42c766a2bc2627a45b57604a572ebf70a89bb227
Rob Pike <r@golang.org>
2011-04-13 14:57:47 -0700

    libmach: fix the windows build. Newly enabled compiler errors need workaround.
    R=rsc
    CC=golang-dev
    https://golang.org/cl/4397047

--- src/libmach/windows.c
+++ src/libmach/windows.c
@@ -8,24 +8,28 @@ int
 ctlproc(int pid, char *msg)
 {
 	sysfatal("ctlproc unimplemented in Windows");
+	return -1;
 }
 
 char*
 proctextfile(int pid)
 {
 	sysfatal("proctextfile unimplemented in Windows");
+	return nil;
 }
 
 char*
 procstatus(int pid)
 {
 	sysfatal("procstatus unimplemented in Windows");
+	return nil;
 }
 
 Map*
 attachproc(int pid, Fhdr *fp)
 {
 	sysfatal("attachproc unimplemented in Windows");
+	return nil;
 }
 
 void
@@ -38,22 +42,26 @@ int
 procthreadpids(int pid, int *p, int np)
 {
 	sysfatal("procthreadpids unimplemented in Windows");
+	return -1;
 }
 
 int 
 pread(int fd, void *buf, int count, int offset)
 {
 	sysfatal("pread unimplemented in Windows");
+	return -1;
 }
 
 int 
 pwrite(int fd, void *buf, int count, int offset)
 {
 	sysfatal("pwrite unimplemented in Windows");
+	return -1;
 }
 
 int 
 nanosleep(const struct timespec *rqtp, struct timespec *rmtp)
 {
 	sysfatal("nanosleep unimplemented in Windows");
+	return -1;
 }
