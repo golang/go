fc0a6acac79faf257c43fb23ba622d99e5367544
Russ Cox <rsc@golang.org>
2011-04-13 22:48:21 -0400

    gc: fix func syntax error
    Fixes #1685.
    
    R=ken2
    CC=golang-dev
    https://golang.org/cl/4414042

---
+++ test/func6.go
@@ -0,0 +1,14 @@
+// $G $D/$F.go
+
+// Copyright 2011 The Go Authors.  All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+func main() {
+	if func() bool { return true }() {}  // 6g used to say this was a syntax error
+	if (func() bool { return true })() {}
+	if (func() bool { return true }()) {}
+}
+
