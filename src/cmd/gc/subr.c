f10a7882c6d2876592bb73a27e7e595caad5c376
Russ Cox <rsc@golang.org>
2011-04-21 08:20:29 -0400

    gc: another pointer to interface message
    R=ken2
    CC=golang-dev
    https://golang.org/cl/4444056

--- src/cmd/gc/subr.c
+++ src/cmd/gc/subr.c
@@ -1928,13 +1928,14 @@ assignop(Type *src, Type *dst, char **why)
 		}
 		return 0;
 	}
+	if(isptrto(dst, TINTER)) {
+		if(why != nil)
+			*why = smprint(":\n\t%T is pointer to interface, not interface", dst);
+		return 0;
+	}
 	if(src->etype == TINTER && dst->etype != TBLANK) {
-		if(why != nil) {
-			if(isptrto(dst, TINTER))
-				*why = smprint(":\n\t%T is interface, not pointer to interface", src);
-			else	
-				*why = ": need type assertion";
-		}
+		if(why != nil)
+			*why = ": need type assertion";
 		return 0;
 	}
 
