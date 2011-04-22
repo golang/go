d911b872ceae5c6d00e519b74db3c6eede5e3b87
David Symonds <dsymonds@golang.org>
2011-04-22 16:29:05 -0400

    container/heap: fix circular dependency in test
    Having the test be in the container/heap package yields a cycle
      container/heap (for the test)
      -> testing
      -> time
      -> container/heap (for timerHeap)
    
    Occasionally the linker would get mixed up, resulting in a test panic
    in a very weird place.
    
    R=rsc, r2
    CC=golang-dev
    https://golang.org/cl/4395042

--- src/pkg/container/heap/heap_test.go
+++ src/pkg/container/heap/heap_test.go
@@ -2,11 +2,12 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-package heap
+package heap_test
 
 import (
 	"testing"
 	"container/vector"
+	. "container/heap"
 )
 
 
