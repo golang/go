cf3e7fa99df7eacc7f161728b237d0f1738c8bbd
Brad Fitzpatrick <bradfitz@golang.org>
2011-05-02 13:35:28 -0700

    runtime: maybe fix Windows build broken by cgo setenv CL
    R=rsc
    CC=golang-dev
    https://golang.org/cl/4428078

---
+++ src/pkg/runtime/cgo/setenv.c
@@ -0,0 +1,16 @@
+// Copyright 20111 The Go Authors.  All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+#include "libcgo.h"
+
+#include <stdlib.h>
+
+/* Stub for calling setenv */
+static void
+xlibcgo_setenv(char **arg)
+{
+	setenv(arg[0], arg[1], 1);
+}
+
+void (*libcgo_setenv)(char**) = xlibcgo_setenv;
