5603a15787a60e266fff278dd4374ec52e240b1e
Rob Pike <r@golang.org>
2011-05-22 09:22:00 +1000

    flags: allow distinct sets of flags.
    A FlagSet is an independent set of flags that may be used,
    for example, to provide flag processing for subcommands
    in a CLI.  The standard, os.Args-derived set of flags is a
    global but non-exported FlagSet and the standard functions
    are wrappers for methods of that FlagSet.
    
    Allow the programmer to control whether the program
    exits if there is a parse error.  For the default set, the behavior
    remains to exit on error.
    
    The handling of Usage is odd due to backward compatibility.
    
    R=golang-dev, bradfitz, r, bradfitz
    CC=golang-dev
    https://golang.org/cl/4517092

--- src/pkg/flag/export_test.go
+++ src/pkg/flag/export_test.go
@@ -9,24 +9,14 @@ import "os"
 // Additional routines compiled into the package only during testing.
 
 // ResetForTesting clears all flag state and sets the usage function as directed.
-// After calling ResetForTesting, parse errors in flag handling will panic rather
-// than exit the program.
+// After calling ResetForTesting, parse errors in flag handling will not
+// exit the program.
 func ResetForTesting(usage func()) {
-	flags = &allFlags{make(map[string]*Flag), make(map[string]*Flag), os.Args[1:]}
+	commandLine = NewFlagSet(os.Args[0], ContinueOnError)
 	Usage = usage
-	panicOnError = true
 }
 
-// ParseForTesting parses the flag state using the provided arguments. It
-// should be called after 1) ResetForTesting and 2) setting up the new flags.
-// The return value reports whether the parse was error-free.
-func ParseForTesting(args []string) (result bool) {
-	defer func() {
-		if recover() != nil {
-			result = false
-		}
-	}()
-	os.Args = args
-	Parse()
-	return true
+// CommandLine returns the default FlagSet.
+func CommandLine() *FlagSet {
+	return commandLine
 }
