d3f399333d3035bdac7eb4c5257ec0b14fbc40fb
Rob Pike <r@golang.org>
2011-05-25 21:25:15 +1000

    fmt: make %q work for integers, printing a quoted character literal.
    R=rsc
    CC=golang-dev
    https://golang.org/cl/4556060

--- src/pkg/fmt/doc.go
+++ src/pkg/fmt/doc.go
@@ -25,6 +25,7 @@
 		%c	the character represented by the corresponding Unicode code point
 		%d	base 10
 		%o	base 8
+		%q	a single-quoted character literal safely escaped with Go syntax.
 		%x	base 16, with lower-case letters for a-f
 		%X	base 16, with upper-case letters for A-F
 		%U	Unicode format: U+1234; same as "U+%04X"
