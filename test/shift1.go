0584947bed5cbd6fbaf835f43dad6f57b966c3e8
Robert Griesemer <gri@golang.org>
2011-05-25 10:26:06 -0700

    test: test cases for issue 1708.
    R=rsc
    CC=golang-dev
    https://golang.org/cl/4548058

---
+++ test/shift1.go
@@ -0,0 +1,36 @@
+// errchk $G -e $D/$F.go
+
+// Copyright 2011 The Go Authors.  All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Issue 1708, illegal cases.
+
+package p
+
+func f(x int) int         { return 0 }
+func g(x interface{}) int { return 0 }
+func h(x float64) int     { return 0 }
+
+// from the spec
+var (
+	s uint    = 33
+	u         = 1.0 << s // ERROR "invalid operation"
+	v float32 = 1 << s   // ERROR "invalid operation"
+)
+
+// non-constant shift expressions
+var (
+	e1       = g(2.0 << s) // ERROR "invalid operation"
+	f1       = h(2 << s)   // ERROR "invalid operation"
+	g1 int64 = 1.1 << s    // ERROR "truncated"
+)
+
+// constant shift expressions
+const c uint = 65
+
+var (
+	a2 int = 1.0 << c    // ERROR "overflow"
+	b2     = 1.0 << c    // ERROR "overflow"
+	d2     = f(1.0 << c) // ERROR "overflow"
+)
