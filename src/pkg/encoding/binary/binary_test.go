3648a03b3a1ae7c73f1853a8413c6ce4d64ade2f
Brad Fitzpatrick <bradfitz@golang.org>
2011-05-26 09:01:05 -0700

    encoding/binary: add a non-reflect fast path for Read
    before/after:
    binary.BenchmarkRead  200000     10860 ns/op
    binary.BenchmarkRead  500000      2846 ns/op
    
    R=rsc
    CC=golang-dev
    https://golang.org/cl/4547062

--- src/pkg/encoding/binary/binary_test.go
+++ src/pkg/encoding/binary/binary_test.go
@@ -5,6 +5,7 @@
 package binary
 
 import (
+	"io"
 	"os"
 	"bytes"
 	"math"
@@ -158,5 +159,45 @@ func TestWriteT(t *testing.T) {
 		if err == nil {
 			t.Errorf("WriteT.%v: have nil, want non-nil", tv.Field(i).Type())
 		}
+	}
+}
+
+type byteSliceReader struct {
+	remain []byte
+}
+
+func (br *byteSliceReader) Read(p []byte) (int, os.Error) {
+	n := copy(p, br.remain)
+	br.remain = br.remain[n:]
+	return n, nil
+}
+
+func BenchmarkRead(b *testing.B) {
+	var ls Struct
+	bsr := &byteSliceReader{}
+	var r io.Reader = bsr
+
+	for i := 0; i < b.N; i++ {
+		bsr.remain = big
+		Read(r, BigEndian, &ls.Int8)
+		Read(r, BigEndian, &ls.Int16)
+		Read(r, BigEndian, &ls.Int32)
+		Read(r, BigEndian, &ls.Int64)
+		Read(r, BigEndian, &ls.Uint8)
+		Read(r, BigEndian, &ls.Uint16)
+		Read(r, BigEndian, &ls.Uint32)
+		Read(r, BigEndian, &ls.Uint64)
+	}
+
+	want := s
+	want.Float32 = 0
+	want.Float64 = 0
+	want.Complex64 = 0
+	want.Complex128 = 0
+	for i := range want.Array {
+		want.Array[i] = 0
+	}
+	if !reflect.DeepEqual(ls, want) {
+		panic("no match")
 	}
 }
