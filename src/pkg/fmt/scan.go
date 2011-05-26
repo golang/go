e8c87a7dddb7a93ebb16a3f4cf0f70b54632da0c
Robert Griesemer <gri@golang.org>
2011-05-26 11:54:10 -0700

    fix build: temporarily disable the use of strings.Reader UnreadRune in fmt
    R=r
    CC=golang-dev
    https://golang.org/cl/4532090

--- src/pkg/fmt/scan.go
+++ src/pkg/fmt/scan.go
@@ -226,6 +226,15 @@ func (s *ss) mustReadRune() (rune int) {
 }
 
 func (s *ss) UnreadRune() os.Error {
+	// Don't use strings.Reader.UnreadRune for now - appears to cause a problem.
+	// TODO(r, gri): Fix this and remove code between --- lines!
+	// ---
+	if _, ok := s.rr.(*strings.Reader); ok {
+		s.peekRune = s.prevRune
+		s.count--
+		return nil
+	}
+	// ---
 	if u, ok := s.rr.(runeUnreader); ok {
 		u.UnreadRune()
 	} else {
