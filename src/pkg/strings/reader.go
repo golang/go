be9666faf8dff6988ffda329b989a9d699176469
Robert Griesemer <gri@golang.org>
2011-05-26 11:02:07 -0700

    strings: implement UnreadByte, UnreadRune
    Added corresponding tests.
    
    R=rsc
    CC=golang-dev
    https://golang.org/cl/4560045

--- src/pkg/strings/reader.go
+++ src/pkg/strings/reader.go
@@ -9,28 +9,45 @@ import (
 	"utf8"
 )
 
-// A Reader satisfies calls to Read, ReadByte, and ReadRune by
-// reading from a string.
-type Reader string
+// A Reader implements the io.Reader, io.ByteScanner, and
+// io.RuneScanner interfaces by reading from a string.
+type Reader struct {
+	s        string
+	i        int // current reading index
+	prevRune int // index of previous rune; or < 0
+}
 
 func (r *Reader) Read(b []byte) (n int, err os.Error) {
-	s := *r
-	if len(s) == 0 {
+	if r.i >= len(r.s) {
 		return 0, os.EOF
 	}
-	n = copy(b, s)
-	*r = s[n:]
+	n = copy(b, r.s[r.i:])
+	r.i += n
+	r.prevRune = -1
 	return
 }
 
 func (r *Reader) ReadByte() (b byte, err os.Error) {
-	s := *r
-	if len(s) == 0 {
+	if r.i >= len(r.s) {
 		return 0, os.EOF
 	}
-	b = s[0]
-	*r = s[1:]
+	b = r.s[r.i]
+	r.i++
+	r.prevRune = -1
 	return
+}
+
+
+// UnreadByte moves the reading position back by one byte.
+// It is an error to call UnreadByte if nothing has been
+// read yet.
+func (r *Reader) UnreadByte() os.Error {
+	if r.i <= 0 {
+		return os.ErrorString("strings.Reader: at beginning of string")
+	}
+	r.i--
+	r.prevRune = -1
+	return nil
 }
 
 // ReadRune reads and returns the next UTF-8-encoded
@@ -39,20 +56,31 @@ func (r *Reader) ReadByte() (b byte, err os.Error) {
 // If the bytes are an erroneous UTF-8 encoding, it
 // consumes one byte and returns U+FFFD, 1.
 func (r *Reader) ReadRune() (rune int, size int, err os.Error) {
-	s := *r
-	if len(s) == 0 {
+	if r.i >= len(r.s) {
 		return 0, 0, os.EOF
 	}
-	c := s[0]
-	if c < utf8.RuneSelf {
-		*r = s[1:]
+	r.prevRune = r.i
+	if c := r.s[r.i]; c < utf8.RuneSelf {
+		r.i++
 		return int(c), 1, nil
 	}
-	rune, size = utf8.DecodeRuneInString(string(s))
-	*r = s[size:]
+	rune, size = utf8.DecodeRuneInString(r.s[r.i:])
+	r.i += size
 	return
+}
+
+// UnreadRune causes the next call to ReadRune to return the same rune
+// as the previous call to ReadRune.
+// The last method called on r must have been ReadRune.
+func (r *Reader) UnreadRune() os.Error {
+	if r.prevRune < 0 {
+		return os.ErrorString("strings.Reader: previous operation was not ReadRune")
+	}
+	r.i = r.prevRune
+	r.prevRune = -1
+	return nil
 }
 
 // NewReader returns a new Reader reading from s.
 // It is similar to bytes.NewBufferString but more efficient and read-only.
-func NewReader(s string) *Reader { return (*Reader)(&s) }
+func NewReader(s string) *Reader { return &Reader{s, 0, -1} }
