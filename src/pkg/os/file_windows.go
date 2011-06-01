225faa81dba48b1d541aafd47ba92e4d8db8a36c
Yuval Pavel Zholkover <paulzhol@gmail.com>
2011-06-01 13:12:37 +1000

    Make unix Readdir and windows Readdirnames return partially successful results on error. Make plan 9 Readdir & Readdirnames return os.EOF at end. Also fix typos in the unix and windows comments.
    R=golang-dev, fshahriar, bradfitz, rsc, r
    CC=golang-dev
    https://golang.org/cl/4557053

--- src/pkg/os/file_windows.go
+++ src/pkg/os/file_windows.go
@@ -123,12 +123,12 @@ func (file *File) Stat() (fi *FileInfo, err Error) {
 }
 
 // Readdir reads the contents of the directory associated with file and
-// returns an array of up to count FileInfo structures, as would be returned
+// returns an array of up to n FileInfo structures, as would be returned
 // by Lstat, in directory order. Subsequent calls on the same file will yield
 // further FileInfos.
 //
-// If n > 0, Readdir returns at most n names. In this case, if
-// Readdirnames returns an empty slice, it will return a non-nil error
+// If n > 0, Readdir returns at most n FileInfo structures. In this case, if
+// Readdir returns an empty slice, it will return a non-nil error
 // explaining why. At the end of a directory, the error is os.EOF.
 //
 // If n <= 0, Readdir returns all the FileInfo from the directory in
