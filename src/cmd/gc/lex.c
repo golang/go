ab8ed7f8ddc3e333b348d58fac62c6ca0753859c
Luuk van Dijk <lvd@golang.org>
2011-06-03 17:44:02 +0200

    gc: renamed walkdef to typecheckdef and moved from walk to typedef.
    also inlined a typechecking function in dcl away.
    
    R=rsc
    CC=golang-dev
    https://golang.org/cl/4550115

--- src/cmd/gc/lex.c
+++ src/cmd/gc/lex.c
@@ -274,7 +274,9 @@ main(int argc, char *argv[])
 			funccompile(l->n, 1);
 	}
 
-	dclchecks();
+	for(l=externdcl; l; l=l->next)
+		if(l->n->op == ONAME)
+			typecheck(&l->n, Erv);
 
 	if(nerrors)
 		errorexit();
