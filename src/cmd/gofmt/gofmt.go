97059c9811a8fadf215f92b712004eada4f1fe0b
Brad Fitzpatrick <bradfitz@golang.org>
2011-06-07 09:38:04 -0700

    gofmt: fix -d regression from exec change
    Fixes #1916
    
    R=golang-dev, gri
    CC=golang-dev
    https://golang.org/cl/4590041

--- src/cmd/gofmt/gofmt.go
+++ src/cmd/gofmt/gofmt.go
@@ -260,5 +260,12 @@ func diff(b1, b2 []byte) (data []byte, err os.Error) {
 	f1.Write(b1)
 	f2.Write(b2)
 
-	return exec.Command("diff", "-u", f1.Name(), f2.Name()).CombinedOutput()
+	data, err = exec.Command("diff", "-u", f1.Name(), f2.Name()).CombinedOutput()
+	if len(data) > 0 {
+		// diff exits with a non-zero status when the files don't match.
+		// Ignore that failure as long as we get output.
+		err = nil
+	}
+	return
+
 }
