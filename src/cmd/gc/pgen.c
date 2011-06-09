17ca32e9db2f58a6ed431c4502f6ab031147673f
Russ Cox <rsc@golang.org>
2011-06-09 11:44:28 -0400

    gc: nits
    R=ken2
    CC=golang-dev
    https://golang.org/cl/4572052

--- src/cmd/gc/pgen.c
+++ src/cmd/gc/pgen.c
@@ -2,10 +2,8 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-#undef	EXTERN
-#define	EXTERN
-#include "gg.h"
-#include "opt.h"
+#include	"gg.h"
+#include	"opt.h"
 
 void
 compile(Node *fn)
