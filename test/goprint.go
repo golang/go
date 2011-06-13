b4bb970e1852e9f5340b846379ce4a434a1b9d26
Stephen Ma <stephenm@golang.org>
2011-06-13 22:50:51 +1000

    gc: handle go print() and go println()
    Fixes #1952.
    
    R=golang-dev, rsc
    CC=golang-dev
    https://golang.org/cl/4611041

---
+++ test/goprint.go
@@ -0,0 +1,14 @@
+// $G $D/$F.go && $L $F.$A && ./$A.out
+
+// Copyright 2011 The Go Authors.  All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+import "time"
+
+func main() {
+	go println(42, true, false, true, 1.5, "world", (chan int)(nil), []int(nil), (map[string]int)(nil), (func())(nil), byte(255))
+	time.Sleep(1e6)
+}
