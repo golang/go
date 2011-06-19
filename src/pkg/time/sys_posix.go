d119e3bc0fccc911052f1ac72ab61be58949a3d2
Anthony Martin <ality@pbrane.org>
2011-06-19 16:36:33 +1000

    time: add support for Plan 9
    R=paulzhol, mirtchovski, r, r, rsc
    CC=golang-dev
    https://golang.org/cl/4362041

---
+++ src/pkg/time/sys_posix.go
@@ -0,0 +1,18 @@
+// Copyright 2011 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package time
+
+import (
+	"os"
+	"syscall"
+)
+
+func sysSleep(t int64) os.Error {
+	errno := syscall.Sleep(t)
+	if errno != 0 && errno != syscall.EINTR {
+		return os.NewSyscallError("sleep", errno)
+	}
+	return nil
+}
