68b58c5e16240b8d82b5679fb812cbc16a60847f
Alex Brainman <alex.brainman@gmail.com>
2011-08-05 17:19:17 +1000

    time: fix windows build
    R=golang-dev, mattn.jp, dsymonds
    CC=golang-dev
    https://golang.org/cl/4849042

--- src/pkg/time/zoneinfo_windows.go
+++ src/pkg/time/zoneinfo_windows.go
@@ -49,7 +49,7 @@ func (z *zone) populate(bias, biasdelta int32, d *syscall.Systemtime, name []uin
 // Pre-calculate cutoff time in seconds since the Unix epoch, if data is supplied in "absolute" format.
 func (z *zone) preCalculateAbsSec() {
 	if z.year != 0 {
-		z.abssec = (&Time{z.year, int(z.month), int(z.day), int(z.hour), int(z.minute), int(z.second), 0, 0, ""}).Seconds()
+		z.abssec = (&Time{z.year, int(z.month), int(z.day), int(z.hour), int(z.minute), int(z.second), 0, 0, 0, ""}).Seconds()
 		// Time given is in "local" time. Adjust it for "utc".
 		z.abssec -= int64(z.prev.offset)
 	}
@@ -62,7 +62,7 @@ func (z *zone) cutoffSeconds(year int64) int64 {
 	// z.dayofweek is appropriate weekday (Sunday=0 to Saturday=6)
 	// z.day is week within the month (1 to 5, where 5 is last week of the month)
 	// z.hour, z.minute and z.second are absolute time
-	t := &Time{year, int(z.month), 1, int(z.hour), int(z.minute), int(z.second), 0, 0, ""}
+	t := &Time{year, int(z.month), 1, int(z.hour), int(z.minute), int(z.second), 0, 0, 0, ""}
 	t = SecondsToUTC(t.Seconds())
 	i := int(z.dayofweek) - t.Weekday
 	if i < 0 {
