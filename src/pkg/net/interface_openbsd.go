fe28c3f95b0f53445a76186f6fc9339f71b070cc
Joel Sing <jsing@google.com>
2011-08-16 17:28:16 -0400

    net: add multicast stubs for openbsd
    Add multicast stubs and sync with recent changes.
    
    R=golang-dev, mikioh.mikioh
    CC=golang-dev
    https://golang.org/cl/4838058

---
+++ src/pkg/net/interface_openbsd.go
@@ -0,0 +1,16 @@
+// Copyright 2011 The Go Authors.  All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Network interface identification for OpenBSD
+
+package net
+
+import "os"
+
+// If the ifindex is zero, interfaceMulticastAddrTable returns
+// addresses for all network interfaces.  Otherwise it returns
+// addresses for a specific interface.
+func interfaceMulticastAddrTable(ifindex int) ([]Addr, os.Error) {
+	return nil, nil
+}
