57273eef4084ed35e15a6e290ca31f69b1dbfdc2
Pascal S. de Kloe <pascal@quies.net>
2011-08-26 16:55:25 -0400

    mime: text charset defaults
    Enforce + document the UTF-8 default.
    
    R=rsc, bradfitz, adg
    CC=golang-dev
    https://golang.org/cl/4627049

--- src/pkg/mime/type_test.go
+++ src/pkg/mime/type_test.go
@@ -2,19 +2,17 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// Tests for type.go
-
 package mime
 
 import "testing"
 
 var typeTests = map[string]string{
 	".t1":  "application/test",
-	".t2":  "text/test; charset=utf-8",
+	".t2":  "text/test;charset=utf-8",
 	".png": "image/png",
 }
 
-func TestType(t *testing.T) {
+func TestTypeByExtension(t *testing.T) {
 	typeFiles = []string{"test.types"}
 
 	for ext, want := range typeTests {
@@ -23,5 +21,15 @@ func TestType(t *testing.T) {
 			t.Errorf("TypeByExtension(%q) = %q, want %q", ext, val, want)
 		}
 
+	}
+}
+
+func TestCustomExtension(t *testing.T) {
+	custom := "text/xml;charset=iso-8859-1"
+	if error := AddExtensionType(".xml", custom); error != nil {
+		t.Fatalf("error %s for AddExtension(%s)", error, custom)
+	}
+	if registered := TypeByExtension(".xml"); registered != custom {
+		t.Fatalf("registered %s instead of %s", registered, custom)
 	}
 }
