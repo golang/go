c586b5b96b8156a97ef24c69fa315f95b4b6bbd2
Jaroslavas Počepko <jp@webmaster.ms>
2011-08-30 22:02:02 +1000

    runtime: windows/amd64 callbacks fixed and syscall fixed to allow using it in callbacks Fixes #2178. Patch2: Fixed allocating shadow space for stdcall (must be at least 32 bytes in any case) Patch3: Made allocated chunk smaller. Patch4: Typo Patch5: suppress linktime warning "runtime.callbackasm: nosplit stack overflow" Patch6: added testcase src/pkg/syscall/callback_windows_test.go Patch7: weakly related files moved to https://golang.org/cl/4965050 https://golang.org/cl/4974041 https://golang.org/cl/4965051 Patch8: reflect changes https://golang.org/cl/4926042/ Patch9: reflect comments
    R=golang-dev, alex.brainman, vcc.163
    CC=golang-dev, hectorchu
    https://golang.org/cl/4958042

--- src/pkg/runtime/syscall_windows_test.go
+++ src/pkg/runtime/syscall_windows_test.go
@@ -59,3 +59,41 @@ func TestCDecl(t *testing.T) {
 		t.Error("cdecl USER32.wsprintfA returns", a, "buf=", buf[:a])
 	}
 }
+
+func TestCallback(t *testing.T) {
+	h, e := syscall.LoadLibrary("user32.dll")
+	if e != 0 {
+		t.Fatal("LoadLibrary(USER32)")
+	}
+	pEnumWindows, e := syscall.GetProcAddress(h, "EnumWindows")
+	if e != 0 {
+		t.Fatal("GetProcAddress(USER32.EnumWindows)")
+	}
+	pIsWindow, e := syscall.GetProcAddress(h, "IsWindow")
+	if e != 0 {
+		t.Fatal("GetProcAddress(USER32.IsWindow)")
+	}
+	counter := 0
+	cb := syscall.NewCallback(func(hwnd syscall.Handle, lparam uintptr) uintptr {
+		if lparam != 888 {
+			t.Error("lparam was not passed to callback")
+		}
+		b, _, _ := syscall.Syscall(uintptr(pIsWindow), 1, uintptr(hwnd), 0, 0)
+		if b == 0 {
+			t.Error("USER32.IsWindow returns FALSE")
+		}
+		counter++
+		return 1 // continue enumeration
+	})
+	a, _, _ := syscall.Syscall(uintptr(pEnumWindows), 2, cb, 888, 0)
+	if a == 0 {
+		t.Error("USER32.EnumWindows returns FALSE")
+	}
+	if counter == 0 {
+		t.Error("Callback has been never called or your have no windows")
+	}
+}
+
+func TestCallbackInAnotherThread(t *testing.T) {
+	// TODO: test a function which calls back in another thread: QueueUserAPC() or CreateThread()
+}
