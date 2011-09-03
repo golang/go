c5670f15488843ebd368f4f3fff417cc7acdd178
Gustavo Niemeyer <gustavo@niemeyer.net>
2011-09-03 16:01:54 -0300

    gofix: do not convert url in field names
    There's some ambiguity in the U{url: url} case as it could be
    both a map or a struct literal, but given context it's more
    likely a struct, so U{url: url_} rather than U{url_: url_}.
    At least that was the case for me.
    
    R=golang-dev, rsc, adg
    CC=golang-dev
    https://golang.org/cl/4972052

--- src/cmd/gofix/url_test.go
+++ src/cmd/gofix/url_test.go
@@ -80,10 +80,15 @@ import (
 	"http"
 )
 
+type U struct{ url int }
+type M map[int]int
+
 func f() {
 	http.ParseURL(a)
 	var url = 23
 	url, x := 45, y
+	_ = U{url: url}
+	_ = M{url + 1: url}
 }
 
 func g(url string) string {
@@ -98,10 +103,15 @@ func h() (url string) {
 
 import "url"
 
+type U struct{ url int }
+type M map[int]int
+
 func f() {
 	url.Parse(a)
 	var url_ = 23
 	url_, x := 45, y
+	_ = U{url: url_}
+	_ = M{url_ + 1: url_}
 }
 
 func g(url_ string) string {
