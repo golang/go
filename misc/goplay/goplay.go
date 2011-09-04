f20e45a0eaddfbe3a6e1820f26f1d0e5fa907617
Andrew Gerrand <adg@golang.org>
2011-09-05 09:50:22 +1000

    misc/goplay: Fix template output
    Fixes #2219.
    
    R=golang-dev, r
    CC=golang-dev
    https://golang.org/cl/4960052

--- misc/goplay/goplay.go
+++ misc/goplay/goplay.go
@@ -256,7 +256,7 @@ function compileUpdate() {
 </head>
 <body>
 <table width="100%"><tr><td width="60%" valign="top">
-<textarea autofocus="true" id="edit" spellcheck="false" onkeydown="keyHandler(event);" onkeyup="autocompile();">{{html .}}</textarea>
+<textarea autofocus="true" id="edit" spellcheck="false" onkeydown="keyHandler(event);" onkeyup="autocompile();">{{printf "%s" . |html}}</textarea>
 <div class="hints">
 (Shift-Enter to compile and run.)&nbsp;&nbsp;&nbsp;&nbsp;
 <input type="checkbox" id="autocompile" value="checked" /> Compile and run after each keystroke
