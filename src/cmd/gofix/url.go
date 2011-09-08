616801951bd8ed946acc70ea80c1d1c8e29c03b8
Andrew Gerrand <adg@golang.org>
2011-09-08 10:52:28 +1000

    [release-branch.r60] gofix: do not convert url in field names
    ««« CL 4972052 / 0f7a647510f9
    gofix: do not convert url in field names
    
    There's some ambiguity in the U{url: url} case as it could be
    both a map or a struct literal, but given context it's more
    likely a struct, so U{url: url_} rather than U{url_: url_}.
    At least that was the case for me.
    
    R=golang-dev, rsc, adg
    CC=golang-dev
    https://golang.org/cl/4972052
    »»»
    
    R=golang-dev, dsymonds
    CC=golang-dev
    https://golang.org/cl/4962058

--- src/cmd/gofix/url.go
+++ src/cmd/gofix/url.go
@@ -46,7 +46,12 @@ func url(f *ast.File) bool {
 	fixed := false
 
 	// Update URL code.
+	var skip interface{}
 	urlWalk := func(n interface{}) {
+		if n == skip {
+			skip = nil
+			return
+		}
 		// Is it an identifier?
 		if ident, ok := n.(*ast.Ident); ok && ident.Name == "url" {
 			ident.Name = "url_"
@@ -57,6 +62,12 @@ func url(f *ast.File) bool {
 			fixed = urlDoFields(fn.Params) || fixed
 			fixed = urlDoFields(fn.Results) || fixed
 		}
+		// U{url: ...} is likely a struct field.
+		if kv, ok := n.(*ast.KeyValueExpr); ok {
+			if ident, ok := kv.Key.(*ast.Ident); ok && ident.Name == "url" {
+				skip = ident
+			}
+		}
 	}
 
 	// Fix up URL code and add import, at most once.
@@ -64,7 +75,7 @@ func url(f *ast.File) bool {
 		if fixed {
 			return
 		}
-		walk(f, urlWalk)
+		walkBeforeAfter(f, urlWalk, nop)
 		addImport(f, "url")
 		fixed = true
 	}
