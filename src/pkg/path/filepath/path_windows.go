33a0d2b07ac16a1608b289f7593e91f2305f5626
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2011-09-09 17:38:29 +1000

    path/filepath: make UNC file names work
    Fixes #2201
    
    R=golang-dev, r, rsc, alex.brainman, robert.hencke, jp
    CC=golang-dev
    https://golang.org/cl/4950051

--- src/pkg/path/filepath/path_windows.go
+++ src/pkg/path/filepath/path_windows.go
@@ -4,7 +4,13 @@
 
 package filepath
 
-import "strings"
+import (
+	"strings"
+)
+
+func isSlash(c uint8) bool {
+	return c == '\\' || c == '/'
+}
 
 // IsAbs returns true if the path is absolute.
 func IsAbs(path string) (b bool) {
@@ -16,11 +22,12 @@ func IsAbs(path string) (b bool) {
 	if path == "" {
 		return false
 	}
-	return path[0] == '/' || path[0] == '\\'
+	return isSlash(path[0])
 }
 
 // VolumeName returns leading volume name.  
 // Given "C:\foo\bar" it returns "C:" under windows.
+// Given "\\host\share\foo" it returns "\\host\share".
 // On other platforms it returns "".
 func VolumeName(path string) (v string) {
 	if len(path) < 2 {
@@ -32,6 +39,30 @@ func VolumeName(path string) (v string) {
 		('0' <= c && c <= '9' || 'a' <= c && c <= 'z' ||
 			'A' <= c && c <= 'Z') {
 		return path[:2]
+	}
+	// is it UNC
+	if l := len(path); l >= 5 && isSlash(path[0]) && isSlash(path[1]) &&
+		!isSlash(path[2]) && path[2] != '.' {
+		// first, leading `\\` and next shouldn't be `\`. its server name.
+		for n := 3; n < l-1; n++ {
+			// second, next '\' shouldn't be repeated.
+			if isSlash(path[n]) {
+				n++
+				// third, following something characters. its share name.
+				if !isSlash(path[n]) {
+					if path[n] == '.' {
+						break
+					}
+					for ; n < l; n++ {
+						if isSlash(path[n]) {
+							break
+						}
+					}
+					return path[:n]
+				}
+				break
+			}
+		}
 	}
 	return ""
 }
