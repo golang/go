721ab2ec3260a2ae6502e50d5ee36e200b1d580b
Mike Samuel <mikesamuel@gmail.com>
2011-09-13 16:57:39 -0700

    exp/template/html: escape {{template}} calls and sets of templates
    This adds support for {{template "callee"}} calls.
    It recognizes that calls can appear in many contexts.
    
    {{if .ImageURL}}
        <img src="{{.ImageURL}}" alt="{{template "description"}}">
    {{else}}
        <p>{{template "description"}}</p>
    {{end}}
    
    calls a template in two different contexts, first in an HTML attribute
    context, and second in an HTML text context.
    
    Those two contexts aren't very different, but when linking text
    to search terms, the escaping context can be materially different:
    
    <a href="/search?q={{template "tags"}}">{{template "tags"}}</a>
    
    This adds API:
    EscapeSet(*template.Set, names ...string) os.Error
    
    takes a set of templates and the names of those which might be called
    in the default context as starting points.
    
    It changes the escape* functions to be methods of an object which
    maintains a conceptual mapping of
    (template names*input context) -> output context.
    
    The actual mapping uses as key a mangled name which combines the
    template name with the input context.
    
    The mangled name when the input context is the default context is the
    same as the unmangled name.
    
    When a template is called in multiple contexts, we clone the template.
    
    {{define "tagLink"}}
      <a href="/search?q={{template "tags"}}">{{template "tags"}}</a>
    {{end}}
    {{define "tags"}}
      {{range .Tags}}{{.}},{{end}}
    {{end}}
    
    given []string{ "foo", "O'Reilly", "bar" } produces
    
      <a href="/search?q=foo,O%27Reilly,bar">foo,O&#39;Reilly,bar</a>
    
    This involves rewriting the above to something like
    
    {{define "tagLink"}}
      <a href="/search?q={{template "tags$1"}}">{{template "tags"}}</a>
    {{end}}
    {{define "tags"}}
      {{range .Tags}}{{. | html}},{{end}}
    {{end}}
    {{define "tags$1"}}
      {{range .Tags}}{{. | urlquery}},{{end}}
    {{end}}
    
    clone.go provides a mechanism for cloning template "tags" to produce
    "tags$1".
    
    changes to escape.go implement the new API and context propagation
    around the call graph.
    
    context.go includes minor changes to support name mangling and
    context_test.go tests those.
    
    js.go contains a bug-fix.
    
    R=nigeltao, r
    CC=golang-dev
    https://golang.org/cl/4969072

---
+++ src/pkg/exp/template/html/clone.go
@@ -0,0 +1,90 @@
+// Copyright 2011 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package html
+
+import (
+	"template/parse"
+)
+
+// clone clones a template Node.
+func clone(n parse.Node) parse.Node {
+	switch t := n.(type) {
+	case *parse.ActionNode:
+		return cloneAction(t)
+	case *parse.IfNode:
+		b := new(parse.IfNode)
+		copyBranch(&b.BranchNode, &t.BranchNode)
+		return b
+	case *parse.ListNode:
+		return cloneList(t)
+	case *parse.RangeNode:
+		b := new(parse.RangeNode)
+		copyBranch(&b.BranchNode, &t.BranchNode)
+		return b
+	case *parse.TemplateNode:
+		return cloneTemplate(t)
+	case *parse.TextNode:
+		return cloneText(t)
+	case *parse.WithNode:
+		b := new(parse.WithNode)
+		copyBranch(&b.BranchNode, &t.BranchNode)
+		return b
+	}
+	panic("cloning " + n.String() + " is unimplemented")
+}
+
+// cloneAction returns a deep clone of n.
+func cloneAction(n *parse.ActionNode) *parse.ActionNode {
+	// We use keyless fields because they won't compile if a field is added.
+	return &parse.ActionNode{n.NodeType, n.Line, clonePipe(n.Pipe)}
+}
+
+// cloneList returns a deep clone of n.
+func cloneList(n *parse.ListNode) *parse.ListNode {
+	if n == nil {
+		return nil
+	}
+	// We use keyless fields because they won't compile if a field is added.
+	c := parse.ListNode{n.NodeType, make([]parse.Node, len(n.Nodes))}
+	for i, child := range n.Nodes {
+		c.Nodes[i] = clone(child)
+	}
+	return &c
+}
+
+// clonePipe returns a shallow clone of n.
+// The escaper does not modify pipe descendants in place so there's no need to
+// clone deeply.
+func clonePipe(n *parse.PipeNode) *parse.PipeNode {
+	if n == nil {
+		return nil
+	}
+	// We use keyless fields because they won't compile if a field is added.
+	return &parse.PipeNode{n.NodeType, n.Line, n.Decl, n.Cmds}
+}
+
+// cloneTemplate returns a deep clone of n.
+func cloneTemplate(n *parse.TemplateNode) *parse.TemplateNode {
+	// We use keyless fields because they won't compile if a field is added.
+	return &parse.TemplateNode{n.NodeType, n.Line, n.Name, clonePipe(n.Pipe)}
+}
+
+// cloneText clones the given node sharing its []byte.
+func cloneText(n *parse.TextNode) *parse.TextNode {
+	// We use keyless fields because they won't compile if a field is added.
+	return &parse.TextNode{n.NodeType, n.Text}
+}
+
+// copyBranch clones src into dst.
+func copyBranch(dst, src *parse.BranchNode) {
+	// We use keyless fields because they won't compile if a field is added.
+	*dst = parse.BranchNode{
+		src.NodeType,
+		src.Line,
+		clonePipe(src.Pipe),
+		cloneList(src.List),
+		cloneList(src.ElseList),
+	}
+}
