096d1dc0cb4edf32fe70845b7010d34473b40fee
Marcel van Lohuizen <mpvl@golang.org>
2011-09-16 11:27:05 +0200

    exp/norm: changed trie to produce smaller tables. Trie now uses sparse block when this makes sense.
    R=r, r
    CC=golang-dev
    https://golang.org/cl/5010043

--- src/pkg/exp/norm/triegen.go
+++ src/pkg/exp/norm/triegen.go
@@ -17,10 +17,13 @@ import (
 	"utf8"
 )
 
+const blockSize = 64
+const maxSparseEntries = 16
+
 // Intermediate trie structure
 type trieNode struct {
 	table [256]*trieNode
-	value uint16
+	value int
 	b     byte
 	leaf  bool
 }
@@ -53,6 +56,44 @@ func (n trieNode) isInternal() bool {
 	return internal
 }
 
+func (n trieNode) mostFrequentStride() int {
+	counts := make(map[int]int)
+	v := 0
+	for _, t := range n.table[0x80 : 0x80+blockSize] {
+		if t != nil {
+			if stride := t.value - v; v != 0 && stride >= 0 {
+				counts[stride]++
+			}
+			v = t.value
+		}
+	}
+	var maxs, maxc int
+	for stride, cnt := range counts {
+		if cnt > maxc {
+			maxs, maxc = stride, cnt
+		}
+	}
+	return maxs
+}
+
+func (n trieNode) countSparseEntries() int {
+	stride := n.mostFrequentStride()
+	var count, v int
+	for _, t := range n.table[0x80 : 0x80+blockSize] {
+		tv := 0
+		if t != nil {
+			tv = t.value
+		}
+		if tv-v != stride {
+			if tv != 0 {
+				count++
+			}
+		}
+		v = tv
+	}
+	return count
+}
+
 func (n *trieNode) insert(rune int, value uint16) {
 	var p [utf8.UTFMax]byte
 	sz := utf8.EncodeRune(p[:], rune)
@@ -69,35 +110,40 @@ func (n *trieNode) insert(rune int, value uint16) {
 		}
 		n = nn
 	}
-	n.value = value
+	n.value = int(value)
 	n.leaf = true
 }
 
 type nodeIndex struct {
 	lookupBlocks []*trieNode
 	valueBlocks  []*trieNode
+	sparseBlocks []*trieNode
+	sparseOffset []uint16
+	sparseCount  int
 
-	lookupBlockIdx map[uint32]uint16
-	valueBlockIdx  map[uint32]uint16
+	lookupBlockIdx map[uint32]int
+	valueBlockIdx  map[uint32]int
 }
 
 func newIndex() *nodeIndex {
 	index := &nodeIndex{}
 	index.lookupBlocks = make([]*trieNode, 0)
 	index.valueBlocks = make([]*trieNode, 0)
-	index.lookupBlockIdx = make(map[uint32]uint16)
-	index.valueBlockIdx = make(map[uint32]uint16)
+	index.sparseBlocks = make([]*trieNode, 0)
+	index.sparseOffset = make([]uint16, 1)
+	index.lookupBlockIdx = make(map[uint32]int)
+	index.valueBlockIdx = make(map[uint32]int)
 	return index
 }
 
-func computeOffsets(index *nodeIndex, n *trieNode) uint16 {
+func computeOffsets(index *nodeIndex, n *trieNode) int {
 	if n.leaf {
 		return n.value
 	}
 	hasher := crc32.New(crc32.MakeTable(crc32.IEEE))
 	// We only index continuation bytes.
-	for i := 0; i < 64; i++ {
-		var v uint16 = 0
+	for i := 0; i < blockSize; i++ {
+		v := 0
 		if nn := n.table[0x80+i]; nn != nil {
 			v = computeOffsets(index, nn)
 		}
@@ -107,7 +153,7 @@ func computeOffsets(index *nodeIndex, n *trieNode) uint16 {
 	if n.isInternal() {
 		v, ok := index.lookupBlockIdx[h]
 		if !ok {
-			v = uint16(len(index.lookupBlocks))
+			v = len(index.lookupBlocks)
 			index.lookupBlocks = append(index.lookupBlocks, n)
 			index.lookupBlockIdx[h] = v
 		}
@@ -115,9 +161,17 @@ func computeOffsets(index *nodeIndex, n *trieNode) uint16 {
 	} else {
 		v, ok := index.valueBlockIdx[h]
 		if !ok {
-			v = uint16(len(index.valueBlocks))
-			index.valueBlocks = append(index.valueBlocks, n)
-			index.valueBlockIdx[h] = v
+			if c := n.countSparseEntries(); c > maxSparseEntries {
+				v = len(index.valueBlocks)
+				index.valueBlocks = append(index.valueBlocks, n)
+				index.valueBlockIdx[h] = v
+			} else {
+				v = -len(index.sparseOffset)
+				index.sparseBlocks = append(index.sparseBlocks, n)
+				index.sparseOffset = append(index.sparseOffset, uint16(index.sparseCount))
+				index.sparseCount += c + 1
+				index.valueBlockIdx[h] = v
+			}
 		}
 		n.value = v
 	}
@@ -125,14 +179,14 @@ func computeOffsets(index *nodeIndex, n *trieNode) uint16 {
 }
 
 func printValueBlock(nr int, n *trieNode, offset int) {
-	boff := nr * 64
+	boff := nr * blockSize
 	fmt.Printf("\n// Block %#x, offset %#x", nr, boff)
 	var printnewline bool
-	for i := 0; i < 64; i++ {
+	for i := 0; i < blockSize; i++ {
 		if i%6 == 0 {
 			printnewline = true
 		}
-		v := uint16(0)
+		v := 0
 		if nn := n.table[i+offset]; nn != nil {
 			v = nn.value
 		}
@@ -141,24 +195,55 @@ func printValueBlock(nr int, n *trieNode, offset int) {
 				fmt.Printf("\n")
 				printnewline = false
 			}
-			fmt.Printf("%#04x:%#04x, ", nr*64+i, v)
+			fmt.Printf("%#04x:%#04x, ", boff+i, v)
 		}
 	}
 }
 
-func printLookupBlock(nr int, n *trieNode, offset int) {
-	boff := nr * 64
+func printSparseBlock(nr int, n *trieNode) {
+	boff := -n.value
+	fmt.Printf("\n// Block %#x, offset %#x", nr, boff)
+	v := 0
+	//stride := f(n)
+	stride := n.mostFrequentStride()
+	c := n.countSparseEntries()
+	fmt.Printf("\n{value:%#04x,lo:%#02x},", stride, uint8(c))
+	for i, nn := range n.table[0x80 : 0x80+blockSize] {
+		nv := 0
+		if nn != nil {
+			nv = nn.value
+		}
+		if nv-v != stride {
+			if v != 0 {
+				fmt.Printf(",hi:%#02x},", 0x80+i-1)
+			}
+			if nv != 0 {
+				fmt.Printf("\n{value:%#04x,lo:%#02x", nv, nn.b)
+			}
+		}
+		v = nv
+	}
+	if v != 0 {
+		fmt.Printf(",hi:%#02x},", 0x80+blockSize-1)
+	}
+}
+
+func printLookupBlock(nr int, n *trieNode, offset, cutoff int) {
+	boff := nr * blockSize
 	fmt.Printf("\n// Block %#x, offset %#x", nr, boff)
 	var printnewline bool
-	for i := 0; i < 64; i++ {
+	for i := 0; i < blockSize; i++ {
 		if i%8 == 0 {
 			printnewline = true
 		}
-		v := uint16(0)
+		v := 0
 		if nn := n.table[i+offset]; nn != nil {
 			v = nn.value
 		}
 		if v != 0 {
+			if v < 0 {
+				v = -v - 1 + cutoff
+			}
 			if printnewline {
 				fmt.Printf("\n")
 				printnewline = false
@@ -182,7 +267,7 @@ func (t *trieNode) printTables(name string) int {
 		}
 	}
 
-	nv := len(index.valueBlocks) * 64
+	nv := len(index.valueBlocks) * blockSize
 	fmt.Printf("// %sValues: %d entries, %d bytes\n", name, nv, nv*2)
 	fmt.Printf("// Block 2 is the null block.\n")
 	fmt.Printf("var %sValues = [%d]uint16 {", name, nv)
@@ -194,18 +279,32 @@ func (t *trieNode) printTables(name string) int {
 	}
 	fmt.Print("\n}\n\n")
 
-	ni := len(index.lookupBlocks) * 64
+	ls := len(index.sparseBlocks)
+	fmt.Printf("// %sSparseOffset: %d entries, %d bytes\n", name, ls, ls*2)
+	fmt.Printf("var %sSparseOffset = %#v\n\n", name, index.sparseOffset[1:])
+
+	ns := index.sparseCount
+	fmt.Printf("// %sSparseValues: %d entries, %d bytes\n", name, ns, ns*4)
+	fmt.Printf("var %sSparseValues = [%d]valueRange {", name, ns)
+	for i, n := range index.sparseBlocks {
+		printSparseBlock(i, n)
+	}
+	fmt.Print("\n}\n\n")
+
+	cutoff := len(index.valueBlocks)
+	ni := len(index.lookupBlocks) * blockSize
 	fmt.Printf("// %sLookup: %d bytes\n", name, ni)
 	fmt.Printf("// Block 0 is the null block.\n")
 	fmt.Printf("var %sLookup = [%d]uint8 {", name, ni)
-	printLookupBlock(0, newNode(), 0)
-	printLookupBlock(1, newNode(), 0)
-	printLookupBlock(2, newNode(), 0)
-	printLookupBlock(3, t, 0xC0)
+	printLookupBlock(0, newNode(), 0, cutoff)
+	printLookupBlock(1, newNode(), 0, cutoff)
+	printLookupBlock(2, newNode(), 0, cutoff)
+	printLookupBlock(3, t, 0xC0, cutoff)
 	for i := 4; i < len(index.lookupBlocks); i++ {
-		printLookupBlock(i, index.lookupBlocks[i], 0x80)
+		printLookupBlock(i, index.lookupBlocks[i], 0x80, cutoff)
 	}
 	fmt.Print("\n}\n\n")
-	fmt.Printf("var %sTrie = trie{ %sLookup[:], %sValues[:] }\n\n", name, name, name)
-	return nv*2 + ni
+	fmt.Printf("var %sTrie = trie{ %sLookup[:], %sValues[:], %sSparseValues[:], %sSparseOffset[:], %d}\n\n",
+		name, name, name, name, name, cutoff)
+	return nv*2 + ns*4 + ni + ls*2
 }
