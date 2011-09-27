94669f7ed29fcd70f08deb824f713b6298437db8
Russ Cox <rsc@golang.org>
2011-09-27 09:13:00 -0400

    test: fix windows build
    TBR=bradfitz
    CC=golang-dev
    https://golang.org/cl/5124049

--- test/sigchld.go
+++ test/sigchld.go
@@ -1,5 +1,5 @@
-// [ "$GOOS" = windows ] ||
-// $G $D/$F.go && $L $F.$A && ./$A.out
+// ([ "$GOOS" = windows ] && echo survived SIGCHLD) ||
+// ($G $D/$F.go && $L $F.$A && ./$A.out)
 
 // Copyright 2009 The Go Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
