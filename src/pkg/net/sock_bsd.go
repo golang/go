2d89db65c0fe6d7f736bb2272b84bb9ab02c013d
Mikio Hara <mikioh.mikioh@gmail.com>
2011-11-08 06:56:35 +0900

    net: fix build comment
    R=golang-dev, rsc
    CC=golang-dev
    https://golang.org/cl/5353045

--- src/pkg/net/sock_bsd.go
+++ src/pkg/net/sock_bsd.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// +build darwin freebsd
+// +build darwin freebsd openbsd
 
 // Sockets for BSD variants
 
