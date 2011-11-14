2f3cd27bc105590722461a203b0eb0e6d2160e41
Russ Cox <rsc@golang.org>
2011-11-13 22:42:57 -0500

    time: remove unused sysSleep
    R=golang-dev, dsymonds, r
    CC=golang-dev
    https://golang.org/cl/5369094

--- src/pkg/time/sys_windows.go
+++ src/pkg/time/sys_windows.go
@@ -4,19 +4,6 @@
 
 package time
 
-import (
-	"os"
-	"syscall"
-)
-
-func sysSleep(t int64) error {
-	errno := syscall.Sleep(t)
-	if errno != 0 && errno != syscall.EINTR {
-		return os.NewSyscallError("sleep", errno)
-	}
-	return nil
-}
-
 // for testing: whatever interrupts a sleep
 func interrupt() {
 }
