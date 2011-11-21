85255f99426b97c6663db6c4a5387d7f958f0282
Benny Siegert <bsiegert@gmail.com>
2011-11-21 09:11:34 -0800

    bcrypt: Correct typo in package comment.
    R=golang-dev, gri
    CC=golang-dev
    https://golang.org/cl/5415062

--- src/pkg/crypto/bcrypt/bcrypt.go
+++ src/pkg/crypto/bcrypt/bcrypt.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// Package bcrypt implements Provos and Mazières's bcrypt adapative hashing
+// Package bcrypt implements Provos and Mazières's bcrypt adaptive hashing
 // algorithm. See http://www.usenix.org/event/usenix99/provos/provos.pdf
 package bcrypt
 
