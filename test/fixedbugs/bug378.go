ffa6b383f5ac5ab317701a4cccea3aa5b585f477
Maxim Pimenov <mpimenov@google.com>
2011-11-28 11:52:16 -0500

    gc: fix copying of types reset xmethod during copytype
    Fixes #2497
    
    R=rsc, dvyukov
    CC=golang-dev
    https://golang.org/cl/5441045

---
+++ test/fixedbugs/bug378.go
@@ -0,0 +1,19 @@
+// $G $D/$F.go && $L $F.$A && ./$A.out || echo BUG: bug378
+
+// Copyright 2011 The Go Authors.  All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Issue 2497
+
+package main
+
+type Header struct{}
+func (h Header) Method() {}
+
+var _ interface{} = Header{}
+
+func main() {
+  	type X Header
+  	var _ interface{} = X{}
+}
