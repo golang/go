0b193d9f04e25dffcedae5b77621da2cdf5745b7
Andrew Balholm <andybalholm@gmail.com>
2011-11-30 11:44:54 +1100

    html: parse the contents of <iframe> elements as raw text
    Pass tests5.dat, test 4:
    <iframe> <!---> </iframe>x
    
    | <html>
    |   <head>
    |   <body>
    |     <iframe>
    |       " <!---> "
    |     "x"
    
    Also pass tests through test 9:
    <style> <!</-- </style>x
    
    R=nigeltao
    CC=golang-dev
    https://golang.org/cl/5450044

--- src/pkg/html/parse_test.go
+++ src/pkg/html/parse_test.go
@@ -153,6 +153,8 @@ func TestParser(t *testing.T) {
 		{"tests1.dat", -1},
 		{"tests2.dat", -1},
 		{"tests3.dat", -1},
+		// tests4.dat is fragment cases.
+		{"tests5.dat", 10},
 	}
 	for _, tf := range testFiles {
 		f, err := os.Open("testdata/webkit/" + tf.filename)
