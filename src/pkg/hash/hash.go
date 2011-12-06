524a598ba169cea4b9860e6272c771d4bb64edff
Andrew Gerrand <adg@golang.org>
2011-12-06 14:12:09 +1100

    hash: rewrite comment on Hash.Sum method
    Fixes #2530.
    
    R=golang-dev, agl
    CC=golang-dev
    https://golang.org/cl/5449101

--- src/pkg/hash/hash.go
+++ src/pkg/hash/hash.go
@@ -13,9 +13,9 @@ type Hash interface {
 	// It never returns an error.
 	io.Writer
 
-	// Sum appends the current hash in the same manner as append(), without
-	// changing the underlying hash state.
-	Sum(in []byte) []byte
+	// Sum appends the current hash to b and returns the resulting slice.
+	// It does not change the underlying hash state.
+	Sum(b []byte) []byte
 
 	// Reset resets the hash to one with zero bytes written.
 	Reset()
