bf59f081c16764633e072824fdc582a6ce9136db
Dave Cheney <dave@cheney.net>
2011-12-06 18:13:20 -0500

    exp/ssh: cleanup client auth tests
    This CL cleans up the client auth tests, making the
    individual test body more manageable.
    
    Also, adds tests for rsa and dsa key negotiation.
    
    Finally, remove the package level use of the variable
    strings, which avoids conflicting with the strings pkg.
    
    R=gustav.paul, agl, n13m3y3r, rsc
    CC=golang-dev
    https://golang.org/cl/5447049

--- src/pkg/exp/ssh/common_test.go
+++ src/pkg/exp/ssh/common_test.go
@@ -8,15 +8,15 @@ import (
 	"testing"
 )
 
-var strings = map[string]string{
-	"\x20\x0d\x0a":  "\x20\x0d\x0a",
-	"flibble":       "flibble",
-	"new\x20line":   "new\x20line",
-	"123456\x07789": "123456 789",
-	"\t\t\x10\r\n":  "\t\t \r\n",
-}
-
 func TestSafeString(t *testing.T) {
+	strings := map[string]string{
+		"\x20\x0d\x0a":  "\x20\x0d\x0a",
+		"flibble":       "flibble",
+		"new\x20line":   "new\x20line",
+		"123456\x07789": "123456 789",
+		"\t\t\x10\r\n":  "\t\t \r\n",
+	}
+
 	for s, expected := range strings {
 		actual := safeString(s)
 		if expected != actual {
