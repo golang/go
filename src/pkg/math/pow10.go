b73e7f5251ae7864ad1d25e13afe6caefd853f42
Charles L. Dorian <cldorian@gmail.com>
2011-12-08 22:06:33 -0500

    math: special cases for Pow10; delete BUG
    R=rsc, golang-dev
    CC=golang-dev
    https://golang.org/cl/5477046

--- src/pkg/math/pow10.go
+++ src/pkg/math/pow10.go
@@ -9,6 +9,10 @@ package math
 var pow10tab [70]float64
 
 // Pow10 returns 10**e, the base-10 exponential of e.
+//
+// Special cases are:
+//	Pow10(e) = +Inf for e > 309
+//	Pow10(e) = 0 for e < -324
 func Pow10(e int) float64 {
 	if e <= -325 {
 		return 0
