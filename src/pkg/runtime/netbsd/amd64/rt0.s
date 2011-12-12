94ebe0f4adf3db20bc674d3cdd7d76c28ddea75f
Christopher Nielsen <m4dh4tt3r@gmail.com>
2011-12-12 18:10:11 -0500

    runtime: Changes to the runtime to support NetBSD.
    R=rsc
    CC=golang-dev
    https://golang.org/cl/5477052

---
+++ src/pkg/runtime/netbsd/amd64/rt0.s
@@ -0,0 +1,8 @@
+// Copyright 2009 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+TEXT _rt0_amd64_netbsd(SB),7,$-8
+	MOVQ	$_rt0_amd64(SB), DX
+	MOVQ	SP, DI
+	JMP	DX
