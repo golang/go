d7634ad7d9a3b914245fba89cb9737f12c07dfe6
Rémy Oudompheng <oudomphe@phare.normalesup.org>
2011-12-12 16:08:32 -0500

    gc: fix wrong arguments to error message for switches.
    Fixes #2502.
    
    R=golang-dev, rsc
    CC=golang-dev, remy
    https://golang.org/cl/5472062

--- src/cmd/gc/swt.c
+++ src/cmd/gc/swt.c
@@ -878,7 +878,7 @@ typecheckswitch(Node *n)
 						if(n->ntest)
 							yyerror("invalid case %N in switch on %N (mismatched types %T and %T)", ll->n, n->ntest, ll->n->type, t);
 						else
-							yyerror("invalid case %N in switch (mismatched types %T and bool)", ll->n, n->ntest, ll->n->type, t);
+							yyerror("invalid case %N in switch (mismatched types %T and bool)", ll->n, ll->n->type);
 					} else if(nilonly && !isconst(ll->n, CTNIL)) {
 						yyerror("invalid case %N in switch (can only compare %s %N to nil)", ll->n, nilonly, n->ntest);
 					}
