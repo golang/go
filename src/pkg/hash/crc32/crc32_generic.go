23cd406496827a7f14ada2a0756c3edd129b0218
Russ Cox <rsc@golang.org>
2011-12-14 10:25:16 -0500

    hash/crc32: make compatible with go/build
    R=golang-dev, r
    CC=golang-dev
    https://golang.org/cl/5486060

--- src/pkg/hash/crc32/crc32_generic.go
+++ src/pkg/hash/crc32/crc32_generic.go
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
+// +build 386 arm
+
 package crc32
 
 // The file contains the generic version of updateCastagnoli which just calls
