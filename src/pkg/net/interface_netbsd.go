8aa783d6213ebf5b23ca5d30aa126250c39a5585
Christopher Nielsen <m4dh4tt3r@gmail.com>
2011-12-15 12:19:19 -0500

    os: OS-dependent bits to support NetBSD.
    R=golang-dev, jsing
    CC=golang-dev
    https://golang.org/cl/5482068

---
+++ src/pkg/net/interface_netbsd.go
@@ -0,0 +1,14 @@
+// Copyright 2011 The Go Authors.  All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Network interface identification for NetBSD
+
+package net
+
+// If the ifindex is zero, interfaceMulticastAddrTable returns
+// addresses for all network interfaces.  Otherwise it returns
+// addresses for a specific interface.
+func interfaceMulticastAddrTable(ifindex int) ([]Addr, error) {
+	return nil, nil
+}
