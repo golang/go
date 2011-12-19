99d067fefa605ea74b6210c9866bff4019695fbb
Andrew Balholm <andybalholm@gmail.com>
2011-12-20 10:57:06 +1100

    html: ignore <caption>, <col>, <tbody> etc. when parsing table fragments
    Pass tests6.dat, test 36:
    <caption><col><colgroup><tbody><tfoot><thead><tr>
    
    | <tr>
    
    Pass tests through test 44:
    <body></body></html>
    
    R=nigeltao
    CC=golang-dev
    https://golang.org/cl/5494055

--- src/pkg/html/parse_test.go
+++ src/pkg/html/parse_test.go
@@ -172,7 +172,7 @@ func TestParser(t *testing.T) {
 		{"tests3.dat", -1},
 		{"tests4.dat", -1},
 		{"tests5.dat", -1},
-		{"tests6.dat", 36},
+		{"tests6.dat", 45},
 		{"tests10.dat", 13},
 	}
 	for _, tf := range testFiles {
