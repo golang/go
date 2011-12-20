90f465e2d5cd04fb0f119794fdcad9831bac3418
Russ Cox <rsc@golang.org>
2011-12-20 16:50:13 -0500

    build: use go command during build
    If something goes wrong, it should suffice to set
    USE_GO_TOOL=false in env.bash to fall back to the
    makefiles.  I will delete the makefiles in January.
    
    R=golang-dev, r
    CC=golang-dev
    https://golang.org/cl/5502047

--- src/pkg/runtime/autogen.sh
+++ src/pkg/runtime/autogen.sh
@@ -9,38 +9,7 @@
 #	autogen.sh rebuilds everything
 #	autogen.sh -clean deletes the generated files
 
-GOARCHES="
-	386
-	amd64
-	arm
-"
-
-GOOSES="
-	darwin
-	freebsd
-	linux
-	netbsd
-	openbsd
-	plan9
-	windows
-"
-
-GOOSARCHES="
-	darwin_386
-	darwin_amd64
-	freebsd_386
-	freebsd_amd64
-	linux_386
-	linux_amd64
-	linux_arm
-	netbsd_386
-	netbsd_amd64
-	openbsd_386
-	openbsd_amd64
-	plan9_386
-	windows_386
-	windows_amd64
-"
+. ../../buildinfo.sh
 
 HELPERS="goc2c mkversion"
 
