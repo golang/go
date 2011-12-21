6645602c0b24e762a9fc009376fef1d83760763d
Anthony Martin <ality@pbrane.org>
2011-12-21 07:52:07 -0500

    os/signal: do not build on Plan 9
    R=golang-dev, rsc
    CC=golang-dev
    https://golang.org/cl/5503057

--- src/pkg/os/signal/signal.go
+++ src/pkg/os/signal/signal.go
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
+// +build darwin freebsd linux netbsd openbsd windows
+
 // Package signal implements operating system-independent signal handling.
 package signal
 
