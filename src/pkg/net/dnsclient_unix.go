9ca57a706c7831f1aab47cedee0d383c3ddb7868
Joel Sing <jsing@google.com>
2011-12-21 21:44:47 +1100

    crypto/mime/net/time: add netbsd to +build tags
    R=golang-dev, mikioh.mikioh
    CC=golang-dev
    https://golang.org/cl/5501052

--- src/pkg/net/dnsclient_unix.go
+++ src/pkg/net/dnsclient_unix.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// +build darwin freebsd linux openbsd
+// +build darwin freebsd linux netbsd openbsd
 
 // DNS client: see RFC 1035.
 // Has to be linked into package net for Dial.
