18f7c0a3f6f39af5cd2db484dbf0817fbfb526d5
Rob Pike <r@golang.org>
2011-12-21 11:46:42 -0800

    path/filepath.Rel: document that the returned path is always relative
    Fixes #2593.
    
    R=rsc, alex.brainman, n13m3y3r
    CC=golang-dev
    https://golang.org/cl/5500052

--- src/pkg/path/filepath/path.go
+++ src/pkg/path/filepath/path.go
@@ -262,6 +262,8 @@ func Abs(path string) (string, error) {
 // Rel returns a relative path that is lexically equivalent to targpath when
 // joined to basepath with an intervening separator. That is,
 // Join(basepath, Rel(basepath, targpath)) is equivalent to targpath itself.
+// On success, the returned path will always be relative to basepath,
+// even if basepath and targpath share no elements.
 // An error is returned if targpath can't be made relative to basepath or if
 // knowing the current working directory would be necessary to compute it.
 func Rel(basepath, targpath string) (string, error) {
