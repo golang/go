c4663f4a1574370ba079bac9332c831a58aa9d50
Robert Griesemer <gri@golang.org>
2012-01-04 14:06:54 -0800

    runtime: fix typo in comment
    R=golang-dev, dsymonds
    CC=golang-dev
    https://golang.org/cl/5511047

--- src/pkg/runtime/extern.go
+++ src/pkg/runtime/extern.go
@@ -19,8 +19,8 @@ func Gosched()
 func Goexit()
 
 // Caller reports file and line number information about function invocations on
-// the calling goroutine's stack.  The argument skip is the number of stack frames to
-// ascend, with 0 identifying the the caller of Caller.  The return values report the
+// the calling goroutine's stack.  The argument skip is the number of stack frames
+// to ascend, with 0 identifying the caller of Caller.  The return values report the
 // program counter, file name, and line number within the file of the corresponding
 // call.  The boolean ok is false if it was not possible to recover the information.
 func Caller(skip int) (pc uintptr, file string, line int, ok bool)
