1421b4ceff1a9229914c2a03e0fc7e6b8520fe5c
Anthony Martin <ality@pbrane.org>
2012-01-09 15:09:40 -0800

    syscall: ignore godefs input when building on Plan 9
    R=golang-dev, akumar, rsc
    CC=golang-dev
    https://golang.org/cl/5534055

--- src/pkg/syscall/types_plan9.c
+++ src/pkg/syscall/types_plan9.c
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
+// +build ignore
+
 /*
 Input to godefs.  See also mkerrors.sh and mkall.sh
 */
