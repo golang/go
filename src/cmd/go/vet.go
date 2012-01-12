32fc88d4d2d054a3a6c5cdfd5d19047d8dd49064
Russ Cox <rsc@golang.org>
2012-01-12 15:28:52 -0800

    cmd/go: use relative paths in go fix, go fmt, go vet output
    Fixes #2686.
    
    R=golang-dev, bradfitz, r
    CC=golang-dev
    https://golang.org/cl/5528089

--- src/cmd/go/vet.go
+++ src/cmd/go/vet.go
@@ -25,6 +25,6 @@ func runVet(cmd *Command, args []string) {
 		// Use pkg.gofiles instead of pkg.Dir so that
 		// the command only applies to this package,
 		// not to packages in subdirectories.
-		run("govet", pkg.gofiles)
+		run("govet", relPaths(pkg.gofiles))
 	}
 }
