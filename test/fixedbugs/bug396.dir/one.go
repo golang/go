6ff01f01f4e477a931d10c133f33bfe7e0c4ef15
Luuk van Dijk <lvd@golang.org>
2012-01-18 17:51:28 +0100

    gc: fieldnames in structliterals in exported inlines should not be qualified if they're embedded builtin types.
    Trust me.
    Fixes #2687.
    
    R=rsc
    CC=golang-dev
    https://golang.org/cl/5545047

---
+++ test/fixedbugs/bug396.dir/one.go
@@ -0,0 +1,9 @@
+// Copyright 2012 The Go Authors.  All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package one
+
+type T struct { int }
+
+func New(i int) T { return T{i} }
