2a6b4e120a6f67d532f698f238547690bc18efba
Christoph Hack <christoph@tux21b.org>
2012-01-19 06:11:02 -0800

    net/http: the documentation should call NewRequest with the right signature.
    R=golang-dev, dsymonds
    CC=golang-dev
    https://golang.org/cl/5554063

--- src/pkg/net/http/doc.go
+++ src/pkg/net/http/doc.go
@@ -34,7 +34,8 @@ settings, create a Client:
 	resp, err := client.Get("http://example.com")
 	// ...
 
-	req := http.NewRequest("GET", "http://example.com", nil)
+	req, err := http.NewRequest("GET", "http://example.com", nil)
+	// ...
 	req.Header.Add("If-None-Match", `W/"wyzzy"`)
 	resp, err := client.Do(req)
 	// ...
