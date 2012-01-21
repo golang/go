61f702d4837c97bcefa85f8a8087bee35d9b6329
David Symonds <dsymonds@golang.org>
2012-01-21 17:02:54 +1100

    gc: test case for recursive interface bug.
    R=rsc
    CC=golang-dev
    https://golang.org/cl/5555066

---
+++ test/interface/recursive2.go
@@ -0,0 +1,22 @@
+// $G $D/recursive1.go && $G $D/$F.go
+
+// Copyright 2012 The Go Authors.  All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Check that the mutually recursive types in recursive1.go made it
+// intact and with the same meaning, by assigning to or using them.
+
+package main
+
+import "./recursive1"
+
+func main() {
+	var i1 p.I1
+	var i2 p.I2
+	i1 = i2
+	i2 = i1
+	i1 = i2.F()
+	i2 = i1.F()
+	_, _ = i1, i2
+}
