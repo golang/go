59ae15106a2ae0a409f710a16a8180ae81f81a3d
Russ Cox <rsc@golang.org>
2012-01-24 14:15:37 -0500

    cmd/go: fix get github
    R=golang-dev, bradfitz
    CC=golang-dev
    https://golang.org/cl/5569054

--- src/cmd/go/vcs.go
+++ src/cmd/go/vcs.go
@@ -286,6 +286,7 @@ var vcsPaths = []*vcsPath{
 	{
 		prefix: "github.com/",
 		re:     `^(?P<root>github\.com/[A-Za-z0-9_.\-]+/[A-Za-z0-9_.\-]+)(/[A-Za-z0-9_.\-]+)*$`,
+		vcs:    "git",
 		repo:   "https://{root}",
 		check:  noVCSSuffix,
 	},
