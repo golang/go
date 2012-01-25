9d4ae0ae5cab63013aac9f7682292324f1951666
Gustavo Niemeyer <gustavo@niemeyer.net>
2012-01-25 21:07:00 -0200

    gofix: handle xml.Unmarshal in xmlapi fix
    This improves the handling of xml.Unmarshal in
    the xmlapi fix by guessing some of the common
    types used on it.
    
    This also fixes a bug in the partial typechecker.
    In an expression such as f(&a), it'd mark a as
    having &T rather than *T.
    
    R=golang-dev, rsc
    CC=golang-dev
    https://golang.org/cl/5572058

--- src/cmd/gofix/xmlapi_test.go
+++ src/cmd/gofix/xmlapi_test.go
@@ -19,11 +19,31 @@ func f() {
 	xml.Marshal(a, b)
 	xml.Unmarshal(a, b)
 
+	var buf1 bytes.Buffer
+	buf2 := &bytes.Buffer{}
+	buf3 := bytes.NewBuffer(data)
+	buf4 := bytes.NewBufferString(data)
+	buf5 := bufio.NewReader(r)
+	xml.Unmarshal(&buf1, v)
+	xml.Unmarshal(buf2, v)
+	xml.Unmarshal(buf3, v)
+	xml.Unmarshal(buf4, v)
+	xml.Unmarshal(buf5, v)
+
+	f := os.Open("foo.xml")
+	xml.Unmarshal(f, v)
+
 	p1 := xml.NewParser(stream)
 	p1.Unmarshal(v, start)
 
-	var p2 xml.Parser
+	var p2 *xml.Parser
 	p2.Unmarshal(v, start)
+}
+
+func g(r io.Reader, f *os.File, b []byte) {
+	xml.Unmarshal(r, v)
+	xml.Unmarshal(f, v)
+	xml.Unmarshal(b, v)
 }
 `,
 		Out: `package main
@@ -34,11 +54,31 @@ func f() {
 	xml.NewEncoder(a).Encode(b)
 	xml.Unmarshal(a, b)
 
+	var buf1 bytes.Buffer
+	buf2 := &bytes.Buffer{}
+	buf3 := bytes.NewBuffer(data)
+	buf4 := bytes.NewBufferString(data)
+	buf5 := bufio.NewReader(r)
+	xml.NewDecoder(&buf1).Decode(v)
+	xml.NewDecoder(buf2).Decode(v)
+	xml.NewDecoder(buf3).Decode(v)
+	xml.NewDecoder(buf4).Decode(v)
+	xml.NewDecoder(buf5).Decode(v)
+
+	f := os.Open("foo.xml")
+	xml.NewDecoder(f).Decode(v)
+
 	p1 := xml.NewDecoder(stream)
 	p1.DecodeElement(v, start)
 
-	var p2 xml.Decoder
+	var p2 *xml.Decoder
 	p2.DecodeElement(v, start)
+}
+
+func g(r io.Reader, f *os.File, b []byte) {
+	xml.NewDecoder(r).Decode(v)
+	xml.NewDecoder(f).Decode(v)
+	xml.Unmarshal(b, v)
 }
 `,
 	},
