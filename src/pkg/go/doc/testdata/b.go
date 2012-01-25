e881e825779207446ce9f007608ac1c5541bee57
Robert Griesemer <gri@golang.org>
2012-01-25 09:54:10 -0800

    go/doc: test case for corner case (override of predecl. type)
    R=rsc
    CC=golang-dev
    https://golang.org/cl/5575055

--- src/pkg/go/doc/testdata/b.go
+++ src/pkg/go/doc/testdata/b.go
@@ -6,8 +6,25 @@ package b
 
 import "a"
 
+// Basic declarations
+
 const Pi = 3.14   // Pi
 var MaxInt int    // MaxInt
 type T struct{}   // T
 var V T           // v
 func F(x int) int {} // F
+func (x *T) M()   {} // M
+
+// Corner cases: association with (presumed) predeclared types
+
+// Always under the package functions list.
+func NotAFactory() int {}
+
+// Associated with uint type if AllDecls is set.
+func UintFactory() uint {}
+
+// Associated with uint type if AllDecls is set.
+func uintFactory() uint {}
+
+// Should only appear if AllDecls is set.
+type uint struct{} // overrides a predeclared type uint
