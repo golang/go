f3959323f999566891eb64c759ba35b2ef288572
Andrew Gerrand <adg@golang.org>
2012-02-01 15:12:24 +1100

    go/build: update syslist.go package comment
    It's no longer generated by make.
    
    R=golang-dev, bradfitz
    CC=golang-dev
    https://golang.org/cl/5602047

--- src/pkg/go/build/syslist.go
+++ src/pkg/go/build/syslist.go
@@ -1,4 +1,7 @@
-// Generated automatically by make.
+// Copyright 2011 The Go Authors.  All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 package build
 
 const goosList = "darwin freebsd linux netbsd openbsd plan9 windows "
