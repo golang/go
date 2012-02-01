0ee9ab2bbd633a9592159d30a1bc309add5ecae4
Anthony Martin <ality@pbrane.org>
2012-01-31 18:14:44 -0800

    build: add include files for Plan 9
    Previously, I had made available a tarball of
    the modified system headers that were necessary
    to build on Plan 9 but that was only a stopgap.
    I think this method is much better since no
    files outside of $GOROOT will have to be added
    or modified during the build process.
    
    Also, this is just the first step. I'll change
    the build to reference these files in another CL
    (that also contains a few more Makefile changes).
    
    R=golang-dev, rsc
    CC=golang-dev
    https://golang.org/cl/5552056

---
+++ include/plan9/libc.h
@@ -0,0 +1,19 @@
+// Copyright 2012 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+#include "/sys/include/libc.h"
+#include "/sys/include/ctype.h"
+
+enum
+{
+	Runemax = 0x10FFFF, /* maximum rune value */
+};
+
+#pragma src "/sys/src/go/src/lib9"
+#pragma lib "/sys/src/go/lib/$M/lib9.a"
+
+char*	getgoos(void);
+char*	getgoarch(void);
+char*	getgoroot(void);
+char*	getgoversion(void);
