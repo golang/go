cc777490fbe5f0c6575f4e84dea31b7d745d747e
Anthony Martin <ality@pbrane.org>
2012-01-31 18:14:44 -0800

    build: add include files for Plan 9
    Previously, I had made available a tarball of
    the modified system headers that were necessary
    to build on Plan 9 but that was only a stopgap.
    I think this method is much better since no
    files outside of $GOROOT will have to be added
    or modified during the build process.
    
    Also, this is just the first step. I'll change
    the build to reference these files in another CL
    (that also contains a few more Makefile changes).
    
    R=golang-dev, rsc
    CC=golang-dev
    https://golang.org/cl/5552056

---
+++ include/plan9/386/u.h
@@ -0,0 +1,14 @@
+// Copyright 2012 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+#include "/386/include/u.h"
+
+typedef char   int8;
+typedef uchar  uint8;
+typedef short  int16;
+typedef ushort uint16;
+typedef int    int32;
+typedef uint   uint32;
+typedef vlong  int64;
+typedef uvlong uint64;
---
+++ include/plan9/mach.h
@@ -0,0 +1,7 @@
+// Copyright 2012 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+#pragma src "/sys/src/go/src/libmach"
+#pragma lib "/sys/src/go/lib/$M/libmach.a"
+#include "../mach.h"
