c0bc0d56fcd39a375c4765d96761698c7c778ab7
Ian Lance Taylor <iant@golang.org>
2012-02-03 07:19:25 -0800

    test: add test for receiver named _
    Was miscompiled by gccgo.
    
    R=golang-dev, dsymonds
    CC=golang-dev
    https://golang.org/cl/5622054

---
+++ test/fixedbugs/bug405.go
@@ -0,0 +1,24 @@
+// $G $D/$F.go && $L $F.$A && ./$A.out
+
+// Copyright 2009 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Test using _ receiver.  Failed with gccgo.
+
+package main
+
+type S struct {}
+
+func (_ S) F(i int) int {
+	return i
+}
+
+func main() {
+	s := S{}
+	const c = 123
+	i := s.F(c)
+	if i != c {
+		panic(i)
+	}
+}
