77cb96c07f72b8e806c48c7d21275ee18751369d
Russ Cox <rsc@golang.org>
2012-02-02 19:42:02 -0500

    syscall: fix // +build comments in types_*.go
    I fixed types_linux.go yesterday but missed the others.
    
    R=golang-dev, bradfitz
    CC=golang-dev
    https://golang.org/cl/5620054

--- src/pkg/syscall/types_linux.go
+++ src/pkg/syscall/types_linux.go
@@ -2,9 +2,11 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// Input to cgo -godefs.  See also mkerrors.sh and mkall.sh
-
 // +build ignore
+
+/*
+Input to cgo -godefs.  See also mkerrors.sh and mkall.sh
+*/
 
 // +godefs map struct_in_addr [4]byte /* in_addr */
 // +godefs map struct_in6_addr [16]byte /* in6_addr */
