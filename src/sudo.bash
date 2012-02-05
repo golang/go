cdfd5b2bede93497f4c74fa0959c3d6c8445fd57
David Symonds <dsymonds@golang.org>
2012-02-05 14:50:38 +1100

    build: fix sudo.bash.
    R=rsc, balasanjay, rsc
    CC=golang-dev
    https://golang.org/cl/5630051

--- src/sudo.bash
+++ src/sudo.bash
@@ -4,7 +4,6 @@
 # license that can be found in the LICENSE file.
 
 set -e
-. ./env.bash
 
 case "`uname`" in
 Darwin)
@@ -18,12 +17,13 @@ if [[ ! -d /usr/local/bin ]]; then
 	exit 2
 fi
 
+cd $(dirname $0)
 for i in prof cov
 do
 	# Remove old binaries if present
 	sudo rm -f /usr/local/bin/6$i
 	# Install new binaries
-	sudo cp "$GOROOT"/bin/tool/$i /usr/local/bin/go$i
+	sudo cp ../bin/tool/$i /usr/local/bin/go$i
 	sudo chgrp procmod /usr/local/bin/go$i
 	sudo chmod g+s /usr/local/bin/go$i
 done
