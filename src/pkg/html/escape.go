e066db3acbd65f12349d0dff36332e2c7648711d
Nigel Tao <nigeltao@golang.org>
2012-02-06 13:24:45 +1100

    html: add package doc.
    Fixes #2857.
    
    R=r, adg
    CC=golang-dev
    https://golang.org/cl/5635046

--- src/pkg/html/escape.go
+++ src/pkg/html/escape.go
@@ -2,6 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
+// Package html provides functions for escaping and unescaping HTML text.
 package html
 
 import (
