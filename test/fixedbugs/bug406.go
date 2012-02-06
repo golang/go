3bccf32eb4066ddde0c828e2963a9b6dc90e3d0c
Luuk van Dijk <lvd@golang.org>
2012-02-06 15:41:01 +0100

    cmd/gc: fix codegen reordering for expressions involving && and ||
    Fixes #2821.
    
    R=rsc
    CC=golang-dev
    https://golang.org/cl/5606061

---
+++ test/fixedbugs/bug406.go
@@ -0,0 +1,25 @@
+// $G $D/$F.go && $L $F.$A && ./$A.out || echo "Bug406"
+
+// Copyright 2012 The Go Authors.  All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Issue 2821
+package main
+
+type matrix struct {
+	e []int
+}
+
+func (a matrix) equal() bool {
+	for _ = range a.e {
+	}
+	return true
+}
+
+func main() {
+	var a matrix
+	var i interface{}
+	i = true && a.equal()
+	_ = i
+}
