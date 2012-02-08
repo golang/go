fc6e94c5c07dafa50ffabaef2785eb1452d44bdb
Rob Pike <r@golang.org>
2012-02-08 14:24:04 +1100

    encoding/csv: document ReadAll behavior at EOF
    Fixes #2847.
    
    R=golang-dev, bradfitz
    CC=golang-dev
    https://golang.org/cl/5641050

--- src/pkg/encoding/csv/reader.go
+++ src/pkg/encoding/csv/reader.go
@@ -156,6 +156,9 @@ func (r *Reader) Read() (record []string, err error) {
 
 // ReadAll reads all the remaining records from r.
 // Each record is a slice of fields.
+// A successful call returns err == nil, not err == EOF. Because ReadAll is
+// defined to read until EOF, it does not treat end of file as an error to be
+// reported.
 func (r *Reader) ReadAll() (records [][]string, err error) {
 	for {
 		record, err := r.Read()
