0846e275a88b1b0b92a8cacab5b7e4cfc747e072
Bjorn Tipling <bjorn.tipling@gmail.com>
2012-02-10 22:39:57 -0200

    net/http: fix reference to URL.RawPath in docs
    R=go.peter.90, n13m3y3r, gustavo
    CC=golang-dev
    https://golang.org/cl/5655053

--- src/pkg/net/http/doc.go
+++ src/pkg/net/http/doc.go
@@ -60,7 +60,7 @@ Handle and HandleFunc add handlers to DefaultServeMux:
 	http.Handle("/foo", fooHandler)
 
 	http.HandleFunc("/bar", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.RawPath))
+		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
 	})
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
