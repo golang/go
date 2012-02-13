f735d2d9d3d9665d0e5058615ac6f62e2ba79887
Russ Cox <rsc@golang.org>
2012-02-12 23:39:40 -0500

    testing: use runtime/debug to format panics
    Among other things, this avoids putting a testing.go:nnn:
    prefix on every line of the stack trace.
    
    R=golang-dev, r, dsymonds, r
    CC=golang-dev
    https://golang.org/cl/5651081

--- src/pkg/runtime/debug/stack_test.go
+++ src/pkg/runtime/debug/stack_test.go
@@ -2,9 +2,10 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-package debug
+package debug_test
 
 import (
+	. "runtime/debug"
 	"strings"
 	"testing"
 )
