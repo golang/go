9f8c2c8bbfacf6eb320361ba93aef2f70c7b1f4f
Anthony Martin <ality@pbrane.org>
2012-02-14 14:22:34 -0500

    os: rename SyscallError.Errno to SyscallError.Err
    This lets us get rid of the OS-dependent implementations
    of SyscallError.  The name "Err" was chosen to match the
    PathError type.
    
    R=golang-dev, rsc
    CC=golang-dev
    https://golang.org/cl/5651084

--- src/pkg/os/error_plan9.go
+++ src/pkg/os/error_plan9.go
@@ -9,24 +9,6 @@ import (
 	"syscall"
 )
 
-// SyscallError records an error from a specific system call.
-type SyscallError struct {
-	Syscall string
-	Err     string
-}
-
-func (e *SyscallError) Error() string { return e.Syscall + ": " + e.Err }
-
-// NewSyscallError returns, as an error, a new SyscallError
-// with the given system call name and error details.
-// As a convenience, if err is nil, NewSyscallError returns nil.
-func NewSyscallError(syscall string, err error) error {
-	if err == nil {
-		return nil
-	}
-	return &SyscallError{syscall, err.Error()}
-}
-
 var (
 	Eshortstat = errors.New("stat buffer too small")
 	Ebadstat   = errors.New("malformed stat buffer")
