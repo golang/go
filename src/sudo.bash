95902d10d2de03b1c22c626462627fb2251cf55b
David Symonds <dsymonds@golang.org>
2012-02-15 08:42:16 +1100

    build: fix sudo.bash.
    R=golang-dev, rsc
    CC=golang-dev
    https://golang.org/cl/5647063

--- src/sudo.bash
+++ src/sudo.bash
@@ -17,13 +17,14 @@ if [[ ! -d /usr/local/bin ]]; then
 	exit 2
 fi
 
+eval $(go tool dist env)
 cd $(dirname $0)
 for i in prof cov
 do
 	# Remove old binaries if present
 	sudo rm -f /usr/local/bin/6$i
 	# Install new binaries
-	sudo cp ../bin/tool/$i /usr/local/bin/go$i
+	sudo cp $GOTOOLDIR/$i /usr/local/bin/go$i
 	sudo chgrp procmod /usr/local/bin/go$i
 	sudo chmod g+s /usr/local/bin/go$i
 done
