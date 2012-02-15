3b105ead4baf144311e6f666a46aa1ac108f7eb3
Andrew Gerrand <adg@golang.org>
2012-02-15 13:38:28 +1100

    strings: add Bernardo O'Higgins example
    R=r, bradfitz
    CC=golang-dev, rogpeppe
    https://golang.org/cl/5673049

--- src/pkg/strings/example_test.go
+++ src/pkg/strings/example_test.go
@@ -93,10 +93,12 @@ func ExampleReplace() {
 // ["a" "b" "c"]
 // ["" "man " "plan " "canal panama"]
 // [" " "x" "y" "z" " "]
+// [""]
 func ExampleSplit() {
 	fmt.Printf("%q\n", strings.Split("a,b,c", ","))
 	fmt.Printf("%q\n", strings.Split("a man a plan a canal panama", "a "))
 	fmt.Printf("%q\n", strings.Split(" xyz ", ""))
+	fmt.Printf("%q\n", strings.Split("", "Bernardo O'Higgins"))
 }
 
 // ["a" "b,c"]
