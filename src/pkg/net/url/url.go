960e3c12c9733a318b01fe498dc11a33e3b7d399
David Symonds <dsymonds@golang.org>
2012-02-17 15:31:07 +1100

    net/url: spell the package name correctly.
    R=bradfitz
    CC=golang-dev
    https://golang.org/cl/5676076

--- src/pkg/net/url/url.go
+++ src/pkg/net/url/url.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// Package URL parses URLs and implements query escaping.
+// Package url parses URLs and implements query escaping.
 // See RFC 3986.
 package url
 
