a4d124d75b1d0610a591ede8b24110b08764feac
Russ Cox <rsc@golang.org>
2012-02-16 22:04:13 -0500

    log/syslog: disable on Windows
    We want to be able to implement good Windows support
    after Go 1.  Right now Windows tries to use Unix domain
    sockets, and I'd rather just have it not be available.
    
    R=golang-dev, bradfitz
    CC=golang-dev
    https://golang.org/cl/5671076

--- src/pkg/log/syslog/syslog_test.go
+++ src/pkg/log/syslog/syslog_test.go
@@ -1,6 +1,9 @@
 // Copyright 2009 The Go Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
+
+// +build !windows,!plan9
+
 package syslog
 
 import (
