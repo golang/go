a9e57f743d658ba27d3165dc6841915b12a98879
Joe Poirier <jdpoirier@gmail.com>
2012-02-19 22:21:41 -0600

    misc/dist/windows: ongoing dev
    Added the (properly formatted) license file back,
    the installer adds go\bin to the system PATH now,
    the output package names are in line with the linux
    and darwin versions, dist.bat extracts GOARCH in a
    sane way, readme cleanup.
    
    Tested on Windows 7 only. It would be helpful if
    someone else could give it a try. See the readme
    for details.
    
    R=golang-dev, bradfitz
    CC=golang-dev
    https://golang.org/cl/5673099

--- misc/dist/windows/dist.bat
+++ misc/dist/windows/dist.bat
@@ -5,31 +5,32 @@
 
 setlocal
 
-:: Requires WiX (candle light heat), 7zip, and hg
+:: Requires Windows Installer XML (WiX), 7zip, and Mercurial (hg)
 
-echo # Setting variable info
+echo # Setting some variables
 for /f %%i in ('hg.exe root') do set ROOT=%%i
 for /f %%i in ('hg.exe id -n') do set ID=%%i
 for /f "tokens=3" %%i in ('%ROOT%\bin\go.exe version') do set VER=%%i
 if errorlevel 1 goto end
 
 echo # Getting GOARCH
-%ROOT%\bin\go tool dist env > env.txt
-set GOARCH /p = find "GOARCH" "env.txt">NUL
-del /F /Q /S env.txt>NUL
+%ROOT%\bin\go tool dist env -w>env.bat
+call env.bat
+del /F /Q /S env.bat>NUL
 if errorlevel 1 goto end
+
 
 rmdir /S /Q go>NUL
 mkdir go
 
-echo # Cloning the go tree
+echo # Cloning the Go tree
 hg clone -r %ID% %ROOT% go
 if errorlevel 1 goto end
 
 rmdir /S /Q  go\.hg>NUL
 del /F /Q /S go\.hgignore go\.hgtags>NUL
 
-echo # Copying pkg, bin and src/pkg/runtime/z*
+echo # Copying pkg, bin, and src/pkg/runtime/z*
 xcopy %ROOT%\pkg                   go\pkg /V /E /Y /I
 xcopy %ROOT%\bin                   go\bin /V /E /Y /I
 xcopy %ROOT%\src\pkg\runtime\z*.c  go\src\pkg\runtime  /V /E /Y
@@ -37,8 +38,9 @@ xcopy %ROOT%\src\pkg\runtime\z*.go go\src\pkg\runtime  /V /E /Y
 xcopy %ROOT%\src\pkg\runtime\z*.h  go\src\pkg\runtime  /V /E /T
 
 echo # Starting zip packaging
-7za a -tzip -mx=9 gowin%GOARCH%"_"%VER%.zip "go/"
+7za a -tzip -mx=9 go.%VER%.windows-%GOARCH%.zip "go/"
 if errorlevel 1 goto end
+
 
 echo # Starting Go directory file harvesting
 heat dir go -nologo -cg AppFiles -gg -g1 -srd -sfrag -template fragment -dr INSTALLDIR -var var.SourceDir -out AppFiles.wxs
@@ -46,9 +48,10 @@ if errorlevel 1 goto end
 
 echo # Starting installer packaging
 candle -nologo -dVersion=%VER% -dArch=%GOARCH% -dSourceDir=go installer.wxs AppFiles.wxs
-light -nologo -ext WixUIExtension -ext WixUtilExtension installer.wixobj AppFiles.wixobj -o gowin%GOARCH%"_"%VER%.msi
+light -nologo -ext WixUIExtension -ext WixUtilExtension installer.wixobj AppFiles.wixobj -o go.%VER%.windows-%GOARCH%.msi
 if errorlevel 1 goto end
 
 del /F /Q /S *.wixobj AppFiles.wxs *.wixpdb>NUL
 
 :end
+endlocal
