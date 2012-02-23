5a5064eb1a806bae470b170157739bcefdc3c2b6
Robert Griesemer <gri@golang.org>
2012-02-22 23:40:28 -0800

    exp/types: fix package comment
    R=golang-dev, dsymonds
    CC=golang-dev
    https://golang.org/cl/5695046

--- src/pkg/exp/types/types.go
+++ src/pkg/exp/types/types.go
@@ -2,8 +2,8 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// PACKAGE UNDER CONSTRUCTION. ANY AND ALL PARTS MAY CHANGE.
-// Package types declares the types used to represent Go types.
+// Package types declares the types used to represent Go types
+// (UNDER CONSTRUCTION). ANY AND ALL PARTS MAY CHANGE.
 //
 package types
 
