b5c3bc2ea0f1a1a4b90ad170593da66bb8936933
Russ Cox <rsc@golang.org>
2012-02-22 21:46:12 -0500

    pprof: add Profile type
    Makes it possible for client code to maintain its own profiles,
    and also reduces the API surface by giving us a type that
    models built-in profiles.
    
    R=golang-dev, r
    CC=golang-dev
    https://golang.org/cl/5684056

--- src/pkg/text/tabwriter/tabwriter_test.go
+++ src/pkg/text/tabwriter/tabwriter_test.go
@@ -2,11 +2,12 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-package tabwriter
+package tabwriter_test
 
 import (
 	"io"
 	"testing"
+	. "text/tabwriter"
 )
 
 type buffer struct {
