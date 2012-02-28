e266d6064c7e485673ea651d673b2ca22e0649f2
Mikio Hara <mikioh.mikioh@gmail.com>
2012-02-29 07:45:38 +0900

    net: fix comment on Dial with unixgram
    We should use DialUnix or ListenPacket for unixgram networks
    because Dial doesn't take a local UnixAddr.
    
    R=golang-dev, rsc
    CC=golang-dev
    https://golang.org/cl/5706043

--- src/pkg/net/dial.go
+++ src/pkg/net/dial.go
@@ -69,7 +69,7 @@ func resolveNetAddr(op, net, addr string) (afnet string, a Addr, err error) {
 //
 // Known networks are "tcp", "tcp4" (IPv4-only), "tcp6" (IPv6-only),
 // "udp", "udp4" (IPv4-only), "udp6" (IPv6-only), "ip", "ip4"
-// (IPv4-only), "ip6" (IPv6-only), "unix", "unixgram" and "unixpacket".
+// (IPv4-only), "ip6" (IPv6-only), "unix" and "unixpacket".
 //
 // For TCP and UDP networks, addresses have the form host:port.
 // If host is a literal IPv6 address, it must be enclosed
