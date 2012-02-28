1787d5ed96613759adb2b9245667814bbe93f25a
Mikio Hara <mikioh.mikioh@gmail.com>
2012-02-28 10:58:01 +0900

    net: no panic on placeholders for netbsd
    Perhaps it might be better than panic.
    
    R=golang-dev, r
    CC=golang-dev
    https://golang.org/cl/5701064

--- src/pkg/net/sockoptip_netbsd.go
+++ src/pkg/net/sockoptip_netbsd.go
@@ -2,14 +2,38 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// +build netbsd
+// IP-level socket options for NetBSD
 
 package net
 
+import "syscall"
+
+func ipv4MulticastInterface(fd *netFD) (*Interface, error) {
+	// TODO: Implement this
+	return nil, syscall.EAFNOSUPPORT
+}
+
 func setIPv4MulticastInterface(fd *netFD, ifi *Interface) error {
-	panic("unimplemented")
+	// TODO: Implement this
+	return syscall.EAFNOSUPPORT
+}
+
+func ipv4MulticastLoopback(fd *netFD) (bool, error) {
+	// TODO: Implement this
+	return false, syscall.EAFNOSUPPORT
 }
 
 func setIPv4MulticastLoopback(fd *netFD, v bool) error {
-	panic("unimplemented")
+	// TODO: Implement this
+	return syscall.EAFNOSUPPORT
+}
+
+func ipv4ReceiveInterface(fd *netFD) (bool, error) {
+	// TODO: Implement this
+	return false, syscall.EAFNOSUPPORT
+}
+
+func setIPv4ReceiveInterface(fd *netFD, v bool) error {
+	// TODO: Implement this
+	return syscall.EAFNOSUPPORT
 }
