95e1da2b9f39391cb4ec1610cfe89cb444e273b1
Rob Pike <r@golang.org>
2012-02-29 13:30:08 +1100

    io: document that i/o is not necessarily safe for parallel access.
    Updates #1599.
    
    R=golang-dev, adg, dsymonds
    CC=golang-dev
    https://golang.org/cl/5704052

--- src/pkg/io/io.go
+++ src/pkg/io/io.go
@@ -6,6 +6,10 @@
 // Its primary job is to wrap existing implementations of such primitives,
 // such as those in package os, into shared public interfaces that
 // abstract the functionality, plus some other related primitives.
+//
+// Because these interfaces and primitives wrap lower-level operations with
+// various implementations, unless otherwise informed clients should not
+// assume they are safe for parallel execution.
 package io
 
 import (
