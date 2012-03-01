7c06560605e95cfb61292cf1b6b66b1b02588a09
Russ Cox <rsc@golang.org>
2012-03-01 17:55:47 -0500

    reflect: expand doc for Value.Interface
    R=golang-dev, r
    CC=golang-dev
    https://golang.org/cl/5716057

--- src/pkg/reflect/value.go
+++ src/pkg/reflect/value.go
@@ -800,13 +800,15 @@ func (v Value) CanInterface() bool {
 	return v.flag&(flagMethod|flagRO) == 0
 }
 
-// Interface returns v's value as an interface{}.
+// Interface returns v's current value as an interface{}.
+// It is equivalent to:
+//	var i interface{} = (v's underlying value)
 // If v is a method obtained by invoking Value.Method
 // (as opposed to Type.Method), Interface cannot return an
 // interface value, so it panics.
 // It also panics if the Value was obtained by accessing
 // unexported struct fields.
-func (v Value) Interface() interface{} {
+func (v Value) Interface() (i interface{}) {
 	return valueInterface(v, true)
 }
 
