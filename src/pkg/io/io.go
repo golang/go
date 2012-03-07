ce84f14aa050877533c65c521a8679ba147a20bc
Brad Fitzpatrick <bradfitz@golang.org>
2012-03-07 10:54:04 -0800

    io: more docs on WriterAt
    Updates #1599
    
    R=golang-dev, gri, rsc
    CC=golang-dev
    https://golang.org/cl/5774043

--- src/pkg/io/io.go
+++ src/pkg/io/io.go
@@ -173,6 +173,13 @@ type ReaderAt interface {
 // at offset off.  It returns the number of bytes written from p (0 <= n <= len(p))
 // and any error encountered that caused the write to stop early.
 // WriteAt must return a non-nil error if it returns n < len(p).
+//
+// If WriteAt is writing to a destination with a seek offset,
+// WriteAt should not affect nor be affected by the underlying
+// seek offset.
+//
+// Clients of WriteAt can execute parallel WriteAt calls on the same
+// destination if the ranges are not overlapping.
 type WriterAt interface {
 	WriteAt(p []byte, off int64) (n int, err error)
 }
