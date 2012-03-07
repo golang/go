eb630d214339d48107da49d91b80472986cfb627
Johan Euphrosine <proppy@google.com>
2012-03-07 11:24:00 +1100

    doc: fix typos in laws_of_reflection article, add copyright notice.
    Update #2547.
    
    R=golang-dev, minux.ma, r, r, adg
    CC=golang-dev
    https://golang.org/cl/5755051

--- doc/progs/interface.go
+++ doc/progs/interface.go
@@ -1,3 +1,9 @@
+// Copyright 2012 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// This file contains the code snippets included in "The Laws of Reflection."
+
 package main
 
 import (
