94a56359fab01eb5c989ed2d124d5d46dccfae69
Francisco Souza <franciscossouza@gmail.com>
2012-03-13 09:07:37 +1100

    doc: add C? Go? Cgo! article
    Originally published on The Go Programming Language Blog, March 17, 2011.
    
    http://blog.golang.org/2011/03/c-go-cgo.html
    
    Update #2547.
    
    R=golang-dev, adg
    CC=golang-dev
    https://golang.org/cl/5777054

---
+++ doc/progs/cgo1.go
@@ -0,0 +1,23 @@
+// Copyright 2012 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+package rand
+
+// INCLUDE OMIT
+
+/*
+#include <stdlib.h>
+*/
+import "C"
+
+// STOP OMIT
+func Random() int {
+	return int(C.random())
+}
+
+// STOP OMIT
+func Seed(i int) {
+	C.srandom(C.uint(i))
+}
+
+// END OMIT
