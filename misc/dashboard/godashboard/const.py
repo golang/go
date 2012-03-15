8b1e861b8b15c6a95ca03cb81138b6d05b88d938
Andrew Gerrand <adg@golang.org>
2012-03-16 08:20:02 +1100

    misc/dashboard: remove old python package dashboard
    This leaves only the project page, which now resides at the web root.
    
    R=golang-dev, bsiegert, rsc
    CC=golang-dev
    https://golang.org/cl/5833044

--- misc/dashboard/godashboard/const.py
+++ misc/dashboard/godashboard/const.py
@@ -3,11 +3,5 @@
 # license that can be found in the LICENSE file.
 
 mail_from           = "Go Dashboard <builder@golang.org>"
-
 mail_submit_to      = "adg@golang.org"
 mail_submit_subject = "New Project Submitted"
-
-mail_fail_to       = "golang-dev@googlegroups.com"
-mail_fail_reply_to = "golang-dev@googlegroups.com"
-mail_fail_subject  = "%s broken by %s"
-
