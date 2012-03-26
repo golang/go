fd9c206718484ace00515f1ed3757c5a0dce8887
Andrew Gerrand <adg@golang.org>
2012-03-27 10:10:25 +1100

    cmd/godoc: fix app engine version
    R=golang-dev, rsc
    CC=golang-dev
    https://golang.org/cl/5902060

--- src/cmd/godoc/appinit.go
+++ src/cmd/godoc/appinit.go
@@ -19,7 +19,7 @@ import (
 func serveError(w http.ResponseWriter, r *http.Request, relpath string, err error) {
 	contents := applyTemplate(errorHTML, "errorHTML", err) // err may contain an absolute path!
 	w.WriteHeader(http.StatusNotFound)
-	servePage(w, "File "+relpath, "", "", contents)
+	servePage(w, relpath, "File "+relpath, "", "", contents)
 }
 
 func init() {
