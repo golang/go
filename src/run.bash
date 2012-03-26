e1e23d6fd6789134df51db95a48bd4f57c2e642e
Rob Pike <r@golang.org>
2012-03-26 16:08:21 +1100

    run.bash: compile the codewalks They could be tested but that requires more than seems wise right now.
    Update #2648.
    
    R=golang-dev, dsymonds
    CC=golang-dev
    https://golang.org/cl/5908054

--- src/run.bash
+++ src/run.bash
@@ -74,6 +74,13 @@ make clean
 ./test.bash
 ) || exit $?
 
+(xcd ../doc/codewalk
+# TODO: test these too.
+go build pig.go
+go build urlpoll.go
+rm -f pig urlpoll
+) || exit $?
+
 echo
 echo '#' ../misc/dashboard/builder ../misc/goplay
 go build ../misc/dashboard/builder ../misc/goplay
