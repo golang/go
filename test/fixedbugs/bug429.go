5eb007dedeb59a52a3000202dcb162a3677a384b
Russ Cox <rsc@golang.org>
2012-03-27 12:22:19 -0400

    runtime: work around false negative in deadlock detection
    Not a complete fix for issue 3342, but fixes the trivial case.
    There may still be a race in the instants before and after
    a scavenger-induced garbage collection.
    
    Intended to be "obviously safe": a call to runtime·gosched
    before main.main is no different than a call to runtime.Gosched
    at the beginning of main.main, and it is (or had better be)
    safe to call runtime.Gosched at any point during main.
    
    Update #3342.
    
    R=iant
    CC=golang-dev
    https://golang.org/cl/5919052

---
+++ test/fixedbugs/bug429.go
@@ -0,0 +1,13 @@
+// $G $D/$F.go && $L $F.$A && ! ./$A.out || echo BUG: bug429
+
+// Copyright 2012 The Go Authors.  All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Should print deadlock message, not hang.
+
+package main
+
+func main() {
+	select {}
+}
