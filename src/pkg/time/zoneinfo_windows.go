7510885c4dc552e343b8a49a8e51210534ef3ba2
Alex Brainman <alex.brainman@gmail.com>
2012-04-03 11:39:38 +1000

    [release-branch.go1] time: fix handling of locations with no dst on windows
    ««« backport 701af83040ac
    time: fix handling of locations with no dst on windows
    
    Fixes #3437.
    
    R=rsc
    CC=golang-dev
    https://golang.org/cl/5967063
    »»»

--- src/pkg/time/zoneinfo_windows.go
+++ src/pkg/time/zoneinfo_windows.go
@@ -83,6 +83,9 @@ func initLocalFromTZI(i *syscall.Timezoneinformation) {
 		l.cacheStart = -1 << 63
 		l.cacheEnd = 1<<63 - 1
 		l.cacheZone = std
+		l.tx = make([]zoneTrans, 1)
+		l.tx[0].when = l.cacheStart
+		l.tx[0].index = 0
 		return
 	}
 
