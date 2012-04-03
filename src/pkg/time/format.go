02c38a83b9a68d4c3e90c18415c99e5f5b36dc94
Rob Pike <r@golang.org>
2012-04-04 06:54:39 +1000

    [release-branch.go1] time.RFC822: make the string standard-compliant
    ««« backport fda7b4c9978d
    time.RFC822: make the string standard-compliant
    For mysterious reasons, the existing string was just wrong: it was missing a colon.
    There is no apparent reason for this discrepancy.
    
    This should be safe to fix because existing uses would not be RFC822-compliant;
    people cannot be depending on it to generate correct mail headers.
    
    Fixes #3444.
    
    R=golang-dev, dsymonds, iant, rsc
    CC=golang-dev
    https://golang.org/cl/5969072
    »»»

--- src/pkg/time/format.go
+++ src/pkg/time/format.go
@@ -41,8 +41,8 @@ const (
 	ANSIC       = "Mon Jan _2 15:04:05 2006"
 	UnixDate    = "Mon Jan _2 15:04:05 MST 2006"
 	RubyDate    = "Mon Jan 02 15:04:05 -0700 2006"
-	RFC822      = "02 Jan 06 1504 MST"
-	RFC822Z     = "02 Jan 06 1504 -0700" // RFC822 with numeric zone
+	RFC822      = "02 Jan 06 15:04 MST"
+	RFC822Z     = "02 Jan 06 15:04 -0700" // RFC822 with numeric zone
 	RFC850      = "Monday, 02-Jan-06 15:04:05 MST"
 	RFC1123     = "Mon, 02 Jan 2006 15:04:05 MST"
 	RFC1123Z    = "Mon, 02 Jan 2006 15:04:05 -0700" // RFC1123 with numeric zone
