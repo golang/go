ad0c81b124a021cc401da0ae1bfe274ac9c792c9
Adam Langley <agl@golang.org>
2012-04-12 12:35:21 -0400

    [release-branch.go1] crypto/tls: don't always use the default private key.
    ««« backport 6a2ea47583df
    crypto/tls: don't always use the default private key.
    
    When SNI based certificate selection is enabled, we previously used
    the default private key even if we selected a non-default certificate.
    
    Fixes #3367.
    
    R=golang-dev, bradfitz
    CC=golang-dev
    https://golang.org/cl/5987058
    »»»

--- src/pkg/crypto/tls/cipher_suites.go
+++ src/pkg/crypto/tls/cipher_suites.go
@@ -23,8 +23,8 @@ type keyAgreement interface {
 	// In the case that the key agreement protocol doesn't use a
 	// ServerKeyExchange message, generateServerKeyExchange can return nil,
 	// nil.
-	generateServerKeyExchange(*Config, *clientHelloMsg, *serverHelloMsg) (*serverKeyExchangeMsg, error)
-	processClientKeyExchange(*Config, *clientKeyExchangeMsg, uint16) ([]byte, error)
+	generateServerKeyExchange(*Config, *Certificate, *clientHelloMsg, *serverHelloMsg) (*serverKeyExchangeMsg, error)
+	processClientKeyExchange(*Config, *Certificate, *clientKeyExchangeMsg, uint16) ([]byte, error)
 
 	// On the client side, the next two methods are called in order.
 
