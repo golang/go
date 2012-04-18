0c64bb396064285fae266ee9cfe3ac5aa43ce0f4
Adam Langley <agl@golang.org>
2012-04-18 13:41:11 -0400

    encoding/asn1: allow lengths up to the maximum int value.
    Previously we capped lengths at 2**24 rather than 2**31.
    
    R=golang-dev, r, r
    CC=golang-dev
    https://golang.org/cl/6050050

--- src/pkg/encoding/asn1/asn1_test.go
+++ src/pkg/encoding/asn1/asn1_test.go
@@ -283,6 +283,12 @@ var tagAndLengthData = []tagAndLengthTest{
 	{[]byte{0x00, 0x83, 0x01, 0x00}, false, tagAndLength{}},
 	{[]byte{0x1f, 0x85}, false, tagAndLength{}},
 	{[]byte{0x30, 0x80}, false, tagAndLength{}},
+	// Superfluous zeros in the length should be an error.
+	{[]byte{0xa0, 0x82, 0x00, 0x01}, false, tagAndLength{}},
+	// Lengths up to the maximum size of an int should work.
+	{[]byte{0xa0, 0x84, 0x7f, 0xff, 0xff, 0xff}, true, tagAndLength{2, 0, 0x7fffffff, true}},
+	// Lengths that would overflow an int should be rejected.
+	{[]byte{0xa0, 0x84, 0x80, 0x00, 0x00, 0x00}, false, tagAndLength{}},
 }
 
 func TestParseTagAndLength(t *testing.T) {
