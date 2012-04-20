901aacdee2b4c0eae78237ce1b2191f02cfec0b7
Shenghou Ma <minux.ma@gmail.com>
2012-04-20 23:45:43 +0800

    test: use testlib in a few more cases (part 2)         Introduced "runoutput" cmd for running generated program
    R=golang-dev, iant, bradfitz, remyoudompheng
    CC=golang-dev
    https://golang.org/cl/5869049

--- test/crlf.go
+++ test/crlf.go
@@ -1,6 +1,4 @@
-// $G $D/$F.go && $L $F.$A && ./$A.out >tmp.go &&
-// $G tmp.go && $L tmp.$A && ./$A.out
-// rm -f tmp.go
+// runoutput
 
 // Copyright 2011 The Go Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
