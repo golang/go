eafdaec5a913daf2193cee3358aca4ce8aa19d8c
Gustavo Niemeyer <gustavo@niemeyer.net>
2012-04-25 02:32:51 -0300

    net/http: revert 97d027b3aa68
    Revert the following change set:
    
            changeset:   13018:97d027b3aa68
            user:        Gustavo Niemeyer <gustavo@niemeyer.net>
            date:        Mon Apr 23 22:00:16 2012 -0300
            summary:     net/http: allow clients to disable keep-alive
    
    This broke a test on Windows 64 and somebody else
    will have to check.
    
    R=golang-dev, r
    CC=golang-dev
    https://golang.org/cl/6112054

--- src/pkg/net/http/server.go
+++ src/pkg/net/http/server.go
@@ -303,7 +303,8 @@ func (w *response) WriteHeader(code int) {
 		if !connectionHeaderSet {
 			w.header.Set("Connection", "keep-alive")
 		}
-	} else if !w.req.ProtoAtLeast(1, 1) || w.req.wantsClose() {
+	} else if !w.req.ProtoAtLeast(1, 1) {
+		// Client did not ask to keep connection alive.
 		w.closeAfterReply = true
 	}
 
