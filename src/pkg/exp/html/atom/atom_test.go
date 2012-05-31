86c6f142a704f6e25c8dd173ae20ed89ca5af241
Nigel Tao <nigeltao@golang.org>
2012-06-01 09:36:05 +1000

    exp/html/atom: faster, hash-based lookup.
    exp/html/atom benchmark:
    benchmark          old ns/op    new ns/op    delta
    BenchmarkLookup       199226        80770  -59.46%
    
    exp/html benchmark:
    benchmark                      old ns/op    new ns/op    delta
    BenchmarkParser                  4864890      4510834   -7.28%
    BenchmarkHighLevelTokenizer      2209192      1969684  -10.84%
    benchmark                       old MB/s     new MB/s  speedup
    BenchmarkParser                    16.07        17.33    1.08x
    BenchmarkHighLevelTokenizer        35.38        39.68    1.12x
    
    R=r
    CC=golang-dev
    https://golang.org/cl/6261054

--- src/pkg/exp/html/atom/atom_test.go
+++ src/pkg/exp/html/atom/atom_test.go
@@ -5,6 +5,7 @@
 package atom
 
 import (
+	"sort"
 	"testing"
 )
 
@@ -42,11 +43,31 @@ func TestMisses(t *testing.T) {
 		"h7",
 		"onClick",
 		"λ",
+		// The following string has the same hash (0xa1d7fab7) as "onmouseover".
+		"\x00\x00\x00\x00\x00\x50\x18\xae\x38\xd0\xb7",
 	}
 	for _, tc := range testCases {
 		got := Lookup([]byte(tc))
 		if got != 0 {
 			t.Errorf("Lookup(%q): got %d, want 0", tc, got)
+		}
+	}
+}
+
+func BenchmarkLookup(b *testing.B) {
+	sortedTable := make([]string, len(table))
+	copy(sortedTable, table[:])
+	sort.Strings(sortedTable)
+
+	x := make([][]byte, 1000)
+	for i := range x {
+		x[i] = []byte(sortedTable[i%len(sortedTable)])
+	}
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		for _, s := range x {
+			Lookup(s)
 		}
 	}
 }
