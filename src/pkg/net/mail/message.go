c8e2ccdc9c4c8851ea5f7142255280df5b7a5d4a
Bill Thiede <couchmoney@gmail.com>
2012-06-13 16:24:19 -0400

    [release-branch.go1] net/mail: more liberal parsing of Date headers.
    ««« backport e995a1bfe589
    net/mail: more liberal parsing of Date headers.
    Fixes #3639.
    
    R=golang-dev, dsymonds
    CC=golang-dev
    https://golang.org/cl/6243045
    
    »»»

--- src/pkg/net/mail/message.go
+++ src/pkg/net/mail/message.go
@@ -69,11 +69,12 @@ var dateLayouts []string
 func init() {
 	// Generate layouts based on RFC 5322, section 3.3.
 
-	dows := [...]string{"", "Mon, "}     // day-of-week
-	days := [...]string{"2", "02"}       // day = 1*2DIGIT
-	years := [...]string{"2006", "06"}   // year = 4*DIGIT / 2*DIGIT
-	seconds := [...]string{":05", ""}    // second
-	zones := [...]string{"-0700", "MST"} // zone = (("+" / "-") 4DIGIT) / "GMT" / ...
+	dows := [...]string{"", "Mon, "}   // day-of-week
+	days := [...]string{"2", "02"}     // day = 1*2DIGIT
+	years := [...]string{"2006", "06"} // year = 4*DIGIT / 2*DIGIT
+	seconds := [...]string{":05", ""}  // second
+	// "-0700 (MST)" is not in RFC 5322, but is common.
+	zones := [...]string{"-0700", "MST", "-0700 (MST)"} // zone = (("+" / "-") 4DIGIT) / "GMT" / ...
 
 	for _, dow := range dows {
 		for _, day := range days {
