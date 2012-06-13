dcd49f5eaf2818b305fdbce41fc9640f884b3b10
Benny Siegert <bsiegert@gmail.com>
2012-06-13 16:23:56 -0400

    [release-branch.go1] log: fix typo in comment
    ««« backport 847b8d48b873
    log: fix typo in comment
    
    R=golang-dev, dsymonds
    CC=golang-dev
    https://golang.org/cl/6195096
    
    »»»

--- src/pkg/log/log.go
+++ src/pkg/log/log.go
@@ -26,7 +26,7 @@ const (
 	// Bits or'ed together to control what's printed. There is no control over the
 	// order they appear (the order listed here) or the format they present (as
 	// described in the comments).  A colon appears after these items:
-	//	2009/0123 01:23:23.123123 /a/b/c/d.go:23: message
+	//	2009/01/23 01:23:23.123123 /a/b/c/d.go:23: message
 	Ldate         = 1 << iota     // the date: 2009/01/23
 	Ltime                         // the time: 01:23:23
 	Lmicroseconds                 // microsecond resolution: 01:23:23.123123.  assumes Ltime.
