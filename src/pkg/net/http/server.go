e5059640b2a636b1912b4763299e4a127ea7c2d4
Andrey Mirtchovski <mirtchovski@gmail.com>
2012-06-28 15:18:35 -0700

    net/http: Fix a verb-tense mismatch and a typo in documentation.
    R=bradfitz
    CC=golang-dev
    https://golang.org/cl/6357049

--- src/pkg/net/http/server.go
+++ src/pkg/net/http/server.go
@@ -848,13 +848,13 @@ func RedirectHandler(url string, code int) Handler {
 // patterns and calls the handler for the pattern that
 // most closely matches the URL.
 //
-// Patterns named fixed, rooted paths, like "/favicon.ico",
+// Patterns name fixed, rooted paths, like "/favicon.ico",
 // or rooted subtrees, like "/images/" (note the trailing slash).
 // Longer patterns take precedence over shorter ones, so that
 // if there are handlers registered for both "/images/"
 // and "/images/thumbnails/", the latter handler will be
 // called for paths beginning "/images/thumbnails/" and the
-// former will receiver requests for any other paths in the
+// former will receive requests for any other paths in the
 // "/images/" subtree.
 //
 // Patterns may optionally begin with a host name, restricting matches to
