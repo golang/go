dd47bc16b71337c62847a25b50dd8819f5e77ab7
Ian Lance Taylor <iant@golang.org>
2012-07-28 10:40:51 -0700

    misc/cgo/test: only run setgid test on GNU/Linux
    Fixes #3874.
    
    R=golang-dev, nj, r, minux.ma
    CC=golang-dev
    https://golang.org/cl/6446060

--- misc/cgo/test/cgo_test.go
+++ misc/cgo/test/cgo_test.go
@@ -27,6 +27,5 @@ func Test1328(t *testing.T)                { test1328(t) }
 func TestParallelSleep(t *testing.T)       { testParallelSleep(t) }
 func TestSetEnv(t *testing.T)              { testSetEnv(t) }
 func TestHelpers(t *testing.T)             { testHelpers(t) }
-func TestSetgid(t *testing.T)              { testSetgid(t) }
 
 func BenchmarkCgoCall(b *testing.B) { benchCgoCall(b) }
