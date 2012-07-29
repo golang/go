e9763e809434af0ab90ae30687bb297b2f921145
Russ Cox <rsc@golang.org>
2012-07-29 19:50:23 -0400

    syscall: apply comment from Mikioh
    This comment was suggested in CL 6456045
    but never got applied.
    
    R=mikioh, mikioh.mikioh
    CC=golang-dev
    https://golang.org/cl/6447056

--- src/pkg/syscall/syscall_bsd.go
+++ src/pkg/syscall/syscall_bsd.go
@@ -13,6 +13,7 @@
 package syscall
 
 import (
+	"runtime"
 	"unsafe"
 )
 
@@ -303,7 +304,7 @@ func Accept(fd int) (nfd int, sa Sockaddr, err error) {
 	if err != nil {
 		return
 	}
-	if len == 0 {
+	if runtime.GOOS == "darwin" && len == 0 {
 		// Accepted socket has no address.
 		// This is likely due to a bug in xnu kernels,
 		// where instead of ECONNABORTED error socket
