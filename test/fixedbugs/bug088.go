adc933726206e7f2be6a2ded826b6a3596df5ffd
Rémy Oudompheng <oudomphe@phare.normalesup.org>
2012-07-30 21:12:05 +0200

    test: add a compiledir pattern in run.go
    The compiledir pattern compiles all files xxx.dir/*.go
    in lexicographic order (which is assumed to coincide with
    the topological order of dependencies).
    
    R=rsc
    CC=golang-dev, remy
    https://golang.org/cl/6440048

--- test/fixedbugs/bug088.go
+++ test/fixedbugs/bug088.go
@@ -1,4 +1,4 @@
-// $G $D/$F.dir/bug0.go && $G $D/$F.dir/bug1.go || echo BUG: fails incorrectly
+// compiledir
 
 // Copyright 2009 The Go Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
