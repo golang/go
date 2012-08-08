4939b7b065d2eee1c37201c0d42ed4dd06d22265
Andrew Gerrand <adg@golang.org>
2012-08-08 15:41:47 +1000

    io: amend ReaderFrom doc as per r's comment
    R=r
    CC=golang-dev
    https://golang.org/cl/6458097

--- src/pkg/io/io.go
+++ src/pkg/io/io.go
@@ -131,9 +131,9 @@ type ReadWriteSeeker interface {
 
 // ReaderFrom is the interface that wraps the ReadFrom method.
 //
-// ReadFrom reads data from r until EOF. The return value n is the
-// number of bytes read. Any error except io.EOF encountered during
-// the read is also returned.
+// ReadFrom reads data from r until EOF or error.
+// The return value n is the number of bytes read.
+// Any error except io.EOF encountered during the read is also returned.
 //
 // The Copy function uses ReaderFrom if available.
 type ReaderFrom interface {
