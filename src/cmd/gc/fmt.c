bedafd2ddb4f758111f9d24729a8815caa6cbcb0
Rémy Oudompheng <oudomphe@phare.normalesup.org>
2012-09-22 05:54:22 +1000

    [release-branch.go1] cmd/gc: fix inlining bug with receive operator.
    ««« backport d872ed20fccb
    cmd/gc: fix inlining bug with receive operator.
    
    The receive operator was given incorrect precedence
    resulting in incorrect deletion of parentheses.
    
    Fixes #3843.
    
    R=rsc
    CC=golang-dev, remy
    https://golang.org/cl/6442049
    
    »»»

--- src/cmd/gc/fmt.c
+++ src/cmd/gc/fmt.c
@@ -962,7 +962,6 @@ static int opprec[] = {
 	[OPAREN] = 8,
 	[OPRINTN] = 8,
 	[OPRINT] = 8,
-	[ORECV] = 8,
 	[ORUNESTR] = 8,
 	[OSTRARRAYBYTE] = 8,
 	[OSTRARRAYRUNE] = 8,
@@ -994,6 +993,7 @@ static int opprec[] = {
 	[OMINUS] = 7,
 	[OADDR] = 7,
 	[OIND] = 7,
+	[ORECV] = 7,
 
 	[OMUL] = 6,
 	[ODIV] = 6,
