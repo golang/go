74282e400c7f52a6e78e09dea7e9f2d1def6fb8b
Rob Pike <r@golang.org>
2012-09-22 05:54:11 +1000

    [release-branch.go1] encoding/gob: revert 6348067, which broke compatibility
    ««« backport 3357d8d34ab8
    encoding/gob: revert 6348067, which broke compatibility
    Add commentary to explain better what's going on, but the
    code change is a simple one-line reversal to the previous
    form.
    
    R=rsc
    CC=golang-dev
    https://golang.org/cl/6428072
    
    »»»

--- src/pkg/encoding/gob/type.go
+++ src/pkg/encoding/gob/type.go
@@ -749,13 +749,29 @@ func Register(value interface{}) {
 	rt := reflect.TypeOf(value)
 	name := rt.String()
 
-	// But for named types (or pointers to them), qualify with import path.
+	// But for named types (or pointers to them), qualify with import path (but see inner comment).
 	// Dereference one pointer looking for a named type.
 	star := ""
 	if rt.Name() == "" {
 		if pt := rt; pt.Kind() == reflect.Ptr {
 			star = "*"
-			rt = pt.Elem()
+			// NOTE: The following line should be rt = pt.Elem() to implement
+			// what the comment above claims, but fixing it would break compatibility
+			// with existing gobs.
+			//
+			// Given package p imported as "full/p" with these definitions:
+			//     package p
+			//     type T1 struct { ... }
+			// this table shows the intended and actual strings used by gob to
+			// name the types:
+			//
+			// Type      Correct string     Actual string
+			//
+			// T1        full/p.T1          full/p.T1
+			// *T1       *full/p.T1         *p.T1
+			//
+			// The missing full path cannot be fixed without breaking existing gob decoders.
+			rt = pt
 		}
 	}
 	if rt.Name() != "" {
