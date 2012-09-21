706832a0882c7300889238d5f4d476dc2ee83ad0
Brad Fitzpatrick <bradfitz@golang.org>
2012-09-22 05:54:20 +1000

    [release-branch.go1] net/http: don't allow zero byte in FileServer paths
    ««« backport 2307a931664e
    net/http: don't allow zero byte in FileServer paths
    
    Should probably be fixed in the syscall package, either
    additional or instead of this CL.
    
    Fixes #3842
    
    R=golang-dev, rsc
    CC=golang-dev
    https://golang.org/cl/6442061
    
    »»»

--- src/pkg/net/http/fs.go
+++ src/pkg/net/http/fs.go
@@ -28,7 +28,8 @@ import (
 type Dir string
 
 func (d Dir) Open(name string) (File, error) {
-	if filepath.Separator != '/' && strings.IndexRune(name, filepath.Separator) >= 0 {
+	if filepath.Separator != '/' && strings.IndexRune(name, filepath.Separator) >= 0 ||
+		strings.Contains(name, "\x00") {
 		return nil, errors.New("http: invalid character in file path")
 	}
 	dir := string(d)
