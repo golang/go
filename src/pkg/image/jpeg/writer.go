da6a2756c8f51372186f2445cc63a1b8cb0450b4
Nigel Tao <nigeltao@golang.org>
2012-09-22 05:54:32 +1000

    [release-branch.go1] image/jpeg: send a correct Start Of Scan (SOS) header.
    ««« backport 4a67b1f1e771
    image/jpeg: send a correct Start Of Scan (SOS) header.
    
    Section B.2.3 of http://www.w3.org/Graphics/JPEG/itu-t81.pdf discusses
    the End of spectral selection (Se) byte.
    
    Apparently many JPEG decoders ignore the Se byte (or let it through
    with a warning), but some configurations reject them. For example,
    http://download.blender.org/source/chest/blender_2.03_tree/jpeg/jcmaster.c
    has these lines:
    
    if (Ss != 0 || Se != DCTSIZE2-1 || Ah != 0 || Al != 0)
      ERREXIT1(cinfo, JERR_BAD_PROG_SCRIPT, scanno);
    
    Fixes #3916.
    
    R=r
    CC=golang-dev
    https://golang.org/cl/6459052
    
    »»»

--- src/pkg/image/jpeg/writer.go
+++ src/pkg/image/jpeg/writer.go
@@ -433,10 +433,12 @@ func scale(dst *block, src *[4]block) {
 //	- component 1 uses DC table 0 and AC table 0 "\x01\x00",
 //	- component 2 uses DC table 1 and AC table 1 "\x02\x11",
 //	- component 3 uses DC table 1 and AC table 1 "\x03\x11",
-//	- padding "\x00\x00\x00".
+//	- the bytes "\x00\x3f\x00". Section B.2.3 of the spec says that for
+//	  sequential DCTs, those bytes (8-bit Ss, 8-bit Se, 4-bit Ah, 4-bit Al)
+//	  should be 0x00, 0x3f, 0x00<<4 | 0x00.
 var sosHeader = []byte{
 	0xff, 0xda, 0x00, 0x0c, 0x03, 0x01, 0x00, 0x02,
-	0x11, 0x03, 0x11, 0x00, 0x00, 0x00,
+	0x11, 0x03, 0x11, 0x00, 0x3f, 0x00,
 }
 
 // writeSOS writes the StartOfScan marker.
