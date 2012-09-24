5d50dbc55ae5fa7240d94068ffc466b2327ebae3
Alex Brainman <alex.brainman@gmail.com>
2012-09-24 12:48:19 +1000

    net/http: use r.Body.Close to close connection during TestServeFileFromCWD
    Fixes #3917.
    
    R=golang-dev, bradfitz
    CC=golang-dev
    https://golang.org/cl/6553061

--- src/pkg/net/http/fs_test.go
+++ src/pkg/net/http/fs_test.go
@@ -340,11 +340,6 @@ func TestServeFileMimeType(t *testing.T) {
 }
 
 func TestServeFileFromCWD(t *testing.T) {
-	if runtime.GOOS == "windows" {
-		// TODO(brainman): find out why this test is broken
-		t.Logf("Temporarily skipping test on Windows; see http://golang.org/issue/3917")
-		return
-	}
 	ts := httptest.NewServer(HandlerFunc(func(w ResponseWriter, r *Request) {
 		ServeFile(w, r, "fs_test.go")
 	}))
@@ -353,6 +348,7 @@ func TestServeFileFromCWD(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
+	r.Body.Close()
 	if r.StatusCode != 200 {
 		t.Fatalf("expected 200 OK, got %s", r.Status)
 	}
