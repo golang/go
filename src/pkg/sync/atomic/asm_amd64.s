59b87453285d952c5b1bc705ca5ffc87e7f15f61
Dmitriy Vyukov <dvyukov@google.com>
2012-10-07 22:07:32 +0400

    race: sync/atomic changes This is a part of a bigger change that adds data race detection feature: https://golang.org/cl/6456044
    R=rsc, remyoudompheng
    CC=gobot, golang-dev
    https://golang.org/cl/6536059

--- src/pkg/sync/atomic/asm_amd64.s
+++ src/pkg/sync/atomic/asm_amd64.s
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
+// +build !race
+
 TEXT ·CompareAndSwapInt32(SB),7,$0
 	JMP	·CompareAndSwapUint32(SB)
 
--- src/pkg/sync/atomic/asm_linux_arm.s
+++ src/pkg/sync/atomic/asm_linux_arm.s
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
+// +build !race
+
 // Linux/ARM atomic operations.
 
 // Because there is so much variation in ARM devices,
