24ab448c69f383cd23a2b88a5a240dd28269e6e2
Shenghou Ma <minux.ma@gmail.com>
2012-10-10 01:30:34 +0800

    misc/cgo/test: add -ldl to LDFLAGS on Linux, ignore issue4029 on windows (fix build)
    R=golang-dev
    CC=golang-dev
    https://golang.org/cl/6631054

--- misc/cgo/test/issue4029.go
+++ misc/cgo/test/issue4029.go
@@ -2,10 +2,13 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
+// +build !windows
+
 package cgotest
 
 /*
 #include <dlfcn.h> 
+#cgo linux LDFLAGS: -ldl
 */
 import "C"
 
