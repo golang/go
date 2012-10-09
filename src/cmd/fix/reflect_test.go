b6851897c3446117760d37ed9e9cf2467fabf0ee
Dmitriy Vyukov <dvyukov@google.com>
2012-10-09 20:21:39 +0400

    cmd/fix: disable reflect test under race detector (very slow)
    R=golang-dev, rsc
    CC=golang-dev
    https://golang.org/cl/6642045

--- src/cmd/fix/reflect_test.go
+++ src/cmd/fix/reflect_test.go
@@ -2,6 +2,9 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file
 
+// Too slow under race detector.
+// +build !race
+
 package main
 
 import (
