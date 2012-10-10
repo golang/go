afdd861de115cee4537b041bb9cf23bc93baca50
Rémy Oudompheng <oudomphe@phare.normalesup.org>
2012-10-10 22:35:27 +0200

    test: convert tests to run.go whenever possible.
    The other tests either need a complex procedure
    or are architecture- or OS-dependent.
    
    Update #4139.
    
    R=golang-dev, daniel.morsing, iant
    CC=golang-dev
    https://golang.org/cl/6618062

--- test/interface/embed1.go
+++ test/interface/embed1.go
@@ -1,7 +1,4 @@
-// $G $D/embed0.go && $G $D/$F.go && $L $F.$A && ./$A.out
-
-// NOTE: This test is not run by 'run.go' and so not run by all.bash.
-// To run this test you must use the ./run shell script.
+// rundir
 
 // Copyright 2009 The Go Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
@@ -9,40 +6,4 @@
 
 // Test that embedded interface types can have local methods.
 
-package main
-
-import "./embed0"
-
-type T int
-func (t T) m() {}
-
-type I interface { m() }
-type J interface { I }
-
-type PI interface { p.I }
-type PJ interface { p.J }
-
-func main() {
-	var i I
-	var j J
-	var t T
-	i = t
-	j = t
-	_ = i
-	_ = j
-	i = j
-	_ = i
-	j = i
-	_ = j
-	var pi PI
-	var pj PJ
-	var pt p.T
-	pi = pt
-	pj = pt
-	_ = pi
-	_ = pj
-	pi = pj
-	_ = pi
-	pj = pi
-	_ = pj
-}
+package ignored
