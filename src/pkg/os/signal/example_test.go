bd6601f4a05df0aedff66b4846d507615f584c5b
Andrew Gerrand <adg@golang.org>
2012-10-12 10:22:13 +1100

    os/signal: add Notify example
    R=golang-dev, dsymonds, r
    CC=golang-dev
    https://golang.org/cl/6615078

---
+++ src/pkg/os/signal/example_test.go
@@ -0,0 +1,19 @@
+package signal_test
+
+import (
+	"fmt"
+	"os"
+	"os/signal"
+)
+
+func ExampleNotify() {
+	// Set up channel on which to send signal notifications.
+	// We must use a buffered channel or risk missing the signal
+	// if we're not ready to receive when the signal is sent.
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt, os.Kill)
+
+	// Block until a signal is received.
+	s := <-c
+	fmt.Println("Got signal:", s)
+}
