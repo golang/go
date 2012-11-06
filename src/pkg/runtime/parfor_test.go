e996c06deb22cc283ad3d433bdc56dc1027d29a4
Dmitriy Vyukov <dvyukov@google.com>
2012-11-06 12:09:40 +0400

    runtime: disable parallel for tests under race detector. The race detector does not understand ParFor synchronization, because it's implemented in C. If run with -cpu=2 currently race detector says:  WARNING: DATA RACE  Read by goroutine 5:   runtime_test.TestParForParallel()       src/pkg/runtime/parfor_test.go:118 +0x2e0   testing.tRunner()       src/pkg/testing/testing.go:301 +0x8f  Previous write by goroutine 6:   runtime_test.func·024()       src/pkg/runtime/parfor_test.go:111 +0x52
    R=golang-dev, bradfitz
    CC=golang-dev
    https://golang.org/cl/6811082

--- src/pkg/runtime/parfor_test.go
+++ src/pkg/runtime/parfor_test.go
@@ -2,6 +2,9 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
+// The race detector does not understand ParFor synchronization.
+// +build !race
+
 package runtime_test
 
 import (
