9edabd11afcfbe99ca98ad007e3c7e544cb710ee
Shenghou Ma <minux.ma@gmail.com>
2012-11-07 05:07:46 +0800

    sort: fix comment for various Search routines Fixes #4205 (again).
    R=r, 0xjnml
    CC=golang-dev
    https://golang.org/cl/6819082

--- src/pkg/sort/search.go
+++ src/pkg/sort/search.go
@@ -12,8 +12,8 @@ package sort
 // f is false for some (possibly empty) prefix of the input range [0, n)
 // and then true for the (possibly empty) remainder; Search returns
 // the first true index.  If there is no such index, Search returns n.
-// (Note that the "not found" return value is n, the length of the input,
-// not -1 as in, for instance, strings.Index).
+// (Note that the "not found" return value is not -1 as in, for instance,
+// strings.Index).
 // Search calls f(i) only for i in the range [0, n).
 //
 // A common use of Search is to find the index i for a value x in
@@ -76,7 +76,8 @@ func Search(n int, f func(int) bool) int {
 // Convenience wrappers for common cases.
 
 // SearchInts searches for x in a sorted slice of ints and returns the index
-// as specified by Search. The return value is len(a) if x is not present.
+// as specified by Search. The return value is the index to insert x if x is
+// not present (it could be len(a)).
 // The slice must be sorted in ascending order.
 //
 func SearchInts(a []int, x int) int {
@@ -84,7 +85,8 @@ func SearchInts(a []int, x int) int {
 }
 
 // SearchFloat64s searches for x in a sorted slice of float64s and returns the index
-// as specified by Search.  The return value is len(a) if x is not present.
+// as specified by Search.  The return value is the index to insert x if x is not
+// present (it could be len(a)).
 // The slice must be sorted in ascending order.
 //
 func SearchFloat64s(a []float64, x float64) int {
@@ -92,7 +94,8 @@ func SearchFloat64s(a []float64, x float64) int {
 }
 
 // SearchStrings searches for x in a sorted slice of strings and returns the index
-// as specified by Search.  The return value is len(a) if x is not present.
+// as specified by Search.  The return value is the index to insert x if x is not
+// present (it could be len(a)).
 // The slice must be sorted in ascending order.
 //
 func SearchStrings(a []string, x string) int {
