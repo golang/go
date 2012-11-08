761830f48196aa1170b5224c1da34f4e20053ebd
Russ Cox <rsc@golang.org>
2012-11-08 16:07:05 -0500

    cmd/gc: fix export of inlined function body with type guard
    When exporting a body containing
            x, ok := v.(Type)
    
    the definition for Type was not being included, so when the body
    was actually used, it would cause an "unknown type" compiler error.
    
    Fixes #4370.
    
    R=ken2
    CC=golang-dev
    https://golang.org/cl/6827064

---
+++ test/fixedbugs/issue4370.dir/p1.go
@@ -0,0 +1,20 @@
+// Copyright 2012 The Go Authors.  All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package p1
+
+type Magic int
+
+type T struct {
+	x interface{}
+}
+
+func (t *T) M() bool {
+	_, ok := t.x.(Magic)
+	return ok
+}
+
+func F(t *T) {
+	println(t)
+}
