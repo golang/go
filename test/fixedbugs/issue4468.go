8990dc8ca09e9255ba447b9d2d361f8e9954d3e9
Ian Lance Taylor <iant@golang.org>
2012-12-03 18:49:14 -0800

    test: add test for issue 4468 (go/defer expr may not be parenthesized)
    R=gri
    CC=golang-dev
    https://golang.org/cl/6870053

---
+++ test/fixedbugs/issue4468.go
@@ -0,0 +1,14 @@
+// errorcheck
+
+// Copyright 2012 The Go Authors.  All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Issue 4468: go/defer calls may not be parenthesized.
+
+package p
+
+func F() {
+	go (F())	// ERROR "must be function call"
+	defer (F())	// ERROR "must be function call"
+}
