122b397c1813bd166ddd5e1aa24b404fb8f5dea2
Brad Fitzpatrick <bradfitz@golang.org>
2012-12-04 07:09:01 -0800

    net/http: clarify that Request.Host may contain a port number
    Fixes #4172
    
    R=golang-dev, dsymonds
    CC=golang-dev
    https://golang.org/cl/6872055

--- src/pkg/net/url/url.go
+++ src/pkg/net/url/url.go
@@ -224,7 +224,7 @@ type URL struct {
 	Scheme   string
 	Opaque   string    // encoded opaque data
 	User     *Userinfo // username and password information
-	Host     string
+	Host     string    // host or host:port
 	Path     string
 	RawQuery string // encoded query values, without '?'
 	Fragment string // fragment for references, without '#'
