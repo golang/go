b7260e89be1a09f3f4680fbb944fbb3b398c7e7c
Shenghou Ma <minux.ma@gmail.com>
2012-12-11 11:49:02 -0500

    encoding/base64: new example for NewEncoder with emphasis on Close Fixes #4394.
    R=iant, bradfitz, rsc, remigius.gieben
    CC=golang-dev
    https://golang.org/cl/6847125

--- src/pkg/encoding/base32/example_test.go
+++ src/pkg/encoding/base32/example_test.go
@@ -2,11 +2,14 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
+// Keep in sync with ../base64/example_test.go.
+
 package base32_test
 
 import (
 	"encoding/base32"
 	"fmt"
+	"os"
 )
 
 func ExampleEncoding_EncodeToString() {
@@ -27,4 +30,16 @@ func ExampleEncoding_DecodeString() {
 	fmt.Printf("%q\n", data)
 	// Output:
 	// "some data with \x00 and \ufeff"
+}
+
+func ExampleNewEncoder() {
+	input := []byte("foo\x00bar")
+	encoder := base32.NewEncoder(base32.StdEncoding, os.Stdout)
+	encoder.Write(input)
+	// Must close the encoder when finished to flush any partial blocks.
+	// If you comment out the following line, the last partial block "r"
+	// won't be encoded.
+	encoder.Close()
+	// Output:
+	// MZXW6ADCMFZA====
 }
