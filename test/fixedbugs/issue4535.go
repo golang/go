28a50c7f51ea031f91b47421322be981a5a0d8a6
Shenghou Ma <minux.ma@gmail.com>
2012-12-13 23:48:48 +0800

    runtime: deletion on nil maps is a no-op now Fixes #4535.
    R=golang-dev, rsc
    CC=golang-dev
    https://golang.org/cl/6942044

---
+++ test/fixedbugs/issue4535.go
@@ -0,0 +1,12 @@
+// run
+
+// Copyright 2012 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+func main() {
+	var m map[int]int
+	delete(m, 0)
+}
