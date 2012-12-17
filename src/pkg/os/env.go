57ae2e7371de9c7c5c2f6314baf95ca8b94a61d8
Shenghou Ma <minux.ma@gmail.com>
2012-12-17 23:37:02 +0800

    os: fix docs for Expand there is no concept of “undefined” variables for Expand。
    R=golang-dev, rsc
    CC=golang-dev
    https://golang.org/cl/6946063

--- src/pkg/os/env.go
+++ src/pkg/os/env.go
@@ -9,7 +9,7 @@ package os
 import "syscall"
 
 // Expand replaces ${var} or $var in the string based on the mapping function.
-// Invocations of undefined variables are replaced with the empty string.
+// For example, os.ExpandEnv(s) is equivalent to os.Expand(s, os.Getenv).
 func Expand(s string, mapping func(string) string) string {
 	buf := make([]byte, 0, 2*len(s))
 	// ${} is all ASCII, so bytes are fine for this operation.
