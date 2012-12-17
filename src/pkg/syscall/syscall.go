39835b4aa0e3e8dbf90849812c8657d85a3b1a44
Christopher Nielsen <m4dh4tt3r@gmail.com>
2012-12-17 22:50:00 +0800

    syscall: document that documentation is platform specific
    Fixes #4051
    
    R=golang-dev, minux.ma, rsc
    CC=golang-dev
    https://golang.org/cl/6943063

--- src/pkg/syscall/syscall.go
+++ src/pkg/syscall/syscall.go
@@ -3,10 +3,15 @@
 // license that can be found in the LICENSE file.
 
 // Package syscall contains an interface to the low-level operating system
-// primitives.  The details vary depending on the underlying system.
-// Its primary use is inside other packages that provide a more portable
-// interface to the system, such as "os", "time" and "net".  Use those
-// packages rather than this one if you can.
+// primitives.  The details vary depending on the underlying system, and
+// by default, godoc will display the syscall documentation for the current
+// system.  If you want godoc to display syscall documentation for another
+// system, set $GOOS and $GOARCH to the desired system.  For example, if
+// you want to view documentation for freebsd/arm on linux/amd64, set $GOOS
+// to freebsd and $GOARCH to arm.
+// The primary use of syscall is inside other packages that provide a more
+// portable interface to the system, such as "os", "time" and "net".  Use
+// those packages rather than this one if you can.
 // For details of the functions and data types in this package consult
 // the manuals for the appropriate operating system.
 // These calls return err == nil to indicate success; otherwise
