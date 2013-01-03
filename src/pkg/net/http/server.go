56cfbf0688574131f7dc8a88f298f7c013251780
Matthew Dempsky <mdempsky@google.com>
2013-01-03 10:06:04 -0800

    net/http: Sync TimeoutHandler godoc with code.
    TimeoutHandler was changed from "ns int64" to "dt time.Duration" on
    Nov 30, 2011, but the godoc still refers to "ns".
    
    R=golang-dev, bradfitz
    CC=golang-dev
    https://golang.org/cl/7031050

--- src/pkg/net/http/server.go
+++ src/pkg/net/http/server.go
@@ -1327,7 +1327,7 @@ func (srv *Server) ListenAndServeTLS(certFile, keyFile string) error {
 // TimeoutHandler returns a Handler that runs h with the given time limit.
 //
 // The new Handler calls h.ServeHTTP to handle each request, but if a
-// call runs for more than ns nanoseconds, the handler responds with
+// call runs for longer than its time limit, the handler responds with
 // a 503 Service Unavailable error and the given message in its body.
 // (If msg is empty, a suitable default message will be sent.)
 // After such a timeout, writes by h to its ResponseWriter will return
