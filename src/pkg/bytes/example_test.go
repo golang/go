5535cede80f9fd8abb5c960730ca248c2e207736
Matthew Dempsky <mdempsky@google.com>
2013-01-07 09:59:37 +1100

    bytes: Examples recommending bytes.Compare(a, b) rel_op 0 to test a rel_op b.
    R=golang-dev, minux.ma, rsc, adg
    CC=golang-dev
    https://golang.org/cl/7042045

--- src/pkg/bytes/example_test.go
+++ src/pkg/bytes/example_test.go
@@ -10,6 +10,7 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"sort"
 )
 
 func ExampleBuffer() {
@@ -26,4 +27,42 @@ func ExampleBuffer_reader() {
 	dec := base64.NewDecoder(base64.StdEncoding, buf)
 	io.Copy(os.Stdout, dec)
 	// Output: Gophers rule!
+}
+
+func ExampleCompare() {
+	// Interpret Compare's result by comparing it to zero.
+	var a, b []byte
+	if bytes.Compare(a, b) < 0 {
+		// a less b
+	}
+	if bytes.Compare(a, b) <= 0 {
+		// a less or equal b
+	}
+	if bytes.Compare(a, b) > 0 {
+		// a greater b
+	}
+	if bytes.Compare(a, b) >= 0 {
+		// a greater or equal b
+	}
+
+	// Prefer Equal to Compare for equality comparisons.
+	if bytes.Equal(a, b) {
+		// a equal b
+	}
+	if !bytes.Equal(a, b) {
+		// a not equal b
+	}
+}
+
+func ExampleCompare_search() {
+	// Binary search to find a matching byte slice.
+	var needle []byte
+	var haystack [][]byte // Assume sorted
+	i := sort.Search(len(haystack), func(i int) bool {
+		// Return needle <= haystack[i].
+		return bytes.Compare(needle, haystack[i]) <= 0
+	})
+	if i < len(haystack) && bytes.Equal(needle, haystack[i]) {
+		// Found it!
+	}
 }
