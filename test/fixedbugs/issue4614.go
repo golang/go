fba96e915dd16974745b199d09cfa6a4839cd03e
Rémy Oudompheng <oudomphe@phare.normalesup.org>
2013-01-08 00:23:02 +0100

    cmd/gc: fix uintptr(nil) issues.
    A constant node of type uintptr with a nil literal could
    happen in two cases: []int(nil)[1:] and
    uintptr(unsafe.Pointer(nil)).
    
    Fixes #4614.
    
    R=golang-dev, rsc
    CC=golang-dev
    https://golang.org/cl/7059043

---
+++ test/fixedbugs/issue4614.go
@@ -0,0 +1,20 @@
+// compile
+
+// Copyright 2012 The Go Authors.  All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Issue 4614: slicing of nil slices confuses the compiler
+// with a uintptr(nil) node.
+
+package p
+
+import "unsafe"
+
+var n int
+
+var _ = []int(nil)[1:]
+var _ = []int(nil)[n:]
+
+var _ = uintptr(unsafe.Pointer(nil))
+var _ = unsafe.Pointer(uintptr(0))
