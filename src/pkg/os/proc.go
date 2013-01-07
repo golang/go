0f64a6ef8db6a025fe2bb3d1756d312e9ea7d702
Andrew Gerrand <adg@golang.org>
2013-01-07 14:46:41 +1100

    os: document that Exit does not run deferred calls
    Fixes #4101.
    
    R=dsymonds, rsc
    CC=golang-dev
    https://golang.org/cl/7065048

--- src/pkg/os/proc.go
+++ src/pkg/os/proc.go
@@ -31,4 +31,6 @@ func Getgroups() ([]int, error) {
 
 // Exit causes the current program to exit with the given status code.
 // Conventionally, code zero indicates success, non-zero an error.
+// The program terminates immediately; deferred functions are
+// not run.
 func Exit(code int) { syscall.Exit(code) }
