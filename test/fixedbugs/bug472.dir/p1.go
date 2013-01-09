ceac3d3751c97ba8eeeab16bd805871020ed3651
Rémy Oudompheng <oudomphe@phare.normalesup.org>
2013-01-09 22:02:53 +0100

    cmd/gc: add space to export data to match linker expectations
    The linker split PKGDEF into (prefix, name, def) pairs,
    and defines def to begin after a space following the identifier.
    This is totally wrong for the following export data:
    
            func "".FunctionName()
            var SomethingCompletelyUnrelated int
    
    The linker would parse
        name=`"".FunctionName()\n\tvar`
        def=`SomethingCompletelyUnrelated int`
    since there is no space after FunctionName.
    
    R=minux.ma, rsc
    CC=golang-dev
    https://golang.org/cl/7068051

---
+++ test/fixedbugs/bug472.dir/p1.go
@@ -0,0 +1,17 @@
+// Copyright 2013 The Go Authors.  All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package p1
+
+import "runtime"
+
+func E() func() int { return runtime.NumCPU }
+
+func F() func() { return runtime.Gosched }
+
+func G() func() string { return runtime.GOROOT }
+
+func H() func() { return runtime.GC }
+
+func I() func() string { return runtime.Version }
