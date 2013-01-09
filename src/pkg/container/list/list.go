ecb75486f80e320dc0a06dd85c1b83a3daf293f4
Robert Griesemer <gri@golang.org>
2013-01-09 15:22:48 -0800

    container/list: avoid memory leaks
    R=golang-dev, dsymonds
    CC=golang-dev
    https://golang.org/cl/7065067

--- src/pkg/container/list/list.go
+++ src/pkg/container/list/list.go
@@ -108,6 +108,8 @@ func (l *List) insertValue(v interface{}, at *Element) *Element {
 func (l *List) remove(e *Element) *Element {
 	e.prev.next = e.next
 	e.next.prev = e.prev
+	e.next = nil // avoid memory leaks
+	e.prev = nil // avoid memory leaks
 	e.list = nil
 	l.len--
 	return e
