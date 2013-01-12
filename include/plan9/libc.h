39ffa83fdbbb21d59ecc07afb201b3839968587b
Lucio De Re <lucio.dere@gmail.com>
2013-01-11 16:58:46 -0800

    build: add missing function declarations for Plan 9
    R=golang-dev, minux.ma, rsc, ality, seed
    CC=golang-dev
    https://golang.org/cl/7034052

--- include/plan9/libc.h
+++ include/plan9/libc.h
@@ -14,3 +14,15 @@ char*	getgoos(void);
 char*	getgoarch(void);
 char*	getgoroot(void);
 char*	getgoversion(void);
+char*	getgoarm(void);
+char*	getgo386(void);
+
+void	flagcount(char*, char*, int*);
+void	flagint32(char*, char*, int32*);
+void	flagint64(char*, char*, int64*);
+void	flagstr(char*, char*, char**);
+void	flagparse(int*, char***, void (*usage)(void));
+void	flagfn0(char*, char*, void(*fn)(void));
+void	flagfn1(char*, char*, void(*fn)(char*));
+void	flagfn2(char*, char*, void(*fn)(char*, char*));
+void	flagprint(int);
