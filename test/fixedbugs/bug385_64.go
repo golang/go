d127ed53784bf8a6e376904af163b58a78179dd2
Rémy Oudompheng <oudomphe@phare.normalesup.org>
2013-01-18 22:36:43 +0100

    cmd/gc, cmd/6g: fix error on large stacks.
    Fixes #4666.
    
    R=golang-dev, daniel.morsing, rsc
    CC=golang-dev
    https://golang.org/cl/7141047

--- test/fixedbugs/bug385_64.go
+++ test/fixedbugs/bug385_64.go
@@ -8,11 +8,17 @@
 // license that can be found in the LICENSE file.
 
 // Issue 2444
+// Issue 4666: issue with arrays of exactly 4GB.
 
 package main
-func main() {  // ERROR "stack frame too large"
+
+func main() { // ERROR "stack frame too large"
 	var arr [1000200030]int32
 	arr_bkup := arr
 	_ = arr_bkup
 }
 
+func F() { // ERROR "stack frame too large"
+	var arr [1 << 30]int32
+	_ = arr[42]
+}
