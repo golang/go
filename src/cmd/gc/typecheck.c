f4ab5e17455aac487b1f6b8ce1ca2ca9f7db6150
Daniel Morsing <daniel.morsing@gmail.com>
2013-01-18 22:46:10 +0100

    cmd/gc: more robust checking of OIND nodes.
    Fixes #4610.
    
    R=golang-dev, remyoudompheng, rsc
    CC=golang-dev, nigeltao
    https://golang.org/cl/7058057

--- src/cmd/gc/typecheck.c
+++ src/cmd/gc/typecheck.c
@@ -482,9 +482,12 @@ reswitch:
 			n->left = N;
 			goto ret;
 		}
-		if((top & (Erv | Etop)) && !isptr[t->etype]) {
-			yyerror("invalid indirect of %lN", n->left);
-			goto error;
+		if(!isptr[t->etype]) {
+			if(top & (Erv | Etop)) {
+				yyerror("invalid indirect of %lN", n->left);
+				goto error;
+			}
+			goto ret;
 		}
 		ok |= Erv;
 		n->type = t->type;
