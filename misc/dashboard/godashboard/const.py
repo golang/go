319a12cebaf790c0d5b622e36d5ccfcf0b8ef99f
Andrew Gerrand <adg@golang.org>
2013-01-18 13:47:01 +1100

    misc/dashboard/godashboard: delete
    This code is obsolete and unmaintained.
    
    R=bradfitz
    CC=golang-dev
    https://golang.org/cl/7135056

--- misc/dashboard/godashboard/const.py
+++
@@ -1,7 +0,0 @@
-# Copyright 2011 The Go Authors. All rights reserved.
-# Use of this source code is governed by a BSD-style
-# license that can be found in the LICENSE file.
-
-mail_from           = "Go Dashboard <builder@golang.org>"
-mail_submit_to      = "adg@golang.org"
-mail_submit_subject = "New Project Submitted"
