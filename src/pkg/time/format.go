3a875f298c95e230044aebcc44d00270ee69d349
Caleb Spare <cespare@gmail.com>
2013-01-22 14:44:49 -0500

    time: standard time doc fix and format example
    This fixes the incorrect unix timestamp of the standard time and adds
    an example for (Time) Format to clarify how timezones work in format strings.
    
    Fixes #4364.
    
    R=golang-dev, remyoudompheng, kevlar, rsc
    CC=golang-dev
    https://golang.org/cl/7069046

--- src/pkg/time/format.go
+++ src/pkg/time/format.go
@@ -9,7 +9,7 @@ import "errors"
 // These are predefined layouts for use in Time.Format.
 // The standard time used in the layouts is:
 //	Mon Jan 2 15:04:05 MST 2006
-// which is Unix time 1136243045. Since MST is GMT-0700,
+// which is Unix time 1136239445. Since MST is GMT-0700,
 // the standard time can be thought of as
 //	01/02 03:04:05PM '06 -0700
 // To define your own format, write down what the standard time would look
