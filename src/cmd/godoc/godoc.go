7e9f00c80c56fe325d0c0e11c4a8ce4bc368b06e
Brad Fitzpatrick <bradfitz@golang.org>
2013-01-30 12:30:26 -0800

    cmd/godoc: fix buggy use of strings.HasSuffix
    This code never worked. Maybe it's not necessary?
    
    R=golang-dev, rsc
    CC=golang-dev
    https://golang.org/cl/7225070

--- src/cmd/godoc/godoc.go
+++ src/cmd/godoc/godoc.go
@@ -652,7 +652,7 @@ func applyTemplate(t *template.Template, name string, data interface{}) []byte {
 
 func redirect(w http.ResponseWriter, r *http.Request) (redirected bool) {
 	canonical := pathpkg.Clean(r.URL.Path)
-	if !strings.HasSuffix("/", canonical) {
+	if !strings.HasSuffix(canonical, "/") {
 		canonical += "/"
 	}
 	if r.URL.Path != canonical {
@@ -666,9 +666,7 @@ func redirect(w http.ResponseWriter, r *http.Request) (redirected bool) {
 
 func redirectFile(w http.ResponseWriter, r *http.Request) (redirected bool) {
 	c := pathpkg.Clean(r.URL.Path)
-	for strings.HasSuffix("/", c) {
-		c = c[:len(c)-1]
-	}
+	c = strings.TrimRight(c, "/")
 	if r.URL.Path != c {
 		url := *r.URL
 		url.Path = c
