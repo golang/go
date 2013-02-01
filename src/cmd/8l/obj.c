ca727f1116d79537f80e546e78a5addb3c09fa34
Russ Cox <rsc@golang.org>
2013-01-31 16:13:48 -0800

    cmd/8l: fix build
    TBR=golang-dev
    CC=golang-dev
    https://golang.org/cl/7241061

--- src/cmd/8l/obj.c
+++ src/cmd/8l/obj.c
@@ -114,7 +114,7 @@ main(int argc, char *argv[])
 	flagcount("d", "disable dynamic executable", &debug['d']);
 	flagcount("f", "ignore version mismatch", &debug['f']);
 	flagcount("g", "disable go package data checks", &debug['g']);
-	flagcount("hostobj", "generate host object file", &hostobj);
+	flagcount("hostobj", "generate host object file", &isobj);
 	flagstr("k", "sym: set field tracking symbol", &tracksym);
 	flagstr("o", "outfile: set output file", &outfile);
 	flagcount("p", "insert profiling code", &debug['p']);
