68631ee44f13f4fb7a71cc984e6ad72b4f30c75e
Russ Cox <rsc@golang.org>
2013-02-03 00:03:10 -0500

    cmd/gc: allow new conversion syntax
    For consistency with conversions that look like function calls,
    conversions that don't look like function calls now allow an
    optional trailing comma.
    
    That is, int(x,) has always been syntactically valid.
    Now []int(x,) is valid too.
    
    Fixes #4162.
    
    R=ken2
    CC=golang-dev
    https://golang.org/cl/7288045

---
+++ test/fixedbugs/issue4162.go
@@ -0,0 +1,17 @@
+// compile
+
+// Copyright 2013 The Go Authors.  All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Issue 4162. Trailing commas now allowed in conversions.
+
+package p
+
+// All these are valid now.
+var (
+	_ = int(1.0,)      // comma was always permitted (like function call)
+	_ = []byte("foo",) // was syntax error: unexpected comma
+	_ = chan int(nil,) // was syntax error: unexpected comma
+	_ = (func())(nil,) // was syntax error: unexpected comma
+)
