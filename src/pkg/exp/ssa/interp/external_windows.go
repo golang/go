5fa6721a311f1a4a71cf88502c44537e519b4787
Alan Donovan <adonovan@google.com>
2013-02-08 11:58:24 -0500

    exp/ssa/interp: fix MS Windows breakage.
    syscall.{Kill,Write} are not portable to MS Windows, so we
    disable them for now.
    
    R=iant, rsc
    CC=golang-dev
    https://golang.org/cl/7312066

---
+++ src/pkg/exp/ssa/interp/external_windows.go
@@ -0,0 +1,19 @@
+// Copyright 2013 The Go Authors.  All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// +build windows plan9
+
+package interp
+
+import (
+	"exp/ssa"
+)
+
+func ext۰syscall۰Kill(fn *ssa.Function, args []value, slots []value) value {
+	panic("syscall.Kill not yet implemented")
+}
+
+func ext۰syscall۰Write(fn *ssa.Function, args []value, slots []value) value {
+	panic("syscall.Write not yet implemented")
+}
