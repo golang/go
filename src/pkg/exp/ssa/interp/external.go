aaa1fd726bfaf5ba5d00f2b9265264d143b9cb26
Alan Donovan <adonovan@google.com>
2013-02-08 11:58:24 -0500

    exp/ssa/interp: fix MS Windows breakage.
    syscall.{Kill,Write} are not portable to MS Windows, so we
    disable them for now.
    
    R=iant, rsc
    CC=golang-dev
    https://golang.org/cl/7312066

--- src/pkg/exp/ssa/interp/external.go
+++ src/pkg/exp/ssa/interp/external.go
@@ -1,3 +1,7 @@
+// Copyright 2013 The Go Authors.  All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 package interp
 
 // Emulated functions that we cannot interpret because they are
@@ -171,26 +175,6 @@ func ext۰syscall۰Exit(fn *ssa.Function, args []value, slots []value) value {
 func ext۰syscall۰Getpid(fn *ssa.Function, args []value, slots []value) value {
 	// We could emulate syscall.Syscall but it's more effort.
 	return syscall.Getpid()
-}
-
-func ext۰syscall۰Kill(fn *ssa.Function, args []value, slots []value) value {
-	// We could emulate syscall.Syscall but it's more effort.
-	err := syscall.Kill(args[0].(int), syscall.Signal(args[1].(int)))
-	err = err // TODO(adonovan): fix: adapt concrete err to interpreted iface (e.g. call interpreted errors.New)
-	return iface{}
-}
-
-func ext۰syscall۰Write(fn *ssa.Function, args []value, slots []value) value {
-	// We could emulate syscall.Syscall but it's more effort.
-	p := args[1].([]value)
-	b := make([]byte, 0, len(p))
-	for i := range p {
-		b = append(b, p[i].(byte))
-	}
-	n, _ := syscall.Write(args[0].(int), b)
-	err := iface{} // TODO(adonovan): fix: adapt concrete err to interpreted iface.
-	return tuple{n, err}
-
 }
 
 // The set of remaining native functions we need to implement (as needed):
---
+++ src/pkg/exp/ssa/interp/external_unix.go
@@ -0,0 +1,32 @@
+// Copyright 2013 The Go Authors.  All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// +build !windows,!plan9
+
+package interp
+
+import (
+	"exp/ssa"
+	"syscall"
+)
+
+func ext۰syscall۰Kill(fn *ssa.Function, args []value, slots []value) value {
+	// We could emulate syscall.Syscall but it's more effort.
+	err := syscall.Kill(args[0].(int), syscall.Signal(args[1].(int)))
+	err = err // TODO(adonovan): fix: adapt concrete err to interpreted iface (e.g. call interpreted errors.New)
+	return iface{}
+}
+
+func ext۰syscall۰Write(fn *ssa.Function, args []value, slots []value) value {
+	// We could emulate syscall.Syscall but it's more effort.
+	p := args[1].([]value)
+	b := make([]byte, 0, len(p))
+	for i := range p {
+		b = append(b, p[i].(byte))
+	}
+	n, _ := syscall.Write(args[0].(int), b)
+	err := iface{} // TODO(adonovan): fix: adapt concrete err to interpreted iface.
+	return tuple{n, err}
+
+}
