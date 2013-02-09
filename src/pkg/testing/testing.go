3edff756ab5e77527d17900ee511d44a9c3537c4
Russ Cox <rsc@golang.org>
2013-02-09 13:43:15 -0500

    testing: be explicit about use of b.N
    R=golang-dev, minux.ma
    CC=golang-dev
    https://golang.org/cl/7314071

--- src/pkg/testing/testing.go
+++ src/pkg/testing/testing.go
@@ -33,6 +33,7 @@
 //         }
 //     }
 //
+// The benchmark function must run the target code b.N times.
 // The benchmark package will vary b.N until the benchmark function lasts
 // long enough to be timed reliably.  The output
 //     BenchmarkHello    10000000    282 ns/op
