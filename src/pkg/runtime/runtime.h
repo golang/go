1f59600ebf275888b66b6e754c9fa42165170486
Dmitriy Vyukov <dvyukov@google.com>
2013-02-20 20:21:45 +0400

    runtime: introduce entersyscallblock() In preparation for the new scheduler.
    R=golang-dev, rsc
    CC=golang-dev
    https://golang.org/cl/7386044

--- src/pkg/runtime/runtime.h
+++ src/pkg/runtime/runtime.h
@@ -692,6 +692,7 @@ M*	runtime·newm(void);
 void	runtime·goexit(void);
 void	runtime·asmcgocall(void (*fn)(void*), void*);
 void	runtime·entersyscall(void);
+void	runtime·entersyscallblock(void);
 void	runtime·exitsyscall(void);
 G*	runtime·newproc1(byte*, byte*, int32, int32, void*);
 bool	runtime·sigsend(int32 sig);
