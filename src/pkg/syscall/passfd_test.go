1192fe15a13f7d68b3f5ed32f6799fb3ff5ec807
Dave Cheney <dave@cheney.net>
2013-02-27 09:13:15 +1100

    syscall: fix FD passing on FreeBSD and NetBSD
    Fixes #3348.
    
    R=devon.odell, minux.ma, bradfitz, mdempsky
    CC=golang-dev
    https://golang.org/cl/7406050

--- src/pkg/syscall/passfd_test.go
+++ src/pkg/syscall/passfd_test.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// +build linux darwin
+// +build linux darwin freebsd netbsd
 
 package syscall_test
 
