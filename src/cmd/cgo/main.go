7428f5f9f0a90e102c9030a601fdd715ed1b4734
Russ Cox <rsc@golang.org>
2013-03-04 11:23:17 -0500

    cmd/cgo: use explicit flag to emit dynamic linker path
    Using -import_runtime_cgo would have worked great except
    that it doesn't get passed to the second invocation of cgo,
    and that's the one that writes the relevant file.
    
    Fixes ARM build on systems with a different dynamic linker
    than the one 5l assumes (like Gentoo).
    
    R=golang-dev, minux.ma
    CC=golang-dev
    https://golang.org/cl/7432048

--- src/cmd/cgo/main.go
+++ src/cmd/cgo/main.go
@@ -142,6 +142,7 @@ var fset = token.NewFileSet()
 
 var dynobj = flag.String("dynimport", "", "if non-empty, print dynamic import data for that file")
 var dynout = flag.String("dynout", "", "write -dynobj output to this file")
+var dynlinker = flag.Bool("dynlinker", false, "record dynamic linker information in dynimport mode")
 
 // These flags are for bootstrapping a new Go implementation,
 // to generate Go and C headers that match the data layout and
