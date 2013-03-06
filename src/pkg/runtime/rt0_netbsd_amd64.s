cc4c1b8ec7ab45e99f2aff8a8e6c0282b01cd9bf
Russ Cox <rsc@golang.org>
2013-03-06 16:55:08 -0500

    runtime: fix netbsd, windows build
    TBR=golang-dev
    CC=golang-dev
    https://golang.org/cl/7539043

--- src/pkg/runtime/rt0_netbsd_amd64.s
+++ src/pkg/runtime/rt0_netbsd_amd64.s
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-TEXT _rt0_amd64_openbsd(SB),7,$-8
+TEXT _rt0_amd64_netbsd(SB),7,$-8
 	LEAQ	8(SP), SI // argv
 	MOVQ	0(SP), DI // argc
 	MOVQ	$main(SB), AX
