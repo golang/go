401e0fea3ad120a495f7c8770cfbf1278c00c16e
Rémy Oudompheng <oudomphe@phare.normalesup.org>
2013-03-11 22:55:14 +0100

    cmd/gc: reject complex calls with mismatched argument types.
    The specification says "the two arguments must be of the same
    floating-point type."
    
    R=rsc, gri
    CC=golang-dev
    https://golang.org/cl/7671045

--- test/cmplx.go
+++ test/cmplx.go
@@ -9,12 +9,23 @@
 
 package main
 
+type (
+	Float32    float32
+	Float64    float64
+	Complex64  complex64
+	Complex128 complex128
+)
+
 var (
 	f32 float32
 	f64 float64
+	F32 Float32
+	F64 Float64
 
 	c64  complex64
 	c128 complex128
+	C64  Complex64
+	C128 Complex128
 )
 
 func main() {
@@ -25,4 +36,19 @@ func main() {
 	_ = complex128(0)     // ok
 	_ = complex(f32, f64) // ERROR "complex"
 	_ = complex(f64, f32) // ERROR "complex"
+	_ = complex(f32, F32) // ERROR "complex"
+	_ = complex(F32, f32) // ERROR "complex"
+	_ = complex(f64, F64) // ERROR "complex"
+	_ = complex(F64, f64) // ERROR "complex"
+
+	c128 = complex(f32, f32) // ERROR "cannot use"
+	c64 = complex(f64, f64)  // ERROR "cannot use"
+
+	c64 = complex(1.0, 2.0) // ok, constant is untyped
+	c128 = complex(1.0, 2.0)
+	C64 = complex(1.0, 2.0)
+	C128 = complex(1.0, 2.0)
+
+	C64 = complex(f32, f32)  // ERROR "cannot use"
+	C128 = complex(f64, f64) // ERROR "cannot use"
 }
