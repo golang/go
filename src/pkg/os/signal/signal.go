07fb6fcd40a1667bd8cca7622cfc472ee3382e06
Shenghou Ma <minux.ma@gmail.com>
2013-03-13 02:40:56 +0800

    os/signal: windows is supported, update comment Fixes #5035.
    R=golang-dev, r
    CC=golang-dev
    https://golang.org/cl/7519045

--- src/pkg/os/signal/signal.go
+++ src/pkg/os/signal/signal.go
@@ -5,7 +5,7 @@
 // Package signal implements access to incoming signals.
 package signal
 
-// BUG(rsc): This package is not yet implemented on Plan 9 and Windows.
+// BUG(rsc): This package is not yet implemented on Plan 9.
 
 import (
 	"os"
