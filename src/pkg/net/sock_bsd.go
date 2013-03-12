e64f3f211ac03f270a2db476e01b34fd1d1c5301
Russ Cox <rsc@golang.org>
2013-03-12 01:48:48 -0400

    net: never use backlog > 65535
    The system call takes an int, but the kernel stores it in a uint16.
    At least one Linux system sets /proc/sys/net/core/somaxconn
    to 262144, which ends up being 0 in the uint16. Avoid being tricked.
    
    FreeBSD sources also store the backlog in a uint16.
    Assume the problem is systemic and fix it everywhere.
    
    Fixes #5030.
    
    R=golang-dev, bradfitz
    CC=golang-dev
    https://golang.org/cl/7480046

--- src/pkg/net/sock_bsd.go
+++ src/pkg/net/sock_bsd.go
@@ -27,5 +27,11 @@ func maxListenerBacklog() int {
 	if n == 0 || err != nil {
 		return syscall.SOMAXCONN
 	}
+	// FreeBSD stores the backlog in a uint16, as does Linux.
+	// Assume the other BSDs do too. Truncate number to avoid wrapping.
+	// See issue 5030.
+	if n > 1<<16-1 {
+		n = 1<<16 - 1
+	}
 	return int(n)
 }
