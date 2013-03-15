e301fc43759560dff3696ae58ddfa3ef94217d03
Shenghou Ma <minux.ma@gmail.com>
2013-03-16 04:01:56 +0800

    net, runtime: enable runtime-integrated pollster on Linux/ARM.
    Results from linux/arm on a Samsung Chromebook (from dfc):
    localhost(~/go/src/pkg/net) % ~/go/misc/benchcmp {old,new}.txt
    benchmark                           old ns/op    new ns/op    delta
    BenchmarkTCP4OneShot                   568840       350526  -38.38%
    BenchmarkTCP4OneShot-2                 359054       206708  -42.43%
    BenchmarkTCP4OneShotTimeout            637464       363550  -42.97%
    BenchmarkTCP4OneShotTimeout-2          374255       216695  -42.10%
    BenchmarkTCP4Persistent                184974        64984  -64.87%
    BenchmarkTCP4Persistent-2              109902        47195  -57.06%
    BenchmarkTCP4PersistentTimeout         210039        64789  -69.15%
    BenchmarkTCP4PersistentTimeout-2       124284        43374  -65.10%
    BenchmarkTCP6OneShot                   672278       362116  -46.14%
    BenchmarkTCP6OneShot-2                 383631       216400  -43.59%
    BenchmarkTCP6OneShotTimeout            680740       378306  -44.43%
    BenchmarkTCP6OneShotTimeout-2          397524       230152  -42.10%
    BenchmarkTCP6Persistent                172346        65292  -62.12%
    BenchmarkTCP6Persistent-2              106229        42096  -60.37%
    BenchmarkTCP6PersistentTimeout         161149        65138  -59.58%
    BenchmarkTCP6PersistentTimeout-2       152276        44548  -70.75%
    
    R=golang-dev, dave, bradfitz, dvyukov, rsc
    CC=golang-dev
    https://golang.org/cl/7820045

--- src/pkg/runtime/netpoll_epoll.c
+++ src/pkg/runtime/netpoll_epoll.c
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// +build linux,386 linux,amd64
+// +build linux
 
 #include "runtime.h"
 #include "defs_GOOS_GOARCH.h"
