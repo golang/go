7f50c23e2d18c445bfe790692e998ff91b37ddc2
Joel Sing <jsing@google.com>
2013-03-18 12:18:49 +1100

    runtime: correct mmap return value checking on netbsd/openbsd
    The current SysAlloc implementation suffers from a signed vs unsigned
    comparision bug. Since the error code from mmap is negated, the
    unsigned comparision of v < 4096 is always false on error. Fix this
    by switching to the darwin/freebsd/linux mmap model and leave the mmap
    return value unmodified.
    
    R=golang-dev, r
    CC=golang-dev
    https://golang.org/cl/7870044

--- src/pkg/runtime/mem_netbsd.c
+++ src/pkg/runtime/mem_netbsd.c
@@ -50,10 +50,9 @@ runtime·SysReserve(void *v, uintptr n)
 		return v;
 
 	p = runtime·mmap(v, n, PROT_NONE, MAP_ANON|MAP_PRIVATE, -1, 0);
-	if (p == ((void *)-ENOMEM))
+	if(p == (void*)ENOMEM)
 		return nil;
-	else
-		return p;
+	return p;
 }
 
 void
@@ -66,7 +65,7 @@ runtime·SysMap(void *v, uintptr n)
 	// On 64-bit, we don't actually have v reserved, so tread carefully.
 	if(sizeof(void*) == 8) {
 		p = runtime·mmap(v, n, PROT_READ|PROT_WRITE, MAP_ANON|MAP_PRIVATE, -1, 0);
-		if(p == (void*)-ENOMEM)
+		if(p == (void*)ENOMEM)
 			runtime·throw("runtime: out of memory");
 		if(p != v) {
 			runtime·printf("runtime: address space conflict: map(%p) = %p\n", v, p);
@@ -76,7 +75,7 @@ runtime·SysMap(void *v, uintptr n)
 	}
 
 	p = runtime·mmap(v, n, PROT_READ|PROT_WRITE, MAP_ANON|MAP_FIXED|MAP_PRIVATE, -1, 0);
-	if(p == (void*)-ENOMEM)
+	if(p == (void*)ENOMEM)
 		runtime·throw("runtime: out of memory");
 	if(p != v)
 		runtime·throw("runtime: cannot map pages in arena address space");
