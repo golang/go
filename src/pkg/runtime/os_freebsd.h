4aeb0fc0a4dc800c57ed9f9d457de74d1158700a
Mikio Hara <mikioh.mikioh@gmail.com>
2013-03-20 02:40:29 +0900

    runtime: add missing copyright
    R=golang-dev, rsc
    CC=golang-dev
    https://golang.org/cl/7884045

--- src/pkg/runtime/os_freebsd.h
+++ src/pkg/runtime/os_freebsd.h
@@ -1,3 +1,7 @@
+// Copyright 2011 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 #define SS_DISABLE 4
 
 int32	runtime·thr_new(ThrParam*, int32);
--- src/pkg/runtime/os_netbsd.c
+++ src/pkg/runtime/os_netbsd.c
@@ -1,5 +1,6 @@
-// Use of this source file is governed by a BSD-style
-// license that can be found in the LICENSE file.`
+// Copyright 2011 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
 
 #include "runtime.h"
 #include "defs_GOOS_GOARCH.h"
