4a87b985cd86b7c97e57939fd7cd7667db5efb52
Mikio Hara <mikioh.mikioh@gmail.com>
2013-03-29 12:16:24 +0900

    net: update documentation for FileConn, FilePacketConn, FileListener
    Closes the API documentation gap between platforms.
    
    R=golang-dev, r
    CC=golang-dev
    https://golang.org/cl/8086043

--- src/pkg/net/file_windows.go
+++ src/pkg/net/file_windows.go
@@ -9,16 +9,28 @@ import (
 	"syscall"
 )
 
+// FileConn returns a copy of the network connection corresponding to
+// the open file f.  It is the caller's responsibility to close f when
+// finished.  Closing c does not affect f, and closing f does not
+// affect c.
 func FileConn(f *os.File) (c Conn, err error) {
 	// TODO: Implement this
 	return nil, os.NewSyscallError("FileConn", syscall.EWINDOWS)
 }
 
+// FileListener returns a copy of the network listener corresponding
+// to the open file f.  It is the caller's responsibility to close l
+// when finished.  Closing l does not affect f, and closing f does not
+// affect l.
 func FileListener(f *os.File) (l Listener, err error) {
 	// TODO: Implement this
 	return nil, os.NewSyscallError("FileListener", syscall.EWINDOWS)
 }
 
+// FilePacketConn returns a copy of the packet network connection
+// corresponding to the open file f.  It is the caller's
+// responsibility to close f when finished.  Closing c does not affect
+// f, and closing f does not affect c.
 func FilePacketConn(f *os.File) (c PacketConn, err error) {
 	// TODO: Implement this
 	return nil, os.NewSyscallError("FilePacketConn", syscall.EWINDOWS)
