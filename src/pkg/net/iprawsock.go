81e993d9b2e131bfc3b079dd1ce5834485465167
Mikio Hara <mikioh.mikioh@gmail.com>
2013-03-31 16:46:59 +0900

    net: update documentation for IPConn and related stuff
    Closes the API documentation gap between platforms.
    
    R=golang-dev, r
    CC=golang-dev
    https://golang.org/cl/8143044

--- src/pkg/net/iprawsock.go
+++ src/pkg/net/iprawsock.go
@@ -2,8 +2,6 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// Raw IP sockets
-
 package net
 
 // IPAddr represents the address of an IP end point.
