f4de042e2c22dcd64c667e3196d27a8b551410a4
Alexei Sholik <alcosholik@gmail.com>
2013-04-07 16:50:23 -0700

    go/ast: fix typo in Fprint documentation
    R=golang-dev, bradfitz
    CC=golang-dev
    https://golang.org/cl/8499043

--- src/pkg/go/ast/print.go
+++ src/pkg/go/ast/print.go
@@ -34,7 +34,7 @@ func NotNilFilter(_ string, v reflect.Value) bool {
 //
 // A non-nil FieldFilter f may be provided to control the output:
 // struct fields for which f(fieldname, fieldvalue) is true are
-// are printed; all others are filtered from the output. Unexported
+// printed; all others are filtered from the output. Unexported
 // struct fields are never printed.
 //
 func Fprint(w io.Writer, fset *token.FileSet, x interface{}, f FieldFilter) (err error) {
