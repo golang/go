1c1d860c56df3cfe13c2b955d4ec2272f7239d45
Shenghou Ma <minux.ma@gmail.com>
2013-04-23 04:42:04 +0800

    misc/cgo/testso: enable test on windows Depends on CL 8715043 and CL 8676050. Fixes #5273.
    R=alex.brainman, r
    CC=gobot, golang-dev
    https://golang.org/cl/8764043

--- misc/cgo/testso/cgoso_c.c
+++ misc/cgo/testso/cgoso_c.c
@@ -4,8 +4,22 @@
 
 // +build ignore
 
+#ifdef WIN32
+// A Windows DLL is unable to call an arbitrary function in
+// the main executable. Work around that by making the main
+// executable pass the callback function pointer to us.
+void (*goCallback)(void);
+__declspec(dllexport) void setCallback(void *f)
+{
+	goCallback = (void (*)())f;
+}
+__declspec(dllexport) void sofunc(void);
+#else
+extern void goCallback(void);
+void setCallback(void *f) { (void)f; }
+#endif
+
 void sofunc(void)
 {
-	extern void goCallback(void);
 	goCallback();
 }
