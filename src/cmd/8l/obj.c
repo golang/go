b29a06f468a0cc2dafd97e79942aacdece9d755a
Lucio De Re <lucio.dere@gmail.com>
2013-05-30 15:02:10 +1000

    cmd/[568]l/obj.c: NULL is not recognised in Plan 9 build, use nil instead.
    Fixes #5591.
    
    R=golang-dev, dave, minux.ma, cshapiro
    CC=carl shapiro <cshapiro, golang-dev
    https://golang.org/cl/9839046

--- src/cmd/8l/obj.c
+++ src/cmd/8l/obj.c
@@ -646,7 +646,7 @@ loop:
 	case APTRS:
 		if(skip)
 			goto casdef;
-		if(cursym->nptrs == -1 || cursym->ptrs == NULL) {
+		if(cursym->nptrs == -1 || cursym->ptrs == nil) {
 			diag("ldobj1: pointer map data provided for %s without a definition", cursym->name);
 			errorexit();
 		}
