f601a14778575464387410bb065abd6eb45b7e2c
Carl Shapiro <cshapiro@google.com>
2013-06-04 16:22:59 -0700

    cmd/5g, cmd/6g, cmd/8g: remove prototypes for proglist
    Each of the backends has two prototypes for this function but
    no corresponding definition.
    
    R=golang-dev, bradfitz, khr
    CC=golang-dev
    https://golang.org/cl/9930045

--- src/cmd/5g/gg.h
+++ src/cmd/5g/gg.h
@@ -73,7 +73,6 @@ EXTERN	int	maxstksize;
  * gen.c
  */
 void	compile(Node*);
-void	proglist(void);
 void	gen(Node*);
 Node*	lookdot(Node*, Node*, int);
 void	cgen_as(Node*, Node*);
@@ -120,7 +119,6 @@ void	cgen64(Node*, Node*);
  * gsubr.c
  */
 void	clearp(Prog*);
-void	proglist(void);
 Prog*	gbranch(int, Type*, int);
 Prog*	prog(int);
 void	gconv(int, int);
