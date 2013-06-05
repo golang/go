3e88a825df1f8a9afaf8a7e2ce79f0dc200d51ab
Andrew Gerrand <adg@golang.org>
2013-06-05 11:19:26 +1000

    [release-branch.go1.1] cmd/gc: fix missing slice/array types in export data.
    ««« CL 9953044 / 0e1b2f7384d2
    cmd/gc: fix missing slice/array types in export data.
    
    Fixes #5614.
    
    R=golang-dev
    CC=golang-dev
    https://golang.org/cl/9953044
    »»»
    
    R=iant, remyoudompheng, dsymonds
    CC=golang-dev
    https://golang.org/cl/10033043

---
+++ test/fixedbugs/issue5614.go
@@ -0,0 +1,11 @@
+// compiledir
+
+// Copyright 2013 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Issue 5614: exported data for inlining may miss
+// named types when used in implicit conversion to
+// their underlying type.
+
+package ignored
