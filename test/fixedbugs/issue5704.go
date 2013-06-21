de47f68c99e9c86a32261b45752819a86cac74f2
Robert Griesemer <gri@golang.org>
2013-06-21 16:11:13 -0700

    spec: fix spec on conversions to match implementations
    The existing compilers convert empty strings to empty
    but non-nil byte and rune slices. The spec required
    a nil byte and rune slice in those cases. That seems
    an odd additional requirement. Adjust the spec to
    match the reality.
    
    Also, removed over-specification for conversions of
    nil []byte and []rune: such nil slices already act
    like empty slices and thus don't need extra language.
    Added extra examples instead.
    
    Fixes #5704.
    
    R=rsc, r, iant
    CC=golang-dev
    https://golang.org/cl/10440045

---
+++ test/fixedbugs/issue5704.go
@@ -0,0 +1,46 @@
+// run
+
+// Copyright 2012 The Go Authors.  All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Issue 5704: Conversions of empty strings to byte
+// or rune slices return empty but non-nil slices.
+
+package main
+
+type (
+	mystring string
+	mybytes  []byte
+	myrunes  []rune
+)
+
+func checkBytes(s []byte, arg string) {
+	if len(s) != 0 {
+		panic("len(" + arg + ") != 0")
+	}
+	if s == nil {
+		panic(arg + " == nil")
+	}
+}
+
+func checkRunes(s []rune, arg string) {
+	if len(s) != 0 {
+		panic("len(" + arg + ") != 0")
+	}
+	if s == nil {
+		panic(arg + " == nil")
+	}
+}
+
+func main() {
+	checkBytes([]byte(""), `[]byte("")`)
+	checkBytes([]byte(mystring("")), `[]byte(mystring(""))`)
+	checkBytes(mybytes(""), `mybytes("")`)
+	checkBytes(mybytes(mystring("")), `mybytes(mystring(""))`)
+
+	checkRunes([]rune(""), `[]rune("")`)
+	checkRunes([]rune(mystring("")), `[]rune(mystring(""))`)
+	checkRunes(myrunes(""), `myrunes("")`)
+	checkRunes(myrunes(mystring("")), `myrunes(mystring(""))`)
+}
