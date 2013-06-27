26473e4642fe8235ad2ab0bf68c0c5b4f7de8052
Frithjof Schulze <schulze@math.uni-hannover.de>
2013-06-27 11:23:55 -0700

    crypto/tls: Change TLS version to 1.1 in the package comment.
    Also use 2048-bit RSA keys as default in generate_cert.go,
    as recommended by the NIST.
    
    R=golang-dev, rsc, bradfitz
    CC=golang-dev
    https://golang.org/cl/10676043

--- src/pkg/crypto/tls/generate_cert.go
+++ src/pkg/crypto/tls/generate_cert.go
@@ -30,7 +30,7 @@ var (
 	validFrom = flag.String("start-date", "", "Creation date formatted as Jan 1 15:04:05 2011")
 	validFor  = flag.Duration("duration", 365*24*time.Hour, "Duration that certificate is valid for")
 	isCA      = flag.Bool("ca", false, "whether this cert should be its own Certificate Authority")
-	rsaBits   = flag.Int("rsa-bits", 1024, "Size of RSA key to generate")
+	rsaBits   = flag.Int("rsa-bits", 2048, "Size of RSA key to generate")
 )
 
 func main() {
