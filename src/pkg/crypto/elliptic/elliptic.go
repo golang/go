d2a19e9fd1c4f2c4941d86e860be0cee8c418170
Adam Langley <agl@golang.org>
2013-06-27 13:31:05 -0400

    crypto/elliptic: add constant-time, P-256 implementation.
    On my 64-bit machine, despite being 32-bit code, fixed-base
    multiplications are 7.1x faster and arbitary multiplications are 2.6x
    faster.
    
    It is difficult to review this change. However, the code is essentially
    the same as code that has been open-sourced in Chromium. There it has
    been successfully performing P-256 operations for several months on
    many machines so the arithmetic of the code should be sound.
    
    R=golang-dev, rsc
    CC=golang-dev
    https://golang.org/cl/10551044

--- src/pkg/crypto/elliptic/elliptic.go
+++ src/pkg/crypto/elliptic/elliptic.go
@@ -322,7 +322,6 @@ func Unmarshal(curve Curve, data []byte) (x, y *big.Int) {
 }
 
 var initonce sync.Once
-var p256 *CurveParams
 var p384 *CurveParams
 var p521 *CurveParams
 
@@ -331,17 +330,6 @@ func initAll() {
 	initP256()
 	initP384()
 	initP521()
-}
-
-func initP256() {
-	// See FIPS 186-3, section D.2.3
-	p256 = new(CurveParams)
-	p256.P, _ = new(big.Int).SetString("115792089210356248762697446949407573530086143415290314195533631308867097853951", 10)
-	p256.N, _ = new(big.Int).SetString("115792089210356248762697446949407573529996955224135760342422259061068512044369", 10)
-	p256.B, _ = new(big.Int).SetString("5ac635d8aa3a93e7b3ebbd55769886bc651d06b0cc53b0f63bce3c3e27d2604b", 16)
-	p256.Gx, _ = new(big.Int).SetString("6b17d1f2e12c4247f8bce6e563a440f277037d812deb33a0f4a13945d898c296", 16)
-	p256.Gy, _ = new(big.Int).SetString("4fe342e2fe1a7f9b8ee7eb4a7c0f9e162bce33576b315ececbb6406837bf51f5", 16)
-	p256.BitSize = 256
 }
 
 func initP384() {
