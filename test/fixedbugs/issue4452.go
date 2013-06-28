7a0dc1c9ecfa899d6b739caf0a2f55989f0f6ec4
Rémy Oudompheng <oudomphe@phare.normalesup.org>
2013-06-28 23:08:07 +0200

    test: match gccgo error strings.
    R=golang-dev, iant
    CC=golang-dev
    https://golang.org/cl/10741043

--- test/fixedbugs/issue4452.go
+++ test/fixedbugs/issue4452.go
@@ -9,5 +9,5 @@
 package main
 
 func main() {
-	_ = [...]int(4) // ERROR "use of \[\.\.\.\] array outside of array literal"
+	_ = [...]int(4) // ERROR "\[\.\.\.\].*outside of array literal"
 }
--- test/fixedbugs/issue4463.go
+++ test/fixedbugs/issue4463.go
@@ -45,17 +45,17 @@ func F() {
 	(println("bar"))
 	(recover())
 
-	go append(a, 0)			// ERROR "discards result"
-	go cap(a)			// ERROR "discards result"
-	go complex(1, 2)		// ERROR "discards result"
-	go imag(1i)			// ERROR "discards result"
-	go len(a)			// ERROR "discards result"
-	go make([]int, 10)		// ERROR "discards result"
-	go new(int)			// ERROR "discards result"
-	go real(1i)			// ERROR "discards result"
-	go unsafe.Alignof(a)		// ERROR "discards result"
-	go unsafe.Offsetof(s.f)		// ERROR "discards result"
-	go unsafe.Sizeof(a)		// ERROR "discards result"
+	go append(a, 0)			// ERROR "not used|discards result"
+	go cap(a)			// ERROR "not used|discards result"
+	go complex(1, 2)		// ERROR "not used|discards result"
+	go imag(1i)			// ERROR "not used|discards result"
+	go len(a)			// ERROR "not used|discards result"
+	go make([]int, 10)		// ERROR "not used|discards result"
+	go new(int)			// ERROR "not used|discards result"
+	go real(1i)			// ERROR "not used|discards result"
+	go unsafe.Alignof(a)		// ERROR "not used|discards result"
+	go unsafe.Offsetof(s.f)		// ERROR "not used|discards result"
+	go unsafe.Sizeof(a)		// ERROR "not used|discards result"
 
 	go close(c)
 	go copy(a, a)
@@ -65,17 +65,17 @@ func F() {
 	go println("bar")
 	go recover()
 
-	defer append(a, 0)		// ERROR "discards result"
-	defer cap(a)			// ERROR "discards result"
-	defer complex(1, 2)		// ERROR "discards result"
-	defer imag(1i)			// ERROR "discards result"
-	defer len(a)			// ERROR "discards result"
-	defer make([]int, 10)		// ERROR "discards result"
-	defer new(int)			// ERROR "discards result"
-	defer real(1i)			// ERROR "discards result"
-	defer unsafe.Alignof(a)		// ERROR "discards result"
-	defer unsafe.Offsetof(s.f)	// ERROR "discards result"
-	defer unsafe.Sizeof(a)		// ERROR "discards result"
+	defer append(a, 0)		// ERROR "not used|discards result"
+	defer cap(a)			// ERROR "not used|discards result"
+	defer complex(1, 2)		// ERROR "not used|discards result"
+	defer imag(1i)			// ERROR "not used|discards result"
+	defer len(a)			// ERROR "not used|discards result"
+	defer make([]int, 10)		// ERROR "not used|discards result"
+	defer new(int)			// ERROR "not used|discards result"
+	defer real(1i)			// ERROR "not used|discards result"
+	defer unsafe.Alignof(a)		// ERROR "not used|discards result"
+	defer unsafe.Offsetof(s.f)	// ERROR "not used|discards result"
+	defer unsafe.Sizeof(a)		// ERROR "not used|discards result"
 
 	defer close(c)
 	defer copy(a, a)
