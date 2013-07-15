918922cf850b13fd8bfe9aacb4e595bcd067c656
David Presotto <presotto@gmail.com>
2013-07-15 18:40:55 -0400

    net: add SetKeepAlivePeriod
    Sets both the duration from the last data packet to the first
    keep alive packet and the duration between keep alive packets to be
    the passed duration.
    
    I've tested the function on both Darwin (10.8.4) and 4.2 Linux.
    I've compiled (make.bash) for all the OS's and tested (all.bash)
    on Darwin and Linux.
    
    R=golang-dev, dave, rsc, dvyukov, presotto+facebook, nick
    CC=golang-dev, veyron-team
    https://golang.org/cl/11130044

---
+++ src/pkg/net/tcpsockopt_windows.go
@@ -0,0 +1,21 @@
+// Copyright 2009 The Go Authors.  All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// TCP socket options for windows
+
+package net
+
+import (
+	"time"
+)
+
+func setKeepAlivePeriod(fd *netFD, d time.Duration) error {
+	if err := fd.incref(false); err != nil {
+		return err
+	}
+	defer fd.decref()
+
+	// We can't actually set this per connection.  Act as a noop rather than an error.
+	return nil
+}
