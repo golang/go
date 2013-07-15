9ebc5be39c5bad22ca0f97849d1ad475bacdc950
Andrew Bonventre <andybons@chromium.org>
2013-07-15 10:57:01 +1000

    image/gif: add writer implementation
    R=r, nigeltao
    CC=golang-dev
    https://golang.org/cl/10896043

--- src/pkg/image/gif/reader.go
+++ src/pkg/image/gif/reader.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// Package gif implements a GIF image decoder.
+// Package gif implements a GIF image decoder and encoder.
 //
 // The GIF specification is at http://www.w3.org/Graphics/GIF/spec-gif89a.txt.
 package gif
