53d9b6fcf3d459c2e550238502b499c462983329
Josh Bleecher Snyder <josharian@gmail.com>
2013-08-04 08:41:19 +1000

    html/template: handle nils during indirection
    Fixes #5982.
    
    R=golang-dev, r
    CC=golang-dev
    https://golang.org/cl/12387043

--- src/pkg/html/template/content.go
+++ src/pkg/html/template/content.go
@@ -74,6 +74,9 @@ const (
 // indirect returns the value, after dereferencing as many times
 // as necessary to reach the base type (or nil).
 func indirect(a interface{}) interface{} {
+	if a == nil {
+		return nil
+	}
 	if t := reflect.TypeOf(a); t.Kind() != reflect.Ptr {
 		// Avoid creating a reflect.Value if it's not a pointer.
 		return a
@@ -94,6 +97,9 @@ var (
 // as necessary to reach the base type (or nil) or an implementation of fmt.Stringer
 // or error,
 func indirectToStringerOrError(a interface{}) interface{} {
+	if a == nil {
+		return nil
+	}
 	v := reflect.ValueOf(a)
 	for !v.Type().Implements(fmtStringerType) && !v.Type().Implements(errorType) && v.Kind() == reflect.Ptr && !v.IsNil() {
 		v = v.Elem()
