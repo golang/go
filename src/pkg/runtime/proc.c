cca6b16052870905938274af50525179bcf2fdc8
Russ Cox <rsc@golang.org>
2013-08-05 16:06:24 -0400

    runtime: remove debugging knob to turn off preemption
    It's still easy to turn off, but the builders are happy.
    Also document.
    
    R=golang-dev, iant, dvyukov
    CC=golang-dev
    https://golang.org/cl/12371043

--- src/pkg/runtime/proc.c
+++ src/pkg/runtime/proc.c
@@ -2275,12 +2275,6 @@ preemptone(P *p)
 	M *mp;
 	G *gp;
 
-// Preemption requires more robust traceback routines.
-// For now, disable.
-// The if(1) silences a compiler warning about the rest of the
-// function being unreachable.
-if(0) return;
-
 	mp = p->m;
 	if(mp == nil || mp == m)
 		return;
