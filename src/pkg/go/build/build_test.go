d9f93b0e0b351de6cfe05c03d8e8f78328178407
Russ Cox <rsc@golang.org>
2013-08-09 18:34:08 -0400

    go/build: add AllTags to Package
    AllTags lists all the tags that can affect the decision
    about which files to include. Tools scanning packages
    can use this to decide how many variants there are
    and what they are.
    
    R=bradfitz
    CC=golang-dev
    https://golang.org/cl/12703044

--- src/pkg/go/build/build_test.go
+++ src/pkg/go/build/build_test.go
@@ -7,6 +7,7 @@ package build
 import (
 	"os"
 	"path/filepath"
+	"reflect"
 	"runtime"
 	"testing"
 )
@@ -14,29 +15,37 @@ import (
 func TestMatch(t *testing.T) {
 	ctxt := Default
 	what := "default"
-	match := func(tag string) {
-		if !ctxt.match(tag) {
+	match := func(tag string, want map[string]bool) {
+		m := make(map[string]bool)
+		if !ctxt.match(tag, m) {
 			t.Errorf("%s context should match %s, does not", what, tag)
 		}
+		if !reflect.DeepEqual(m, want) {
+			t.Errorf("%s tags = %v, want %v", tag, m, want)
+		}
 	}
-	nomatch := func(tag string) {
-		if ctxt.match(tag) {
+	nomatch := func(tag string, want map[string]bool) {
+		m := make(map[string]bool)
+		if ctxt.match(tag, m) {
 			t.Errorf("%s context should NOT match %s, does", what, tag)
+		}
+		if !reflect.DeepEqual(m, want) {
+			t.Errorf("%s tags = %v, want %v", tag, m, want)
 		}
 	}
 
-	match(runtime.GOOS + "," + runtime.GOARCH)
-	match(runtime.GOOS + "," + runtime.GOARCH + ",!foo")
-	nomatch(runtime.GOOS + "," + runtime.GOARCH + ",foo")
+	match(runtime.GOOS+","+runtime.GOARCH, map[string]bool{runtime.GOOS: true, runtime.GOARCH: true})
+	match(runtime.GOOS+","+runtime.GOARCH+",!foo", map[string]bool{runtime.GOOS: true, runtime.GOARCH: true, "foo": true})
+	nomatch(runtime.GOOS+","+runtime.GOARCH+",foo", map[string]bool{runtime.GOOS: true, runtime.GOARCH: true, "foo": true})
 
 	what = "modified"
 	ctxt.BuildTags = []string{"foo"}
-	match(runtime.GOOS + "," + runtime.GOARCH)
-	match(runtime.GOOS + "," + runtime.GOARCH + ",foo")
-	nomatch(runtime.GOOS + "," + runtime.GOARCH + ",!foo")
-	match(runtime.GOOS + "," + runtime.GOARCH + ",!bar")
-	nomatch(runtime.GOOS + "," + runtime.GOARCH + ",bar")
-	nomatch("!")
+	match(runtime.GOOS+","+runtime.GOARCH, map[string]bool{runtime.GOOS: true, runtime.GOARCH: true})
+	match(runtime.GOOS+","+runtime.GOARCH+",foo", map[string]bool{runtime.GOOS: true, runtime.GOARCH: true, "foo": true})
+	nomatch(runtime.GOOS+","+runtime.GOARCH+",!foo", map[string]bool{runtime.GOOS: true, runtime.GOARCH: true, "foo": true})
+	match(runtime.GOOS+","+runtime.GOARCH+",!bar", map[string]bool{runtime.GOOS: true, runtime.GOARCH: true, "bar": true})
+	nomatch(runtime.GOOS+","+runtime.GOARCH+",bar", map[string]bool{runtime.GOOS: true, runtime.GOARCH: true, "bar": true})
+	nomatch("!", map[string]bool{})
 }
 
 func TestDotSlashImport(t *testing.T) {
@@ -92,28 +101,44 @@ func TestLocalDirectory(t *testing.T) {
 func TestShouldBuild(t *testing.T) {
 	const file1 = "// +build tag1\n\n" +
 		"package main\n"
+	want1 := map[string]bool{"tag1": true}
 
 	const file2 = "// +build cgo\n\n" +
 		"// This package implements parsing of tags like\n" +
 		"// +build tag1\n" +
 		"package build"
+	want2 := map[string]bool{"cgo": true}
 
 	const file3 = "// Copyright The Go Authors.\n\n" +
 		"package build\n\n" +
 		"// shouldBuild checks tags given by lines of the form\n" +
 		"// +build tag\n" +
 		"func shouldBuild(content []byte)\n"
+	want3 := map[string]bool{}
 
 	ctx := &Context{BuildTags: []string{"tag1"}}
-	if !ctx.shouldBuild([]byte(file1)) {
-		t.Errorf("should not build file1, expected the contrary")
+	m := map[string]bool{}
+	if !ctx.shouldBuild([]byte(file1), m) {
+		t.Errorf("shouldBuild(file1) = false, want true")
 	}
-	if ctx.shouldBuild([]byte(file2)) {
-		t.Errorf("should build file2, expected the contrary")
+	if !reflect.DeepEqual(m, want1) {
+		t.Errorf("shoudBuild(file1) tags = %v, want %v", m, want1)
 	}
 
+	m = map[string]bool{}
+	if ctx.shouldBuild([]byte(file2), m) {
+		t.Errorf("shouldBuild(file2) = true, want fakse")
+	}
+	if !reflect.DeepEqual(m, want2) {
+		t.Errorf("shoudBuild(file2) tags = %v, want %v", m, want2)
+	}
+
+	m = map[string]bool{}
 	ctx = &Context{BuildTags: nil}
-	if !ctx.shouldBuild([]byte(file3)) {
-		t.Errorf("should not build file3, expected the contrary")
+	if !ctx.shouldBuild([]byte(file3), m) {
+		t.Errorf("shouldBuild(file3) = false, want true")
+	}
+	if !reflect.DeepEqual(m, want3) {
+		t.Errorf("shoudBuild(file3) tags = %v, want %v", m, want3)
 	}
 }
