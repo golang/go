afe2a3c9cc228ff63481bc467c2bd1c44038b1fc
Russ Cox <rsc@golang.org>
2013-08-14 12:57:05 -0400

    cmd/dist: fix darwin build
    The TLS block on Darwin is not the same as on ELF.
    
    TBR=elias.naur
    CC=golang-dev
    https://golang.org/cl/12741044

--- src/cmd/ld/data.c
+++ src/cmd/ld/data.c
@@ -1212,7 +1212,7 @@ dodata(void)
 		diag("data or bss segment too large");
 	}
 	
-	if(iself && linkmode == LinkExternal && s != nil && s->type == STLSBSS && HEADTYPE != Hopenbsd) {
+	if(iself && linkmode == LinkExternal && s != nil && s->type == STLSBSS && iself && HEADTYPE != Hopenbsd) {
 		sect = addsection(&segdata, ".tbss", 06);
 		sect->align = PtrSize;
 		sect->vaddr = 0;
