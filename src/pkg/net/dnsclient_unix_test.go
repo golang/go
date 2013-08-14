a361f498d904f761ad9e0467ee3f87a5379ae4a9
Mikio Hara <mikioh.mikioh@gmail.com>
2013-08-15 05:07:35 +0900

    net: fix TestTCPLookup
    R=golang-dev, dvyukov, dave
    CC=golang-dev
    https://golang.org/cl/12766044

--- src/pkg/net/dnsclient_unix_test.go
+++ src/pkg/net/dnsclient_unix_test.go
@@ -15,10 +15,10 @@ func TestTCPLookup(t *testing.T) {
 		t.Skip("skipping test to avoid external network")
 	}
 	c, err := Dial("tcp", "8.8.8.8:53")
-	defer c.Close()
 	if err != nil {
 		t.Fatalf("Dial failed: %v", err)
 	}
+	defer c.Close()
 	cfg := &dnsConfig{timeout: 10, attempts: 3}
 	_, err = exchange(cfg, c, "com.", dnsTypeALL)
 	if err != nil {
