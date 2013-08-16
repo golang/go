71eae5a46a1ecb21dfe5eacd42fcdad8145fac52
Rob Pike <r@golang.org>
2013-08-16 10:30:05 +1000

    cmd/go: delete 'go doc' It's next to useless and confusing as well. Let's make godoc better instead.
    Fixes #4849.
    
    R=golang-dev, dsymonds, adg, rogpeppe, rsc
    CC=golang-dev
    https://golang.org/cl/12974043

--- src/cmd/go/fmt.go
+++ src/cmd/go/fmt.go
@@ -4,11 +4,8 @@
 
 package main
 
-import "os/exec"
-
 func init() {
 	addBuildFlagsNX(cmdFmt)
-	addBuildFlagsNX(cmdDoc)
 }
 
 var cmdFmt = &Command{
@@ -27,7 +24,7 @@ The -x flag prints commands as they are executed.
 
 To run gofmt with specific options, run gofmt itself.
 
-See also: go doc, go fix, go vet.
+See also: go fix, go vet.
 	`,
 }
 
@@ -37,44 +34,5 @@ func runFmt(cmd *Command, args []string) {
 		// the command only applies to this package,
 		// not to packages in subdirectories.
 		run(stringList("gofmt", "-l", "-w", relPaths(pkg.allgofiles)))
-	}
-}
-
-var cmdDoc = &Command{
-	Run:       runDoc,
-	UsageLine: "doc [-n] [-x] [packages]",
-	Short:     "run godoc on package sources",
-	Long: `
-Doc runs the godoc command on the packages named by the
-import paths.
-
-For more about godoc, see 'godoc godoc'.
-For more about specifying packages, see 'go help packages'.
-
-The -n flag prints commands that would be executed.
-The -x flag prints commands as they are executed.
-
-To run godoc with specific options, run godoc itself.
-
-See also: go fix, go fmt, go vet.
-	`,
-}
-
-func runDoc(cmd *Command, args []string) {
-	_, err := exec.LookPath("godoc")
-	if err != nil {
-		errorf("go doc: can't find godoc; to install:\n\tgo get code.google.com/p/go.tools/cmd/godoc")
-		return
-	}
-	for _, pkg := range packages(args) {
-		if pkg.ImportPath == "command-line arguments" {
-			errorf("go doc: cannot use package file list")
-			continue
-		}
-		if pkg.local {
-			run("godoc", pkg.Dir)
-		} else {
-			run("godoc", pkg.ImportPath)
-		}
 	}
 }
