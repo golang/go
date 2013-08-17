96d7997f0355d165e953f15e10d626556b08e46c
Mikio Hara <mikioh.mikioh@gmail.com>
2013-08-17 13:40:14 +0900

    net: enable runtime-integrated network pollster on netbsd
    R=golang-dev, bradfitz
    CC=golang-dev
    https://golang.org/cl/13080043

--- src/pkg/net/fd_bsd.go
+++ src/pkg/net/fd_bsd.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// +build freebsd,arm netbsd
+// +build freebsd,arm
 
 // Waiting for FDs via kqueue/kevent.
 
