6401e0f83f29b0f3b03b9d07ebcab56e5ddb461b
Keith Randall <khr@golang.org>
2013-08-19 12:20:50 -0700

    runtime: don't run finalizers if we're still on the g0 stack.
    R=golang-dev, rsc, dvyukov, khr
    CC=golang-dev
    https://golang.org/cl/11386044

--- src/pkg/runtime/stack.c
+++ src/pkg/runtime/stack.c
@@ -105,7 +105,7 @@ runtime·stackalloc(uint32 n)
 		m->stackinuse++;
 		return v;
 	}
-	return runtime·mallocgc(n, 0, FlagNoProfiling|FlagNoGC|FlagNoZero|FlagNoInvokeGC);
+	return runtime·mallocgc(n, 0, FlagNoProfiling|FlagNoGC|FlagNoZero);
 }
 
 void
