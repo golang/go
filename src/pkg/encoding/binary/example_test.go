d5c806d581f4d110dfc2696c8b18b4ecde82e453
Mathieu Lonjaret <mathieu.lonjaret@gmail.com>
2013-08-27 06:32:24 -0700

    encoding/binary: use bytes.Reader in read example
    R=golang-dev, dave
    CC=golang-dev
    https://golang.org/cl/13274043

--- src/pkg/encoding/binary/example_test.go
+++ src/pkg/encoding/binary/example_test.go
@@ -42,7 +42,7 @@ func ExampleWrite_multi() {
 func ExampleRead() {
 	var pi float64
 	b := []byte{0x18, 0x2d, 0x44, 0x54, 0xfb, 0x21, 0x09, 0x40}
-	buf := bytes.NewBuffer(b)
+	buf := bytes.NewReader(b)
 	err := binary.Read(buf, binary.LittleEndian, &pi)
 	if err != nil {
 		fmt.Println("binary.Read failed:", err)
