c01945afc90e17b214ccbc7d49d477075ec8b463
Robert Daniel Kortschak <dan.kortschak@adelaide.edu.au>
2013-09-08 13:21:03 +1000

    sort: fix up example expected output formatting
    R=golang-dev, r
    CC=golang-dev
    https://golang.org/cl/13426046

--- src/pkg/sort/example_interface_test.go
+++ src/pkg/sort/example_interface_test.go
@@ -38,6 +38,7 @@ func ExampleInterface() {
 	sort.Sort(ByAge(people))
 	fmt.Println(people)
 
-	// Output: [Bob: 31 John: 42 Michael: 17 Jenny: 26]
+	// Output:
+	// [Bob: 31 John: 42 Michael: 17 Jenny: 26]
 	// [Michael: 17 Jenny: 26 Bob: 31 John: 42]
 }
