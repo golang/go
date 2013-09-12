eaf67b69f00a11e6c582d90d5dc1d6cbe8d48597
Brad Fitzpatrick <bradfitz@golang.org>
2013-09-12 11:20:16 +0100

    net/http: document ServeMux handling of pattern "/"
    Fixes #4799
    
    R=golang-dev, dave, rsc
    CC=golang-dev
    https://golang.org/cl/13457047

--- src/pkg/net/http/example_test.go
+++ src/pkg/net/http/example_test.go
@@ -68,3 +68,21 @@ func ExampleStripPrefix() {
 	// URL's path before the FileServer sees it:
 	http.Handle("/tmpfiles/", http.StripPrefix("/tmpfiles/", http.FileServer(http.Dir("/tmp"))))
 }
+
+type apiHandler struct{}
+
+func (apiHandler) ServeHTTP(http.ResponseWriter, *http.Request) {}
+
+func ExampleServeMux_Handle() {
+	mux := http.NewServeMux()
+	mux.Handle("/api/", apiHandler{})
+	mux.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
+		// The "/" pattern matches everything, so we need to check
+		// that we're at the root here.
+		if req.URL.Path != "/" {
+			http.NotFound(w, req)
+			return
+		}
+		fmt.Fprintf(w, "Welcome to the home page!")
+	})
+}
