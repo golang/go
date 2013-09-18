f70c51d91a7eeec9c654b11ae38407ef3d67a571
Ian Lance Taylor <iant@golang.org>
2013-09-18 16:30:38 -0700

    test: add a test that gccgo failed to compile
    R=golang-dev, rsc
    CC=golang-dev
    https://golang.org/cl/13632057

---
+++ test/fixedbugs/bug478.go
@@ -0,0 +1,10 @@
+// compiledir
+
+// Copyright 2013 The Go Authors.  All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Using the same unexported name for a method as a method on an
+// imported embedded type caused a gccgo compilation failure.
+
+package ignored
