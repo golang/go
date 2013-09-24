b0ef6aeff961053b4351cc103e3d4e4f72542e10
Kamil Kisiel <kamil@kamilkisiel.net>
2013-09-24 16:32:23 -0700

    math/big: Fix variable name in documentation for *Int.Bytes and BitLen.
    R=golang-dev, iant, gri
    CC=golang-dev
    https://golang.org/cl/13869043

--- src/pkg/math/big/int.go
+++ src/pkg/math/big/int.go
@@ -563,13 +563,13 @@ func (z *Int) SetBytes(buf []byte) *Int {
 	return z
 }
 
-// Bytes returns the absolute value of z as a big-endian byte slice.
+// Bytes returns the absolute value of x as a big-endian byte slice.
 func (x *Int) Bytes() []byte {
 	buf := make([]byte, len(x.abs)*_S)
 	return buf[x.abs.bytes(buf):]
 }
 
-// BitLen returns the length of the absolute value of z in bits.
+// BitLen returns the length of the absolute value of x in bits.
 // The bit length of 0 is 0.
 func (x *Int) BitLen() int {
 	return x.abs.bitLen()
