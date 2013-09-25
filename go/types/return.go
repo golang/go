a6c151c04db078d856d9bae182646c11f4662a1b
Robert Griesemer <gri@golang.org>
2013-09-25 10:19:24 -0700

    go.tools/go/types: (panic(0)) (parenthesized) is a terminating statement
    R=adonovan
    CC=golang-dev
    https://golang.org/cl/13913043

--- go/types/return.go
+++ go/types/return.go
@@ -28,8 +28,8 @@ func (check *checker) isTerminating(s ast.Stmt, label string) bool {
 		return check.isTerminating(s.Stmt, s.Label.Name)
 
 	case *ast.ExprStmt:
-		// the predeclared panic() function is terminating
-		if call, _ := s.X.(*ast.CallExpr); call != nil {
+		// the predeclared (possibly parenthesized) panic() function is terminating
+		if call, _ := unparen(s.X).(*ast.CallExpr); call != nil {
 			if id, _ := call.Fun.(*ast.Ident); id != nil {
 				if obj := check.topScope.LookupParent(id.Name); obj != nil {
 					if b, _ := obj.(*Builtin); b != nil && b.id == _Panic {
