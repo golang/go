795b337139a20b18d1e2b8fbaed2d271817bd0d6
Rémy Oudompheng <oudomphe@phare.normalesup.org>
2013-10-02 12:27:33 -0400

    cmd/gc: qualified embedded fields with owner package.
    R=rsc
    CC=golang-dev
    https://golang.org/cl/14188044

--- src/cmd/gc/yerr.h
+++ src/cmd/gc/yerr.h
@@ -14,28 +14,28 @@ static struct {
 	// is converted by bisonerrors into the yystate and yychar caused
 	// by that token list.
 
-	221, ',',
+	222, ',',
 	"unexpected comma during import block",
 
 	32, ';',
 	"missing import path; require quoted string",
 
-	377, ';',
+	378, ';',
 	"missing { after if clause",
 
-	398, ';',
+	399, ';',
 	"missing { after switch clause",
 
-	237, ';',
+	238, ';',
 	"missing { after for clause",
 
-	475, LBODY,
+	476, LBODY,
 	"missing { after for clause",
 
 	22, '{',
 	"unexpected semicolon or newline before {",
 
-	144, ';',
+	145, ';',
 	"unexpected semicolon or newline in type declaration",
 
 	37, '}',
@@ -47,33 +47,33 @@ static struct {
 	37, ',',
 	"unexpected comma in channel type",
 
-	438, LELSE,
+	439, LELSE,
 	"unexpected semicolon or newline before else",
 
-	257, ',',
+	258, ',',
 	"name list not allowed in interface type",
 
-	237, LVAR,
+	238, LVAR,
 	"var declaration not allowed in for initializer",
 
 	65, '{',
 	"unexpected { at end of statement",
 
-	376, '{',
+	377, '{',
 	"unexpected { at end of statement",
 	
-	125, ';',
+	126, ';',
 	"argument to go/defer must be function call",
 	
-	425, ';',
+	426, ';',
 	"need trailing comma before newline in composite literal",
 	
-	436, ';',
+	437, ';',
 	"need trailing comma before newline in composite literal",
 	
-	112, LNAME,
+	113, LNAME,
 	"nested func not allowed",
 
-	644, ';',
+	645, ';',
 	"else must be followed by if or statement block"
 };
