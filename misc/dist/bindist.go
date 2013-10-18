73d7d12ea62e99fef210ec3d9d2ffbe87d1628f0
Andrew Gerrand <adg@golang.org>
2013-10-18 10:51:21 +0900

    misc/dist: set default go.tools tag
    Fixes #6607.
    
    R=dsymonds
    CC=golang-dev
    https://golang.org/cl/14830043

--- misc/dist/bindist.go
+++ misc/dist/bindist.go
@@ -49,7 +49,7 @@ const (
 	blogPath       = "code.google.com/p/go.blog"
 	toolPath       = "code.google.com/p/go.tools"
 	tourPath       = "code.google.com/p/go-tour"
-	defaultToolTag = "tip" // TOOD(adg): set this once Go 1.2 settles
+	defaultToolTag = "9e246fa49449" // go.tools as of 2013-10-18, for go1.2
 )
 
 // Import paths for tool commands.
