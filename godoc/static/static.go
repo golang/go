99890b82e729083584a44e56d5e45587cd3fab60
Andrew Gerrand <adg@golang.org>
2013-10-30 13:13:42 +1100

    go.tools/godoc/static: use preferred JS for Google+ buttons
    R=bradfitz, dsymonds
    CC=golang-dev
    https://golang.org/cl/19070044

--- godoc/static/static.go
+++ godoc/static/static.go
@@ -481,7 +481,7 @@ function addPlusButtons() {
   var po = document.createElement('script');
   po.type = 'text/javascript';
   po.async = true;
-  po.src = 'https://apis.google.com/js/plusone.js';
+  po.src = 'https://apis.google.com/js/platform.js';
   var s = document.getElementsByTagName('script')[0];
   s.parentNode.insertBefore(po, s);
 }
