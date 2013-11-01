7097ed7337135c68b3c721a4963ef936e003e487
Andrew Gerrand <adg@golang.org>
2013-11-01 11:20:21 +1100

    [release-branch.go1.2] net/url: fix Encode doc comment
    ««« CL 16430043 / f9af8b83c78c
    net/url: fix Encode doc comment
    
    Encoded query strings are always sorted by key; the example wasn't.
    
    R=golang-dev, dsymonds, minux.ma, bradfitz
    CC=golang-dev
    https://golang.org/cl/16430043
    »»»
    
    R=golang-dev
    CC=golang-dev
    https://golang.org/cl/20480044

--- src/pkg/net/url/url.go
+++ src/pkg/net/url/url.go
@@ -558,8 +558,8 @@ func parseQuery(m Values, query string) (err error) {
 	return err
 }
 
-// Encode encodes the values into ``URL encoded'' form.
-// e.g. "foo=bar&bar=baz"
+// Encode encodes the values into ``URL encoded'' form
+// ("bar=baz&foo=quux") sorted by key.
 func (v Values) Encode() string {
 	if v == nil {
 		return ""
