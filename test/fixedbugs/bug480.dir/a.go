e8d5461f9719f564a4078c47e3ab6abc335b3a46
Ian Lance Taylor <iant@golang.org>
2013-12-10 10:47:30 -0800

    test: add some test cases that were miscompiled by gccgo
    R=golang-dev, r
    CC=golang-dev
    https://golang.org/cl/40310043

---
+++ test/fixedbugs/bug480.dir/a.go
@@ -0,0 +1,17 @@
+// Copyright 2013 The Go Authors.  All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package a
+
+type S interface{
+	F() T
+}
+
+type T struct {
+	S
+}
+
+type U struct {
+	error
+}
