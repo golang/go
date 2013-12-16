a6ebc88bace75ea1eb978ed2d1267e4ac3e9a99a
Robin Eklind <r.eklind.87@gmail.com>
2013-12-16 09:43:03 -0800

    strings: Add FieldsFunc example.
    R=golang-dev, dave
    CC=golang-dev
    https://golang.org/cl/42360043

--- src/pkg/strings/example_test.go
+++ src/pkg/strings/example_test.go
@@ -7,11 +7,20 @@ package strings_test
 import (
 	"fmt"
 	"strings"
+	"unicode"
 )
 
 func ExampleFields() {
 	fmt.Printf("Fields are: %q", strings.Fields("  foo bar  baz   "))
 	// Output: Fields are: ["foo" "bar" "baz"]
+}
+
+func ExampleFieldsFunc() {
+	f := func(c rune) bool {
+		return !unicode.IsLetter(c) && !unicode.IsNumber(c)
+	}
+	fmt.Printf("Fields are: %q", strings.FieldsFunc("  foo1;bar2,baz3...", f))
+	// Output: Fields are: ["foo1" "bar2" "baz3"]
 }
 
 func ExampleContains() {
