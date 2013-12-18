049db393ec07cdce52d3e18f3440305b75143508
Mikio Hara <mikioh.mikioh@gmail.com>
2013-12-19 07:40:10 +0900

    net: add test cases for the both of netgo, cgo DNS lookups
    Update #4078
    
    R=golang-dev, rsc
    CC=golang-dev
    https://golang.org/cl/14638043

---
+++ src/pkg/net/cgo_unix_test.go
@@ -0,0 +1,24 @@
+// Copyright 2013 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// +build cgo,!netgo
+// +build darwin dragonfly freebsd linux netbsd openbsd
+
+package net
+
+import "testing"
+
+func TestCgoLookupIP(t *testing.T) {
+	host := "localhost"
+	_, err, ok := cgoLookupIP(host)
+	if !ok {
+		t.Errorf("cgoLookupIP must not be a placeholder")
+	}
+	if err != nil {
+		t.Errorf("cgoLookupIP failed: %v", err)
+	}
+	if _, err := goLookupIP(host); err != nil {
+		t.Errorf("goLookupIP failed: %v", err)
+	}
+}
