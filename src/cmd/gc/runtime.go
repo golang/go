deb554934c0c2a87b24f4eb29cdcbbd4ea68a6d6
Keith Randall <khr@golang.org>
2013-12-17 16:55:06 -0800

    runtime, gc: call interface conversion routines by reference.
    Part of getting rid of vararg C calls.
    
    R=golang-dev, bradfitz
    CC=golang-dev
    https://golang.org/cl/23310043

--- src/cmd/gc/runtime.go
+++ src/cmd/gc/runtime.go
@@ -58,8 +58,8 @@ func slicestringcopy(to any, fr any) int
 func typ2Itab(typ *byte, typ2 *byte, cache **byte) (ret *byte)
 func convI2E(elem any) (ret any)
 func convI2I(typ *byte, elem any) (ret any)
-func convT2E(typ *byte, elem any) (ret any)
-func convT2I(typ *byte, typ2 *byte, cache **byte, elem any) (ret any)
+func convT2E(typ *byte, elem *any) (ret any)
+func convT2I(typ *byte, typ2 *byte, cache **byte, elem *any) (ret any)
 
 // interface type assertions  x.(T)
 func assertE2E(typ *byte, iface any) (ret any)
