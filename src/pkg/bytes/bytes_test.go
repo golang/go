f4ddeadc85c8dc193aede58dabcfd23cbcd237fa
Shawn Smith <shawn.p.smith@gmail.com>
2013-12-20 23:19:32 -0800

    bytes, strings: improve Title test coverage by adding cases with underscore and unicode line separator
    R=golang-codereviews, gobot, r
    CC=golang-codereviews
    https://golang.org/cl/42310045

--- src/pkg/bytes/bytes_test.go
+++ src/pkg/bytes/bytes_test.go
@@ -1073,6 +1073,8 @@ var TitleTests = []TitleTest{
 	{"123a456", "123a456"},
 	{"double-blind", "Double-Blind"},
 	{"ÿøû", "Ÿøû"},
+	{"with_underscore", "With_underscore"},
+	{"unicode \xe2\x80\xa8 line separator", "Unicode \xe2\x80\xa8 Line Separator"},
 }
 
 func TestTitle(t *testing.T) {
