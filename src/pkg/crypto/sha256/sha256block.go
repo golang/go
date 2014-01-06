eca42f998153e41e6f7818a37e9b3e912fd12c88
Joel Sing <jsing@google.com>
2014-01-06 13:31:22 -0500

    crypto/sha256: block implementation in 386 assembly
    Benchmark on Intel(R) Core(TM) i5-2500S CPU @ 2.70GHz (albeit in a VM)
    
    benchmark              old ns/op    new ns/op    delta
    BenchmarkHash8Bytes         1606          699  -56.48%
    BenchmarkHash1K            21920         7268  -66.84%
    BenchmarkHash8K           165696        53756  -67.56%
    
    benchmark               old MB/s     new MB/s  speedup
    BenchmarkHash8Bytes         4.98        11.44    2.30x
    BenchmarkHash1K            46.72       140.88    3.02x
    BenchmarkHash8K            49.44       152.39    3.08x
    
    R=agl, cldorian
    CC=golang-codereviews
    https://golang.org/cl/44800044

--- src/pkg/crypto/sha256/sha256block.go
+++ src/pkg/crypto/sha256/sha256block.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// +build !amd64
+// +build !386,!amd64
 
 // SHA256 block step.
 // In its own file so that a faster assembly or C version
