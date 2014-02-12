f7f8bde2792de8e12b160b71d3b99ce728897217
Brad Fitzpatrick <bradfitz@golang.org>
2014-02-12 13:31:05 -0800

    crypto/md5: always test the portable block function too
    So it doesn't bitrot.
    
    Like the sha1 version (https://golang.org/cl/62270043)
    
    LGTM=agl
    R=agl
    CC=golang-codereviews
    https://golang.org/cl/62420043

--- src/pkg/crypto/md5/gen.go
+++ src/pkg/crypto/md5/gen.go
@@ -167,8 +167,6 @@ var program = `// Copyright 2013 The Go Authors. All rights reserved.
 // DO NOT EDIT.
 // Generate with: go run gen.go{{if .Full}} -full{{end}} | gofmt >md5block.go
 
-// +build !amd64,!386,!arm
-
 package md5
 
 import (
@@ -204,7 +202,7 @@ func init() {
 	littleEndian = *(*[4]byte)(unsafe.Pointer(&x)) == y
 }
 
-func block(dig *digest, p []byte) {
+func blockGeneric(dig *digest, p []byte) {
 	a := dig.s[0]
 	b := dig.s[1]
 	c := dig.s[2]
---
+++ src/pkg/crypto/md5/md5block_generic.go
@@ -0,0 +1,9 @@
+// Copyright 2014 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// +build !amd64,!386,!arm
+
+package md5
+
+var block = blockGeneric
