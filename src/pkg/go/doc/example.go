2c0eb9052ce391a2f68cf12ce5ef70ca61358d96
Andrew Gerrand <adg@golang.org>
2014-02-18 15:53:22 +1100

    go/doc: document the conditions where examples are "playable"
    LGTM=r
    R=r
    CC=golang-codereviews
    https://golang.org/cl/64910043

--- src/pkg/go/doc/example.go
+++ src/pkg/go/doc/example.go
@@ -32,6 +32,17 @@ type Example struct {
 
 // Examples returns the examples found in the files, sorted by Name field.
 // The Order fields record the order in which the examples were encountered.
+//
+// Playable Examples must be in a package whose name ends in "_test".
+// An Example is "playable" (the Play field is non-nil) in either of these
+// circumstances:
+//   - The example function is self-contained: the function references only
+//     identifiers from other packages (or predeclared identifiers, such as
+//     "int") and the test file does not include a dot import.
+//   - The entire test file is the example: the file contains exactly one
+//     example function, zero test or benchmark functions, and at least one
+//     top-level function, type, variable, or constant declaration other
+//     than the example function.
 func Examples(files ...*ast.File) []*Example {
 	var list []*Example
 	for _, file := range files {
