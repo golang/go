f9b384f554143901ddf771214bfacf8041862f0c
Dave Cheney <dave@cheney.net>
2014-02-26 07:56:41 +1100

    syscall: fix solaris build
    Solaris does not define syscall.{Mmap,Munmap}. Move the Mmap test to a new file and exclude solaris as discussed.
    
    LGTM=aram
    R=aram, mikioh.mikioh, iant
    CC=golang-codereviews
    https://golang.org/cl/68720043

--- src/pkg/syscall/syscall_unix_test.go
+++ src/pkg/syscall/syscall_unix_test.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// +build freebsd dragonfly darwin linux netbsd openbsd solaris
+// +build darwin dragonfly freebsd linux netbsd openbsd solaris
 
 package syscall_test
 
@@ -74,16 +74,6 @@ func TestFcntlFlock(t *testing.T) {
 	}
 	if err := syscall.FcntlFlock(uintptr(fd), syscall.F_GETLK, &flock); err != nil {
 		t.Fatalf("FcntlFlock failed: %v", err)
-	}
-}
-
-func TestMmap(t *testing.T) {
-	b, err := syscall.Mmap(-1, 0, syscall.Getpagesize(), syscall.PROT_NONE, syscall.MAP_ANON|syscall.MAP_PRIVATE)
-	if err != nil {
-		t.Fatalf("Mmap: %v", err)
-	}
-	if err := syscall.Munmap(b); err != nil {
-		t.Fatalf("Munmap: %v", err)
 	}
 }
 
