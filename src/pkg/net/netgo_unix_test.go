f706d707b7f36d3465fdb2fb1489eb5ad3709d3d
Mikio Hara <mikioh.mikioh@gmail.com>
2014-03-03 07:09:28 +0900

    net: remove the dreg of solaris merge
    Also enables netgo test on solaris.
    
    LGTM=aram, jsing
    R=golang-codereviews, dave, aram, jsing
    CC=golang-codereviews
    https://golang.org/cl/70300043

--- src/pkg/net/netgo_unix_test.go
+++ src/pkg/net/netgo_unix_test.go
@@ -3,7 +3,7 @@
 // license that can be found in the LICENSE file.
 
 // +build !cgo netgo
-// +build darwin dragonfly freebsd linux netbsd openbsd
+// +build darwin dragonfly freebsd linux netbsd openbsd solaris
 
 package net
 
