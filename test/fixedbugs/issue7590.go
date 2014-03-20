df364dd9f0b377e9623f9e2b2c620557a02027dd
Chris Manghane <cmang@golang.org>
2014-03-20 11:46:45 -0700

    test: add extra test case for issue 7590
    LGTM=iant
    R=iant
    CC=golang-codereviews
    https://golang.org/cl/78040043

---
+++ test/fixedbugs/issue7590.go
@@ -0,0 +1,21 @@
+// compile
+
+// Copyright 2014 The Go Authors.  All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Issue 7590: gccgo incorrectly traverses nested composite literals.
+
+package p
+
+type S struct {
+	F int
+}
+
+var M = map[string]S{
+	"a": { F: 1 },
+}
+
+var P = M["a"]
+
+var F = P.F
