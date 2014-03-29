83ac901fb9298bf29059d52bc8b084c885586d1b
Mikio Hara <mikioh.mikioh@gmail.com>
2014-03-29 13:04:25 +0900

    net: tweak the ephemeral port range on dragonfly
    On DragonFly BSD, we adjust the ephemeral port range because
    unlike other BSD systems its default ephemeral port range
    doesn't conform to IANA recommendation as described in RFC 6355
    and is pretty narrow.
    
    On DragonFly BSD 3.6: default range [1024, 5000], high range [49152, 65535]
    On FreeBSD 10: default range [10000, 65535], high range [49152, 65535]
    On Linux 3.11: default range [32768, 61000]
    
    Fixes #7541.
    
    LGTM=iant
    R=jsing, gobot, iant
    CC=golang-codereviews
    https://golang.org/cl/80610044

--- src/pkg/net/sockopt_bsd.go
+++ src/pkg/net/sockopt_bsd.go
@@ -8,10 +8,23 @@ package net
 
 import (
 	"os"
+	"runtime"
 	"syscall"
 )
 
 func setDefaultSockopts(s, family, sotype int, ipv6only bool) error {
+	if runtime.GOOS == "dragonfly" && sotype != syscall.SOCK_RAW {
+		// On DragonFly BSD, we adjust the ephemeral port
+		// range because unlike other BSD systems its default
+		// port range doesn't conform to IANA recommendation
+		// as described in RFC 6355 and is pretty narrow.
+		switch family {
+		case syscall.AF_INET:
+			syscall.SetsockoptInt(s, syscall.IPPROTO_IP, syscall.IP_PORTRANGE, syscall.IP_PORTRANGE_HIGH)
+		case syscall.AF_INET6:
+			syscall.SetsockoptInt(s, syscall.IPPROTO_IPV6, syscall.IPV6_PORTRANGE, syscall.IPV6_PORTRANGE_HIGH)
+		}
+	}
 	if family == syscall.AF_INET6 && sotype != syscall.SOCK_RAW {
 		// Allow both IP versions even if the OS default
 		// is otherwise.  Note that some operating systems
