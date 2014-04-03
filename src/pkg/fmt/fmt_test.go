c274ff6761ed8bdaea7d99fcaeb1116fff3763dd
Shenghou Ma <minux.ma@gmail.com>
2014-04-03 16:11:03 -0400

    fmt: fix go syntax formatting of []byte(nil) Fixes #7639.
    LGTM=rsc
    R=r, adg, rsc
    CC=golang-codereviews
    https://golang.org/cl/81240043

--- src/pkg/fmt/fmt_test.go
+++ src/pkg/fmt/fmt_test.go
@@ -403,6 +403,8 @@ var fmtTests = []struct {
 	{"%#v", "foo", `"foo"`},
 	{"%#v", barray, `[5]fmt_test.renamedUint8{0x1, 0x2, 0x3, 0x4, 0x5}`},
 	{"%#v", bslice, `[]fmt_test.renamedUint8{0x1, 0x2, 0x3, 0x4, 0x5}`},
+	{"%#v", []byte(nil), "[]byte(nil)"},
+	{"%#v", []int32(nil), "[]int32(nil)"},
 
 	// slices with other formats
 	{"%#x", []int{1, 2, 15}, `[0x1 0x2 0xf]`},
