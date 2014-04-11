7841d43e6fa65201d1e7626d556d12347dce3760
Dmitriy Vyukov <dvyukov@google.com>
2014-04-11 13:01:10 +0400

    net/http/httptest: add test for issue 7264 The test fails now with -race, so it's disabled. The intention is that the fix for issue 7264 will also modify this test the same way and enable it. Reporduce with 'go test -race -issue7264 -cpu=4'. Update #7264
    LGTM=bradfitz
    R=golang-codereviews, bradfitz
    CC=golang-codereviews
    https://golang.org/cl/86770043

--- src/pkg/net/http/httptest/server_test.go
+++ src/pkg/net/http/httptest/server_test.go
@@ -5,9 +5,11 @@
 package httptest
 
 import (
+	"flag"
 	"io/ioutil"
 	"net/http"
 	"testing"
+	"time"
 )
 
 func TestServer(t *testing.T) {
@@ -25,5 +27,28 @@ func TestServer(t *testing.T) {
 	}
 	if string(got) != "hello" {
 		t.Errorf("got %q, want hello", string(got))
+	}
+}
+
+var testIssue7264 = flag.Bool("issue7264", false, "enable failing test for issue 7264")
+
+func TestIssue7264(t *testing.T) {
+	if !*testIssue7264 {
+		t.Skip("skipping failing test for issue 7264")
+	}
+	for i := 0; i < 1000; i++ {
+		func() {
+			ts := NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {}))
+			defer ts.Close()
+			tr := &http.Transport{
+				ResponseHeaderTimeout: time.Nanosecond,
+			}
+			defer tr.CloseIdleConnections()
+			c := &http.Client{Transport: tr}
+			res, err := c.Get(ts.URL)
+			if err == nil {
+				res.Body.Close()
+			}
+		}()
 	}
 }
