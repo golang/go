44f96d4488fb6f11e7b1a3b7197fd47267e00dd9
Russ Cox <rsc@golang.org>
2014-04-15 20:06:08 -0400

    addr2line, objdump: write doc comments
    LGTM=r
    R=r
    CC=golang-codereviews
    https://golang.org/cl/88050046

--- src/cmd/addr2line/main.go
+++ src/cmd/addr2line/main.go
@@ -2,8 +2,19 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// addr2line simulation - only enough to make pprof work on Macs
-
+// Addr2line is a minimal simulation of the GNU addr2line tool,
+// just enough to support pprof.
+//
+// Usage:
+//	go tool addr2line binary
+//
+// Addr2line reads hexadecimal addresses, one per line and without a 0x prefix,
+// from standard input. For each input address, addr2line prints two output lines,
+// first the name of the function containing the address and second the file:line
+// of the source code corresponding to that address.
+//
+// This tool is intended for use only by pprof; its interface may change or
+// it may be deleted entirely in future releases.
 package main
 
 import (
