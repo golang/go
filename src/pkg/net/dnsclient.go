a1f335e6dc927ab7cd914f21fff15daba2886d1e
Brad Fitzpatrick <bradfitz@golang.org>
2014-04-17 13:28:40 -0700

    net: fix probabilities in DNS SRV shuffleByWeight
    Patch from msolo. Just moving it to a CL.
    The test fails before and passes with the fix.
    
    Fixes #7098
    
    LGTM=msolo, rsc
    R=rsc, iant, msolo
    CC=golang-codereviews
    https://golang.org/cl/88900044

--- src/pkg/net/dnsclient.go
+++ src/pkg/net/dnsclient.go
@@ -191,10 +191,10 @@ func (addrs byPriorityWeight) shuffleByWeight() {
 	}
 	for sum > 0 && len(addrs) > 1 {
 		s := 0
-		n := rand.Intn(sum + 1)
+		n := rand.Intn(sum)
 		for i := range addrs {
 			s += int(addrs[i].Weight)
-			if s >= n {
+			if s > n {
 				if i > 0 {
 					t := addrs[i]
 					copy(addrs[1:i+1], addrs[0:i])
