e6020f72295a58c1483b87275941562642137fcc
Josh Bleecher Snyder <josharian@gmail.com>
2014-05-05 17:42:00 -0700

    go.tools/godoc/static: fix import path doc typo
    LGTM=bradfitz
    R=golang-codereviews, bradfitz
    CC=golang-codereviews
    https://golang.org/cl/98970043

--- godoc/static/doc.go
+++ godoc/static/doc.go
@@ -4,5 +4,5 @@
 
 // Package static exports a map of static file content that supports the godoc
 // user interface. The map should be used with the mapfs package, see
-// code.google.com/p/godoc/vfs/mapfs.
+// code.google.com/p/go.tools/godoc/vfs/mapfs.
 package static
