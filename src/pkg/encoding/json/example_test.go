1c2cc125fb2baad78167ee543f1d96aea9135734
Stephen McQuay <stephen@mcquay.me>
2014-05-08 16:52:36 +1000

    encoding/json: add example for Indent, clarify the docs.
    There was confusion in the behavior of json.Indent; This change
    attempts to clarify the behavior by providing a bit more verbiage
    to the documentation as well as provide an example function.
    
    Fixes #7821.
    
    LGTM=robert.hencke, adg
    R=golang-codereviews, minux.ma, bradfitz, aram, robert.hencke, r, adg
    CC=golang-codereviews
    https://golang.org/cl/97840044

--- src/pkg/encoding/json/example_test.go
+++ src/pkg/encoding/json/example_test.go
@@ -5,6 +5,7 @@
 package json_test
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"io"
@@ -126,4 +127,35 @@ func ExampleRawMessage() {
 	// Output:
 	// YCbCr &{255 0 -10}
 	// RGB &{98 218 255}
+}
+
+func ExampleIndent() {
+	type Road struct {
+		Name   string
+		Number int
+	}
+	roads := []Road{
+		{"Diamond Fork", 29},
+		{"Sheep Creek", 51},
+	}
+
+	b, err := json.Marshal(roads)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var out bytes.Buffer
+	json.Indent(&out, b, "=", "\t")
+	out.WriteTo(os.Stdout)
+	// Output:
+	// [
+	// =	{
+	// =		"Name": "Diamond Fork",
+	// =		"Number": 29
+	// =	},
+	// =	{
+	// =		"Name": "Sheep Creek",
+	// =		"Number": 51
+	// =	}
+	// =]
 }
