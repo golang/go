4f9862e351ccb45198c07dde56ffe0137b0c4b7c
Russ Cox <rsc@golang.org>
2014-05-21 17:12:06 -0400

    test/float_lit2.go: rewrite to test values near boundaries
    Add larger comment explaining testing methodology,
    and derive tests arithmetically.
    
    (These tests are checking rounding again; the derived
    tests they replace were checking exact values.)
    
    LGTM=r, gri
    R=gri, r
    CC=golang-codereviews
    https://golang.org/cl/100660044

--- test/float_lit3.go
+++ test/float_lit3.go
@@ -8,21 +8,41 @@
 
 package main
 
+// See float_lit2.go for motivation for these values.
+const (
+	two24   = 1.0 * (1 << 24)
+	two53   = 1.0 * (1 << 53)
+	two64   = 1.0 * (1 << 64)
+	two128  = two64 * two64
+	two256  = two128 * two128
+	two512  = two256 * two256
+	two768  = two512 * two256
+	two1024 = two512 * two512
+
+	ulp32 = two128 / two24
+	max32 = two128 - ulp32
+
+	ulp64 = two1024 / two53
+	max64 = two1024 - ulp64
+)
+
 var x = []interface{}{
-	float32(-340282356779733661637539395458142568448), // ERROR "constant -3\.40282e\+38 overflows float32"
-	float32(-340282356779733661637539395458142568447),
-	float32(-340282326356119256160033759537265639424),
-	float32(340282326356119256160033759537265639424),
-	float32(340282356779733661637539395458142568447),
-	float32(340282356779733661637539395458142568448), // ERROR "constant 3\.40282e\+38 overflows float32"
-	-1e1000, // ERROR "constant -1\.00000e\+1000 overflows float64"
-	float64(-1.797693134862315907937289714053e+308), // ERROR "constant -1\.79769e\+308 overflows float64"
-	float64(-1.797693134862315807937289714053e+308),
-	float64(-1.797693134862315708145274237317e+308),
-	float64(-1.797693134862315608353258760581e+308),
-	float64(1.797693134862315608353258760581e+308),
-	float64(1.797693134862315708145274237317e+308),
-	float64(1.797693134862315807937289714053e+308),
-	float64(1.797693134862315907937289714053e+308), // ERROR "constant 1\.79769e\+308 overflows float64"
-	1e1000, // ERROR "constant 1\.00000e\+1000 overflows float64"
+	float32(max32 + ulp32/2 - 1),             // ok
+	float32(max32 + ulp32/2 - two128/two256), // ok
+	float32(max32 + ulp32/2),                 // ERROR "constant 3\.40282e\+38 overflows float32"
+
+	float32(-max32 - ulp32/2 + 1),             // ok
+	float32(-max32 - ulp32/2 + two128/two256), // ok
+	float32(-max32 - ulp32/2),                 // ERROR "constant -3\.40282e\+38 overflows float32"
+
+	// If the compiler's internal floating point representation
+	// is shorter than 1024 bits, it cannot distinguish max64+ulp64/2-1 and max64+ulp64/2.
+	// gc uses fewer than 1024 bits, so allow it to print the overflow error for the -1 case.
+	float64(max64 + ulp64/2 - two1024/two256), // ok
+	float64(max64 + ulp64/2 - 1),              // GC_ERROR "constant 1\.79769e\+308 overflows float64"
+	float64(max64 + ulp64/2),                  // ERROR "constant 1\.79769e\+308 overflows float64"
+
+	float64(-max64 - ulp64/2 + two1024/two256), // ok
+	float64(-max64 - ulp64/2 + 1),              // GC_ERROR "constant -1\.79769e\+308 overflows float64"
+	float64(-max64 - ulp64/2),                  // ERROR "constant -1\.79769e\+308 overflows float64"
 }
