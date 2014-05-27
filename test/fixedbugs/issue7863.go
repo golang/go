bd401baef2349e41d99280557bd5d709be78c894
Brad Fitzpatrick <bradfitz@golang.org>
2014-05-27 16:01:43 -0700

    test: add test for fixed issue 7863
    Fixes #7863
    
    LGTM=rsc
    R=rsc, ruiu
    CC=golang-codereviews
    https://golang.org/cl/98610045

---
+++ test/fixedbugs/issue7863.go
@@ -0,0 +1,17 @@
+// run
+
+// Copyright 2014 The Go Authors.  All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+import "time"
+
+func main() {
+	now := time.Now()
+	f := now.Unix
+	if now.Unix() != f() {
+		println("BUG: ", now.Unix(), "!=", f())
+	}
+}
