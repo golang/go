a9035ede1b7f705f7cd73c7de51d54f6119b123b
Robert Griesemer <gri@golang.org>
2014-06-11 09:09:37 -0700

    math: implement Nextafter32
    Provide Nextafter64 as alias to Nextafter.
    For submission after the 1.3 release.
    
    Fixes #8117.
    
    LGTM=adonovan
    R=adonovan
    CC=golang-codereviews
    https://golang.org/cl/101750048

--- src/pkg/math/nextafter.go
+++ src/pkg/math/nextafter.go
@@ -4,13 +4,33 @@
 
 package math
 
-// Nextafter returns the next representable value after x towards y.
-// If x == y, then x is returned.
-//
-// Special cases are:
-//      Nextafter(NaN, y) = NaN
-//      Nextafter(x, NaN) = NaN
-func Nextafter(x, y float64) (r float64) {
+// Nextafter32 returns the next representable float32 value after x towards y.
+// Special cases:
+//	Nextafter32(x, x)   = x
+//      Nextafter32(NaN, y) = NaN
+//      Nextafter32(x, NaN) = NaN
+func Nextafter32(x, y float32) (r float32) {
+	switch {
+	case IsNaN(float64(x)) || IsNaN(float64(y)): // special case
+		r = float32(NaN())
+	case x == y:
+		r = x
+	case x == 0:
+		r = float32(Copysign(float64(Float32frombits(1)), float64(y)))
+	case (y > x) == (x > 0):
+		r = Float32frombits(Float32bits(x) + 1)
+	default:
+		r = Float32frombits(Float32bits(x) - 1)
+	}
+	return
+}
+
+// Nextafter64 returns the next representable float64 value after x towards y.
+// Special cases:
+//	Nextafter64(x, x)   = x
+//      Nextafter64(NaN, y) = NaN
+//      Nextafter64(x, NaN) = NaN
+func Nextafter64(x, y float64) (r float64) {
 	switch {
 	case IsNaN(x) || IsNaN(y): // special case
 		r = NaN()
@@ -24,4 +44,10 @@ func Nextafter(x, y float64) (r float64) {
 		r = Float64frombits(Float64bits(x) - 1)
 	}
 	return
+}
+
+// Nextafter is the same as Nextafter64.
+// It is provided for backward-compatibility only.
+func Nextafter(x, y float64) float64 {
+	return Nextafter64(x, y)
 }
