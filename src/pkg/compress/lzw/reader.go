9f08c5c3830d42de9bf18b80b79516c61e1ea360
Nigel Tao <nigeltao@golang.org>
2014-06-13 17:44:29 +1000

    compress/lzw: add commentary that TIFF's LZW differs from the standard algorithm.
    See https://golang.org/cl/105750045/ for an implementation of
    TIFF's LZW.
    
    LGTM=r
    R=r
    CC=golang-codereviews
    https://golang.org/cl/102940043

--- src/pkg/compress/lzw/reader.go
+++ src/pkg/compress/lzw/reader.go
@@ -6,12 +6,16 @@
 // described in T. A. Welch, ``A Technique for High-Performance Data
 // Compression'', Computer, 17(6) (June 1984), pp 8-19.
 //
-// In particular, it implements LZW as used by the GIF, TIFF and PDF file
+// In particular, it implements LZW as used by the GIF and PDF file
 // formats, which means variable-width codes up to 12 bits and the first
 // two non-literal codes are a clear code and an EOF code.
+//
+// The TIFF file format uses a similar but incompatible version of the LZW
+// algorithm. See the code.google.com/p/go.image/tiff/lzw package for an
+// implementation.
 package lzw
 
-// TODO(nigeltao): check that TIFF and PDF use LZW in the same way as GIF,
+// TODO(nigeltao): check that PDF uses LZW in the same way as GIF,
 // modulo LSB/MSB packing order.
 
 import (
