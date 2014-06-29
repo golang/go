3e692becfe69a7b80dbf4af983eb620f65549f02
Dave Cheney <dave@cheney.net>
2014-06-29 20:34:35 +1000

    test/fixedbugs: fix typo in comment
    Fix copy paste error pointed out by rsc, https://golang.org/cl/107290043/diff/60001/test/fixedbugs/issue8074.go#newcode7
    
    LGTM=ruiu, r
    R=golang-codereviews, ruiu, r
    CC=golang-codereviews
    https://golang.org/cl/106210047

--- test/fixedbugs/issue8074.go
+++ test/fixedbugs/issue8074.go
@@ -4,7 +4,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// issue 8073.
+// issue 8074.
 // was "cannot take the address of 1"
 
 package main
