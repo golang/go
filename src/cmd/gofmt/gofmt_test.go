138099ae96332d2a5a63888c96001286d7273907
Simon Whitehead <chemnova@gmail.com>
2014-07-01 09:32:03 -0700

    gofmt/main: Added removal of empty declaration groups.
    Fixes #7631.
    
    LGTM=gri
    R=golang-codereviews, bradfitz, gri
    CC=golang-codereviews
    https://golang.org/cl/101410046

--- src/cmd/gofmt/gofmt_test.go
+++ src/cmd/gofmt/gofmt_test.go
@@ -87,8 +87,9 @@ var tests = []struct {
 	{"testdata/stdin*.input", "-stdin"},
 	{"testdata/comments.input", ""},
 	{"testdata/import.input", ""},
-	{"testdata/crlf.input", ""},       // test case for issue 3961; see also TestCRLF
-	{"testdata/typeswitch.input", ""}, // test case for issue 4470
+	{"testdata/crlf.input", ""},        // test case for issue 3961; see also TestCRLF
+	{"testdata/typeswitch.input", ""},  // test case for issue 4470
+	{"testdata/emptydecl.input", "-s"}, // test case for issue 7631
 }
 
 func TestRewrite(t *testing.T) {
