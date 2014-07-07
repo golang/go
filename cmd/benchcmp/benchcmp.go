3e7bc48329b7469ef3b703ab2e9b32e470ae8566
Josh Bleecher Snyder <josharian@gmail.com>
2014-07-07 11:55:26 -0700

    go.tools/cmd/benchcmp: do not generate trailing whitespace
    text/tabwriter cells are tab-terminated, not tab-separated. This creates trailing whitespace. However, with left-aligned columns, it is ok not to treat the final element as a cell. Demo: http://play.golang.org/p/m_ajG8SSZe
    
    LGTM=gri
    R=golang-codereviews, gobot, gri
    CC=golang-codereviews, r
    https://golang.org/cl/103650043

--- cmd/benchcmp/benchcmp.go
+++ cmd/benchcmp/benchcmp.go
@@ -71,10 +71,10 @@ func main() {
 		}
 		if delta := cmp.DeltaNsOp(); !*changedOnly || delta.Changed() {
 			if !header {
-				fmt.Fprintf(w, "benchmark\told ns/op\tnew ns/op\tdelta\t\n")
+				fmt.Fprint(w, "benchmark\told ns/op\tnew ns/op\tdelta\n")
 				header = true
 			}
-			fmt.Fprintf(w, "%s\t%s\t%s\t%s\t\n", cmp.Name(), formatNs(cmp.Before.NsOp), formatNs(cmp.After.NsOp), delta.Percent())
+			fmt.Fprintf(w, "%s\t%s\t%s\t%s\n", cmp.Name(), formatNs(cmp.Before.NsOp), formatNs(cmp.After.NsOp), delta.Percent())
 		}
 	}
 
@@ -88,10 +88,10 @@ func main() {
 		}
 		if delta := cmp.DeltaMbS(); !*changedOnly || delta.Changed() {
 			if !header {
-				fmt.Fprintf(w, "\nbenchmark\told MB/s\tnew MB/s\tspeedup\t\n")
+				fmt.Fprint(w, "\nbenchmark\told MB/s\tnew MB/s\tspeedup\n")
 				header = true
 			}
-			fmt.Fprintf(w, "%s\t%.2f\t%.2f\t%s\t\n", cmp.Name(), cmp.Before.MbS, cmp.After.MbS, delta.Multiple())
+			fmt.Fprintf(w, "%s\t%.2f\t%.2f\t%s\n", cmp.Name(), cmp.Before.MbS, cmp.After.MbS, delta.Multiple())
 		}
 	}
 
@@ -105,10 +105,10 @@ func main() {
 		}
 		if delta := cmp.DeltaAllocsOp(); !*changedOnly || delta.Changed() {
 			if !header {
-				fmt.Fprintf(w, "\nbenchmark\told allocs\tnew allocs\tdelta\t\n")
+				fmt.Fprint(w, "\nbenchmark\told allocs\tnew allocs\tdelta\n")
 				header = true
 			}
-			fmt.Fprintf(w, "%s\t%d\t%d\t%s\t\n", cmp.Name(), cmp.Before.AllocsOp, cmp.After.AllocsOp, delta.Percent())
+			fmt.Fprintf(w, "%s\t%d\t%d\t%s\n", cmp.Name(), cmp.Before.AllocsOp, cmp.After.AllocsOp, delta.Percent())
 		}
 	}
 
@@ -122,10 +122,10 @@ func main() {
 		}
 		if delta := cmp.DeltaBOp(); !*changedOnly || delta.Changed() {
 			if !header {
-				fmt.Fprintf(w, "\nbenchmark\told bytes\tnew bytes\tdelta\t\n")
+				fmt.Fprint(w, "\nbenchmark\told bytes\tnew bytes\tdelta\n")
 				header = true
 			}
-			fmt.Fprintf(w, "%s\t%d\t%d\t%s\t\n", cmp.Name(), cmp.Before.BOp, cmp.After.BOp, cmp.DeltaBOp().Percent())
+			fmt.Fprintf(w, "%s\t%d\t%d\t%s\n", cmp.Name(), cmp.Before.BOp, cmp.After.BOp, cmp.DeltaBOp().Percent())
 		}
 	}
 }
