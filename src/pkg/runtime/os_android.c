c1c8c3c8c4194de357d1be925b2b70f6fc88b72e
David Crawshaw <david.crawshaw@zentus.com>
2014-07-07 07:35:39 -0400

    runtime: export _rt0 for android
    LGTM=iant, minux
    R=golang-codereviews, minux, iant
    CC=golang-codereviews
    https://golang.org/cl/109470043

---
+++ src/pkg/runtime/os_android.c
@@ -0,0 +1,16 @@
+// Copyright 2014 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+#include "runtime.h"
+#include "defs_GOOS_GOARCH.h"
+#include "os_GOOS.h"
+
+// Export the runtime entry point symbol.
+//
+// Used by the app package to start the Go runtime after loading
+// a shared library via JNI. See code.google.com/p/go.mobile/app.
+
+void _rt0_arm_linux1();
+#pragma cgo_export_static _rt0_arm_linux1
+#pragma cgo_export_dynamic _rt0_arm_linux1
