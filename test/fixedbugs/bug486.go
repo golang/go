f2b59a3483e3d7638b55f4e01a20edac75890759
Ian Lance Taylor <iant@golang.org>
2014-07-08 14:09:35 -0700

    test: add test for gccgo comment lexing failure
    http://gcc.gnu.org/PR61746
    
    http://code.google.com/p/gofrontend/issues/detail?id=35
    
    LGTM=crawshaw
    R=golang-codereviews, crawshaw
    CC=golang-codereviews
    https://golang.org/cl/111980043

---
+++ test/fixedbugs/bug486.go
@@ -0,0 +1,14 @@
+// compile
+
+// Copyright 2014 The Go Authors.  All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// The gccgo lexer had a bug handling nested comments.
+// http://gcc.gnu.org/PR61746
+// http://code.google.com/p/gofrontend/issues/detail?id=35
+
+package main
+
+/*// comment
+*/
