7a1e9c40c630db6de9b39a75415da4a9000f2787
David Crawshaw <david.crawshaw@zentus.com>
2014-07-08 13:42:14 -0400

    net: no AI_ALL on android
    LGTM=bradfitz
    R=golang-codereviews, bradfitz
    CC=golang-codereviews
    https://golang.org/cl/112800043

---
+++ src/pkg/net/cgo_android.go
@@ -0,0 +1,14 @@
+// Copyright 2014 The Go Authors.  All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// +build cgo,!netgo
+
+package net
+
+//#include <netdb.h>
+import "C"
+
+func cgoAddrInfoFlags() C.int {
+	return C.AI_CANONNAME
+}
