496afd8307a559ac47bf369137ed22dafdb53288
Josh Bleecher Snyder <josharian@gmail.com>
2014-07-18 09:58:54 -0700

    go/ast: fix typo in comment
    LGTM=bradfitz
    R=golang-codereviews, bradfitz
    CC=golang-codereviews
    https://golang.org/cl/111330043

--- src/pkg/go/ast/scope.go
+++ src/pkg/go/ast/scope.go
@@ -80,7 +80,7 @@ type Object struct {
 	Name string      // declared name
 	Decl interface{} // corresponding Field, XxxSpec, FuncDecl, LabeledStmt, AssignStmt, Scope; or nil
 	Data interface{} // object-specific data; or nil
-	Type interface{} // place holder for type information; may be nil
+	Type interface{} // placeholder for type information; may be nil
 }
 
 // NewObj creates a new object of a given kind and name.
