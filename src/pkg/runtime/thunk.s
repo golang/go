5462b9a5dd172799582e1d71620800fadece4dac
Dmitriy Vyukov <dvyukov@google.com>
2014-08-24 12:41:23 +0400

    runtime: convert synchronous semaphores to Go
    LGTM=rsc
    R=golang-codereviews, khr, rsc
    CC=golang-codereviews, rlh
    https://golang.org/cl/130340043

---
+++ src/pkg/runtime/thunk.s
@@ -0,0 +1,21 @@
+// Copyright 2014 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// This file exposes various internal runtime functions to other packages in std lib.
+
+#include "zasm_GOOS_GOARCH.h"
+#include "../../cmd/ld/textflag.h"
+
+#ifdef GOARCH_arm
+#define JMP B
+#endif
+
+TEXT sync·runtime_Syncsemacquire(SB),NOSPLIT,$0-0
+	JMP	runtime·syncsemacquire(SB)
+
+TEXT sync·runtime_Syncsemrelease(SB),NOSPLIT,$0-0
+	JMP	runtime·syncsemrelease(SB)
+
+TEXT sync·runtime_Syncsemcheck(SB),NOSPLIT,$0-0
+	JMP	runtime·syncsemcheck(SB)
