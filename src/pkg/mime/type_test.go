b155e79f23d66d70f761d73cb7478c5fb59c915c
Brad Fitzpatrick <bradfitz@golang.org>
2014-08-28 11:07:46 -0700

    mime: style, perf, and test updates to case-insensitive lookups
    Only grab the lock once, don't allocate, add more tests.
    
    LGTM=ruiu
    R=ruiu, josharian
    CC=golang-codereviews
    https://golang.org/cl/139780043

--- src/pkg/mime/type_test.go
+++ src/pkg/mime/type_test.go
@@ -19,17 +19,36 @@ func TestTypeByExtension(t *testing.T) {
 	}
 }
 
-func TestCustomExtension(t *testing.T) {
-	custom := "test/test; charset=iso-8859-1"
-	if error := AddExtensionType(".tesT", custom); error != nil {
-		t.Fatalf("error %s for AddExtension(%s)", error, custom)
+func TestTypeByExtensionCase(t *testing.T) {
+	const custom = "test/test; charset=iso-8859-1"
+	const caps = "test/test; WAS=ALLCAPS"
+	if err := AddExtensionType(".TEST", caps); err != nil {
+		t.Fatalf("error %s for AddExtension(%s)", err, custom)
 	}
-	// test with same capitalization
-	if registered := TypeByExtension(".tesT"); registered != custom {
-		t.Fatalf("registered %s instead of %s", registered, custom)
+	if err := AddExtensionType(".tesT", custom); err != nil {
+		t.Fatalf("error %s for AddExtension(%s)", err, custom)
 	}
-	// test with different capitalization
-	if registered := TypeByExtension(".Test"); registered != custom {
-		t.Fatalf("registered %s instead of %s", registered, custom)
+
+	// case-sensitive lookup
+	if got := TypeByExtension(".tesT"); got != custom {
+		t.Fatalf("for .tesT, got %q; want %q", got, custom)
+	}
+	if got := TypeByExtension(".TEST"); got != caps {
+		t.Fatalf("for .TEST, got %q; want %s", got, caps)
+	}
+
+	// case-insensitive
+	if got := TypeByExtension(".TesT"); got != custom {
+		t.Fatalf("for .TesT, got %q; want %q", got, custom)
+	}
+}
+
+func TestLookupMallocs(t *testing.T) {
+	n := testing.AllocsPerRun(10000, func() {
+		TypeByExtension(".html")
+		TypeByExtension(".HtML")
+	})
+	if n > 0 {
+		t.Errorf("allocs = %v; want 0", n)
 	}
 }
