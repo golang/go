6317ef59ce96a91c99e507cb51497da700e78a0c
Kelsey Hightower <kelsey.hightower@gmail.com>
2014-08-29 22:19:30 -0700

    net/http: add BasicAuth method to *http.Request
    The net/http package supports setting the HTTP Authorization header
    using the Basic Authentication Scheme as defined in RFC 2617, but does
    not provide support for extracting the username and password from an
    authenticated request using the Basic Authentication Scheme.
    
    Add BasicAuth method to *http.Request that returns the username and
    password from authenticated requests using the Basic Authentication
    Scheme.
    
    Fixes #6779.
    
    LGTM=bradfitz
    R=golang-codereviews, josharian, bradfitz, alberto.garcia.hierro, blakesgentry
    CC=golang-codereviews
    https://golang.org/cl/76540043

--- src/pkg/net/http/request.go
+++ src/pkg/net/http/request.go
@@ -10,6 +10,7 @@ import (
 	"bufio"
 	"bytes"
 	"crypto/tls"
+	"encoding/base64"
 	"errors"
 	"fmt"
 	"io"
@@ -519,6 +520,35 @@ func NewRequest(method, urlStr string, body io.Reader) (*Request, error) {
 	}
 
 	return req, nil
+}
+
+// BasicAuth returns the username and password provided in the request's
+// Authorization header, if the request uses HTTP Basic Authentication.
+// See RFC 2617, Section 2.
+func (r *Request) BasicAuth() (username, password string, ok bool) {
+	auth := r.Header.Get("Authorization")
+	if auth == "" {
+		return
+	}
+	return parseBasicAuth(auth)
+}
+
+// parseBasicAuth parses an HTTP Basic Authentication string.
+// "Basic QWxhZGRpbjpvcGVuIHNlc2FtZQ==" returns ("Aladdin", "open sesame", true).
+func parseBasicAuth(auth string) (username, password string, ok bool) {
+	if !strings.HasPrefix(auth, "Basic ") {
+		return
+	}
+	c, err := base64.StdEncoding.DecodeString(strings.TrimPrefix(auth, "Basic "))
+	if err != nil {
+		return
+	}
+	cs := string(c)
+	s := strings.IndexByte(cs, ':')
+	if s < 0 {
+		return
+	}
+	return cs[:s], cs[s+1:], true
 }
 
 // SetBasicAuth sets the request's Authorization header to use HTTP
