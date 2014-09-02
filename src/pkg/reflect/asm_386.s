01f6c8968639fd076920254abbce8cef05b20c31
Keith Randall <khr@golang.org>
2014-09-02 10:07:02 -0700

    runtime: move reflect trampolines into thunk file.
    LGTM=bradfitz
    R=golang-codereviews, bradfitz
    CC=golang-codereviews
    https://golang.org/cl/133460043

--- src/pkg/reflect/asm_386.s
+++ src/pkg/reflect/asm_386.s
@@ -25,32 +25,3 @@ TEXT ·methodValueCall(SB),(NOSPLIT|WRAPPER),$8
 	MOVL	CX, 4(SP)
 	CALL	·callMethod(SB)
 	RET
-
-// Stubs to give reflect package access to runtime services
-// TODO: should probably be done another way.
-TEXT ·makemap(SB),NOSPLIT,$0-0
-	JMP	runtime·reflect_makemap(SB)
-TEXT ·mapaccess(SB),NOSPLIT,$0-0
-	JMP	runtime·reflect_mapaccess(SB)
-TEXT ·mapassign(SB),NOSPLIT,$0-0
-	JMP	runtime·reflect_mapassign(SB)
-TEXT ·mapdelete(SB),NOSPLIT,$0-0
-	JMP	runtime·reflect_mapdelete(SB)
-TEXT ·mapiterinit(SB),NOSPLIT,$0-0
-	JMP	runtime·reflect_mapiterinit(SB)
-TEXT ·mapiterkey(SB),NOSPLIT,$0-0
-	JMP	runtime·reflect_mapiterkey(SB)
-TEXT ·mapiternext(SB),NOSPLIT,$0-0
-	JMP	runtime·reflect_mapiternext(SB)
-TEXT ·maplen(SB),NOSPLIT,$0-0
-	JMP	runtime·reflect_maplen(SB)
-TEXT ·ismapkey(SB),NOSPLIT,$0-0
-	JMP	runtime·reflect_ismapkey(SB)
-TEXT ·ifaceE2I(SB),NOSPLIT,$0-0
-	JMP	runtime·reflect_ifaceE2I(SB)
-TEXT ·unsafe_New(SB),NOSPLIT,$0-0
-	JMP	runtime·newobject(SB)
-TEXT ·unsafe_NewArray(SB),NOSPLIT,$0-0
-	JMP	runtime·newarray(SB)
-TEXT ·makechan(SB),NOSPLIT,$0-0
-	JMP	runtime·makechan(SB)
--- src/pkg/reflect/asm_arm.s
+++ src/pkg/reflect/asm_arm.s
@@ -25,32 +25,3 @@ TEXT ·methodValueCall(SB),(NOSPLIT|WRAPPER),$8
 	MOVW	R1, 8(R13)
 	BL	·callMethod(SB)
 	RET
-
-// Stubs to give reflect package access to runtime services
-// TODO: should probably be done another way.
-TEXT ·makemap(SB),NOSPLIT,$-4-0
-	B	runtime·reflect_makemap(SB)
-TEXT ·mapaccess(SB),NOSPLIT,$-4-0
-	B	runtime·reflect_mapaccess(SB)
-TEXT ·mapassign(SB),NOSPLIT,$-4-0
-	B	runtime·reflect_mapassign(SB)
-TEXT ·mapdelete(SB),NOSPLIT,$-4-0
-	B	runtime·reflect_mapdelete(SB)
-TEXT ·mapiterinit(SB),NOSPLIT,$-4-0
-	B	runtime·reflect_mapiterinit(SB)
-TEXT ·mapiterkey(SB),NOSPLIT,$-4-0
-	B	runtime·reflect_mapiterkey(SB)
-TEXT ·mapiternext(SB),NOSPLIT,$-4-0
-	B	runtime·reflect_mapiternext(SB)
-TEXT ·maplen(SB),NOSPLIT,$-4-0
-	B	runtime·reflect_maplen(SB)
-TEXT ·ismapkey(SB),NOSPLIT,$-4-0
-	B	runtime·reflect_ismapkey(SB)
-TEXT ·ifaceE2I(SB),NOSPLIT,$0-0
-	B	runtime·reflect_ifaceE2I(SB)
-TEXT ·unsafe_New(SB),NOSPLIT,$0-0
-	B	runtime·newobject(SB)
-TEXT ·unsafe_NewArray(SB),NOSPLIT,$0-0
-	B	runtime·newarray(SB)
-TEXT ·makechan(SB),NOSPLIT,$0-0
-	B	runtime·makechan(SB)
