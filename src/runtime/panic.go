15274e5c9bd5393f67a77200f0669ab00f2ab0ed
Keith Randall <khr@golang.org>
2014-09-16 12:50:05 -0700

    runtime: make it clear that Goexit cannot be recover'd.
    LGTM=r
    R=r, bradfitz, khr
    CC=golang-codereviews
    https://golang.org/cl/136660044

--- src/runtime/panic.go
+++ src/runtime/panic.go
@@ -238,7 +238,8 @@ func deferreturn(arg0 uintptr) {
 }
 
 // Goexit terminates the goroutine that calls it.  No other goroutine is affected.
-// Goexit runs all deferred calls before terminating the goroutine.
+// Goexit runs all deferred calls before terminating the goroutine.  Because Goexit
+// is not panic, however, any recover calls in those deferred functions will return nil.
 //
 // Calling Goexit from the main goroutine terminates that goroutine
 // without func main returning. Since func main has not returned,
