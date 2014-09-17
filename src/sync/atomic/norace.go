98a1e207e29359dba86769877021838bb77b12c3
Dmitriy Vyukov <dvyukov@google.com>
2014-09-16 19:54:26 -0700

    sync/atomic: add Value A Value provides an atomic load and store of a consistently typed value. It's intended to be used with copy-on-write idiom (see the example).
    Performance:
    BenchmarkValueRead	50000000	        21.7 ns/op
    BenchmarkValueRead-2	200000000	         8.63 ns/op
    BenchmarkValueRead-4	300000000	         4.33 ns/op
    
    TBR=rsc
    R=golang-codereviews
    CC=golang-codereviews
    https://golang.org/cl/136710045

---
+++ src/sync/atomic/norace.go
@@ -0,0 +1,17 @@
+// Copyright 2014 The Go Authors.  All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// +build !race
+
+package atomic
+
+import "unsafe"
+
+const raceenabled = false
+
+func raceAcquire(addr unsafe.Pointer) {
+}
+
+func raceReleaseMerge(addr unsafe.Pointer) {
+}
