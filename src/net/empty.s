60f37faea12507aac6fe15eb9c0bc14db104f76d
Russ Cox <rsc@golang.org>
2014-09-24 19:09:43 -0400

    net: only "build" empty.s in non-cgo mode
    In cgo mode it gets passed to gcc, and on ARM
    it appears that gcc does not support // comments.
    
    TBR=iant
    CC=golang-codereviews
    https://golang.org/cl/142640043

--- src/net/empty.s
+++ src/net/empty.s
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
+// +build !cgo
+
 // This file is required to prevent compiler errors
 // when the package built with CGO_ENABLED=0.
 // Otherwise the compiler says:
