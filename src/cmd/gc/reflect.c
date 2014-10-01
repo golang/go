637025a6add4c51632a19d6732c97b3c0719ecbb
Russ Cox <rsc@golang.org>
2014-10-01 17:38:09 -0400

    [dev.garbage] cmd/gc: never generate BitsMultiWord
    LGTM=rlh
    R=rlh, minux
    CC=golang-codereviews
    https://golang.org/cl/151940043

--- src/cmd/gc/reflect.c
+++ src/cmd/gc/reflect.c
@@ -1506,11 +1506,9 @@ gengcprog1(ProgGen *g, Type *t, vlong *xoffset)
 		*xoffset += t->width;
 		break;
 	case TINTER:
-		proggendata(g, BitsMultiWord);
-		if(isnilinter(t))
-			proggendata(g, BitsEface);
-		else
-			proggendata(g, BitsIface);
+		// Assuming IfacePointerOnly=1.
+		proggendata(g, BitsPointer);
+		proggendata(g, BitsPointer);
 		*xoffset += t->width;
 		break;
 	case TARRAY:
