3a68be9fcdde091227dcf6d8c2440d738baba294
Robert Griesemer <gri@golang.org>
2014-10-01 16:44:52 -0700

    time: A Timer must be properly created before use (documentation).
    Fixes #8776.
    
    LGTM=bradfitz
    R=rsc, bradfitz
    CC=golang-codereviews
    https://golang.org/cl/145710043

--- src/time/sleep.go
+++ src/time/sleep.go
@@ -43,6 +43,7 @@ func stopTimer(*runtimeTimer) bool
 // The Timer type represents a single event.
 // When the Timer expires, the current time will be sent on C,
 // unless the Timer was created by AfterFunc.
+// A Timer must be created with NewTimer or AfterFunc.
 type Timer struct {
 	C <-chan Time
 	r runtimeTimer
