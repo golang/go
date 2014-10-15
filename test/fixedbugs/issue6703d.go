b3d4e5f5af1c79d8c1e58438f203c0d09acb7ca3
Chris Manghane <cmang@golang.org>
2014-10-14 19:12:10 -0700

    cmd/gc: check for initialization cycles in method values
    Fixes #7960.
    
    LGTM=rsc
    R=rsc
    CC=golang-codereviews, gri
    https://golang.org/cl/159800045

---
+++ test/fixedbugs/issue6703d.go
@@ -0,0 +1,18 @@
+// errorcheck
+
+// Copyright 2014 The Go Authors.  All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Check for cycles in a method expression call.
+
+package methexprcall
+
+type T int
+
+func (T) m() int {
+	_ = x
+	return 0
+}
+
+var x = T.m(0) // ERROR "initialization loop|depends upon itself"
