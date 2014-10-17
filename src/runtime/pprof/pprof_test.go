fb1ff4bfafe52359e3b0920752deb4250fe6c998
Russ Cox <rsc@golang.org>
2014-10-17 11:23:15 -0400

    runtime/pprof: fix test
    gogo called from GC is okay
    for the same reasons that
    gogo called from System or ExternalCode is okay.
    All three are fake stack traces.
    
    Fixes #8408.
    
    LGTM=dvyukov, r
    R=r, dvyukov
    CC=golang-codereviews
    https://golang.org/cl/152580043

--- src/runtime/pprof/pprof_test.go
+++ src/runtime/pprof/pprof_test.go
@@ -249,7 +249,7 @@ func TestGoroutineSwitch(t *testing.T) {
 			// exists to record a PC without a traceback. Those are okay.
 			if len(stk) == 2 {
 				f := runtime.FuncForPC(stk[1])
-				if f != nil && (f.Name() == "System" || f.Name() == "ExternalCode") {
+				if f != nil && (f.Name() == "System" || f.Name() == "ExternalCode" || f.Name() == "GC") {
 					return
 				}
 			}
