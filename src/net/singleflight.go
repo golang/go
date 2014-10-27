d2d87f4da4afe4f3bdb79967876c55f713e7b3c8
Ian Lance Taylor <iant@golang.org>
2014-10-27 08:46:18 -0700

    net: if a DNS lookup times out, forget that it is in flight
    Before this CL, if the system resolver does a very slow DNS
    lookup for a particular host, all subsequent requests for that
    host will hang waiting for that lookup to complete.  That is
    more or less expected when Dial is called with no deadline.
    When Dial has a deadline, though, we can accumulate a large
    number of goroutines waiting for that slow DNS lookup.  Try to
    avoid this problem by restarting the DNS lookup when it is
    redone after a deadline is passed.
    
    This CL also avoids creating an extra goroutine purely to
    handle the deadline.
    
    No test because we would have to simulate a slow DNS lookup
    followed by a fast DNS lookup.
    
    Fixes #8602.
    
    LGTM=bradfitz
    R=bradfitz, mikioh.mikioh
    CC=golang-codereviews, r, rsc
    https://golang.org/cl/154610044

--- src/net/singleflight.go
+++ src/net/singleflight.go
@@ -8,10 +8,18 @@ import "sync"
 
 // call is an in-flight or completed singleflight.Do call
 type call struct {
-	wg   sync.WaitGroup
-	val  interface{}
-	err  error
-	dups int
+	wg sync.WaitGroup
+
+	// These fields are written once before the WaitGroup is done
+	// and are only read after the WaitGroup is done.
+	val interface{}
+	err error
+
+	// These fields are read and written with the singleflight
+	// mutex held before the WaitGroup is done, and are read but
+	// not written after the WaitGroup is done.
+	dups  int
+	chans []chan<- singleflightResult
 }
 
 // singleflight represents a class of work and forms a namespace in
@@ -19,6 +27,14 @@ type call struct {
 type singleflight struct {
 	mu sync.Mutex       // protects m
 	m  map[string]*call // lazily initialized
+}
+
+// singleflightResult holds the results of Do, so they can be passed
+// on a channel.
+type singleflightResult struct {
+	v      interface{}
+	err    error
+	shared bool
 }
 
 // Do executes and returns the results of the given function, making
@@ -42,12 +58,52 @@ func (g *singleflight) Do(key string, fn func() (interface{}, error)) (v interfa
 	g.m[key] = c
 	g.mu.Unlock()
 
+	g.doCall(c, key, fn)
+	return c.val, c.err, c.dups > 0
+}
+
+// DoChan is like Do but returns a channel that will receive the
+// results when they are ready.
+func (g *singleflight) DoChan(key string, fn func() (interface{}, error)) <-chan singleflightResult {
+	ch := make(chan singleflightResult, 1)
+	g.mu.Lock()
+	if g.m == nil {
+		g.m = make(map[string]*call)
+	}
+	if c, ok := g.m[key]; ok {
+		c.dups++
+		c.chans = append(c.chans, ch)
+		g.mu.Unlock()
+		return ch
+	}
+	c := &call{chans: []chan<- singleflightResult{ch}}
+	c.wg.Add(1)
+	g.m[key] = c
+	g.mu.Unlock()
+
+	go g.doCall(c, key, fn)
+
+	return ch
+}
+
+// doCall handles the single call for a key.
+func (g *singleflight) doCall(c *call, key string, fn func() (interface{}, error)) {
 	c.val, c.err = fn()
 	c.wg.Done()
 
 	g.mu.Lock()
 	delete(g.m, key)
+	for _, ch := range c.chans {
+		ch <- singleflightResult{c.val, c.err, c.dups > 0}
+	}
 	g.mu.Unlock()
+}
 
-	return c.val, c.err, c.dups > 0
+// Forget tells the singleflight to forget about a key.  Future calls
+// to Do for this key will call the function rather than waiting for
+// an earlier call to complete.
+func (g *singleflight) Forget(key string) {
+	g.mu.Lock()
+	delete(g.m, key)
+	g.mu.Unlock()
 }
