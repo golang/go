e607c09b4e188bfacb003ab70174c982545b36a3
Alan Donovan <adonovan@google.com>
2014-11-03 13:41:03 -0500

    misc/cgo/test: fix freebsd test failure by moving test to its own package.
    (The assertion depends on a per-package gensym counter whose
    value varies based on what else is in the package.)
    
    LGTM=khr
    R=khr, rsc
    CC=golang-codereviews
    https://golang.org/cl/169930043

--- misc/cgo/test/issue9026.go
+++ misc/cgo/test/issue9026.go
@@ -1,33 +1,9 @@
 package cgotest
 
-/*
-typedef struct {} git_merge_file_input;
-
-typedef struct {} git_merge_file_options;
-
-void git_merge_file(
-        git_merge_file_input *in,
-        git_merge_file_options *opts) {}
-*/
-import "C"
 import (
-	"fmt"
 	"testing"
+
+	"./issue9026"
 )
 
-func test9026(t *testing.T) {
-	var in C.git_merge_file_input
-	var opts *C.git_merge_file_options
-	C.git_merge_file(&in, opts)
-
-	// Test that the generated type names are deterministic.
-	// (Previously this would fail about 10% of the time.)
-	//
-	// Brittle: the assertion may fail spuriously when the algorithm
-	// changes, but should remain stable otherwise.
-	got := fmt.Sprintf("%T %T", in, opts)
-	want := "cgotest._Ctype_struct___12 *cgotest._Ctype_struct___13"
-	if got != want {
-		t.Errorf("Non-deterministic type names: got %s, want %s", got, want)
-	}
-}
+func test9026(t *testing.T) { issue9026.Test(t) }
