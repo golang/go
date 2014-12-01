7c1e33033ddc2370c468b0f3087d5a584dfd2c7e
Keith Randall <khr@golang.org>
2014-12-01 07:52:09 -0800

    reflect: Fix reflect.funcLayout.  The GC bitmap has two bits per pointer, not one.
    Fixes #9179
    
    LGTM=iant, rsc
    R=golang-codereviews, iant, rsc
    CC=golang-codereviews
    https://golang.org/cl/182160043

--- src/reflect/export_test.go
+++ src/reflect/export_test.go
@@ -17,3 +17,22 @@ func IsRO(v Value) bool {
 
 var ArrayOf = arrayOf
 var CallGC = &callGC
+
+const PtrSize = ptrSize
+const BitsPointer = bitsPointer
+const BitsScalar = bitsScalar
+
+func FuncLayout(t Type, rcvr Type) (frametype Type, argSize, retOffset uintptr, stack []byte) {
+	var ft *rtype
+	var s *bitVector
+	if rcvr != nil {
+		ft, argSize, retOffset, s = funcLayout(t.(*rtype), rcvr.(*rtype))
+	} else {
+		ft, argSize, retOffset, s = funcLayout(t.(*rtype), nil)
+	}
+	frametype = ft
+	for i := uint32(0); i < s.n; i += 2 {
+		stack = append(stack, s.data[i/8]>>(i%8)&3)
+	}
+	return
+}
