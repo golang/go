98c15d46be3806e7f37a076d900601e902d48bd3
Brad Fitzpatrick <bradfitz@golang.org>
2014-12-08 21:49:59 -0800

    dashboard/builder: change default gcpath back to hg for now
    We're doing this so we can update the main builders to fix the
    problems with subrepos. The coordinator doesn't know how to
    pass -gcpath to this.
    
    We can change it back to git after Go 1.4 is out.
    
    Change-Id: I3924a34ac974cb77954acea4772ef0f52e36fa45
    Reviewed-on: https://go-review.googlesource.com/1202
    Reviewed-by: Andrew Gerrand <adg@golang.org>

--- dashboard/builder/main.go
+++ dashboard/builder/main.go
@@ -53,7 +53,7 @@ var (
 	buildRevision = flag.String("rev", "", "Build specified revision and exit")
 	buildCmd      = flag.String("cmd", filepath.Join(".", allCmd), "Build command (specify relative to go/src/)")
 	buildTool     = flag.String("tool", "go", "Tool to build.")
-	gcPath        = flag.String("gcpath", "go.googlesource.com/go", "Path to download gc from")
+	gcPath        = flag.String("gcpath", "code.google.com/p/go", "Path to download gc from")
 	gccPath       = flag.String("gccpath", "https://github.com/mirrors/gcc.git", "Path to download gcc from")
 	benchPath     = flag.String("benchpath", "golang.org/x/benchmarks/bench", "Path to download benchmarks from")
 	failAll       = flag.Bool("fail", false, "fail all builds")
