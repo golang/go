fc71d7b8ca9a911ad670ec56570fa90a3c4c76f5
Shenghou Ma <minux@golang.org>
2014-12-09 19:41:52 -0500

    misc/chrome/gophertool: update for the git/gerrit transition
    Change-Id: I7b9601ae6e1cfb18ef79a7b189aa7e689c0fe942
    Reviewed-on: https://go-review.googlesource.com/1621
    Reviewed-by: Andrew Gerrand <adg@golang.org>
    Reviewed-by: Brad Fitzpatrick <bradfitz@golang.org>

--- misc/chrome/gophertool/gopher.js
+++ misc/chrome/gophertool/gopher.js
@@ -3,7 +3,8 @@
 // license that can be found in the LICENSE file.
 
 var numericRE = /^\d+$/;
-var commitRE = /^(?:\d+:)?([0-9a-f]{6,20})$/; // e.g "8486:ab29d2698a47" or "ab29d2698a47"
+var commitRE = /^(?:\d+:)?([0-9a-f]{6,40})$/; // e.g "8486:ab29d2698a47" or "ab29d2698a47"
+var gerritChangeIdRE = /^I[0-9a-f]{4,40}$/; // e.g. Id69c00d908d18151486007ec03da5495b34b05f5
 var pkgRE = /^[a-z0-9_\/]+$/;
 
 function urlForInput(t) {
@@ -19,6 +20,10 @@ function urlForInput(t) {
             return "https://github.com/golang/go/issues/" + t;
         }
         return "https://golang.org/cl/" + t;
+    }
+
+    if (gerritChangeIdRE.test(t)) {
+        return "http://golang.org/cl/" + t;
     }
 
     var match = commitRE.exec(t);
