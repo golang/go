9b69b9f078f2610a5c8014b43507f49af0b147c7
David Symonds <dsymonds@golang.org>
2014-12-11 21:54:20 +1100

    doc/play: update obsolete comment in peano.go.
    Change-Id: Ibecc2ef1e70c927c325c48bc381091f52c09da85
    Reviewed-on: https://go-review.googlesource.com/1355
    Reviewed-by: Russ Cox <rsc@golang.org>
    Reviewed-by: Minux Ma <minux@golang.org>
    Reviewed-by: Andrew Gerrand <adg@golang.org>

--- doc/play/peano.go
+++ doc/play/peano.go
@@ -3,9 +3,9 @@
 // (the nodes are the data).
 // http://en.wikipedia.org/wiki/Peano_axioms
 
-// This program demonstrates the power of Go's
-// segmented stacks when doing massively
-// recursive computations.
+// This program demonstrates the effectiveness
+// of the Go runtime's dynamically growing
+// stacks for heavily recursive computations.
 
 package main
 
