458cff5230751b488dc6eb6d2adb799ff544f6be
Shenghou Ma <minux@golang.org>
2014-12-18 17:01:26 -0500

    cmd/tipgodoc: add appenginevm build tag
    Fixes golang/go#9381.
    
    Change-Id: Ied5fd00475dde2b781cedff398dc9ee8d7f642a4
    Signed-off-by: Shenghou Ma <minux@golang.org>
    Reviewed-on: https://go-review.googlesource.com/1801
    Reviewed-by: Brad Fitzpatrick <bradfitz@golang.org>

--- cmd/tipgodoc/tip.go
+++ cmd/tipgodoc/tip.go
@@ -2,6 +2,8 @@
 // Use of this source code is governed by the Apache 2.0
 // license that can be found in the LICENSE file.
 
+// +build appenginevm
+
 // Command tipgodoc is the beginning of the new tip.golang.org server,
 // serving the latest HEAD straight from the Git oven.
 package main
