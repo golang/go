3f1954175231156ce57a67938a58511e242c2213
Alex Brainman <alex.brainman@gmail.com>
2014-12-18 16:30:46 +1100

    runtime: use go generate for zcallback_windows.go
    replacement for CL 180640043
    
    Change-Id: I8ff36645cfcbbda338faf7b29cbfdb95c47d5ec4
    Reviewed-on: https://go-review.googlesource.com/1765
    Reviewed-by: Brad Fitzpatrick <bradfitz@golang.org>

--- src/runtime/runtime2_windows.go
+++
@@ -1,8 +0,0 @@
-// Copyright 2009 The Go Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
-
-package runtime
-
-// TODO(brainman): move generation of zsys_windows_*.s out from cmd/dist/buildruntime.c and into here
-const cb_max = 2000 // maximum number of windows callbacks allowed (must be in sync with MAXWINCB from cmd/dist/buildruntime.c)
