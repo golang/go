6bb8ee2e67409771192524f8c50751128091b8f9
Josh Bleecher Snyder <josharian@gmail.com>
2015-01-07 14:24:18 -0800

    runtime: remove stray commas in assembly
    Change-Id: I4dc97ff8111bdc5ca6e4e3af06aaf4f768031c68
    Reviewed-on: https://go-review.googlesource.com/2473
    Reviewed-by: Minux Ma <minux@golang.org>

--- src/runtime/asm_amd64.s
+++ src/runtime/asm_amd64.s
@@ -439,7 +439,7 @@ TEXT runtime·cas(SB), NOSPLIT, $0-17
 	MOVL	new+12(FP), CX
 	LOCK
 	CMPXCHGL	CX, 0(BX)
-	SETEQ	, ret+16(FP)
+	SETEQ	ret+16(FP)
 	RET
 
 // bool	runtime·cas64(uint64 *val, uint64 old, uint64 new)
@@ -456,7 +456,7 @@ TEXT runtime·cas64(SB), NOSPLIT, $0-25
 	MOVQ	new+16(FP), CX
 	LOCK
 	CMPXCHGQ	CX, 0(BX)
-	SETEQ	, ret+24(FP)
+	SETEQ	ret+24(FP)
 	RET
 	
 TEXT runtime·casuintptr(SB), NOSPLIT, $0-25
@@ -484,7 +484,7 @@ TEXT runtime·casp1(SB), NOSPLIT, $0-25
 	MOVQ	new+16(FP), CX
 	LOCK
 	CMPXCHGQ	CX, 0(BX)
-	SETEQ	, ret+24(FP)
+	SETEQ	ret+24(FP)
 	RET
 
 // uint32 xadd(uint32 volatile *val, int32 delta)
