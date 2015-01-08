ee94cd1dfff26e3044713367adcdf901a5e2653a
Matthew Dempsky <mdempsky@google.com>
2015-01-07 16:30:24 -0800

    cmd/cgo, go/build: finish a cleanup TODO
    Removing #cgo directive parsing from cmd/cgo was done in
    https://golang.org/cl/8610044.
    
    Change-Id: Id1bec58c6ec1f932df0ce0ee84ff253655bb73ff
    Reviewed-on: https://go-review.googlesource.com/2501
    Reviewed-by: Ian Lance Taylor <iant@golang.org>

--- src/go/build/build.go
+++ src/go/build/build.go
@@ -1073,9 +1073,6 @@ func (ctxt *Context) shouldBuild(content []byte, allTags map[string]bool) bool {
 // saveCgo saves the information from the #cgo lines in the import "C" comment.
 // These lines set CFLAGS, CPPFLAGS, CXXFLAGS and LDFLAGS and pkg-config directives
 // that affect the way cgo's C code is built.
-//
-// TODO(rsc): This duplicates code in cgo.
-// Once the dust settles, remove this code from cgo.
 func (ctxt *Context) saveCgo(filename string, di *Package, cg *ast.CommentGroup) error {
 	text := cg.Text()
 	for _, line := range strings.Split(text, "\n") {
