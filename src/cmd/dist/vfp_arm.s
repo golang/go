d369f97342db25a6edd204ff938baa43d2f0ca5d
Dave Cheney <dave@cheney.net>
2015-01-11 06:12:20 +0000

    cmd/dist: fix arm vfp detection
    * Use WORD declaration so 5a can't rewrite the instruction or complain
      about forms it doesn't know about.
    * Add the interpunct to function declaration.
    
    Change-Id: I8494548db21b3ea52f0e1e0e547d9ead8b93dfd1
    Reviewed-on: https://go-review.googlesource.com/2682
    Reviewed-by: Minux Ma <minux@golang.org>

--- src/cmd/dist/vfp_arm.s
+++ src/cmd/dist/vfp_arm.s
@@ -5,11 +5,11 @@
 #include "textflag.h"
 
 // try to run "vmov.f64 d0, d0" instruction
-TEXT useVFPv1(SB),NOSPLIT,$0
-	VMOV.F64 D0, D0
+TEXT ·useVFPv1(SB),NOSPLIT,$0
+	WORD $0xeeb00b40	// vomv.f64 d0, d0
 	RET
 
 // try to run VFPv3-only "vmov.f64 d0, #112" instruction
-TEXT useVFPv3(SB),NOSPLIT,$0
-	VMOV.F64 $112, D0
+TEXT ·useVFPv3(SB),NOSPLIT,$0
+        WORD $0xeeb70b00	// vmov.f64 d0, #112
 	RET
