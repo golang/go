e8e89347cd44c67d97fc0973ec2f15bf137b2d77
Dave Cheney <dave@cheney.net>
2015-01-11 12:23:52 +1100

    runtime: remove duplicate copyright notice
    Change-Id: Ibe3ba6426cc6e683ff3712faf6119922d0f88b5a
    Reviewed-on: https://go-review.googlesource.com/2680
    Reviewed-by: Minux Ma <minux@golang.org>

--- src/runtime/os_linux_arm.go
+++ src/runtime/os_linux_arm.go
@@ -2,10 +2,6 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// Copyright 2009 The Go Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
-
 package runtime
 
 import "unsafe"
