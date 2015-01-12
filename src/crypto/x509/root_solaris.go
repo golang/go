3c7bdd4d768444c349f6647b00465627d5ee1345
Dave Cheney <dave@cheney.net>
2015-01-13 10:43:43 +1100

    crypto/x509: add cert SmartOS cert path
    Fix SmartOS build that was broken in 682922908f7.
    
    SmartOS pretends to be Ubuntu/Debian with respect to its SSL
    certificate location.
    
    Change-Id: I5405c6472c8a1e812e472e7301bf6084c17549d6
    Reviewed-on: https://go-review.googlesource.com/2704
    Reviewed-by: Brad Fitzpatrick <bradfitz@golang.org>

--- src/crypto/x509/root_solaris.go
+++ src/crypto/x509/root_solaris.go
@@ -6,5 +6,6 @@ package x509
 
 // Possible certificate files; stop after finding one.
 var certFiles = []string{
-	"/etc/certs/ca-certificates.crt", // Solaris 11.2+
+	"/etc/certs/ca-certificates.crt",     // Solaris 11.2+
+	"/etc/ssl/certs/ca-certificates.crt", // Joyent SmartOS
 }
