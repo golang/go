020637daa979cff4035e68b09802e045456fa943
Michael Matloob <matloob@google.com>
2015-01-18 16:04:47 -0800

    cmd/dist: produce a properly formatted zversion.go
    gofmt inserts a blank line line between const and var declarations
    
    Change-Id: I3f2ddbd9e66a74eb3f37a2fe641b93820b02229e
    Reviewed-on: https://go-review.googlesource.com/3022
    Reviewed-by: Brad Fitzpatrick <bradfitz@golang.org>

--- src/cmd/dist/buildruntime.go
+++ src/cmd/dist/buildruntime.go
@@ -27,6 +27,7 @@ func mkzversion(dir, file string) {
 			"\n"+
 			"const defaultGoroot = `%s`\n"+
 			"const theVersion = `%s`\n"+
+			"\n"+
 			"var buildVersion = theVersion\n", goroot_final, goversion)
 
 	writefile(out, file, 0)
