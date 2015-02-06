1b523384dc5c46e0a137d73f65dda636bfeda350
Shenghou Ma <minux@golang.org>
2015-02-06 14:33:44 -0500

    runtime: skip the gdb test if -iex is not supported
    It turns out -iex argument is not supported by all gdb versions,
    but as we need to add the auto-load safe path before loading the
    inferior, test -iex support first and skip the test if it's not
    available.
    
    We should still update our builders though.
    
    Change-Id: I355697de51baf12162ba6cb82f389dad93f93dc5
    Reviewed-on: https://go-review.googlesource.com/4070
    Reviewed-by: Ian Lance Taylor <iant@golang.org>

--- src/runtime/runtime-gdb_test.go
+++ src/runtime/runtime-gdb_test.go
@@ -11,7 +11,7 @@ import (
 )
 
 func checkGdbPython(t *testing.T) {
-	cmd := exec.Command("gdb", "-nx", "-q", "--batch", "-ex", "python import sys; print('go gdb python support')")
+	cmd := exec.Command("gdb", "-nx", "-q", "--batch", "-iex", "python import sys; print('go gdb python support')")
 	out, err := cmd.CombinedOutput()
 
 	if err != nil {
