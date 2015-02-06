01fe1492712d53a9f0c47041736b6162a6ffa517
Mikio Hara <mikioh.mikioh@gmail.com>
2015-02-06 14:54:28 +0900

    syscall: gofmt
    And the silence of the git-codereview.
    
    Change-Id: If3f7fe2de2ab4c1756f3cef8267199049d468b31
    Reviewed-on: https://go-review.googlesource.com/3983
    Reviewed-by: Minux Ma <minux@golang.org>

--- src/syscall/syscall_darwin_arm.go
+++ src/syscall/syscall_darwin_arm.go
@@ -68,4 +68,3 @@ func sendfile(outfd int, infd int, offset *int64, count int) (written int, err e
 }
 
 func Syscall9(num, a1, a2, a3, a4, a5, a6, a7, a8, a9 uintptr) (r1, r2 uintptr, err Errno) // sic
-
