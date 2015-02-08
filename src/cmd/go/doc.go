3813799931f5819b86d5d58f0787a5f8600ae69b
Shenghou Ma <minux@golang.org>
2015-02-08 18:09:23 -0500

    cmd/go: make consistent use of leading Tabs
    The only remaining uses of four spaces instead of a tab is
    when the line is too long (e.g. type Package).
    
    Fixes #9809
    
    Change-Id: Ifffd3639aa9264e795686ef1879a7686f182d2e5
    Reviewed-on: https://go-review.googlesource.com/4182
    Reviewed-by: Andrew Gerrand <adg@golang.org>

--- src/cmd/go/doc.go
+++ src/cmd/go/doc.go
@@ -14,32 +14,32 @@ Usage:
 
 The commands are:
 
-    build       compile packages and dependencies
-    clean       remove object files
-    env         print Go environment information
-    fix         run go tool fix on packages
-    fmt         run gofmt on package sources
-    generate    generate Go files by processing source
-    get         download and install packages and dependencies
-    install     compile and install packages and dependencies
-    list        list packages
-    run         compile and run Go program
-    test        test packages
-    tool        run specified go tool
-    version     print Go version
-    vet         run go tool vet on packages
+	build       compile packages and dependencies
+	clean       remove object files
+	env         print Go environment information
+	fix         run go tool fix on packages
+	fmt         run gofmt on package sources
+	generate    generate Go files by processing source
+	get         download and install packages and dependencies
+	install     compile and install packages and dependencies
+	list        list packages
+	run         compile and run Go program
+	test        test packages
+	tool        run specified go tool
+	version     print Go version
+	vet         run go tool vet on packages
 
 Use "go help [command]" for more information about a command.
 
 Additional help topics:
 
-    c           calling between Go and C
-    filetype    file types
-    gopath      GOPATH environment variable
-    importpath  import path syntax
-    packages    description of package lists
-    testflag    description of testing flags
-    testfunc    description of testing functions
+	c           calling between Go and C
+	filetype    file types
+	gopath      GOPATH environment variable
+	importpath  import path syntax
+	packages    description of package lists
+	testflag    description of testing flags
+	testfunc    description of testing functions
 
 Use "go help [topic]" for more information about that topic.
 
