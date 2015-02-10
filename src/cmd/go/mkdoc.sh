b5bf69ec26c4d9c563610626cf5bbba50149d0e6
Andrew Gerrand <adg@golang.org>
2015-02-10 13:24:47 +1100

    cmd/go: use current go source code when generating doc.go
    Change-Id: Iad1764707d173a09467fd36e8c49a58147f12219
    Reviewed-on: https://go-review.googlesource.com/4320
    Reviewed-by: Minux Ma <minux@golang.org>

--- src/cmd/go/mkdoc.sh
+++ src/cmd/go/mkdoc.sh
@@ -1,9 +1,12 @@
-#!/bin/sh
+#!/bin/bash
 # Copyright 2012 The Go Authors.  All rights reserved.
 # Use of this source code is governed by a BSD-style
 # license that can be found in the LICENSE file.
 
-go install # So the next line will produce updated documentation.
-go help documentation | sed 's; \*/; * /;' >doc.go
+set -e
+
+go build -o go.latest
+./go.latest help documentation | sed 's; \*/; * /;' >doc.go
 gofmt -w doc.go
+rm go.latest
 
