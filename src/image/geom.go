5c8f9e38eb96d274f82a93c936a8ec8015f90845
Nigel Tao <nigeltao@golang.org>
2015-02-17 11:15:06 +1100

    image: fix Rectangle.Overlaps and Rectangle.Union for empty rectangles.
    Fixes #9895.
    
    Change-Id: I37d78ced9ff8196e32d299504908a1c41ad4592d
    Reviewed-on: https://go-review.googlesource.com/4990
    Reviewed-by: Rob Pike <r@golang.org>

--- src/image/geom.go
+++ src/image/geom.go
@@ -164,6 +164,12 @@ func (r Rectangle) Intersect(s Rectangle) Rectangle {
 
 // Union returns the smallest rectangle that contains both r and s.
 func (r Rectangle) Union(s Rectangle) Rectangle {
+	if r.Empty() {
+		return s
+	}
+	if s.Empty() {
+		return r
+	}
 	if r.Min.X > s.Min.X {
 		r.Min.X = s.Min.X
 	}
@@ -192,7 +198,8 @@ func (r Rectangle) Eq(s Rectangle) bool {
 
 // Overlaps reports whether r and s have a non-empty intersection.
 func (r Rectangle) Overlaps(s Rectangle) bool {
-	return r.Min.X < s.Max.X && s.Min.X < r.Max.X &&
+	return !r.Empty() && !s.Empty() &&
+		r.Min.X < s.Max.X && s.Min.X < r.Max.X &&
 		r.Min.Y < s.Max.Y && s.Min.Y < r.Max.Y
 }
 
