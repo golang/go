b2c2bc48564a322a0d68ae429ec6a19836dfd6c1
David Crawshaw <crawshaw@golang.org>
2015-02-17 15:17:46 -0500

    crypto/x509: embed certificates on darwin/arm
    Change-Id: Ia6b06f19e5ac424f01a1b90b78b507363b0c4577
    Reviewed-on: https://go-review.googlesource.com/5061
    Reviewed-by: Josh Bleecher Snyder <josharian@gmail.com>

--- src/crypto/x509/root_darwin.go
+++ src/crypto/x509/root_darwin.go
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
+//go:generate go run root_darwin_arm_gen.go -output root_darwin_arm.go
+
 package x509
 
 import "os/exec"
