feba0160523381684ab7847ce01b2491c391125d
Alexandre Cesaro <alexandre.cesaro@gmail.com>
2015-02-25 19:13:43 +0100

    mime/quotedprintable: create the package
    This commit creates the mime/quotedprintable package. It moves and
    exports the QP reader of mime/internal/quotedprintable.
    
    The code is almost unchanged to preserve the commit history.
    
    Updates #4943
    
    Change-Id: I4b7b5a2a40a4c84346d42e4cdd2c11a91b28f9e3
    Reviewed-on: https://go-review.googlesource.com/5940
    Run-TryBot: Brad Fitzpatrick <bradfitz@golang.org>
    TryBot-Result: Gobot Gobot <gobot@golang.org>
    Reviewed-by: Brad Fitzpatrick <bradfitz@golang.org>

--- src/mime/quotedprintable/reader.go
+++ src/mime/quotedprintable/reader.go
@@ -2,12 +2,8 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// The file define a quoted-printable decoder, as specified in RFC 2045.
-// Deviations:
-// 1. in addition to "=\r\n", "=\n" is also treated as soft line break.
-// 2. it will pass through a '\r' or '\n' not preceded by '=', consistent
-//    with other broken QP encoders & decoders.
-
+// Package quotedprintable implements quoted-printable encoding as specified by
+// RFC 2045.
 package quotedprintable
 
 import (
@@ -17,14 +13,19 @@ import (
 	"io"
 )
 
-type qpReader struct {
+// Deviations from RFC 2045:
+// 1. in addition to "=\r\n", "=\n" is also treated as soft line break.
+// 2. it will pass through a '\r' or '\n' not preceded by '=', consistent
+//    with other broken QP encoders & decoders.
+type reader struct {
 	br   *bufio.Reader
 	rerr error  // last read error
 	line []byte // to be consumed before more of br
 }
 
+// NewReader returns a quoted-printable reader, decoding from r.
 func NewReader(r io.Reader) io.Reader {
-	return &qpReader{
+	return &reader{
 		br: bufio.NewReader(r),
 	}
 }
@@ -36,10 +37,10 @@ func fromHex(b byte) (byte, error) {
 	case b >= 'A' && b <= 'F':
 		return b - 'A' + 10, nil
 	}
-	return 0, fmt.Errorf("multipart: invalid quoted-printable hex byte 0x%02x", b)
+	return 0, fmt.Errorf("quotedprintable: invalid hex byte 0x%02x", b)
 }
 
-func (q *qpReader) readHexByte(v []byte) (b byte, err error) {
+func (q *reader) readHexByte(v []byte) (b byte, err error) {
 	if len(v) < 2 {
 		return 0, io.ErrUnexpectedEOF
 	}
@@ -67,7 +68,7 @@ var (
 	softSuffix = []byte("=")
 )
 
-func (q *qpReader) Read(p []byte) (n int, err error) {
+func (q *reader) Read(p []byte) (n int, err error) {
 	for len(p) > 0 {
 		if len(q.line) == 0 {
 			if q.rerr != nil {
@@ -84,7 +85,7 @@ func (q *qpReader) Read(p []byte) (n int, err error) {
 				rightStripped := wholeLine[len(q.line):]
 				q.line = q.line[:len(q.line)-1]
 				if !bytes.HasPrefix(rightStripped, lf) && !bytes.HasPrefix(rightStripped, crlf) {
-					q.rerr = fmt.Errorf("multipart: invalid bytes after =: %q", rightStripped)
+					q.rerr = fmt.Errorf("quotedprintable: invalid bytes after =: %q", rightStripped)
 				}
 			} else if hasLF {
 				if hasCR {
@@ -107,7 +108,7 @@ func (q *qpReader) Read(p []byte) (n int, err error) {
 		case b == '\t' || b == '\r' || b == '\n':
 			break
 		case b < ' ' || b > '~':
-			return n, fmt.Errorf("multipart: invalid unescaped byte 0x%02x in quoted-printable body", b)
+			return n, fmt.Errorf("quotedprintable: invalid unescaped byte 0x%02x in body", b)
 		}
 		p[0] = b
 		p = p[1:]
