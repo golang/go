a3c59779ffdc3d8280f7c32a6aa421ba56bad90d
David Crawshaw <crawshaw@golang.org>
2015-02-26 17:55:11 -0500

    syscall: exclude Go tool test on darwin/arm
    Change-Id: I44c1911beceaedaa35dad71d8be8a814528dce67
    Reviewed-on: https://go-review.googlesource.com/6192
    Reviewed-by: Minux Ma <minux@golang.org>

--- src/syscall/syscall_unix_test.go
+++ src/syscall/syscall_unix_test.go
@@ -92,6 +92,10 @@ func TestPassFD(t *testing.T) {
 	case "solaris":
 		// TODO(aram): Figure out why ReadMsgUnix is returning empty message.
 		t.Skip("skipping test on solaris, see issue 7402")
+	case "darwin":
+		if runtime.GOARCH == "arm" {
+			t.Skipf("skipping test on %d/%s, no fork", runtime.GOOS, runtime.GOARCH)
+		}
 	}
 	if os.Getenv("GO_WANT_HELPER_PROCESS") == "1" {
 		passFDChild()
