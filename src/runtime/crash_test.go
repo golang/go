95bf77bc68dec4423dec3599398e08d99c637d9b
David Crawshaw <crawshaw@golang.org>
2015-02-26 18:05:47 -0500

    runtime: skip tests that need fork on darwin/arm
    Change-Id: I1bb0b8b11e8c7686b85657050fd7cf926afe4d29
    Reviewed-on: https://go-review.googlesource.com/6200
    Reviewed-by: Hyang-Ah Hana Kim <hyangah@gmail.com>

--- src/runtime/crash_test.go
+++ src/runtime/crash_test.go
@@ -41,6 +41,10 @@ func executeTest(t *testing.T, templ string, data interface{}, extra ...string) 
 	switch runtime.GOOS {
 	case "android", "nacl":
 		t.Skipf("skipping on %s", runtime.GOOS)
+	case "darwin":
+		if runtime.GOARCH == "arm" {
+			t.Skipf("skipping on %s/%s", runtime.GOOS, runtime.GOARCH)
+		}
 	}
 
 	checkStaleRuntime(t)
