344f424007cc679dc06a25f918394b02a51e66a1
David Crawshaw <crawshaw@golang.org>
2015-02-27 19:52:11 -0500

    os: fix tests on darwin/arm
    Tests that fork are skipped. Tests that create files do so in a
    temporary directory, as the initial PWD is read-only. And
    darwin/arm returns a strange mkdir error when trying to write to /.
    
    Change-Id: I2de661a85524300bbac226693d72142992dc188d
    Reviewed-on: https://go-review.googlesource.com/6312
    Reviewed-by: Brad Fitzpatrick <bradfitz@golang.org>

--- src/os/path_test.go
+++ src/os/path_test.go
@@ -207,6 +207,10 @@ func TestMkdirAllAtSlash(t *testing.T) {
 	switch runtime.GOOS {
 	case "android", "plan9", "windows":
 		t.Skipf("skipping on %s", runtime.GOOS)
+	case "darwin":
+		if runtime.GOARCH == "arm" {
+			t.Skipf("skipping on darwin/%s, mkdir returns EPERM", runtime.GOARCH)
+		}
 	}
 	RemoveAll("/_go_os_test")
 	const dir = "/go_os_test/dir"
@@ -217,7 +221,7 @@ func TestMkdirAllAtSlash(t *testing.T) {
 		if ok && (pathErr.Err == syscall.EACCES || isReadonlyError(pathErr.Err)) {
 			t.Skipf("could not create %v: %v", dir, err)
 		}
-		t.Fatalf(`MkdirAll "/_go_os_test/dir": %v`, err)
+		t.Fatalf(`MkdirAll "/_go_os_test/dir": %v, %s, %d`, err, pathErr.Err, pathErr.Err)
 	}
 	RemoveAll("/_go_os_test")
 }
