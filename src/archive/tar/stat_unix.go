8d6a0f81264d129eed79f66ff7d20f5bf7f481f9
Vincent Batts <vbatts@hashbangbash.com>
2015-03-04 12:29:16 -0500

    archive/tar: fix round-trip attributes
    The issue was identified while
    working with round trip FileInfo of the headers of hardlinks. Also,
    additional test cases for hard link handling.
    (review carried over from http://golang.org/cl/165860043)
    
    Fixes #9027
    
    Change-Id: I9e3a724c8de72eb1b0fbe0751a7b488894911b76
    Reviewed-on: https://go-review.googlesource.com/6790
    Reviewed-by: Russ Cox <rsc@golang.org>

--- src/archive/tar/stat_unix.go
+++ src/archive/tar/stat_unix.go
@@ -16,17 +16,41 @@ func init() {
 }
 
 func statUnix(fi os.FileInfo, h *Header) error {
-	sys, ok := fi.Sys().(*syscall.Stat_t)
-	if !ok {
-		return nil
+	switch sys := fi.Sys().(type) {
+	case *syscall.Stat_t:
+		h.Uid = int(sys.Uid)
+		h.Gid = int(sys.Gid)
+		// TODO(bradfitz): populate username & group.  os/user
+		// doesn't cache LookupId lookups, and lacks group
+		// lookup functions.
+		h.AccessTime = statAtime(sys)
+		h.ChangeTime = statCtime(sys)
+		// TODO(bradfitz): major/minor device numbers?
+		if fi.Mode().IsRegular() && sys.Nlink > 1 {
+			h.Typeflag = TypeLink
+			h.Size = 0
+			// TODO(vbatts): Linkname?
+		}
+	case *Header:
+		// for the roundtrip logic
+		h.Uid = sys.Uid
+		h.Gid = sys.Gid
+		h.Uname = sys.Uname
+		h.Gname = sys.Gname
+		h.AccessTime = sys.AccessTime
+		h.ChangeTime = sys.ChangeTime
+		if sys.Xattrs != nil {
+			h.Xattrs = make(map[string]string)
+			for k, v := range sys.Xattrs {
+				h.Xattrs[k] = v
+			}
+		}
+		if sys.Typeflag == TypeLink {
+			// hard link
+			h.Typeflag = TypeLink
+			h.Size = 0
+			h.Linkname = sys.Linkname
+		}
 	}
-	h.Uid = int(sys.Uid)
-	h.Gid = int(sys.Gid)
-	// TODO(bradfitz): populate username & group.  os/user
-	// doesn't cache LookupId lookups, and lacks group
-	// lookup functions.
-	h.AccessTime = statAtime(sys)
-	h.ChangeTime = statCtime(sys)
-	// TODO(bradfitz): major/minor device numbers?
 	return nil
 }
