ea5f472ce99448d1e007a0183ae5a9bea09cccd0
Joël Stemmer <stemmertech@gmail.com>
2015-03-06 14:59:12 +0100

    crypto/tls: fix typo in tls handshake error
    Change-Id: Ia9f39250619ea6e94157efceddfb2e02d35f3ae2
    Reviewed-on: https://go-review.googlesource.com/7041
    Reviewed-by: Brad Fitzpatrick <bradfitz@golang.org>
    Run-TryBot: Brad Fitzpatrick <bradfitz@golang.org>

--- src/crypto/tls/handshake_server_test.go
+++ src/crypto/tls/handshake_server_test.go
@@ -780,7 +780,7 @@ func TestFallbackSCSV(t *testing.T) {
 		config: serverConfig,
 		// OpenSSL 1.0.1j is needed for the -fallback_scsv option.
 		command: []string{"openssl", "s_client", "-fallback_scsv"},
-		expectHandshakeErrorIncluding: "inppropriate protocol fallback",
+		expectHandshakeErrorIncluding: "inappropriate protocol fallback",
 	}
 	runServerTestTLS11(t, test)
 }
