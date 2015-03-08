c719d608ee9e96dd4b47c7671fcb223bdde34934
Aram Hăvărneanu <aram@mgk.ro>
2015-03-08 14:25:50 +0100

    math, math/big: add support for GOARCH=arm64
    Change-Id: Ief12e1435a40dd2eaddc3f97f63be44c4dd2e050
    Reviewed-on: https://go-review.googlesource.com/7144
    Reviewed-by: Minux Ma <minux@golang.org>
    Reviewed-by: Dave Cheney <dave@cheney.net>
    Reviewed-by: Rob Pike <r@golang.org>
    Reviewed-by: Russ Cox <rsc@golang.org>

---
+++ src/math/big/arith_arm64.s
@@ -0,0 +1,44 @@
+// Copyright 2013 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+#include "textflag.h"
+
+// This file provides fast assembly versions for the elementary
+// arithmetic operations on vectors implemented in arith.go.
+
+TEXT ·mulWW(SB),NOSPLIT,$0
+	B ·mulWW_g(SB)
+
+TEXT ·divWW(SB),NOSPLIT,$0
+	B ·divWW_g(SB)
+
+TEXT ·addVV(SB),NOSPLIT,$0
+	B ·addVV_g(SB)
+
+TEXT ·subVV(SB),NOSPLIT,$0
+	B ·subVV_g(SB)
+
+TEXT ·addVW(SB),NOSPLIT,$0
+	B ·addVW_g(SB)
+
+TEXT ·subVW(SB),NOSPLIT,$0
+	B ·subVW_g(SB)
+
+TEXT ·shlVU(SB),NOSPLIT,$0
+	B ·shlVU_g(SB)
+
+TEXT ·shrVU(SB),NOSPLIT,$0
+	B ·shrVU_g(SB)
+
+TEXT ·mulAddVWW(SB),NOSPLIT,$0
+	B ·mulAddVWW_g(SB)
+
+TEXT ·addMulVVW(SB),NOSPLIT,$0
+	B ·addMulVVW_g(SB)
+
+TEXT ·divWVW(SB),NOSPLIT,$0
+	B ·divWVW_g(SB)
+
+TEXT ·bitLen(SB),NOSPLIT,$0
+	B ·bitLen_g(SB)
