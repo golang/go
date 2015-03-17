ad49c7bb6223b64da5a4dfae2150cd5396955b49
Michael Hudson-Doyle <michael.hudson@canonical.com>
2015-03-17 16:28:30 +1300

    cmd/internal/obj: remove dead fields and code
    A quick pass through link.go, mostly removing fields that are not
    used on the "creating a single object file" side of the fence.
    
    Change-Id: I35ba41378c2c418f7df2f2f88dce65bc64a1a45d
    Reviewed-on: https://go-review.googlesource.com/7672
    Run-TryBot: Ian Lance Taylor <iant@golang.org>
    TryBot-Result: Gobot Gobot <gobot@golang.org>
    Reviewed-by: Ian Lance Taylor <iant@golang.org>

--- src/cmd/internal/obj/x86/obj6.go
+++ src/cmd/internal/obj/x86/obj6.go
@@ -246,7 +246,6 @@ func progedit(ctxt *obj.Link, p *obj.Prog) {
 			if s.Type == 0 {
 				s.Type = obj.SRODATA
 				obj.Adduint32(ctxt, s, i32)
-				s.Reachable = 0
 			}
 
 			p.From.Type = obj.TYPE_MEM
@@ -290,7 +289,6 @@ func progedit(ctxt *obj.Link, p *obj.Prog) {
 			if s.Type == 0 {
 				s.Type = obj.SRODATA
 				obj.Adduint64(ctxt, s, i64)
-				s.Reachable = 0
 			}
 
 			p.From.Type = obj.TYPE_MEM
