fd2f8d458e862968d2f7c755e2b2a2a868a49c31
Robert Griesemer <gri@golang.org>
2015-03-18 10:37:53 -0700

    cmd/internal/gc: remove duplicate copyright notice
    Change-Id: Ifa71fb443a66eb8d7732f3b0c1408947b583c1f1
    Reviewed-on: https://go-review.googlesource.com/7800
    Reviewed-by: Ian Lance Taylor <iant@golang.org>

--- src/cmd/internal/gc/go.go
+++ src/cmd/internal/gc/go.go
@@ -9,10 +9,6 @@ import (
 	"cmd/internal/obj"
 )
 
-// Copyright 2009 The Go Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
-
 // avoid <ctype.h>
 
 // The parser's maximum stack size.
