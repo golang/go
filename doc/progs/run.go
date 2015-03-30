2d18ab75e6f828811ff205b4bf968809e2d8fdb2
Aram Hăvărneanu <aram@mgk.ro>
2015-03-30 23:12:04 +0200

    doc/progs: disable cgo tests that use C.Stdout on Solaris
    Solaris, like Windows, NetBSD and OpenBSD, uses macros for stdin, stdout,
    and stderr. Cgo can't access them without getters/setters written in
    C. Because of this we disable affected tests like for the other platforms.
    
    Updates #10715.
    
    Change-Id: I3d33a5554b5ba209273dbdff992925a38a281b42
    Reviewed-on: https://go-review.googlesource.com/8264
    Reviewed-by: Minux Ma <minux@golang.org>
    Reviewed-by: Russ Cox <rsc@golang.org>

--- doc/progs/run.go
+++ doc/progs/run.go
@@ -219,11 +219,11 @@ func fixcgo() {
 		// cgo1 and cgo2 don't run on netbsd, srandom has a different signature
 		skipTest("cgo1")
 		skipTest("cgo2")
-		// cgo3 and cgo4 don't run on netbsd, since cgo cannot handle stdout correctly
+		// cgo3 and cgo4 don't run on netbsd, since cgo cannot handle stdout correctly, see issue #10715.
 		skipTest("cgo3")
 		skipTest("cgo4")
-	case "openbsd":
-		// cgo3 and cgo4 don't run on openbsd and solaris, since cgo cannot handle stdout correctly
+	case "openbsd", "solaris":
+		// cgo3 and cgo4 don't run on openbsd and solaris, since cgo cannot handle stdout correctly, see issue #10715.
 		skipTest("cgo3")
 		skipTest("cgo4")
 	}
