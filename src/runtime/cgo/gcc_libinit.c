83eabf9387b1623d29cd807e41f78bbc93d5f251
Aram Hăvărneanu <aram@mgk.ro>
2015-03-30 23:03:09 +0200

    runtime/cgo: add cgo support for solaris/amd64
    Change-Id: Ic9744c7716cdd53f27c6e5874230963e5fff0333
    Reviewed-on: https://go-review.googlesource.com/8260
    Reviewed-by: Minux Ma <minux@golang.org>

--- src/runtime/cgo/gcc_libinit.c
+++ src/runtime/cgo/gcc_libinit.c
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// +build darwin dragonfly freebsd linux netbsd
+// +build darwin dragonfly freebsd linux netbsd solaris
 // +build !ppc64,!ppc64le
 
 #include <pthread.h>
