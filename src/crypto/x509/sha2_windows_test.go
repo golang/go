32f88acf0c2ce8bbec5cdedfe23570ee5302d3e9
Alex Brainman <alex.brainman@gmail.com>
2015-03-31 16:59:02 +1100

    crypto/x509: use syscall.GetVersion instead of internal/syscall/windows.GetVersion
    cl8167 introduced internal/syscall/windows.GetVersion, but we already
    have that function in syscall.GetVersion. Use that instead.
    Also revert all internal/syscall/windows cl8167 changes.
    
    Change-Id: I512a5bf4b3b696e93aaf69e9e8b7df7022670ec0
    Reviewed-on: https://go-review.googlesource.com/8302
    Reviewed-by: Daniel Theophanes <kardianos@gmail.com>
    Reviewed-by: Minux Ma <minux@golang.org>

--- src/crypto/x509/sha2_windows_test.go
+++ src/crypto/x509/sha2_windows_test.go
@@ -4,10 +4,14 @@
 
 package x509
 
-import "internal/syscall/windows"
+import "syscall"
 
 func init() {
-	if major, _ := windows.GetVersion(); major < 6 {
+	v, err := syscall.GetVersion()
+	if err != nil {
+		return
+	}
+	if major := byte(v); major < 6 {
 		// Windows XP SP2 and Windows 2003 do not support SHA2.
 		// http://blogs.technet.com/b/pki/archive/2010/09/30/sha2-and-windows.aspx
 		supportSHA2 = false
