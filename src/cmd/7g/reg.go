ca0adeddd5515a02b76f30e5b72eb202c65f5f2f
David Crawshaw <crawshaw@golang.org>
2015-04-03 12:43:43 -0400

    cmd/7g: reserve registers R26 to R32
    These registers are not available for programs to use. Prior to this
    change, the compiler would crash attempting to use ZR as a general
    purpose register. Other programs would compile but on execution would
    overwrite the G register and cause havoc.
    
    Fixes linux/arm64 build.
    Fixes #10304
    Fixes #10320
    
    Change-Id: I5cf51d3b77cfe3db7dd6377324950cafb02f8d8b
    Reviewed-on: https://go-review.googlesource.com/8456
    Reviewed-by: Minux Ma <minux@golang.org>

--- src/cmd/7g/reg.go
+++ src/cmd/7g/reg.go
@@ -115,6 +115,11 @@ func excludedregs() uint64 {
 	// Exclude registers with fixed functions
 	regbits := uint64(RtoB(arm64.REGRT1) | RtoB(arm64.REGRT2) | RtoB(arm64.REGPR))
 
+	// Exclude R26 - R31.
+	for r := arm64.REGMAX + 1; r <= arm64.REGZERO; r++ {
+		regbits |= RtoB(r)
+	}
+
 	// Also exclude floating point registers with fixed constants
 	regbits |= RtoB(arm64.REG_F27) | RtoB(arm64.REG_F28) | RtoB(arm64.REG_F29) | RtoB(arm64.REG_F30) | RtoB(arm64.REG_F31)
 
