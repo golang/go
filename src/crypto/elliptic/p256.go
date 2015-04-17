7bacfc640fba4fb2e50bbcc16a4c15fe4bf5b870
Vlad Krasnov <vlad@cloudflare.com>
2015-04-17 06:10:35 -0700

    crypto/elliptic,crypto/ecdsa: P256 amd64 assembly
    This is based on the implementation used in OpenSSL, from a
    submission by Shay Gueron and myself. Besides using assembly,
    this implementation employs several optimizations described in:
    
        S.Gueron and V.Krasnov, "Fast prime field elliptic-curve
                                 cryptography with 256-bit primes"
    
    In addition a new and improved modular inverse modulo N is
    implemented here.
    
    The performance measured on a Haswell based Macbook Pro shows 21X
    speedup for the sign and 9X for the verify operations.
    The operation BaseMult is 30X faster (and the Diffie-Hellman/ECDSA
    key generation that use it are sped up as well).
    
    The adaptation to Go with the help of Filippo Valsorda
    
    Updated the submission for faster verify/ecdh, fixed some asm syntax
    and API problems and added benchmarks.
    
    Change-Id: I86a33636747d5c92f15e0c8344caa2e7e07e0028
    Reviewed-on: https://go-review.googlesource.com/8968
    Run-TryBot: Adam Langley <agl@golang.org>
    TryBot-Result: Gobot Gobot <gobot@golang.org>
    Reviewed-by: Adam Langley <agl@golang.org>

--- src/crypto/elliptic/p256.go
+++ src/crypto/elliptic/p256.go
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
+// +build !amd64
+
 package elliptic
 
 // This file contains a constant-time, 32-bit implementation of P256.
