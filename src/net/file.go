6f7961da28232c609f7c51b3bed7f15db7dd33e1
Mikio Hara <mikioh.mikioh@gmail.com>
2015-04-23 23:57:00 +0900

    net, internal/syscall/unix: add SocketConn, SocketPacketConn
    FileConn and FilePacketConn APIs accept user-configured socket
    descriptors to make them work together with runtime-integrated network
    poller, but there's a limitation. The APIs reject protocol sockets that
    are not supported by standard library. It's very hard for the net,
    syscall packages to look after all platform, feature-specific sockets.
    
    This change allows various platform, feature-specific socket descriptors
    to use runtime-integrated network poller by using SocketConn,
    SocketPacketConn APIs that bridge between the net, syscall packages and
    platforms.
    
    New exposed APIs:
    pkg net, func SocketConn(*os.File, SocketAddr) (Conn, error)
    pkg net, func SocketPacketConn(*os.File, SocketAddr) (PacketConn, error)
    pkg net, type SocketAddr interface { Addr, Raw }
    pkg net, type SocketAddr interface, Addr([]uint8) Addr
    pkg net, type SocketAddr interface, Raw(Addr) []uint8
    
    Fixes #10565.
    
    Change-Id: Iec57499b3d84bb5cb0bcf3f664330c535eec11e3
    Reviewed-on: https://go-review.googlesource.com/9275
    Reviewed-by: Ian Lance Taylor <iant@golang.org>

--- src/net/file.go
+++ src/net/file.go
@@ -46,3 +46,47 @@ func FilePacketConn(f *os.File) (c PacketConn, err error) {
 	}
 	return
 }
+
+// A SocketAddr is used with SocketConn or SocketPacketConn to
+// implement a user-configured socket address.
+// The net package does not provide any implementations of SocketAddr;
+// the caller of SocketConn or SocketPacketConn is expected to provide
+// one.
+type SocketAddr interface {
+	// Addr takes a platform-specific socket address and returns
+	// a net.Addr. The result may be nil when the syscall package,
+	// system call or underlying protocol does not support the
+	// socket address.
+	Addr([]byte) Addr
+
+	// Raw takes a net.Addr and returns a platform-specific socket
+	// address. The result may be nil when the syscall package,
+	// system call or underlying protocol does not support the
+	// socket address.
+	Raw(Addr) []byte
+}
+
+// SocketConn returns a copy of the network connection corresponding
+// to the open file f and user-defined socket address sa.
+// It is the caller's responsibility to close f when finished.
+// Closing c does not affect f, and closing f does not affect c.
+func SocketConn(f *os.File, sa SocketAddr) (c Conn, err error) {
+	c, err = socketConn(f, sa)
+	if err != nil {
+		err = &OpError{Op: "file", Net: "file+net", Source: nil, Addr: fileAddr(f.Name()), Err: err}
+	}
+	return
+}
+
+// SocketPacketConn returns a copy of the packet network connection
+// corresponding to the open file f and user-defined socket address
+// sa.
+// It is the caller's responsibility to close f when finished.
+// Closing c does not affect f, and closing f does not affect c.
+func SocketPacketConn(f *os.File, sa SocketAddr) (c PacketConn, err error) {
+	c, err = socketPacketConn(f, sa)
+	if err != nil {
+		err = &OpError{Op: "file", Net: "file+net", Source: nil, Addr: fileAddr(f.Name()), Err: err}
+	}
+	return
+}
