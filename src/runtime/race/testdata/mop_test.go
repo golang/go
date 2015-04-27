d0501d93f869ebb8e5c56164d6ae9f0c78a72c89
Austin Clements <austin@google.com>
2015-04-27 17:15:30 -0400

    runtime/race: fix failing tests
    Some race tests were sensitive to the goroutine scheduling order.
    When this changed in commit 78b4009, these tests started to fail.
    
    Fix TestRaceHeapParam by ensuring that the racing goroutine has
    run before the test exits. Fix TestRaceRWMutexMultipleReaders by
    adding a third reader to ensure that two readers wind up on the
    same side of the writer (and race with each other) regardless of
    the schedule. Fix TestRaceRange by ensuring that the racing
    goroutine runs before the main goroutine exits the loop it races
    with.
    
    Change-Id: Iaf002f8730ea42227feaf2f3c51b9a1e57ccffdd
    Reviewed-on: https://go-review.googlesource.com/9402
    Reviewed-by: Russ Cox <rsc@golang.org>

--- src/runtime/race/testdata/mop_test.go
+++ src/runtime/race/testdata/mop_test.go
@@ -335,6 +335,8 @@ func TestRaceRange(t *testing.T) {
 			}
 			done <- true
 		}(i)
+		// Ensure the goroutine runs before we continue the loop.
+		runtime.Gosched()
 	}
 	for i := 0; i < N; i++ {
 		<-done
@@ -1727,13 +1729,16 @@ func TestNoRaceAsFunc4(t *testing.T) {
 }
 
 func TestRaceHeapParam(t *testing.T) {
+	done := make(chan bool)
 	x := func() (x int) {
 		go func() {
 			x = 42
+			done <- true
 		}()
 		return
 	}()
 	_ = x
+	<-done
 }
 
 func TestNoRaceEmptyStruct(t *testing.T) {
