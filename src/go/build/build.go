971d40d20d049bde718ea75695b756dd626ecfa3
Shenghou Ma <minux@golang.org>
2015-05-14 00:58:06 -0400

    go/build: introduce go1.5 build tag
    Change-Id: Iab2f8e1c4443f39b79c1c63a7a30062074b48764
    Signed-off-by: Shenghou Ma <minux@golang.org>
    Reviewed-on: https://go-review.googlesource.com/10042
    Reviewed-by: Brad Fitzpatrick <bradfitz@golang.org>

--- src/go/build/build.go
+++ src/go/build/build.go
@@ -296,11 +296,7 @@ func defaultContext() Context {
 	// in all releases >= Go 1.x. Code that requires Go 1.x or later should
 	// say "+build go1.x", and code that should only be built before Go 1.x
 	// (perhaps it is the stub to use in that case) should say "+build !go1.x".
-	//
-	// When we reach Go 1.5 the line will read
-	//	c.ReleaseTags = []string{"go1.1", "go1.2", "go1.3", "go1.4", "go1.5"}
-	// and so on.
-	c.ReleaseTags = []string{"go1.1", "go1.2", "go1.3", "go1.4"}
+	c.ReleaseTags = []string{"go1.1", "go1.2", "go1.3", "go1.4", "go1.5"}
 
 	switch os.Getenv("CGO_ENABLED") {
 	case "1":
