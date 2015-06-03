bbdf631f342585cb316cc0b4823241d45058fe9b
Shenghou Ma <minux@golang.org>
2015-06-02 20:52:46 -0400

    cmd/dist: enable extlink test on linux/arm64
    It was an oversight (but as linux/arm64 doesn't support internal
    linking and always use external linking with cgo, no harm is done.)
    
    Change-Id: Ie5f2b445cb67a8e63d6b868e63379c68847554f9
    Reviewed-on: https://go-review.googlesource.com/10636
    Reviewed-by: Brad Fitzpatrick <bradfitz@golang.org>

--- src/cmd/dist/test.go
+++ src/cmd/dist/test.go
@@ -476,7 +476,7 @@ func (t *tester) extLink() bool {
 		"darwin-arm", "darwin-arm64",
 		"dragonfly-386", "dragonfly-amd64",
 		"freebsd-386", "freebsd-amd64", "freebsd-arm",
-		"linux-386", "linux-amd64", "linux-arm",
+		"linux-386", "linux-amd64", "linux-arm", "linux-arm64",
 		"netbsd-386", "netbsd-amd64",
 		"openbsd-386", "openbsd-amd64",
 		"windows-386", "windows-amd64":
