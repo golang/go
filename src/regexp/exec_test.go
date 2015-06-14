0d3d7b972db3bd46a6257a8dd40a6821de1744d3
Michael Matloob <matloob@google.com>
2015-06-14 09:57:46 -0700

    regexp: small correction to test comment
    s/Backtrace/Backtrack/
    
    Change-Id: I062aab18f23f2bc2110cf7210c2e7264747e02cf
    Reviewed-on: https://go-review.googlesource.com/11091
    Reviewed-by: Brad Fitzpatrick <bradfitz@golang.org>

--- src/regexp/exec_test.go
+++ src/regexp/exec_test.go
@@ -714,7 +714,7 @@ func TestLongest(t *testing.T) {
 	}
 }
 
-// TestProgramTooLongForBacktrace tests that a regex which is too long
+// TestProgramTooLongForBacktrack tests that a regex which is too long
 // for the backtracker still executes properly.
 func TestProgramTooLongForBacktrack(t *testing.T) {
 	longRegex := MustCompile(`(one|two|three|four|five|six|seven|eight|nine|ten|eleven|twelve|thirteen|fourteen|fifteen|sixteen|seventeen|eighteen|nineteen|twenty|twentyone|twentytwo|twentythree|twentyfour|twentyfive|twentysix|twentyseven|twentyeight|twentynine|thirty|thirtyone|thirtytwo|thirtythree|thirtyfour|thirtyfive|thirtysix|thirtyseven|thirtyeight|thirtynine|forty|fortyone|fortytwo|fortythree|fortyfour|fortyfive|fortysix|fortyseven|fortyeight|fortynine|fifty|fiftyone|fiftytwo|fiftythree|fiftyfour|fiftyfive|fiftysix|fiftyseven|fiftyeight|fiftynine|sixty|sixtyone|sixtytwo|sixtythree|sixtyfour|sixtyfive|sixtysix|sixtyseven|sixtyeight|sixtynine|seventy|seventyone|seventytwo|seventythree|seventyfour|seventyfive|seventysix|seventyseven|seventyeight|seventynine|eighty|eightyone|eightytwo|eightythree|eightyfour|eightyfive|eightysix|eightyseven|eightyeight|eightynine|ninety|ninetyone|ninetytwo|ninetythree|ninetyfour|ninetyfive|ninetysix|ninetyseven|ninetyeight|ninetynine|onehundred)`)
