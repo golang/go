5fa79f303b568495d4fdd9f8b496b2fb0d1b95f9
Mikio Hara <mikioh.mikioh@gmail.com>
2015-07-02 15:02:03 +0900

    Revert "net, internal/syscall/unix: add SocketConn, SocketPacketConn"
    This reverts commit a31c279e4b682a1ffac107f1fd5b53792b1b3d81.
    
    Russ suggests changing the frozon syscall package and obviously it's a
    better solution. Perhaps he will also let me know the way how to get the
    project owners to agree later.
    
    Fixes #11492.
    
    Change-Id: I98f9f366b72b85db54b4acfc3a604b62fb6d783c
    Reviewed-on: https://go-review.googlesource.com/11854
    Run-TryBot: Mikio Hara <mikioh.mikioh@gmail.com>
    Reviewed-by: Rob Pike <r@golang.org>
    Reviewed-by: Brad Fitzpatrick <bradfitz@golang.org>

--- src/net/file_unix.go
+++ src/net/file_unix.go
@@ -7,7 +7,6 @@
 package net
 
 import (
-	"internal/syscall/unix"
 	"os"
 	"syscall"
 )
@@ -24,55 +23,38 @@ func dupSocket(f *os.File) (int, error) {
 	return s, nil
 }
 
-func newFileFD(f *os.File, sa SocketAddr) (*netFD, error) {
+func newFileFD(f *os.File) (*netFD, error) {
 	s, err := dupSocket(f)
 	if err != nil {
 		return nil, err
 	}
-	var laddr, raddr Addr
-	var fd *netFD
-	if sa != nil {
-		lsa := make([]byte, syscall.SizeofSockaddrAny)
-		if err := unix.Getsockname(s, lsa); err != nil {
-			lsa = nil
-		}
-		rsa := make([]byte, syscall.SizeofSockaddrAny)
-		if err := unix.Getpeername(s, rsa); err != nil {
-			rsa = nil
-		}
-		laddr = sa.Addr(lsa)
-		raddr = sa.Addr(rsa)
-		fd, err = newFD(s, -1, -1, laddr.Network())
-	} else {
-		family := syscall.AF_UNSPEC
-		var sotype int
-		sotype, err = syscall.GetsockoptInt(s, syscall.SOL_SOCKET, syscall.SO_TYPE)
-		if err != nil {
-			closeFunc(s)
-			return nil, os.NewSyscallError("getsockopt", err)
-		}
-		lsa, _ := syscall.Getsockname(s)
-		rsa, _ := syscall.Getpeername(s)
-		switch lsa.(type) {
-		case *syscall.SockaddrInet4:
-			family = syscall.AF_INET
-		case *syscall.SockaddrInet6:
-			family = syscall.AF_INET6
-		case *syscall.SockaddrUnix:
-			family = syscall.AF_UNIX
-		default:
-			closeFunc(s)
-			return nil, syscall.EPROTONOSUPPORT
-		}
-		fd, err = newFD(s, family, sotype, "")
-		laddr = fd.addrFunc()(lsa)
-		raddr = fd.addrFunc()(rsa)
-		fd.net = laddr.Network()
+	family := syscall.AF_UNSPEC
+	sotype, err := syscall.GetsockoptInt(s, syscall.SOL_SOCKET, syscall.SO_TYPE)
+	if err != nil {
+		closeFunc(s)
+		return nil, os.NewSyscallError("getsockopt", err)
 	}
+	lsa, _ := syscall.Getsockname(s)
+	rsa, _ := syscall.Getpeername(s)
+	switch lsa.(type) {
+	case *syscall.SockaddrInet4:
+		family = syscall.AF_INET
+	case *syscall.SockaddrInet6:
+		family = syscall.AF_INET6
+	case *syscall.SockaddrUnix:
+		family = syscall.AF_UNIX
+	default:
+		closeFunc(s)
+		return nil, syscall.EPROTONOSUPPORT
+	}
+	fd, err := newFD(s, family, sotype, "")
 	if err != nil {
 		closeFunc(s)
 		return nil, err
 	}
+	laddr := fd.addrFunc()(lsa)
+	raddr := fd.addrFunc()(rsa)
+	fd.net = laddr.Network()
 	if err := fd.init(); err != nil {
 		fd.Close()
 		return nil, err
@@ -82,7 +64,7 @@ func newFileFD(f *os.File, sa SocketAddr) (*netFD, error) {
 }
 
 func fileConn(f *os.File) (Conn, error) {
-	fd, err := newFileFD(f, nil)
+	fd, err := newFileFD(f)
 	if err != nil {
 		return nil, err
 	}
@@ -101,7 +83,7 @@ func fileConn(f *os.File) (Conn, error) {
 }
 
 func fileListener(f *os.File) (Listener, error) {
-	fd, err := newFileFD(f, nil)
+	fd, err := newFileFD(f)
 	if err != nil {
 		return nil, err
 	}
@@ -116,7 +98,7 @@ func fileListener(f *os.File) (Listener, error) {
 }
 
 func filePacketConn(f *os.File) (PacketConn, error) {
-	fd, err := newFileFD(f, nil)
+	fd, err := newFileFD(f)
 	if err != nil {
 		return nil, err
 	}
@@ -130,56 +112,4 @@ func filePacketConn(f *os.File) (PacketConn, error) {
 	}
 	fd.Close()
 	return nil, syscall.EINVAL
-}
-
-func socketConn(f *os.File, sa SocketAddr) (Conn, error) {
-	fd, err := newFileFD(f, sa)
-	if err != nil {
-		return nil, err
-	}
-	return &socketFile{conn: conn{fd}, SocketAddr: sa}, nil
-}
-
-func socketPacketConn(f *os.File, sa SocketAddr) (PacketConn, error) {
-	fd, err := newFileFD(f, sa)
-	if err != nil {
-		return nil, err
-	}
-	return &socketFile{conn: conn{fd}, SocketAddr: sa}, nil
-}
-
-var (
-	_ Conn       = &socketFile{}
-	_ PacketConn = &socketFile{}
-)
-
-// A socketFile is a placeholder that holds a user-specified socket
-// descriptor and a profile of socket address encoding.
-// It implements both Conn and PacketConn interfaces.
-type socketFile struct {
-	conn
-	SocketAddr
-}
-
-func (c *socketFile) ReadFrom(b []byte) (int, Addr, error) {
-	if !c.ok() {
-		return 0, nil, syscall.EINVAL
-	}
-	from := make([]byte, syscall.SizeofSockaddrAny)
-	n, err := c.fd.recvFrom(b, 0, from)
-	if err != nil {
-		return n, nil, &OpError{Op: "read", Net: c.fd.net, Source: c.fd.laddr, Addr: c.fd.raddr, Err: err}
-	}
-	return n, c.SocketAddr.Addr(from), nil
-}
-
-func (c *socketFile) WriteTo(b []byte, addr Addr) (int, error) {
-	if !c.ok() {
-		return 0, syscall.EINVAL
-	}
-	n, err := c.fd.sendTo(b, 0, c.SocketAddr.Raw(addr))
-	if err != nil {
-		return n, &OpError{Op: "write", Net: c.fd.net, Source: c.fd.laddr, Addr: c.fd.raddr, Err: err}
-	}
-	return n, nil
 }
