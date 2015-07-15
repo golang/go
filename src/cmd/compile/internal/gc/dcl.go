50c772f57fdedbc5fc5e85398d737b11ff8188ff
Russ Cox <rsc@golang.org>
2015-07-15 04:11:26 +0000

    Revert "cmd/compile: define func value symbols at declaration"
    This reverts commit 3b411bf1a1bb08d6868083981cecba8088dc7aea.
    
    Change-Id: I321a43fa378a43b3e4d7aa97e0222775640af64b
    Reviewed-on: https://go-review.googlesource.com/12205
    Reviewed-by: Russ Cox <rsc@golang.org>

--- src/cmd/compile/internal/gc/dcl.go
+++ src/cmd/compile/internal/gc/dcl.go
@@ -585,10 +585,6 @@ func funchdr(n *Node) {
 		Fatal("funchdr: dclcontext")
 	}
 
-	if importpkg == nil && n.Func.Nname != nil {
-		makefuncsym(n.Func.Nname.Sym)
-	}
-
 	dclcontext = PAUTO
 	markdcl()
 	Funcdepth++
@@ -1493,16 +1489,12 @@ func funcsym(s *Sym) *Sym {
 	}
 
 	s1 := Pkglookup(s.Name+"·f", s.Pkg)
-	s.Fsym = s1
-	return s1
-}
-
-func makefuncsym(s *Sym) {
-	if isblanksym(s) {
-		return
+	if s1.Def == nil {
+		s1.Def = newfuncname(s1)
+		s1.Def.Func.Shortname = newname(s)
+		funcsyms = list(funcsyms, s1.Def)
 	}
-	s1 := funcsym(s)
-	s1.Def = newfuncname(s1)
-	s1.Def.Func.Shortname = newname(s)
-	funcsyms = list(funcsyms, s1.Def)
+	s.Fsym = s1
+
+	return s1
 }
