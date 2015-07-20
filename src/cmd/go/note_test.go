f7e7719f653b91cf84ed80c03dd4a17e0558b5eb
Ian Lance Taylor <iant@golang.org>
2015-07-19 18:14:53 -0700

    cmd/go: rewrite TestNoteReading to use test harness
    On my laptop reduces time required for test from 22 seconds to 0.14
    seconds.
    
    Update #11779.
    
    Change-Id: I715d85bd9c6f7683c6915eedd2539813aa5efc58
    Reviewed-on: https://go-review.googlesource.com/12363
    Reviewed-by: David Crawshaw <crawshaw@golang.org>
    Reviewed-by: Andrew Gerrand <adg@golang.org>

--- src/cmd/go/note_test.go
+++ src/cmd/go/note_test.go
@@ -2,42 +2,23 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-package main
+package main_test
 
 import (
-	"internal/testenv"
-	"io/ioutil"
-	"os"
-	"os/exec"
+	"cmd/go"
 	"testing"
 )
 
 func TestNoteReading(t *testing.T) {
-	testenv.MustHaveGoBuild(t)
-
-	// TODO: Replace with new test scaffolding by iant.
-	d, err := ioutil.TempDir("", "go-test-")
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer os.RemoveAll(d)
-
-	out, err := exec.Command("go", "build", "-o", d+"/go.exe", "cmd/go").CombinedOutput()
-	if err != nil {
-		t.Fatalf("go build cmd/go: %v\n%s", err, out)
-	}
-
+	tg := testgo(t)
+	defer tg.cleanup()
+	tg.tempFile("hello.go", `package main; func main() { print("hello, world\n") }`)
 	const buildID = "TestNoteReading-Build-ID"
-	out, err = exec.Command(d+"/go.exe", "build", "-ldflags", "-buildid="+buildID, "-o", d+"/hello.exe", "../../../test/helloworld.go").CombinedOutput()
-	if err != nil {
-		t.Fatalf("go build hello: %v\n%s", err, out)
-	}
-
-	id, err := readBuildIDFromBinary(d + "/hello.exe")
+	tg.run("build", "-ldflags", "-buildid="+buildID, "-o", tg.path("hello.exe"), tg.path("hello.go"))
+	id, err := main.ReadBuildIDFromBinary(tg.path("hello.exe"))
 	if err != nil {
 		t.Fatalf("reading build ID from hello binary: %v", err)
 	}
-
 	if id != buildID {
 		t.Fatalf("buildID in hello binary = %q, want %q", id, buildID)
 	}
