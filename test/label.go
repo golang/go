3170b9d8d2a44bbc0f4bd62449d71847f5d10e78
Josh Bleecher Snyder <josharian@gmail.com>
2015-07-20 13:00:28 -0700

    [dev.ssa] test: gofmt {goto,label,label1}.go
    Change-Id: I971d0c93632e39aad4e2ba1862f085df820baf8b
    Reviewed-on: https://go-review.googlesource.com/12431
    Reviewed-by: Brad Fitzpatrick <bradfitz@golang.org>

--- test/label.go
+++ test/label.go
@@ -17,8 +17,7 @@ L1: // ERROR "label .*L1.* defined and not used"
 	for {
 	}
 L2: // ERROR "label .*L2.* defined and not used"
-	select {
-	}
+	select {}
 L3: // ERROR "label .*L3.* defined and not used"
 	switch {
 	}
