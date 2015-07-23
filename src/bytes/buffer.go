b1177d390cf0c3733836e5941f77b828e21c8a09
Alan Donovan <adonovan@google.com>
2015-07-23 12:07:58 -0400

    bytes: document that Buffer values must not be copied
    Change-Id: If0821a2af987b78ed8024b40d9ffa68032518b22
    Reviewed-on: https://go-review.googlesource.com/12572
    Reviewed-by: Robert Griesemer <gri@golang.org>

--- src/bytes/buffer.go
+++ src/bytes/buffer.go
@@ -14,6 +14,7 @@ import (
 
 // A Buffer is a variable-sized buffer of bytes with Read and Write methods.
 // The zero value for Buffer is an empty buffer ready to use.
+// Buffer values must not be copied.
 type Buffer struct {
 	buf       []byte            // contents are the bytes buf[off : len(buf)]
 	off       int               // read at &buf[off], write at &buf[len(buf)]
