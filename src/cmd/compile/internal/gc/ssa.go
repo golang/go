762c1dcfd37a222cc37ee78e72cd4ce43c671ae8
Alexandru Moșoi <mosoi@google.com>
2015-07-30 12:33:36 +0200

    [dev.ssa] cmd/compile/internal/ssa/gen: implement OXOR.
    From compiling go there were 260 functions where XOR was needed.
    
    Much of the required changes for implementing XOR were already
    done in 12813.
    
    Change-Id: I5a68aa028f5ed597bc1d62cedbef3620753dfe82
    Reviewed-on: https://go-review.googlesource.com/12901
    Reviewed-by: Keith Randall <khr@golang.org>

--- src/cmd/compile/internal/gc/ssa.go
+++ src/cmd/compile/internal/gc/ssa.go
@@ -763,6 +763,15 @@ var opToSSA = map[opAndType]ssa.Op{
 	opAndType{OOR, TINT64}:  ssa.OpOr64,
 	opAndType{OOR, TUINT64}: ssa.OpOr64,
 
+	opAndType{OXOR, TINT8}:   ssa.OpXor8,
+	opAndType{OXOR, TUINT8}:  ssa.OpXor8,
+	opAndType{OXOR, TINT16}:  ssa.OpXor16,
+	opAndType{OXOR, TUINT16}: ssa.OpXor16,
+	opAndType{OXOR, TINT32}:  ssa.OpXor32,
+	opAndType{OXOR, TUINT32}: ssa.OpXor32,
+	opAndType{OXOR, TINT64}:  ssa.OpXor64,
+	opAndType{OXOR, TUINT64}: ssa.OpXor64,
+
 	opAndType{OEQ, TBOOL}:      ssa.OpEq8,
 	opAndType{OEQ, TINT8}:      ssa.OpEq8,
 	opAndType{OEQ, TUINT8}:     ssa.OpEq8,
@@ -1123,7 +1132,7 @@ func (s *state) expr(n *Node) *ssa.Value {
 		a := s.expr(n.Left)
 		b := s.expr(n.Right)
 		return s.newValue2(s.ssaOp(n.Op, n.Left.Type), ssa.TypeBool, a, b)
-	case OADD, OSUB, OMUL, OAND, OOR:
+	case OADD, OAND, OMUL, OOR, OSUB, OXOR:
 		a := s.expr(n.Left)
 		b := s.expr(n.Right)
 		return s.newValue2(s.ssaOp(n.Op, n.Type), a.Type, a, b)
