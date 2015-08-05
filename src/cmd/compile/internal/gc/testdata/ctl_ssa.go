ca088cf4e59a1e9ef97dbbf16f035a152a8ddda8
Josh Bleecher Snyder <josharian@gmail.com>
2015-08-05 15:56:31 -0700

    [dev.ssa] cmd/compile: handle phi control values
    Tests courtesy of Todd Neal.
    
    Change-Id: If657c7c7d3cd1ce01e9d9ad79eb6b2110230c0f9
    Reviewed-on: https://go-review.googlesource.com/13267
    Reviewed-by: Todd Neal <todd@tneal.org>
    Reviewed-by: Keith Randall <khr@golang.org>

---
+++ src/cmd/compile/internal/gc/testdata/ctl_ssa.go
@@ -0,0 +1,53 @@
+// run
+
+// Copyright 2015 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Test control flow
+
+package main
+
+// nor_ssa calculates NOR(a, b).
+// It is implemented in a way that generates
+// phi control values.
+func nor_ssa(a, b bool) bool {
+	var c bool
+	if a {
+		c = true
+	}
+	if b {
+		c = true
+	}
+	if c {
+		return false
+	}
+	return true
+}
+
+func testPhiControl() {
+	tests := [...][3]bool{ // a, b, want
+		{false, false, true},
+		{true, false, false},
+		{false, true, false},
+		{true, true, false},
+	}
+	for _, test := range tests {
+		a, b := test[0], test[1]
+		got := nor_ssa(a, b)
+		want := test[2]
+		if want != got {
+			print("nor(", a, ", ", b, ")=", want, " got ", got, "\n")
+			failed = true
+		}
+	}
+}
+
+var failed = false
+
+func main() {
+	testPhiControl()
+	if failed {
+		panic("failed")
+	}
+}
