8c2c35debebaaa86aab1f180940f0a77db0b6f30
Nodir Turakulov <nodir@google.com>
2015-09-04 10:05:31 -0700

    net/http/httptest: ResponseRecorder.WriteString
    Fixes #11000
    
    Change-Id: Ic137e8a6c5c6b5b7eee213aca9acf78368e1d686
    Reviewed-on: https://go-review.googlesource.com/14296
    Reviewed-by: Brad Fitzpatrick <bradfitz@golang.org>
    Run-TryBot: Brad Fitzpatrick <bradfitz@golang.org>
    TryBot-Result: Gobot Gobot <gobot@golang.org>

--- src/net/http/httptest/recorder_test.go
+++ src/net/http/httptest/recorder_test.go
@@ -6,6 +6,7 @@ package httptest
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 	"testing"
 )
@@ -64,6 +65,13 @@ func TestRecorder(t *testing.T) {
 				w.Write([]byte("hi first"))
 				w.WriteHeader(201)
 				w.WriteHeader(202)
+			},
+			check(hasStatus(200), hasContents("hi first"), hasFlush(false)),
+		},
+		{
+			"write string",
+			func(w http.ResponseWriter, r *http.Request) {
+				io.WriteString(w, "hi first")
 			},
 			check(hasStatus(200), hasContents("hi first"), hasFlush(false)),
 		},
