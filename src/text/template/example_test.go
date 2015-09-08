3842b8f7604d90195ebdcc9b642c664a3d260fc0
Rob Pike <r@golang.org>
2015-09-08 14:58:12 -0700

    text/template: provide a way to trim leading and trailing space between actions
    Borrowing a suggestion from the issue listed below, we modify the lexer to
    trim spaces at the beginning (end) of a block of text if the action immediately
    before (after) is marked with a minus sign. To avoid parsing/lexing ambiguity,
    we require an ASCII space between the minus sign and the rest of the action.
    Thus:
    
    	{{23 -}}
    	<
    	{{- 45}}
    
    produces the output
    	23<45
    
    All the work is done in the lexer. The modification is invisible to the parser
    or any outside package (except I guess for noticing some gaps in the input
    if one tracks error positions). Thus it slips in without worry in text/template
    and html/template both.
    
    Fixes long-requested issue #9969.
    
    Change-Id: I3774be650bfa6370cb993d0899aa669c211de7b2
    Reviewed-on: https://go-review.googlesource.com/14391
    Reviewed-by: Andrew Gerrand <adg@golang.org>

--- src/text/template/example_test.go
+++ src/text/template/example_test.go
@@ -15,9 +15,12 @@ func ExampleTemplate() {
 	const letter = `
 Dear {{.Name}},
 {{if .Attended}}
-It was a pleasure to see you at the wedding.{{else}}
-It is a shame you couldn't make it to the wedding.{{end}}
-{{with .Gift}}Thank you for the lovely {{.}}.
+It was a pleasure to see you at the wedding.
+{{- else}}
+It is a shame you couldn't make it to the wedding.
+{{- end}}
+{{with .Gift -}}
+Thank you for the lovely {{.}}.
 {{end}}
 Best wishes,
 Josie
