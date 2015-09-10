9b6f9efc5cbfa1873feba374ae8e3fd821fccc19
Yao Zhang <lunaria21@gmail.com>
2015-09-10 11:33:26 -0400

    hash/crc32: added mips64{,le} build tags
    Change-Id: I77c6768fff6f0163b36800307c4d573bb6521fe5
    Reviewed-on: https://go-review.googlesource.com/14454
    Reviewed-by: Minux Ma <minux@golang.org>
    Reviewed-by: Ian Lance Taylor <iant@golang.org>

--- src/hash/crc32/crc32_generic.go
+++ src/hash/crc32/crc32_generic.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// +build 386 arm arm64 ppc64 ppc64le
+// +build 386 arm arm64 mips64 mips64le ppc64 ppc64le
 
 package crc32
 
