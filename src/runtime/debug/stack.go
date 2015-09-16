b6b6fe6465e4b339b143a59df925a4781e98302d
David Crawshaw <crawshaw@golang.org>
2015-09-16 10:29:53 -0400

    runtime/debug: more explicit Stack docs
    Change-Id: I81a7f22be827519b5290b4acbcba357680cad3c4
    Reviewed-on: https://go-review.googlesource.com/14605
    Reviewed-by: Rob Pike <r@golang.org>

--- src/runtime/debug/stack.go
+++ src/runtime/debug/stack.go
@@ -11,15 +11,13 @@ import (
 	"runtime"
 )
 
-// PrintStack prints to standard error the stack trace returned by Stack.
+// PrintStack prints to standard error the stack trace returned by runtime.Stack.
 func PrintStack() {
 	os.Stderr.Write(Stack())
 }
 
 // Stack returns a formatted stack trace of the goroutine that calls it.
-// For each routine, it includes the source line information and PC value,
-// then attempts to discover, for Go functions, the calling function or
-// method.
+// It calls runtime.Stack with a large enough buffer to capture the entire trace.
 func Stack() []byte {
 	buf := make([]byte, 1024)
 	for {
