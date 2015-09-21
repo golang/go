03feb61781275ef6aee91713a75a938f870c01f4
Robert Griesemer <gri@golang.org>
2015-09-21 15:58:19 -0700

    src/cmd/compile/internal/gc: remove now unnecessary restriction on float exponent parsing
    https://go-review.googlesource.com/#/c/13778/ fixed this issue in math/big.
    Remove restriction in compiler.
    
    Fixes #11326.
    
    Change-Id: I1429d0dd0d79431706c65616413373fff58f081e
    Reviewed-on: https://go-review.googlesource.com/14830
    Reviewed-by: Rob Pike <r@golang.org>

--- test/fixedbugs/issue11326.go
+++ test/fixedbugs/issue11326.go
@@ -4,25 +4,20 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
+// Tests for golang.org/issue/11326.
+
 package main
 
-import "fmt"
-
 func main() {
-	var g = 1e81391777742999 // ERROR "exponent too large"
-	// The next should only cause a problem when converted to float64
-	// by the assignment, but instead the compiler rejects it outright,
-	// rather than mishandle it. Specifically, when handled, 'var h' prints:
-	//	issue11326.go:N: constant 0.93342e+536870911 overflows float64
-	// The rejection of 'var i' is just insurance. It seems to work correctly.
-	// See golang.org/issue/11326.
-	// var h = 1e2147483647     // should be "1.00000e+2147483647 overflows float64"
-	var h = 1e2147483647 // ERROR "exponent too large"
-	// var i = 1e214748364  // should be "1.00000e\+214748364 overflows float64"
-	var i = 1e214748364 // ERROR "exponent too large"
-	var j = 1e21474836  // ERROR "1.00000e\+21474836 overflows float64"
-	var k = 1e2147483   // ERROR "1.00000e\+2147483 overflows float64"
-	var l = 1e214748    // ERROR "1.00000e\+214748 overflows float64"
-	var m = 1e21474     // ERROR "1.00000e\+21474 overflows float64"
-	fmt.Println(g)
+	var _ = 1e2147483647 // ERROR "constant too large"
+	var _ = 1e646456993  // ERROR "constant too large"
+	var _ = 1e646456992  // ERROR "1.00000e\+646456992 overflows float64"
+	var _ = 1e64645699   // ERROR "1.00000e\+64645699 overflows float64"
+	var _ = 1e6464569    // ERROR "1.00000e\+6464569 overflows float64"
+	var _ = 1e646456     // ERROR "1.00000e\+646456 overflows float64"
+	var _ = 1e64645      // ERROR "1.00000e\+64645 overflows float64"
+	var _ = 1e6464       // ERROR "1.00000e\+6464 overflows float64"
+	var _ = 1e646        // ERROR "1.00000e\+646 overflows float64"
+	var _ = 1e309        // ERROR "1.00000e\+309 overflows float64"
+	var _ = 1e308
 }
