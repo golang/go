89e2f0083884917fc2a1af4c2d8509d79b736883
Yao Zhang <lunaria21@gmail.com>
2015-09-23 18:58:05 -0400

    runtime: added mips64{,le} build tags and GOARCH cases
    Change-Id: I381c03d957a0dccae5f655f02e92760e5c0e9629
    Reviewed-on: https://go-review.googlesource.com/14929
    Reviewed-by: Ian Lance Taylor <iant@golang.org>
    Reviewed-by: Minux Ma <minux@golang.org>

--- src/runtime/gcinfo_test.go
+++ src/runtime/gcinfo_test.go
@@ -144,7 +144,7 @@ func infoBigStruct() []byte {
 			typeScalar, typeScalar, typeScalar, typeScalar, // t int; y uint16; u uint64
 			typePointer, typeScalar, // i string
 		}
-	case "arm64", "amd64", "ppc64", "ppc64le":
+	case "arm64", "amd64", "mips64", "mips64le", "ppc64", "ppc64le":
 		return []byte{
 			typePointer,                        // q *int
 			typeScalar, typeScalar, typeScalar, // w byte; e [17]byte
--- src/runtime/unaligned2.go
+++ src/runtime/unaligned2.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// +build arm ppc64 ppc64le
+// +build arm ppc64 ppc64le mips64 mips64le
 
 package runtime
 
