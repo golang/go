624f84536d52b13066c268c83e1b84c088d29471
Yao Zhang <lunaria21@gmail.com>
2015-09-24 08:50:13 -0400

    runtime: renamed os2_linux.go to os2_linux_generic.go, added mips64 support
    Linux/mips64 has a different sigset type and some different constants.
    os2_linux.go is renamed to os2_linux_generic.go, and not used in mips64.
    The corresponding file os2_linux_mips64x.go is added.
    
    Change-Id: Ief83845a2779f7fe048d236d3c7da52b627ab533
    Reviewed-on: https://go-review.googlesource.com/14992
    Reviewed-by: Minux Ma <minux@golang.org>

--- src/runtime/os2_linux_generic.go
+++ src/runtime/os2_linux_generic.go
@@ -2,6 +2,10 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
+// +build !mips64
+// +build !mips64le
+// +build linux
+
 package runtime
 
 const (
