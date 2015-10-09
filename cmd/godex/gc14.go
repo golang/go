fe3445e822d021942dd58cd4e2ed5cc833103d8a
Robert Griesemer <gri@golang.org>
2015-10-09 13:35:07 -0700

    x/tools/cmd/godex: prepare sources for switch to 1.5 go/types
    - copied all affected xxx.go files to a respective xxx14.go file
    - added build tags
    - no other source changes
    
    Change-Id: Ic150a097e537ee29a0e0616ab8e1e74b21e3066d
    Reviewed-on: https://go-review.googlesource.com/15702
    Reviewed-by: Alan Donovan <adonovan@google.com>

--- cmd/godex/gc14.go
+++ cmd/godex/gc14.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// +build go1.5
+// +build !go1.5
 
 // This file implements access to gc-generated export data.
 
