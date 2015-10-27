00f42437fdc68684f36412243adc1c1458369f0e
Michael Hudson-Doyle <michael.hudson@canonical.com>
2015-10-27 16:30:32 +1300

    cmd/internal/obj/x86: remove REGTMP
    Nothing uses this.
    
    Change-Id: Ibc13066940bd2ea5c74d955a67f9dc531bef2758
    Reviewed-on: https://go-review.googlesource.com/16344
    Reviewed-by: Brad Fitzpatrick <bradfitz@golang.org>

--- src/cmd/internal/obj/x86/a.out.go
+++ src/cmd/internal/obj/x86/a.out.go
@@ -904,7 +904,6 @@ const (
 	REGRET   = REG_AX
 	FREGRET  = REG_X0
 	REGSP    = REG_SP
-	REGTMP   = REG_DI
 	REGCTXT  = REG_DX
 	REGEXT   = REG_R15     /* compiler allocates external registers R15 down */
 	FREGMIN  = REG_X0 + 5  /* first register variable */
