10cb39afc4e81d126c71153dba7b9f1878bae61a
Matthew Dempsky <mdempsky@google.com>
2015-11-25 13:09:14 -0800

    cmd/cgo: fix C.complexfloat and C.complexdouble
    This also fixes an unintended behavior where C's "complex float" and
    "complex double" types were interchangeable with Go's "complex64" and
    "complex128" types.
    
    Fixes #13402.
    
    Change-Id: I73f96d9a4772088d495073783c6982e9634430e8
    Reviewed-on: https://go-review.googlesource.com/17208
    Reviewed-by: Ian Lance Taylor <iant@golang.org>

--- src/cmd/cgo/doc.go
+++ src/cmd/cgo/doc.go
@@ -117,7 +117,8 @@ The standard C numeric types are available under the names
 C.char, C.schar (signed char), C.uchar (unsigned char),
 C.short, C.ushort (unsigned short), C.int, C.uint (unsigned int),
 C.long, C.ulong (unsigned long), C.longlong (long long),
-C.ulonglong (unsigned long long), C.float, C.double.
+C.ulonglong (unsigned long long), C.float, C.double,
+C.complexfloat (complex float), and C.complexdouble (complex double).
 The C type void* is represented by Go's unsafe.Pointer.
 The C types __int128_t and __uint128_t are represented by [16]byte.
 
