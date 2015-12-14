aa812300c7568d7bd730a586c877b2328aec61c4
Russ Cox <rsc@golang.org>
2015-12-14 13:42:13 -0500

    cmd/compile: diagnose invalid switch interface{} case earlier
    Fixes #11737.
    
    Change-Id: Id231b502ac5a44035dc3a02515b43bf665cb1e87
    Reviewed-on: https://go-review.googlesource.com/17816
    Reviewed-by: Ian Lance Taylor <iant@golang.org>

--- src/cmd/compile/internal/gc/swt.go
+++ src/cmd/compile/internal/gc/swt.go
@@ -139,6 +139,8 @@ func typecheckswitch(n *Node) {
 						}
 					case nilonly != "" && !isnil(ll.N):
 						Yyerror("invalid case %v in switch (can only compare %s %v to nil)", ll.N, nilonly, n.Left)
+					case Isinter(t) && !Isinter(ll.N.Type) && algtype1(ll.N.Type, nil) == ANOEQ:
+						Yyerror("invalid case %v in switch (incomparable type)", Nconv(ll.N, obj.FmtLong))
 					}
 
 				// type switch
