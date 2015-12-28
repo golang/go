58c73de7d09ca750f5d1927448f086ee25977dad
Ian Lance Taylor <iant@golang.org>
2015-12-28 11:29:22 -0800

    os, runtime: better EPIPE behavior for command line programs
    Old behavior: 10 consecutive EPIPE errors on any descriptor cause the
    program to exit with a SIGPIPE signal.
    
    New behavior: an EPIPE error on file descriptors 1 or 2 cause the
    program to raise a SIGPIPE signal.  If os/signal.Notify was not used to
    catch SIGPIPE signals, this will cause the program to exit with SIGPIPE.
    An EPIPE error on a file descriptor other than 1 or 2 will simply be
    returned from Write.
    
    Fixes #11845.
    Update #9896.
    
    Change-Id: Ic85d77e386a8bb0255dc4be1e4b3f55875d10f18
    Reviewed-on: https://go-review.googlesource.com/18151
    Reviewed-by: Russ Cox <rsc@golang.org>

--- src/os/signal/doc.go
+++ src/os/signal/doc.go
@@ -87,6 +87,18 @@ for a blocked signal, it will be unblocked. If, later, Reset is
 called for that signal, or Stop is called on all channels passed to
 Notify for that signal, the signal will once again be blocked.
 
+SIGPIPE
+
+When a Go program receives an EPIPE error from the kernel while
+writing to file descriptors 1 or 2 (standard output or standard
+error), it will raise a SIGPIPE signal.  If the program is not
+currently receiving SIGPIPE via a call to Notify, this will cause the
+program to exit with SIGPIPE.  On descriptors other than 1 or 2, the
+write will return the EPIPE error.  This means that, by default,
+command line programs will behave like typical Unix command line
+programs, while other programs will not crash with SIGPIPE when
+writing to a closed network connection.
+
 Go programs that use cgo or SWIG
 
 In a Go program that includes non-Go code, typically C/C++ code
