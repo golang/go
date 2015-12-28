f75338cbc6f21a4058f39115735379386a179e1c
Alan Donovan <adonovan@google.com>
2015-12-28 13:59:28 -0500

    go/types: skip two failing $GOROOT/test/fixedbugs tests
    Change-Id: I7dfb95f34ae23c0b7905714b32068dc336a05656
    Reviewed-on: https://go-review.googlesource.com/18192
    Reviewed-by: Brad Fitzpatrick <bradfitz@golang.org>

--- go/types/stdlib_test.go
+++ go/types/stdlib_test.go
@@ -143,6 +143,8 @@ func TestStdFixed(t *testing.T) {
 		"issue7746.go",   // large constants - consumes too much memory
 		"issue11326.go",  // large constants
 		"issue11326b.go", // large constants
+		"issue11362.go",  // canonical import path check is implementation-defined behavior
+		"issue13471.go",  // large constants
 	)
 }
 
