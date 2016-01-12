546daad00410e2e76ac13a27b1da3e598187338b
Keith Randall <khr@golang.org>
2016-01-12 12:42:28 -0800

    cmd/compile: stop using fucomi* ops for 387 builds
    The fucomi* opcodes were only introduced for the Pentium Pro.
    They do not exist for an MMX Pentium.  Use the fucom* instructions
    instead and move the condition codes from the fp flags register to
    the integer flags register explicitly.
    
    The use of fucomi* opcodes in ggen.go was introduced in 1.5 (CL 8738).
    The bad ops were generated for 64-bit floating-point comparisons.
    
    The use of fucomi* opcodes in gsubr.go dates back to at least 1.1.
    The bad ops were generated for float{32,64} to uint64 conversions.
    
    Fixes #13923
    
    Change-Id: I5290599f5edea8abf8fb18036f44fa78bd1fc9e6
    Reviewed-on: https://go-review.googlesource.com/18590
    Reviewed-by: Minux Ma <minux@golang.org>
    Reviewed-by: Russ Cox <rsc@golang.org>

--- src/cmd/compile/internal/x86/prog.go
+++ src/cmd/compile/internal/x86/prog.go
@@ -91,8 +91,12 @@ var progtable = [x86.ALAST]obj.ProgInfo{
 	x86.AFCOMDPP:   {Flags: gc.SizeD | gc.LeftAddr | gc.RightRead},
 	x86.AFCOMF:     {Flags: gc.SizeF | gc.LeftAddr | gc.RightRead},
 	x86.AFCOMFP:    {Flags: gc.SizeF | gc.LeftAddr | gc.RightRead},
-	x86.AFUCOMIP:   {Flags: gc.SizeF | gc.LeftAddr | gc.RightRead},
-	x86.AFCHS:      {Flags: gc.SizeD | RightRdwr}, // also SizeF
+	// NOTE(khr): don't use FUCOMI* instructions, not available
+	// on Pentium MMX.  See issue 13923.
+	//x86.AFUCOMIP:   {Flags: gc.SizeF | gc.LeftAddr | gc.RightRead},
+	x86.AFUCOMP:  {Flags: gc.SizeD | gc.LeftRead | gc.RightRead},
+	x86.AFUCOMPP: {Flags: gc.SizeD | gc.LeftRead | gc.RightRead},
+	x86.AFCHS:    {Flags: gc.SizeD | RightRdwr}, // also SizeF
 
 	x86.AFDIVDP:  {Flags: gc.SizeD | gc.LeftAddr | RightRdwr},
 	x86.AFDIVF:   {Flags: gc.SizeF | gc.LeftAddr | RightRdwr},
