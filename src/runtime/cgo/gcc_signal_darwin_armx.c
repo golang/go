ba439e7db6828f7c525974df04f71cecc7f928f1
Russ Cox <rsc@golang.org>
2016-01-27 16:27:03 -0500

    runtime/cgo: more +build cgo tags
    Followup to CL 19001.
    
    Change-Id: I7fa838b1ee8df53229e9dd29a231c2f9b2aa3f69
    Reviewed-on: https://go-review.googlesource.com/19003
    Reviewed-by: Brad Fitzpatrick <bradfitz@golang.org>

--- src/runtime/cgo/gcc_signal_darwin_armx.c
+++ src/runtime/cgo/gcc_signal_darwin_armx.c
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
+// +build cgo
+
 // Emulation of the Unix signal SIGSEGV.
 //
 // On iOS, Go tests and apps under development are run by lldb.
