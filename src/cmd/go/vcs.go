3e23442518d9c66def2ae48f83026e29c5f26609
Russ Cox <rsc@golang.org>
2016-01-28 22:51:50 -0500

    cmd/go: remove GO15VENDOREXPERIMENT variable
    The Go 1.6 release notes say that Go 1.7 will remove support
    for the GO15VENDOREXPERIMENT environment variable,
    making vendoring always on. Do that.
    
    Change-Id: Iba8b79532455828869c1a8076a82edce84259468
    Reviewed-on: https://go-review.googlesource.com/19615
    Reviewed-by: Ian Lance Taylor <iant@golang.org>

--- src/cmd/go/vcs.go
+++ src/cmd/go/vcs.go
@@ -383,7 +383,7 @@ func (v *vcsCmd) ping(scheme, repo string) error {
 // The parent of dir must exist; dir must not.
 func (v *vcsCmd) create(dir, repo string) error {
 	for _, cmd := range v.createCmd {
-		if !go15VendorExperiment && strings.Contains(cmd, "submodule") {
+		if strings.Contains(cmd, "submodule") {
 			continue
 		}
 		if err := v.run(".", cmd, "dir", dir, "repo", repo); err != nil {
@@ -396,7 +396,7 @@ func (v *vcsCmd) create(dir, repo string) error {
 // download downloads any new changes for the repo in dir.
 func (v *vcsCmd) download(dir string) error {
 	for _, cmd := range v.downloadCmd {
-		if !go15VendorExperiment && strings.Contains(cmd, "submodule") {
+		if strings.Contains(cmd, "submodule") {
 			continue
 		}
 		if err := v.run(dir, cmd); err != nil {
@@ -445,7 +445,7 @@ func (v *vcsCmd) tagSync(dir, tag string) error {
 
 	if tag == "" && v.tagSyncDefault != nil {
 		for _, cmd := range v.tagSyncDefault {
-			if !go15VendorExperiment && strings.Contains(cmd, "submodule") {
+			if strings.Contains(cmd, "submodule") {
 				continue
 			}
 			if err := v.run(dir, cmd); err != nil {
@@ -456,7 +456,7 @@ func (v *vcsCmd) tagSync(dir, tag string) error {
 	}
 
 	for _, cmd := range v.tagSyncCmd {
-		if !go15VendorExperiment && strings.Contains(cmd, "submodule") {
+		if strings.Contains(cmd, "submodule") {
 			continue
 		}
 		if err := v.run(dir, cmd, "tag", tag); err != nil {
