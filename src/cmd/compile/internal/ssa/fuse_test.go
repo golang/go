5949524fc48aa514154cfb939ae28af58aaf6540
Alexandru Moșoi <mosoi@google.com>
2016-02-17 17:21:53 +0100

    [dev.ssa] cmd/compile/internal/ssa: handle phis in fuse.
    Change-Id: Idd880cc6c1e5dc34dddbdea0841a7a718d2fa836
    Reviewed-on: https://go-review.googlesource.com/19544
    Reviewed-by: David Chase <drchase@google.com>

--- src/cmd/compile/internal/ssa/fuse_test.go
+++ src/cmd/compile/internal/ssa/fuse_test.go
@@ -65,6 +65,40 @@ func TestFuseEliminatesBothBranches(t *testing.T) {
 	}
 }
 
+func TestFuseHandlesPhis(t *testing.T) {
+	ptrType := &TypeImpl{Size_: 8, Ptr: true, Name: "testptr"} // dummy for testing
+	c := NewConfig("amd64", DummyFrontend{t}, nil, true)
+	fun := Fun(c, "entry",
+		Bloc("entry",
+			Valu("mem", OpInitMem, TypeMem, 0, nil),
+			Valu("sb", OpSB, TypeInvalid, 0, nil),
+			Goto("checkPtr")),
+		Bloc("checkPtr",
+			Valu("ptr1", OpLoad, ptrType, 0, nil, "sb", "mem"),
+			Valu("nilptr", OpConstNil, ptrType, 0, nil, "sb"),
+			Valu("bool1", OpNeqPtr, TypeBool, 0, nil, "ptr1", "nilptr"),
+			If("bool1", "then", "else")),
+		Bloc("then",
+			Goto("exit")),
+		Bloc("else",
+			Goto("exit")),
+		Bloc("exit",
+			Valu("phi", OpPhi, ptrType, 0, nil, "ptr1", "ptr1"),
+			Exit("mem")))
+
+	CheckFunc(fun.f)
+	fuse(fun.f)
+
+	for _, b := range fun.f.Blocks {
+		if b == fun.blocks["then"] && b.Kind != BlockInvalid {
+			t.Errorf("then was not eliminated, but should have")
+		}
+		if b == fun.blocks["else"] && b.Kind != BlockInvalid {
+			t.Errorf("then was not eliminated, but should have")
+		}
+	}
+}
+
 func TestFuseEliminatesEmptyBlocks(t *testing.T) {
 	c := NewConfig("amd64", DummyFrontend{t}, nil, true)
 	fun := Fun(c, "entry",
