8cfaf8e112c5e9209519012451736e427ff5c825
Ian Lance Taylor <iant@golang.org>
2016-02-19 11:42:34 -0800

    cmd/api: fix benchmark to ignore internal packages
    Change-Id: I8ee46287ae0744efa83ad343997ad6835520fa5c
    Reviewed-on: https://go-review.googlesource.com/19688
    Run-TryBot: Ian Lance Taylor <iant@golang.org>
    TryBot-Result: Gobot Gobot <gobot@golang.org>
    Reviewed-by: Brad Fitzpatrick <bradfitz@golang.org>

--- src/cmd/api/goapi_test.go
+++ src/cmd/api/goapi_test.go
@@ -178,7 +178,7 @@ func BenchmarkAll(b *testing.B) {
 		for _, context := range contexts {
 			w := NewWalker(context, filepath.Join(build.Default.GOROOT, "src"))
 			for _, name := range pkgNames {
-				if name != "unsafe" && !strings.HasPrefix(name, "cmd/") {
+				if name != "unsafe" && !strings.HasPrefix(name, "cmd/") && !internalPkg.MatchString(name) {
 					pkg, _ := w.Import(name)
 					w.export(pkg)
 				}
