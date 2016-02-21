5dc053b9dec4dc25fac195065ad32462ac28a543
Martin Möhrmann <martisch@uos.de>
2016-02-21 10:46:59 +0100

    fmt: fix zero padding for NaN
    Makes zero padding of NaN and infinities consistent
    by using spaces instead of zeroes to pad NaN.
    Adds more tests for NaN formatting.
    
    Fixes #14421
    
    Change-Id: Ia20f8e878cc81ac72a744ec10d65e84b94e09c6a
    Reviewed-on: https://go-review.googlesource.com/19723
    Reviewed-by: Rob Pike <r@golang.org>

--- src/fmt/format.go
+++ src/fmt/format.go
@@ -414,11 +414,15 @@ func (f *fmt) formatFloat(v float64, verb byte, prec, n int) {
 	if f.space && num[0] == '+' {
 		num[0] = ' '
 	}
-	// Special handling for "+Inf" and "-Inf",
+	// Special handling for infinities and NaN,
 	// which don't look like a number so shouldn't be padded with zeros.
-	if num[1] == 'I' {
+	if num[1] == 'I' || num[1] == 'N' {
 		oldZero := f.zero
 		f.zero = false
+		// Remove sign before NaN if not asked for.
+		if num[1] == 'N' && !f.space && !f.plus {
+			num = num[1:]
+		}
 		f.pad(num)
 		f.zero = oldZero
 		return
