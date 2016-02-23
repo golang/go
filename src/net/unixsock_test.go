08f1a778c90b572716c6333c792e5f9bacef81c2
Mikio Hara <mikioh.mikioh@gmail.com>
2016-02-23 18:06:31 +0900

    net: rename test files
    This change renames {ipraw,tcp,udp,unix}_test.go to
    {ipraw,tcp,udp,unix}sock_test.go for clarification. Also moves
    NSS-related system configuration test helpers into main_conf_test.go and
    main_noconf_test.go.
    
    Change-Id: I28ba1e8ceda7b182ee3aa85f0ca3321388ba45e2
    Reviewed-on: https://go-review.googlesource.com/19787
    Reviewed-by: Ian Lance Taylor <iant@golang.org>

--- src/net/unixsock_test.go
+++ src/net/unixsock_test.go
@@ -440,34 +440,3 @@ func TestUnixUnlink(t *testing.T) {
 		t.Fatal("closing unix listener did not remove unix socket")
 	}
 }
-
-// forceGoDNS forces the resolver configuration to use the pure Go resolver
-// and returns a fixup function to restore the old settings.
-func forceGoDNS() func() {
-	c := systemConf()
-	oldGo := c.netGo
-	oldCgo := c.netCgo
-	fixup := func() {
-		c.netGo = oldGo
-		c.netCgo = oldCgo
-	}
-	c.netGo = true
-	c.netCgo = false
-	return fixup
-}
-
-// forceCgoDNS forces the resolver configuration to use the cgo resolver
-// and returns a fixup function to restore the old settings.
-// (On non-Unix systems forceCgoDNS returns nil.)
-func forceCgoDNS() func() {
-	c := systemConf()
-	oldGo := c.netGo
-	oldCgo := c.netCgo
-	fixup := func() {
-		c.netGo = oldGo
-		c.netCgo = oldCgo
-	}
-	c.netGo = false
-	c.netCgo = true
-	return fixup
-}
