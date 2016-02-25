4a346e7489038a0913f590da98a12f6e660b683a
Keith Randall <khr@golang.org>
2016-02-25 13:45:22 -0800

    [dev.ssa] cmd/compile: get rid of nil checks before float loads/stores
    Just like we do for integer loads/stores.
    
    Update #14511
    
    Change-Id: Ic6ca6b54301438a5701ea5fb0be755451cb24d45
    Reviewed-on: https://go-review.googlesource.com/19923
    Reviewed-by: Josh Bleecher Snyder <josharian@gmail.com>
    Run-TryBot: Keith Randall <khr@golang.org>

--- src/cmd/compile/internal/gc/ssa.go
+++ src/cmd/compile/internal/gc/ssa.go
@@ -4588,7 +4588,9 @@ func (s *genState) genValue(v *ssa.Value) {
 			case ssa.OpAMD64MOVQload, ssa.OpAMD64MOVLload, ssa.OpAMD64MOVWload, ssa.OpAMD64MOVBload,
 				ssa.OpAMD64MOVQstore, ssa.OpAMD64MOVLstore, ssa.OpAMD64MOVWstore, ssa.OpAMD64MOVBstore,
 				ssa.OpAMD64MOVBQSXload, ssa.OpAMD64MOVBQZXload, ssa.OpAMD64MOVWQSXload,
-				ssa.OpAMD64MOVWQZXload, ssa.OpAMD64MOVLQSXload, ssa.OpAMD64MOVLQZXload:
+				ssa.OpAMD64MOVWQZXload, ssa.OpAMD64MOVLQSXload, ssa.OpAMD64MOVLQZXload,
+				ssa.OpAMD64MOVSSload, ssa.OpAMD64MOVSDload, ssa.OpAMD64MOVOload,
+				ssa.OpAMD64MOVSSstore, ssa.OpAMD64MOVSDstore, ssa.OpAMD64MOVOstore:
 				if w.Args[0] == v.Args[0] && w.Aux == nil && w.AuxInt >= 0 && w.AuxInt < minZeroPage {
 					if Debug_checknil != 0 && int(v.Line) > 1 {
 						Warnl(int(v.Line), "removed nil check")
@@ -4605,6 +4607,11 @@ func (s *genState) genValue(v *ssa.Value) {
 				}
 			}
 			if w.Type.IsMemory() {
+				if w.Op == ssa.OpVarDef || w.Op == ssa.OpVarKill || w.Op == ssa.OpVarLive {
+					// these ops are OK
+					mem = w
+					continue
+				}
 				// We can't delay the nil check past the next store.
 				break
 			}
