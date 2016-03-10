5f38122e9b4bffa45ac48a2361a847d964c07b58
Adam Langley <agl@golang.org>
2016-03-10 15:31:11 -0800

    crypto/x509: correct OID for DSA-with-SHA-256.
    I copied this down incorrectly. See
    https://tools.ietf.org/html/rfc5758#section-3.1.
    
    Thankfully it's incredibly obscure.
    
    Fixes #14663.
    
    Change-Id: I4792ee979916cc15aa2e300c8bc989bc4f9c63ac
    Reviewed-on: https://go-review.googlesource.com/20549
    Reviewed-by: Brad Fitzpatrick <bradfitz@golang.org>
    Run-TryBot: Adam Langley <agl@golang.org>

--- src/crypto/x509/x509.go
+++ src/crypto/x509/x509.go
@@ -270,7 +270,7 @@ var (
 	oidSignatureSHA384WithRSA   = asn1.ObjectIdentifier{1, 2, 840, 113549, 1, 1, 12}
 	oidSignatureSHA512WithRSA   = asn1.ObjectIdentifier{1, 2, 840, 113549, 1, 1, 13}
 	oidSignatureDSAWithSHA1     = asn1.ObjectIdentifier{1, 2, 840, 10040, 4, 3}
-	oidSignatureDSAWithSHA256   = asn1.ObjectIdentifier{2, 16, 840, 1, 101, 4, 3, 2}
+	oidSignatureDSAWithSHA256   = asn1.ObjectIdentifier{2, 16, 840, 1, 101, 3, 4, 3, 2}
 	oidSignatureECDSAWithSHA1   = asn1.ObjectIdentifier{1, 2, 840, 10045, 4, 1}
 	oidSignatureECDSAWithSHA256 = asn1.ObjectIdentifier{1, 2, 840, 10045, 4, 3, 2}
 	oidSignatureECDSAWithSHA384 = asn1.ObjectIdentifier{1, 2, 840, 10045, 4, 3, 3}
