3d18e935d13c0f18aed22ebfb48e816168765050
Michael Munday <munday@ca.ibm.com>
2016-03-20 19:35:59 -0400

    test: skip nilptr3 test on s390x
    Fails for the same reason as ppc64 and mips64 (incomplete
    optimization).
    
    Change-Id: Ieb4d997fc27d4f2b756e63dd7f588abe10c0213a
    Reviewed-on: https://go-review.googlesource.com/20963
    Reviewed-by: Bill O'Farrell <billotosyr@gmail.com>
    Reviewed-by: Brad Fitzpatrick <bradfitz@golang.org>

--- test/nilptr3.go
+++ test/nilptr3.go
@@ -1,8 +1,8 @@
 // errorcheck -0 -d=nil
 // Fails on ppc64x because of incomplete optimization.
 // See issues 9058.
-// Same reason for mips64x.
-// +build !ppc64,!ppc64le,!mips64,!mips64le,!amd64
+// Same reason for mips64x and s390x.
+// +build !ppc64,!ppc64le,!mips64,!mips64le,!amd64,!s390x
 
 // Copyright 2013 The Go Authors.  All rights reserved.
 // Use of this source code is governed by a BSD-style
