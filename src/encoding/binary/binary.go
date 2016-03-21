fc1bc2752abda48d767b8b4b743a2cb995587269
Alexandru Moșoi <mosoi@google.com>
2016-03-21 15:05:54 +0100

    encoding/binary: fix bound check
    The inserted early bound checks cause the slice
    to expand beyond the original length of the slice.
    
    Change-Id: Ib38891605f4a9a12d3b9e2071a5f77640b083d2d
    Reviewed-on: https://go-review.googlesource.com/20981
    Reviewed-by: Keith Randall <khr@golang.org>
    Reviewed-by: Minux Ma <minux@golang.org>

--- src/encoding/binary/binary.go
+++ src/encoding/binary/binary.go
@@ -49,23 +49,23 @@ var BigEndian bigEndian
 type littleEndian struct{}
 
 func (littleEndian) Uint16(b []byte) uint16 {
-	b = b[:2] // bounds check hint to compiler; see golang.org/issue/14808
+	b = b[:2:len(b)] // bounds check hint to compiler; see golang.org/issue/14808
 	return uint16(b[0]) | uint16(b[1])<<8
 }
 
 func (littleEndian) PutUint16(b []byte, v uint16) {
-	b = b[:2] // early bounds check to guarantee safety of writes below
+	b = b[:2:len(b)] // early bounds check to guarantee safety of writes below
 	b[0] = byte(v)
 	b[1] = byte(v >> 8)
 }
 
 func (littleEndian) Uint32(b []byte) uint32 {
-	b = b[:4] // bounds check hint to compiler; see golang.org/issue/14808
+	b = b[:4:len(b)] // bounds check hint to compiler; see golang.org/issue/14808
 	return uint32(b[0]) | uint32(b[1])<<8 | uint32(b[2])<<16 | uint32(b[3])<<24
 }
 
 func (littleEndian) PutUint32(b []byte, v uint32) {
-	b = b[:4] // early bounds check to guarantee safety of writes below
+	b = b[:4:len(b)] // early bounds check to guarantee safety of writes below
 	b[0] = byte(v)
 	b[1] = byte(v >> 8)
 	b[2] = byte(v >> 16)
@@ -73,13 +73,13 @@ func (littleEndian) PutUint32(b []byte, v uint32) {
 }
 
 func (littleEndian) Uint64(b []byte) uint64 {
-	b = b[:8] // bounds check hint to compiler; see golang.org/issue/14808
+	b = b[:8:len(b)] // bounds check hint to compiler; see golang.org/issue/14808
 	return uint64(b[0]) | uint64(b[1])<<8 | uint64(b[2])<<16 | uint64(b[3])<<24 |
 		uint64(b[4])<<32 | uint64(b[5])<<40 | uint64(b[6])<<48 | uint64(b[7])<<56
 }
 
 func (littleEndian) PutUint64(b []byte, v uint64) {
-	b = b[:8] // early bounds check to guarantee safety of writes below
+	b = b[:8:len(b)] // early bounds check to guarantee safety of writes below
 	b[0] = byte(v)
 	b[1] = byte(v >> 8)
 	b[2] = byte(v >> 16)
@@ -97,23 +97,23 @@ func (littleEndian) GoString() string { return "binary.LittleEndian" }
 type bigEndian struct{}
 
 func (bigEndian) Uint16(b []byte) uint16 {
-	b = b[:2] // bounds check hint to compiler; see golang.org/issue/14808
+	b = b[:2:len(b)] // bounds check hint to compiler; see golang.org/issue/14808
 	return uint16(b[1]) | uint16(b[0])<<8
 }
 
 func (bigEndian) PutUint16(b []byte, v uint16) {
-	b = b[:2] // early bounds check to guarantee safety of writes below
+	b = b[:2:len(b)] // early bounds check to guarantee safety of writes below
 	b[0] = byte(v >> 8)
 	b[1] = byte(v)
 }
 
 func (bigEndian) Uint32(b []byte) uint32 {
-	b = b[:4] // bounds check hint to compiler; see golang.org/issue/14808
+	b = b[:4:len(b)] // bounds check hint to compiler; see golang.org/issue/14808
 	return uint32(b[3]) | uint32(b[2])<<8 | uint32(b[1])<<16 | uint32(b[0])<<24
 }
 
 func (bigEndian) PutUint32(b []byte, v uint32) {
-	b = b[:4] // early bounds check to guarantee safety of writes below
+	b = b[:4:len(b)] // early bounds check to guarantee safety of writes below
 	b[0] = byte(v >> 24)
 	b[1] = byte(v >> 16)
 	b[2] = byte(v >> 8)
@@ -121,13 +121,13 @@ func (bigEndian) PutUint32(b []byte, v uint32) {
 }
 
 func (bigEndian) Uint64(b []byte) uint64 {
-	b = b[:8] // bounds check hint to compiler; see golang.org/issue/14808
+	b = b[:8:len(b)] // bounds check hint to compiler; see golang.org/issue/14808
 	return uint64(b[7]) | uint64(b[6])<<8 | uint64(b[5])<<16 | uint64(b[4])<<24 |
 		uint64(b[3])<<32 | uint64(b[2])<<40 | uint64(b[1])<<48 | uint64(b[0])<<56
 }
 
 func (bigEndian) PutUint64(b []byte, v uint64) {
-	b = b[:8] // early bounds check to guarantee safety of writes below
+	b = b[:8:len(b)] // early bounds check to guarantee safety of writes below
 	b[0] = byte(v >> 56)
 	b[1] = byte(v >> 48)
 	b[2] = byte(v >> 40)
