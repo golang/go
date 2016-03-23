9e351fcb180e6461645a72c6113a42620fac7a83
Brad Fitzpatrick <bradfitz@golang.org>
2016-03-23 16:25:27 +1100

    net/http/httptest: add NewRequest helper for ease of testing handlers
    Fixes #14199
    
    Change-Id: Ic9284023b663de3db1ca7b7b1e96eeab82ec0944
    Reviewed-on: https://go-review.googlesource.com/21016
    Run-TryBot: Brad Fitzpatrick <bradfitz@golang.org>
    Reviewed-by: Andrew Gerrand <adg@golang.org>

--- src/net/http/httptest/recorder.go
+++ src/net/http/httptest/recorder.go
@@ -2,7 +2,6 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// Package httptest provides utilities for HTTP testing.
 package httptest
 
 import (
