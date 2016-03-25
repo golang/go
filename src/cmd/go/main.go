4e31221bd1ea3e35db4a236f656619dbba13ff6d
Marcel van Lohuizen <mpvl@golang.org>
2016-03-25 10:16:40 +0100

    cmd/go: remove double space in template
    Change-Id: I6113145baa727b9fd103765f74dc5d7af86dfdf8
    Reviewed-on: https://go-review.googlesource.com/21131
    Run-TryBot: Marcel van Lohuizen <mpvl@golang.org>
    Reviewed-by: Brad Fitzpatrick <bradfitz@golang.org>

--- src/cmd/go/main.go
+++ src/cmd/go/main.go
@@ -214,7 +214,7 @@ var helpTemplate = `{{if .Runnable}}usage: go {{.UsageLine}}
 {{end}}{{.Long | trim}}
 `
 
-var documentationTemplate = `// Copyright 2011 The Go Authors.  All rights reserved.
+var documentationTemplate = `// Copyright 2011 The Go Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
