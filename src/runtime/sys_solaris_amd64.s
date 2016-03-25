562e38c0afedc32887eccb093fa710b36eb7588d
Joe Sylve <joe.sylve@gmail.com>
2016-03-25 13:51:56 -0500

    runtime: fix signal handling on Solaris
    This fixes the problems with signal handling that were inadvertently
    introduced in https://go-review.googlesource.com/21006.
    
    Fixes #14899
    
    Change-Id: Ia746914dcb3146a52413d32c57b089af763f0810
    Reviewed-on: https://go-review.googlesource.com/21145
    Reviewed-by: Ian Lance Taylor <iant@golang.org>

--- src/runtime/sys_solaris_amd64.s
+++ src/runtime/sys_solaris_amd64.s
@@ -173,10 +173,9 @@ TEXT runtime·sigtramp(SB),NOSPLIT,$0
 	MOVQ	g(BX), R10
 	CMPQ	R10, $0
 	JNE	allgood
-	MOVQ	SI, 0(SP)
-	MOVQ	DX, 8(SP)
-	CALL	runtime·makesigctxt(SB)
-	MOVQ	16(SP), AX
+	MOVQ	SI, 80(SP)
+	MOVQ	DX, 88(SP)
+	LEAQ	80(SP), AX
 	MOVQ	DI, 0(SP)
 	MOVQ	AX, 8(SP)
 	MOVQ	$runtime·badsignal(SB), AX
