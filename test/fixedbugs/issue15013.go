golang/go

commit 480179a163fdaae0633e0e202d603f0949bcb4f6

cmd/compile: clear OTFUNC info when converting to OTYPE
I want to get rid of OTFUNC, which serves no useful purpose.  However,
it turns out that the escape analysis pass looks at the node slices set
up for OTFUNC, even though by the time escape analysis runs the OTFUNC
has been converted to OTYPE.  This CL converts the escape analysis code
to look at the function decls instead, and clears the OTFUNC info when
converting to OTYPE to ensure that nothing else looks at it.

Change-Id: I3f2f5997ea8ea7a127a858e94b20aabfab84a5bf
Reviewed-on: https://go-review.googlesource.com/21202
Run-TryBot: Ian Lance Taylor <iant@golang.org>
Reviewed-by: Brad Fitzpatrick <bradfitz@golang.org>

src/cmd/compile/internal/gc/esc.go
@@ -1474,48 +1474,51 @@ func esccall(e *EscState, n *Node, up *Node) {
 		if fn.Name.Defn.Esc == EscFuncUnknown || nE.Escretval.Len() != 0 {
 			Fatalf("graph inconsistency")
 		}
-		// set up out list on this call node
-		for _, n2 := range fn.Name.Param.Ntype.Rlist.Slice() {
-			nE.Escretval.Append(n2.Left) // type.rlist ->  dclfield -> ONAME (PPARAMOUT)
-		}
 
-		// Receiver.
-		if n.Op != OCALLFUNC {
-			escassignNilWhy(e, fn.Name.Param.Ntype.Left.Left, n.Left.Left, "call receiver")
-		}
-
-		var src *Node
 		lls := ll.Slice()
-		lrs := fn.Name.Param.Ntype.List.Slice()
-		i := 0
-		for ; i < len(lls) && i < len(lrs); i++ {
-			src = lls[i]
-			if lrs[i].Isddd && !n.Isddd {
-				// Introduce ODDDARG node to represent ... allocation.
-				src = Nod(ODDDARG, nil, nil)
-				src.Type = typ(TARRAY)
-				src.Type.Type = lrs[i].Type.Type
-				src.Type.Bound = int64(len(lls) - i)
-				src.Type = Ptrto(src.Type) // make pointer so it will be tracked
-				src.Lineno = n.Lineno
-				e.track(src)
-				n.Right = src
-			}
+		sawRcvr := false
+		var src *Node
+	DclLoop:
+		for _, n2 := range fn.Name.Defn.Func.Dcl {
+			switch n2.Class {
+			case PPARAM:
+				if n.Op != OCALLFUNC && !sawRcvr {
+					escassignNilWhy(e, n2, n.Left.Left, "call receiver")
+					sawRcvr = true
+					continue DclLoop
+				}
+				if len(lls) == 0 {
+					continue DclLoop
+				}
+				src = lls[0]
+				if n2.Isddd && !n.Isddd {
+					// Introduce ODDDARG node to represent ... allocation.
+					src = Nod(ODDDARG, nil, nil)
+					src.Type = typ(TARRAY)
+					src.Type.Type = n2.Type.Type
+					src.Type.Bound = int64(len(lls))
+					src.Type = Ptrto(src.Type) // make pointer so it will be tracked
+					src.Lineno = n.Lineno
+					e.track(src)
+					n.Right = src
+				}
+				escassignNilWhy(e, n2, src, "arg to recursive call")
+				if src != lls[0] {
+					break DclLoop
+				}
+				lls = lls[1:]
 
-			if lrs[i].Left != nil {
-				escassignNilWhy(e, lrs[i].Left, src, "arg to recursive call")
-			}
-			if src != lls[i] {
-				break
+			case PPARAMOUT:
+				nE.Escretval.Append(n2)
 			}
 		}
 
 		// "..." arguments are untracked
-		for ; i < len(lls); i++ {
+		for _, n2 := range lls {
 			if Debug['m'] > 3 {
-				fmt.Printf("%v::esccall:: ... <- %v, untracked\n", linestr(lineno), Nconv(lls[i], FmtShort))
+				fmt.Printf("%v::esccall:: ... <- %v, untracked\n", linestr(lineno), Nconv(n2, FmtShort))
 			}
-			escassignSinkNilWhy(e, src, lls[i], "... arg to recursive call")
+			escassignSinkNilWhy(e, src, n2, "... arg to recursive call")
 		}
 
 		return

src/cmd/compile/internal/gc/typecheck.go
@@ -448,6 +448,9 @@ OpSwitch:
 			n.Type = nil
 			return n
 		}
+		n.Left = nil
+		n.List.Set(nil)
+		n.Rlist.Set(nil)
 
 	// type or expr
 	case OIND:

commit 777a77b4d29853e13dc803b2923bf88b498ed306

cmd/compile: don't skip PPARAMOUT in esccall after varargs
Fixes bug I introduced in CL 21202.

Fixes #15013.

Change-Id: I2344d7e22b8273425a0a56f4a77588b5c6e4d8c6
Reviewed-on: https://go-review.googlesource.com/21270
Run-TryBot: Ian Lance Taylor <iant@golang.org>
TryBot-Result: Gobot Gobot <gobot@golang.org>
Reviewed-by: Brad Fitzpatrick <bradfitz@golang.org>

src/cmd/compile/internal/gc/esc.go
@@ -1478,17 +1478,16 @@ func esccall(e *EscState, n *Node, up *Node) {
 		lls := ll.Slice()
 		sawRcvr := false
 		var src *Node
-	DclLoop:
 		for _, n2 := range fn.Name.Defn.Func.Dcl {
 			switch n2.Class {
 			case PPARAM:
 				if n.Op != OCALLFUNC && !sawRcvr {
 					escassignNilWhy(e, n2, n.Left.Left, "call receiver")
 					sawRcvr = true
-					continue DclLoop
+					continue
 				}
 				if len(lls) == 0 {
-					continue DclLoop
+					continue
 				}
 				src = lls[0]
 				if n2.Isddd && !n.Isddd {
@@ -1502,21 +1501,23 @@ func esccall(e *EscState, n *Node, up *Node) {
 				}
 				escassignNilWhy(e, n2, src, "arg to recursive call")
 				if src != lls[0] {
-					break DclLoop
+					// "..." arguments are untracked
+					for _, n2 := range lls {
+						if Debug['m'] > 3 {
+							fmt.Printf("%v::esccall:: ... <- %v, untracked\n", linestr(lineno), Nconv(n2, FmtShort))
+						}
+						escassignSinkNilWhy(e, src, n2, "... arg to recursive call")
+					}
+					// No more PPARAM processing, but keep
+					// going for PPARAMOUT.
+					lls = nil
+					continue
 				}
 				lls = lls[1:]
 
 			case PPARAMOUT:
 				nE.Escretval.Append(n2)
 			}
-		}
-
-		// "..." arguments are untracked
-		for _, n2 := range lls {
-			if Debug['m'] > 3 {
-				fmt.Printf("%v::esccall:: ... <- %v, untracked\n", linestr(lineno), Nconv(n2, FmtShort))
-			}
-			escassignSinkNilWhy(e, src, n2, "... arg to recursive call")
 		}
 
 		return

test/fixedbugs/issue15013.go
@@ -1,0 +1,24 @@
+// compile
+
+// Copyright 2016 The Go Authors.  All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// CL 21202 introduced a compiler crash in the handling of a varargs
+// function in the same recursive group as a function that calls it.
+// Nothing in the standard library caught the problem, so adding a test.
+
+package p
+
+func F1(p *int, a ...*int) (int, *int) {
+	if p == nil {
+		return F2(), a[0]
+	}
+	return 0, a[0]
+}
+
+func F2() int {
+	var i0, i1 int
+	a, _ := F1(&i0, &i1)
+	return a
+}