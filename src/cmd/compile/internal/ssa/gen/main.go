7208a2cd785fee77d951ad575adc705a3a012486
Dave Cheney <dave@cheney.net>
2016-04-05 15:11:08 +1000

    cmd/compile/internal/ssa: hide gen packge from ./make.bash
    Fixes #15122
    
    Change-Id: Ie2c802d78aea731e25bf4b193b3c2e4c884e0573
    Reviewed-on: https://go-review.googlesource.com/21524
    Run-TryBot: Dave Cheney <dave@cheney.net>
    Reviewed-by: Brad Fitzpatrick <bradfitz@golang.org>
    TryBot-Result: Gobot Gobot <gobot@golang.org>

--- src/cmd/compile/internal/ssa/gen/main.go
+++ src/cmd/compile/internal/ssa/gen/main.go
@@ -2,9 +2,10 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
+// +build ignore
+
 // The gen command generates Go code (in the parent directory) for all
 // the architecture-specific opcodes, blocks, and rewrites.
-
 package main
 
 import (
