5103fbfdb29278533c666163a9d56f85408224d9
Brad Fitzpatrick <bradfitz@golang.org>
2016-04-06 02:51:55 +0000

    runtime: merge os_linux.go into os1_linux.go
    Change-Id: I791c47014fe69e8529c7b2f0b9a554e47902d46c
    Reviewed-on: https://go-review.googlesource.com/21566
    Reviewed-by: Minux Ma <minux@golang.org>

--- src/runtime/os1_linux.go
+++ src/runtime/os1_linux.go
@@ -9,6 +9,11 @@ import (
 	"unsafe"
 )
 
+type mOS struct{}
+
+//go:noescape
+func futex(addr unsafe.Pointer, op int32, val uint32, ts, addr2 unsafe.Pointer, val3 uint32) int32
+
 // Linux futex.
 //
 //	futexsleep(uint32 *addr, uint32 val)
@@ -126,6 +131,9 @@ const (
 		_CLONE_SIGHAND | /* share sig handler table */
 		_CLONE_THREAD /* revisit - okay for now */
 )
+
+//go:noescape
+func clone(flags int32, stk, mm, gg, fn unsafe.Pointer) int32
 
 // May run with m.p==nil, so write barriers are not allowed.
 //go:nowritebarrier
@@ -306,6 +314,27 @@ func memlimit() uintptr {
 func sigreturn()
 func sigtramp()
 func cgoSigtramp()
+
+//go:noescape
+func rt_sigaction(sig uintptr, new, old *sigactiont, size uintptr) int32
+
+//go:noescape
+func sigaltstack(new, old *sigaltstackt)
+
+//go:noescape
+func setitimer(mode int32, new, old *itimerval)
+
+//go:noescape
+func rtsigprocmask(sig uint32, new, old *sigset, size int32)
+
+//go:noescape
+func getrlimit(kind int32, limit unsafe.Pointer) int32
+func raise(sig int32)
+func raiseproc(sig int32)
+
+//go:noescape
+func sched_getaffinity(pid, len uintptr, buf *uintptr) int32
+func osyield()
 
 //go:nosplit
 //go:nowritebarrierrec
