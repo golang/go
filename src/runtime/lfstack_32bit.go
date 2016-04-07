9cc9e95b288648d796d92f0b92cb713b35f20062
Dave Cheney <dave@cheney.net>
2016-04-07 14:05:06 +0000

    Revert "runtime: merge lfstack{Pack,Unpack} into one file"
    This broke solaris, which apparently does use the upper 17 bits of the address space.
    
    This reverts commit 3b02c5b1b66df9cdb23d5a3243bb37b2c312ea1b.
    
    Change-Id: Iedfe54abd0384960845468205f20191a97751c0b
    Reviewed-on: https://go-review.googlesource.com/21652
    Reviewed-by: Dave Cheney <dave@cheney.net>

---
+++ src/runtime/lfstack_32bit.go
@@ -0,0 +1,19 @@
+// Copyright 2014 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// +build 386 arm nacl
+
+package runtime
+
+import "unsafe"
+
+// On 32-bit systems, the stored uint64 has a 32-bit pointer and 32-bit count.
+
+func lfstackPack(node *lfnode, cnt uintptr) uint64 {
+	return uint64(uintptr(unsafe.Pointer(node)))<<32 | uint64(cnt)
+}
+
+func lfstackUnpack(val uint64) *lfnode {
+	return (*lfnode)(unsafe.Pointer(uintptr(val >> 32)))
+}
