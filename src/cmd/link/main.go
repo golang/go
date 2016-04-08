e20b0474c474ea3c3cd9924025627250bfd3ebb2
Michael Munday <munday@ca.ibm.com>
2016-04-08 11:55:05 -0400

    cmd/link: add s390x to link tool main function
    Change-Id: I83bc2b4a00216b069f133113e4ae9ad76c98a708
    Reviewed-on: https://go-review.googlesource.com/21741
    Run-TryBot: Michael Munday <munday@ca.ibm.com>
    Reviewed-by: Brad Fitzpatrick <bradfitz@golang.org>

--- src/cmd/link/main.go
+++ src/cmd/link/main.go
@@ -11,6 +11,7 @@ import (
 	"cmd/link/internal/arm64"
 	"cmd/link/internal/mips64"
 	"cmd/link/internal/ppc64"
+	"cmd/link/internal/s390x"
 	"cmd/link/internal/x86"
 	"fmt"
 	"os"
@@ -33,5 +34,7 @@ func main() {
 		mips64.Main()
 	case "ppc64", "ppc64le":
 		ppc64.Main()
+	case "s390x":
+		s390x.Main()
 	}
 }
