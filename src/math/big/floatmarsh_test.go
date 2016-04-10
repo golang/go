f9d2f5aec2ba6267ed2d09a38041dc30692983a2
OneOfOne <oneofone@gmail.com>
2016-04-10 03:50:11 +0200

    math/big: implement GobDecode/Encode for big.Float
    Added GobEncode/Decode and a test for them.
    
    Fixes #14593
    
    Change-Id: Ic8d3efd24d0313a1a66f01da293c4c1fd39764a8
    Reviewed-on: https://go-review.googlesource.com/21755
    Reviewed-by: Robert Griesemer <gri@golang.org>

--- src/math/big/floatmarsh_test.go
+++ src/math/big/floatmarsh_test.go
@@ -5,7 +5,10 @@
 package big
 
 import (
+	"bytes"
+	"encoding/gob"
 	"encoding/json"
+	"io"
 	"testing"
 )
 
@@ -23,6 +26,66 @@ var floatVals = []string{
 	"Inf",
 }
 
+func TestFloatGobEncoding(t *testing.T) {
+	var medium bytes.Buffer
+	for _, test := range floatVals {
+		for _, sign := range []string{"", "+", "-"} {
+			for _, prec := range []uint{0, 1, 2, 10, 53, 64, 100, 1000} {
+				medium.Reset() // empty buffer for each test case (in case of failures)
+				enc := gob.NewEncoder(&medium)
+				dec := gob.NewDecoder(&medium)
+				x := sign + test
+				var tx Float
+				_, _, err := tx.SetPrec(prec).Parse(x, 0)
+				if err != nil {
+					t.Errorf("parsing of %s (prec = %d) failed (invalid test case): %v", x, prec, err)
+					continue
+				}
+				tx.SetMode(ToPositiveInf)
+				if err := enc.Encode(&tx); err != nil {
+					t.Errorf("encoding of %v (prec = %d) failed: %v", &tx, prec, err)
+					continue
+				}
+
+				var rx Float
+				if err := dec.Decode(&rx); err != nil {
+					t.Errorf("decoding of %v (prec = %d) failed: %v", &tx, prec, err)
+					continue
+				}
+
+				if rx.Cmp(&tx) != 0 {
+					t.Errorf("transmission of %s failed: got %s want %s", x, rx.String(), tx.String())
+					continue
+				}
+
+				if rx.Mode() != ToPositiveInf {
+					t.Errorf("transmission of %s's mode failed: got %s want %s", x, rx.Mode(), ToPositiveInf)
+				}
+			}
+		}
+	}
+}
+func TestFloatCorruptGob(t *testing.T) {
+	var buf bytes.Buffer
+	tx := NewFloat(4 / 3).SetPrec(1000).SetMode(ToPositiveInf)
+	if err := gob.NewEncoder(&buf).Encode(tx); err != nil {
+		t.Fatal(err)
+	}
+	b := buf.Bytes()
+	var rx Float
+	if err := gob.NewDecoder(bytes.NewReader(b)).Decode(&rx); err != nil {
+		t.Fatal(err)
+	}
+	var rx2 Float
+	if err := gob.NewDecoder(bytes.NewReader(b[:10])).Decode(&rx2); err != io.ErrUnexpectedEOF {
+		t.Errorf("expected io.ErrUnexpectedEOF, got %v", err)
+	}
+	b[1] = 0
+	if err := gob.NewDecoder(bytes.NewReader(b)).Decode(&rx); err == nil {
+		t.Fatal("expected a version error, got nil")
+	}
+
+}
 func TestFloatJSONEncoding(t *testing.T) {
 	for _, test := range floatVals {
 		for _, sign := range []string{"", "+", "-"} {
