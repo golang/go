53fd522c0db58f3bd75d85295f46bb06e8ab1a9b
Emmanuel Odeke <emm.odeke@gmail.com>
2016-04-10 14:32:26 -0700

    all: make copyright headers consistent with one space after period
    Follows suit with https://go-review.googlesource.com/#/c/20111.
    
    Generated by running
    $ grep -R 'Go Authors.  All' * | cut -d":" -f1 | while read F;do perl -pi -e 's/Go
    Authors.  All/Go Authors. All/g' $F;done
    
    The code in cmd/internal/unvendor wasn't changed.
    
    Fixes #15213
    
    Change-Id: I4f235cee0a62ec435f9e8540a1ec08ae03b1a75f
    Reviewed-on: https://go-review.googlesource.com/21819
    Reviewed-by: Ian Lance Taylor <iant@golang.org>
    Run-TryBot: Ian Lance Taylor <iant@golang.org>
    TryBot-Result: Gobot Gobot <gobot@golang.org>

--- test/nilptr3_ssa.go
+++ test/nilptr3_ssa.go
@@ -3,7 +3,7 @@
 // See issues 9058.
 // +build !ppc64,!ppc64le,amd64
 
-// Copyright 2013 The Go Authors.  All rights reserved.
+// Copyright 2013 The Go Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
