ace8b83e5aee9db95dddb90175457e3517714e0a
Brad Fitzpatrick <bradfitz@golang.org>
2016-04-11 05:12:43 +0000

    net/http: add ServerContextKey to let a handler access its Server
    Fixes #12438
    Updates #15229 (to decide context key variable naming convention)
    
    Change-Id: I3ba423e91b689e232143247d044495a12c97a7d2
    Reviewed-on: https://go-review.googlesource.com/21829
    Reviewed-by: Andrew Gerrand <adg@golang.org>
    Run-TryBot: Brad Fitzpatrick <bradfitz@golang.org>
    TryBot-Result: Gobot Gobot <gobot@golang.org>

--- src/net/http/http.go
+++ src/net/http/http.go
@@ -10,3 +10,11 @@ const maxInt64 = 1<<63 - 1
 
 // TODO(bradfitz): move common stuff here. The other files have accumulated
 // generic http stuff in random places.
+
+// contextKey is a value for use with context.WithValue. It's used as
+// a pointer so it fits in an interface{} without allocation.
+type contextKey struct {
+	name string
+}
+
+func (k *contextKey) String() string { return "net/http context value " + k.name }
