c81a3532fea42df33dea54497dfaa96873c2d976
Aliaksandr Valialkin <valyala@gmail.com>
2016-04-15 00:33:28 +0300

    cmd/vet: check sync.* types' copying
    Embed noLock struct into the following types, so `go vet -copylocks` catches
    their copying additionally to types containing sync.Mutex:
      - sync.Cond
      - sync.WaitGroup
      - sync.Pool
      - atomic.Value
    
    Fixes #14582
    
    Change-Id: Icb543ef5ad10524ad239a15eec8a9b334b0e0660
    Reviewed-on: https://go-review.googlesource.com/22015
    Reviewed-by: Russ Cox <rsc@golang.org>
    Run-TryBot: Russ Cox <rsc@golang.org>
    TryBot-Result: Gobot Gobot <gobot@golang.org>

--- src/sync/waitgroup.go
+++ src/sync/waitgroup.go
@@ -15,7 +15,11 @@ import (
 // goroutines to wait for. Then each of the goroutines
 // runs and calls Done when finished. At the same time,
 // Wait can be used to block until all goroutines have finished.
+//
+// A WaitGroup must not be copied after first use.
 type WaitGroup struct {
+	noCopy noCopy
+
 	// 64-bit value: high 32 bits are counter, low 32 bits are waiter count.
 	// 64-bit atomic operations require 64-bit alignment, but 32-bit
 	// compilers do not ensure it. So we allocate 12 bytes and then use
