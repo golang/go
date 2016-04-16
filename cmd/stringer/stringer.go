321c6fd1712ce3e7b050d41b524e6ec44f945f15
Hironao OTSUBO <motemen@gmail.com>
2016-04-17 02:19:34 +0900

    cmd/stringer: fixed usage line typo
    Change-Id: I5852d03c0326d82c13d9e1078f9648ecc695fbbf
    Reviewed-on: https://go-review.googlesource.com/22150
    Reviewed-by: Brad Fitzpatrick <bradfitz@golang.org>

--- cmd/stringer/stringer.go
+++ cmd/stringer/stringer.go
@@ -89,7 +89,7 @@ var (
 func Usage() {
 	fmt.Fprintf(os.Stderr, "Usage of %s:\n", os.Args[0])
 	fmt.Fprintf(os.Stderr, "\tstringer [flags] -type T [directory]\n")
-	fmt.Fprintf(os.Stderr, "\tstringer [flags[ -type T files... # Must be a single package\n")
+	fmt.Fprintf(os.Stderr, "\tstringer [flags] -type T files... # Must be a single package\n")
 	fmt.Fprintf(os.Stderr, "For more information, see:\n")
 	fmt.Fprintf(os.Stderr, "\thttp://godoc.org/golang.org/x/tools/cmd/stringer\n")
 	fmt.Fprintf(os.Stderr, "Flags:\n")
