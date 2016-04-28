02371460dcd13ea5dce6dd422a0c6d61de835377
Cherry Zhang <lunaria21@gmail.com>
2016-04-27 22:17:44 -0400

    cmd/asm, cmd/internal/obj/mips: add an alias of RSB on mips64x
    Change-Id: I724ce0a48c1aeed14267c049fa415a6fa2fffbcf
    Reviewed-on: https://go-review.googlesource.com/19864
    Reviewed-by: Minux Ma <minux@golang.org>

--- src/cmd/internal/obj/mips/list0.go
+++ src/cmd/internal/obj/mips/list0.go
@@ -47,6 +47,10 @@ func Rconv(r int) string {
 		// Special case.
 		return "g"
 	}
+	if r == REGSB {
+		// Special case.
+		return "RSB"
+	}
 	if REG_R0 <= r && r <= REG_R31 {
 		return fmt.Sprintf("R%d", r-REG_R0)
 	}
