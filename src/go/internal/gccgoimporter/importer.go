42b647bde669ffa1d6f056eac20a683b9d02a786
David du Colombier <0intro@gmail.com>
2016-05-10 07:50:09 +0200

    go/internal/gccgoimporter: remove workaround on Plan 9
    We fixed the implementation of the pread syscall in
    the Plan 9 kernel, so calling pread doesn't update the
    channel offset when reading a file.
    
    Fixes #11194.
    
    Change-Id: Ie4019e445542a73479728af861a50bb54caea3f6
    Reviewed-on: https://go-review.googlesource.com/22245
    Reviewed-by: Minux Ma <minux@golang.org>
    Run-TryBot: David du Colombier <0intro@gmail.com>
    TryBot-Result: Gobot Gobot <gobot@golang.org>

--- src/go/internal/gccgoimporter/importer.go
+++ src/go/internal/gccgoimporter/importer.go
@@ -88,12 +88,6 @@ func openExportFile(fpath string) (reader io.ReadSeeker, closer io.Closer, err e
 	if err != nil {
 		return
 	}
-	// reset to offset 0 - needed on Plan 9 (see issue #11265)
-	// TODO: remove once issue #11265 has been resolved.
-	_, err = f.Seek(0, io.SeekStart)
-	if err != nil {
-		return
-	}
 
 	var elfreader io.ReaderAt
 	switch string(magic[:]) {
