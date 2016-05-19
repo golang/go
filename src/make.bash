07ee3145684b0d6809abdf9eae2c41eb87423f0c
Brad Fitzpatrick <bradfitz@golang.org>
2016-05-19 17:43:04 +0000

    build: unset GOBIN during build
    Fixes #14340
    
    Change-Id: I43e1624fafc972fb868708c3857fc8acf1bfbbd7
    Reviewed-on: https://go-review.googlesource.com/23255
    Run-TryBot: Brad Fitzpatrick <bradfitz@golang.org>
    Reviewed-by: Rob Pike <r@golang.org>

--- src/make.bash
+++ src/make.bash
@@ -50,6 +50,9 @@
 # GO_DISTFLAGS: extra flags to provide to "dist bootstrap".
 
 set -e
+
+unset GOBIN # Issue 14340
+
 if [ ! -f run.bash ]; then
 	echo 'make.bash must be run from $GOROOT/src' 1>&2
 	exit 1
--- src/run.bat
+++ src/run.bat
@@ -15,6 +15,8 @@ set GOBUILDFAIL=0
 :: we disallow local import for non-local packages, if %GOROOT% happens
 :: to be under %GOPATH%, then some tests below will fail
 set GOPATH=
+:: Issue 14340: ignore GOBIN during all.bat.
+set GOBIN=
 
 rem TODO avoid rebuild if possible
 
