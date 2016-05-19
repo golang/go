c6d432e45bef2fce4c48859b30800132a5414d4a
Scott Bell <scott@sctsm.com>
2016-05-18 18:44:46 -0700

    expvar: slightly expand documentation for Var's String method
    Fixes #15088.
    
    Change-Id: I7727829a4062e15c0e5e3beff4d0bfc1fa327b0f
    Reviewed-on: https://go-review.googlesource.com/23232
    Reviewed-by: Andrew Gerrand <adg@golang.org>
    Reviewed-by: Brad Fitzpatrick <bradfitz@golang.org>

--- src/expvar/expvar.go
+++ src/expvar/expvar.go
@@ -39,6 +39,8 @@ import (
 // Var is an abstract type for all exported variables.
 type Var interface {
 	// String returns a valid JSON value for the variable.
+	// Types with String methods that do not return valid JSON
+	// (such as time.Time) must not be used as a Var.
 	String() string
 }
 
