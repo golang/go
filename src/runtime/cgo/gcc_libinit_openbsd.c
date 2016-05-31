020ca6c2e2922433ca8f9cc89e0de2491c6e6b0f
Ian Lance Taylor <iant@golang.org>
2016-05-31 16:04:00 -0700

    runtime/cgo: avoid races on cgo_context_function
    Change-Id: Ie9e6fda675e560234e90b9022526fd689d770818
    Reviewed-on: https://go-review.googlesource.com/23610
    Reviewed-by: Dmitry Vyukov <dvyukov@google.com>
    TryBot-Result: Gobot Gobot <gobot@golang.org>

--- src/runtime/cgo/gcc_libinit_openbsd.c
+++ src/runtime/cgo/gcc_libinit_openbsd.c
@@ -6,6 +6,9 @@
 #include <stdlib.h>
 #include "libcgo.h"
 
+// The context function, used when tracing back C calls into Go.
+static void (*cgo_context_function)(struct context_arg*);
+
 void
 x_cgo_sys_thread_create(void* (*func)(void*), void* arg) {
 	fprintf(stderr, "x_cgo_sys_thread_create not implemented");
@@ -14,12 +17,16 @@ x_cgo_sys_thread_create(void* (*func)(void*), void* arg) {
 
 uintptr_t
 _cgo_wait_runtime_init_done() {
+	void (*pfn)(struct context_arg*);
+
 	// TODO(spetrovic): implement this method.
-	if (x_cgo_context_function != nil) {
+
+	pfn = _cgo_get_context_function();
+	if (pfn != nil) {
 		struct context_arg arg;
 
 		arg.Context = 0;
-		(*x_cgo_context_function)(&arg);
+		(*pfn)(&arg);
 		return arg.Context;
 	}
 	return 0;
@@ -28,4 +35,16 @@ _cgo_wait_runtime_init_done() {
 void
 x_cgo_notify_runtime_init_done(void* dummy) {
 	// TODO(spetrovic): implement this method.
+}
+
+// Sets the context function to call to record the traceback context
+// when calling a Go function from C code. Called from runtime.SetCgoTraceback.
+void x_cgo_set_context_function(void (*context)(struct context_arg*)) {
+	// TODO(iant): Needs synchronization.
+	cgo_context_function = context;
+}
+
+// Gets the context function.
+void (*(_cgo_get_context_function(void)))(struct context_arg*) {
+	return cgo_context_function;
 }
