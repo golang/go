22e0225af6c46ee68793f87194abda8c0cf0032d
Josh Bleecher Snyder <josharian@gmail.com>
2016-06-01 15:21:56 -0700

    cmd/compile: eliminate switch case kinds
    We used to have separate kinds for the default
    case and the nil type case. Now that those are
    gone, we can use a simple bool instead.
    
    Change-Id: I65488e945df68178e893cddd2e091ebb6e32ef4d
    Reviewed-on: https://go-review.googlesource.com/26763
    Run-TryBot: Josh Bleecher Snyder <josharian@gmail.com>
    Reviewed-by: Matthew Dempsky <mdempsky@google.com>

--- src/cmd/compile/internal/gc/swt_test.go
+++ src/cmd/compile/internal/gc/swt_test.go
@@ -16,120 +16,120 @@ func TestExprcmp(t *testing.T) {
 	}{
 		// Non-constants.
 		{
-			caseClause{node: Nod(OXXX, nil, nil), typ: caseKindExprVar},
-			caseClause{node: Nod(OXXX, nil, nil), typ: caseKindExprConst},
+			caseClause{node: Nod(OXXX, nil, nil)},
+			caseClause{node: Nod(OXXX, nil, nil), isconst: true},
 			+1,
 		},
 		{
-			caseClause{node: Nod(OXXX, nil, nil), typ: caseKindExprConst},
-			caseClause{node: Nod(OXXX, nil, nil), typ: caseKindExprVar},
+			caseClause{node: Nod(OXXX, nil, nil), isconst: true},
+			caseClause{node: Nod(OXXX, nil, nil)},
 			-1,
 		},
 		// Type switches
 		{
-			caseClause{node: Nod(OXXX, Nodintconst(0), nil), typ: caseKindExprConst},
-			caseClause{node: Nod(OXXX, Nodbool(true), nil), typ: caseKindExprConst},
+			caseClause{node: Nod(OXXX, Nodintconst(0), nil), isconst: true},
+			caseClause{node: Nod(OXXX, Nodbool(true), nil), isconst: true},
 			-1,
 		},
 		{
-			caseClause{node: Nod(OXXX, Nodbool(true), nil), typ: caseKindExprConst},
-			caseClause{node: Nod(OXXX, Nodintconst(1), nil), typ: caseKindExprConst},
+			caseClause{node: Nod(OXXX, Nodbool(true), nil), isconst: true},
+			caseClause{node: Nod(OXXX, Nodintconst(1), nil), isconst: true},
 			+1,
 		},
 		{
-			caseClause{node: Nod(OXXX, &Node{Type: &Type{Etype: TBOOL, Vargen: 1}}, nil), typ: caseKindExprConst},
-			caseClause{node: Nod(OXXX, &Node{Type: &Type{Etype: TINT, Vargen: 0}}, nil), typ: caseKindExprConst},
+			caseClause{node: Nod(OXXX, &Node{Type: &Type{Etype: TBOOL, Vargen: 1}}, nil), isconst: true},
+			caseClause{node: Nod(OXXX, &Node{Type: &Type{Etype: TINT, Vargen: 0}}, nil), isconst: true},
 			+1,
 		},
 		{
-			caseClause{node: Nod(OXXX, &Node{Type: &Type{Etype: TBOOL, Vargen: 1}}, nil), typ: caseKindExprConst},
-			caseClause{node: Nod(OXXX, &Node{Type: &Type{Etype: TINT, Vargen: 1}}, nil), typ: caseKindExprConst},
+			caseClause{node: Nod(OXXX, &Node{Type: &Type{Etype: TBOOL, Vargen: 1}}, nil), isconst: true},
+			caseClause{node: Nod(OXXX, &Node{Type: &Type{Etype: TINT, Vargen: 1}}, nil), isconst: true},
 			-1,
 		},
 		{
-			caseClause{node: Nod(OXXX, &Node{Type: &Type{Etype: TBOOL, Vargen: 0}}, nil), typ: caseKindExprConst},
-			caseClause{node: Nod(OXXX, &Node{Type: &Type{Etype: TINT, Vargen: 1}}, nil), typ: caseKindExprConst},
+			caseClause{node: Nod(OXXX, &Node{Type: &Type{Etype: TBOOL, Vargen: 0}}, nil), isconst: true},
+			caseClause{node: Nod(OXXX, &Node{Type: &Type{Etype: TINT, Vargen: 1}}, nil), isconst: true},
 			-1,
 		},
 		// Constant values.
 		// CTFLT
 		{
-			caseClause{node: Nod(OXXX, nodlit(Val{&Mpflt{Val: *big.NewFloat(0.1)}}), nil), typ: caseKindExprConst},
-			caseClause{node: Nod(OXXX, nodlit(Val{&Mpflt{Val: *big.NewFloat(0.2)}}), nil), typ: caseKindExprConst},
+			caseClause{node: Nod(OXXX, nodlit(Val{&Mpflt{Val: *big.NewFloat(0.1)}}), nil), isconst: true},
+			caseClause{node: Nod(OXXX, nodlit(Val{&Mpflt{Val: *big.NewFloat(0.2)}}), nil), isconst: true},
 			-1,
 		},
 		{
-			caseClause{node: Nod(OXXX, nodlit(Val{&Mpflt{Val: *big.NewFloat(0.1)}}), nil), typ: caseKindExprConst},
-			caseClause{node: Nod(OXXX, nodlit(Val{&Mpflt{Val: *big.NewFloat(0.1)}}), nil), typ: caseKindExprConst},
+			caseClause{node: Nod(OXXX, nodlit(Val{&Mpflt{Val: *big.NewFloat(0.1)}}), nil), isconst: true},
+			caseClause{node: Nod(OXXX, nodlit(Val{&Mpflt{Val: *big.NewFloat(0.1)}}), nil), isconst: true},
 			0,
 		},
 		{
-			caseClause{node: Nod(OXXX, nodlit(Val{&Mpflt{Val: *big.NewFloat(0.2)}}), nil), typ: caseKindExprConst},
-			caseClause{node: Nod(OXXX, nodlit(Val{&Mpflt{Val: *big.NewFloat(0.1)}}), nil), typ: caseKindExprConst},
+			caseClause{node: Nod(OXXX, nodlit(Val{&Mpflt{Val: *big.NewFloat(0.2)}}), nil), isconst: true},
+			caseClause{node: Nod(OXXX, nodlit(Val{&Mpflt{Val: *big.NewFloat(0.1)}}), nil), isconst: true},
 			+1,
 		},
 		// CTINT
 		{
-			caseClause{node: Nod(OXXX, Nodintconst(0), nil), typ: caseKindExprConst},
-			caseClause{node: Nod(OXXX, Nodintconst(1), nil), typ: caseKindExprConst},
+			caseClause{node: Nod(OXXX, Nodintconst(0), nil), isconst: true},
+			caseClause{node: Nod(OXXX, Nodintconst(1), nil), isconst: true},
 			-1,
 		},
 		{
-			caseClause{node: Nod(OXXX, Nodintconst(1), nil), typ: caseKindExprConst},
-			caseClause{node: Nod(OXXX, Nodintconst(1), nil), typ: caseKindExprConst},
+			caseClause{node: Nod(OXXX, Nodintconst(1), nil), isconst: true},
+			caseClause{node: Nod(OXXX, Nodintconst(1), nil), isconst: true},
 			0,
 		},
 		{
-			caseClause{node: Nod(OXXX, Nodintconst(1), nil), typ: caseKindExprConst},
-			caseClause{node: Nod(OXXX, Nodintconst(0), nil), typ: caseKindExprConst},
+			caseClause{node: Nod(OXXX, Nodintconst(1), nil), isconst: true},
+			caseClause{node: Nod(OXXX, Nodintconst(0), nil), isconst: true},
 			+1,
 		},
 		// CTRUNE
 		{
-			caseClause{node: Nod(OXXX, nodlit(Val{&Mpint{Val: *big.NewInt('a'), Rune: true}}), nil), typ: caseKindExprConst},
-			caseClause{node: Nod(OXXX, nodlit(Val{&Mpint{Val: *big.NewInt('b'), Rune: true}}), nil), typ: caseKindExprConst},
+			caseClause{node: Nod(OXXX, nodlit(Val{&Mpint{Val: *big.NewInt('a'), Rune: true}}), nil), isconst: true},
+			caseClause{node: Nod(OXXX, nodlit(Val{&Mpint{Val: *big.NewInt('b'), Rune: true}}), nil), isconst: true},
 			-1,
 		},
 		{
-			caseClause{node: Nod(OXXX, nodlit(Val{&Mpint{Val: *big.NewInt('b'), Rune: true}}), nil), typ: caseKindExprConst},
-			caseClause{node: Nod(OXXX, nodlit(Val{&Mpint{Val: *big.NewInt('b'), Rune: true}}), nil), typ: caseKindExprConst},
+			caseClause{node: Nod(OXXX, nodlit(Val{&Mpint{Val: *big.NewInt('b'), Rune: true}}), nil), isconst: true},
+			caseClause{node: Nod(OXXX, nodlit(Val{&Mpint{Val: *big.NewInt('b'), Rune: true}}), nil), isconst: true},
 			0,
 		},
 		{
-			caseClause{node: Nod(OXXX, nodlit(Val{&Mpint{Val: *big.NewInt('b'), Rune: true}}), nil), typ: caseKindExprConst},
-			caseClause{node: Nod(OXXX, nodlit(Val{&Mpint{Val: *big.NewInt('a'), Rune: true}}), nil), typ: caseKindExprConst},
+			caseClause{node: Nod(OXXX, nodlit(Val{&Mpint{Val: *big.NewInt('b'), Rune: true}}), nil), isconst: true},
+			caseClause{node: Nod(OXXX, nodlit(Val{&Mpint{Val: *big.NewInt('a'), Rune: true}}), nil), isconst: true},
 			+1,
 		},
 		// CTSTR
 		{
-			caseClause{node: Nod(OXXX, nodlit(Val{"ab"}), nil), typ: caseKindExprConst},
-			caseClause{node: Nod(OXXX, nodlit(Val{"abc"}), nil), typ: caseKindExprConst},
+			caseClause{node: Nod(OXXX, nodlit(Val{"ab"}), nil), isconst: true},
+			caseClause{node: Nod(OXXX, nodlit(Val{"abc"}), nil), isconst: true},
 			-1,
 		},
 		{
-			caseClause{node: Nod(OXXX, nodlit(Val{"abc"}), nil), typ: caseKindExprConst},
-			caseClause{node: Nod(OXXX, nodlit(Val{"xyz"}), nil), typ: caseKindExprConst},
+			caseClause{node: Nod(OXXX, nodlit(Val{"abc"}), nil), isconst: true},
+			caseClause{node: Nod(OXXX, nodlit(Val{"xyz"}), nil), isconst: true},
 			-1,
 		},
 		{
-			caseClause{node: Nod(OXXX, nodlit(Val{"abc"}), nil), typ: caseKindExprConst},
-			caseClause{node: Nod(OXXX, nodlit(Val{"abc"}), nil), typ: caseKindExprConst},
+			caseClause{node: Nod(OXXX, nodlit(Val{"abc"}), nil), isconst: true},
+			caseClause{node: Nod(OXXX, nodlit(Val{"abc"}), nil), isconst: true},
 			0,
 		},
 		{
-			caseClause{node: Nod(OXXX, nodlit(Val{"abc"}), nil), typ: caseKindExprConst},
-			caseClause{node: Nod(OXXX, nodlit(Val{"ab"}), nil), typ: caseKindExprConst},
+			caseClause{node: Nod(OXXX, nodlit(Val{"abc"}), nil), isconst: true},
+			caseClause{node: Nod(OXXX, nodlit(Val{"ab"}), nil), isconst: true},
 			+1,
 		},
 		{
-			caseClause{node: Nod(OXXX, nodlit(Val{"xyz"}), nil), typ: caseKindExprConst},
-			caseClause{node: Nod(OXXX, nodlit(Val{"abc"}), nil), typ: caseKindExprConst},
+			caseClause{node: Nod(OXXX, nodlit(Val{"xyz"}), nil), isconst: true},
+			caseClause{node: Nod(OXXX, nodlit(Val{"abc"}), nil), isconst: true},
 			+1,
 		},
 		// Everything else should compare equal.
 		{
-			caseClause{node: Nod(OXXX, nodnil(), nil), typ: caseKindExprConst},
-			caseClause{node: Nod(OXXX, nodnil(), nil), typ: caseKindExprConst},
+			caseClause{node: Nod(OXXX, nodnil(), nil), isconst: true},
+			caseClause{node: Nod(OXXX, nodnil(), nil), isconst: true},
 			0,
 		},
 	}
@@ -137,8 +137,8 @@ func TestExprcmp(t *testing.T) {
 		got := exprcmp(d.a, d.b)
 		if d.want != got {
 			t.Errorf("%d: exprcmp(a, b) = %d; want %d", i, got, d.want)
-			t.Logf("\ta = caseClause{node: %#v, typ: %#v}", d.a.node, d.a.typ)
-			t.Logf("\tb = caseClause{node: %#v, typ: %#v}", d.b.node, d.b.typ)
+			t.Logf("\ta = caseClause{node: %#v, isconst: %v}", d.a.node, d.a.isconst)
+			t.Logf("\tb = caseClause{node: %#v, isconst: %v}", d.b.node, d.b.isconst)
 		}
 	}
 }
