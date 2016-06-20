e1a6e71e746d511570f269d43b9abf838505a8e5
Ian Lance Taylor <iant@golang.org>
2016-06-20 15:43:21 -0700

    test: add missing copyright notice
    Change-Id: I2a5353203ca2958fa37fc7a5ea3f22ad4fc62b0e
    Reviewed-on: https://go-review.googlesource.com/24282
    Run-TryBot: Ian Lance Taylor <iant@golang.org>
    TryBot-Result: Gobot Gobot <gobot@golang.org>
    Reviewed-by: David Chase <drchase@google.com>

--- test/fixedbugs/issue16008.go
+++ test/fixedbugs/issue16008.go
@@ -1,5 +1,9 @@
 // errorcheck -0 -race
 
+// Copyright 2016 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 package foo
 
 const benchmarkNumNodes = 10000
