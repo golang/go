fe251d2581420811891741b49f38737c9fa4e1cc
Ian Lance Taylor <iant@golang.org>
2016-07-07 21:01:21 -0700

    runtime: remove unused function in test
    Change-Id: I43f14cdd9eb4a1d5471fc88c1b4759ceb2c674cf
    Reviewed-on: https://go-review.googlesource.com/24817
    Run-TryBot: Ian Lance Taylor <iant@golang.org>
    TryBot-Result: Gobot Gobot <gobot@golang.org>
    Reviewed-by: Brad Fitzpatrick <bradfitz@golang.org>

--- src/runtime/testdata/testprogcgo/threadpprof.go
+++ src/runtime/testdata/testprogcgo/threadpprof.go
@@ -39,17 +39,6 @@ struct cgoTracebackArg {
 	uintptr_t  max;
 };
 
-static void *pprofThread(void* p) {
-	time_t start;
-
-	(void)p;
-	start = time(NULL);
-	while (__sync_add_and_fetch(&cpuHogThreadCount, 0) < 2 && time(NULL) - start < 2) {
-		cpuHogThread();
-	}
-}
-
-
 // pprofCgoThreadTraceback is passed to runtime.SetCgoTraceback.
 // For testing purposes it pretends that all CPU hits in C code are in cpuHog.
 void pprofCgoThreadTraceback(void* parg) {
