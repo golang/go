3fe2afc9e626f32e91aff6eddb78b14743446865
Brad Fitzpatrick <bradfitz@golang.org>
2016-08-15 15:27:39 -0700

    imports: update zstdlib to Go 1.7
    Change-Id: I936c6f03e8c3dc14832c8493937fdb64e6d1a853
    Reviewed-on: https://go-review.googlesource.com/27079
    Reviewed-by: Chris Broadfoot <cbro@golang.org>

--- imports/mkstdlib.go
+++ imports/mkstdlib.go
@@ -51,6 +51,7 @@ func main() {
 		mustOpen(api("go1.4.txt")),
 		mustOpen(api("go1.5.txt")),
 		mustOpen(api("go1.6.txt")),
+		mustOpen(api("go1.7.txt")),
 	)
 	sc := bufio.NewScanner(f)
 	fullImport := map[string]string{} // "zip.NewReader" => "archive/zip"
