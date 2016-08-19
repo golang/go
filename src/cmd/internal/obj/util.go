a25a7ad70323e8edea4b607aad7c9d2bb96fcc82
Brad Fitzpatrick <bradfitz@golang.org>
2016-08-18 18:24:21 -0700

    cmd/internal/obj: update Bool2int to the form optimized by the compiler
    As of https://golang.org/cl/22711 the compiler optimizes this form.
    
    Updates #6011
    
    Change-Id: Ibc6c529dfa24d42f4aab78ebd6722e1d72cb6038
    Reviewed-on: https://go-review.googlesource.com/27395
    Reviewed-by: Josh Bleecher Snyder <josharian@gmail.com>
    Run-TryBot: Josh Bleecher Snyder <josharian@gmail.com>
    TryBot-Result: Gobot Gobot <gobot@golang.org>

--- src/cmd/internal/obj/util.go
+++ src/cmd/internal/obj/util.go
@@ -504,8 +504,13 @@ var Anames = []string{
 }
 
 func Bool2int(b bool) int {
+	// The compiler currently only optimizes this form.
+	// See issue 6011.
+	var i int
 	if b {
-		return 1
+		i = 1
+	} else {
+		i = 0
 	}
-	return 0
+	return i
 }
