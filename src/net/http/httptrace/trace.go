4187e1f49f45b1cfa66e7cdf8c118ee926285019
Billy Lynch <wlynch@google.com>
2016-08-19 01:40:08 -0400

    net/http/httptrace: add simple example and fix copyright header
    Partially addresses #16360
    
    Change-Id: I67a328302d7d91231f348d934e4232fcb844830a
    Reviewed-on: https://go-review.googlesource.com/27398
    Reviewed-by: Brad Fitzpatrick <bradfitz@golang.org>
    Run-TryBot: Brad Fitzpatrick <bradfitz@golang.org>
    TryBot-Result: Gobot Gobot <gobot@golang.org>

--- src/net/http/httptrace/trace.go
+++ src/net/http/httptrace/trace.go
@@ -1,6 +1,6 @@
 // Copyright 2016 The Go Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.h
+// license that can be found in the LICENSE file.
 
 // Package httptrace provides mechanisms to trace the events within
 // HTTP client requests.
