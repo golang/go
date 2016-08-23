8050782cba45c739bfb2cd735766879098c48d99
Ian Lance Taylor <iant@golang.org>
2016-08-23 13:54:01 -0700

    flag: improve comment for calling String with zero value
    Update #16694.
    
    Change-Id: Id6be1535d8a146b3dac3bee429ce407a51272032
    Reviewed-on: https://go-review.googlesource.com/27634
    Run-TryBot: Ian Lance Taylor <iant@golang.org>
    TryBot-Result: Gobot Gobot <gobot@golang.org>
    Reviewed-by: Rob Pike <r@golang.org>

--- src/flag/flag.go
+++ src/flag/flag.go
@@ -238,7 +238,8 @@ func (d *durationValue) String() string { return (*time.Duration)(d).String() }
 // rather than using the next command-line argument.
 //
 // Set is called once, in command line order, for each flag present.
-// String must work on the zero value of the dynamic type.
+// The flag package may call the String method with a zero-valued receiver,
+// such as a nil pointer.
 type Value interface {
 	String() string
 	Set(string) error
