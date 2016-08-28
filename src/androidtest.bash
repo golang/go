d04b9494d2c9fc4f6a812ccdcc23f3616b7b2ff1
Josh Bleecher Snyder <josharian@gmail.com>
2016-08-28 08:53:20 -0700

    androidtest.bash: require GOARCH
    When finding the pkg dir, androidtest.bash assumes
    that GOARCH is set. Require it up front.
    
    Change-Id: I143f7b59ad9d98b9c3cfb53c1d65c2d33a6acc12
    Reviewed-on: https://go-review.googlesource.com/27926
    Run-TryBot: Josh Bleecher Snyder <josharian@gmail.com>
    TryBot-Result: Gobot Gobot <gobot@golang.org>
    Reviewed-by: Elias Naur <elias.naur@gmail.com>
    Reviewed-by: David Crawshaw <crawshaw@golang.org>

--- src/androidtest.bash
+++ src/androidtest.bash
@@ -30,6 +30,10 @@ if [ "$GOARM" != "7" ]; then
 	echo "android only supports GOARM=7, got GOARM=$GOARM" 1>&2
 	exit 1
 fi
+if [ "$GOARCH" = "" ]; then
+	echo "GOARCH must be set" 1>&2
+	exit 1
+fi
 
 export CGO_ENABLED=1
 unset GOBIN
