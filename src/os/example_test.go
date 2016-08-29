428d79bd38db30405fe2b5d264d856e52030c338
Jaana Burcu Dogan <jbd@google.com>
2016-08-28 23:11:00 -0700

    os: add example for OpenFile
    New beginners are not familiar with open(2)-style masking of the
    flags. Add an example demonstrates the flag or'ing.
    
    Change-Id: Ifa8009c55173ba0dc6642c1d3b3124c766b1ebbb
    Reviewed-on: https://go-review.googlesource.com/27996
    Reviewed-by: Brad Fitzpatrick <bradfitz@golang.org>

---
+++ src/os/example_test.go
@@ -0,0 +1,20 @@
+// Copyright 2016 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package os_test
+
+import (
+	"log"
+	"os"
+)
+
+func ExampleOpenFile() {
+	f, err := os.OpenFile("notes.txt", os.O_RDWR|os.O_CREATE, 0755)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if err := f.Close(); err != nil {
+		log.Fatal(err)
+	}
+}
