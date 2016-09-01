210ac4d5e0fea2bfd4287b0865104bdaaeaffe05
Adam Langley <agl@golang.org>
2016-09-01 16:00:25 -0700

    crypto/cipher: enforce message size limits for GCM.
    The maximum input plaintext for GCM is 64GiB - 64. Since the GCM
    interface is one-shot, it's very hard to hit this in Go (one would need
    a 64GiB buffer in memory), but we should still enforce this limit.
    
    Thanks to Quan Nguyen for pointing it out.
    
    Change-Id: Icced47bf8d4d5dfbefa165cf13e893205c9577b8
    Reviewed-on: https://go-review.googlesource.com/28410
    Run-TryBot: Adam Langley <agl@golang.org>
    TryBot-Result: Gobot Gobot <gobot@golang.org>
    Reviewed-by: Andrew Gerrand <adg@golang.org>

--- src/crypto/cipher/gcm.go
+++ src/crypto/cipher/gcm.go
@@ -135,6 +135,10 @@ func (g *gcm) Seal(dst, nonce, plaintext, data []byte) []byte {
 	if len(nonce) != g.nonceSize {
 		panic("cipher: incorrect nonce length given to GCM")
 	}
+	if uint64(len(plaintext)) > ((1<<32)-2)*uint64(g.cipher.BlockSize()) {
+		panic("cipher: message too large for GCM")
+	}
+
 	ret, out := sliceForAppend(dst, len(plaintext)+gcmTagSize)
 
 	var counter, tagMask [gcmBlockSize]byte
@@ -159,6 +163,10 @@ func (g *gcm) Open(dst, nonce, ciphertext, data []byte) ([]byte, error) {
 	if len(ciphertext) < gcmTagSize {
 		return nil, errOpen
 	}
+	if uint64(len(ciphertext)) > ((1<<32)-2)*uint64(g.cipher.BlockSize())+gcmTagSize {
+		return nil, errOpen
+	}
+
 	tag := ciphertext[len(ciphertext)-gcmTagSize:]
 	ciphertext = ciphertext[:len(ciphertext)-gcmTagSize]
 
