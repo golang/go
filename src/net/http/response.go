88ccb3c945daeba7c08dfe9b39be18ec78941a45
Matt Layher <mdlayher@gmail.com>
2016-09-01 16:30:15 -0400

    net/http: omit Content-Length in Response.Write for 1xx or 204 status
    Per RFC 7230, Section 3.3.2: "A server MUST NOT send a Content-Length
    header field in any response with a status code of 1xx (Informational)
    or 204 (No Content).".
    
    Fixes #16942
    
    Change-Id: I8006c76c126304e13618966e6eafb08a3885d3cd
    Reviewed-on: https://go-review.googlesource.com/28351
    Reviewed-by: Brad Fitzpatrick <bradfitz@golang.org>
    Run-TryBot: Brad Fitzpatrick <bradfitz@golang.org>
    TryBot-Result: Gobot Gobot <gobot@golang.org>

--- src/net/http/response.go
+++ src/net/http/response.go
@@ -300,7 +300,7 @@ func (r *Response) Write(w io.Writer) error {
 	// contentLengthAlreadySent may have been already sent for
 	// POST/PUT requests, even if zero length. See Issue 8180.
 	contentLengthAlreadySent := tw.shouldSendContentLength()
-	if r1.ContentLength == 0 && !chunked(r1.TransferEncoding) && !contentLengthAlreadySent {
+	if r1.ContentLength == 0 && !chunked(r1.TransferEncoding) && !contentLengthAlreadySent && bodyAllowedForStatus(r.StatusCode) {
 		if _, err := io.WriteString(w, "Content-Length: 0\r\n"); err != nil {
 			return err
 		}
