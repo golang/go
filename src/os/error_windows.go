9f2521cd2e2153c1b96940ea1754d12cae290c8e
bogem <albertnigma@gmail.com>
2016-09-10 18:34:07 +0500

    os: delete code duplications in error files
    Change-Id: I1ec2fcf81a7a9e45a2fae8c02c8adabc7841b4fa
    Reviewed-on: https://go-review.googlesource.com/29013
    Reviewed-by: Brad Fitzpatrick <bradfitz@golang.org>
    Reviewed-by: Ian Lance Taylor <iant@golang.org>
    Run-TryBot: Brad Fitzpatrick <bradfitz@golang.org>
    TryBot-Result: Gobot Gobot <gobot@golang.org>

--- src/os/error_windows.go
+++ src/os/error_windows.go
@@ -7,16 +7,7 @@ package os
 import "syscall"
 
 func isExist(err error) bool {
-	switch pe := err.(type) {
-	case nil:
-		return false
-	case *PathError:
-		err = pe.Err
-	case *LinkError:
-		err = pe.Err
-	case *SyscallError:
-		err = pe.Err
-	}
+	err = underlyingError(err)
 	return err == syscall.ERROR_ALREADY_EXISTS ||
 		err == syscall.ERROR_FILE_EXISTS || err == ErrExist
 }
@@ -24,31 +15,13 @@ func isExist(err error) bool {
 const _ERROR_BAD_NETPATH = syscall.Errno(53)
 
 func isNotExist(err error) bool {
-	switch pe := err.(type) {
-	case nil:
-		return false
-	case *PathError:
-		err = pe.Err
-	case *LinkError:
-		err = pe.Err
-	case *SyscallError:
-		err = pe.Err
-	}
+	err = underlyingError(err)
 	return err == syscall.ERROR_FILE_NOT_FOUND ||
 		err == _ERROR_BAD_NETPATH ||
 		err == syscall.ERROR_PATH_NOT_FOUND || err == ErrNotExist
 }
 
 func isPermission(err error) bool {
-	switch pe := err.(type) {
-	case nil:
-		return false
-	case *PathError:
-		err = pe.Err
-	case *LinkError:
-		err = pe.Err
-	case *SyscallError:
-		err = pe.Err
-	}
+	err = underlyingError(err)
 	return err == syscall.ERROR_ACCESS_DENIED || err == ErrPermission
 }
