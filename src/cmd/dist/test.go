d31737245eb79377d36ea5db88a1cdf20a2b151a
Josh Bleecher Snyder <josharian@gmail.com>
2016-09-12 11:36:02 -0700

    cmd/dist: skip broken cgo race tests on darwin
    CL 26668 exposed #17065.
    Skip the cgo race tests on darwin for now.
    
    Updates #17065
    
    Change-Id: I0ad0ce2ff1af6d515b8ce6184ddeabc49806950f
    Reviewed-on: https://go-review.googlesource.com/29077
    Run-TryBot: Josh Bleecher Snyder <josharian@gmail.com>
    TryBot-Result: Gobot Gobot <gobot@golang.org>
    Reviewed-by: Ian Lance Taylor <iant@golang.org>
    Reviewed-by: Brad Fitzpatrick <bradfitz@golang.org>

--- src/cmd/dist/test.go
+++ src/cmd/dist/test.go
@@ -1016,7 +1016,8 @@ func (t *tester) raceTest(dt *distTest) error {
 	// The race builder should catch any error here, but doesn't.
 	// TODO(iant): Figure out how to catch this.
 	// t.addCmd(dt, "src", "go", "test", "-race", "-run=TestParallelTest", "cmd/go")
-	if t.cgoEnabled {
+	// TODO: Remove t.goos != "darwin" when issue 17065 is fixed.
+	if t.cgoEnabled && t.goos != "darwin" {
 		env := mergeEnvLists([]string{"GOTRACEBACK=2"}, os.Environ())
 		cmd := t.addCmd(dt, "misc/cgo/test", "go", "test", "-race", "-short", t.runFlag(""))
 		cmd.Env = env
