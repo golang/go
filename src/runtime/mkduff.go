cade9a94395396d12122d5395dbfb38400635718
Emmanuel Odeke <emm.odeke@gmail.com>
2016-09-11 19:05:29 -0700

    runtime: update mkduff legacy comments
    Update comments for duffzero and duffcopy
    which referred to legacy locations:
    + cmd/?g/cgen.go
    + cmd/?g/ggen.go
    
    Remnants of the old days when we had 5g, 6g etc.
    
    Those locations have since moved to:
    + cmd/compile/internal/<arch>/cgen.go
    + cmd/compile/internal/<arch>/ggen.go
    
    Change-Id: Ie2ea668559d52d42b747260ea69a6d5b3d70e859
    Reviewed-on: https://go-review.googlesource.com/29073
    Reviewed-by: Russ Cox <rsc@golang.org>

--- src/runtime/mkduff.go
+++ src/runtime/mkduff.go
@@ -8,14 +8,14 @@
 // The compiler jumps to computed addresses within
 // the routine to zero chunks of memory.
 // Do not change duffzero without also
-// changing clearfat in cmd/?g/ggen.go.
+// changing the uses in cmd/compile/internal/*/*.go.
 
 // runtime·duffcopy is a Duff's device for copying memory.
 // The compiler jumps to computed addresses within
 // the routine to copy chunks of memory.
 // Source and destination must not overlap.
 // Do not change duffcopy without also
-// changing blockcopy in cmd/?g/cgen.go.
+// changing the uses in cmd/compile/internal/*/*.go.
 
 // See the zero* and copy* generators below
 // for architecture-specific comments.
