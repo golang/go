47c1ff4b4248dc383bc2ec2ac03c0a8acfdfbcb3
Suyash <dextrous93@gmail.com>
2016-09-16 19:54:44 +0530

    sort: fix search descending order example
    Change-Id: I27b82d8c63a06ddf7e148b15853aba24a22a6282
    Reviewed-on: https://go-review.googlesource.com/29336
    Reviewed-by: Joe Tsai <thebrokentoaster@gmail.com>
    Run-TryBot: Joe Tsai <thebrokentoaster@gmail.com>
    TryBot-Result: Gobot Gobot <gobot@golang.org>

--- src/sort/example_search_test.go
+++ src/sort/example_search_test.go
@@ -32,7 +32,7 @@ func ExampleSearch_descendingOrder() {
 	x := 6
 
 	i := sort.Search(len(a), func(i int) bool { return a[i] <= x })
-	if a[i] == x {
+	if i < len(a) && a[i] == x {
 		fmt.Printf("found %d at index %d in %v\n", x, i, a)
 	} else {
 		fmt.Printf("%d not found in %v\n", x, a)
