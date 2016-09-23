c441469720a55c7926dd8eb2cb8af922a15e75bc
Alex Brainman <alex.brainman@gmail.com>
2016-09-23 16:58:31 +1000

    syscall: use ERROR_IO_PENDING value in errnoErr
    So errnoErr can be used in other packages.
    This is something I missed when I sent CL 28990.
    
    Fixes #17539
    
    Change-Id: I8ee3b79c4d70ca1e5b29e5b40024f7ae9a86061e
    Reviewed-on: https://go-review.googlesource.com/29690
    Reviewed-by: Brad Fitzpatrick <bradfitz@golang.org>
    Run-TryBot: Brad Fitzpatrick <bradfitz@golang.org>
    TryBot-Result: Gobot Gobot <gobot@golang.org>

--- src/internal/syscall/windows/registry/zsyscall_windows.go
+++ src/internal/syscall/windows/registry/zsyscall_windows.go
@@ -13,11 +13,11 @@ var _ unsafe.Pointer
 // Do the interface allocations only once for common
 // Errno values.
 const (
-	errnoWSAEINPROGRESS = 10036
+	errnoERROR_IO_PENDING = 997
 )
 
 var (
-	errWSAEINPROGRESS error = syscall.Errno(errnoWSAEINPROGRESS)
+	errERROR_IO_PENDING error = syscall.Errno(errnoERROR_IO_PENDING)
 )
 
 // errnoErr returns common boxed Errno values, to prevent
@@ -26,8 +26,8 @@ func errnoErr(e syscall.Errno) error {
 	switch e {
 	case 0:
 		return nil
-	case errnoWSAEINPROGRESS:
-		return errWSAEINPROGRESS
+	case errnoERROR_IO_PENDING:
+		return errERROR_IO_PENDING
 	}
 	// TODO: add more here, after collecting data on the common
 	// error values see on Windows. (perhaps when running
