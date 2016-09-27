47b3eb95076b777aee4e216d20468231e03ff38f
Mikio Hara <mikioh.mikioh@gmail.com>
2016-09-27 19:51:47 +0900

    vendor: import golang.org/x/net/lif
    golang.org/x/net/lif becomes vendor/golang_org/x/net/lif.
    
    At git rev 9f0e377 (golang.org/cl/29893)
    
    Updates #7177.
    
    Change-Id: Id838fcc234e71f735bb2609073f4c2214b48a970
    Reviewed-on: https://go-review.googlesource.com/29891
    Reviewed-by: Ian Lance Taylor <iant@golang.org>

---
+++ src/vendor/golang_org/x/net/lif/syscall.go
@@ -0,0 +1,33 @@
+// Copyright 2016 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// +build solaris
+
+package lif
+
+import (
+	"syscall"
+	"unsafe"
+)
+
+//go:cgo_import_dynamic libc_ioctl ioctl "libc.so"
+
+//go:linkname procIoctl libc_ioctl
+
+var procIoctl uintptr
+
+func sysvicall6(trap, nargs, a1, a2, a3, a4, a5, a6 uintptr) (uintptr, uintptr, syscall.Errno)
+
+// TODO: replace with runtime.KeepAlive when available
+//go:noescape
+func keepAlive(p unsafe.Pointer)
+
+func ioctl(s, ioc uintptr, arg unsafe.Pointer) error {
+	_, _, errno := sysvicall6(uintptr(unsafe.Pointer(&procIoctl)), 3, s, ioc, uintptr(arg), 0, 0, 0)
+	keepAlive(arg)
+	if errno != 0 {
+		return error(errno)
+	}
+	return nil
+}
