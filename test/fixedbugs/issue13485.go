a46a51617b83ed072f064f4c3ef43148ffc2efea
Robert Griesemer <gri@golang.org>
2016-10-05 13:53:39 -0700

    cmd/compile: untyped arrays bounds representable as integers are valid
    Fixes #13485.
    
    Change-Id: I11dd15c7d14fc19d42a3b48427a4cc1208b18e6a
    Reviewed-on: https://go-review.googlesource.com/30456
    Reviewed-by: Matthew Dempsky <mdempsky@google.com>

---
+++ test/fixedbugs/issue13485.go
@@ -0,0 +1,18 @@
+// errorcheck
+
+// Copyright 2010 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package p
+
+var (
+	_ [10]int
+	_ [10.0]int
+	_ [float64(10)]int                // ERROR "invalid array bound"
+	_ [10 + 0i]int
+	_ [complex(10, 0)]int
+	_ [complex128(complex(10, 0))]int // ERROR "invalid array bound"
+	_ ['a']int
+	_ [rune(65)]int
+)
