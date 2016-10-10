35101cc1a9816169e7c5945d1cd98c508fe906bb
Adam Langley <agl@golang.org>
2016-10-10 15:27:34 -0700

    crypto/tls: add Config.GetConfigForClient
    GetConfigForClient allows the tls.Config to be updated on a per-client
    basis.
    
    Fixes #16066.
    Fixes #15707.
    Fixes #15699.
    
    Change-Id: I2c675a443d557f969441226729f98502b38901ea
    Reviewed-on: https://go-review.googlesource.com/30790
    Run-TryBot: Adam Langley <agl@golang.org>
    TryBot-Result: Gobot Gobot <gobot@golang.org>
    Reviewed-by: Brad Fitzpatrick <bradfitz@golang.org>

--- src/crypto/tls/tls_test.go
+++ src/crypto/tls/tls_test.go
@@ -477,7 +477,7 @@ func TestClone(t *testing.T) {
 		case "Rand":
 			f.Set(reflect.ValueOf(io.Reader(os.Stdin)))
 			continue
-		case "Time", "GetCertificate":
+		case "Time", "GetCertificate", "GetConfigForClient":
 			// DeepEqual can't compare functions.
 			continue
 		case "Certificates":
