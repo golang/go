6509ec15217098868defca2c28c895bcf3b7dd09
Mikio Hara <mikioh.mikioh@gmail.com>
2016-10-12 19:41:29 +0900

    net: update doc for unimplemented feature on Plan 9
    Also removes unnecessary allocation.
    
    Change-Id: I3406cf75a7b64d93b2b99c7f1f5c78f580452b60
    Reviewed-on: https://go-review.googlesource.com/30891
    Run-TryBot: Mikio Hara <mikioh.mikioh@gmail.com>
    TryBot-Result: Gobot Gobot <gobot@golang.org>
    Reviewed-by: Ian Lance Taylor <iant@golang.org>

--- src/net/interface.go
+++ src/net/interface.go
@@ -13,8 +13,8 @@ import (
 // BUG(mikio): On NaCl, methods and functions related to
 // Interface are not implemented.
 
-// BUG(mikio): On DragonFly BSD, NetBSD, OpenBSD and Solaris, the
-// MulticastAddrs method of Interface is not implemented.
+// BUG(mikio): On DragonFly BSD, NetBSD, OpenBSD, Plan 9 and Solaris,
+// the MulticastAddrs method of Interface is not implemented.
 
 var (
 	errInvalidInterface         = errors.New("invalid network interface")
