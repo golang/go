c44c81a2ee2e401356b1d7c99e02011718fdbee1
Rob Pike <r@golang.org>
2016-10-16 11:25:37 -0700

    fmt: fix documention for %#v on uints
    It's the same as %#x not %x.
    
    Just a documentation change; tests already cover it.
    
    Fixes #17322
    
    Change-Id: Ia9db229f781f9042ac5c0bb824e3d7a26fb74ec5
    Reviewed-on: https://go-review.googlesource.com/31254
    Reviewed-by: Russ Cox <rsc@golang.org>

--- src/fmt/doc.go
+++ src/fmt/doc.go
@@ -51,7 +51,7 @@
 	The default format for %v is:
 		bool:                    %t
 		int, int8 etc.:          %d
-		uint, uint8 etc.:        %d, %x if printed with %#v
+		uint, uint8 etc.:        %d, %#x if printed with %#v
 		float32, complex64, etc: %g
 		string:                  %s
 		chan:                    %p
