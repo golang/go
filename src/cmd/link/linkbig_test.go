348fb61d52ab42c7b99e647b34f0686e8a6dbad0
Cherry Zhang <cherryyz@google.com>
2016-10-17 13:40:18 -0400

    cmd/link: trampoline support for external linking on ARM
    all.bash passes with -debugtramp=2 (except the unavoidable
    disassembly test as we change instructions). And successfully
    build k8s.io/kubernetes/cmd/hyperkube in both internal linking
    and external linking mode.
    
    Fixes #17028.
    
    Change-Id: Ic8fac6a394488155c5eba9215662db1c1086e24b
    Reviewed-on: https://go-review.googlesource.com/31143
    Reviewed-by: David Chase <drchase@google.com>

--- src/cmd/link/linkbig_test.go
+++ src/cmd/link/linkbig_test.go
@@ -12,6 +12,7 @@ import (
 	"bytes"
 	"cmd/internal/obj"
 	"fmt"
+	"internal/testenv"
 	"io/ioutil"
 	"os"
 	"os/exec"
@@ -19,29 +20,33 @@ import (
 )
 
 func TestLargeText(t *testing.T) {
+	if testing.Short() || (obj.GOARCH != "ppc64le" && obj.GOARCH != "ppc64" && obj.GOARCH != "arm") {
+		t.Skip("Skipping large text section test in short mode or on %s", obj.GOARCH)
+	}
+	testenv.MustHaveGoBuild(t)
 
 	var w bytes.Buffer
-
-	if testing.Short() || (obj.GOARCH != "ppc64le" && obj.GOARCH != "ppc64") {
-		t.Skip("Skipping large text section test in short mode or if not ppc64x")
-	}
 	const FN = 4
 	tmpdir, err := ioutil.TempDir("", "bigtext")
 
 	defer os.RemoveAll(tmpdir)
 
 	// Generate the scenario where the total amount of text exceeds the
-	// limit for the bl instruction, on RISC architectures like ppc64le,
+	// limit for the jmp/call instruction, on RISC architectures like ppc64le,
 	// which is 2^26.  When that happens the call requires special trampolines or
 	// long branches inserted by the linker where supported.
-
 	// Multiple .s files are generated instead of one.
-
+	instOnArch := map[string]string{
+		"ppc64":   "\tMOVD\tR0,R3\n",
+		"ppc64le": "\tMOVD\tR0,R3\n",
+		"arm":     "\tMOVW\tR0,R1\n",
+	}
+	inst := instOnArch[obj.GOARCH]
 	for j := 0; j < FN; j++ {
 		testname := fmt.Sprintf("bigfn%d", j)
 		fmt.Fprintf(&w, "TEXT ·%s(SB),$0\n", testname)
 		for i := 0; i < 2200000; i++ {
-			fmt.Fprintf(&w, "\tMOVD\tR0,R3\n")
+			fmt.Fprintf(&w, inst)
 		}
 		fmt.Fprintf(&w, "\tRET\n")
 		err := ioutil.WriteFile(tmpdir+"/"+testname+".s", w.Bytes(), 0666)
@@ -64,7 +69,6 @@ func TestLargeText(t *testing.T) {
 	// There are lots of dummy code generated in the .s files just to generate a lot
 	// of text. Link them in but guard their call so their code is not executed but
 	// the main part of the program can be run.
-
 	fmt.Fprintf(&w, "\tif os.Getenv(\"LINKTESTARG\") != \"\" {\n")
 	for i := 0; i < FN; i++ {
 		fmt.Fprintf(&w, "\t\tbigfn%d()\n", i)
@@ -78,9 +82,8 @@ func TestLargeText(t *testing.T) {
 	}
 
 	// Build and run with internal linking.
-
 	os.Chdir(tmpdir)
-	cmd := exec.Command("go", "build", "-o", "bigtext")
+	cmd := exec.Command(testenv.GoToolPath(t), "build", "-o", "bigtext")
 	out, err := cmd.CombinedOutput()
 	if err != nil {
 		t.Fatalf("Build failed for big text program with internal linking: %v, output: %s", err, out)
@@ -92,9 +95,8 @@ func TestLargeText(t *testing.T) {
 	}
 
 	// Build and run with external linking
-
 	os.Chdir(tmpdir)
-	cmd = exec.Command("go", "build", "-o", "bigtext", "-ldflags", "'-linkmode=external'")
+	cmd = exec.Command(testenv.GoToolPath(t), "build", "-o", "bigtext", "-ldflags", "'-linkmode=external'")
 	out, err = cmd.CombinedOutput()
 	if err != nil {
 		t.Fatalf("Build failed for big text program with external linking: %v, output: %s", err, out)
