43e2f2c7affb4c0149eda153175f89c4e769e72b
Vladimir Stefanovic <vladimir.stefanovic@imgtec.com>
2016-10-18 23:50:45 +0200

    cmd/link/internal/ld: add support for GOARCH=mips{,le}
    Change-Id: Ida214ccc5858969ea60abb0787f4d98bab4336d6
    Reviewed-on: https://go-review.googlesource.com/31480
    TryBot-Result: Gobot Gobot <gobot@golang.org>
    Run-TryBot: Brad Fitzpatrick <bradfitz@golang.org>
    Reviewed-by: Cherry Zhang <cherryyz@google.com>

--- src/cmd/link/internal/ld/ldelf.go
+++ src/cmd/link/internal/ld/ldelf.go
@@ -546,6 +546,12 @@ func ldelf(ctxt *Link, f *bio.Reader, pkg string, length int64, pn string) {
 		Errorf(nil, "%s: elf %s unimplemented", pn, SysArch.Name)
 		return
 
+	case sys.MIPS:
+		if elfobj.machine != ElfMachMips || hdr.Ident[4] != ElfClass32 {
+			Errorf(nil, "%s: elf object but not mips", pn)
+			return
+		}
+
 	case sys.MIPS64:
 		if elfobj.machine != ElfMachMips || hdr.Ident[4] != ElfClass64 {
 			Errorf(nil, "%s: elf object but not mips64", pn)
