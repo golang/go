6c295a9a71924478a344e7b447ff3b44b1e94511
Russ Cox <rsc@golang.org>
2016-10-18 22:56:14 -0400

    syscall: for ForkExec on Linux, always use 32-bit setgroups system call
    Fixes #17092.
    
    Change-Id: If203d802a919e00594ddc1282782fc59a083fd63
    Reviewed-on: https://go-review.googlesource.com/31458
    Reviewed-by: Brad Fitzpatrick <bradfitz@golang.org>

--- src/syscall/syscall_linux_386.go
+++ src/syscall/syscall_linux_386.go
@@ -10,8 +10,9 @@ package syscall
 import "unsafe"
 
 const (
-	_SYS_dup      = SYS_DUP2
-	_SYS_getdents = SYS_GETDENTS64
+	_SYS_dup       = SYS_DUP2
+	_SYS_getdents  = SYS_GETDENTS64
+	_SYS_setgroups = SYS_SETGROUPS32
 )
 
 func setTimespec(sec, nsec int64) Timespec {
