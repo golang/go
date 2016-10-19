5b147122d6094b792a027b892884b994fe77a4d6
Vladimir Stefanovic <vladimir.stefanovic@imgtec.com>
2016-10-19 17:55:10 +0200

    cmd/dist: add support for GOARCH=mips{,le}
    Change-Id: I6e24d22eada190e9aa2adc161be7a753c8e5054b
    Reviewed-on: https://go-review.googlesource.com/31514
    Run-TryBot: Brad Fitzpatrick <bradfitz@golang.org>
    Reviewed-by: Cherry Zhang <cherryyz@google.com>

--- src/cmd/compile/main.go
+++ src/cmd/compile/main.go
@@ -9,6 +9,7 @@ import (
 	"cmd/compile/internal/arm"
 	"cmd/compile/internal/arm64"
 	"cmd/compile/internal/gc"
+	"cmd/compile/internal/mips"
 	"cmd/compile/internal/mips64"
 	"cmd/compile/internal/ppc64"
 	"cmd/compile/internal/s390x"
@@ -36,6 +37,8 @@ func main() {
 		arm.Init()
 	case "arm64":
 		arm64.Init()
+	case "mips", "mipsle":
+		mips.Init()
 	case "mips64", "mips64le":
 		mips64.Init()
 	case "ppc64", "ppc64le":
