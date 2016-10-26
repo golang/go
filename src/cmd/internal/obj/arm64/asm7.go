a866df26711c615dfe92ab1b00d256d3d1085f0e
Cherry Zhang <cherryyz@google.com>
2016-10-26 18:10:40 -0400

    cmd/internal/obj/arm64: materialize float constant 0 from zero register
    Materialize float constant 0 from integer zero register, instead
    of loading from constant pool.
    
    Also fix assembling FMOV from zero register to FP register.
    
    Change-Id: Ie413dd342cedebdb95ba8cfc220e23ed2a39e885
    Reviewed-on: https://go-review.googlesource.com/32250
    Run-TryBot: Cherry Zhang <cherryyz@google.com>
    TryBot-Result: Gobot Gobot <gobot@golang.org>
    Reviewed-by: David Chase <drchase@google.com>

--- src/cmd/internal/obj/arm64/asm7.go
+++ src/cmd/internal/obj/arm64/asm7.go
@@ -2380,13 +2380,15 @@ func asmout(ctxt *obj.Link, p *obj.Prog, o *Optab, out []uint32) {
 		o2 |= uint32(p.To.Reg & 31)
 
 	case 29: /* op Rn, Rd */
-		if (p.As == AFMOVD || p.As == AFMOVS) && (aclass(ctxt, &p.From) == C_REG || aclass(ctxt, &p.To) == C_REG) {
+		fc := aclass(ctxt, &p.From)
+		tc := aclass(ctxt, &p.To)
+		if (p.As == AFMOVD || p.As == AFMOVS) && (fc == C_REG || fc == C_ZCON || tc == C_REG || tc == C_ZCON) {
 			// FMOV Rx, Fy or FMOV Fy, Rx
 			o1 = FPCVTI(0, 0, 0, 0, 6)
 			if p.As == AFMOVD {
 				o1 |= 1<<31 | 1<<22 // 64-bit
 			}
-			if aclass(ctxt, &p.From) == C_REG {
+			if fc == C_REG || fc == C_ZCON {
 				o1 |= 1 << 16 // FMOV Rx, Fy
 			}
 		} else {
