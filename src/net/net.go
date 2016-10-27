80bcfaf4d164e38f3f10f723be54dde149a471a8
Russ Cox <rsc@golang.org>
2016-10-26 20:05:20 -0400

    net: add examples to Addr definition
    Fixes #16014.
    
    Change-Id: I68b096df7924a3258e940c4d94ec5d06f5bdfcbb
    Reviewed-on: https://go-review.googlesource.com/32097
    Reviewed-by: Brad Fitzpatrick <bradfitz@golang.org>

--- src/net/net.go
+++ src/net/net.go
@@ -102,9 +102,13 @@ func init() {
 }
 
 // Addr represents a network end point address.
+//
+// The two methods Network and String conventionally return strings
+// that can be passed as the arguments to Dial, but the exact form
+// and meaning of the strings is up to the implementation.
 type Addr interface {
-	Network() string // name of the network
-	String() string  // string form of address
+	Network() string // name of the network (for example, "tcp", "udp")
+	String() string  // string form of address (for example, "127.0.0.1:25", "[2001:db8::1]:80")
 }
 
 // Conn is a generic stream-oriented network connection.
