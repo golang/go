8462b1669fefea1e1b88d7bce510d5b684996224
Zac Bergquist <zbergquist99@gmail.com>
2016-12-05 08:18:38 -0700

    go/buildutil: fix doc reference
    Fix reference to OverlayContext in the doc for
    ParseOverlayArchive.
    
    Change-Id: I0b1db478489764fa309cb2e2f788865ac7ff71bb
    Reviewed-on: https://go-review.googlesource.com/33916
    Reviewed-by: Brad Fitzpatrick <bradfitz@golang.org>

--- go/buildutil/overlay.go
+++ go/buildutil/overlay.go
@@ -58,7 +58,7 @@ func OverlayContext(orig *build.Context, overlay map[string][]byte) *build.Conte
 }
 
 // ParseOverlayArchive parses an archive containing Go files and their
-// contents. The result is intended to be used with Overlay.
+// contents. The result is intended to be used with OverlayContext.
 //
 //
 // Archive format
