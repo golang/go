cbef450df797c473c9ca01f8d0c81ea26d106c24
Michael Matloob <matloob@golang.org>
2016-12-08 16:54:07 -0500

    runtime/pprof: symbolize proto profiles
    When generating pprof profiles in proto format, symbolize the profiles.
    
    Change-Id: I2471ed7f919483e5828868306418a63e41aff5c5
    Reviewed-on: https://go-review.googlesource.com/34192
    Run-TryBot: Russ Cox <rsc@golang.org>
    TryBot-Result: Gobot Gobot <gobot@golang.org>
    Reviewed-by: Russ Cox <rsc@golang.org>

--- src/runtime/pprof/internal/protopprof/protopprof.go
+++ src/runtime/pprof/internal/protopprof/protopprof.go
@@ -11,6 +11,7 @@ import (
 	"fmt"
 	"os"
 	"runtime"
+	"strings"
 	"time"
 	"unsafe"
 
@@ -91,6 +92,7 @@ func TranslateCPUProfile(b []byte, startTime time.Time) (*profile.Profile, error
 			return nil, err
 		}
 	}
+	symbolize(p)
 	return p, nil
 }
 
@@ -102,4 +104,74 @@ func addMappings(p *profile.Profile) error {
 	}
 	defer f.Close()
 	return p.ParseMemoryMap(f)
+}
+
+type function interface {
+	Name() string
+	FileLine(pc uintptr) (string, int)
+}
+
+// funcForPC is a wrapper for runtime.FuncForPC. Defined as var for testing.
+var funcForPC = func(pc uintptr) function {
+	if f := runtime.FuncForPC(pc); f != nil {
+		return f
+	}
+	return nil
+}
+
+func symbolize(p *profile.Profile) {
+	fns := profileFunctionMap{}
+	for _, l := range p.Location {
+		pc := uintptr(l.Address)
+		f := funcForPC(pc)
+		if f == nil {
+			continue
+		}
+		file, lineno := f.FileLine(pc)
+		l.Line = []profile.Line{
+			{
+				Function: fns.findOrAddFunction(f.Name(), file, p),
+				Line:     int64(lineno),
+			},
+		}
+	}
+	// Trim runtime functions. Always hide runtime.goexit. Other runtime
+	// functions are only hidden for heapz when they appear at the beginning.
+	isHeapz := p.PeriodType != nil && p.PeriodType.Type == "space"
+	for _, s := range p.Sample {
+		show := !isHeapz
+		var i int
+		for _, l := range s.Location {
+			if len(l.Line) > 0 && l.Line[0].Function != nil {
+				name := l.Line[0].Function.Name
+				if name == "runtime.goexit" || !show && strings.HasPrefix(name, "runtime.") {
+					continue
+				}
+			}
+			show = true
+			s.Location[i] = l
+			i++
+		}
+		s.Location = s.Location[:i]
+	}
+}
+
+type profileFunctionMap map[profile.Function]*profile.Function
+
+func (fns profileFunctionMap) findOrAddFunction(name, filename string, p *profile.Profile) *profile.Function {
+	f := profile.Function{
+		Name:       name,
+		SystemName: name,
+		Filename:   filename,
+	}
+	if fp := fns[f]; fp != nil {
+		return fp
+	}
+	fp := new(profile.Function)
+	fns[f] = fp
+
+	*fp = f
+	fp.ID = uint64(len(p.Function) + 1)
+	p.Function = append(p.Function, fp)
+	return fp
 }
