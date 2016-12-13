49d2be4cff1c07f4402dbd54ace2c533fc2d4747
Matt Layher <mdlayher@gmail.com>
2016-12-13 17:57:06 -0500

    encoding/json: add Valid for checking validity of input bytes
    Fixes #18086
    
    Change-Id: Idc501dd37893e04a01c6ed9920147d24c0c1fa18
    Reviewed-on: https://go-review.googlesource.com/34202
    Reviewed-by: Brad Fitzpatrick <bradfitz@golang.org>
    Run-TryBot: Brad Fitzpatrick <bradfitz@golang.org>
    TryBot-Result: Gobot Gobot <gobot@golang.org>

--- src/encoding/json/scanner.go
+++ src/encoding/json/scanner.go
@@ -15,6 +15,11 @@ package json
 
 import "strconv"
 
+// Valid reports whether data is a valid JSON encoding.
+func Valid(data []byte) bool {
+	return checkValid(data, &scanner{}) == nil
+}
+
 // checkValid verifies that data is valid JSON-encoded data.
 // scan is passed in for use by checkValid to avoid an allocation.
 func checkValid(data []byte, scan *scanner) error {
