908849c3379245f71612058fbf92cf6bf58c3c75
Russ Cox <rsc@golang.org>
2016-12-20 15:06:13 -0500

    cmd/godoc: add perf subrepo
    For golang/go#14304.
    
    Change-Id: Ia90193d3a9ad027231f977ffa6b66cf60ea40683
    Reviewed-on: https://go-review.googlesource.com/34615
    Run-TryBot: Russ Cox <rsc@golang.org>
    Reviewed-by: Brad Fitzpatrick <bradfitz@golang.org>

--- cmd/godoc/x.go
+++ cmd/godoc/x.go
@@ -35,6 +35,7 @@ var xMap = map[string]xRepo{
 	"mobile":     {"https://go.googlesource.com/mobile", "git"},
 	"net":        {"https://go.googlesource.com/net", "git"},
 	"oauth2":     {"https://go.googlesource.com/oauth2", "git"},
+	"perf":       {"https://go.googlesource.com/perf", "git"},
 	"playground": {"https://go.googlesource.com/playground", "git"},
 	"review":     {"https://go.googlesource.com/review", "git"},
 	"sync":       {"https://go.googlesource.com/sync", "git"},
