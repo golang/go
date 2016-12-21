459c9c36453a5802ebf38483123cce11fe170e74
Brad Fitzpatrick <bradfitz@golang.org>
2016-12-21 03:24:08 +0000

    Revert "cmd/go: note when some Go files were ignored on no-Go-files errors"
    This reverts commit 029097f5a0603601aa7a92586ea31d244ed7c987
    (https://golang.org/cl/29113)
    
    The " (.go files ignored due to build tags)" error message is not
    always accurate.
    
    Fixes #18396
    Updates #17008
    
    Change-Id: I609653120603a7f6094bc1dc3a83856f4b259241
    Reviewed-on: https://go-review.googlesource.com/34662
    Run-TryBot: Brad Fitzpatrick <bradfitz@golang.org>
    TryBot-Result: Gobot Gobot <gobot@golang.org>
    Reviewed-by: Russ Cox <rsc@golang.org>

--- src/go/build/build.go
+++ src/go/build/build.go
@@ -439,16 +439,11 @@ func (ctxt *Context) ImportDir(dir string, mode ImportMode) (*Package, error) {
 // containing no buildable Go source files. (It may still contain
 // test files, files hidden by build tags, and so on.)
 type NoGoError struct {
-	Dir     string
-	Ignored bool // whether any Go files were ignored due to build tags
+	Dir string
 }
 
 func (e *NoGoError) Error() string {
-	msg := "no buildable Go source files in " + e.Dir
-	if e.Ignored {
-		msg += " (.go files ignored due to build tags)"
-	}
-	return msg
+	return "no buildable Go source files in " + e.Dir
 }
 
 // MultiplePackageError describes a directory containing
@@ -880,7 +875,7 @@ Found:
 		return p, badGoError
 	}
 	if len(p.GoFiles)+len(p.CgoFiles)+len(p.TestGoFiles)+len(p.XTestGoFiles) == 0 {
-		return p, &NoGoError{Dir: p.Dir, Ignored: len(p.IgnoredGoFiles) > 0}
+		return p, &NoGoError{p.Dir}
 	}
 
 	for tag := range allTags {
