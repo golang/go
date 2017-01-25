b2a6c147a4b8355cfc023a633c67a86a7e91b476
David Crawshaw <crawshaw@golang.org>
2017-01-24 20:19:36 -0800

    runtime: reorder modules so main.main comes first
    Modules appear in the moduledata linked list in the order they are
    loaded by the dynamic loader, with one exception: the
    firstmoduledata itself the module that contains the runtime.
    This is not always the first module (when using -buildmode=shared,
    it is typically libstd.so, the second module).
    
    The order matters for typelinksinit, so we swap the first module
    with whatever module contains the main function.
    
    Updates #18729
    
    This fixes the test case extracted with -linkshared, and now
    
    	go test -linkshared encoding/...
    
    passes. However the original issue about a plugin failure is not
    yet fixed.
    
    Change-Id: I9f399ecc3518e22e6b0a350358e90b0baa44ac96
    Reviewed-on: https://go-review.googlesource.com/35644
    Run-TryBot: David Crawshaw <crawshaw@golang.org>
    TryBot-Result: Gobot Gobot <gobot@golang.org>
    Reviewed-by: Brad Fitzpatrick <bradfitz@golang.org>
    Reviewed-by: Michael Hudson-Doyle <michael.hudson@canonical.com>
    Reviewed-by: Ian Lance Taylor <iant@golang.org>

--- misc/cgo/testshared/src/exe/exe.go
+++ misc/cgo/testshared/src/exe/exe.go
@@ -19,6 +19,8 @@ func F() *C {
 	return nil
 }
 
+var slicePtr interface{} = &[]int{}
+
 func main() {
 	defer depBase.ImplementedInAsm()
 	// This code below causes various go.itab.* symbols to be generated in
@@ -31,5 +33,12 @@ func main() {
 	var c *C
 	if reflect.TypeOf(F).Out(0) != reflect.TypeOf(c) {
 		panic("bad reflection results, see golang.org/issue/18252")
+	}
+
+	sp := reflect.New(reflect.TypeOf(slicePtr).Elem())
+	s := sp.Interface()
+
+	if reflect.TypeOf(s) != reflect.TypeOf(slicePtr) {
+		panic("bad reflection results, see golang.org/issue/18729")
 	}
 }
