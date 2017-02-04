b15e1f19bc3453acdeebb3a9230662b38b3bafd2
Alex Brainman <alex.brainman@gmail.com>
2017-02-04 17:33:14 +1100

    cmd/nm: skip TestInternalLinkerCgoFile if no internal linking is supported
    Fixes build.
    
    Change-Id: I2fee624c8a4b228bb9f2889e241ea016a317bb11
    Reviewed-on: https://go-review.googlesource.com/36373
    TryBot-Result: Gobot Gobot <gobot@golang.org>
    Reviewed-by: Alex Brainman <alex.brainman@gmail.com>

--- src/cmd/nm/nm_cgo_test.go
+++ src/cmd/nm/nm_cgo_test.go
@@ -7,11 +7,28 @@
 package main
 
 import (
+	"runtime"
 	"testing"
 )
 
 func TestInternalLinkerCgoFile(t *testing.T) {
+	if !canInternalLink() {
+		t.Skip("skipping; internal linking is not supported")
+	}
 	testGoFile(t, true, false)
+}
+
+func canInternalLink() bool {
+	switch runtime.GOOS {
+	case "dragonfly":
+		return false
+	case "linux":
+		switch runtime.GOARCH {
+		case "arm64", "mips64", "mips64le", "mips", "mipsle":
+			return false
+		}
+	}
+	return true
 }
 
 func TestExternalLinkerCgoFile(t *testing.T) {
