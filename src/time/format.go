67c3d4dab06530b4a444c3a076fceaa5573cf2b6
Sameer Ajmani <sameer@golang.org>
2017-02-07 12:27:29 -0500

    time: delete incorrect docs about day-of-month checks.
    Documentation was introduced by CL https://golang.org/cl/14123
    but that behavior was changed later by CL https://golang.org/cl/17710.
    This CL deletes the stale paragraph.
    
    Fixes #18980
    
    Change-Id: Ib434f1eac6fc814fde1be112a8f52afe6e3e0fcc
    Reviewed-on: https://go-review.googlesource.com/36532
    Reviewed-by: Brad Fitzpatrick <bradfitz@golang.org>

--- src/time/format.go
+++ src/time/format.go
@@ -758,11 +758,6 @@ func skip(value, prefix string) (string, error) {
 // location and zone in the returned time. Otherwise it records the time as
 // being in a fabricated location with time fixed at the given zone offset.
 //
-// No checking is done that the day of the month is within the month's
-// valid dates; any one- or two-digit value is accepted. For example
-// February 31 and even February 99 are valid dates, specifying dates
-// in March and May. This behavior is consistent with time.Date.
-//
 // When parsing a time with a zone abbreviation like MST, if the zone abbreviation
 // has a defined offset in the current location, then that offset is used.
 // The zone abbreviation "UTC" is recognized as UTC regardless of location.
