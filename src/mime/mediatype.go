b9f6b22a0100716db1f196a395f9ded8456337f8
Alexey Neganov <neganovalexey@gmail.com>
2017-03-14 21:11:42 +0300

    mime: handling invalid mime media parameters
    Sometimes it's necessary to deal with emails that do not follow the specification; in particular, it's possible to download such email via gmail.
    When the existing implementation handle invalid mime media parameters, it returns nils and error, although there is a valid media type, which may be returned.
    If this behavior changes, it may not affect any existing programs, but it will help to parse some emails.
    
    Fixes #19498
    
    Change-Id: Ieb2fdbddfd93857faee941d2aa49d59e286d57fd
    Reviewed-on: https://go-review.googlesource.com/38190
    Reviewed-by: Ian Lance Taylor <iant@golang.org>
    Run-TryBot: Ian Lance Taylor <iant@golang.org>
    TryBot-Result: Gobot Gobot <gobot@golang.org>

--- src/mime/mediatype.go
+++ src/mime/mediatype.go
@@ -94,11 +94,19 @@ func checkMediaTypeDisposition(s string) error {
 	return nil
 }
 
+// ErrInvalidMediaParameter is returned by ParseMediaType if
+// the media type value was found but there was an error parsing
+// the optional parameters
+var ErrInvalidMediaParameter = errors.New("mime: invalid media parameter")
+
 // ParseMediaType parses a media type value and any optional
 // parameters, per RFC 1521.  Media types are the values in
 // Content-Type and Content-Disposition headers (RFC 2183).
 // On success, ParseMediaType returns the media type converted
 // to lowercase and trimmed of white space and a non-nil map.
+// If there is an error parsing the optional parameter,
+// the media type will be returned along with the error
+// ErrInvalidMediaParameter.
 // The returned map, params, maps from the lowercase
 // attribute to the attribute value with its case preserved.
 func ParseMediaType(v string) (mediatype string, params map[string]string, err error) {
@@ -134,7 +142,7 @@ func ParseMediaType(v string) (mediatype string, params map[string]string, err e
 				return
 			}
 			// Parse error.
-			return "", nil, errors.New("mime: invalid media parameter")
+			return mediatype, nil, ErrInvalidMediaParameter
 		}
 
 		pmap := params
