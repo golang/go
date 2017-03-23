9330ef869ce9cf281ec5d78e73824a8b3a6c7c2b
Josh Bleecher Snyder <josharian@gmail.com>
2017-03-23 11:21:42 -0700

    cmd/compile: use Widthptr instead of Types[Tptr].Width
    Change-Id: I21e3abcfd1859f933f55fe875476dec07e43b038
    Reviewed-on: https://go-review.googlesource.com/38466
    Run-TryBot: Josh Bleecher Snyder <josharian@gmail.com>
    TryBot-Result: Gobot Gobot <gobot@golang.org>
    Reviewed-by: Matthew Dempsky <mdempsky@google.com>

--- src/cmd/compile/internal/gc/dcl.go
+++ src/cmd/compile/internal/gc/dcl.go
@@ -1003,7 +1003,7 @@ func methodsym(nsym *Sym, t0 *Type, iface int) *Sym {
 	suffix = ""
 	if iface != 0 {
 		dowidth(t0)
-		if t0.Width < Types[Tptr].Width {
+		if t0.Width < int64(Widthptr) {
 			suffix = "·i"
 		}
 	}
