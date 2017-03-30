a8b2e4a630a5991e91095d85c604dc1fa23c1e56
Lynn Boger <laboger@linux.vnet.ibm.com>
2017-03-30 11:07:36 -0400

    cmd/compile: improve LoweredMove performance on ppc64x
    This change improves the performance for LoweredMove on ppc64le
    and ppc64.
    
    benchmark                   old ns/op     new ns/op     delta
    BenchmarkCopyFat8-16        0.93          0.69          -25.81%
    BenchmarkCopyFat12-16       2.61          1.85          -29.12%
    BenchmarkCopyFat16-16       9.68          1.89          -80.48%
    BenchmarkCopyFat24-16       4.48          1.85          -58.71%
    BenchmarkCopyFat32-16       6.12          1.82          -70.26%
    BenchmarkCopyFat64-16       21.2          2.70          -87.26%
    BenchmarkCopyFat128-16      29.6          3.97          -86.59%
    BenchmarkCopyFat256-16      52.6          13.4          -74.52%
    BenchmarkCopyFat512-16      97.1          18.7          -80.74%
    BenchmarkCopyFat1024-16     186           35.3          -81.02%
    
    BenchmarkAssertE2TLarge-16      14.2          5.06          -64.37%
    
    Fixes #19785
    
    Change-Id: I7d5e0052712b75811c02c7d86c5112e5649ad782
    Reviewed-on: https://go-review.googlesource.com/38950
    Reviewed-by: Keith Randall <khr@golang.org>

--- src/cmd/compile/internal/ssa/opGen.go
+++ src/cmd/compile/internal/ssa/opGen.go
@@ -17409,17 +17409,16 @@ var opcodeTable = [...]opInfo{
 	{
 		name:           "LoweredMove",
 		auxType:        auxInt64,
-		argLen:         4,
+		argLen:         3,
 		clobberFlags:   true,
 		faultOnNilArg0: true,
 		faultOnNilArg1: true,
 		reg: regInfo{
 			inputs: []inputInfo{
-				{0, 8},          // R3
-				{1, 16},         // R4
-				{2, 1073733624}, // R3 R4 R5 R6 R7 R8 R9 R10 R11 R12 R14 R15 R16 R17 R18 R19 R20 R21 R22 R23 R24 R25 R26 R27 R28 R29
+				{0, 8},  // R3
+				{1, 16}, // R4
 			},
-			clobbers: 24, // R3 R4
+			clobbers: 1944, // R3 R4 R7 R8 R9 R10
 		},
 	},
 	{
