fb28f5ba3aa1ca704295b9960c332b0178335788
Michael Munday <munday@ca.ibm.com>
2017-04-19 13:48:33 -0400

    runtime: avoid restricting GOARCH values in documentation
    Changes the text to match GOOS which appends 'and so on' at the
    end to avoid restricting the set of possible values.
    
    Change-Id: I54bcde71334202cf701662cdc2582c974ba8bf53
    Reviewed-on: https://go-review.googlesource.com/41074
    Reviewed-by: Ian Lance Taylor <iant@golang.org>

--- src/runtime/extern.go
+++ src/runtime/extern.go
@@ -235,5 +235,5 @@ func Version() string {
 const GOOS string = sys.GOOS
 
 // GOARCH is the running program's architecture target:
-// 386, amd64, arm, or s390x.
+// one of 386, amd64, arm, s390x, and so on.
 const GOARCH string = sys.GOARCH
