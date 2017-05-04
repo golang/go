37b37421eb98d5da3fe6a88944b8adfcdbeb5824
Josh Bleecher Snyder <josharian@gmail.com>
2017-05-04 08:41:49 -0700

    go/constant: avoid generating rats for large negative exponents
    Fixes #20228
    
    Change-Id: I1893ae3e192da01f9befe5469b2a32e534a691ba
    Reviewed-on: https://go-review.googlesource.com/42592
    Reviewed-by: Robert Griesemer <gri@golang.org>

--- src/go/constant/value.go
+++ src/go/constant/value.go
@@ -247,6 +247,13 @@ func makeFloatFromLiteral(lit string) Value {
 	if f, ok := newFloat().SetString(lit); ok {
 		if smallRat(f) {
 			// ok to use rationals
+			if f.Sign() == 0 {
+				// Issue 20228: If the float underflowed to zero, parse just "0".
+				// Otherwise, lit might contain a value with a large negative exponent,
+				// such as -6e-1886451601. As a float, that will underflow to 0,
+				// but it'll take forever to parse as a Rat.
+				lit = "0"
+			}
 			r, _ := newRat().SetString(lit)
 			return ratVal{r}
 		}
