ec0ee7d35737ef137175e92d563c8c4315820204
Brad Fitzpatrick <bradfitz@golang.org>
2017-05-04 02:49:23 +0000

    crypto: revise wording about cryptographically broken algorithms
    Updates text from https://golang.org/cl/42511
    
    Updates #14395
    
    Change-Id: I711100525e074ab360e577520280c37645db1c95
    Reviewed-on: https://go-review.googlesource.com/42614
    Reviewed-by: Emmanuel Odeke <emm.odeke@gmail.com>
    Reviewed-by: Rob Pike <r@golang.org>

--- src/crypto/rc4/rc4.go
+++ src/crypto/rc4/rc4.go
@@ -5,7 +5,8 @@
 // Package rc4 implements RC4 encryption, as defined in Bruce Schneier's
 // Applied Cryptography.
 //
-// RC4 is broken and should not be used anymore.
+// RC4 is cryptographically broken and should not be used for secure
+// applications.
 package rc4
 
 import "strconv"
--- src/crypto/sha1/sha1.go
+++ src/crypto/sha1/sha1.go
@@ -4,7 +4,8 @@
 
 // Package sha1 implements the SHA-1 hash algorithm as defined in RFC 3174.
 //
-// SHA-1 is broken and should not be used anymore.
+// SHA-1 is cryptographically broken and should not be used for secure
+// applications.
 package sha1
 
 import (
