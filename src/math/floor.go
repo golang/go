31bfacbe3685244255a14a834b5f2871a79adf3e
Mark Pulford <mark@kyne.com.au>
2017-05-18 22:15:18 +1000

    math: Add Round function (ties away from zero)
    This function avoids subtle faults found in many ad-hoc implementations,
    and is simple enough to be inlined by the compiler.
    
    Fixes #20100
    
    Change-Id: Ib320254e9b1f1f798c6ef906b116f63bc29e8d08
    Reviewed-on: https://go-review.googlesource.com/43652
    Reviewed-by: Robert Griesemer <gri@golang.org>

--- src/math/floor.go
+++ src/math/floor.go
@@ -1,4 +1,4 @@
-// Copyright 2009-2010 The Go Authors. All rights reserved.
+// Copyright 2009 The Go Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
@@ -53,4 +53,47 @@ func trunc(x float64) float64 {
 	}
 	d, _ := Modf(x)
 	return d
+}
+
+// Round returns the nearest integer, rounding half away from zero.
+//
+// Special cases are:
+//	Round(±0) = ±0
+//	Round(±Inf) = ±Inf
+//	Round(NaN) = NaN
+func Round(x float64) float64 {
+	// Round is a faster implementation of:
+	//
+	// func Round(x float64) float64 {
+	//   t := Trunc(x)
+	//   if Abs(x-t) >= 0.5 {
+	//     return t + Copysign(1, x)
+	//   }
+	//   return t
+	// }
+	const (
+		signMask = 1 << 63
+		fracMask = 1<<shift - 1
+		half     = 1 << (shift - 1)
+		one      = bias << shift
+	)
+
+	bits := Float64bits(x)
+	e := uint(bits>>shift) & mask
+	if e < bias {
+		// Round abs(x) < 1 including denormals.
+		bits &= signMask // +-0
+		if e == bias-1 {
+			bits |= one // +-1
+		}
+	} else if e < bias+shift {
+		// Round any abs(x) >= 1 containing a fractional component [0,1).
+		//
+		// Numbers with larger exponents are returned unchanged since they
+		// must be either an integer, infinity, or NaN.
+		e -= bias
+		bits += half >> e
+		bits &^= fracMask >> e
+	}
+	return Float64frombits(bits)
 }
