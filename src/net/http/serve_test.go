4337c8395a27b047b0c86ad317a709c757dbca05
Emmanuel Odeke <emm.odeke@gmail.com>
2017-05-24 19:36:41 -0600

    net/http: revert CL 43779
    CL 43779/commit 68b936ef4b8c02ecbc7bc29b4d894ae3863a92a1
    broke the builds at tip, and that CL doesn't account for
    cases where Redirect is directly invoked with a full URL
    that itself has a query string.
    
    Updates #17841
    
    Change-Id: Idb0486bae8625e1f9e033ca4cfcd87de95bc835c
    Reviewed-on: https://go-review.googlesource.com/44100
    Reviewed-by: Brad Fitzpatrick <bradfitz@golang.org>

--- src/net/http/serve_test.go
+++ src/net/http/serve_test.go
@@ -5552,48 +5552,6 @@ func TestServerValidatesMethod(t *testing.T) {
 	}
 }
 
-// Test that the special cased "/route" redirect
-// implicitly created by a registered "/route/"
-// properly sets the query string in the redirect URL.
-// See Issue 17841.
-func TestServeWithSlashRedirectKeepsQueryString(t *testing.T) {
-	setParallel(t)
-	defer afterTest(t)
-
-	writeBackQuery := func(w ResponseWriter, r *Request) {
-		fmt.Fprintf(w, "%s", r.URL.RawQuery)
-	}
-
-	mux := NewServeMux()
-	mux.HandleFunc("/testOne", writeBackQuery)
-	mux.HandleFunc("/testTwo/", writeBackQuery)
-
-	ts := httptest.NewServer(mux)
-	defer ts.Close()
-
-	tests := [...]struct {
-		path string
-		want string
-	}{
-		0: {"/testOne?this=that", "this=that"},
-		1: {"/testTwo?foo=bar", "foo=bar"},
-		2: {"/testTwo?a=1&b=2&a=3", "a=1&b=2&a=3"},
-		3: {"/testTwo?", ""},
-	}
-
-	for i, tt := range tests {
-		res, err := ts.Client().Get(ts.URL + tt.path)
-		if err != nil {
-			continue
-		}
-		slurp, _ := ioutil.ReadAll(res.Body)
-		res.Body.Close()
-		if got, want := string(slurp), tt.want; got != want {
-			t.Errorf("#%d: got = %q; want = %q", i, got, want)
-		}
-	}
-}
-
 func BenchmarkResponseStatusLine(b *testing.B) {
 	b.ReportAllocs()
 	b.RunParallel(func(pb *testing.PB) {
