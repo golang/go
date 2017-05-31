82cfda2910242bb7933555defdcab1a5d90eeced
Cherry Zhang <cherryyz@google.com>
2017-05-31 08:30:25 -0400

    [release-branch.go1.8] cmd/compile: fix subword store/load elision for MIPS
    Apply the fix in CL 44355 to MIPS.
    
    ARM64 has these rules but commented out for performance reason.
    Fix the commented rules, in case they are enabled in the future.
    
    Enhance the test so it triggers the failure on ARM and MIPS without
    the fix.
    
    Updates #20530.
    
    Change-Id: I82d77448e3939a545fe519d0a29a164f8fa5417c
    Reviewed-on: https://go-review.googlesource.com/44430
    Run-TryBot: Cherry Zhang <cherryyz@google.com>
    TryBot-Result: Gobot Gobot <gobot@golang.org>
    Reviewed-by: David Chase <drchase@google.com>
    Reviewed-on: https://go-review.googlesource.com/70840
    Run-TryBot: Russ Cox <rsc@golang.org>
    Reviewed-by: Cherry Zhang <cherryyz@google.com>

--- test/fixedbugs/issue20530.go
+++ test/fixedbugs/issue20530.go
@@ -8,11 +8,27 @@ package main
 
 var a uint8
 
-func main() {
+//go:noinline
+func f() {
 	b := int8(func() int32 { return -1 }())
 	a = uint8(b)
 	if int32(a) != 255 {
 		// Failing case prints 'got 255 expected 255'
 		println("got", a, "expected 255")
 	}
+}
+
+//go:noinline
+func g() {
+	b := int8(func() uint32 { return 0xffffffff }())
+	a = uint8(b)
+	if int32(a) != 255 {
+		// Failing case prints 'got 255 expected 255'
+		println("got", a, "expected 255")
+	}
+}
+
+func main() {
+	f()
+	g()
 }
