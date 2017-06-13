56e5485d211a908f21820ac2c84d05ca02a58c38
Brad Fitzpatrick <bradfitz@golang.org>
2017-06-13 22:55:33 +0000

    path, path/filepath: clarify and cross-reference packages
    The path package has a reference to the path/filepath package, so add
    a reverse reference.
    
    And clarify the path package doesn't do Windows paths.
    
    Fixes #20117
    
    Change-Id: I65c5ce24e600b32ea20c5821b744bd89f6aff98c
    Reviewed-on: https://go-review.googlesource.com/45653
    Reviewed-by: Jaana Burcu Dogan <jbd@google.com>
    Reviewed-by: Ian Lance Taylor <iant@golang.org>

--- src/path/filepath/path.go
+++ src/path/filepath/path.go
@@ -4,6 +4,11 @@
 
 // Package filepath implements utility routines for manipulating filename paths
 // in a way compatible with the target operating system-defined file paths.
+//
+// The filepath package uses either forward slashes or backslashes,
+// depending on the operating system. To process paths such as URLs
+// that always use forward slashes regardless of the operating
+// system, see the path package.
 package filepath
 
 import (
