18fc49b1361910105abf0eaecd577478360706f6
Gabríel Arthúr Pétursson <gabriel@system.is>
2017-06-26 23:54:04 +0000

    test/fixedbugs: Close opening parenthesis in comment in issue20789.go
    Updates #20789
    
    Change-Id: Ic7a94394e283bc1b667cdad999ceee9513f35e37
    Reviewed-on: https://go-review.googlesource.com/46770
    Reviewed-by: Robert Griesemer <gri@golang.org>

--- test/fixedbugs/issue20789.go
+++ test/fixedbugs/issue20789.go
@@ -7,7 +7,7 @@
 // Make sure this doesn't crash the compiler.
 // (This test should be part of the internal/syntax
 // package, but we don't have a general test framework
-// there yet, so put it here for now. See also #20800.
+// there yet, so put it here for now. See also #20800.)
 
 package e
 func([<-chan<-[func u){go // ERROR "unexpected u", ERROR "must be function call"
