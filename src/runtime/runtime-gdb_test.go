e7823d656e8118534d9ddbd6d2b948c6fdad0549
Shawn Walker-Salas <shawn.walker@oracle.com>
2017-06-27 16:49:09 -0700

    runtime: temporarily skip gdb python-related tests on solaris
    Updates #20821
    
    Change-Id: I77a5b9a3bbb931845ef52a479549d71069af9540
    Reviewed-on: https://go-review.googlesource.com/46913
    Run-TryBot: Shawn Walker-Salas <shawn.walker@oracle.com>
    TryBot-Result: Gobot Gobot <gobot@golang.org>
    Reviewed-by: Brad Fitzpatrick <bradfitz@golang.org>
    Reviewed-by: Ian Lance Taylor <iant@golang.org>

--- src/runtime/runtime-gdb_test.go
+++ src/runtime/runtime-gdb_test.go
@@ -56,6 +56,10 @@ func checkGdbVersion(t *testing.T) {
 }
 
 func checkGdbPython(t *testing.T) {
+	if runtime.GOOS == "solaris" && testenv.Builder() != "solaris-amd64-smartosbuildlet" {
+		t.Skip("skipping gdb python tests on solaris; see golang.org/issue/20821")
+	}
+
 	cmd := exec.Command("gdb", "-nx", "-q", "--batch", "-iex", "python import sys; print('go gdb python support')")
 	out, err := cmd.CombinedOutput()
 
