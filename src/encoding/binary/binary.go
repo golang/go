f081266e4af4bdce22756aa58489225dfbe6bb23
Brad Fitzpatrick <bradfitz@golang.org>
2017-06-28 23:12:41 +0000

    encoding/binary: clarify the repercussions for not following the docs
    Fixes #19794
    
    Change-Id: I462cbc432fe9d4a9e6e79a9833b0013d82a0780e
    Reviewed-on: https://go-review.googlesource.com/47093
    Reviewed-by: Ian Lance Taylor <iant@golang.org>

--- src/encoding/binary/binary.go
+++ src/encoding/binary/binary.go
@@ -152,7 +152,8 @@ func (bigEndian) GoString() string { return "binary.BigEndian" }
 // When reading into structs, the field data for fields with
 // blank (_) field names is skipped; i.e., blank field names
 // may be used for padding.
-// When reading into a struct, all non-blank fields must be exported.
+// When reading into a struct, all non-blank fields must be exported
+// or Read may panic.
 //
 // The error is EOF only if no bytes were read.
 // If an EOF happens after reading some but not all the bytes,
