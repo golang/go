e58723a4bc4ceabccad25c1b2e32beb9f4565ed7
OneOfOne <oneofone@gmail.com>
2017-07-08 20:19:25 +0200

    net/url: allow *User functions to work on a nil receiver.
    Fixes #20924
    
    Change-Id: If89f31da63cbea38d7e615a428b7b07629770a45
    Reviewed-on: https://go-review.googlesource.com/47851
    Run-TryBot: Brad Fitzpatrick <bradfitz@golang.org>
    TryBot-Result: Gobot Gobot <gobot@golang.org>
    Reviewed-by: Tim Cooper <tim.cooper@layeh.com>
    Reviewed-by: Brad Fitzpatrick <bradfitz@golang.org>

--- src/net/url/url.go
+++ src/net/url/url.go
@@ -372,17 +372,26 @@ type Userinfo struct {
 
 // Username returns the username.
 func (u *Userinfo) Username() string {
+	if u == nil {
+		return ""
+	}
 	return u.username
 }
 
 // Password returns the password in case it is set, and whether it is set.
 func (u *Userinfo) Password() (string, bool) {
+	if u == nil {
+		return "", false
+	}
 	return u.password, u.passwordSet
 }
 
 // String returns the encoded userinfo information in the standard form
 // of "username[:password]".
 func (u *Userinfo) String() string {
+	if u == nil {
+		return ""
+	}
 	s := escape(u.username, encodeUserPassword)
 	if u.passwordSet {
 		s += ":" + escape(u.password, encodeUserPassword)
