ca360c39924bc1a4658c14ff023c4628c11f13a3
David Chase <drchase@google.com>
2017-07-21 12:00:38 -0400

    cmd/compile: better XPos for rematerialized values and JMPs
    This attempts to choose better values for values that are
    rematerialized (uses the XPos of the consumer, not the
    original) and for unconditional branches (uses the last
    assigned XPos in the block).
    
    The JMP branches seem to sometimes end up with a PC in the
    destination block, I think because of register movement
    or rematerialization that gets placed in predecessor blocks.
    This may be acceptable because (eyeball-empirically) that is
    often the line number of the target block, so the line number
    flow is correct.
    
    Added proper test, that checks both -N -l and regular compilation.
    The test is also capable (for gdb, delve soon) of tracking
    variable printing based on comments in the source code.
    
    There's substantial room for improvement in debugger behavior.
    
    Updates #21098.
    
    Change-Id: I13abd48a39141583b85576a015f561065819afd0
    Reviewed-on: https://go-review.googlesource.com/50610
    Run-TryBot: David Chase <drchase@google.com>
    TryBot-Result: Gobot Gobot <gobot@golang.org>
    Reviewed-by: Keith Randall <khr@golang.org>

---
+++ src/cmd/compile/internal/ssa/testdata/hist.go
@@ -0,0 +1,68 @@
+// Copyright 2017 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func ensure(n int, sl []int) []int {
+	for len(sl) <= n {
+		sl = append(sl, 0)
+	}
+	return sl
+}
+
+var cannedInput string = `1
+1
+1
+1
+2
+2
+2
+4
+4
+8
+`
+
+func main() {
+	hist := make([]int, 100)
+	var reader io.Reader = strings.NewReader(cannedInput) //gdb-dbg=(hist/A,cannedInput/A)
+	if len(os.Args) > 1 {
+		var err error
+		reader, err = os.Open(os.Args[1])
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "There was an error opening %s: %v\n", os.Args[1], err)
+			return
+		}
+	}
+	scanner := bufio.NewScanner(reader)
+	for scanner.Scan() {
+		s := scanner.Text()
+		i, err := strconv.ParseInt(s, 10, 64)
+		if err != nil { //gdb-dbg=(i)
+			fmt.Fprintf(os.Stderr, "There was an error: %v\n", err)
+			return
+		}
+		hist = ensure(int(i), hist)
+		hist[int(i)]++
+	}
+	t := 0
+	n := 0
+	for i, a := range hist {
+		if a == 0 {
+			continue
+		}
+		t += i * a
+		n += a
+		fmt.Fprintf(os.Stderr, "%d\t%d\t%d\t%d\t%d\n", i, a, n, i*a, t) //gdb-dbg=(n,i,t)
+	}
+
+}
