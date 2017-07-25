e72595ee0f97746be3ce594834a7003d5e804795
Austin Clements <austin@google.com>
2017-07-25 15:03:44 -0400

    cmd/trace: notes on MMU view improvements
    Change-Id: Ib9dcdc76095f6718f1cdc83349503f52567c76d4
    Reviewed-on: https://go-review.googlesource.com/c/60801
    Run-TryBot: Austin Clements <austin@google.com>
    TryBot-Result: Gobot Gobot <gobot@golang.org>
    Reviewed-by: Hyang-Ah Hana Kim <hyangah@gmail.com>

--- src/cmd/trace/mmu.go
+++ src/cmd/trace/mmu.go
@@ -4,6 +4,25 @@
 
 // Minimum mutator utilization (MMU) graphing.
 
+// TODO:
+//
+// In worst window list, show break-down of GC utilization sources
+// (STW, assist, etc). Probably requires a different MutatorUtil
+// representation.
+//
+// When a window size is selected, show a second plot of the mutator
+// utilization distribution for that window size.
+//
+// Render plot progressively so rough outline is visible quickly even
+// for very complex MUTs. Start by computing just a few window sizes
+// and then add more window sizes.
+//
+// Consider using sampling to compute an approximate MUT. This would
+// work by sampling the mutator utilization at randomly selected
+// points in time in the trace to build an empirical distribution. We
+// could potentially put confidence intervals on these estimates and
+// render this progressively as we refine the distributions.
+
 package main
 
 import (
