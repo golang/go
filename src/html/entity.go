8ac2bbf1e07dd8a8ff204f6999c2779858ec2d1f
Seiji Takahashi <timaki.st@gmail.com>
2017-08-03 22:08:55 +0900

    html: updated entity spec link
    Fixes #21194
    
    Change-Id: Iac5187335df67f90f0f47c7ef6574de147c2ac9b
    Reviewed-on: https://go-review.googlesource.com/52970
    Reviewed-by: Avelino <t@avelino.xxx>
    Reviewed-by: Brad Fitzpatrick <bradfitz@golang.org>

--- src/html/entity.go
+++ src/html/entity.go
@@ -8,7 +8,7 @@ package html
 const longestEntityWithoutSemicolon = 6
 
 // entity is a map from HTML entity names to their values. The semicolon matters:
-// http://www.whatwg.org/specs/web-apps/current-work/multipage/named-character-references.html
+// https://html.spec.whatwg.org/multipage/named-characters.html
 // lists both "amp" and "amp;" as two separate entries.
 //
 // Note that the HTML5 list is larger than the HTML4 list at
