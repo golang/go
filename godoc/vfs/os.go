f03b3350b7d6cb09e4221e7f2ecffaf41f89fce1
Ian Lance Taylor <iant@golang.org>
2017-08-07 16:04:23 -0700

    godoc: don't try to follow all symlinks
    Revert https://golang.org/cl/45096.
    
    Original change description:
        godoc: follow symbolic links to folders in GOROOT
    
        Directory walking in godoc relies on ReadDir which returns the result
        of os.Lstat.
    
        Instead make the the OS VFS's ReadDir use os.Stat on symlinks before
        returning.
    
    Updates golang/go#15049
    Fixes golang/go#21061
    
    Change-Id: Ieaa7923d85842f3da5696a7f46134d16407dae66
    Reviewed-on: https://go-review.googlesource.com/53634
    Run-TryBot: Ian Lance Taylor <iant@golang.org>
    TryBot-Result: Gobot Gobot <gobot@golang.org>
    Reviewed-by: Brad Fitzpatrick <bradfitz@golang.org>

--- godoc/vfs/os.go
+++ godoc/vfs/os.go
@@ -7,11 +7,9 @@ package vfs
 import (
 	"fmt"
 	"io/ioutil"
-	"log"
 	"os"
 	pathpkg "path"
 	"path/filepath"
-	"strings"
 )
 
 // OS returns an implementation of FileSystem reading from the
@@ -59,35 +57,9 @@ func (root osFS) Lstat(path string) (os.FileInfo, error) {
 }
 
 func (root osFS) Stat(path string) (os.FileInfo, error) {
-	return stat(root.resolve(path))
+	return os.Stat(root.resolve(path))
 }
 
-var readdir = ioutil.ReadDir // for testing
-var stat = os.Stat           // for testing
-
 func (root osFS) ReadDir(path string) ([]os.FileInfo, error) {
-	fis, err := readdir(root.resolve(path))
-	if err != nil {
-		return fis, err
-	}
-	ret := fis[:0]
-
-	// reread the files with os.Stat since they might be symbolic links
-	for _, fi := range fis {
-		if fi.Mode()&os.ModeSymlink != 0 {
-			baseName := fi.Name()
-			fi, err = root.Stat(pathpkg.Join(path, baseName))
-			if err != nil {
-				if os.IsNotExist(err) && strings.HasPrefix(baseName, ".") {
-					// Ignore editor spam files without log spam.
-					continue
-				}
-				log.Printf("ignoring symlink: %v", err)
-				continue
-			}
-		}
-		ret = append(ret, fi)
-	}
-
-	return ret, nil // is sorted
+	return ioutil.ReadDir(root.resolve(path)) // is sorted
 }
