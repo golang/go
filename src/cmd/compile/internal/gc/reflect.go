043302c56d8a6f021e097e63ba25e734df1c4cd6
Russ Cox <rsc@golang.org>
2017-08-28 14:27:03 -0400

    [dev.boringcrypto.go1.8] cmd/compile: hide new boring fields from reflection
    This is terrible but much simpler, cleaner, and more effective
    than all the alternatives I have come up with.
    
    Lots of code assumes that reflect.DeepEqual is meaningful
    on rsa.PublicKey etc, because previously they consisted only of
    exported meaningful fields.
    
    Worse, there exists code that assumes asn1.Marshal can be
    passed an rsa.PublicKey, because that struct has historically
    matched exactly the form that would be needed to produce
    the official ASN.1 DER encoding of an RSA public key.
    
    Instead of tracking down and fixing all of that code
    (and probably more), we can limit the BoringCrypto-induced
    damage by ensliting the compiler to hide the new field
    from reflection. Then nothing can get at it and nothing can
    be disrupted by it.
    
    Kill two birds with one cannon ball.
    
    I'm very sorry.
    
    Change-Id: I0ca4d6047c7e98f880cbb81904048c1952e278cc
    Reviewed-on: https://go-review.googlesource.com/60271
    Reviewed-by: Adam Langley <agl@golang.org>
    Reviewed-by: Ian Lance Taylor <iant@golang.org>
    Reviewed-on: https://go-review.googlesource.com/65478
    Run-TryBot: Russ Cox <rsc@golang.org>

--- src/cmd/compile/internal/gc/reflect.go
+++ src/cmd/compile/internal/gc/reflect.go
@@ -1312,9 +1312,25 @@ ok:
 	// ../../../../runtime/type.go:/structType
 	// for security, only the exported fields.
 	case TSTRUCT:
+
+		// omitFieldForAwfulBoringCryptoKludge reports whether
+		// the field t should be omitted from the reflect data.
+		// In the crypto/... packages we omit an unexported field
+		// named "boring", to keep from breaking client code that
+		// expects rsa.PublicKey etc to have only public fields.
+		// As the name suggests, this is an awful kludge, but it is
+		// limited to the dev.boringcrypto branch and avoids
+		// much more invasive effects elsewhere.
+		omitFieldForAwfulBoringCryptoKludge := func(t *types.Field) bool {
+			return strings.HasPrefix(myimportpath, "crypto/") && t.Sym != nil && t.Sym.Name == "boring"
+		}
+
 		n := 0
 
 		for _, t1 := range t.Fields().Slice() {
+			if omitFieldForAwfulBoringCryptoKludge(t1) {
+				continue
+			}
 			dtypesym(t1.Type)
 			n++
 		}
@@ -1342,6 +1358,9 @@ ok:
 		ot = dextratype(s, ot, t, dataAdd)
 
 		for _, f := range t.Fields().Slice() {
+			if omitFieldForAwfulBoringCryptoKludge(f) {
+				continue
+			}
 			// ../../../../runtime/type.go:/structField
 			ot = dnameField(s, ot, pkg, f)
 			ot = dsymptr(s, ot, dtypesym(f.Type), 0)
