65238c5251730d8200bef3cbfb4fef3a32c72227
Emmanuel Odeke <emm.odeke@gmail.com>
2017-09-14 19:12:26 -0600

    runtime: return deltimer early if timer.timersBucket is unset
    Return early from deltimer, with false as the result,
    to indicate that we couldn't delete the timer since its
    timersBucket was nil(not set) in the first place.
    
    That happens in such a case where a user created
    the timer from a Ticker with:
    
      t := time.Ticker{C: c}
    
    The above usage skips the entire setup of assigning
    the appropriate underlying runtimeTimer and timersBucket,
    steps that are done for us by time.NewTicker.
    
    CL 34784 introduced this bug with an optimization, by changing
    stopTimer to retrieve the timersBucket from the timer itself
    (which is unset with the mentioned usage pattern above),
    whereas the old  behavior relied on indexing
    by goroutine ID into the global slice of runtime
    timers, to retrieve the appropriate timersBucket.
    
    Fixes #21874
    
    Change-Id: Ie9ccc6bdee685414b2430dc4aa74ef618cea2b33
    Reviewed-on: https://go-review.googlesource.com/63970
    Run-TryBot: Emmanuel Odeke <emm.odeke@gmail.com>
    TryBot-Result: Gobot Gobot <gobot@golang.org>
    Reviewed-by: Ian Lance Taylor <iant@golang.org>

--- src/time/tick_test.go
+++ src/time/tick_test.go
@@ -35,6 +35,13 @@ func TestTicker(t *testing.T) {
 	}
 }
 
+// Issue 21874
+func TestTickerStopWithDirectInitialization(t *testing.T) {
+	c := make(chan Time)
+	tk := &Ticker{C: c}
+	tk.Stop()
+}
+
 // Test that a bug tearing down a ticker has been fixed. This routine should not deadlock.
 func TestTeardown(t *testing.T) {
 	Delta := 100 * Millisecond
