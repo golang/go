f22ef70254a6a79834ed3fa21c4d1ccadeb758bd
Matthew Dempsky <mdempsky@google.com>
2017-09-27 23:52:04 -0700

    cmd/compile: allow := to shadow dot-imported names
    Historically, gc optimistically parsed the left-hand side of
    assignments as expressions. Later, if it discovered a ":=" assignment,
    it rewrote the parsed expressions as declarations.
    
    This failed in the presence of dot imports though, because we lost
    information about whether an imported object was named via a bare
    identifier "Foo" or a normal qualified "pkg.Foo".
    
    This CL fixes the issue by specially noding the left-hand side of ":="
    assignments.
    
    Fixes #22076.
    
    Change-Id: I18190ecdb863112e7d009e1687e6112eec559921
    Reviewed-on: https://go-review.googlesource.com/66810
    Run-TryBot: Matthew Dempsky <mdempsky@google.com>
    Reviewed-by: Daniel Martí <mvdan@mvdan.cc>
    Reviewed-by: Robert Griesemer <gri@golang.org>
    TryBot-Result: Gobot Gobot <gobot@golang.org>

--- test/fixedbugs/issue20250.go
+++ test/fixedbugs/issue20250.go
@@ -16,8 +16,8 @@ type T struct {
 
 func f(a T) { // ERROR "live at entry to f: a"
 	var e interface{}
-	func() { // ERROR "live at entry to f.func1: &e a"
-		e = a.s // ERROR "live at call to convT2Estring: &e a" "live at call to writebarrierptr: a"
+	func() { // ERROR "live at entry to f.func1: a &e"
+		e = a.s // ERROR "live at call to convT2Estring: a &e" "live at call to writebarrierptr: a"
 	}() // ERROR "live at call to f.func1: e$"
 	// Before the fix, both a and e were live at the previous line.
 	_ = e
