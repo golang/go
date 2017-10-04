cb3b34520985a2d1a6797f00b9e4529271212bdd
Peter Wu <pwu@cloudflare.com>
2017-10-04 13:44:34 +0100

    crypto/tls: fix first byte test for 255 CBC padding bytes
    The BadCBCPadding255 test from bogo failed because at most 255 trailing
    bytes were checked, but for a padding of 255 there are 255 padding bytes
    plus 1 length byte with value 255.
    
    Change-Id: I7dd237c013d2c7c8599067246e31b7ba93106cf7
    Reviewed-on: https://go-review.googlesource.com/68070
    Reviewed-by: Adam Langley <agl@golang.org>
    Run-TryBot: Adam Langley <agl@golang.org>
    TryBot-Result: Gobot Gobot <gobot@golang.org>

--- src/crypto/tls/conn.go
+++ src/crypto/tls/conn.go
@@ -213,10 +213,11 @@ func extractPadding(payload []byte) (toRemove int, good byte) {
 	// if len(payload) >= (paddingLen - 1) then the MSB of t is zero
 	good = byte(int32(^t) >> 31)
 
-	toCheck := 255 // the maximum possible padding length
+	// The maximum possible padding length plus the actual length field
+	toCheck := 256
 	// The length of the padded data is public, so we can use an if here
-	if toCheck+1 > len(payload) {
-		toCheck = len(payload) - 1
+	if toCheck > len(payload) {
+		toCheck = len(payload)
 	}
 
 	for i := 0; i < toCheck; i++ {
