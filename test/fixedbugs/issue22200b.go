66978b207988cd1e8cec297b070a839fa9f6f0cc
Cherry Zhang <cherryyz@google.com>
2017-10-11 16:05:10 -0400

    test: skip issue22200b.go on mipsle
    It should be skipped on 32-bit architectures.
    
    Change-Id: If7a64b9e90e47c3e8734dd62729bfd2944ae926c
    Reviewed-on: https://go-review.googlesource.com/70071
    Reviewed-by: Ian Lance Taylor <iant@golang.org>
    Reviewed-by: Keith Randall <khr@golang.org>
    Run-TryBot: Ian Lance Taylor <iant@golang.org>

--- test/fixedbugs/issue22200b.go
+++ test/fixedbugs/issue22200b.go
@@ -4,7 +4,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// +build !386,!amd64p32,!arm,!mips
+// +build !386,!amd64p32,!arm,!mips,!mipsle
 
 package p
 
