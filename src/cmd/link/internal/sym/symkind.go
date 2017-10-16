28b7635622ca7fe8f4828dddda6834915753c6a2
Michael Hudson-Doyle <michael.hudson@canonical.com>
2017-10-16 14:20:01 +1300

    cmd/link: replace SCONTAINER with an attribute bit
    This is much easier than replacing SSUB so split it out from my other CL.
    
    Change-Id: If01e4005da5355895404456320a2156bde4ec09a
    Reviewed-on: https://go-review.googlesource.com/71050
    Run-TryBot: Michael Hudson-Doyle <michael.hudson@canonical.com>
    Reviewed-by: Ian Lance Taylor <iant@golang.org>

--- src/cmd/link/internal/sym/symkind.go
+++ src/cmd/link/internal/sym/symkind.go
@@ -107,7 +107,6 @@ const (
 	SDWARFLOC
 	SSUB       = SymKind(1 << 8)
 	SMASK      = SymKind(SSUB - 1)
-	SCONTAINER = SymKind(1 << 10) // has a sub-symbol
 )
 
 // AbiSymKindToSymKind maps values read from object files (which are
