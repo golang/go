5d168a90d907f872bd5dd6eaa11884035d19c328
Tim Cooper <tim.cooper@layeh.com>
2017-10-17 20:00:55 -0300

    os: add period at end of O_EXCL comment
    Change-Id: I9d723e49bf2cabf97a09e29e46fd2c426845fd1d
    Reviewed-on: https://go-review.googlesource.com/71470
    Reviewed-by: Ian Lance Taylor <iant@golang.org>

--- src/os/file.go
+++ src/os/file.go
@@ -66,7 +66,7 @@ const (
 	O_RDWR   int = syscall.O_RDWR   // open the file read-write.
 	O_APPEND int = syscall.O_APPEND // append data to the file when writing.
 	O_CREATE int = syscall.O_CREAT  // create a new file if none exists.
-	O_EXCL   int = syscall.O_EXCL   // used with O_CREATE, file must not exist
+	O_EXCL   int = syscall.O_EXCL   // used with O_CREATE, file must not exist.
 	O_SYNC   int = syscall.O_SYNC   // open for synchronous I/O.
 	O_TRUNC  int = syscall.O_TRUNC  // if possible, truncate file when opened.
 )
