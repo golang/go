7092a312e5651c31e4a075503b0f5799371ceef9
Matthew Dempsky <mdempsky@google.com>
2017-10-20 15:20:56 -0700

    cmd/compile: replace -l=2 with -d typecheckinl
    Currently, benchmarking compile performance under -l=4 is confounded
    by -l=2 enabling eager typechecking of unused inline function bodies
    for debugging. This isn't logically an "inlining aggressiveness"
    level, so instead move this logic under the -d umbrella flag.
    
    Change-Id: I713f68952efbe25b6941d3ebc2f3707ccbbd6240
    Reviewed-on: https://go-review.googlesource.com/72253
    Run-TryBot: Matthew Dempsky <mdempsky@google.com>
    TryBot-Result: Gobot Gobot <gobot@golang.org>
    Reviewed-by: Robert Griesemer <gri@golang.org>

--- src/cmd/compile/internal/gc/bexport.go
+++ src/cmd/compile/internal/gc/bexport.go
@@ -594,7 +594,7 @@ func isInlineable(n *Node) bool {
 	if exportInlined && n != nil && n.Func != nil && n.Func.Inl.Len() != 0 {
 		// when lazily typechecking inlined bodies, some re-exported ones may not have been typechecked yet.
 		// currently that can leave unresolved ONONAMEs in import-dot-ed packages in the wrong package
-		if Debug['l'] < 2 {
+		if Debug_typecheckinl == 0 {
 			typecheckinl(n)
 		}
 		return true
