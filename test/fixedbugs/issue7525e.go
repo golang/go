96073de8176418093213adfdf43acb65d00b8afa
Matthew Dempsky <mdempsky@google.com>
2017-10-20 11:01:43 -0700

    cmd/compile: silence unnecessary unsafe error
    If n.Type==nil after typechecking, then we should have already
    reported a more useful error somewhere else. Just return 0 in
    evalunsafe without trying to do anything else that's likely to cause
    problems.
    
    Also, further split out issue7525.go into more test files, because
    cmd/compile reports at most one typechecking loop per compilation
    unit.
    
    Fixes #22351.
    
    Change-Id: I3ebf505f72c48fcbfef5ec915606224406026597
    Reviewed-on: https://go-review.googlesource.com/72251
    Run-TryBot: Matthew Dempsky <mdempsky@google.com>
    TryBot-Result: Gobot Gobot <gobot@golang.org>
    Reviewed-by: Daniel Martí <mvdan@mvdan.cc>
    Reviewed-by: Robert Griesemer <gri@golang.org>

--- test/fixedbugs/issue7525e.go
+++ test/fixedbugs/issue7525e.go
@@ -1,6 +1,6 @@
 // errorcheck
 
-// Copyright 2014 The Go Authors. All rights reserved.
+// Copyright 2017 The Go Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
@@ -11,5 +11,5 @@ package main
 import "unsafe"
 
 var x struct {
-	a [unsafe.Sizeof(x.a)]int // ERROR "array bound|typechecking loop|invalid expression"
+	c [unsafe.Alignof(x.c)]int // ERROR "array bound|typechecking loop|invalid array"
 }
