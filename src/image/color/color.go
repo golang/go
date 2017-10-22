268d6bd6f78ba9fae23fbce43ea44268d73d98ad
Artyom Pervukhin <artyom.pervukhin@gmail.com>
2017-10-22 13:33:06 +0300

    image/draw, image/color: optimize hot path sqDiff function
    Function sqDiff is called multiple times in the hot path (x, y loops) of
    drawPaletted from the image/draw package; number of sqDiff calls is
    between 4×width×height and 4×width×height×len(palette) for each
    drawPaletted call.
    
    Simplify this function by removing arguments comparison and relying
    instead on signed to unsigned integer conversion rules and properties of
    unsigned integer values operations guaranteed by the spec:
    
    > For unsigned integer values, the operations +, -, *, and << are
    > computed modulo 2n, where n is the bit width of the unsigned integer's
    > type. Loosely speaking, these unsigned integer operations discard high
    > bits upon overflow, and programs may rely on ``wrap around''.
    
    image/gif package benchmark that depends on the code updated shows
    throughput improvements:
    
    name               old time/op    new time/op    delta
    QuantizedEncode-4     788ms ± 2%     468ms ± 9%  -40.58%  (p=0.000 n=9+10)
    
    name               old speed      new speed      delta
    QuantizedEncode-4  1.56MB/s ± 2%  2.63MB/s ± 8%  +68.47%  (p=0.000 n=9+10)
    
    Closes #22375.
    
    Change-Id: Ic9a540e39ceb21e7741d308af1cfbe61b4ac347b
    Reviewed-on: https://go-review.googlesource.com/72373
    Reviewed-by: Nigel Tao <nigeltao@golang.org>

--- src/image/color/color.go
+++ src/image/color/color.go
@@ -312,12 +312,29 @@ func (p Palette) Index(c Color) int {
 //
 // x and y are both assumed to be in the range [0, 0xffff].
 func sqDiff(x, y uint32) uint32 {
-	var d uint32
-	if x > y {
-		d = x - y
-	} else {
-		d = y - x
-	}
+	// The canonical code of this function looks as follows:
+	//
+	//	var d uint32
+	//	if x > y {
+	//		d = x - y
+	//	} else {
+	//		d = y - x
+	//	}
+	//	return (d * d) >> 2
+	//
+	// Language spec guarantees the following properties of unsigned integer
+	// values operations with respect to overflow/wrap around:
+	//
+	// > For unsigned integer values, the operations +, -, *, and << are
+	// > computed modulo 2n, where n is the bit width of the unsigned
+	// > integer's type. Loosely speaking, these unsigned integer operations
+	// > discard high bits upon overflow, and programs may rely on ``wrap
+	// > around''.
+	//
+	// Considering these properties and the fact that this function is
+	// called in the hot paths (x,y loops), it is reduced to the below code
+	// which is slightly faster. See TestSqDiff for correctness check.
+	d := x - y
 	return (d * d) >> 2
 }
 
