3caa02f603fcb895763f2f5c3f737ef69fa9cf0a
Russ Cox <rsc@golang.org>
2017-10-27 10:21:46 -0400

    sort: split post-Go1.4 code into its own file
    This will let us build the latest sort when bootstrapping the compiler.
    The compiler depends on the precise tie-breaks used by sort in
    some cases, and it's easier to bring sort along than require checking
    every sort call ever added to the compiler.
    
    Change-Id: Idc622f89aedbb40d848708c76650fc28779d0c3c
    Reviewed-on: https://go-review.googlesource.com/73951
    Run-TryBot: Russ Cox <rsc@golang.org>
    TryBot-Result: Gobot Gobot <gobot@golang.org>
    Reviewed-by: Cherry Zhang <cherryyz@google.com>

--- src/sort/sort.go
+++ src/sort/sort.go
@@ -8,8 +8,6 @@
 // collections.
 package sort
 
-import "reflect"
-
 // A type, typically a collection, that satisfies sort.Interface can be
 // sorted by the routines in this package. The methods require that the
 // elements of the collection be enumerated by an integer index.
@@ -236,43 +234,6 @@ func maxDepth(n int) int {
 type lessSwap struct {
 	Less func(i, j int) bool
 	Swap func(i, j int)
-}
-
-// Slice sorts the provided slice given the provided less function.
-//
-// The sort is not guaranteed to be stable. For a stable sort, use
-// SliceStable.
-//
-// The function panics if the provided interface is not a slice.
-func Slice(slice interface{}, less func(i, j int) bool) {
-	rv := reflect.ValueOf(slice)
-	swap := reflect.Swapper(slice)
-	length := rv.Len()
-	quickSort_func(lessSwap{less, swap}, 0, length, maxDepth(length))
-}
-
-// SliceStable sorts the provided slice given the provided less
-// function while keeping the original order of equal elements.
-//
-// The function panics if the provided interface is not a slice.
-func SliceStable(slice interface{}, less func(i, j int) bool) {
-	rv := reflect.ValueOf(slice)
-	swap := reflect.Swapper(slice)
-	stable_func(lessSwap{less, swap}, rv.Len())
-}
-
-// SliceIsSorted tests whether a slice is sorted.
-//
-// The function panics if the provided interface is not a slice.
-func SliceIsSorted(slice interface{}, less func(i, j int) bool) bool {
-	rv := reflect.ValueOf(slice)
-	n := rv.Len()
-	for i := n - 1; i > 0; i-- {
-		if less(i, i-1) {
-			return false
-		}
-	}
-	return true
 }
 
 type reverse struct {
