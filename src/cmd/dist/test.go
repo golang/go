8a26bdfd6590ecff58b903dcb79b017c93f02f78
Russ Cox <rsc@golang.org>
2017-10-30 15:28:11 -0400

    all: respect $GO_GCFLAGS during run.bash
    If the go install doesn't use the same flags as the main build
    it can overwrite the installed standard library, leading to
    flakiness and slow future tests.
    
    Force uses of 'go install' etc to propagate $GO_GCFLAGS
    or disable them entirely, to avoid problems.
    
    As I understand it, the main place this happens is the ssacheck builder.
    If there are other uses that need to run some of the now-disabled
    tests we can reenable fixed tests in followup CLs.
    
    Change-Id: Ib860a253539f402f8a96a3c00ec34f0bbf137c9a
    Reviewed-on: https://go-review.googlesource.com/74470
    Reviewed-by: David Crawshaw <crawshaw@golang.org>

--- src/cmd/dist/test.go
+++ src/cmd/dist/test.go
@@ -578,7 +578,12 @@ func (t *tester) registerTests() {
 	if t.hasBash() && t.cgoEnabled && goos != "android" && goos != "darwin" {
 		t.registerTest("testgodefs", "../misc/cgo/testgodefs", "./test.bash")
 	}
-	if t.cgoEnabled {
+
+	// Don't run these tests with $GO_GCFLAGS because most of them
+	// assume that they can run "go install" with no -gcflags and not
+	// recompile the entire standard library. If make.bash ran with
+	// special -gcflags, that's not true.
+	if t.cgoEnabled && gogcflags == "" {
 		if t.cgoTestSOSupported() {
 			t.tests = append(t.tests, distTest{
 				name:    "testso",
