9adcb2aa4107eee297917bb99f890a495f7afff9
David R. Jenni <david.r.jenni@gmail.com>
2017-11-05 10:15:23 +0100

    cmd/getgo: fix vet failure caused by missing argument to Errorf call.
    Change-Id: I2812e2c655a0e463203f5c2ede08182c40556953
    Reviewed-on: https://go-review.googlesource.com/75975
    Reviewed-by: Gabriel Aszalos <gabriel.aszalos@gmail.com>
    Reviewed-by: Brad Fitzpatrick <bradfitz@golang.org>
    Run-TryBot: Gabriel Aszalos <gabriel.aszalos@gmail.com>

--- cmd/getgo/download.go
+++ cmd/getgo/download.go
@@ -44,7 +44,7 @@ func downloadGoVersion(version, ops, arch, dest string) error {
 		return fmt.Errorf("Downloading Go from %s failed: %v", uri, err)
 	}
 	if resp.StatusCode > 299 {
-		return fmt.Errorf("Downloading Go from %s failed with HTTP status %s", resp.Status)
+		return fmt.Errorf("Downloading Go from %s failed with HTTP status %s", uri, resp.Status)
 	}
 	defer resp.Body.Close()
 
@@ -69,7 +69,7 @@ func downloadGoVersion(version, ops, arch, dest string) error {
 	}
 	defer sresp.Body.Close()
 	if sresp.StatusCode > 299 {
-		return fmt.Errorf("Downloading Go sha256 from %s.sha256 failed with HTTP status %s", sresp.Status)
+		return fmt.Errorf("Downloading Go sha256 from %s.sha256 failed with HTTP status %s", uri, sresp.Status)
 	}
 
 	shasum, err := ioutil.ReadAll(sresp.Body)
