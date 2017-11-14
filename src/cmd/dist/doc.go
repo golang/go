f1966de63f1174f93cdca87b36e8c02b7b95f652
Russ Cox <rsc@golang.org>
2017-11-13 23:06:54 -0500

    cmd/dist: adjust package doc
    Mainly capitalize the first letter.
    (Followup to CL 54351.)
    
    Change-Id: I2d5c3d72c53d3468de7a9d4af8bd009182ff3d38
    Reviewed-on: https://go-review.googlesource.com/78114
    Run-TryBot: Russ Cox <rsc@golang.org>
    Reviewed-by: Brad Fitzpatrick <bradfitz@golang.org>

--- src/cmd/dist/doc.go
+++ src/cmd/dist/doc.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// dist is the bootstrapping tool for the Go distribution.
+// Dist helps bootstrap, build, and test the Go distribution.
 //
 // Usage:
 //   go tool dist [command]
