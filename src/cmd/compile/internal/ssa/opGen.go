2ae1e1ae2f8726057914f26d5360c3403b8f049a
Austin Clements <austin@google.com>
2017-11-15 14:54:24 -0800

    runtime: buffered write barrier for s390x
    Updates #22460.
    
    Change-Id: I3f793e69577c1b837ad2666e6209a97a452405d4
    Reviewed-on: https://go-review.googlesource.com/92703
    Run-TryBot: Austin Clements <austin@google.com>
    TryBot-Result: Gobot Gobot <gobot@golang.org>
    Reviewed-by: Cherry Zhang <cherryyz@google.com>

--- src/cmd/compile/internal/ssa/opGen.go
+++ src/cmd/compile/internal/ssa/opGen.go
@@ -1686,6 +1686,7 @@ const (
 	OpS390XLoweredNilCheck
 	OpS390XLoweredRound32F
 	OpS390XLoweredRound64F
+	OpS390XLoweredWB
 	OpS390XMOVDconvert
 	OpS390XFlagEQ
 	OpS390XFlagLT
@@ -22072,6 +22073,20 @@ var opcodeTable = [...]opInfo{
 			outputs: []outputInfo{
 				{0, 4294901760}, // F0 F1 F2 F3 F4 F5 F6 F7 F8 F9 F10 F11 F12 F13 F14 F15
 			},
+		},
+	},
+	{
+		name:         "LoweredWB",
+		auxType:      auxSym,
+		argLen:       3,
+		clobberFlags: true,
+		symEffect:    SymNone,
+		reg: regInfo{
+			inputs: []inputInfo{
+				{0, 4}, // R2
+				{1, 8}, // R3
+			},
+			clobbers: 4294918144, // R14 F0 F1 F2 F3 F4 F5 F6 F7 F8 F9 F10 F11 F12 F13 F14 F15
 		},
 	},
 	{
--- src/cmd/compile/internal/ssa/rewriteS390X.go
+++ src/cmd/compile/internal/ssa/rewriteS390X.go
@@ -753,6 +753,8 @@ func rewriteValueS390X(v *Value) bool {
 		return rewriteValueS390X_OpTrunc64to32_0(v)
 	case OpTrunc64to8:
 		return rewriteValueS390X_OpTrunc64to8_0(v)
+	case OpWB:
+		return rewriteValueS390X_OpWB_0(v)
 	case OpXor16:
 		return rewriteValueS390X_OpXor16_0(v)
 	case OpXor32:
@@ -39215,6 +39217,24 @@ func rewriteValueS390X_OpTrunc64to8_0(v *Value) bool {
 		v.reset(OpCopy)
 		v.Type = x.Type
 		v.AddArg(x)
+		return true
+	}
+}
+func rewriteValueS390X_OpWB_0(v *Value) bool {
+	// match: (WB {fn} destptr srcptr mem)
+	// cond:
+	// result: (LoweredWB {fn} destptr srcptr mem)
+	for {
+		fn := v.Aux
+		_ = v.Args[2]
+		destptr := v.Args[0]
+		srcptr := v.Args[1]
+		mem := v.Args[2]
+		v.reset(OpS390XLoweredWB)
+		v.Aux = fn
+		v.AddArg(destptr)
+		v.AddArg(srcptr)
+		v.AddArg(mem)
 		return true
 	}
 }
