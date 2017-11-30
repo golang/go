871b79316ad7f2b10f1347f8d9077713afaff451
Brad Fitzpatrick <bradfitz@golang.org>
2017-11-30 16:36:34 +0000

    os: clarify docs on Interrupt and Kill
    Note that Interrupt will compile but not work on Windows.
    
    Fixes #22454
    
    Change-Id: If011c32211f4bb45d458317e113b9794d5b4a4b1
    Reviewed-on: https://go-review.googlesource.com/81035
    Reviewed-by: Ian Lance Taylor <iant@golang.org>

--- src/os/exec_posix.go
+++ src/os/exec_posix.go
@@ -10,9 +10,10 @@ import (
 	"syscall"
 )
 
-// The only signal values guaranteed to be present on all systems
-// are Interrupt (send the process an interrupt) and Kill (force
-// the process to exit).
+// The only signal values guaranteed to be present in the os package
+// on all systems are Interrupt (send the process an interrupt) and
+// Kill (force the process to exit). Interrupt is not implemented on
+// Windows; using it with os.Process.Signal will return an error.
 var (
 	Interrupt Signal = syscall.SIGINT
 	Kill      Signal = syscall.SIGKILL
