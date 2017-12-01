dc27d68936767277984ac731a06cb15c4f64c997
Tobias Klauser <tklauser@distanz.ch>
2017-12-01 12:57:57 +0000

    syscall: reuse BSD forkExecPipe for Solaris
    The function was duplicated for Solaris. Reuse the BSD version instead.
    
    Change-Id: Ibc812bcf36d21f4a7ceeef7b4fb091fa9479bfa8
    Reviewed-on: https://go-review.googlesource.com/81395
    Run-TryBot: Tobias Klauser <tobias.klauser@gmail.com>
    TryBot-Result: Gobot Gobot <gobot@golang.org>
    Reviewed-by: Ian Lance Taylor <iant@golang.org>

--- src/syscall/exec_solaris.go
+++ src/syscall/exec_solaris.go
@@ -261,17 +261,3 @@ childerror:
 		exit(253)
 	}
 }
-
-// Try to open a pipe with O_CLOEXEC set on both file descriptors.
-func forkExecPipe(p []int) error {
-	err := Pipe(p)
-	if err != nil {
-		return err
-	}
-	_, err = fcntl(p[0], F_SETFD, FD_CLOEXEC)
-	if err != nil {
-		return err
-	}
-	_, err = fcntl(p[1], F_SETFD, FD_CLOEXEC)
-	return err
-}
