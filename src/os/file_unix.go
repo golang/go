ddae7fb1e8ae0bd7c5a011b122482352185b5951
Ian Lance Taylor <iant@golang.org>
2017-12-11 16:41:37 -0800

    os: don't use test logger for Getwd
    Otherwise, on systems for which syscall does not implement Getwd,
    a lot of unnecessary files and directories get added to the testlog,
    right up the root directory. This was causing tests on such systems
    to fail to cache in practice.
    
    Updates #22593
    
    Change-Id: Ic8cb3450ea62aa0ca8eeb15754349f151cd76f85
    Reviewed-on: https://go-review.googlesource.com/83455
    Run-TryBot: Ian Lance Taylor <iant@golang.org>
    Reviewed-by: Brad Fitzpatrick <bradfitz@golang.org>
    Reviewed-by: Russ Cox <rsc@golang.org>
    TryBot-Result: Gobot Gobot <gobot@golang.org>

--- src/os/file_unix.go
+++ src/os/file_unix.go
@@ -8,7 +8,6 @@ package os
 
 import (
 	"internal/poll"
-	"internal/testlog"
 	"runtime"
 	"syscall"
 )
@@ -154,14 +153,8 @@ func epipecheck(file *File, e error) {
 // On Unix-like systems, it is "/dev/null"; on Windows, "NUL".
 const DevNull = "/dev/null"
 
-// OpenFile is the generalized open call; most users will use Open
-// or Create instead. It opens the named file with specified flag
-// (O_RDONLY etc.) and perm, (0666 etc.) if applicable. If successful,
-// methods on the returned File can be used for I/O.
-// If there is an error, it will be of type *PathError.
-func OpenFile(name string, flag int, perm FileMode) (*File, error) {
-	testlog.Open(name)
-
+// openFileNolog is the Unix implementation of OpenFile.
+func openFileNolog(name string, flag int, perm FileMode) (*File, error) {
 	chmod := false
 	if !supportsCreateWithStickyBit && flag&O_CREATE != 0 && perm&ModeSticky != 0 {
 		if _, err := Stat(name); IsNotExist(err) {
