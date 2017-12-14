c7b7c433637ba7bf14328e0fbccdd4bdbc5f65b3
Russ Cox <rsc@golang.org>
2017-12-13 21:11:47 -0500

    cmd/go: do not let test vet failures stop reporting of later test results
    (This only manifested in test vet failures for packages without tests,
    or else we'd probably have seen this sooner.)
    
    Fixes #23047.
    
    Change-Id: I41d09a7780999bbe1951377ffcc811ba86ea5000
    Reviewed-on: https://go-review.googlesource.com/83955
    Run-TryBot: Russ Cox <rsc@golang.org>
    TryBot-Result: Gobot Gobot <gobot@golang.org>
    Reviewed-by: Ian Lance Taylor <iant@golang.org>

--- src/cmd/go/internal/test/test.go
+++ src/cmd/go/internal/test/test.go
@@ -1087,7 +1087,7 @@ func builderTest(b *work.Builder, p *load.Package) (buildAction, runAction, prin
 			Func:       c.builderRunTest,
 			Deps:       []*work.Action{buildAction},
 			Package:    p,
-			IgnoreFail: true,
+			IgnoreFail: true, // run (prepare output) even if build failed
 			TryCache:   c.tryCache,
 			Objdir:     testDir,
 		}
@@ -1098,17 +1098,19 @@ func builderTest(b *work.Builder, p *load.Package) (buildAction, runAction, prin
 			addTestVet(b, pxtest, runAction, installAction)
 		}
 		cleanAction = &work.Action{
-			Mode:    "test clean",
-			Func:    builderCleanTest,
-			Deps:    []*work.Action{runAction},
-			Package: p,
-			Objdir:  testDir,
+			Mode:       "test clean",
+			Func:       builderCleanTest,
+			Deps:       []*work.Action{runAction},
+			Package:    p,
+			IgnoreFail: true, // clean even if test failed
+			Objdir:     testDir,
 		}
 		printAction = &work.Action{
-			Mode:    "test print",
-			Func:    builderPrintTest,
-			Deps:    []*work.Action{cleanAction},
-			Package: p,
+			Mode:       "test print",
+			Func:       builderPrintTest,
+			Deps:       []*work.Action{cleanAction},
+			Package:    p,
+			IgnoreFail: true, // print even if test failed
 		}
 	}
 	if installAction != nil {
