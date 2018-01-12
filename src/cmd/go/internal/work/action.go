95945d0a6f02531bd22d5cfb097bc8bcb8f607f7
Ian Lance Taylor <iant@golang.org>
2018-01-11 18:22:23 -0800

    cmd/go: make gccgo -buildmode=shared and -linkshared work again
    After CL 69831, addTransitiveLinkDeps ensures that all dependencies of
    a link appear in Deps. We no longer need to traverse through all
    actions to find them. And the old scheme of looking through all the
    actions and assuming we would see shared library actions before
    libraries they depend on no longer works.
    
    Now that we have complete deps, change to a simpler scheme in which we
    find the shared libraries in the deps, and then use that to sort the
    deps into archives and shared libraries.
    
    Fixes #22224
    
    Change-Id: I14fcc773ac59b6f5c2965cc04d4ed962442cc89e
    Reviewed-on: https://go-review.googlesource.com/87497
    Run-TryBot: Ian Lance Taylor <iant@golang.org>
    TryBot-Result: Gobot Gobot <gobot@golang.org>
    Reviewed-by: Russ Cox <rsc@golang.org>

--- src/cmd/go/internal/work/action.go
+++ src/cmd/go/internal/work/action.go
@@ -647,11 +647,9 @@ func (b *Builder) linkSharedAction(mode, depMode BuildMode, shlib string, a1 *Ac
 		// it is not present in another shared library, add it here.
 		// TODO(rsc): Maybe this should only happen if "runtime" is in the original package set.
 		// TODO(rsc): This should probably be changed to use load.LinkerDeps(p).
-		// TODO(rsc): Find out and explain here why gccgo is excluded.
-		// If the answer is that gccgo is different in implicit linker deps, maybe
-		// load.LinkerDeps should be used and updated.
-		// Link packages into a shared library.
-
+		// TODO(rsc): We don't add standard library imports for gccgo
+		// because they are all always linked in anyhow.
+		// Maybe load.LinkerDeps should be used and updated.
 		a := &Action{
 			Mode:    "go build -buildmode=shared",
 			Package: p,
