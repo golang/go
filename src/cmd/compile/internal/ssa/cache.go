4449e2ba38062044747263e75c951f4c9358cd8c
Heschi Kreinick <heschi@google.com>
2018-01-29 17:01:41 -0500

    cmd/compile/internal: reuse memory for valueToProgAfter
    Not a big improvement, but does help edge cases like the SSA package.
    Change-Id: I40e531110b97efd5f45955be477fd0f4faa8d545
    Reviewed-on: https://go-review.googlesource.com/92396
    Run-TryBot: Heschi Kreinick <heschi@google.com>
    TryBot-Result: Gobot Gobot <gobot@golang.org>
    Reviewed-by: David Chase <drchase@google.com>

--- src/cmd/compile/internal/ssa/cache.go
+++ src/cmd/compile/internal/ssa/cache.go
@@ -4,7 +4,10 @@
 
 package ssa
 
-import "sort"
+import (
+	"cmd/internal/obj"
+	"sort"
+)
 
 // A Cache holds reusable compiler state.
 // It is intended to be re-used for multiple Func compilations.
@@ -21,12 +24,13 @@ type Cache struct {
 	domblockstore []ID         // scratch space for computing dominators
 	scrSparse     []*sparseSet // scratch sparse sets to be re-used.
 
-	blockDebug      []BlockDebug
-	valueNames      [][]SlotID
-	slotLocs        []VarLoc
-	regContents     [][]SlotID
-	pendingEntries  []pendingEntry
-	pendingSlotLocs []VarLoc
+	ValueToProgAfter []*obj.Prog
+	blockDebug       []BlockDebug
+	valueNames       [][]SlotID
+	slotLocs         []VarLoc
+	regContents      [][]SlotID
+	pendingEntries   []pendingEntry
+	pendingSlotLocs  []VarLoc
 
 	liveSlotSliceBegin int
 	liveSlots          []liveSlot
