a923638b87d703bca241513a5c9fb7969e5170b6
Martin Möhrmann <moehrmann@google.com>
2018-02-04 20:57:56 +0100

    internal/cpu: remove platform specific prefix from cpu hwcap variables
    Go runtime currently only populates hwcap for ppc64 and arm64.
    While the interpretation of hwcap is platform specific the hwcap
    information is generally available on linux.
    
    Changing the runtime variable name to cpu_hwcap for cpu.hwcap makes it
    consistent with the general naming of runtime variables that are linked
    to other packages.
    
    Change-Id: I1e1f932a73ed624a219b9298faafbb6355e47ada
    Reviewed-on: https://go-review.googlesource.com/94757
    Reviewed-by: Brad Fitzpatrick <bradfitz@golang.org>
    Run-TryBot: Martin Möhrmann <moehrmann@google.com>
    TryBot-Result: Gobot Gobot <gobot@golang.org>

--- src/internal/cpu/cpu_arm64.go
+++ src/internal/cpu/cpu_arm64.go
@@ -9,8 +9,8 @@ const CacheLineSize = 64
 // arm64 doesn't have a 'cpuid' equivalent, so we rely on HWCAP/HWCAP2.
 // These are linknamed in runtime/os_linux_arm64.go and are initialized by
 // archauxv().
-var arm64_hwcap uint
-var arm64_hwcap2 uint
+var hwcap uint
+var hwcap2 uint
 
 // HWCAP/HWCAP2 bits. These are exposed by Linux.
 const (
@@ -42,30 +42,30 @@ const (
 
 func doinit() {
 	// HWCAP feature bits
-	ARM64.HasFP = isSet(arm64_hwcap, hwcap_FP)
-	ARM64.HasASIMD = isSet(arm64_hwcap, hwcap_ASIMD)
-	ARM64.HasEVTSTRM = isSet(arm64_hwcap, hwcap_EVTSTRM)
-	ARM64.HasAES = isSet(arm64_hwcap, hwcap_AES)
-	ARM64.HasPMULL = isSet(arm64_hwcap, hwcap_PMULL)
-	ARM64.HasSHA1 = isSet(arm64_hwcap, hwcap_SHA1)
-	ARM64.HasSHA2 = isSet(arm64_hwcap, hwcap_SHA2)
-	ARM64.HasCRC32 = isSet(arm64_hwcap, hwcap_CRC32)
-	ARM64.HasATOMICS = isSet(arm64_hwcap, hwcap_ATOMICS)
-	ARM64.HasFPHP = isSet(arm64_hwcap, hwcap_FPHP)
-	ARM64.HasASIMDHP = isSet(arm64_hwcap, hwcap_ASIMDHP)
-	ARM64.HasCPUID = isSet(arm64_hwcap, hwcap_CPUID)
-	ARM64.HasASIMDRDM = isSet(arm64_hwcap, hwcap_ASIMDRDM)
-	ARM64.HasJSCVT = isSet(arm64_hwcap, hwcap_JSCVT)
-	ARM64.HasFCMA = isSet(arm64_hwcap, hwcap_FCMA)
-	ARM64.HasLRCPC = isSet(arm64_hwcap, hwcap_LRCPC)
-	ARM64.HasDCPOP = isSet(arm64_hwcap, hwcap_DCPOP)
-	ARM64.HasSHA3 = isSet(arm64_hwcap, hwcap_SHA3)
-	ARM64.HasSM3 = isSet(arm64_hwcap, hwcap_SM3)
-	ARM64.HasSM4 = isSet(arm64_hwcap, hwcap_SM4)
-	ARM64.HasASIMDDP = isSet(arm64_hwcap, hwcap_ASIMDDP)
-	ARM64.HasSHA512 = isSet(arm64_hwcap, hwcap_SHA512)
-	ARM64.HasSVE = isSet(arm64_hwcap, hwcap_SVE)
-	ARM64.HasASIMDFHM = isSet(arm64_hwcap, hwcap_ASIMDFHM)
+	ARM64.HasFP = isSet(hwcap, hwcap_FP)
+	ARM64.HasASIMD = isSet(hwcap, hwcap_ASIMD)
+	ARM64.HasEVTSTRM = isSet(hwcap, hwcap_EVTSTRM)
+	ARM64.HasAES = isSet(hwcap, hwcap_AES)
+	ARM64.HasPMULL = isSet(hwcap, hwcap_PMULL)
+	ARM64.HasSHA1 = isSet(hwcap, hwcap_SHA1)
+	ARM64.HasSHA2 = isSet(hwcap, hwcap_SHA2)
+	ARM64.HasCRC32 = isSet(hwcap, hwcap_CRC32)
+	ARM64.HasATOMICS = isSet(hwcap, hwcap_ATOMICS)
+	ARM64.HasFPHP = isSet(hwcap, hwcap_FPHP)
+	ARM64.HasASIMDHP = isSet(hwcap, hwcap_ASIMDHP)
+	ARM64.HasCPUID = isSet(hwcap, hwcap_CPUID)
+	ARM64.HasASIMDRDM = isSet(hwcap, hwcap_ASIMDRDM)
+	ARM64.HasJSCVT = isSet(hwcap, hwcap_JSCVT)
+	ARM64.HasFCMA = isSet(hwcap, hwcap_FCMA)
+	ARM64.HasLRCPC = isSet(hwcap, hwcap_LRCPC)
+	ARM64.HasDCPOP = isSet(hwcap, hwcap_DCPOP)
+	ARM64.HasSHA3 = isSet(hwcap, hwcap_SHA3)
+	ARM64.HasSM3 = isSet(hwcap, hwcap_SM3)
+	ARM64.HasSM4 = isSet(hwcap, hwcap_SM4)
+	ARM64.HasASIMDDP = isSet(hwcap, hwcap_ASIMDDP)
+	ARM64.HasSHA512 = isSet(hwcap, hwcap_SHA512)
+	ARM64.HasSVE = isSet(hwcap, hwcap_SVE)
+	ARM64.HasASIMDFHM = isSet(hwcap, hwcap_ASIMDFHM)
 }
 
 func isSet(hwc uint, value uint) bool {
