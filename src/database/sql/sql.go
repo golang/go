4e5d8ecfc9cb40ed7fff127bd0788af8b18f929d
Alexey Palazhchenko <alexey.palazhchenko@gmail.com>
2018-02-06 08:56:53 +0300

    database/sql: add String method to IsolationLevel
    Fixes #23632
    
    Change-Id: I7197e13df6cf28400a6dd86c110f41129550abb6
    Reviewed-on: https://go-review.googlesource.com/92235
    Reviewed-by: Daniel Theophanes <kardianos@gmail.com>

--- src/database/sql/sql.go
+++ src/database/sql/sql.go
@@ -24,6 +24,7 @@ import (
 	"reflect"
 	"runtime"
 	"sort"
+	"strconv"
 	"sync"
 	"sync/atomic"
 	"time"
@@ -131,6 +132,31 @@ const (
 	LevelSerializable
 	LevelLinearizable
 )
+
+func (i IsolationLevel) String() string {
+	switch i {
+	case LevelDefault:
+		return "Default"
+	case LevelReadUncommitted:
+		return "Read Uncommitted"
+	case LevelReadCommitted:
+		return "Read Committed"
+	case LevelWriteCommitted:
+		return "Write Committed"
+	case LevelRepeatableRead:
+		return "Repeatable Read"
+	case LevelSnapshot:
+		return "Snapshot"
+	case LevelSerializable:
+		return "Serializable"
+	case LevelLinearizable:
+		return "Linearizable"
+	default:
+		return "IsolationLevel(" + strconv.Itoa(int(i)) + ")"
+	}
+}
+
+var _ fmt.Stringer = LevelDefault
 
 // TxOptions holds the transaction options to be used in DB.BeginTx.
 type TxOptions struct {
