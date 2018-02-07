4e1b69c6e7ad382edf9be4656148f7f0beaff16e
Mansour Rahimi <rahimi.mnr@gmail.com>
2018-02-07 02:17:50 +0100

    net: fix UDPConn readers to return truncated payload size instead of 0
    Calling UDPConn readers (Read, ReadFrom, ReadMsgUDP) to read part of
    datagram returns error (in Windows), mentioning there is more data
    available, and 0 as size of read data, even though part of data is
    already read.
    
    This fix makes UDPConn readers to return truncated payload size,
    even there is error due more data available to read.
    
    Fixes #14074
    Updates #18056
    
    Change-Id: Id7eec7f544dd759b2d970fa2561eef2937ec4662
    Reviewed-on: https://go-review.googlesource.com/92475
    Run-TryBot: Mikio Hara <mikioh.mikioh@gmail.com>
    TryBot-Result: Gobot Gobot <gobot@golang.org>
    Reviewed-by: Mikio Hara <mikioh.mikioh@gmail.com>

--- src/net/udpsock_test.go
+++ src/net/udpsock_test.go
@@ -398,9 +398,56 @@ func TestUDPZeroByteBuffer(t *testing.T) {
 		switch err {
 		case nil: // ReadFrom succeeds
 		default: // Read may timeout, it depends on the platform
-			if nerr, ok := err.(Error); (!ok || !nerr.Timeout()) && runtime.GOOS != "windows" { // Windows returns WSAEMSGSIZ
+			if nerr, ok := err.(Error); (!ok || !nerr.Timeout()) && runtime.GOOS != "windows" { // Windows returns WSAEMSGSIZE
 				t.Fatal(err)
 			}
+		}
+	}
+}
+
+func TestUDPReadSizeError(t *testing.T) {
+	switch runtime.GOOS {
+	case "nacl", "plan9":
+		t.Skipf("not supported on %s", runtime.GOOS)
+	}
+
+	c1, err := newLocalPacketListener("udp")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer c1.Close()
+
+	c2, err := Dial("udp", c1.LocalAddr().String())
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer c2.Close()
+
+	b1 := []byte("READ SIZE ERROR TEST")
+	for _, genericRead := range []bool{false, true} {
+		n, err := c2.Write(b1)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if n != len(b1) {
+			t.Errorf("got %d; want %d", n, len(b1))
+		}
+		c1.SetReadDeadline(time.Now().Add(100 * time.Millisecond))
+		b2 := make([]byte, len(b1)-1)
+		if genericRead {
+			n, err = c1.(Conn).Read(b2)
+		} else {
+			n, _, err = c1.ReadFrom(b2)
+		}
+		switch err {
+		case nil: // ReadFrom succeeds
+		default: // Read may timeout, it depends on the platform
+			if nerr, ok := err.(Error); (!ok || !nerr.Timeout()) && runtime.GOOS != "windows" { // Windows returns WSAEMSGSIZE
+				t.Fatal(err)
+			}
+		}
+		if n != len(b1)-1 {
+			t.Fatalf("got %d; want %d", n, len(b1)-1)
 		}
 	}
 }
