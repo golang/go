82b5a39ca1545198bb3885cc3ab0eb31698409f2
Peter Teichman <pteichman@fastly.com>
2018-02-12 19:23:31 +0000

    image/gif: support non-looping animated gifs (LoopCount=-1)
    The Netscape looping application extension encodes how many
    times the animation should restart, and if it's present
    there is no way to signal that a GIF should play only once.
    
    Use LoopCount=-1 to signal when a decoded GIF had no looping
    extension, and update the encoder to omit that extension
    block when LoopCount=-1.
    
    Fixes #15768
    
    GitHub-Last-Rev: 249744f0e28ef8907aa876070a102cb5493f5084
    GitHub-Pull-Request: golang/go#23761
    Change-Id: Ic915268505bf12bdad690b59148983a7d78d693b
    Reviewed-on: https://go-review.googlesource.com/93076
    Reviewed-by: Andrew Bonventre <andybons@golang.org>
    Reviewed-by: Brad Fitzpatrick <bradfitz@golang.org>
    Run-TryBot: Andrew Bonventre <andybons@golang.org>
    TryBot-Result: Gobot Gobot <gobot@golang.org>

--- src/image/gif/reader.go
+++ src/image/gif/reader.go
@@ -224,6 +224,8 @@ func (d *decoder) decode(r io.Reader, configOnly, keepAllFrames bool) error {
 		d.r = bufio.NewReader(r)
 	}
 
+	d.loopCount = -1
+
 	err := d.readHeaderAndScreenDescriptor()
 	if err != nil {
 		return err
@@ -566,9 +568,14 @@ func Decode(r io.Reader) (image.Image, error) {
 
 // GIF represents the possibly multiple images stored in a GIF file.
 type GIF struct {
-	Image     []*image.Paletted // The successive images.
-	Delay     []int             // The successive delay times, one per frame, in 100ths of a second.
-	LoopCount int               // The loop count.
+	Image []*image.Paletted // The successive images.
+	Delay []int             // The successive delay times, one per frame, in 100ths of a second.
+	// LoopCount controls the number of times an animation will be
+	// restarted during display.
+	// A LoopCount of 0 means to loop forever.
+	// A LoopCount of -1 means to show each frame only once.
+	// Otherwise, the animation is looped LoopCount+1 times.
+	LoopCount int
 	// Disposal is the successive disposal methods, one per frame. For
 	// backwards compatibility, a nil Disposal is valid to pass to EncodeAll,
 	// and implies that each frame's disposal method is 0 (no disposal
