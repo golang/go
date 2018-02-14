e3b49431c362a061b383b76b4c8f7faeb8e88c5f
Tobias Klauser <tklauser@distanz.ch>
2018-02-14 14:56:39 +0000

    syscall, os: use pipe2 syscall on NetBSD instead of pipe
    The pipe2 syscall is part of NetBSD since version 6.0 and thus exists in
    all officially supported versions (6.0 through 6.1 and 7.0+).
    
    Follows CL 38426
    
    Change-Id: I7b62b507300c3dfbcc6ae56408a7d7088ddccc77
    Reviewed-on: https://go-review.googlesource.com/94035
    Run-TryBot: Tobias Klauser <tobias.klauser@gmail.com>
    Reviewed-by: Brad Fitzpatrick <bradfitz@golang.org>
    Reviewed-by: Benny Siegert <bsiegert@gmail.com>
    TryBot-Result: Gobot Gobot <gobot@golang.org>

--- src/syscall/syscall_netbsd.go
+++ src/syscall/syscall_netbsd.go
@@ -102,13 +102,20 @@ func direntNamlen(buf []byte) (uint64, bool) {
 	return readInt(buf, unsafe.Offsetof(Dirent{}.Namlen), unsafe.Sizeof(Dirent{}.Namlen))
 }
 
-//sysnb pipe() (fd1 int, fd2 int, err error)
 func Pipe(p []int) (err error) {
+	return Pipe2(p, 0)
+}
+
+//sysnb pipe2(p *[2]_C_int, flags int) (err error)
+func Pipe2(p []int, flags int) error {
 	if len(p) != 2 {
 		return EINVAL
 	}
-	p[0], p[1], err = pipe()
-	return
+	var pp [2]_C_int
+	err := pipe2(&pp, flags)
+	p[0] = int(pp[0])
+	p[1] = int(pp[1])
+	return err
 }
 
 //sys getdents(fd int, buf []byte) (n int, err error)
