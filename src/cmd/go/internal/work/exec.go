fd5bf0393eb7b3a21b55adf2c0f1e293b24cf308
motemen <motemen@gmail.com>
2018-02-26 18:22:46 +0000

    cmd/go: fix formatting of file paths under cwd
    The output of go with -x flag is formatted in a manner that file paths
    under current directory are modified to start with a dot (.), but when
    the directory path ends with a slash (/), the formatting goes wrong.
    
    Fixes #23982
    
    Change-Id: I8f8d15dd52bee882a9c6357eb9eabdc3eaa887c3
    GitHub-Last-Rev: 1493f38bafdf2c40f16392b794fd1a12eb12a151
    GitHub-Pull-Request: golang/go#23985
    Reviewed-on: https://go-review.googlesource.com/95755
    Run-TryBot: Ian Lance Taylor <iant@golang.org>
    TryBot-Result: Gobot Gobot <gobot@golang.org>
    Reviewed-by: Ian Lance Taylor <iant@golang.org>

--- src/cmd/go/internal/work/exec.go
+++ src/cmd/go/internal/work/exec.go
@@ -1364,7 +1364,11 @@ func mayberemovefile(s string) {
 func (b *Builder) fmtcmd(dir string, format string, args ...interface{}) string {
 	cmd := fmt.Sprintf(format, args...)
 	if dir != "" && dir != "/" {
-		cmd = strings.Replace(" "+cmd, " "+dir, " .", -1)[1:]
+		dot := " ."
+		if dir[len(dir)-1] == filepath.Separator {
+			dot += string(filepath.Separator)
+		}
+		cmd = strings.Replace(" "+cmd, " "+dir, dot, -1)[1:]
 		if b.scriptDir != dir {
 			b.scriptDir = dir
 			cmd = "cd " + dir + "\n" + cmd
