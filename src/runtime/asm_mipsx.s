f6332bb84ad87e958290ae23b29a2b13a41ee2a2
Keith Randall <khr@golang.org>
2018-03-03 14:24:54 -0800

    internal/bytealg: move compare functions to bytealg
    Move bytes.Compare and runtime·cmpstring to bytealg.
    
    Update #19792
    
    Change-Id: I139e6d7c59686bef7a3017e3dec99eba5fd10447
    Reviewed-on: https://go-review.googlesource.com/98515
    Run-TryBot: Keith Randall <khr@golang.org>
    TryBot-Result: Gobot Gobot <gobot@golang.org>
    Reviewed-by: Brad Fitzpatrick <bradfitz@golang.org>

--- src/runtime/asm_mipsx.s
+++ src/runtime/asm_mipsx.s
@@ -633,69 +633,6 @@ TEXT runtime·aeshash64(SB),NOSPLIT,$0
 // Not implemented.
 TEXT runtime·aeshashstr(SB),NOSPLIT,$0
 	UNDEF
-TEXT runtime·cmpstring(SB),NOSPLIT,$0-20
-	MOVW	s1_base+0(FP), R3
-	MOVW	s1_len+4(FP), R1
-	MOVW	s2_base+8(FP), R4
-	MOVW	s2_len+12(FP), R2
-	BEQ	R3, R4, samebytes
-	SGTU	R1, R2, R7
-	MOVW	R1, R8
-	CMOVN	R7, R2, R8	// R8 is min(R1, R2)
-
-	ADDU	R3, R8	// R3 is current byte in s1, R8 is last byte in s1 to compare
-loop:
-	BEQ	R3, R8, samebytes	// all compared bytes were the same; compare lengths
-
-	MOVBU	(R3), R6
-	ADDU	$1, R3
-	MOVBU	(R4), R7
-	ADDU	$1, R4
-	BEQ	R6, R7 , loop
-	// bytes differed
-	SGTU	R6, R7, R8
-	MOVW	$-1, R6
-	CMOVZ	R8, R6, R8
-	JMP	cmp_ret
-samebytes:
-	SGTU	R1, R2, R6
-	SGTU	R2, R1, R7
-	SUBU	R7, R6, R8
-cmp_ret:
-	MOVW	R8, ret+16(FP)
-	RET
-
-TEXT bytes·Compare(SB),NOSPLIT,$0-28
-	MOVW	s1_base+0(FP), R3
-	MOVW	s2_base+12(FP), R4
-	MOVW	s1_len+4(FP), R1
-	MOVW	s2_len+16(FP), R2
-	BEQ	R3, R4, samebytes
-	SGTU	R1, R2, R7
-	MOVW	R1, R8
-	CMOVN	R7, R2, R8	// R8 is min(R1, R2)
-
-	ADDU	R3, R8	// R3 is current byte in s1, R8 is last byte in s1 to compare
-loop:
-	BEQ	R3, R8, samebytes
-
-	MOVBU	(R3), R6
-	ADDU	$1, R3
-	MOVBU	(R4), R7
-	ADDU	$1, R4
-	BEQ	R6, R7 , loop
-
-	SGTU	R6, R7, R8
-	MOVW	$-1, R6
-	CMOVZ	R8, R6, R8
-	JMP	cmp_ret
-samebytes:
-	SGTU	R1, R2, R6
-	SGTU	R2, R1, R7
-	SUBU	R7, R6, R8
-cmp_ret:
-	MOVW	R8, ret+24(FP)
-	RET
 
 TEXT runtime·return0(SB),NOSPLIT,$0
 	MOVW	$0, R1
