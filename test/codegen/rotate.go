97e56a413aadde3ea7a3bcef95b64447d545863b
Giovanni Bajo <rasky@develer.com>
2018-03-03 19:17:20 +0100

    test: convert all math-related tests from asm_test
    Change-Id: If542f0b5c5754e6eb2f9b302fe5a148ba9a57338
    Reviewed-on: https://go-review.googlesource.com/98443
    Run-TryBot: Giovanni Bajo <rasky@develer.com>
    TryBot-Result: Gobot Gobot <gobot@golang.org>
    Reviewed-by: Keith Randall <khr@golang.org>

--- test/codegen/rotate.go
+++ test/codegen/rotate.go
@@ -6,8 +6,6 @@
 
 package codegen
 
-import "math"
-
 func rot32(x uint32) uint32 {
 	var a uint32
 	a += x<<7 | x>>25 // amd64:"ROLL.*[$]7"  arm:"MOVW.*@>25"
@@ -22,10 +20,4 @@ func rot64(x uint64) uint64 {
 	a += x<<8 + x>>56 // amd64:"ROL"
 	a += x<<9 ^ x>>55 // amd64:"ROL"
 	return a
-}
-
-func copysign(a, b float64) float64 {
-	// amd64:"SHLQ\t[$]1","SHRQ\t[$]1","SHRQ\t[$]63","SHLQ\t[$]63","ORQ"
-	// ppc64le:"FCPSGN" s390x:"CPSDR",-"MOVD"
-	return math.Copysign(a, b)
 }
