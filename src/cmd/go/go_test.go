709da95513a64499c6974aadc563b9638fe9f49d
Ian Lance Taylor <iant@golang.org>
2018-03-07 07:03:46 -0800

    cmd/go: run vet on packages with only cgo files
    CgoFiles is not included in GoFiles, so we need to check both.
    
    Fixes #24193
    
    Change-Id: I6a67bd912e3d9a4be0eae8fa8db6fa8a07fb5df3
    Reviewed-on: https://go-review.googlesource.com/99175
    Run-TryBot: Ian Lance Taylor <iant@golang.org>
    TryBot-Result: Gobot Gobot <gobot@golang.org>
    Reviewed-by: Brad Fitzpatrick <bradfitz@golang.org>

--- src/cmd/go/go_test.go
+++ src/cmd/go/go_test.go
@@ -3233,6 +3233,16 @@ func TestGoVetWithOnlyTestFiles(t *testing.T) {
 	tg.run("vet", "p")
 }
 
+// Issue 24193.
+func TestVetWithOnlyCgoFiles(t *testing.T) {
+	tg := testgo(t)
+	defer tg.cleanup()
+	tg.parallel()
+	tg.tempFile("src/p/p.go", "package p; import \"C\"; func F() {}")
+	tg.setenv("GOPATH", tg.path("."))
+	tg.run("vet", "p")
+}
+
 // Issue 9767, 19769.
 func TestGoGetDotSlashDownload(t *testing.T) {
 	testenv.MustHaveExternalNetwork(t)
