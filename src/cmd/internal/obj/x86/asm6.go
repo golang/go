762fb1b37bf2ea2bbbf60c83190ffbe5d9fb734f
Daniel Martí <mvdan@mvdan.cc>
2018-03-13 20:37:05 +0000

    cmd/asm: VPERMQ's imm8 arg is an uint8
    The imm8 argument consists of 4 2-bit indices, so it can take values up
    to $255. However, the assembler was treating it as Yi8, which reads
    "fits in int8". Add a Yu8 variant, to also keep backwards compatibility
    with negative values possible with Yi8.
    
    Fixes #24378.
    
    Change-Id: I24ddb19c219b54d039a6c1bcdb903717d1c7c3b8
    Reviewed-on: https://go-review.googlesource.com/100475
    Run-TryBot: Daniel Martí <mvdan@mvdan.cc>
    TryBot-Result: Gobot Gobot <gobot@golang.org>
    Reviewed-by: Keith Randall <khr@golang.org>

--- src/cmd/internal/obj/x86/asm6.go
+++ src/cmd/internal/obj/x86/asm6.go
@@ -952,6 +952,9 @@ var yvex_xi3 = []ytab{
 }
 
 var yvex_vpermpd = []ytab{
+	{Zvex_i_rm_r, 2, argList{Yu8, Yym, Yyr}},
+	// Allow int8 for backwards compatibility with negative values
+	// like $-1.
 	{Zvex_i_rm_r, 2, argList{Yi8, Yym, Yyr}},
 }
 
