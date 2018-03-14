cd3aae9b815389a5e2f05066beffdf9678534325
Alberto Donizetti <alb.donizetti@gmail.com>
2018-03-14 11:07:06 +0100

    test/codegen: port all small memmove tests to codegen
    This change ports all the remaining tests checking that small memmoves
    are replaced with MOVs to the new codegen test harness, and deletes
    them from the asm_test file.
    
    Change-Id: I01c94b441e27a5d61518035af62d62779dafeb56
    Reviewed-on: https://go-review.googlesource.com/100476
    Run-TryBot: Alberto Donizetti <alb.donizetti@gmail.com>
    TryBot-Result: Gobot Gobot <gobot@golang.org>
    Reviewed-by: Keith Randall <khr@golang.org>

--- src/cmd/compile/internal/gc/asm_test.go
+++ src/cmd/compile/internal/gc/asm_test.go
@@ -631,36 +631,6 @@ var linuxAMD64Tests = []*asmTest{
 		pos: []string{"\tANDQ\t\\$4095,"},
 	},
 	{
-		// Test that small memmove was replaced with direct movs
-		fn: `
-                func $() {
-                       x := [...]byte{1, 2, 3, 4, 5, 6, 7}
-                       copy(x[1:], x[:])
-                }
-		`,
-		neg: []string{"memmove"},
-	},
-	{
-		// Same as above but with different size
-		fn: `
-                func $() {
-                       x := [...]byte{1, 2, 3, 4}
-                       copy(x[1:], x[:])
-                }
-		`,
-		neg: []string{"memmove"},
-	},
-	{
-		// Same as above but with different size
-		fn: `
-                func $() {
-                       x := [...]byte{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16}
-                       copy(x[1:], x[:])
-                }
-		`,
-		neg: []string{"memmove"},
-	},
-	{
 		fn: `
 		func $(p int, q *int) bool {
 			return p < *q
@@ -764,26 +734,6 @@ var linux386Tests = []*asmTest{
 		}
 		`,
 		pos: []string{"\tANDL\t\\$4095,"},
-	},
-	{
-		// Test that small memmove was replaced with direct movs
-		fn: `
-                func $() {
-                       x := [...]byte{1, 2, 3, 4, 5, 6, 7}
-                       copy(x[1:], x[:])
-                }
-		`,
-		neg: []string{"memmove"},
-	},
-	{
-		// Same as above but with different size
-		fn: `
-                func $() {
-                       x := [...]byte{1, 2, 3, 4}
-                       copy(x[1:], x[:])
-                }
-		`,
-		neg: []string{"memmove"},
 	},
 }
 
