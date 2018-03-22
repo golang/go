ea629b4ea0d8b2d911211aefee9fd4dd2412c818
Matthew Dempsky <mdempsky@google.com>
2018-03-22 11:18:24 -0700

    cmd/compile: change unsafeUintptrTag from var to const
    Change-Id: Ie30878199e24cce5b75428e6b602c017ebd16642
    Reviewed-on: https://go-review.googlesource.com/102175
    Run-TryBot: Matthew Dempsky <mdempsky@google.com>
    Reviewed-by: Daniel Martí <mvdan@mvdan.cc>

--- src/cmd/compile/internal/gc/esc.go
+++ src/cmd/compile/internal/gc/esc.go
@@ -2168,9 +2168,7 @@ func moveToHeap(n *Node) {
 // This special tag is applied to uintptr variables
 // that we believe may hold unsafe.Pointers for
 // calls into assembly functions.
-// It is logically a constant, but using a var
-// lets us take the address below to get a *string.
-var unsafeUintptrTag = "unsafe-uintptr"
+const unsafeUintptrTag = "unsafe-uintptr"
 
 // This special tag is applied to uintptr parameters of functions
 // marked go:uintptrescapes.
