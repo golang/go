2222668fc2b5c465370314ae63977bfbb24d5c11
Matthew Dempsky <mdempsky@google.com>
2018-03-23 10:28:39 -0700

    cmd/link: skip __.PKGDEF in archives
    The __.PKGDEF file is a compiler object file only intended for other
    compilers. Also, for build systems that use -linkobj, all of the
    information it contains is present within the linker object files
    already, so look for it there instead.
    
    This requires a little bit of code reorganization. Significantly,
    previously when loading an archive file, the __.PKGDEF file was
    authoritative on whether the package was "main" and/or "safe". Now
    that we're using the Go object files instead, there's the issue that
    there can be multiple Go object files in an archive (because when
    using assembly, each assembly file becomes its own additional object
    file).
    
    The solution taken here is to check if any object file within the
    package declares itself as "main" and/or "safe".
    
    Updates #24512.
    
    Change-Id: I70243a293bdf34b8555c0bf1833f8933b2809449
    Reviewed-on: https://go-review.googlesource.com/102281
    Run-TryBot: Matthew Dempsky <mdempsky@google.com>
    TryBot-Result: Gobot Gobot <gobot@golang.org>
    Reviewed-by: Ian Lance Taylor <iant@golang.org>

--- src/cmd/link/internal/sym/library.go
+++ src/cmd/link/internal/sym/library.go
@@ -15,6 +15,8 @@ type Library struct {
 	Imports       []*Library
 	Textp         []*Symbol // text symbols defined in this library
 	DupTextSyms   []*Symbol // dupok text symbols defined in this library
+	Main          bool
+	Safe          bool
 }
 
 func (l Library) String() string {
