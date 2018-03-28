9cdedd9cb78f14a0fb49f3cb4871070323bf9ffb
Ian Lance Taylor <iant@golang.org>
2018-03-28 07:44:06 -0700

    flag: document use of FlagSet name parameter
    Fixes #24225
    
    Change-Id: I876ac1b9d3615283f7b88cfa0b965ef81a57f056
    Reviewed-on: https://go-review.googlesource.com/102955
    Reviewed-by: Rob Pike <r@golang.org>

--- src/flag/flag.go
+++ src/flag/flag.go
@@ -981,7 +981,8 @@ func commandLineUsage() {
 }
 
 // NewFlagSet returns a new, empty flag set with the specified name and
-// error handling property.
+// error handling property. If the name is not empty, it will be printed
+// in the default usage message and in error messages.
 func NewFlagSet(name string, errorHandling ErrorHandling) *FlagSet {
 	f := &FlagSet{
 		name:          name,
