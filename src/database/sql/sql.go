7a7b63f3e749e3608ee16b1807349a778d562c08
Daniel Theophanes <kardianos@gmail.com>
2018-03-29 10:01:27 -0700

    database/sql: clarify behavior of DB.Close
    It wasn't clear for existing docs if DB.Close forcefully closed
    connections or waited for them to finish.
    
    Fixes #23753
    
    Change-Id: Id7df31224c93181c8d01bab7b0b23da25b42a288
    Reviewed-on: https://go-review.googlesource.com/103397
    Reviewed-by: Brad Fitzpatrick <bradfitz@golang.org>

--- src/database/sql/sql.go
+++ src/database/sql/sql.go
@@ -738,7 +738,9 @@ func (db *DB) Ping() error {
 	return db.PingContext(context.Background())
 }
 
-// Close closes the database, releasing any open resources.
+// Close closes the database and prevents new queries from starting.
+// Close then waits for all queries that have started processing on the server
+// to finish.
 //
 // It is rare to Close a DB, as the DB handle is meant to be
 // long-lived and shared between many goroutines.
