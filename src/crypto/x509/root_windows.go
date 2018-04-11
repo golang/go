8f737798543639f821a1abd2077f91f333da3516
Alex Brainman <alex.brainman@gmail.com>
2018-04-11 19:43:39 +1000

    syscall: introduce Pointer type and use it instead of uintptr
    Some syscall structures used by crypto/x509 have uintptr
    fields that store pointers. These pointers are set with
    a pointer to another Go structure. But the pointers are
    not visible by garbage collector, and GC does not update
    the fields after they were set. So when structure with
    invalid uintptr pointers passed to Windows, we get
    memory corruption.
    
    This CL introduces CertInfo, CertTrustListInfo and
    CertRevocationCrlInfo types. It uses pointers to new types
    instead of uintptr in CertContext, CertSimpleChain and
    CertRevocationInfo.
    
    CertRevocationInfo, CertChainPolicyPara and
    CertChainPolicyStatus types have uintptr field that can
    be pointer to many different things (according to Windows
    API). So this CL introduces Pointer type to be used for
    those cases.
    
    As suggested by Austin Clements.
    
    Fixes #21376
    Updates #24820
    
    Change-Id: If95cd9eee3c69e4cfc35b7b25b1b40c2dc8f0df7
    Reviewed-on: https://go-review.googlesource.com/106275
    Reviewed-by: Austin Clements <austin@google.com>
    Run-TryBot: Austin Clements <austin@google.com>
    TryBot-Result: Gobot Gobot <gobot@golang.org>

--- src/crypto/x509/root_windows.go
+++ src/crypto/x509/root_windows.go
@@ -109,7 +109,7 @@ func checkChainSSLServerPolicy(c *Certificate, chainCtx *syscall.CertChainContex
 	sslPara.Size = uint32(unsafe.Sizeof(*sslPara))
 
 	para := &syscall.CertChainPolicyPara{
-		ExtraPolicyPara: uintptr(unsafe.Pointer(sslPara)),
+		ExtraPolicyPara: (syscall.Pointer)(unsafe.Pointer(sslPara)),
 	}
 	para.Size = uint32(unsafe.Sizeof(*para))
 
