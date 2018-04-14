70c5839fe0e2149d505c0e28c42e133d4bc01503
quasilyte <quasilyte@gmail.com>
2018-04-14 12:17:09 +0300

    cmd/internal/obj/x86: forbid mem args for MOV_DR and MOV_CR
    Memory arguments for debug/control register moves are a
    minefield for programmer: not useful, but can lead to errors.
    
    See referenced issue for detailed explanation.
    
    Fixes #24981
    
    Change-Id: I918e81cd4a8b1dfcfc9023cdfc3de45abe29e749
    Reviewed-on: https://go-review.googlesource.com/107075
    Run-TryBot: Iskander Sharipov <iskander.sharipov@intel.com>
    TryBot-Result: Gobot Gobot <gobot@golang.org>
    Reviewed-by: Cherry Zhang <cherryyz@google.com>

--- src/cmd/asm/internal/asm/testdata/386enc.s
+++ src/cmd/asm/internal/asm/testdata/386enc.s
@@ -18,5 +18,15 @@ TEXT asmtest(SB),DUPOK|NOSPLIT,$0
 	MOVL -2147483648(AX), AX // 8b8000000080
 	ADDL 2147483648(AX), AX  // 038000000080
 	ADDL -2147483648(AX), AX // 038000000080
+	// Make sure MOV CR/DR continues to work after changing it's movtabs.
+	MOVL CR0, AX // 0f20c0
+	MOVL CR0, DX // 0f20c2
+	MOVL CR4, DI // 0f20e7
+	MOVL AX, CR0 // 0f22c0
+	MOVL DX, CR0 // 0f22c2
+	MOVL DI, CR4 // 0f22e7
+	MOVL DR0, AX // 0f21c0
+	MOVL DR6, DX // 0f21f2
+	MOVL DR7, SI // 0f21fe
 	// End of tests.
 	RET
