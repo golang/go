a6008afe0a243f24a7efc85f8240aef4e051f8a9
quasilyte <quasilyte@gmail.com>
2018-04-14 12:17:09 +0300

    cmd/internal/obj/x86: forbid mem args for MOV_DR and MOV_CR
    Memory arguments for debug/control register moves are a
    minefield for programmer: not useful, but can lead to errors.
    
    See referenced issue for detailed explanation.
    
    Fixes #24981
    
    Change-Id: I918e81cd4a8b1dfcfc9023cdfc3de45abe29e749
    Reviewed-on: https://go-review.googlesource.com/107075
    Run-TryBot: Iskander Sharipov <iskander.sharipov@intel.com>
    TryBot-Result: Gobot Gobot <gobot@golang.org>
    Reviewed-by: Cherry Zhang <cherryyz@google.com>

--- src/cmd/asm/internal/asm/testdata/amd64enc_extra.s
+++ src/cmd/asm/internal/asm/testdata/amd64enc_extra.s
@@ -299,5 +299,15 @@ TEXT asmtest(SB),DUPOK|NOSPLIT,$0
 	// Check that LEAL is permitted to use overflowing offset.
 	LEAL 2400959708(BP)(R10*1), BP // 428dac15dcbc1b8f
 	LEAL 3395469782(AX)(R10*1), AX // 428d8410d6c162ca
+	// Make sure MOV CR/DR continues to work after changing it's movtabs.
+	MOVQ CR0, AX // 0f20c0
+	MOVQ CR0, DX // 0f20c2
+	MOVQ CR4, DI // 0f20e7
+	MOVQ AX, CR0 // 0f22c0
+	MOVQ DX, CR0 // 0f22c2
+	MOVQ DI, CR4 // 0f22e7
+	MOVQ DR0, AX // 0f21c0
+	MOVQ DR6, DX // 0f21f2
+	MOVQ DR7, SI // 0f21fe
 	// End of tests.
 	RET
