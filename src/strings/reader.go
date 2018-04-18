e4f5f72b0397aa96cc968334c2a22eda6566c511
maiyang <yangwen.yw@gmail.com>
2018-04-18 21:33:03 +0800

    Update reader.go

--- src/strings/reader.go
+++ src/strings/reader.go
@@ -44,15 +44,15 @@ func (r *Reader) Read(b []byte) (n int, err error) {
 	return
 }
 
-func (r *Reader) ReadAt(b []byte, off int64) (n int, err error) {
+func (r *Reader) ReadAt(b []byte, offset int64) (n int, err error) {
 	// cannot modify state - see io.ReaderAt
-	if off < 0 {
+	if offset < 0 {
 		return 0, errors.New("strings.Reader.ReadAt: negative offset")
 	}
-	if off >= int64(len(r.s)) {
+	if offset >= int64(len(r.s)) {
 		return 0, io.EOF
 	}
-	n = copy(b, r.s[off:])
+	n = copy(b, r.s[offset:])
 	if n < len(b) {
 		err = io.EOF
 	}
