35778aa4972e5c005877fe77970183281fab1d44
Russ Cox <rsc@golang.org>
2018-04-19 10:54:12 -0400

    cmd/go: final edits for add go list -cgo and -export
    These got lost because I violated the cardinal rule of Gerrit
    which is never click the Submit button in the web UI.
    
    Change-Id: I8ccdfb5d8691960f244941036d33fb5a5a3f0c9a
    Reviewed-on: https://go-review.googlesource.com/117015
    Reviewed-by: Russ Cox <rsc@golang.org>

--- src/cmd/go/internal/list/list.go
+++ src/cmd/go/internal/list/list.go
@@ -23,7 +23,7 @@ import (
 )
 
 var CmdList = &base.Command{
-	UsageLine: "list [-cgo] [-deps] [-e] [-f format] [-json] [-list] [-test] [build flags] [packages]",
+	UsageLine: "list [-cgo] [-deps] [-e] [-export] [-f format] [-json] [-list] [-test] [build flags] [packages]",
 	Short:     "list packages",
 	Long: `
 List lists the packages named by the import paths, one per line.
@@ -151,9 +151,8 @@ printing. Erroneous packages will have a non-empty ImportPath and
 a non-nil Error field; other information may or may not be missing
 (zeroed).
 
-The -export flag causes list to set the package's Export field to
-the name of a file containing up-to-date export information for
-the given package.
+The -export flag causes list to set the Export field to the name of a
+file containing up-to-date export information for the given package.
 
 The -test flag causes list to report not only the named packages
 but also their test binaries (for packages with tests), to convey to
