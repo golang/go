f2df0ec7dd0552af9a7ee4ab1bf77c302f08bd57
Russ Cox <rsc@golang.org>
2018-04-18 21:26:55 -0400

    cmd/go: avoid infinite loop in go list -json -e on import cycle
    Don't chase import cycles forever preparing list JSON.
    
    Fixes #24086.
    
    Change-Id: Ia1139d0c8d813d068c367a8baee59d240a545617
    Reviewed-on: https://go-review.googlesource.com/108016
    Run-TryBot: Russ Cox <rsc@golang.org>
    TryBot-Result: Gobot Gobot <gobot@golang.org>
    Reviewed-by: Bryan C. Mills <bcmills@google.com>

--- src/cmd/go/go_test.go
+++ src/cmd/go/go_test.go
@@ -1373,6 +1373,9 @@ func TestImportCycle(t *testing.T) {
 	if count > 1 {
 		t.Fatal("go build mentioned import cycle more than once")
 	}
+
+	// Don't hang forever.
+	tg.run("list", "-e", "-json", "selfimport")
 }
 
 // cmd/go: custom import path checking should not apply to Go packages without import comment.
