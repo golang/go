bd04d6086df521c0cf9ae8375afa8b81ded62688
Adam Jones <adam@modsrus.com>
2018-04-19 23:18:40 -0400

    Fixed Go

--- src/cmd/go/internal/version/version.go
+++ src/cmd/go/internal/version/version.go
@@ -14,7 +14,7 @@ import (
 
 var CmdVersion = &base.Command{
 	Run:       runVersion,
-	UsageLine: "version",
+	UsageLine: "-v",
 	Short:     "print Go version",
 	Long:      `Version prints the Go version, as reported by runtime.Version.`,
 }
