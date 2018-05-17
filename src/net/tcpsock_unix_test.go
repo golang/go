168e3f9b06759e80f37c2d446bb3bdaba2872436
Elias Naur <elias.naur@gmail.com>
2018-05-17 15:20:57 +0200

    net: skip external net tests on iOS
    CL 113095 tried to deflake net tests on iOS by skipping the test
    that uses the most sockets. That didn't work well enough and will
    be reverted in CL 113555.
    
    The flakes appeared after the iOS exec harness started to forward
    environment variables, causing testenv.Builder to be non-empty on
    the iOS builder. This CL attempts to fix the flakes with the more
    conservative strategy of skipping tests that only run on builders.
    
    The skipped tests happen to be those requiring external network
    access; it's plausible that the iOS builder network isn't reliable
    enough to run the many parallel DNS lookups and dial outs, while
    keeping the number of open file descriptors below the 250 limit.
    
    Change-Id: I9cafdaf2845dd6f3844c4819dcaaaa5970f5da15
    Reviewed-on: https://go-review.googlesource.com/113575
    Run-TryBot: Elias Naur <elias.naur@gmail.com>
    TryBot-Result: Gobot Gobot <gobot@golang.org>
    Reviewed-by: Brad Fitzpatrick <bradfitz@golang.org>

--- src/net/tcpsock_unix_test.go
+++ src/net/tcpsock_unix_test.go
@@ -8,7 +8,6 @@ package net
 
 import (
 	"context"
-	"internal/testenv"
 	"math/rand"
 	"runtime"
 	"sync"
@@ -84,9 +83,8 @@ func TestTCPSpuriousConnSetupCompletion(t *testing.T) {
 // Issue 19289.
 // Test that a canceled Dial does not cause a subsequent Dial to succeed.
 func TestTCPSpuriousConnSetupCompletionWithCancel(t *testing.T) {
-	if testenv.Builder() == "" {
-		testenv.MustHaveExternalNetwork(t)
-	}
+	mustHaveExternalNetwork(t)
+
 	defer dnsWaitGroup.Wait()
 	t.Parallel()
 	const tries = 10000
