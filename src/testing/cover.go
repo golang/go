f5b9e1d49d1c00f59ce4d3684915e5e93ec0297a
Stephen L <36011612+steuhs@users.noreply.github.com>
2018-06-11 15:09:55 -0700

    Update cover.go

--- src/testing/cover.go
+++ src/testing/cover.go
@@ -19,7 +19,7 @@ type CoverBlock struct {
 	Line0 uint32 // line number for block start
 	Col0  uint16 // column number for block start
 	Line1 uint32 // line number for block end
-	Col1  uint16 // column number for block end 
+	Col1  uint16 // column number for block end
 	Stmts uint16 // number of statements included in this block
 }
 
