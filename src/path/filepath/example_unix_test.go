940811922fb528fabda91a2b2dbb401a06aeb1b3
Mostyn Bramley-Moore <mostyn@antipode.se>
2018-07-04 23:32:59 +0200

    path/filepath: make Walk example runnable in the playground
    Relates to #9679
    
    Change-Id: I68951f664d2a03812dae309c580c181869d8af21
    Reviewed-on: https://go-review.googlesource.com/122237
    Run-TryBot: Ian Lance Taylor <iant@golang.org>
    TryBot-Result: Gobot Gobot <gobot@golang.org>
    Reviewed-by: Ian Lance Taylor <iant@golang.org>

--- src/path/filepath/example_unix_test.go
+++ src/path/filepath/example_unix_test.go
@@ -8,7 +8,6 @@ package filepath_test
 
 import (
 	"fmt"
-	"os"
 	"path/filepath"
 )
 
@@ -79,26 +78,4 @@ func ExampleJoin() {
 	// a/b/c
 	// a/b/c
 	// a/b/c
-}
-
-func ExampleWalk() {
-	dir := "dir/to/walk"
-	subDirToSkip := "skip" // dir/to/walk/skip
-
-	err := filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
-		if err != nil {
-			fmt.Printf("prevent panic by handling failure accessing a path %q: %v\n", path, err)
-			return err
-		}
-		if info.IsDir() && info.Name() == subDirToSkip {
-			fmt.Printf("skipping a dir without errors: %+v \n", info.Name())
-			return filepath.SkipDir
-		}
-		fmt.Printf("visited file: %q\n", path)
-		return nil
-	})
-
-	if err != nil {
-		fmt.Printf("error walking the path %q: %v\n", dir, err)
-	}
 }
