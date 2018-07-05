efea01788d5d8b14426e31698ca30f27365c0670
Dan Kortschak <dan@kortschak.io>
2018-07-06 09:14:30 +0930

    testshared/src/depBase: conform build tag comment to convention
    Also add missing copyright headers with year determined from git log.
    
    Change-Id: Iafc9881e746543f0a582dad2b0874d8399baf618
    Reviewed-on: https://go-review.googlesource.com/122415
    Reviewed-by: Rob Pike <r@golang.org>
    Reviewed-by: Brad Fitzpatrick <bradfitz@golang.org>
    Run-TryBot: Rob Pike <r@golang.org>
    TryBot-Result: Gobot Gobot <gobot@golang.org>

--- misc/cgo/testshared/src/depBase/stubs.go
+++ misc/cgo/testshared/src/depBase/stubs.go
@@ -1,4 +1,8 @@
-//+build !gccgo
+// Copyright 2016 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// +build !gccgo
 
 package depBase
 
