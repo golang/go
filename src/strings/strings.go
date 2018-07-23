8597ad4e4f4b33a9cfd3b62c73865fa11a9bb0d6
Zhang Qiang <dotslash.lu@gmail.com>
2018-07-23 15:33:43 +0800

    strings: remove unused var primeRK redefinition

--- src/strings/strings.go
+++ src/strings/strings.go
@@ -46,7 +46,7 @@ func hashStr(sep string) (uint32, uint32) {
 	for i := 0; i < len(sep); i++ {
 		hash = hash*primeRK + uint32(sep[i])
 	}
-	var pow, sq uint32 = 1, primeRK
+	var pow, sq uint32 = 1
 	for i := len(sep); i > 0; i >>= 1 {
 		if i&1 != 0 {
 			pow *= sq
