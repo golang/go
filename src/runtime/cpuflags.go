c15c04d9e85a6a2c46ae57cb830192e0eee276dc
Martin Möhrmann <moehrmann@google.com>
2018-07-28 10:56:48 +0200

    runtime: use internal/cpu variables in assembler code
    Using internal/cpu variables has the benefit of avoiding false sharing
    (as those are padded) and allows memory and cache usage for these variables
    to be shared by multiple packages.
    
    Change-Id: I2bf68d03091bf52b466cf689230d5d25d5950037
    Reviewed-on: https://go-review.googlesource.com/126599
    Run-TryBot: Martin Möhrmann <moehrmann@google.com>
    TryBot-Result: Gobot Gobot <gobot@golang.org>
    Reviewed-by: Keith Randall <khr@golang.org>

---
+++ src/runtime/cpuflags.go
@@ -0,0 +1,17 @@
+// Copyright 2018 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package runtime
+
+import (
+	"internal/cpu"
+	"unsafe"
+)
+
+// Offsets into internal/cpu records for use in assembly.
+const (
+	offset_x86_HasAVX2 = unsafe.Offsetof(cpu.X86.HasAVX2)
+	offset_x86_HasERMS = unsafe.Offsetof(cpu.X86.HasERMS)
+	offset_x86_HasSSE2 = unsafe.Offsetof(cpu.X86.HasSSE2)
+)
