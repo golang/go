6ffeee342474182ddaba7a39a672a05bcf783b67
Martin Möhrmann <moehrmann@google.com>
2018-07-30 22:37:50 +0200

    internal/cpu: remove parentheses from arm64 feature constants
    The parentheses are not required for the definitions and it brings
    the declaration style in line with other architectures feature bits
    defined in internal/cpu.
    
    Change-Id: I86cc3812c1488216779e0d1f0e7481687502e592
    Reviewed-on: https://go-review.googlesource.com/126775
    Run-TryBot: Martin Möhrmann <moehrmann@google.com>
    TryBot-Result: Gobot Gobot <gobot@golang.org>
    Reviewed-by: Cherry Zhang <cherryyz@google.com>

--- src/internal/cpu/cpu_arm64.go
+++ src/internal/cpu/cpu_arm64.go
@@ -14,30 +14,30 @@ var hwcap2 uint
 
 // HWCAP/HWCAP2 bits. These are exposed by Linux.
 const (
-	hwcap_FP       = (1 << 0)
-	hwcap_ASIMD    = (1 << 1)
-	hwcap_EVTSTRM  = (1 << 2)
-	hwcap_AES      = (1 << 3)
-	hwcap_PMULL    = (1 << 4)
-	hwcap_SHA1     = (1 << 5)
-	hwcap_SHA2     = (1 << 6)
-	hwcap_CRC32    = (1 << 7)
-	hwcap_ATOMICS  = (1 << 8)
-	hwcap_FPHP     = (1 << 9)
-	hwcap_ASIMDHP  = (1 << 10)
-	hwcap_CPUID    = (1 << 11)
-	hwcap_ASIMDRDM = (1 << 12)
-	hwcap_JSCVT    = (1 << 13)
-	hwcap_FCMA     = (1 << 14)
-	hwcap_LRCPC    = (1 << 15)
-	hwcap_DCPOP    = (1 << 16)
-	hwcap_SHA3     = (1 << 17)
-	hwcap_SM3      = (1 << 18)
-	hwcap_SM4      = (1 << 19)
-	hwcap_ASIMDDP  = (1 << 20)
-	hwcap_SHA512   = (1 << 21)
-	hwcap_SVE      = (1 << 22)
-	hwcap_ASIMDFHM = (1 << 23)
+	hwcap_FP       = 1 << 0
+	hwcap_ASIMD    = 1 << 1
+	hwcap_EVTSTRM  = 1 << 2
+	hwcap_AES      = 1 << 3
+	hwcap_PMULL    = 1 << 4
+	hwcap_SHA1     = 1 << 5
+	hwcap_SHA2     = 1 << 6
+	hwcap_CRC32    = 1 << 7
+	hwcap_ATOMICS  = 1 << 8
+	hwcap_FPHP     = 1 << 9
+	hwcap_ASIMDHP  = 1 << 10
+	hwcap_CPUID    = 1 << 11
+	hwcap_ASIMDRDM = 1 << 12
+	hwcap_JSCVT    = 1 << 13
+	hwcap_FCMA     = 1 << 14
+	hwcap_LRCPC    = 1 << 15
+	hwcap_DCPOP    = 1 << 16
+	hwcap_SHA3     = 1 << 17
+	hwcap_SM3      = 1 << 18
+	hwcap_SM4      = 1 << 19
+	hwcap_ASIMDDP  = 1 << 20
+	hwcap_SHA512   = 1 << 21
+	hwcap_SVE      = 1 << 22
+	hwcap_ASIMDFHM = 1 << 23
 )
 
 func doinit() {
