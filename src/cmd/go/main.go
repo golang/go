b1049d79e9dacadde92e669fb41e6c14707e1df2
Eric Ponce <tricokun@gmail.com>
2018-08-30 02:27:35 +0200

    cmd/go: display correct options in "go help get" using modules
    Fixes: #27298
    
    Change-Id: Icfc6992b470136bb25a77912f670a25883642316
    Reviewed-on: https://go-review.googlesource.com/132095
    Reviewed-by: Bryan C. Mills <bcmills@google.com>
    Run-TryBot: Bryan C. Mills <bcmills@google.com>
    TryBot-Result: Gobot Gobot <gobot@golang.org>

--- src/cmd/go/main.go
+++ src/cmd/go/main.go
@@ -93,6 +93,15 @@ func main() {
 		*get.CmdGet = *modget.CmdGet
 	}
 
+	if args[0] == "get" || args[0] == "help" {
+		// Replace get with module-aware get if appropriate.
+		// Note that if MustUseModules is true, this happened already above,
+		// but no harm in doing it again.
+		if modload.Init(); modload.Enabled() {
+			*get.CmdGet = *modget.CmdGet
+		}
+	}
+
 	cfg.CmdName = args[0] // for error messages
 	if args[0] == "help" {
 		help.Help(os.Stdout, args[1:])
@@ -159,15 +168,6 @@ func main() {
 	case "verify":
 		fmt.Fprintf(os.Stderr, "go: vgo verify is now go mod verify\n")
 		os.Exit(2)
-	}
-
-	if args[0] == "get" {
-		// Replace get with module-aware get if appropriate.
-		// Note that if MustUseModules is true, this happened already above,
-		// but no harm in doing it again.
-		if modload.Init(); modload.Enabled() {
-			*get.CmdGet = *modget.CmdGet
-		}
 	}
 
 	// Set environment (GOOS, GOARCH, etc) explicitly.
