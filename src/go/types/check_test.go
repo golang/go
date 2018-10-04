8ae8576abf483cf928eb0f63bc55c09c7bf73576
Robert Griesemer <gri@golang.org>
2018-10-04 16:20:48 -0700

    go/types: don't type-check method signatures eagerly anymore
    As a side-effect we also get slightly clearer errors for some
    pathological cyclic method declarations.
    
    Fixes #23203.
    Updates #26854.
    
    Change-Id: I30bd6634ac6be26d3f4ef8c7b32e5c1bf76987dd
    Reviewed-on: https://go-review.googlesource.com/c/139897
    Reviewed-by: Alan Donovan <adonovan@google.com>

--- src/go/types/check_test.go
+++ src/go/types/check_test.go
@@ -92,6 +92,8 @@ var tests = [][]string{
 	{"testdata/blank.src"},
 	{"testdata/issue25008b.src", "testdata/issue25008a.src"}, // order (b before a) is crucial!
 	{"testdata/issue26390.src"},                              // stand-alone test to ensure case is triggered
+	{"testdata/issue23203a.src"},
+	{"testdata/issue23203b.src"},
 }
 
 var fset = token.NewFileSet()
