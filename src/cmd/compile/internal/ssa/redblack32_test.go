3a93b74330bcab0f64f600a2961824c6e7bb05d1
OlgaVlPetrova <OVPpetrova@gmail.com>
2018-10-13 14:38:21 +0300

    cmd/compile/internal/ssa: simplify `s = s <op> x` to `s <op>= x`
    Change-Id: I366b89e35d194ca2a7eb97a8253497bd3fc2af94
    Reviewed-on: https://go-review.googlesource.com/c/go/+/142019
    Run-TryBot: Ian Lance Taylor <iant@golang.org>
    TryBot-Result: Gobot Gobot <gobot@golang.org>
    Reviewed-by: Keith Randall <khr@golang.org>

--- src/cmd/compile/internal/ssa/redblack32_test.go
+++ src/cmd/compile/internal/ssa/redblack32_test.go
@@ -134,15 +134,15 @@ func (t *RBTint32) DebugString() string {
 func (t *node32) DebugString() string {
 	s := ""
 	if t.left != nil {
-		s = s + "["
-		s = s + t.left.DebugString()
-		s = s + "]"
+		s += "["
+		s += t.left.DebugString()
+		s += "]"
 	}
-	s = s + fmt.Sprintf("%v=%v:%d", t.key, t.data, t.rank)
+	s += fmt.Sprintf("%v=%v:%d", t.key, t.data, t.rank)
 	if t.right != nil {
-		s = s + "["
-		s = s + t.right.DebugString()
-		s = s + "]"
+		s += "["
+		s += t.right.DebugString()
+		s += "]"
 	}
 	return s
 }
