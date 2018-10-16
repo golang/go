e94054f4104a5f41efd6edfd762152ddcb3acd70
Alan Donovan <adonovan@google.com>
2018-10-16 16:22:17 -0400

    go/analysis/passes/composite: add testing.Internal* to whitelist
    These structs appear in test main files generated by "go test"
    and so can be trusted.
    
    Change-Id: I1514a8cdcbd633392ccaaedfa8eccf944d514129
    Reviewed-on: https://go-review.googlesource.com/c/142699
    Reviewed-by: Michael Matloob <matloob@golang.org>
    Run-TryBot: Michael Matloob <matloob@golang.org>

--- go/analysis/passes/composite/whitelist.go
+++ go/analysis/passes/composite/whitelist.go
@@ -24,4 +24,10 @@ var unkeyedLiteral = map[string]bool{
 	"image.Uniform":       true,
 
 	"unicode.Range16": true,
+
+	// These three structs are used in generated test main files,
+	// but the generator can be trusted.
+	"testing.InternalBenchmark": true,
+	"testing.InternalExample":   true,
+	"testing.InternalTest":      true,
 }
