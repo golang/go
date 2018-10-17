7099c87f61d54175d5b3a8ac30dbb386eb68fce4
Robert Griesemer <gri@golang.org>
2018-10-17 13:12:44 -0700

    go/internal/gccgoimporter: add missing copyright notice
    Change-Id: I20f7aba8a76cfe8549771ef96244e8641a4157e6
    Reviewed-on: https://go-review.googlesource.com/c/142891
    Reviewed-by: Ian Lance Taylor <iant@golang.org>

--- go/internal/gccgoimporter/backdoor.go
+++ go/internal/gccgoimporter/backdoor.go
@@ -1,3 +1,7 @@
+// Copyright 2018 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 package gccgoimporter
 
 // This file opens a back door to the parser for golang.org/x/tools/go/gccgoexportdata.
