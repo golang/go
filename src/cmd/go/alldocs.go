0b071e40195c7f202933d3b3ec3da3b4f40cd472
diplozoon <huyuumi.dev@gmail.com>
2018-10-26 05:03:15 +0000

    cmd/go: add /v2 to go.mod require example
    I added /v2 to go.mod require example
    
    Fixes #28374
    
    Change-Id: I74cca374838d106eb79acb9189a02fe9443962c0
    Reviewed-on: https://go-review.googlesource.com/c/144917
    Reviewed-by: Bryan C. Mills <bcmills@google.com>
    Run-TryBot: Bryan C. Mills <bcmills@google.com>
    TryBot-Result: Gobot Gobot <gobot@golang.org>

--- src/cmd/go/alldocs.go
+++ src/cmd/go/alldocs.go
@@ -1605,7 +1605,7 @@
 //
 // 	module my/thing
 // 	require other/thing v1.0.2
-// 	require new/thing v2.3.4
+// 	require new/thing/v2 v2.3.4
 // 	exclude old/thing v1.2.3
 // 	replace bad/thing v1.4.5 => good/thing v1.4.5
 //
