d51e88b5aec5e5af4b3c8a4083777f5d1623d846
Ian Cottrell <iancottrell@google.com>
2018-10-31 14:51:16 -0400

    go/packages: make visit order stable
    Change-Id: Iee9b29364dd986e1f1676ff0aa989267c4149c30
    Reviewed-on: https://go-review.googlesource.com/c/146357
    Run-TryBot: Ian Cottrell <iancottrell@google.com>
    Run-TryBot: Michael Matloob <matloob@golang.org>
    TryBot-Result: Gobot Gobot <gobot@golang.org>
    Reviewed-by: Michael Matloob <matloob@golang.org>

--- go/packages/visit.go
+++ go/packages/visit.go
@@ -3,6 +3,7 @@ package packages
 import (
 	"fmt"
 	"os"
+	"sort"
 )
 
 // Visit visits all the packages in the import graph whose roots are
@@ -23,6 +24,7 @@ func Visit(pkgs []*Package, pre func(*Package) bool, post func(*Package)) {
 				for path := range pkg.Imports {
 					paths = append(paths, path)
 				}
+				sort.Strings(paths) // Imports is a map, this makes visit stable
 				for _, path := range paths {
 					visit(pkg.Imports[path])
 				}
