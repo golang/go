cbbb83ee06bfa3ef9913dbd79c04bdbc6e664349
Tobias Klauser <tklauser@distanz.ch>
2018-11-05 12:39:11 +0100

    runtime/internal/sys: regenerate zgoos_*.go files
    zgoos_aix.go is missing GoosJs, the order of GoosAndroid and GoosAix is
    mixed up in all files and GoosHurd was added after CL 146023 introduced
    GOOS=hurd.
    
    Change-Id: I7e2f5a15645272e9020cfca86e44c364fc072a2b
    Reviewed-on: https://go-review.googlesource.com/c/147397
    Run-TryBot: Tobias Klauser <tobias.klauser@gmail.com>
    TryBot-Result: Gobot Gobot <gobot@golang.org>
    Reviewed-by: Ian Lance Taylor <iant@golang.org>

--- src/runtime/internal/sys/zgoos_js.go
+++ src/runtime/internal/sys/zgoos_js.go
@@ -6,10 +6,12 @@ package sys
 
 const GOOS = `js`
 
+const GoosAix = 0
 const GoosAndroid = 0
 const GoosDarwin = 0
 const GoosDragonfly = 0
 const GoosFreebsd = 0
+const GoosHurd = 0
 const GoosJs = 1
 const GoosLinux = 0
 const GoosNacl = 0
