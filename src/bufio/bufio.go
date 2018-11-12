de50ea3cd848f187766825402f8d6c8109536443
Russ Cox <rsc@golang.org>
2018-11-12 11:36:12 -0500

    bufio: put notes about len(p) together
    CL 145577 added the part about io.ReadFull to read len(p)
    but it should be next to the existing sentence about not
    reading len(p) bytes.
    
    Change-Id: Idfa037c59a3085d44d5da6129188473db0e96d23
    Reviewed-on: https://go-review.googlesource.com/c/148903
    Run-TryBot: Russ Cox <rsc@golang.org>
    TryBot-Result: Gobot Gobot <gobot@golang.org>
    Reviewed-by: Brad Fitzpatrick <bradfitz@golang.org>
    Reviewed-by: Alan Donovan <adonovan@google.com>

--- src/bufio/bufio.go
+++ src/bufio/bufio.go
@@ -186,9 +186,8 @@ func (b *Reader) Discard(n int) (discarded int, err error) {
 // It returns the number of bytes read into p.
 // The bytes are taken from at most one Read on the underlying Reader,
 // hence n may be less than len(p).
-// At EOF, the count will be zero and err will be io.EOF.
-//
 // To read exactly len(p) bytes, use io.ReadFull(b, p).
+// At EOF, the count will be zero and err will be io.EOF.
 func (b *Reader) Read(p []byte) (n int, err error) {
 	n = len(p)
 	if n == 0 {
