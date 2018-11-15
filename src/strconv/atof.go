307b5295669e25b661c4e1a3938bf30949b77eec
Sandy <openset.wang@gmail.com>
2018-11-15 11:41:16 +0800

    strconv: fix a typo mistake in comments

--- src/strconv/atof.go
+++ src/strconv/atof.go
@@ -7,7 +7,7 @@ package strconv
 // decimal to binary floating point conversion.
 // Algorithm:
 //   1) Store input in multiprecision decimal.
-//   2) Multiply/divide decimal by powers of two until in range [0.5, 1)
+//   2) Multiply/divide decimal by powers of two until in range [0.5, 1]
 //   3) Multiply by 2^precision and round to get mantissa.
 
 import "math"
