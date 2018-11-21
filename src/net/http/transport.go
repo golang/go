4a8ce14af9941e3efbeb88b564bde76cc0b2e121
haormj <haormj@gmail.com>
2018-11-21 12:18:56 +0000

    net/http: fix spelling mistake in a comment
    Fixes #28904
    
    Change-Id: I8d416c47479a266735a39c926fd2f0f2bb25d57b
    GitHub-Last-Rev: 3a7865a5be27937833cf4f65c242c639e51665c4
    GitHub-Pull-Request: golang/go#28907
    Reviewed-on: https://go-review.googlesource.com/c/150737
    Reviewed-by: Daniel Martí <mvdan@mvdan.cc>
    Run-TryBot: Daniel Martí <mvdan@mvdan.cc>
    TryBot-Result: Gobot Gobot <gobot@golang.org>

--- src/net/http/transport.go
+++ src/net/http/transport.go
@@ -2416,7 +2416,7 @@ type fakeLocker struct{}
 func (fakeLocker) Lock()   {}
 func (fakeLocker) Unlock() {}
 
-// clneTLSConfig returns a shallow clone of cfg, or a new zero tls.Config if
+// cloneTLSConfig returns a shallow clone of cfg, or a new zero tls.Config if
 // cfg is nil. This is safe to call even if cfg is in active use by a TLS
 // client or server.
 func cloneTLSConfig(cfg *tls.Config) *tls.Config {
