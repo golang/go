d8a6d0f5febb8c9e70ab3d12aca0f803dd3e514d
Gergely Brautigam <skarlso777@gmail.com>
2018-11-27 06:33:20 +0100

    cmd/compile: suppress typecheck errors in a type switch case with broken type

--- src/cmd/compile/internal/gc/swt.go
+++ src/cmd/compile/internal/gc/swt.go
@@ -110,6 +110,7 @@ func typecheckswitch(n *Node) {
 
 	var def, niltype *Node
 	for _, ncase := range n.List.Slice() {
+		caseTypeBroken := false
 		if ncase.List.Len() == 0 {
 			// default
 			if def != nil {
@@ -125,6 +126,7 @@ func typecheckswitch(n *Node) {
 				ls[i1] = typecheck(ls[i1], ctxExpr|Etype)
 				n1 = ls[i1]
 				if n1.Type == nil || t == nil {
+					caseTypeBroken = true
 					continue
 				}
 
@@ -207,8 +209,9 @@ func typecheckswitch(n *Node) {
 				ncase.Rlist.SetFirst(nvar)
 			}
 		}
-
-		typecheckslice(ncase.Nbody.Slice(), ctxStmt)
+		if !caseTypeBroken {
+			typecheckslice(ncase.Nbody.Slice(), ctxStmt)
+		}
 	}
 	switch top {
 	// expression switch
