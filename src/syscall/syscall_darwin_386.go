d0e64fd5decef2ef9d9a64f17da6cfef3cd33be2
Elias Naur <elias.naur@gmail.com>
2018-11-30 12:25:18 +0100

    syscall: avoid "64"-postfixed libSystem syscalls on iOS
    The stat(2) man page contain this comment about the 64-bit versions
    of the system file functions:
    
    "Platforms that were released after these updates only have the
    newer variants available to them.  These platforms have the macro
    _DARWIN_FEATURE_ONLY_64_BIT_INODE defined."
    
    It turns out that on iOS the _DARWIN_FEATURE_ONLY_64_BIT_INODE is
    defined and that even though the "64"-postfixed versions are
    accessible they are deemed private. Apps that refer to private
    API are not admissible on App Store, and after the Go runtime
    started using libSystem instead of direct syscalls, the App Store
    submission checks reject apps built with Go tip.
    
    The fix is simple: use the non-postfixed versions on iOS.
    
    getdirentries(2) is not changed; it is not available at all on iOS
    and needs replacement.
    
    Updates #28984
    
    Change-Id: Icb8d44e271456acaa1913ba486fcf5b569722fa9
    Reviewed-on: https://go-review.googlesource.com/c/151938
    Run-TryBot: Elias Naur <elias.naur@gmail.com>
    TryBot-Result: Gobot Gobot <gobot@golang.org>
    Reviewed-by: Keith Randall <khr@golang.org>

--- src/syscall/syscall_darwin_386.go
+++ src/syscall/syscall_darwin_386.go
@@ -14,7 +14,13 @@ func setTimeval(sec, usec int64) Timeval {
 	return Timeval{Sec: int32(sec), Usec: int32(usec)}
 }
 
+//sys	Fstat(fd int, stat *Stat_t) (err error) = SYS_fstat64
+//sys	Fstatfs(fd int, stat *Statfs_t) (err error) = SYS_fstatfs64
 //sysnb	Gettimeofday(tp *Timeval) (err error)
+//sys	Lstat(path string, stat *Stat_t) (err error) = SYS_lstat64
+//sys	Stat(path string, stat *Stat_t) (err error) = SYS_stat64
+//sys	Statfs(path string, stat *Statfs_t) (err error) = SYS_statfs64
+//sys   fstatat(fd int, path string, stat *Stat_t, flags int) (err error) = SYS_fstatat64
 
 func SetKevent(k *Kevent_t, fd, mode, flags int) {
 	k.Ident = uint32(fd)
