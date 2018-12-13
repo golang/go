35edc960c42e570fd196292148c985ffc3a17692
Bryan C. Mills <bcmills@google.com>
2018-12-13 14:30:57 -0500

    cmd/go/internal/work: skip TestRespectSetgidDir on js.
    chown is not implemented on js: see https://build.golang.org/log/43d7b12602660b786a6e080e685165193df0de00.
    
    Change-Id: I3f461338825bb670d682c3f47b17ee1638343fc8
    Reviewed-on: https://go-review.googlesource.com/c/154097
    Run-TryBot: Bryan C. Mills <bcmills@google.com>
    TryBot-Result: Gobot Gobot <gobot@golang.org>
    Reviewed-by: Brad Fitzpatrick <bradfitz@golang.org>

--- src/cmd/go/internal/work/build_test.go
+++ src/cmd/go/internal/work/build_test.go
@@ -227,8 +227,8 @@ func TestRespectSetgidDir(t *testing.T) {
 		if runtime.GOARCH == "arm" || runtime.GOARCH == "arm64" {
 			t.Skip("can't set SetGID bit with chmod on iOS")
 		}
-	case "windows", "plan9":
-		t.Skip("chown/chmod setgid are not supported on Windows and Plan 9")
+	case "windows", "plan9", "js":
+		t.Skip("chown/chmod setgid are not supported on Windows, Plan 9, or JS")
 	}
 
 	var b Builder
