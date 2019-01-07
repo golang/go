e063def13b29c433b3c57fc99257c18c1f9399ce
Daniel Martí <mvdan@mvdan.cc>
2019-01-07 16:24:50 +0100

    go/analysis: fix package doc copy-paste typo
    Change-Id: I17495106c76bbf270f621abe2eca367ed818a79b
    Reviewed-on: https://go-review.googlesource.com/c/156500
    Reviewed-by: Brad Fitzpatrick <bradfitz@golang.org>

--- go/analysis/passes/lostcancel/cmd/lostcancel/main.go
+++ go/analysis/passes/lostcancel/cmd/lostcancel/main.go
@@ -1,4 +1,4 @@
-// The nilness command applies the golang.org/x/tools/go/analysis/passes/lostcancel
+// The lostcancel command applies the golang.org/x/tools/go/analysis/passes/lostcancel
 // analysis to the specified packages of Go source code.
 package main
 
