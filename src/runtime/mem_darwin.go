abf8e355a8fe4b77009cb55f6bef11f74e6ade03
Michael Anthony Knyszek <mknyszek@google.com>
2019-01-23 17:33:35 +0000

    runtime: use MADV_FREE_REUSABLE on darwin
    Currently on darwin we use MADV_FREE, which unfortunately doesn't result
    in a change in the process's RSS until pages actually get kicked out,
    which the OS is free to do lazily (e.g. until it finds itself under
    memory pressure).
    
    To remedy this, we instead use MADV_FREE_REUSABLE which has similar
    semantics, except that it also sets a reusable bit on each page so the
    process's RSS gets reported more accurately. The one caveat is for every
    time we call MADV_FREE_REUSABLE on a region we must call MADV_FREE_REUSE
    to keep the kernel's accounting updated.
    
    Also, because this change requires adding new constants that only exist
    on darwin, it splits mem_bsd.go into mem_bsd.go and mem_darwin.go.
    
    Fixes #29844.
    
    Change-Id: Idb6421698511138a430807bcbbd1516cd57557c8
    Reviewed-on: https://go-review.googlesource.com/c/go/+/159117
    Reviewed-by: Brad Fitzpatrick <bradfitz@golang.org>
    Run-TryBot: Michael Knyszek <mknyszek@google.com>
    TryBot-Result: Gobot Gobot <gobot@golang.org>

--- src/runtime/mem_darwin.go
+++ src/runtime/mem_darwin.go
@@ -1,8 +1,6 @@
-// Copyright 2010 The Go Authors. All rights reserved.
+// Copyright 2018 The Go Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
-
-// +build dragonfly freebsd nacl netbsd openbsd solaris
 
 package runtime
 
@@ -23,10 +21,16 @@ func sysAlloc(n uintptr, sysStat *uint64) unsafe.Pointer {
 }
 
 func sysUnused(v unsafe.Pointer, n uintptr) {
-	madvise(v, n, _MADV_FREE)
+	// MADV_FREE_REUSABLE is like MADV_FREE except it also propagates
+	// accounting information about the process to task_info.
+	madvise(v, n, _MADV_FREE_REUSABLE)
 }
 
 func sysUsed(v unsafe.Pointer, n uintptr) {
+	// MADV_FREE_REUSE is necessary to keep the kernel's accounting
+	// accurate. If called on any memory region that hasn't been
+	// MADV_FREE_REUSABLE'd, it's a no-op.
+	madvise(v, n, _MADV_FREE_REUSE)
 }
 
 // Don't split the stack as this function may be invoked without a valid G,
@@ -42,21 +46,32 @@ func sysFault(v unsafe.Pointer, n uintptr) {
 }
 
 func sysReserve(v unsafe.Pointer, n uintptr) unsafe.Pointer {
-	p, err := mmap(v, n, _PROT_NONE, _MAP_ANON|_MAP_PRIVATE, -1, 0)
+	flags := int32(_MAP_ANON | _MAP_PRIVATE)
+	if raceenabled {
+		// Currently the race detector expects memory to live within a certain
+		// range, and on Darwin 10.10 mmap is prone to ignoring hints, moreso
+		// than later versions and other BSDs (#26475). So, even though it's
+		// potentially dangerous to MAP_FIXED, we do it in the race detection
+		// case because it'll help maintain the race detector's invariants.
+		//
+		// TODO(mknyszek): Drop this once support for Darwin 10.10 is dropped,
+		// and reconsider this when #24133 is addressed.
+		flags |= _MAP_FIXED
+	}
+	p, err := mmap(v, n, _PROT_NONE, flags, -1, 0)
 	if err != 0 {
 		return nil
 	}
 	return p
 }
 
-const _sunosEAGAIN = 11
 const _ENOMEM = 12
 
 func sysMap(v unsafe.Pointer, n uintptr, sysStat *uint64) {
 	mSysStatInc(sysStat, n)
 
 	p, err := mmap(v, n, _PROT_READ|_PROT_WRITE, _MAP_ANON|_MAP_FIXED|_MAP_PRIVATE, -1, 0)
-	if err == _ENOMEM || (GOOS == "solaris" && err == _sunosEAGAIN) {
+	if err == _ENOMEM {
 		throw("runtime: out of memory")
 	}
 	if p != v || err != 0 {
