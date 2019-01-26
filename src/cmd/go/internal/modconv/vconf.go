bf5749d9a85fe94da229b33bb6c40000fff8b523
Tibor Vass <tibor@docker.com>
2019-01-26 00:03:15 +0000

    cmd/go: honor vendor.conf fork repositories in modconv
    With this patch, many projects using vendor.conf format should be able
    to transition to go mod.
    
    Example of a problematic line in cmd/go/internal/modconv/testdata/moby.out:
    
      github.com/go-check/check 4ed411733c5785b40214c70bce814c3a3a689609 https://github.com/cpuguy83/check.git
    
    In vendor.conf format, the third column represents the URL of a fork
    repository which was being ignored before this patch.
    
    Because of this, go mod was incorrectly assuming the revision (4ed...609)
    exists in the upstream github.com/go-check/check when in fact it only does
    in the github.com/cpuguy83/check fork.
    
    One cannot simply expect a user to change github.com/go-check/check to
    github.com/cpuguy83/check because the import path in both cases is
    github.com/go-check/check.
    
    For that example, this patch would do the equivalent of:
      go mod edit -require=github.com/go-check/check@latest
                  -replace=github.com/go-check/check=github.com/cpuguy83/check@4ed411733c5785b40214c70bce814c3a3a689609

--- src/cmd/go/internal/modconv/vconf.go
+++ src/cmd/go/internal/modconv/vconf.go
@@ -20,8 +20,27 @@ func ParseVendorConf(file string, data []byte) (*modfile.File, error) {
 		}
 		f := strings.Fields(line)
 		if len(f) >= 2 {
-			mf.Require = append(mf.Require, &modfile.Require{Mod: module.Version{Path: f[0], Version: f[1]}})
+			v := module.Version{Path: f[0]}
+			if len(f) >= 3 {
+				vNew := module.Version{Path: repoPathToImportPath(f[2]), Version: f[1]}
+				mf.Replace = append(mf.Replace, &modfile.Replace{Old: v, New: vNew})
+				v.Version = "latest"
+			} else {
+				v.Version = f[1]
+			}
+			mf.Require = append(mf.Require, &modfile.Require{Mod: v})
 		}
 	}
 	return mf, nil
+}
+
+func repoPathToImportPath(repo string) string {
+	path := repo
+	for _, prefix := range []string{"https://", "git://", "ssh://", "git+ssh://", "svn://", "svn+ssh://", "bzr://", "bzr+ssh://", "http://"} {
+		if strings.HasPrefix(path, prefix) {
+			path = strings.TrimPrefix(path, prefix)
+			break
+		}
+	}
+	return strings.TrimSuffix(path, ".git")
 }
