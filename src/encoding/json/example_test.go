0a8e107ff0223f697f4e6c5aed307b365db34eca
Ketan Parmar <ketanbparmar@gmail.com>
2019-01-31 07:28:15 +0530

    encoding/json: add example for json.HTMLEscape
    Change-Id: Ib00fcfd46eae27eea0a3d4cab4406f4c461fb57b
    Reviewed-on: https://go-review.googlesource.com/c/160517
    Reviewed-by: Andrew Bonventre <andybons@golang.org>
    Run-TryBot: Andrew Bonventre <andybons@golang.org>
    TryBot-Result: Gobot Gobot <gobot@golang.org>

--- src/encoding/json/example_test.go
+++ src/encoding/json/example_test.go
@@ -301,3 +301,11 @@ func ExampleValid() {
 	// Output:
 	// true false
 }
+
+func ExampleHTMLEscape() {
+	var out bytes.Buffer
+	json.HTMLEscape(&out, []byte(`{"Name":"<b>HTML content</b>"}`))
+	out.WriteTo(os.Stdout)
+	// Output:
+	//{"Name":"\u003cb\u003eHTML content\u003c/b\u003e"}
+}
