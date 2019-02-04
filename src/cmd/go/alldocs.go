b5be877ba4318422547068b85c673639cd843b7d
Alberto Donizetti <alb.donizetti@gmail.com>
2019-02-04 20:07:46 +0100

    cmd/go: clarify @none effect on dependants modules
    Expand modules documentation to clarify why @none is useful. The
    wording is the one suggested by rsc on the issue.
    
    Fixes #26684
    
    Change-Id: I76dc4ff87e50f1dd8536fd9ac1fd938adb29bee3
    Reviewed-on: https://go-review.googlesource.com/c/161037
    Reviewed-by: Bryan C. Mills <bcmills@google.com>

--- src/cmd/go/alldocs.go
+++ src/cmd/go/alldocs.go
@@ -2496,7 +2496,8 @@
 // development module, then get will update the required version.
 // Specifying a version earlier than the current required version is valid and
 // downgrades the dependency. The version suffix @none indicates that the
-// dependency should be removed entirely.
+// dependency should be removed entirely, downgrading or removing modules
+// depending on it as needed.
 //
 // Although get defaults to using the latest version of the module containing
 // a named package, it does not use the latest version of that module's
