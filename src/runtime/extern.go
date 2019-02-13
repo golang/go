0cfe46ce7430b1b14d31d0ab0dd02d14b0472143
Brad Fitzpatrick <bradfitz@golang.org>
2019-02-13 16:15:09 +0000

    [release-branch.go1.12] crypto/tls, runtime: document GODEBUG TLS 1.3 option
    Change-Id: I6801676335924414ce50249df2b7bea08886b203
    Reviewed-on: https://go-review.googlesource.com/c/162360
    Reviewed-by: Filippo Valsorda <filippo@golang.org>
    (cherry picked from commit 48bb61166711f47eb401f245c704a5a4887d4503)
    Reviewed-on: https://go-review.googlesource.com/c/162497

--- src/runtime/extern.go
+++ src/runtime/extern.go
@@ -127,7 +127,7 @@ It is a comma-separated list of name=val pairs setting these named variables:
 	IDs will refer to the ID of the goroutine at the time of creation; it's possible for this
 	ID to be reused for another goroutine. Setting N to 0 will report no ancestry information.
 
-The net and net/http packages also refer to debugging variables in GODEBUG.
+The net, net/http, and crypto/tls packages also refer to debugging variables in GODEBUG.
 See the documentation for those packages for details.
 
 The GOMAXPROCS variable limits the number of operating system threads that
