0fc89a72edc2c73651f7f6841b1146af723f517f
Bryan C. Mills <bcmills@google.com>
2019-02-20 18:25:37 -0500

    cmd,std: add go.mod files
    Updates #30241
    Updates #30228
    
    Change-Id: Ida0fe8263bf44e0498fed2048e22283ba5716835
    Reviewed-on: https://go-review.googlesource.com/c/go/+/164622
    Run-TryBot: Bryan C. Mills <bcmills@google.com>
    TryBot-Result: Gobot Gobot <gobot@golang.org>
    Reviewed-by: Jay Conrod <jayconrod@google.com>

---
+++ src/cmd/go.mod
@@ -0,0 +1,12 @@
+module cmd
+
+go 1.12
+
+require (
+	github.com/google/pprof v0.0.0-20181206194817-3ea8567a2e57
+	github.com/ianlancetaylor/demangle v0.0.0-20180524225900-fc6590592b44 // indirect
+	golang.org/x/arch v0.0.0-20181203225421-5a4828bb7045
+	golang.org/x/crypto v0.0.0-20190211182817-74369b46fc67
+	golang.org/x/sys v0.0.0-20190225065934-cc5685c2db12 // indirect
+	golang.org/x/tools v0.0.0-20190307163923-6a08e3108db3
+)
--- src/make.bash
+++ src/make.bash
@@ -176,7 +176,7 @@ if [ "$GOROOT_BOOTSTRAP" = "$GOROOT" ]; then
 	exit 1
 fi
 rm -f cmd/dist/dist
-GOROOT="$GOROOT_BOOTSTRAP" GOOS="" GOARCH="" "$GOROOT_BOOTSTRAP/bin/go" build -o cmd/dist/dist ./cmd/dist
+GOROOT="$GOROOT_BOOTSTRAP" GOOS="" GOARCH="" GO111MODULE=off "$GOROOT_BOOTSTRAP/bin/go" build -o cmd/dist/dist ./cmd/dist
 
 # -e doesn't propagate out of eval, so check success by hand.
 eval $(./cmd/dist/dist env -p || echo FAIL=true)
