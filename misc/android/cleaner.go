394b7b44e6b3d75a36feb27cbdfaa729415125ad
Bryan C. Mills <bcmills@google.com>
2019-02-21 09:20:42 -0500

    misc/android: add build constraints on files intended to be built by filename only
    Updates #30228
    
    Change-Id: I91a763d94de935d9102d927b5cefee564bbf049b
    Reviewed-on: https://go-review.googlesource.com/c/163208
    Run-TryBot: Bryan C. Mills <bcmills@google.com>
    TryBot-Result: Gobot Gobot <gobot@golang.org>
    Reviewed-by: Brad Fitzpatrick <bradfitz@golang.org>

--- misc/android/cleaner.go
+++ misc/android/cleaner.go
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
+// +build ignore
+
 // Cleaner removes anything from /data/local/tmp/goroot not on a builtin list.
 // Used by androidtest.bash.
 package main
