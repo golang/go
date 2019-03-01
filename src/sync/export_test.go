2b605670020fc637e20a609b1dc86c1f0e7afdd1
Austin Clements <austin@google.com>
2019-03-01 13:16:37 -0500

    sync: internal fixed size lock-free queue for sync.Pool
    This is the first step toward fixing multiple issues with sync.Pool.
    This adds a fixed size, lock-free, single-producer, multi-consumer
    queue that will be used in the new Pool stealing implementation.
    
    For #22950, #22331.
    
    Change-Id: I50e85e3cb83a2ee71f611ada88e7f55996504bb5
    Reviewed-on: https://go-review.googlesource.com/c/go/+/166957
    Run-TryBot: Austin Clements <austin@google.com>
    TryBot-Result: Gobot Gobot <gobot@golang.org>
    Reviewed-by: David Chase <drchase@google.com>

--- src/sync/export_test.go
+++ src/sync/export_test.go
@@ -9,3 +9,28 @@ var Runtime_Semacquire = runtime_Semacquire
 var Runtime_Semrelease = runtime_Semrelease
 var Runtime_procPin = runtime_procPin
 var Runtime_procUnpin = runtime_procUnpin
+
+// poolDequeue testing.
+type PoolDequeue interface {
+	PushHead(val interface{}) bool
+	PopHead() (interface{}, bool)
+	PopTail() (interface{}, bool)
+}
+
+func NewPoolDequeue(n int) PoolDequeue {
+	return &poolDequeue{
+		vals: make([]eface, n),
+	}
+}
+
+func (d *poolDequeue) PushHead(val interface{}) bool {
+	return d.pushHead(val)
+}
+
+func (d *poolDequeue) PopHead() (interface{}, bool) {
+	return d.popHead()
+}
+
+func (d *poolDequeue) PopTail() (interface{}, bool) {
+	return d.popTail()
+}
