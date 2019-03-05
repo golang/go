5652e61069d77cee4de12a62de1a830cd5bb9c72
grant <grantseltzer@gmail.com>
2019-03-05 17:00:11 -0500

    Update lookup_darwin.go
    Signed-off-by: grant <grantseltzer@gmail.com>

--- src/net/lookup_darwin.go
+++ src/net/lookup_darwin.go
@@ -9,20 +9,74 @@ package net
 import (
 	"context"
 	"errors"
+	"fmt"
+	"strings"
 	"unsafe"
 
-	"github.com/golang/go.bkp/src/runtime"
+	"golang.org/x/net/dns/dnsmessage"
 )
 
-func resSearch(ctx context.Context, hostname string) ([]IPAddr, error) {
+func resSearch(ctx context.Context, hostname string, rtype, class int32) ([]IPAddr, error) {
 
 	var byteHostname = []byte(hostname)
 	var responseBuffer = [512]byte{}
-	retcode := runtime.Res_search(&byteHostname[0], 1, 1, &responseBuffer[0], 512)
+
+	retcode := res_search(&byteHostname[0], class, rtype, &responseBuffer[0], 512)
 	if retcode < 0 {
-		return nil, errors.New("//TODO:")
+		return nil, errors.New("could not complete domain resolution")
 	}
 
+	msg := &dnsmessage.Message{}
+	err := msg.Unpack(responseBuffer[:])
+	if err != nil {
+		return nil, fmt.Errorf("could not parse dns response: %s", err.Error())
+	}
+
+	// parse received answers
+	var dnsParser dnsmessage.Parser
+
+	if _, err := dnsParser.Start(responseBuffer); err != nil {
+		return nil, err
+	}
+
+	var answers []IPAddr
+	for {
+		h, err := dnsParser.AnswerHeader()
+		if err == dnsmessage.ErrSectionDone {
+			break
+		}
+		if err != nil {
+			return nil, err
+		}
+
+		if (h.Type != dnsmessage.TypeA && h.Type != dnsmessage.TypeAAAA) ||
+			h.Class != dnsmessage.ClassINET {
+			continue
+		}
+
+		if !strings.EqualFold(h.Name.String(), hostname) {
+			if err := p.SkipAnswer(); err != nil {
+				return nil, err
+			}
+			continue
+		}
+
+		switch h.Type {
+		case dnsmessage.TypeA:
+			r, err := dnsParser.AResource()
+			if err != nil {
+				return nil, err
+			}
+			answers = append(answers, IPAddr{IP: r.A[:]})
+		case dnsmessage.TypeAAAA:
+			r, err := dnsParser.AAAAResource()
+			if err != nil {
+				return nil, err
+			}
+			answers = append(answers, IPAddr{IP: r.AAAA[:]})
+		}
+	}
+	return answers, nil
 }
 
 //go:nosplit
