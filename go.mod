6a08e3108db3c45254bbacc63c70c6addc62e6cb
Dmitri Shuralyov <dmitshur@golang.org>
2019-03-05 16:57:57 -0500

    playground: use stdlib instead of appengine packages
    With modern versions of App Engine, it's no longer needed to use the
    google.golang.org/appengine/... packages.
    
    Package log from standard library can be used instead of the
    google.golang.org/appengine/log package. Packages net/http and
    context from standard library can be used instead of
    google.golang.org/appengine/urlfetch.
    
    This simplifies the code and reduces the number of dependences.
    
    Start using the golangorgenv package from previous commit to
    make the decision of whether to enforce sharing restrictions,
    rather than relying on the appengine build tag. The appengine
    build tag is no longer set in App Engine Standard with Go 1.11+
    runtime. An alternative solution would be detect App Engine by
    doing something like:
    
    	// GAE_ENV environment variable is set to "standard" in App Engine environment, Go 1.11 runtime.
    	// See https://cloud.google.com/appengine/docs/standard/go111/runtime#environment_variables.
    	var onAppengine = os.Getenv("GAE_ENV") == "standard"
    
    But we choose to upgrade to explicit app-scoped environment variable
    configuration as part of this change. It provides better security
    properties, and the value of adding an intermediate transitional step
    is not high enough to justify doing it.
    
    When getting the value of "X-AppEngine-Country" header, use its
    canonical format "X-Appengine-Country" to avoid an allocation.
    This does not change behavior.
    
    Run go mod tidy (using Go 1.12).
    
    Updates golang/go#29981
    Updates golang/go#30486
    
    Change-Id: I82a59e0f28623e06762b7ebdf3930b5ee243acda
    Reviewed-on: https://go-review.googlesource.com/c/tools/+/160837
    Reviewed-by: Michael Matloob <matloob@golang.org>
    Reviewed-by: Brad Fitzpatrick <bradfitz@golang.org>

--- go.mod
+++ go.mod
@@ -1,7 +1,3 @@
 module golang.org/x/tools
 
-require (
-	golang.org/x/net v0.0.0-20190213061140-3a22650c66bd
-	golang.org/x/sync v0.0.0-20181108010431-42b317875d0f // indirect
-	google.golang.org/appengine v1.4.0
-)
+require golang.org/x/net v0.0.0-20190213061140-3a22650c66bd
---
+++ playground/playground.go
@@ -0,0 +1,90 @@
+// Copyright 2013 The Go Authors.  All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Package playground registers HTTP handlers at "/compile" and "/share" that
+// proxy requests to the golang.org playground service.
+// This package may be used unaltered on App Engine Standard with Go 1.11+ runtime.
+package playground // import "golang.org/x/tools/playground"
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+	"time"
+
+	"golang.org/x/tools/godoc/golangorgenv"
+)
+
+const baseURL = "https://play.golang.org"
+
+func init() {
+	http.HandleFunc("/compile", bounce)
+	http.HandleFunc("/share", bounce)
+}
+
+func bounce(w http.ResponseWriter, r *http.Request) {
+	b := new(bytes.Buffer)
+	if err := passThru(b, r); os.IsPermission(err) {
+		http.Error(w, "403 Forbidden", http.StatusForbidden)
+		log.Println(err)
+		return
+	} else if err != nil {
+		http.Error(w, "500 Internal Server Error", http.StatusInternalServerError)
+		log.Println(err)
+		return
+	}
+	io.Copy(w, b)
+}
+
+func passThru(w io.Writer, req *http.Request) error {
+	if req.URL.Path == "/share" && googleCN(req) {
+		return os.ErrPermission
+	}
+	defer req.Body.Close()
+	url := baseURL + req.URL.Path
+	ctx, cancel := context.WithTimeout(req.Context(), 60*time.Second)
+	defer cancel()
+	r, err := post(ctx, url, req.Header.Get("Content-Type"), req.Body)
+	if err != nil {
+		return fmt.Errorf("making POST request: %v", err)
+	}
+	defer r.Body.Close()
+	if _, err := io.Copy(w, r.Body); err != nil {
+		return fmt.Errorf("copying response Body: %v", err)
+	}
+	return nil
+}
+
+func post(ctx context.Context, url, contentType string, body io.Reader) (*http.Response, error) {
+	req, err := http.NewRequest(http.MethodPost, url, body)
+	if err != nil {
+		return nil, fmt.Errorf("http.NewRequest: %v", err)
+	}
+	req.Header.Set("Content-Type", contentType)
+	return http.DefaultClient.Do(req.WithContext(ctx))
+}
+
+// googleCN reports whether request r is considered
+// to be served from golang.google.cn.
+func googleCN(r *http.Request) bool {
+	if r.FormValue("googlecn") != "" {
+		return true
+	}
+	if strings.HasSuffix(r.Host, ".cn") {
+		return true
+	}
+	if !golangorgenv.CheckCountry() {
+		return false
+	}
+	switch r.Header.Get("X-Appengine-Country") {
+	case "", "ZZ", "CN":
+		return true
+	}
+	return false
+}
