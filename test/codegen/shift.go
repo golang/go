61945fc5022e36ae88758f2c4e8f7c29f0824ae0
Josh Bleecher Snyder <josharian@gmail.com>
2019-03-13 13:53:38 -0700

    cmd/compile: don't generate panicshift for masked int shifts
    We know that a & 31 is non-negative for all a, signed or not.
    We can avoid checking that and needing to write out an
    unreachable call to panicshift.
    
    Change-Id: I32f32fb2c950d2b2b35ac5c0e99b7b2dbd47f917
    Reviewed-on: https://go-review.googlesource.com/c/go/+/167499
    Run-TryBot: Josh Bleecher Snyder <josharian@gmail.com>
    Reviewed-by: Keith Randall <khr@golang.org>

--- test/codegen/shift.go
+++ test/codegen/shift.go
@@ -70,6 +70,34 @@ func rshMask64x32Ext(v int64, s int32) int64 {
 	return v >> uint(s&63)
 }
 
+// --------------- //
+//  signed shifts  //
+// --------------- //
+
+// We do want to generate a test + panicshift for these cases.
+func lshSigned(v8 int8, v16 int16, v32 int32, v64 int64, x int) {
+	// amd64:"TESTB"
+	_ = x << v8
+	// amd64:"TESTW"
+	_ = x << v16
+	// amd64:"TESTL"
+	_ = x << v32
+	// amd64:"TESTQ"
+	_ = x << v64
+}
+
+// We want to avoid generating a test + panicshift for these cases.
+func lshSignedMasked(v8 int8, v16 int16, v32 int32, v64 int64, x int) {
+	// amd64:-"TESTB"
+	_ = x << (v8 & 7)
+	// amd64:-"TESTW"
+	_ = x << (v16 & 15)
+	// amd64:-"TESTL"
+	_ = x << (v32 & 31)
+	// amd64:-"TESTQ"
+	_ = x << (v64 & 63)
+}
+
 // ------------------ //
 //   bounded shifts   //
 // ------------------ //
