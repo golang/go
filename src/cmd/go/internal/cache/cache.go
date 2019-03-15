37e4a61d26d3b10aa11c5155b72c6bc882bd3122
Bryan C. Mills <bcmills@google.com>
2019-03-15 09:10:09 -0400

    cmd/go/internal/cache: remove log.txt from the cache directory
    Also remove an existing log.txt in the cache directory during
    'go clean -cache' if it exists.
    
    Fixes #25323
    
    Change-Id: I1c6e20554db0edc6d850a7b4379d71ef5844eaea
    Reviewed-on: https://go-review.googlesource.com/c/go/+/167741
    Run-TryBot: Bryan C. Mills <bcmills@google.com>
    TryBot-Result: Gobot Gobot <gobot@golang.org>
    Reviewed-by: Jay Conrod <jayconrod@google.com>

--- src/cmd/go/internal/cache/cache.go
+++ src/cmd/go/internal/cache/cache.go
@@ -33,7 +33,6 @@ type OutputID [HashSize]byte
 // A Cache is a package cache, backed by a file system directory tree.
 type Cache struct {
 	dir string
-	log *os.File
 	now func() time.Time
 }
 
@@ -63,13 +62,8 @@ func Open(dir string) (*Cache, error) {
 			return nil, err
 		}
 	}
-	f, err := os.OpenFile(filepath.Join(dir, "log.txt"), os.O_WRONLY|os.O_APPEND|os.O_CREATE, 0666)
-	if err != nil {
-		return nil, err
-	}
 	c := &Cache{
 		dir: dir,
-		log: f,
 		now: time.Now,
 	}
 	return c, nil
@@ -141,7 +135,6 @@ type Entry struct {
 // get is Get but does not respect verify mode, so that Put can use it.
 func (c *Cache) get(id ActionID) (Entry, error) {
 	missing := func() (Entry, error) {
-		fmt.Fprintf(c.log, "%d miss %x\n", c.now().Unix(), id)
 		return Entry{}, errMissing
 	}
 	f, err := os.Open(c.fileName(id, "a"))
@@ -183,8 +176,6 @@ func (c *Cache) get(id ActionID) (Entry, error) {
 	if err != nil || size < 0 {
 		return missing()
 	}
-
-	fmt.Fprintf(c.log, "%d get %x\n", c.now().Unix(), id)
 
 	c.used(c.fileName(id, "a"))
 
@@ -349,7 +340,6 @@ func (c *Cache) putIndexEntry(id ActionID, out OutputID, size int64, allowVerify
 	}
 	os.Chtimes(file, c.now(), c.now()) // mainly for tests
 
-	fmt.Fprintf(c.log, "%d put %x %x %d\n", c.now().Unix(), id, out, size)
 	return nil
 }
 
