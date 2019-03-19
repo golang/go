72026d66e2e0b1cc28239f22f8a5c9ef6a66f8d5
Clément Chigot <clement.chigot@atos.net>
2019-03-19 14:00:03 +0100

    runtime/cgo: correct cgo_export directives in callbacks_aix.go
    This commit removes spaces which were wrongly added in
    //go:cgo_export_static during CL 164010.
    
    Change-Id: Iadd18efdde9ff32e907d793a72ef0f9efda35fe6
    Reviewed-on: https://go-review.googlesource.com/c/go/+/168317
    Run-TryBot: Tobias Klauser <tobias.klauser@gmail.com>
    TryBot-Result: Gobot Gobot <gobot@golang.org>
    Reviewed-by: Ian Lance Taylor <iant@golang.org>

--- src/runtime/cgo/callbacks_aix.go
+++ src/runtime/cgo/callbacks_aix.go
@@ -6,5 +6,5 @@ package cgo
 
 // These functions must be exported in order to perform
 // longcall on cgo programs (cf gcc_aix_ppc64.c).
-// go:cgo_export_static __cgo_topofstack
-// go:cgo_export_static runtime.rt0_go
+//go:cgo_export_static __cgo_topofstack
+//go:cgo_export_static runtime.rt0_go
