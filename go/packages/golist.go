golang/go

commit 86483bace61a0658d207b92f8ed38e06b386a5cb

go/packages: make error message for conflicting packages more clear
Make it slightly more clear that this is not a user error, but an
internal error.

Updates golang/go#30519

Change-Id: I7adb3b5bb1548eab8e46db48946d55f9d59a4311
Reviewed-on: https://go-review.googlesource.com/c/tools/+/168657
Run-TryBot: Michael Matloob <matloob@golang.org>
Reviewed-by: Ian Cottrell <iancottrell@google.com>

go/packages/golist.go
@@ -586,7 +586,7 @@ func golistDriverCurrent(cfg *Config, words ...string) (*driverResponse, error) 
 
 		if old, found := seen[p.ImportPath]; found {
 			if !reflect.DeepEqual(p, old) {
-				return nil, fmt.Errorf("go list repeated package %v with different values", p.ImportPath)
+				return nil, fmt.Errorf("internal error: go list gives conflicting information for package ", p.ImportPath)
 			}
 			// skip the duplicate
 			continue

commit e250d351ecad161c4cf2f50bce18a1b2df5dfd78

go/packages: fix the build
Broken by CL 168657

Change-Id: I359d53c13eb470a5d95440d89b0cca3616b94f00
Reviewed-on: https://go-review.googlesource.com/c/tools/+/168798
Run-TryBot: Brad Fitzpatrick <bradfitz@golang.org>
Reviewed-by: Ian Cottrell <iancottrell@google.com>
TryBot-Result: Gobot Gobot <gobot@golang.org>

go/packages/golist.go
@@ -587,7 +587,7 @@ func golistDriverCurrent(cfg *Config, words ...string) (*driverResponse, error) 
 
 		if old, found := seen[p.ImportPath]; found {
 			if !reflect.DeepEqual(p, old) {
-				return nil, fmt.Errorf("internal error: go list gives conflicting information for package ", p.ImportPath)
+				return nil, fmt.Errorf("internal error: go list gives conflicting information for package %v", p.ImportPath)
 			}
 			// skip the duplicate
 			continue