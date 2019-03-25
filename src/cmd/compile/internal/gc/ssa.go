5ee1d5d39f0c802be0de31533042ddc3871a0b1e
Josh Bleecher Snyder <josharian@gmail.com>
2019-03-25 16:06:16 -0700

    cmd/compile: minor cleanup
    Use constants that are easier to read.
    
    Change-Id: I11fd6363b3bd283a4cc7c9908c2327123c64dcf7
    Reviewed-on: https://go-review.googlesource.com/c/go/+/169723
    Run-TryBot: Josh Bleecher Snyder <josharian@gmail.com>
    TryBot-Result: Gobot Gobot <gobot@golang.org>
    Reviewed-by: Brad Fitzpatrick <bradfitz@golang.org>

--- src/cmd/compile/internal/gc/ssa.go
+++ src/cmd/compile/internal/gc/ssa.go
@@ -4695,7 +4695,7 @@ var f32_u64 = f2uCvtTab{
 	or:         ssa.OpOr64,
 	floatValue: (*state).constFloat32,
 	intValue:   (*state).constInt64,
-	cutoff:     9223372036854775808,
+	cutoff:     1 << 63,
 }
 
 var f64_u64 = f2uCvtTab{
@@ -4705,7 +4705,7 @@ var f64_u64 = f2uCvtTab{
 	or:         ssa.OpOr64,
 	floatValue: (*state).constFloat64,
 	intValue:   (*state).constInt64,
-	cutoff:     9223372036854775808,
+	cutoff:     1 << 63,
 }
 
 var f32_u32 = f2uCvtTab{
@@ -4715,7 +4715,7 @@ var f32_u32 = f2uCvtTab{
 	or:         ssa.OpOr32,
 	floatValue: (*state).constFloat32,
 	intValue:   func(s *state, t *types.Type, v int64) *ssa.Value { return s.constInt32(t, int32(v)) },
-	cutoff:     2147483648,
+	cutoff:     1 << 31,
 }
 
 var f64_u32 = f2uCvtTab{
@@ -4725,7 +4725,7 @@ var f64_u32 = f2uCvtTab{
 	or:         ssa.OpOr32,
 	floatValue: (*state).constFloat64,
 	intValue:   func(s *state, t *types.Type, v int64) *ssa.Value { return s.constInt32(t, int32(v)) },
-	cutoff:     2147483648,
+	cutoff:     1 << 31,
 }
 
 func (s *state) float32ToUint64(n *Node, x *ssa.Value, ft, tt *types.Type) *ssa.Value {
