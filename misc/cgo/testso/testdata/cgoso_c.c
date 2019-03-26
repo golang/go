53c9c068115168ebcc1e649fa7a15a804a99d92f
Clément Chigot <clement.chigot@atos.net>
2019-03-26 15:00:00 +0100

    misc/cgo: enable testso and testsovar on aix/ppc64
    On AIX, shared objects must be wrapped under an archive file.
    
    For testso, creating libcgosotest with an extern symbol isn't
    AIX-friendly. By default, ld will block such behavior. Rather than
    forcing ld to work as on Linux and using the run-time linking,
    goCallback became a function pointer which is set by setCallback().
    
    Updates #30565
    
    Change-Id: I455ab32faddd41f1b0c84cc9e503788044ad49b2
    Reviewed-on: https://go-review.googlesource.com/c/go/+/169020
    Run-TryBot: Ian Lance Taylor <iant@golang.org>
    Reviewed-by: Ian Lance Taylor <iant@golang.org>

--- misc/cgo/testso/testdata/cgoso_c.c
+++ misc/cgo/testso/testdata/cgoso_c.c
@@ -14,6 +14,15 @@ __declspec(dllexport) void setCallback(void *f)
 	goCallback = (void (*)())f;
 }
 __declspec(dllexport) void sofunc(void);
+#elif defined(_AIX)
+// AIX doesn't allow the creation of a shared object with an
+// undefined symbol. It's possible to bypass this problem by
+// using -Wl,-G and -Wl,-brtl option which allows run-time linking.
+// However, that's not how most of AIX shared object works.
+// Therefore, it's better to consider goCallback as a pointer and
+// to set up during an init function.
+void (*goCallback)(void);
+void setCallback(void *f) { goCallback = f; }
 #else
 extern void goCallback(void);
 void setCallback(void *f) { (void)f; }
