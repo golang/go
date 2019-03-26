cea6b6f4f61f779cddd81868eb51fda25eae4208
grant <grantseltzer@gmail.com>
2019-03-26 17:34:36 -0400

    Fix style issues
    Signed-off-by: grant <grantseltzer@gmail.com>

--- src/net/cgo_darwin_stub.go
+++ src/net/cgo_darwin_stub.go
@@ -79,28 +79,31 @@ func cgoLookupPTR(ctx context.Context, addr string) (ptrs []string, err error, c
 	return ptrs, nil, true
 }
 
-var resInitOnce sync.Once
+var (
+	resInitOnce sync.Once
+	retcode     int32
+)
 
 // resolverGetResources will make a call to the 'res_search' routine in libSystem
 // and parse the output as a slice of resource resources which can then be parsed
 func resolverGetResources(ctx context.Context, hostname string, rtype, class int32) ([]dnsmessage.Resource, error) {
-	var byteHostname = []byte(hostname)
-	var responseBuffer = [512]byte{}
 
-	var retcode int32
 	resInitOnce.Do(func() {
 		retcode = res_init()
 	})
 	if retcode < 0 {
-		return nil, errors.New("could not initialize resolution data")
+		return nil, errors.New("could not initialize name resolver data")
 	}
 
-	retcode = res_search(&byteHostname[0], class, rtype, &responseBuffer[0], 512)
+	var byteHostname = []byte(hostname)
+	var responseBuffer [512]byte
+
+	retcode = res_search(&byteHostname[0], class, rtype, &responseBuffer[0], len(responseBuffer))
 	if retcode < 0 {
 		return nil, errors.New("could not complete domain resolution")
 	}
 
-	msg := &dnsmessage.Message{}
+	var msg dnsmessage.Message
 	err := msg.Unpack(responseBuffer[:])
 	if err != nil {
 		return nil, err
@@ -131,13 +134,11 @@ func parseHostsFromResources(resources []dnsmessage.Resource) ([]string, error) 
 	for i := range resources {
 		switch resources[i].Header.Type {
 		case dnsmessage.TypeA:
-			b, ok := resources[i].Body.(*dnsmessage.AResource)
-			if !ok {
-				return nil, errors.New("could not parse A resource record")
-			}
+			b := resources[i].Body.(*dnsmessage.AResource)
+
 			answers = append(answers, string(b.A[:]))
 		case dnsmessage.TypeAAAA:
-			b, ok := resources[i].Body.(*dnsmessage.AAAAResource)
+			b := resources[i].Body.(*dnsmessage.AAAAResource)
 			if !ok {
 				return nil, errors.New("could not parse AAAA resource record")
 			}
@@ -155,14 +156,14 @@ func parseIPsFromResources(resources []dnsmessage.Resource) ([]IPAddr, error) {
 	for i := range resources {
 		switch resources[i].Header.Type {
 		case dnsmessage.TypeA:
-			b, ok := resources[i].Body.(*dnsmessage.AResource)
+			b := resources[i].Body.(*dnsmessage.AResource)
 			if !ok {
 				return nil, errors.New("could not parse A resource record")
 			}
 			ip := parseIPv4(string(b.A[:]))
 			answers = append(answers, IPAddr{IP: ip})
 		case dnsmessage.TypeAAAA:
-			b, ok := resources[i].Body.(*dnsmessage.AAAAResource)
+			b := resources[i].Body.(*dnsmessage.AAAAResource)
 			if !ok {
 				return nil, errors.New("could not parse AAAA resource record")
 			}
@@ -191,10 +192,7 @@ func parsePTRsFromResources(resources []dnsmessage.Resource) ([]string, error) {
 	for i := range resources {
 		switch resources[i].Header.Type {
 		case dnsmessage.TypePTR:
-			p, ok := resources[0].Body.(*dnsmessage.PTRResource)
-			if !ok {
-				return nil, errors.New("could not parse PTR record")
-			}
+			p := resources[0].Body.(*dnsmessage.PTRResource)
 			answers = append(answers, p.PTR.String())
 		default:
 			return nil, errors.New("could not parse a PTR response from message buffer")
@@ -204,7 +202,7 @@ func parsePTRsFromResources(resources []dnsmessage.Resource) ([]string, error) {
 	return answers, nil
 }
 
-// res_init and res_search are defined in runtimne/lookup_darwin.go
+// res_init and res_search are defined in runtime/lookup_darwin.go
 
 func res_init() int32
 
--- src/net/cgo_stub.go
+++ src/net/cgo_stub.go
@@ -2,18 +2,14 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// +build netgo !cgo
+// +build !cgo netgo
 // +build !darwin
 
 package net
 
-import (
-	"context"
-)
+import "context"
 
-func init() {
-	netGo = true
-}
+func init() { netGo = true }
 
 type addrinfoErrno int
 
