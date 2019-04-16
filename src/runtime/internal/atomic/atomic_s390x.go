5960430197cbfb8bf53b96fcef7fb6831152afdd
Matthew Dempsky <mdempsky@google.com>
2019-04-16 11:59:05 -0700

    runtime/internal/atomic: fix s390x's StorepNoWB implementation
    Same as CL 170323, but for s390x instead of wasm.
    
    Fixes #31495.
    
    Change-Id: Ie39f649f5e33690375a8bcb1bc3b92d912ca4398
    Reviewed-on: https://go-review.googlesource.com/c/go/+/172417
    Run-TryBot: Matthew Dempsky <mdempsky@google.com>
    Reviewed-by: Brad Fitzpatrick <bradfitz@golang.org>
    TryBot-Result: Gobot Gobot <gobot@golang.org>

--- src/runtime/internal/atomic/atomic_s390x.go
+++ src/runtime/internal/atomic/atomic_s390x.go
@@ -42,11 +42,14 @@ func Store64(ptr *uint64, val uint64) {
 	*ptr = val
 }
 
+//go:notinheap
+type noWB struct{}
+
 // NO go:noescape annotation; see atomic_pointer.go.
 //go:noinline
 //go:nosplit
 func StorepNoWB(ptr unsafe.Pointer, val unsafe.Pointer) {
-	*(*uintptr)(ptr) = uintptr(val)
+	*(**noWB)(ptr) = (*noWB)(val)
 }
 
 //go:noinline
