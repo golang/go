f6a57b66a7baa69b704ee45650d487ae7f70f344
Russ Cox <rsc@golang.org>
2019-04-22 23:30:03 -0400

    cmd/go: make 'go version' apply to files
    Expand "go version" from printing just the version of the
    go command itself to being able to print the version of any
    go binary (when listed on the command line).
    
    This is a simplified version of rsc.io/goversion, which will
    now be deprecated in favor of "go version".
    
    (Preparatory changes to runtime, cmd/go, and cmd/link
    made this version information easier to find, allowing a
    much simpler implementation than in rsc.io/goversion.)
    
    Fixes #31624.
    
    Change-Id: I5f6f5b8392ce86a617e32ce287f6bf2aceafccff
    Reviewed-on: https://go-review.googlesource.com/c/go/+/173343
    Run-TryBot: Russ Cox <rsc@golang.org>
    TryBot-Result: Gobot Gobot <gobot@golang.org>
    Reviewed-by: Jay Conrod <jayconrod@google.com>

--- src/cmd/go/alldocs.go
+++ src/cmd/go/alldocs.go
@@ -1357,9 +1357,27 @@
 //
 // Usage:
 //
-// 	go version
+// 	go version [-m] [-v] [file ...]
 //
-// Version prints the Go version, as reported by runtime.Version.
+// Version prints the build information for Go executables.
+//
+// Go version reports the Go version used to build each of the named
+// executable files.
+//
+// If no files are named on the command line, go version prints its own
+// version information.
+//
+// If a directory is named, go version walks that directory, recursively,
+// looking for recognized Go binaries and reporting their versions.
+// By default, go version does not report unrecognized files found
+// during a directory scan. The -v flag causes it to report unrecognized files.
+//
+// The -m flag causes go version to print each executable's embedded
+// module version information, when available. In the output, the modul
+// information consists of multiple lines following the version line, each
+// indented by a leading tab character.
+//
+// See also: go doc runtime/debug.BuildInfo.
 //
 //
 // Report likely mistakes in packages
