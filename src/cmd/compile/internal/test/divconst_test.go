17615969b6c910972a4babb4d1c8f009b1d851e3
Keith Randall <khr@golang.org>
2019-04-23 20:49:05 +0000

    Revert "cmd/compile: add signed divisibility by power of 2 rules"
    This reverts CL 168038 (git 68819fb6d2bab59e4eadcdf62aa4a2a54417d640)
    
    Reason for revert: Doesn't work on 32 bit archs.
    
    Change-Id: Idec9098060dc65bc2f774c5383f0477f8eb63a3d
    Reviewed-on: https://go-review.googlesource.com/c/go/+/173442
    Reviewed-by: Brad Fitzpatrick <bradfitz@golang.org>

--- src/cmd/compile/internal/test/divconst_test.go
+++ src/cmd/compile/internal/test/divconst_test.go
@@ -8,19 +8,11 @@ import (
 	"testing"
 )
 
-var boolres bool
-
 var i64res int64
 
 func BenchmarkDivconstI64(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		i64res = int64(i) / 7
-	}
-}
-
-func BenchmarkDivisiblePow2constI64(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		boolres = int64(i)%16 == 0
 	}
 }
 
@@ -40,12 +32,6 @@ func BenchmarkDivconstI32(b *testing.B) {
 	}
 }
 
-func BenchmarkDivisiblePow2constI32(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		boolres = int32(i)%16 == 0
-	}
-}
-
 var u32res uint32
 
 func BenchmarkDivconstU32(b *testing.B) {
@@ -62,12 +48,6 @@ func BenchmarkDivconstI16(b *testing.B) {
 	}
 }
 
-func BenchmarkDivisiblePow2constI16(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		boolres = int16(i)%16 == 0
-	}
-}
-
 var u16res uint16
 
 func BenchmarkDivconstU16(b *testing.B) {
@@ -81,12 +61,6 @@ var i8res int8
 func BenchmarkDivconstI8(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		i8res = int8(i) / 7
-	}
-}
-
-func BenchmarkDivisiblePow2constI8(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		boolres = int8(i)%16 == 0
 	}
 }
 
