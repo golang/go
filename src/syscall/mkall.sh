ba9bc8e5fefcc62216f7bf2f85923d226e8906e2
Tobias Klauser <tklauser@distanz.ch>
2019-05-04 11:36:13 +0200

    syscall: support generating netbsd/arm64 files in mkall.sh
    CL 155739 added the generated files but didn't update mkall.sh. Do so
    now.
    
    Updates #30824
    
    Change-Id: I642bbff6afbc976091a0dc291fa2beff5e245246
    Reviewed-on: https://go-review.googlesource.com/c/go/+/175237
    Reviewed-by: Brad Fitzpatrick <bradfitz@golang.org>
    Reviewed-by: Benny Siegert <bsiegert@gmail.com>
    Run-TryBot: Benny Siegert <bsiegert@gmail.com>
    Run-TryBot: Tobias Klauser <tobias.klauser@gmail.com>
    TryBot-Result: Gobot Gobot <gobot@golang.org>

--- src/syscall/mkall.sh
+++ src/syscall/mkall.sh
@@ -288,6 +288,12 @@ netbsd_arm)
 	mksysnum="curl -s 'http://cvsweb.netbsd.org/bsdweb.cgi/~checkout~/src/sys/kern/syscalls.master' | ./mksysnum_netbsd.pl"
 	mktypes="GOARCH=$GOARCH go tool cgo -godefs"
 	;;
+netbsd_arm64)
+	mkerrors="$mkerrors -m64"
+	mksyscall="./mksyscall.pl -netbsd"
+	mksysnum="curl -s 'http://cvsweb.netbsd.org/bsdweb.cgi/~checkout~/src/sys/kern/syscalls.master' | ./mksysnum_netbsd.pl"
+	mktypes="GOARCH=$GOARCH go tool cgo -godefs"
+	;;
 openbsd_386)
 	mkerrors="$mkerrors -m32"
 	mksyscall="./mksyscall.pl -l32 -openbsd"
