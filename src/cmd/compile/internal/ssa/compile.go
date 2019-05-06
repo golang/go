9f6ef5c04e320210d989ad1078cb7ad2d2c68156
Josh Bleecher Snyder <josharian@gmail.com>
2019-05-05 18:34:18 -0700

    cmd/compile: note that some rules know the name of the opt pass
    Change-Id: I4a70f4a52f84cf50f99939351319504b1c5dff76
    Reviewed-on: https://go-review.googlesource.com/c/go/+/175777
    Run-TryBot: Josh Bleecher Snyder <josharian@gmail.com>
    Reviewed-by: Keith Randall <khr@golang.org>
    TryBot-Result: Gobot Gobot <gobot@golang.org>

--- src/cmd/compile/internal/ssa/compile.go
+++ src/cmd/compile/internal/ssa/compile.go
@@ -386,7 +386,7 @@ var passes = [...]pass{
 	{name: "short circuit", fn: shortcircuit},
 	{name: "decompose args", fn: decomposeArgs, required: true},
 	{name: "decompose user", fn: decomposeUser, required: true},
-	{name: "opt", fn: opt, required: true},               // TODO: split required rules and optimizing rules
+	{name: "opt", fn: opt, required: true},               // NB: some generic rules know the name of the opt pass. TODO: split required rules and optimizing rules
 	{name: "zero arg cse", fn: zcse, required: true},     // required to merge OpSB values
 	{name: "opt deadcode", fn: deadcode, required: true}, // remove any blocks orphaned during opt
 	{name: "generic cse", fn: cse},
