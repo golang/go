d56199d68e3f34d88dac92997662dc6d6ab6ae14
Russ Cox <rsc@golang.org>
2019-05-09 11:45:38 -0400

    cmd/vendor: import vet fixes from x/tools
    Fixes build - I did not understand that vetall was
    effectively pinned to a vet version by cmd/go.mod.
    
    Change-Id: I56bfd8f62eadacc97cad0ed48e41a178bbc18b8f
    Reviewed-on: https://go-review.googlesource.com/c/go/+/176179
    Run-TryBot: Russ Cox <rsc@golang.org>
    Reviewed-by: Brad Fitzpatrick <bradfitz@golang.org>

--- src/cmd/vendor/golang.org/x/tools/go/analysis/passes/printf/printf.go
+++ src/cmd/vendor/golang.org/x/tools/go/analysis/passes/printf/printf.go
@@ -856,18 +856,26 @@ func recursiveStringer(pass *analysis.Pass, e ast.Expr) bool {
 		return false
 	}
 
-	// Is it the receiver r, or &r?
-	recv := stringMethod.Type().(*types.Signature).Recv()
-	if recv == nil {
+	sig := stringMethod.Type().(*types.Signature)
+	if !isStringer(sig) {
 		return false
 	}
+
+	// Is it the receiver r, or &r?
 	if u, ok := e.(*ast.UnaryExpr); ok && u.Op == token.AND {
 		e = u.X // strip off & from &r
 	}
 	if id, ok := e.(*ast.Ident); ok {
-		return pass.TypesInfo.Uses[id] == recv
+		return pass.TypesInfo.Uses[id] == sig.Recv()
 	}
 	return false
+}
+
+// isStringer reports whether the method signature matches the String() definition in fmt.Stringer.
+func isStringer(sig *types.Signature) bool {
+	return sig.Params().Len() == 0 &&
+		sig.Results().Len() == 1 &&
+		sig.Results().At(0).Type() == types.Typ[types.String]
 }
 
 // isFunctionValue reports whether the expression is a function as opposed to a function call.
