afcfe0d3c2980722af0dc55a3422c19cc53f5a65
Jason A. Donenfeld <Jason@zx2c4.com>
2019-05-12 14:34:30 +0200

    [release-branch.go1.12] os: pass correct environment when creating Windows processes
    This is CVE-2019-11888.
    
    Previously, passing a nil environment but a non-nil token would result
    in the new potentially unprivileged process inheriting the parent
    potentially privileged environment, or would result in the new
    potentially privileged process inheriting the parent potentially
    unprivileged environment. Either way, it's bad. In the former case, it's
    an infoleak. In the latter case, it's a possible EoP, since things like
    PATH could be overwritten.
    
    Not specifying an environment currently means, "use the existing
    environment". This commit amends the behavior to be, "use the existing
    environment of the token the process is being created for." The behavior
    therefore stays the same when creating processes without specifying a
    token. And it does the correct thing when creating processes when
    specifying a token.
    
    Updates #32000
    Fixes #32081
    
    Change-Id: Ib4a90cfffb6ba866c855f66f1313372fdd34ce41
    Reviewed-on: https://go-review.googlesource.com/c/go/+/177538
    Run-TryBot: Jason Donenfeld <Jason@zx2c4.com>
    TryBot-Result: Gobot Gobot <gobot@golang.org>
    Reviewed-by: Ian Lance Taylor <iant@golang.org>

--- src/os/exec_posix.go
+++ src/os/exec_posix.go
@@ -38,7 +38,10 @@ func startProcess(name string, argv []string, attr *ProcAttr) (p *Process, err e
 		Sys: attr.Sys,
 	}
 	if sysattr.Env == nil {
-		sysattr.Env = Environ()
+		sysattr.Env, err = environForSysProcAttr(sysattr.Sys)
+		if err != nil {
+			return nil, err
+		}
 	}
 	for _, f := range attr.Files {
 		sysattr.Files = append(sysattr.Files, f.Fd())
