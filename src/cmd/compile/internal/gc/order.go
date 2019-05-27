515bb0129df2d8462d561301cf9982f2b01de7f8
LE Manh Cuong <cuong.manhle.vn@gmail.com>
2019-05-27 15:58:24 +0700

    cmd/compile: remove isfat from order expr
    isfat was removed in walkexpr in CL 32313. For consistency,
    remove it from order expr, too.
    
    Change-Id: I0a47e0da13ba0168d6a055d990b8efad26ad790d
    Reviewed-on: https://go-review.googlesource.com/c/go/+/179057
    Run-TryBot: Cuong Manh Le <cuong.manhle.vn@gmail.com>
    TryBot-Result: Gobot Gobot <gobot@golang.org>
    Reviewed-by: Matthew Dempsky <mdempsky@google.com>

--- src/cmd/compile/internal/gc/order.go
+++ src/cmd/compile/internal/gc/order.go
@@ -1028,7 +1028,6 @@ func (o *Order) expr(n, lhs *Node) *Node {
 			}
 		}
 
-		// key must be addressable
 	case OINDEXMAP:
 		n.Left = o.expr(n.Left, nil)
 		n.Right = o.expr(n.Right, nil)
@@ -1048,6 +1047,7 @@ func (o *Order) expr(n, lhs *Node) *Node {
 			}
 		}
 
+		// key must be addressable
 		n.Right = o.mapKeyTemp(n.Left.Type, n.Right)
 		if needCopy {
 			n = o.copyExpr(n, n.Type, false)
@@ -1205,10 +1205,7 @@ func (o *Order) expr(n, lhs *Node) *Node {
 
 	case ODOTTYPE, ODOTTYPE2:
 		n.Left = o.expr(n.Left, nil)
-		// TODO(rsc): The isfat is for consistency with componentgen and walkexpr.
-		// It needs to be removed in all three places.
-		// That would allow inlining x.(struct{*int}) the same as x.(*int).
-		if !isdirectiface(n.Type) || isfat(n.Type) || instrumenting {
+		if !isdirectiface(n.Type) || instrumenting {
 			n = o.copyExpr(n, n.Type, true)
 		}
 
