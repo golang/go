b0816ba041f99e6bb3d0c5f86b281224d98c6fe8
LE Manh Cuong <cuong.manhle.vn@gmail.com>
2019-06-01 00:36:02 +0700

    cmd/compile: fix wrong field type in scasetype
    The only place set releasetime to negative is in runtime.selectgo
    (when blockprofilerate greater than zero), so we are safe in compiler
    code.
    
    But scasetype must keep in sync with runtime/select.go scase struct, so
    releasetime must be int64.
    
    Change-Id: I39ea944f5f2872452d3ffd57f7604d51e0d2590a
    Reviewed-on: https://go-review.googlesource.com/c/go/+/179799
    Run-TryBot: Cuong Manh Le <cuong.manhle.vn@gmail.com>
    TryBot-Result: Gobot Gobot <gobot@golang.org>
    Reviewed-by: Matthew Dempsky <mdempsky@google.com>

--- src/cmd/compile/internal/gc/select.go
+++ src/cmd/compile/internal/gc/select.go
@@ -388,7 +388,7 @@ func scasetype() *types.Type {
 			namedfield("elem", types.Types[TUNSAFEPTR]),
 			namedfield("kind", types.Types[TUINT16]),
 			namedfield("pc", types.Types[TUINTPTR]),
-			namedfield("releasetime", types.Types[TUINT64]),
+			namedfield("releasetime", types.Types[TINT64]),
 		})
 		scase.SetNoalg(true)
 	}
