6a9b22ce9d04993d3297ace5ec531292edb120f2
Lynn Boger <laboger@linux.vnet.ibm.com>
2019-06-11 15:21:51 -0400

    vendor: go get -u golang.org/x/crypto && go mod vendor
    Pick up asm ppc64le implementations for internal/chacha20 and
    poly1305.
    
    Change-Id: Ide6fc91da3b10d1c3d9e8e00957c52be06a3cf93
    Reviewed-on: https://go-review.googlesource.com/c/go/+/181877
    Run-TryBot: Lynn Boger <laboger@linux.vnet.ibm.com>
    TryBot-Result: Gobot Gobot <gobot@golang.org>
    Reviewed-by: Carlos Eduardo Seo <cseo@linux.vnet.ibm.com>

--- src/vendor/golang.org/x/crypto/internal/chacha20/chacha_noasm.go
+++ src/vendor/golang.org/x/crypto/internal/chacha20/chacha_noasm.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// +build !arm64,!s390x arm64,!go1.11 gccgo appengine
+// +build !ppc64le,!arm64,!s390x arm64,!go1.11 gccgo appengine
 
 package chacha20
 
--- src/vendor/golang.org/x/crypto/poly1305/sum_noasm.go
+++ src/vendor/golang.org/x/crypto/poly1305/sum_noasm.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// +build s390x,!go1.11 !arm,!amd64,!s390x gccgo appengine nacl
+// +build s390x,!go1.11 !arm,!amd64,!s390x,!ppc64le gccgo appengine nacl
 
 package poly1305
 
