d4e310b4a8a5f0fe33e0a7e813a14ff19b433da4
Ian Cottrell <iancottrell@google.com>
2019-06-12 15:54:15 -0400

    gopls: adding the gopls module
    Change-Id: I521ff33f8733a7d636b8ebc611715cb19e41bc83
    Reviewed-on: https://go-review.googlesource.com/c/tools/+/181946
    Reviewed-by: Rebecca Stambler <rstambler@golang.org>

---
+++ gopls/go.mod
@@ -0,0 +1,7 @@
+module golang.org/x/tools/gopls
+
+go 1.11
+
+require golang.org/x/tools v0.0.0-20190612231717-10539ce30318
+
+replace golang.org/x/tools => ../
---
+++ gopls/main.go
@@ -0,0 +1,21 @@
+// Copyright 2019 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// The gopls command is an LSP server for Go.
+// The Language Server Protocol allows any text editor
+// to be extended with IDE-like features;
+// see https://langserver.org/ for details.
+package main // import "golang.org/x/tools/gopls"
+
+import (
+	"context"
+	"os"
+
+	"golang.org/x/tools/internal/lsp/cmd"
+	"golang.org/x/tools/internal/tool"
+)
+
+func main() {
+	tool.Main(context.Background(), cmd.New("", nil), os.Args[1:])
+}
