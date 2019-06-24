8ea4f8e3e5bfd414380c369ad66f924066e789f7
Agniva De Sarker <agnivade@yahoo.co.in>
2019-06-24 14:01:44 +0530

    refactor/rename: remove extraneous space
    Fixes golang/go#27573
    
    Change-Id: I484878c612c50009316bc7d03e4157b455421236
    Reviewed-on: https://go-review.googlesource.com/c/tools/+/183577
    Reviewed-by: Ian Cottrell <iancottrell@google.com>

--- refactor/rename/rename.go
+++ refactor/rename/rename.go
@@ -90,7 +90,7 @@ affected.  For a local renaming, this is just the package specified by
 the workspace ($GOROOT and $GOPATH).
 
 gorename rejects renamings of concrete methods that would change the
-assignability relation between types and interfaces.  If the interface
+assignability relation between types and interfaces. If the interface
 change was intentional, initiate the renaming at the interface method.
 
 gorename rejects any renaming that would create a conflict at the point
