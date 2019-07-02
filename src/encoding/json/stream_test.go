13327f219e5bb0f050eb41f25f9dd07ec3d56f32
Daniel Martí <mvdan@mvdan.cc>
2019-07-02 23:56:41 +0200

    encoding/json: obey SetEscapeHTML in all MarshalJSON cases
    It wasn't obeyed in the case where the MarshalJSON method uses a pointer
    receiver, and the encoder grabs the address of a value to find that
    method. addrMarshalerEncoder is the function that does this work, but it
    ignored opts.escapeHTML.
    
    Here's the before and after of the added test case, which was failing
    before the fix. Now the two cases are correct and consistent.
    
    	{"NonPtr":"<str>","Ptr":"\u003cstr\u003e"}
    	{"NonPtr":"<str>","Ptr":"<str>"}
    
    Fixes #32896.
    
    Change-Id: Idc53077ece074973558bd3bb5ad036380db0d02c
    Reviewed-on: https://go-review.googlesource.com/c/go/+/184757
    Run-TryBot: Daniel Martí <mvdan@mvdan.cc>
    TryBot-Result: Gobot Gobot <gobot@golang.org>
    Reviewed-by: Emmanuel Odeke <emm.odeke@gmail.com>
    Reviewed-by: Caleb Spare <cespare@gmail.com>

--- src/encoding/json/stream_test.go
+++ src/encoding/json/stream_test.go
@@ -90,6 +90,18 @@ func TestEncoderIndent(t *testing.T) {
 	}
 }
 
+type strMarshaler string
+
+func (s strMarshaler) MarshalJSON() ([]byte, error) {
+	return []byte(s), nil
+}
+
+type strPtrMarshaler string
+
+func (s *strPtrMarshaler) MarshalJSON() ([]byte, error) {
+	return []byte(*s), nil
+}
+
 func TestEncoderSetEscapeHTML(t *testing.T) {
 	var c C
 	var ct CText
@@ -97,6 +109,15 @@ func TestEncoderSetEscapeHTML(t *testing.T) {
 		Valid   int `json:"<>&#! "`
 		Invalid int `json:"\\"`
 	}
+
+	// This case is particularly interesting, as we force the encoder to
+	// take the address of the Ptr field to use its MarshalJSON method. This
+	// is why the '&' is important.
+	marshalerStruct := &struct {
+		NonPtr strMarshaler
+		Ptr    strPtrMarshaler
+	}{`"<str>"`, `"<str>"`}
+
 	for _, tt := range []struct {
 		name       string
 		v          interface{}
@@ -110,6 +131,11 @@ func TestEncoderSetEscapeHTML(t *testing.T) {
 			"tagStruct", tagStruct,
 			`{"\u003c\u003e\u0026#! ":0,"Invalid":0}`,
 			`{"<>&#! ":0,"Invalid":0}`,
+		},
+		{
+			`"<str>"`, marshalerStruct,
+			`{"NonPtr":"\u003cstr\u003e","Ptr":"\u003cstr\u003e"}`,
+			`{"NonPtr":"<str>","Ptr":"<str>"}`,
 		},
 	} {
 		var buf bytes.Buffer
