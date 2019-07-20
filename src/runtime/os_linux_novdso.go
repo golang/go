03a58ac2e4e036a4f61227cfd013082871e92863
Wang Xuerui <git@xen0n.name>
2019-07-20 11:27:50 +0800

    runtime: use vDSO clock_gettime on linux/mips64x
    Speed up nanotime1 and walltime1 on MIPS64 with vDSO, just like the
    other vDSO-enabled targets.
    
    Benchmark numbers on Loongson 3A3000 (GOARCH=mips64le, 1.4GHz) against
    current master:
    
    benchmark                old ns/op     new ns/op     delta
    BenchmarkNow             868           293           -66.24%
    BenchmarkNowUnixNano     851           296           -65.22%

--- src/runtime/os_linux_novdso.go
+++ src/runtime/os_linux_novdso.go
@@ -3,7 +3,7 @@
 // license that can be found in the LICENSE file.
 
 // +build linux
-// +build !386,!amd64,!arm,!arm64,!ppc64,!ppc64le
+// +build !386,!amd64,!arm,!arm64,!mips64,!mips64le,!ppc64,!ppc64le
 
 package runtime
 
