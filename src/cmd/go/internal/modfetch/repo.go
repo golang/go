2b8b34aa30b69d66d48aedc5ffb4a9f26f37988f
Bryan C. Mills <bcmills@google.com>
2019-08-08 18:09:54 -0400

    cmd/go: query each path only once in 'go get'
    If we don't know whether a path is a module path or a package path,
    previously we would first try a module query for it, then fall back to
    a package query.
    
    If we are using a sequence of proxies with fallback (as will be the
    default in Go 1.13), and the path is not actually a module path, that
    initial module query will fail against the first proxy, then
    immediately fall back to the next proxy in the sequence — even if the
    query could have been satisfied by some other (prefix) module
    available from the first proxy.
    
    Instead, we now query the requested path as only one kind of path.
    If we query it as a package path but it turns out to only exist as a
    module, we can detect that as a PackageNotInModuleError with an
    appropriate module path — we do not need to issue a second query to
    classify it.
    
    Fixes #31785
    
    Change-Id: I581d44279196e41d1fed27ec25489e75d62654e3
    Reviewed-on: https://go-review.googlesource.com/c/go/+/189517
    Run-TryBot: Bryan C. Mills <bcmills@google.com>
    TryBot-Result: Gobot Gobot <gobot@golang.org>
    Reviewed-by: Jay Conrod <jayconrod@google.com>

--- src/cmd/go/internal/modfetch/repo.go
+++ src/cmd/go/internal/modfetch/repo.go
@@ -240,7 +240,7 @@ func lookup(proxy, path string) (r Repo, err error) {
 
 var (
 	errModVendor       = errors.New("module lookup disabled by -mod=vendor")
-	errProxyOff        = errors.New("module lookup disabled by GOPROXY=off")
+	errProxyOff        = notExistError("module lookup disabled by GOPROXY=off")
 	errNoproxy   error = notExistError("disabled by GOPRIVATE/GONOPROXY")
 	errUseProxy  error = notExistError("path does not match GOPRIVATE/GONOPROXY")
 )
