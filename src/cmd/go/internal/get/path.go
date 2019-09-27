db87c9f2d0cc0e695bc7686bdcd04ea075b28deb
Bryan C. Mills <bcmills@google.com>
2019-09-27 13:03:07 -0400

    cmd/go: do not reject internal double-dots in path elements
    The relative path element ".." is already rejected
    by the checks for leading and trailing dots.
    
    Fixes #27299
    
    Change-Id: Ia8ab543c93288cdc0615abd6d22521d44bc56d72
    Reviewed-on: https://go-review.googlesource.com/c/go/+/197720
    Run-TryBot: Bryan C. Mills <bcmills@google.com>
    Reviewed-by: Jay Conrod <jayconrod@google.com>
    TryBot-Result: Gobot Gobot <gobot@golang.org>

--- src/cmd/go/internal/get/path.go
+++ src/cmd/go/internal/get/path.go
@@ -44,9 +44,6 @@ func checkPath(path string, fileName bool) error {
 	if path[0] == '-' {
 		return fmt.Errorf("leading dash")
 	}
-	if strings.Contains(path, "..") {
-		return fmt.Errorf("double dot")
-	}
 	if strings.Contains(path, "//") {
 		return fmt.Errorf("double slash")
 	}
