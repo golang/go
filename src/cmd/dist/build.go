eb46432598539cf3d3cfc158efa47f75ce21ab9a
Tobias Klauser <tklauser@distanz.ch>
2019-10-04 11:53:22 +0200

    cmd/dist: add support for freebsd/arm64
    Updates #24715
    
    Change-Id: I110a10a5d5ed4a471f67f35cbcdcbea296c5dcaf
    Reviewed-on: https://go-review.googlesource.com/c/go/+/198542
    Run-TryBot: Tobias Klauser <tobias.klauser@gmail.com>
    TryBot-Result: Gobot Gobot <gobot@golang.org>
    Reviewed-by: Ian Lance Taylor <iant@golang.org>

--- src/cmd/dist/build.go
+++ src/cmd/dist/build.go
@@ -1485,6 +1485,7 @@ var cgoEnabled = map[string]bool{
 	"freebsd/386":     true,
 	"freebsd/amd64":   true,
 	"freebsd/arm":     true,
+	"freebsd/arm64":   false,
 	"illumos/amd64":   true,
 	"linux/386":       true,
 	"linux/amd64":     true,
