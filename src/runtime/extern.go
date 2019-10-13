40b74558771ba9db493728dcaabe43318daf9b97
Austin Clements <austin@google.com>
2019-10-13 11:27:47 -0400

    runtime: add GODEBUG=asyncpreemptoff=1
    This doesn't do anything yet, but it will provide a way to disable
    non-cooperative preemption.
    
    For #10958, #24543.
    
    Change-Id: Ifdef303f103eabd0922ced8d9bebbd5f0aa2cda4
    Reviewed-on: https://go-review.googlesource.com/c/go/+/201757
    Run-TryBot: Austin Clements <austin@google.com>
    TryBot-Result: Gobot Gobot <gobot@golang.org>
    Reviewed-by: Brad Fitzpatrick <bradfitz@golang.org>

--- src/runtime/extern.go
+++ src/runtime/extern.go
@@ -127,6 +127,13 @@ It is a comma-separated list of name=val pairs setting these named variables:
 	IDs will refer to the ID of the goroutine at the time of creation; it's possible for this
 	ID to be reused for another goroutine. Setting N to 0 will report no ancestry information.
 
+	asyncpreemptoff: asyncpreemptoff=1 disables signal-based
+	asynchronous goroutine preemption. This makes some loops
+	non-preemptible for long periods, which may delay GC and
+	goroutine scheduling. This is useful for debugging GC issues
+	because it also disables the conservative stack scanning used
+	for asynchronously preempted goroutines.
+
 The net, net/http, and crypto/tls packages also refer to debugging variables in GODEBUG.
 See the documentation for those packages for details.
 
