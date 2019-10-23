7ad8def9cee8170fc2572b0b79d3ba07b4334256
Jason A. Donenfeld <Jason@zx2c4.com>
2019-10-23 13:08:46 +0200

    [release-branch.go1.13] syscall: reenable sysctl on iOS
    This was disabled due to a report that the App Store rejects the symbol
    __sysctl. However, we use the sysctl symbol, which is fine. The __sysctl
    symbol is used by x/sys/unix, which needs fixing instead. So, this
    commit reenables sysctl on iOS, so that things like net.InterfaceByName
    can work again.
    
    This reverts CL 193843, CL 193844, CL 193845, and CL 193846.
    
    Fixes #35105
    Updates #35101
    Updates #34133
    Updates #35103
    
    Change-Id: Ib8eb9f87b81db24965b0de29d99eb52887c7c60a
    Reviewed-on: https://go-review.googlesource.com/c/go/+/202778
    Run-TryBot: Jason A. Donenfeld <Jason@zx2c4.com>
    Reviewed-by: David Crawshaw <crawshaw@golang.org>
    Reviewed-by: Brad Fitzpatrick <bradfitz@golang.org>
    TryBot-Result: Gobot Gobot <gobot@golang.org>
    Reviewed-on: https://go-review.googlesource.com/c/go/+/202779
    Reviewed-by: Elias Naur <mail@eliasnaur.com>
    Reviewed-by: Ian Lance Taylor <iant@golang.org>

--- src/os/os_test.go
+++ src/os/os_test.go
@@ -1521,9 +1521,6 @@ func testWindowsHostname(t *testing.T, hostname string) {
 }
 
 func TestHostname(t *testing.T) {
-	if runtime.GOOS == "darwin" && (runtime.GOARCH == "arm" || runtime.GOARCH == "arm64") {
-		t.Skipf("sysctl is not supported on iOS")
-	}
 	hostname, err := Hostname()
 	if err != nil {
 		t.Fatal(err)
