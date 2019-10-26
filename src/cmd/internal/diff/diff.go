e39143878eb3c7a6f14b9b046eb293af91784127
Mikhail Fesenko <proggga@gmail.com>
2019-10-26 11:30:15 +0300

    Small doc fix, change returns to return nil, err

--- src/cmd/internal/diff/diff.go
+++ src/cmd/internal/diff/diff.go
@@ -2,8 +2,8 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// Package diff implements Diff function that compares two byte[]
-// using linux 'diff' tool in temp files
+// Package diff implements Diff function that compares two []byte
+// using 'diff' tool in temp files.
 package diff
 
 import (
@@ -13,18 +13,18 @@ import (
 	"runtime"
 )
 
-// Returns diff of two arrays of bytes in diff tool format
+// Returns diff of two arrays of bytes in diff tool format.
 func Diff(prefix string, b1, b2 []byte) ([]byte, error) {
-	data := make([]byte, 0)
+
 	f1, err := writeTempFile("", prefix, b1)
 	if err != nil {
-		return data, err
+		return nil, err
 	}
 	defer os.Remove(f1)
 
 	f2, err := writeTempFile("", prefix, b2)
 	if err != nil {
-		return data, err
+		return nil, err
 	}
 	defer os.Remove(f2)
 
@@ -33,7 +33,7 @@ func Diff(prefix string, b1, b2 []byte) ([]byte, error) {
 		cmd = "/bin/ape/diff"
 	}
 
-	data, err = exec.Command(cmd, "-u", f1, f2).CombinedOutput()
+	data, err := exec.Command(cmd, "-u", f1, f2).CombinedOutput()
 	if len(data) > 0 {
 		// diff exits with a non-zero status when the files don't match.
 		// Ignore that failure as long as we get output.
