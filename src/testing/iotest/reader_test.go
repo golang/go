f5578343a7911fdf5dba1059eff823698a6d5d2e
Pantelis Sampaziotis <psampaz@gmail.com>
2019-11-05 07:16:09 +0200

    Update OneByteReader tests

--- src/testing/iotest/reader_test.go
+++ src/testing/iotest/reader_test.go
@@ -10,32 +10,55 @@ import (
 	"testing"
 )
 
-var oneByteReaderTests = []struct {
-	data []byte
-	p    []byte
-	n    int
-}{
-	{[]byte(""), []byte(""), 0},
-	{[]byte("h"), []byte("h"), 1},
-	{[]byte("hello, world"), []byte("h"), 1},
+func TestOneByteReader_nonEmptyReader(t *testing.T) {
+	msg := "Hello, World!"
+	buf := new(bytes.Buffer)
+	buf.WriteString(msg)
+
+	obr := OneByteReader(buf)
+	var b []byte
+	n, err := obr.Read(b)
+	if err != nil || n != 0 {
+		t.Errorf("Empty buffer read returned n=%d err=%v", n, err)
+	}
+
+	b = make([]byte, 3)
+	// Read from obr until EOF.
+	got := new(bytes.Buffer)
+	for i := 0; ; i++ {
+		n, err = obr.Read(b)
+		if err != nil {
+			break
+		}
+		if g, w := n, 1; g != w {
+			t.Errorf("Iteration #%d read %d bytes, want %d", i, g, w)
+		}
+		got.Write(b[:n])
+	}
+	if g, w := err, io.EOF; g != w {
+		t.Errorf("Unexpected error after reading all bytes\n\tGot:  %v\n\tWant: %v", g, w)
+	}
+	if g, w := got.String(), "Hello, World!"; g != w {
+		t.Errorf("Read mismatch\n\tGot:  %q\n\tWant: %q", g, w)
+	}
 }
 
-func TestOneByteReader(t *testing.T) {
-	for _, tt := range oneByteReaderTests {
-		r := OneByteReader(bytes.NewReader(tt.data))
-		p := make([]byte, len(tt.data))
-		n, err := r.Read(p)
-		if err != nil {
-			t.Error(err)
-		}
-		if n != tt.n {
-			t.Errorf("read %d, but expected to have read %d bytes", n, tt.n)
-		}
-		got := p[:n]
-		want := tt.p
-		if !bytes.Equal(got, want) {
-			t.Errorf("got %q, expected %q", got, want)
-		}
+func TestOneByteReader_emptyReader(t *testing.T) {
+	r := new(bytes.Buffer)
+
+	obr := OneByteReader(r)
+	var b []byte
+	if n, err := obr.Read(b); err != nil || n != 0 {
+		t.Errorf("Empty buffer read returned n=%d err=%v", n, err)
+	}
+
+	b = make([]byte, 5)
+	n, err := obr.Read(b)
+	if g, w := err, io.EOF; g != w {
+		t.Errorf("Error mismatch\n\tGot:  %v\n\tWant: %v", g, w)
+	}
+	if g, w := n, 0; g != w {
+		t.Errorf("Unexpectedly read %d bytes, wanted %d", g, w)
 	}
 }
 
