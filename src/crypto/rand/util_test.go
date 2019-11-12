57fd0cfdab6f44305d0b46265128f3d7dfa49a31
Scott <scott@paragonie.com>
2019-11-12 12:23:48 -0500

    Update util_test.go
    Suggested in https://twitter.com/saleemrash1d/status/1194304117407129601

--- src/crypto/rand/util_test.go
+++ src/crypto/rand/util_test.go
@@ -81,28 +81,29 @@ func TestIntReads(t *testing.T) {
 
 // Test that Int does not bias values
 func testIntUnbiased(t *testing.T) {
-	max := int64(1 << 24);
-	found := false
-	for i := 0; i < 100; i++ {
-		if (found) {
-			break
+	for max := int64(1 << 24); max < int64(1 << 32); max = max << 1 {
+		found := false
+		for i := 0; i < 100; i++ {
+			if (found) {
+				break
+			}
+			t.Run(fmt.Sprintf("i=%d", i), func(t *testing.T) {
+				reader := &countingReader{r: rand.Reader}
+
+				_, err := rand.Int(reader, big.newInt(max))
+				if err != nil {
+					t.Fatalf("Can't generate random value: %d, %v", max, err)
+				}
+
+				if (_.bytes()[0] != 0) {
+					// We've found a result with a nonzero leading byte
+					found := true
+				}
+				if (i >= 99 && !found) {
+					t.Fatalf("Security: Int(reader, %d) returned biased output (100 samples with a leading 0 byte).", max)
+				}
+			})
 		}
-		t.Run(fmt.Sprintf("i=%d", i), func(t *testing.T) {
-			reader := &countingReader{r: rand.Reader}
-			
-			_, err := rand.Int(reader, big.newInt(max))
-			if err != nil {
-				t.Fatalf("Can't generate random value: %d, %v", max, err)
-			}
-			
-			if (_.bytes()[0] != 0) {
-				// We've found a result with a nonzero leading byte
-				found := true
-			}
-			if (i >= 99 && !found) {
-				t.Fatal("Security: Int(reader, %d) returned biased output (100 samples with a leading 0 byte).", max)
-			}
-		})
 	}
 }
 
