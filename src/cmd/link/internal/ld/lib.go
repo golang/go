d3c2b1f17600fadeebf62d65c85baf3bef879e2b
Elias Naur <mail@eliasnaur.com>
2019-11-13 11:57:17 +0100

    cmd/link/internal/ld,misc/cgo/testcshared: don't -fuse-ld=gold on Android
    The NDK is switching to ldd, and will stop including the gold linker.
    
    Change-Id: If74168017c9874134b34010906ab1d94001528b6
    Reviewed-on: https://go-review.googlesource.com/c/go/+/206840
    TryBot-Result: Gobot Gobot <gobot@golang.org>
    Reviewed-by: Ian Lance Taylor <iant@golang.org>

--- src/cmd/link/internal/ld/lib.go
+++ src/cmd/link/internal/ld/lib.go
@@ -1313,7 +1313,7 @@ func (ctxt *Link) hostlink() {
 		// from the beginning of the section (like sym.STYPE).
 		argv = append(argv, "-Wl,-znocopyreloc")
 
-		if ctxt.Arch.InFamily(sys.ARM, sys.ARM64) && (objabi.GOOS == "linux" || objabi.GOOS == "android") {
+		if ctxt.Arch.InFamily(sys.ARM, sys.ARM64) && objabi.GOOS == "linux" {
 			// On ARM, the GNU linker will generate COPY relocations
 			// even with -znocopyreloc set.
 			// https://sourceware.org/bugzilla/show_bug.cgi?id=19962
