933c99c21c042964702b220978210fad6b05e909
Tao Qingyun <qingyunha@gmail.com>
2019-12-02 09:38:30 +0800

    cmd/compile: declare with type for fmtMode constant
    Like FmtFlag constant in fmt.go
    
    Change-Id: I351bcb27095549cf19db531f532ea72d5c682610
    Reviewed-on: https://go-review.googlesource.com/c/go/+/209497
    Run-TryBot: Robert Griesemer <gri@golang.org>
    TryBot-Result: Gobot Gobot <gobot@golang.org>
    Reviewed-by: Robert Griesemer <gri@golang.org>

--- src/cmd/compile/internal/gc/main.go
+++ src/cmd/compile/internal/gc/main.go
@@ -526,7 +526,7 @@ func Main(archInit func(*Arch)) {
 	}
 	types.FmtLeft = int(FmtLeft)
 	types.FmtUnsigned = int(FmtUnsigned)
-	types.FErr = FErr
+	types.FErr = int(FErr)
 	types.Ctxt = Ctxt
 
 	initUniverse()
