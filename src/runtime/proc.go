7a0aa25e555edd901add25a1101e7b145d1f3bd8
徐志强 <shore.cloud@gmail.com>
2019-12-11 13:33:44 +0800

    call osyield directly

--- src/runtime/proc.go
+++ src/runtime/proc.go
@@ -1678,8 +1678,7 @@ func lockextra(nilokay bool) *m {
 	for {
 		old := atomic.Loaduintptr(&extram)
 		if old == locked {
-			yield := osyield
-			yield()
+			osyield()
 			continue
 		}
 		if old == 0 && !nilokay {
@@ -1696,8 +1695,7 @@ func lockextra(nilokay bool) *m {
 		if atomic.Casuintptr(&extram, old, locked) {
 			return (*m)(unsafe.Pointer(old))
 		}
-		yield := osyield
-		yield()
+		osyield()
 		continue
 	}
 }
