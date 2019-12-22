8c5978f193d4bb6443a3bb47dbc1b58366632fe3
awh6al <awh6al@yandex.com>
2019-12-22 22:32:55 +0000

    internal/lsp: make golint happy
    This PR fixes internal/lsp/fuzzy/matcher.go:230:4: should replace skipPenalty += 1 with skipPenalty++ .
    
    Change-Id: I7d5b6c20e25503ea266e26783e68ad92fd8b36e0
    GitHub-Last-Rev: 36fd2c25232f593f905051692e84d8c634fb7a46
    GitHub-Pull-Request: golang/tools#194
    Reviewed-on: https://go-review.googlesource.com/c/tools/+/212400
    Reviewed-by: Rebecca Stambler <rstambler@golang.org>
    Run-TryBot: Rebecca Stambler <rstambler@golang.org>
    TryBot-Result: Gobot Gobot <gobot@golang.org>

--- internal/lsp/fuzzy/matcher.go
+++ internal/lsp/fuzzy/matcher.go
@@ -227,7 +227,7 @@ func (m *Matcher) computeScore(candidate string, candidateLower []byte) int {
 		var skipPenalty int
 		if i == 1 || (i-1) == lastSegStart {
 			// Skipping the start of first or last segment.
-			skipPenalty += 1
+			skipPenalty++
 		}
 
 		for j := 0; j <= pattLen; j++ {
