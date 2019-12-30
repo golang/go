9fb4d21460e13907582de6ab3016d495c0d3fd30
Dan Kortschak <dan@kortschak.io>
2019-12-30 10:41:25 +1030

    tools/internal/lsp/cmd: fix documentation
    Change-Id: Ie21babc8879977fb1a3ef02737dbd24904722c26
    Reviewed-on: https://go-review.googlesource.com/c/tools/+/212579
    Reviewed-by: Rebecca Stambler <rstambler@golang.org>
    Run-TryBot: Rebecca Stambler <rstambler@golang.org>
    TryBot-Result: Gobot Gobot <gobot@golang.org>

--- internal/lsp/cmd/info.go
+++ internal/lsp/cmd/info.go
@@ -22,9 +22,6 @@ type version struct {
 	app *Application
 }
 
-// bug implements the bug command.
-type bug struct{}
-
 func (v *version) Name() string      { return "version" }
 func (v *version) Usage() string     { return "" }
 func (v *version) ShortHelp() string { return "print the gopls version information" }
@@ -33,12 +30,14 @@ func (v *version) DetailedHelp(f *flag.FlagSet) {
 	f.PrintDefaults()
 }
 
-// Run collects some basic information and then prepares an issue ready to
-// be reported.
+// Run prints version information to stdout.
 func (v *version) Run(ctx context.Context, args ...string) error {
 	debug.PrintVersionInfo(os.Stdout, v.app.Verbose, debug.PlainText)
 	return nil
 }
+
+// bug implements the bug command.
+type bug struct{}
 
 func (b *bug) Name() string      { return "bug" }
 func (b *bug) Usage() string     { return "" }
