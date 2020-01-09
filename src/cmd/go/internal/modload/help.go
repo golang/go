4e8d0e8ce2e55dc3dcdd6e43f79fa3513bc6aee5
Jay Conrod <jayconrod@google.com>
2020-01-09 16:22:01 -0500

    cmd/go: remove references to 1.13 in 'go help modules'
    In "Module support" section, there were two mentions of "Go 1.13",
    assuming that's the latest version. Rather than update these to 1.14,
    this CL changes those to "The go command".
    
    Also, a minor change in wording for finding go.mod files.
    
    Change-Id: Id194be9405b540f221464814e71c361a22cc0f55
    Reviewed-on: https://go-review.googlesource.com/c/go/+/214140
    Run-TryBot: Jay Conrod <jayconrod@google.com>
    Reviewed-by: Bryan C. Mills <bcmills@google.com>
    TryBot-Result: Gobot Gobot <gobot@golang.org>

--- src/cmd/go/internal/modload/help.go
+++ src/cmd/go/internal/modload/help.go
@@ -21,14 +21,15 @@ which source files are used in a given build.
 
 Module support
 
-Go 1.13 includes support for Go modules. Module-aware mode is active by default
-whenever a go.mod file is found in, or in a parent of, the current directory.
+The go command includes support for Go modules. Module-aware mode is active
+by default whenever a go.mod file is found in the current directory or in
+any parent directory.
 
 The quickest way to take advantage of module support is to check out your
 repository, create a go.mod file (described in the next section) there, and run
 go commands from within that file tree.
 
-For more fine-grained control, Go 1.13 continues to respect
+For more fine-grained control, the go command continues to respect
 a temporary environment variable, GO111MODULE, which can be set to one
 of three string values: off, on, or auto (the default).
 If GO111MODULE=on, then the go command requires the use of modules,
