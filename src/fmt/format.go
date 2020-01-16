6ca317c026f5d6563a2deb6d200ea005b4da195b
yah01 <kagaminehuan@live.com>
2020-01-16 12:34:15 +0800

    add comments and fix bug when output 0

--- src/fmt/format.go
+++ src/fmt/format.go
@@ -541,8 +541,10 @@ func (f *fmt) fmtFloat(v float64, size int, verb rune, prec int) {
 			switch num[i] {
 			case '.':
 				hasDecimalPoint = true
+				// Increase digits with sharp flag
+				//because it's reduced once more at '0' when the input is 0.xxx
 				if f.sharp && num[i-1] == '0' &&
-					(num[i-2] < '0' || num[i-2] > '9') {
+					(num[i-2] < '0' || '9' < num[i-2]) {
 					digits++
 				}
 			case 'p', 'P':
@@ -560,6 +562,10 @@ func (f *fmt) fmtFloat(v float64, size int, verb rune, prec int) {
 			}
 		}
 		if !hasDecimalPoint {
+			// Output 0 with sharp flag
+			if f.sharp && num[1] == '0' && len(num) == 2 {
+				digits++
+			}
 			num = append(num, '.')
 		}
 		for digits > 0 {
