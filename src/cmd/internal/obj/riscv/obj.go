1424889921159ed28866dff40b5d54884aaddd9c
Joel Sing <joel@sing.id.au>
2020-01-17 02:51:40 +1100

    cmd/internal/obj/riscv: correctly split immediates for FLW/FLD/FSW/FSD
    The FLW/FLD/FSW/FSD instructions can have immediates that exceed 12-bits and
    therefore cannot be encoded in the RISCV instruction. Handle these as we do
    for other load/store instructions. Also add test coverage for all load/store
    instructions with large immediates.
    
    Fixes compilation issue reported by Carlos Eduardo de Paula.
    
    Updates #27532
    
    Change-Id: Ifa62f19493b3acaba5a90ac31d2df209a3afea81
    Reviewed-on: https://go-review.googlesource.com/c/go/+/215037
    Reviewed-by: Carlos Eduardo de Paula <me@carlosedp.com>
    Reviewed-by: Cherry Zhang <cherryyz@google.com>
    Run-TryBot: Cherry Zhang <cherryyz@google.com>
    TryBot-Result: Gobot Gobot <gobot@golang.org>

--- src/cmd/internal/obj/riscv/obj.go
+++ src/cmd/internal/obj/riscv/obj.go
@@ -716,11 +716,7 @@ func preprocess(ctxt *obj.Link, cursym *obj.LSym, newprog obj.ProgAlloc) {
 
 		// <load> $imm, REG, TO (load $imm+(REG), TO)
 		// <store> $imm, REG, TO (store $imm+(TO), REG)
-		case ALD, ALB, ALH, ALW, ALBU, ALHU, ALWU,
-			ASD, ASB, ASH, ASW:
-			// LUI $high, TMP
-			// ADDI $low, TMP, TMP
-			q := *p
+		case ALB, ALH, ALW, ALD, ALBU, ALHU, ALWU, AFLW, AFLD, ASB, ASH, ASW, ASD, AFSW, AFSD:
 			low, high, err := Split32BitImmediate(p.From.Offset)
 			if err != nil {
 				ctxt.Diag("%v: constant %d too large", p, p.From.Offset)
@@ -729,8 +725,9 @@ func preprocess(ctxt *obj.Link, cursym *obj.LSym, newprog obj.ProgAlloc) {
 				break // no need to split
 			}
 
+			q := *p
 			switch q.As {
-			case ALD, ALB, ALH, ALW, ALBU, ALHU, ALWU:
+			case ALB, ALH, ALW, ALD, ALBU, ALHU, ALWU, AFLW, AFLD:
 				// LUI $high, TMP
 				// ADD TMP, REG, TMP
 				// <load> $low, TMP, TO
@@ -752,7 +749,7 @@ func preprocess(ctxt *obj.Link, cursym *obj.LSym, newprog obj.ProgAlloc) {
 				p.From = obj.Addr{Type: obj.TYPE_CONST, Offset: low}
 				p.Reg = REG_TMP
 
-			case ASD, ASB, ASH, ASW:
+			case ASB, ASH, ASW, ASD, AFSW, AFSD:
 				// LUI $high, TMP
 				// ADD TMP, TO, TMP
 				// <store> $low, REG, TMP
