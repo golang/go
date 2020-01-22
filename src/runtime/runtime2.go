895b7c85addfffe19b66d8ca71c31799d6e55990
Ian Lance Taylor <iant@golang.org>
2020-01-21 16:50:56 -0800

    runtime: don't skip checkTimers if we would clear deleted timers
    The timers code used to have a problem: if code started and stopped a
    lot of timers, as would happen with, for example, lots of calls to
    context.WithTimeout, then it would steadily use memory holding timers
    that had stopped but not been removed from the timer heap.
    That problem was fixed by CL 214299, which would remove all deleted
    timers whenever they got to be more than 1/4 of the total number of
    timers on the heap.
    
    The timers code had a different problem: if there were some idle P's,
    the running P's would have lock contention trying to steal their timers.
    That problem was fixed by CL 214185, which only acquired the timer lock
    if the next timer was ready to run or there were some timers to adjust.
    
    Unfortunately, CL 214185 partially undid 214299, in that we could now
    accumulate an increasing number of deleted timers while there were no
    timers ready to run. This CL restores the 214299 behavior, by checking
    whether there are lots of deleted timers without acquiring the lock.
    
    This is a performance issue to consider for the 1.14 release.
    
    Change-Id: I13c980efdcc2a46eb84882750c39e3f7c5b2e7c3
    Reviewed-on: https://go-review.googlesource.com/c/go/+/215722
    Run-TryBot: Ian Lance Taylor <iant@golang.org>
    Reviewed-by: Austin Clements <austin@google.com>
    TryBot-Result: Gobot Gobot <gobot@golang.org>

--- src/runtime/runtime2.go
+++ src/runtime/runtime2.go
@@ -649,13 +649,17 @@ type p struct {
 	// Must hold timersLock to access.
 	timers []*timer
 
+	// Number of timers in P's heap.
+	// Modified using atomic instructions.
+	numTimers uint32
+
 	// Number of timerModifiedEarlier timers on P's heap.
 	// This should only be modified while holding timersLock,
 	// or while the timer status is in a transient state
 	// such as timerModifying.
 	adjustTimers uint32
 
-	// Number of timerDeleted times in P's heap.
+	// Number of timerDeleted timers in P's heap.
 	// Modified using atomic instructions.
 	deletedTimers uint32
 
