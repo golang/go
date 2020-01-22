82a2f825b7163eafebbf9b8640ef6406e4dda9a9
Joel Sing <joel@sing.id.au>
2020-01-23 03:42:07 +1100

    runtime: add missing code for linux/riscv64
    Makes linux/riscv64 runtime buildable.
    
    Updates #27532
    
    Change-Id: I91bcadaaecb8ff3ffd70fcb437b2b6e4bbe11eda
    Reviewed-on: https://go-review.googlesource.com/c/go/+/215839
    Run-TryBot: Joel Sing <joel@sing.id.au>
    TryBot-Result: Gobot Gobot <gobot@golang.org>
    Reviewed-by: Cherry Zhang <cherryyz@google.com>

--- src/runtime/mpagealloc_64bit.go
+++ src/runtime/mpagealloc_64bit.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// +build amd64 !darwin,arm64 mips64 mips64le ppc64 ppc64le s390x
+// +build amd64 !darwin,arm64 mips64 mips64le ppc64 ppc64le riscv64 s390x
 
 // See mpagealloc_32bit.go for why darwin/arm64 is excluded here.
 
