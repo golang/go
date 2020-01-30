2b939d3a3a164d6652ea92837cb6c2e6b92ab201
Gurjus Bhasin <48339289+gsbhasin123@users.noreply.github.com>
2020-01-30 15:55:09 -0800

    Add `strings.Reverse()`
    Closes #36887

--- src/strings/strings.go
+++ src/strings/strings.go
@@ -1127,3 +1127,11 @@ func indexRabinKarp(s, substr string) int {
 	}
 	return -1
 }
+
+func Reverse(s string) string {
+	r := []rune(s)
+	for i, j := 0, len(r)-1; i < len(r)/2; i, j = i+1, j-1 {
+		r[i], r[j] = r[j], r[i]
+	}
+	return string(r)
+}
