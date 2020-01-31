63e00809dd79b3e7428e1f237f96a740854dd44f
Gurjus Bhasin <48339289+gsbhasin123@users.noreply.github.com>
2020-01-30 16:09:41 -0800

    Update strings.go

--- src/strings/strings.go
+++ src/strings/strings.go
@@ -1127,7 +1127,7 @@ func indexRabinKarp(s, substr string) int {
 	}
 	return -1
 }
-
+// Reverse takes string s and returns it reversed rune-wise (left to right)
 func Reverse(s string) string {
 	r := []rune(s)
 	for i, j := 0, len(r)-1; i < len(r)/2; i, j = i+1, j-1 {
