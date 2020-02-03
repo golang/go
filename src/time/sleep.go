fb4a35f8cc6f32c195619a62a3fc828cb267ce52
Changkun Ou <hi@changkun.us>
2020-02-03 14:42:32 +0100

    time: add Ticker.Reset
    This CL implements Ticker.Reset method in time package.
    
    Benchmark:
    name                 time/op
    TickerReset-12       6.41µs ±10%
    TickerResetNaive-12  95.7µs ±12%
    
    Fixes #33184
    
    Change-Id: I4cbd31796efa012b2a297bb342158f11a4a31fef
    Reviewed-on: https://go-review.googlesource.com/c/go/+/220424
    Run-TryBot: Ian Lance Taylor <iant@golang.org>
    TryBot-Result: Gobot Gobot <gobot@golang.org>
    Reviewed-by: Ian Lance Taylor <iant@golang.org>

--- src/time/sleep.go
+++ src/time/sleep.go
@@ -39,6 +39,7 @@ func when(d Duration) int64 {
 func startTimer(*runtimeTimer)
 func stopTimer(*runtimeTimer) bool
 func resetTimer(*runtimeTimer, int64)
+func modTimer(t *runtimeTimer, when, period int64, f func(interface{}, uintptr), arg interface{}, seq uintptr)
 
 // The Timer type represents a single event.
 // When the Timer expires, the current time will be sent on C,
