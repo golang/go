a0c9fb6bd359f111f19176ff176244b91a3e7eaa
Keith Randall <khr@golang.org>
2020-02-13 10:24:41 -0800

    hash/maphash: mention the results are 64-bit integers
    Change-Id: I0d2ba52d79c34d77d475ec8d673286d0e56b826b
    Reviewed-on: https://go-review.googlesource.com/c/go/+/219340
    Reviewed-by: Alan Donovan <adonovan@google.com>

--- src/hash/maphash/maphash.go
+++ src/hash/maphash/maphash.go
@@ -5,7 +5,7 @@
 // Package maphash provides hash functions on byte sequences.
 // These hash functions are intended to be used to implement hash tables or
 // other data structures that need to map arbitrary strings or byte
-// sequences to a uniform distribution of integers.
+// sequences to a uniform distribution on unsigned 64-bit integers.
 //
 // The hash functions are collision-resistant but not cryptographically secure.
 // (See crypto/sha256 and crypto/sha512 for cryptographic use.)
