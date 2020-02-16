f3181fd50cef0245fa3cd2cd3d1dc1868a62a2eb
decanus <dean.eigenmann@icloud.com>
2020-02-16 17:41:26 +0100

    fixes less to work if duplicate values in array

--- src/sort/sort.go
+++ src/sort/sort.go
@@ -269,7 +269,7 @@ func IsSorted(data Interface) bool {
 type IntSlice []int
 
 func (p IntSlice) Len() int           { return len(p) }
-func (p IntSlice) Less(i, j int) bool { return p[i] < p[j] }
+func (p IntSlice) Less(i, j int) bool { return p[i] <= p[j] }
 func (p IntSlice) Swap(i, j int)      { p[i], p[j] = p[j], p[i] }
 
 // Sort is a convenience method.
