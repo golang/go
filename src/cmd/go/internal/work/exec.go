8992f7c4597dcc60b91e8b088fb31b49b9b55496
GrigoriyMikhalkin <grigoriymikhalkin@gmail.com>
2020-02-26 15:02:36 +0000

    cmd/go: escape $WORK in ccompile when -n is set
    Fixes #37012
    
    Change-Id: I169807788c022042a2b5cf04f67ae41a7325fbcf
    GitHub-Last-Rev: 9e77301c7ab3125a6f21f2c9cd998503976a5d5d
    GitHub-Pull-Request: golang/go#37289
    Reviewed-on: https://go-review.googlesource.com/c/go/+/219919
    Reviewed-by: Bryan C. Mills <bcmills@google.com>
    Run-TryBot: Bryan C. Mills <bcmills@google.com>
    TryBot-Result: Gobot Gobot <gobot@golang.org>

--- src/cmd/go/internal/work/exec.go
+++ src/cmd/go/internal/work/exec.go
@@ -2129,9 +2129,8 @@ func (b *Builder) gfortran(a *Action, p *load.Package, workdir, out string, flag
 func (b *Builder) ccompile(a *Action, p *load.Package, outfile string, flags []string, file string, compiler []string) error {
 	file = mkAbs(p.Dir, file)
 	desc := p.ImportPath
-	if !filepath.IsAbs(outfile) {
-		outfile = filepath.Join(p.Dir, outfile)
-	}
+	outfile = mkAbs(p.Dir, outfile)
+
 	output, err := b.runOut(a, filepath.Dir(file), b.cCompilerEnv(), compiler, flags, "-o", outfile, "-c", filepath.Base(file))
 	if len(output) > 0 {
 		// On FreeBSD 11, when we pass -g to clang 3.8 it
