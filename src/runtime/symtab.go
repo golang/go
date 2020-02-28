5194be8a1add260a8c7e7f217df33379e3911c96
Michael Pratt <mpratt@google.com>
2020-02-28 14:16:41 -0500

    runtime/pprof: expand final stack frame to avoid truncation
    When generating stacks, the runtime automatically expands inline
    functions to inline all inline frames in the stack. However, due to the
    stack size limit, the final frame may be truncated in the middle of
    several inline frames at the same location.
    
    As-is, we assume that the final frame is a normal function, and emit and
    cache a Location for it. If we later receive a complete stack frame, we
    will first use the cached Location for the inlined function and then
    generate a new Location for the "caller" frame, in violation of the
    pprof requirement to merge inlined functions into the same Location.
    
    As a result, we:
    
    1. Nondeterministically may generate a profile with the different stacks
    combined or split, depending on which is encountered first. This is
    particularly problematic when performing a diff of profiles.
    
    2. When split stacks are generated, we lose the inlining information.
    
    We avoid both of these problems by performing a second expansion of the
    last stack frame to recover additional inline frames that may have been
    lost. This expansion is a bit simpler than the one done by the runtime
    because we don't have to handle skipping, and we know that the last
    emitted frame is not an elided wrapper, since it by definition is
    already included in the stack.
    
    Fixes #37446
    
    Change-Id: If3ca2af25b21d252cf457cc867dd932f107d4c61
    Reviewed-on: https://go-review.googlesource.com/c/go/+/221577
    Run-TryBot: Michael Pratt <mpratt@google.com>
    TryBot-Result: Gobot Gobot <gobot@golang.org>
    Reviewed-by: Heschi Kreinick <heschi@google.com>
    Reviewed-by: Keith Randall <khr@golang.org>
    Reviewed-by: Hyang-Ah Hana Kim <hyangah@gmail.com>

--- src/runtime/symtab.go
+++ src/runtime/symtab.go
@@ -148,6 +148,59 @@ func (ci *Frames) Next() (frame Frame, more bool) {
 	return
 }
 
+// runtime_expandFinalInlineFrame expands the final pc in stk to include all
+// "callers" if pc is inline.
+//
+//go:linkname runtime_expandFinalInlineFrame runtime/pprof.runtime_expandFinalInlineFrame
+func runtime_expandFinalInlineFrame(stk []uintptr) []uintptr {
+	pc := stk[len(stk)-1]
+	tracepc := pc - 1
+
+	f := findfunc(tracepc)
+	if !f.valid() {
+		// Not a Go function.
+		return stk
+	}
+
+	inldata := funcdata(f, _FUNCDATA_InlTree)
+	if inldata == nil {
+		// Nothing inline in f.
+		return stk
+	}
+
+	// Treat the previous func as normal. We haven't actually checked, but
+	// since this pc was included in the stack, we know it shouldn't be
+	// elided.
+	lastFuncID := funcID_normal
+
+	// Remove pc from stk; we'll re-add it below.
+	stk = stk[:len(stk)-1]
+
+	// See inline expansion in gentraceback.
+	var cache pcvalueCache
+	inltree := (*[1 << 20]inlinedCall)(inldata)
+	for {
+		ix := pcdatavalue(f, _PCDATA_InlTreeIndex, tracepc, &cache)
+		if ix < 0 {
+			break
+		}
+		if inltree[ix].funcID == funcID_wrapper && elideWrapperCalling(lastFuncID) {
+			// ignore wrappers
+		} else {
+			stk = append(stk, pc)
+		}
+		lastFuncID = inltree[ix].funcID
+		// Back up to an instruction in the "caller".
+		tracepc = f.entry + uintptr(inltree[ix].parentPc)
+		pc = tracepc + 1
+	}
+
+	// N.B. we want to keep the last parentPC which is not inline.
+	stk = append(stk, pc)
+
+	return stk
+}
+
 // expandCgoFrames expands frame information for pc, known to be
 // a non-Go function, using the cgoSymbolizer hook. expandCgoFrames
 // returns nil if pc could not be expanded.
