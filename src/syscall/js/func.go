246a4991660927f88f48290580e96b15c16663c1
Torben Schinke <torben.schinke@neotos.de>
2020-03-01 21:06:26 +0100

    syscall/js: improve documentation of js.FuncOf
    The existing documentation is improved to be more
    explicit about the lifecycle and its consequences.
    
    Fixes #34324

--- src/syscall/js/func.go
+++ src/syscall/js/func.go
@@ -31,12 +31,12 @@ type Func struct {
 // Invoking the wrapped Go function from JavaScript will
 // pause the event loop and spawn a new goroutine.
 // Other wrapped functions which are triggered during a call from Go to JavaScript
-// gets executed on the same goroutine.
+// get executed on the same goroutine.
 //
 // As a consequence, if one wrapped function blocks, JavaScript's event loop
-// is blocked until that function returns. Hence, calling any async JavaScript 
-// API, which requires the event loop, like fetch (http.Client), will cause an 
-// immediate deadlock. Therefore a blocking function should explicitly start a 
+// is blocked until that function returns. Hence, calling any async JavaScript
+// API, which requires the event loop, like fetch (http.Client), will cause an
+// immediate deadlock. Therefore a blocking function should explicitly start a
 // new goroutine.
 //
 // Func.Release must be called to free up resources when the function will not be used any more.
