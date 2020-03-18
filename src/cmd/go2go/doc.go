01b52127976c428fa9b3242b0541d573938bf338
Ian Lance Taylor <iant@golang.org>
2020-03-18 16:06:29 -0700

    cmd/go2go: point to testdata/go2path in package docs
    Change-Id: I94857bb943503d1107b814e5111ccd6512c4a477

--- src/cmd/go2go/doc.go
+++ src/cmd/go2go/doc.go
@@ -20,9 +20,14 @@
 //
 // Non-local imported packages will be first looked up using the GO2PATH
 // environment variable, which should point to a GOPATH-like directory.
-// For example, import "x" will first look for $GO2PATH/src/x.
-// If not found in GO2PATH, imports will be looked up in the usual way.
-// If an import includes .go2 files, it will be translated into .go files.
+// For example, import "x" will first look for GO2PATHDIR/src/x,
+// for each colon-separated component in GO2PATH. If not found in GO2PATH,
+// imports will be looked up in the usual way. If an import includes
+// .go2 files, they will be translated into .go files.
+//
+// There is a sample GO2PATH in cmd/go2go/testdata/go2path. It provides
+// several packages that serve as examples of using generics, and may
+// be useful in experimenting with your own generic code.
 //
 // Translation into standard Go requires generating Go code with mangled names.
 // The mangled names will always include Odia (Oriya) digits, such as ୦ and ୮.
