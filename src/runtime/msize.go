4bc5a5b8946e9d8d362fe7107462dec9f22338b5
Austin Clements <austin@google.com>
2020-03-20 11:37:54 -0400

    runtime: use divRoundUp
    There are a handful of places where the runtime wants to round up the
    result of a division. We just introduced a helper to do this. This CL
    replaces all of the hand-coded round-ups (that I could find) with this
    helper.
    
    Change-Id: I465d152157ff0f3cad40c0aa57491e4f2de510ad
    Reviewed-on: https://go-review.googlesource.com/c/go/+/224385
    Run-TryBot: Austin Clements <austin@google.com>
    TryBot-Result: Gobot Gobot <gobot@golang.org>
    Reviewed-by: Cherry Zhang <cherryyz@google.com>

--- src/runtime/msize.go
+++ src/runtime/msize.go
@@ -13,9 +13,9 @@ package runtime
 func roundupsize(size uintptr) uintptr {
 	if size < _MaxSmallSize {
 		if size <= smallSizeMax-8 {
-			return uintptr(class_to_size[size_to_class8[(size+smallSizeDiv-1)/smallSizeDiv]])
+			return uintptr(class_to_size[size_to_class8[divRoundUp(size, smallSizeDiv)]])
 		} else {
-			return uintptr(class_to_size[size_to_class128[(size-smallSizeMax+largeSizeDiv-1)/largeSizeDiv]])
+			return uintptr(class_to_size[size_to_class128[divRoundUp(size-smallSizeMax, largeSizeDiv)]])
 		}
 	}
 	if size+_PageSize < size {
