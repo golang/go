faee2dfe70556780739721b421722263c7ee6d9f
Katie Hockman <katie@golang.org>
2020-03-23 10:11:43 -0400

    crypto: implement Hash.String
    Fixes #33430
    
    Change-Id: I323323b3136dd7b408005c3bb5ea05e3b566bd38
    Reviewed-on: https://go-review.googlesource.com/c/go/+/224937
    Run-TryBot: Katie Hockman <katie@golang.org>
    TryBot-Result: Gobot Gobot <gobot@golang.org>
    Reviewed-by: Filippo Valsorda <filippo@golang.org>

--- src/crypto/crypto.go
+++ src/crypto/crypto.go
@@ -20,6 +20,51 @@ func (h Hash) HashFunc() Hash {
 	return h
 }
 
+func (h Hash) String() string {
+	switch h {
+	case MD4:
+		return "MD4"
+	case MD5:
+		return "MD5"
+	case SHA1:
+		return "SHA-1"
+	case SHA224:
+		return "SHA-224"
+	case SHA256:
+		return "SHA-256"
+	case SHA384:
+		return "SHA-384"
+	case SHA512:
+		return "SHA-512"
+	case MD5SHA1:
+		return "MD5+SHA1"
+	case RIPEMD160:
+		return "RIPEMD-160"
+	case SHA3_224:
+		return "SHA3-224"
+	case SHA3_256:
+		return "SHA3-256"
+	case SHA3_384:
+		return "SHA3-384"
+	case SHA3_512:
+		return "SHA3-512"
+	case SHA512_224:
+		return "SHA-512/224"
+	case SHA512_256:
+		return "SHA-512/256"
+	case BLAKE2s_256:
+		return "BLAKE2s-256"
+	case BLAKE2b_256:
+		return "BLAKE2b-256"
+	case BLAKE2b_384:
+		return "BLAKE2b-384"
+	case BLAKE2b_512:
+		return "BLAKE2b-512"
+	default:
+		return "unknown hash value " + strconv.Itoa(int(h))
+	}
+}
+
 const (
 	MD4         Hash = 1 + iota // import golang.org/x/crypto/md4
 	MD5                         // import crypto/md5
