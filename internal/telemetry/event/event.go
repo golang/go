063d392fe0837e006c90a9d000396ec205ce0ab4
Ian Cottrell <iancottrell@google.com>
2020-03-24 21:11:55 -0400

    internal/telemetry: normalize the event reciever names to all use ev
    Change-Id: Ie622d8d5c4f1c7c272400e549981cd182fe0ab67
    Reviewed-on: https://go-review.googlesource.com/c/tools/+/225377
    Run-TryBot: Ian Cottrell <iancottrell@google.com>
    TryBot-Result: Gobot Gobot <gobot@golang.org>
    Reviewed-by: Robert Findley <rfindley@google.com>

--- internal/telemetry/event/event.go
+++ internal/telemetry/event/event.go
@@ -30,26 +30,26 @@ type Event struct {
 	tags []Tag
 }
 
-func (e Event) IsLog() bool       { return e.typ == LogType }
-func (e Event) IsEndSpan() bool   { return e.typ == EndSpanType }
-func (e Event) IsStartSpan() bool { return e.typ == StartSpanType }
-func (e Event) IsLabel() bool     { return e.typ == LabelType }
-func (e Event) IsDetach() bool    { return e.typ == DetachType }
-func (e Event) IsRecord() bool    { return e.typ == RecordType }
+func (ev Event) IsLog() bool       { return ev.typ == LogType }
+func (ev Event) IsEndSpan() bool   { return ev.typ == EndSpanType }
+func (ev Event) IsStartSpan() bool { return ev.typ == StartSpanType }
+func (ev Event) IsLabel() bool     { return ev.typ == LabelType }
+func (ev Event) IsDetach() bool    { return ev.typ == DetachType }
+func (ev Event) IsRecord() bool    { return ev.typ == RecordType }
 
-func (e Event) Format(f fmt.State, r rune) {
-	if !e.At.IsZero() {
-		fmt.Fprint(f, e.At.Format("2006/01/02 15:04:05 "))
+func (ev Event) Format(f fmt.State, r rune) {
+	if !ev.At.IsZero() {
+		fmt.Fprint(f, ev.At.Format("2006/01/02 15:04:05 "))
 	}
-	fmt.Fprint(f, e.Message)
-	if e.Error != nil {
+	fmt.Fprint(f, ev.Message)
+	if ev.Error != nil {
 		if f.Flag('+') {
-			fmt.Fprintf(f, ": %+v", e.Error)
+			fmt.Fprintf(f, ": %+v", ev.Error)
 		} else {
-			fmt.Fprintf(f, ": %v", e.Error)
+			fmt.Fprintf(f, ": %v", ev.Error)
 		}
 	}
-	for it := e.Tags(); it.Valid(); it.Advance() {
+	for it := ev.Tags(); it.Valid(); it.Advance() {
 		tag := it.Tag()
 		fmt.Fprintf(f, "\n\t%s = %v", tag.Key.Name(), tag.Value)
 	}
