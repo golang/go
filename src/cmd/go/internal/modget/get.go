44ae94751ab35232f72845c7b885c5338220bca5
Jay Conrod <jayconrod@google.com>
2020-04-01 13:08:41 -0400

    cmd/go: report 'go get' errors for absolute paths outside module root
    'go get' will now check absolute paths without wildcards the same way
    it checks relative paths. modload.DirImportPath may be used for both
    without converting path separators.
    
    Fixes #38038
    
    Change-Id: I453299898ece58f3b5002a5e80021d6bfe815fdd
    Reviewed-on: https://go-review.googlesource.com/c/go/+/226857
    Run-TryBot: Jay Conrod <jayconrod@google.com>
    TryBot-Result: Gobot Gobot <gobot@golang.org>
    Reviewed-by: Bryan C. Mills <bcmills@google.com>
    Reviewed-by: Michael Matloob <matloob@golang.org>

--- src/cmd/go/internal/modget/get.go
+++ src/cmd/go/internal/modget/get.go
@@ -326,15 +326,18 @@ func runGet(cmd *base.Command, args []string) {
 		// patterns like golang.org/x/tools/..., which can't be expanded
 		// during package loading until they're in the build list.
 		switch {
-		case search.IsRelativePath(path):
-			// Relative paths like ../../foo or ../../foo... are restricted to
-			// matching packages in the main module. If the path is explicit and
-			// contains no wildcards (...), check that it is a package in
-			// the main module. If the path contains wildcards but matches no
-			// packages, we'll warn after package loading.
+		case filepath.IsAbs(path) || search.IsRelativePath(path):
+			// Absolute paths like C:\foo and relative paths like ../foo...
+			// are restricted to matching packages in the main module. If the path
+			// is explicit and contains no wildcards (...), check that it is a
+			// package in the main module. If the path contains wildcards but
+			// matches no packages, we'll warn after package loading.
 			if !strings.Contains(path, "...") {
-				pkgPath := modload.DirImportPath(filepath.FromSlash(path))
-				if pkgs := modload.TargetPackages(pkgPath); len(pkgs) == 0 {
+				var pkgs []string
+				if pkgPath := modload.DirImportPath(path); pkgPath != "." {
+					pkgs = modload.TargetPackages(pkgPath)
+				}
+				if len(pkgs) == 0 {
 					abs, err := filepath.Abs(path)
 					if err != nil {
 						abs = path
@@ -520,7 +523,7 @@ func runGet(cmd *base.Command, args []string) {
 					// If the pattern did not match any packages, look up a new module.
 					// If the pattern doesn't match anything on the last iteration,
 					// we'll print a warning after the outer loop.
-					if !search.IsRelativePath(arg.path) && !match.IsLiteral() && arg.path != "all" {
+					if !match.IsLocal() && !match.IsLiteral() && arg.path != "all" {
 						addQuery(&query{querySpec: querySpec{path: arg.path, vers: arg.vers}, arg: arg.raw})
 					} else {
 						for _, err := range match.Errs {
