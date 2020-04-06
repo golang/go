db6c7a46d7c8c5281dca29e177df7b16920c37d0
Cherry Zhang <cherryyz@google.com>
2020-04-06 11:39:44 -0400

    cmd/link: remove darwin/arm test
    The darwin/arm port is removed in Go 1.15. Setting GOOS=darwin
    GOARCH=arm will fail, therefore "go test cmd/link" on macOS will
    fail (in non -short mode). Remove this test point.
    
    Updates #37611.
    
    Change-Id: Ia9531c4b4a6692a0c49153517af9fdddd1f3e0bf
    Reviewed-on: https://go-review.googlesource.com/c/go/+/227341
    Reviewed-by: Ian Lance Taylor <iant@golang.org>

--- src/cmd/link/dwarf_test.go
+++ src/cmd/link/dwarf_test.go
@@ -193,9 +193,7 @@ func TestDWARFiOS(t *testing.T) {
 	}
 	cc := "CC=" + runtime.GOROOT() + "/misc/ios/clangwrap.sh"
 	// iOS doesn't allow unmapped segments, so iOS executables don't have DWARF.
-	testDWARF(t, "", false, cc, "CGO_ENABLED=1", "GOOS=darwin", "GOARCH=arm", "GOARM=7")
 	testDWARF(t, "", false, cc, "CGO_ENABLED=1", "GOOS=darwin", "GOARCH=arm64")
 	// However, c-archive iOS objects have embedded DWARF.
-	testDWARF(t, "c-archive", true, cc, "CGO_ENABLED=1", "GOOS=darwin", "GOARCH=arm", "GOARM=7")
 	testDWARF(t, "c-archive", true, cc, "CGO_ENABLED=1", "GOOS=darwin", "GOARCH=arm64")
 }
