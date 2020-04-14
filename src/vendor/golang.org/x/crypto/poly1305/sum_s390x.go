edabd8058b51aa90875b3d7450f4a1d543f0aff1
Hana (Hyang-Ah) Kim <hyangah@gmail.com>
2020-04-14 12:15:52 -0400

    std,cmd: update golang.org/x/crypto to v0.0.0-20200414155820-4f8f47aa7992
    That includes https://golang.org/cl/228223
    
    Also, update src/vendor/golang.org/x/crypto to match vendored
    golang.org/x/crypto version.
    Otherwise cmd/internal/goobj.TestDependencyVersionsConsistent
    fails.
    
    Fixes #27147
    
    Change-Id: I4a3f1502fdee887762b10348811a08850a15a47a
    Reviewed-on: https://go-review.googlesource.com/c/go/+/228226
    Reviewed-by: Filippo Valsorda <filippo@golang.org>
    Reviewed-by: Daniel Martí <mvdan@mvdan.cc>

--- src/vendor/golang.org/x/crypto/poly1305/sum_s390x.go
+++ src/vendor/golang.org/x/crypto/poly1305/sum_s390x.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// +build s390x,go1.11,!gccgo,!appengine
+// +build go1.11,!gccgo,!purego
 
 package poly1305
 
