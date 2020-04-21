39c41e6c4d3e7fe0d04448b4d02c2425e5152115
pj <pj@pjebs.com.au>
2020-04-21 13:28:07 +1000

    - add all test cases except hexadecimal

--- src/strconv/atoc_test.go
+++ src/strconv/atoc_test.go
@@ -6,18 +6,14 @@ package strconv_test
 
 import (
 	. "strconv"
+	"math"
+	"math/cmplx"
+	"reflect"
 	"testing"
 )
 
 // Test cases required:
 // hex form
-// exp form
-// Only real
-// Only imag
-// Both real and imag
-// With and without parentheses
-// NaN
-// ±Inf
 
 func TestParseComplex(t *testing.T) {
 	tests := []struct {
@@ -101,18 +97,128 @@ func TestParseComplex(t *testing.T) {
 			str:  "+3e+3+3e+3i",
 			want: complex(3e+3, 3e+3),
 		},
+		{
+			str:  "Infi",
+			want: complex(0, math.Inf(1)),
+		},
+		{
+			str:  "-Infi",
+			want: complex(0, math.Inf(-1)),
+		},
+		{
+			str:  "Inf",
+			want: complex(math.Inf(1), 0),
+		},
+		{
+			str:  "-Inf",
+			want: complex(math.Inf(-1), 0),
+		},
+		{
+			str:  "-Inf-Infi",
+			want: complex(math.Inf(-1), math.Inf(-1)),
+		},
+		{
+			str:  "-Inf+Infi",
+			want: complex(math.Inf(-1), math.Inf(1)),
+		},
+		{
+			str:  "NaN",
+			want: complex(math.NaN(), 0),
+		},
+		{
+			str:  "NaNi",
+			want: complex(0, math.NaN()),
+		},
+		{
+			str:  "NaN+NaNi",
+			want: complex(math.NaN(), math.NaN()),
+		},
+		{
+			str:  "NaN+NaNi",
+			want: complex(math.NaN(), math.NaN()),
+		},
+		{
+			str:  "NaN+NaNi",
+			want: complex(math.NaN(), math.NaN()),
+		},
+		// {
+		// 	str:  "0xBadFace+0x677a2fcc40c6i",
+		// 	want: complex(0xBadFace, 0x677a2fcc40c6),
+		// },
+		// {
+		// 	str:  "0x10.3p-8+0x3p3i",
+		// 	want: complex(0x10.3p-8, 0x3p3),
+		// },
+		// {
+		// 	str:  "+0x10.3p-8+0x3p3i",
+		// 	want: complex(+0x10.3p-8, 0x3p3),
+		// },
+		// {
+		// 	str:  "0x10.3p+8-0x3p3i",
+		// 	want: complex(0x10.3p+8, -0x3p3),
+		// },
+
+		// Malformed cases
+		{
+			str:     "30+3i)",
+			wantErr: ErrSyntax,
+		},
+		{
+			str:     "(30+4i",
+			wantErr: ErrSyntax,
+		},
+		{
+			str:     "(",
+			wantErr: ErrSyntax,
+		},
+		{
+			str:     ")",
+			wantErr: ErrSyntax,
+		},
+		{
+			str:     "foo",
+			wantErr: ErrSyntax,
+		},
+		{
+			str:     "10e+10+30i+",
+			wantErr: ErrSyntax,
+		},
+		{
+			str:     "10  + 5i",
+			wantErr: ErrSyntax,
+		},
 	}
 
 	for _, tt := range tests {
 		tt := tt
+		if tt.wantErr != nil {
+			tt.wantErr = &NumError{Func: "ParseComplex", Num: tt.str, Err: tt.wantErr}
+		}
+
 		t.Run(tt.str, func(t *testing.T) {
 			got, err := ParseComplex(tt.str, 128)
-			if g, w := err, tt.wantErr; g != w {
+			if g, w := err, tt.wantErr; !reflect.DeepEqual(g, w) {
 				t.Fatalf("Error mismatch\nGot:  %v\nWant: %v", g, w)
 			}
-			if got != tt.want {
+
+			if !(cmplx.IsNaN(tt.want) && cmplx.IsNaN(got)) && got != tt.want {
 				t.Fatalf("Result mismatch\nGot:  %v\nWant: %v", got, tt.want)
 			}
 		})
+
+		// Test with parentheses
+		if tt.wantErr == nil {
+			tt.str = "(" + tt.str + ")"
+			t.Run(tt.str, func(t *testing.T) {
+				got, err := ParseComplex(tt.str, 128)
+				if g, w := err, tt.wantErr; g != w {
+					t.Fatalf("Error mismatch\nGot:  %v\nWant: %v", g, w)
+				}
+
+				if !(cmplx.IsNaN(tt.want) && cmplx.IsNaN(got)) && got != tt.want {
+					t.Fatalf("Result mismatch\nGot:  %v\nWant: %v", got, tt.want)
+				}
+			})
+		}
 	}
 }
