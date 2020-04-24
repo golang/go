05d5fc94cec43d5f153b3a18313431083537de7b
Tobias Klauser <tklauser@distanz.ch>
2020-04-24 12:17:01 +0200

    testing: replace all GOOS-specific path separators in TempDir
    For GOOS=windows the path separator characters '\' and ':' also need be
    replaced.
    
    Updates #38465
    
    Change-Id: If7c8cf93058c87d7df6cda140e82fd76578fe699
    Reviewed-on: https://go-review.googlesource.com/c/go/+/229837
    Run-TryBot: Tobias Klauser <tobias.klauser@gmail.com>
    TryBot-Result: Gobot Gobot <gobot@golang.org>
    Reviewed-by: Ian Lance Taylor <iant@golang.org>
    Reviewed-by: Brad Fitzpatrick <bradfitz@golang.org>

--- src/testing/testing.go
+++ src/testing/testing.go
@@ -797,6 +797,11 @@ func (c *common) Cleanup(f func()) {
 	}
 }
 
+var (
+	rOnce sync.Once
+	r     *strings.Replacer
+)
+
 // TempDir returns a temporary directory for the test to use.
 // It is lazily created on first access, and calls t.Fatal if the directory
 // creation fails.
@@ -809,7 +814,10 @@ func (c *common) TempDir() string {
 
 		// ioutil.TempDir doesn't like path separators in its pattern,
 		// so mangle the name to accommodate subtests.
-		pattern := strings.ReplaceAll(c.Name(), "/", "_")
+		rOnce.Do(func() {
+			r = strings.NewReplacer("/", "_", "\\", "_", ":", "_")
+		})
+		pattern := r.Replace(c.Name())
 
 		c.tempDir, c.tempDirErr = ioutil.TempDir("", pattern)
 		if c.tempDirErr == nil {
