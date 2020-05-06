002d75468346b6b9b5aad492701ab93d84497293
Rebecca Stambler <rstambler@golang.org>
2020-05-06 02:20:44 -0400

    internal/lsp/regtest: add test for a GOPATH that's missing an element
    Test the case described in
    https://github.com/fatih/vim-go/issues/2673#issuecomment-622307211.
    
    Change-Id: I55ff3b8719fc255ec0901cf3778e68b48630323d
    Reviewed-on: https://go-review.googlesource.com/c/tools/+/232360
    Run-TryBot: Rebecca Stambler <rstambler@golang.org>
    TryBot-Result: Gobot Gobot <gobot@golang.org>
    Reviewed-by: Robert Findley <rfindley@google.com>

---
+++ internal/lsp/regtest/unix_test.go
@@ -0,0 +1,32 @@
+// Copyright 2020 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// +build !windows
+
+package regtest
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestBadGOPATH(t *testing.T) {
+	const missingImport = `
+-- main.go --
+package main
+
+func _() {
+	fmt.Println("Hello World")
+}
+`
+	// Test the case given in
+	// https://github.com/fatih/vim-go/issues/2673#issuecomment-622307211.
+	runner.Run(t, missingImport, func(t *testing.T, env *Env) {
+		env.OpenFile("main.go")
+		env.Await(env.DiagnosticAtRegexp("main.go", "fmt"))
+		if err := env.Editor.OrganizeImports(env.Ctx, "main.go"); err != nil {
+			t.Fatal(err)
+		}
+	}, WithEnv(fmt.Sprintf("GOPATH=:/path/to/gopath")))
+}
