d33605719e1902c80a04a50f3c2c47da3cfd17ea
pj <pj@pjebs.com.au>
2020-05-08 22:47:28 +1000

    - final tests for atoc

--- src/strconv/atoc_test.go
+++ src/strconv/atoc_test.go
@@ -5,19 +5,24 @@
 package strconv_test
 
 import (
-	. "strconv"
 	"math"
 	"math/cmplx"
 	"reflect"
+	. "strconv"
 	"testing"
 )
 
 var (
+	infp0 = complex(math.Inf(+1), 0)
+	infm0 = complex(math.Inf(-1), 0)
+	inf0p = complex(0, math.Inf(+1))
+	inf0m = complex(0, math.Inf(-1))
 	infpp = complex(math.Inf(+1), math.Inf(+1))
 	infpm = complex(math.Inf(+1), math.Inf(-1))
 	infmp = complex(math.Inf(-1), math.Inf(+1))
 	infmm = complex(math.Inf(-1), math.Inf(-1))
 )
+
 type atocTest struct {
 	in  string
 	out complex128
@@ -25,27 +30,30 @@ type atocTest struct {
 }
 
 func TestParseComplex(t *testing.T) {
+
 	tests := []atocTest{
-		// Clear Invalids
+		// Clearly invalid
 		{"", 0, ErrSyntax},
 		{" ", 0, ErrSyntax},
 		{"(", 0, ErrSyntax},
 		{")", 0, ErrSyntax},
-		{"foo", 0, ErrSyntax},
+		{"i", 0, ErrSyntax},
+		{"+i", 0, ErrSyntax},
+		{"-i", 0, ErrSyntax},
+		{"1I", 0, ErrSyntax},
 		{"10  + 5i", 0, ErrSyntax},
-		{"3+3+5.5", 0, ErrSyntax},
-		{"3+3+5.5i", 0, ErrSyntax},
-		{"3+3+5.5I", 0, ErrSyntax},
-
+		{"3+", 0, ErrSyntax},
+		{"3+5", 0, ErrSyntax},
+		{"3+5+5i", 0, ErrSyntax},
 		// Parentheses
 		{"()", 0, ErrSyntax},
-		{"(0)", 0, nil},
 		{"(i)", 0, ErrSyntax},
+		{"(0)", 0, nil},
 		{"(1i)", 1i, nil},
 		{"(3.0+5.5i)", 3.0 + 5.5i, nil},
+		{"(1)+1i", 0, ErrSyntax},
 		{"(3.0+5.5i", 0, ErrSyntax},
 		{"3.0+5.5i)", 0, ErrSyntax},
-
 		// NaNs
 		{"NaN", complex(math.NaN(), 0), nil},
 		{"NANi", complex(0, math.NaN()), nil},
@@ -53,18 +61,19 @@ func TestParseComplex(t *testing.T) {
 		{"+NaN", 0, ErrSyntax},
 		{"-NaN", 0, ErrSyntax},
 		{"NaN-NaNi", 0, ErrSyntax},
-
 		// Infs
-		{"Inf", complex(math.Inf(1), 0), nil},
-		{"+inf", complex(math.Inf(1), 0), nil},
-		{"Infinity", complex(math.Inf(1), 0), nil},
-		{"+INFINITY", complex(math.Inf(1), 0), nil},
-		{"-infinity", complex(math.Inf(-1), 0), nil},
-		{"Inf+Infi", complex(math.Inf(1), math.Inf(1)), nil},
-		{"+Inf-Infi", complex(math.Inf(1), math.Inf(-1)), nil},
-		{"-Infinity+Infi", complex(math.Inf(-1), math.Inf(1)), nil},
-		{"inf - inf", 0, ErrSyntax},
-
+		{"Inf", infp0, nil},
+		{"+inf", infp0, nil},
+		{"-inf", infm0, nil},
+		{"Infinity", infp0, nil},
+		{"+INFINITY", infp0, nil},
+		{"-infinity", infm0, nil},
+		{"+infi", inf0p, nil},
+		{"0-infinityi", inf0m, nil},
+		{"Inf+Infi", infpp, nil},
+		{"+Inf-Infi", infpm, nil},
+		{"-Infinity+Infi", infmp, nil},
+		{"inf-inf", 0, ErrSyntax},
 		// Zeros
 		{"0", 0, nil},
 		{"0i", 0, nil},
@@ -73,16 +82,13 @@ func TestParseComplex(t *testing.T) {
 		{"0e+0i", 0, nil},
 		{"0e-0+0i", 0, nil},
 		{"-0.0-0.0i", 0, nil},
-		{"0e+01234567890123456789", 0, nil},
-		{"0x0p+01234567890123456789i", 0, nil},
-		{"0x0.00p-01234567890123456789i", 0, nil},
+		{"0e+012345", 0, nil},
+		{"0x0p+012345i", 0, nil},
+		{"0x0.00p-012345i", 0, nil},
 		{"+0e-0+0e-0i", 0, nil},
 		{"0e+0+0e+0i", 0, nil},
 		{"-0e+0-0e+0i", 0, nil},
-		{"-0.00e-01234567890123456789-0.00e-01234567890123456789i", 0, nil},
-		{"0x0p+01234567890123456789+0x0p+01234567890123456789i", 0, nil},
-		{"0x0.00p-01234567890123456789+0x0.00p-01234567890123456789i", 0, nil},
-
+		// Regular non-zeroes
 		{"0.1", 0.1, nil},
 		{"0.1i", 0 + 0.1i, nil},
 		{"0.123", 0.123, nil},
@@ -92,147 +98,97 @@ func TestParseComplex(t *testing.T) {
 		{"+99", 99, nil},
 		{"-99", -99, nil},
 		{"+1i", 1i, nil},
-		{"i", 0, ErrSyntax},
-		{"+i", 0, ErrSyntax},
 		{"-1i", -1i, nil},
-		{"-i", 0, ErrSyntax},
 		{"+3+1i", 3 + 1i, nil},
 		{"30+3i", 30 + 3i, nil},
 		{"+3e+3-3e+3i", 3e+3 - 3e+3i, nil},
 		{"+3e+3+3e+3i", 3e+3 + 3e+3i, nil},
 		{"+3e+3+3e+3i+", 0, ErrSyntax},
-
+		// Separators
+		{"0.1", 0.1, nil},
+		{"0.1i", 0 + 0.1i, nil},
+		{"0.1_2_3", 0.123, nil},
+		{"+0x_3p3i", 0x3p3i, nil},
+		{"0x_10.3p-8+0x3p3i", 0x10.3p-8 + 0x3p3i, nil},
+		{"+0x_1_0.3p-8+0x3p3i", 0x10.3p-8 + 0x3p3i, nil},
+		{"0x10.3p+8-0x_3p3i", 0x10.3p+8 - 0x3p3i, nil},
 		// Hexadecimals
-		{"0x10.3p-8+0x3p3i",0x10.3p-8+0x3p3i, nil},
-		{"+0x10.3p-8+0x3p3i",0x10.3p-8+0x3p3i, nil},
-		{"0x10.3p+8-0x3p3i",0x10.3p+8-0x3p3i, nil},
+		{"0x10.3p-8+0x3p3i", 0x10.3p-8 + 0x3p3i, nil},
+		{"+0x10.3p-8+0x3p3i", 0x10.3p-8 + 0x3p3i, nil},
+		{"0x10.3p+8-0x3p3i", 0x10.3p+8 - 0x3p3i, nil},
 		{"0x1p0", 1, nil},
 		{"0x1p1", 2, nil},
 		{"0x1p-1", 0.5, nil},
 		{"0x1ep-1", 15, nil},
 		{"-0x1ep-1", -15, nil},
-		{"-0x1_ep-1", -15, nil},
-		{"0x1p-200", 6.223015277861142e-61, nil},
-		{"0x1p200", 1.6069380442589903e+60, nil},
-		{"0x1fFe2.p0", 131042, nil},
-		{"0x1fFe2.P0", 131042, nil},
 		{"-0x2p3", -16, nil},
-		{"0x0.fp4", 15, nil},
-		{"0x0.fp0", 0.9375, nil},
 		{"0x1e2", 0, ErrSyntax},
 		{"1p2", 0, ErrSyntax},
-		{"0x1p0i", 1i, nil},
-		{"0x1p1i", 2i, nil},
-		{"0x1p-1i", 0.5i, nil},
-		{"0x1ep-1i", 15i, nil},
-		{"-0x1ep-1i", -15i, nil},
-		{"-0x1_ep-1i", -15i, nil},
-		{"0x1p-200i", 6.223015277861142e-61i, nil},
-		{"0x1p200i", 1.6069380442589903e+60i, nil},
-		{"0x1fFe2.p0i", 131042i, nil},
-		{"0x1fFe2.P0i", 131042i, nil},
-		{"-0x2p3i", -16i, nil},
-		{"0x0.fp4i", 15i, nil},
-		{"0x0.fp0i", 0.9375i, nil},
 		{"0x1e2i", 0, ErrSyntax},
-		{"1p2i", 0, ErrSyntax},
-		{"0x1p0+0x1p0i", 1 + 1i, nil},
-		{"0x1p1+0x1p1i", 2 + 2i, nil},
-		{"0x1p-1+0x1p-1i", 0.5 + 0.5i, nil},
-		{"0x1ep-1+0x1ep-1i", 15 + 15i, nil},
-		{"-0x1ep-1-0x1ep-1i", -15 - 15i, nil},
-		{"-0x1_ep-1-0x1_ep-1i", -15 - 15i, nil},
-		{"0x1p-200+0x1p-200i", 6.223015277861142e-61 + 6.223015277861142e-61i, nil},
-		{"0x1p200+0x1p200i", 1.6069380442589903e+60 + 1.6069380442589903e+60i, nil},
-		{"0x1fFe2.p0+0x1fFe2.p0i", 131042 + 131042i, nil},
-		{"0x1fFe2.P0+0x1fFe2.P0i", 131042 + 131042i, nil},
-		{"-0x2p3-0x2p3i", -16 - 16i, nil},
-		{"0x0.fp4+0x0.fp4i", 15 + 15i, nil},
-		{"0x0.fp0+0x0.fp0i", 0.9375 + 0.9375i, nil},
-
 		// ErrRange
-
 		// next float64 - too large
-		{"1.7976931348623159e308+1.7976931348623159e308i", infpp, ErrRange},
-		{"-1.7976931348623159e308-1.7976931348623159e308i", infmm, ErrRange},
-		{"0x1p1024+0x1p1024i", infpp, ErrRange},
-		{"0x1p1024-0x1p1024i", infpm, ErrRange},
+		{"+0x1p1024", infp0, ErrRange},
+		{"-0x1p1024", infm0, ErrRange},
+		{"+0x1p1024i", inf0p, ErrRange},
+		{"-0x1p1024i", inf0m, ErrRange},
+		{"+0x1p1024+0x1p1024i", infpp, ErrRange},
+		{"+0x1p1024-0x1p1024i", infpm, ErrRange},
 		{"-0x1p1024+0x1p1024i", infmp, ErrRange},
 		{"-0x1p1024-0x1p1024i", infmm, ErrRange},
-		{"0x2p1023+0x2p1023i", infpp, ErrRange},
-		{"-0x2p1023-0x2p1023i", infmm, ErrRange},
-		{"0x.1p1028+0x.1p1028i", infpp, ErrRange},
-		{"-0x.1p1028-0x.1p1028i", infmm, ErrRange},
-		{"0x.2p1027+0x.2p1027i", infpp, ErrRange},
-		{"-0x.2p1027-0x.2p1027i", infmm, ErrRange},
-
 		// the border is ...158079
 		// borderline - okay
-		{"1.7976931348623158e308+1.7976931348623158e308i", 1.7976931348623157e+308 + 1.7976931348623157e+308i, nil},
-		{"-1.7976931348623158e308-1.7976931348623158e308i", -1.7976931348623157e+308 - 1.7976931348623157e+308i, nil},
-		{"0x1.fffffffffffff7fffp1023+0x1.fffffffffffff7fffp1023i", 1.7976931348623157e+308 + 1.7976931348623157e+308i, nil},
+		{"+0x1.fffffffffffff7fffp1023+0x1.fffffffffffff7fffp1023i", 1.7976931348623157e+308 + 1.7976931348623157e+308i, nil},
+		{"+0x1.fffffffffffff7fffp1023-0x1.fffffffffffff7fffp1023i", 1.7976931348623157e+308 - 1.7976931348623157e+308i, nil},
+		{"-0x1.fffffffffffff7fffp1023+0x1.fffffffffffff7fffp1023i", -1.7976931348623157e+308 + 1.7976931348623157e+308i, nil},
 		{"-0x1.fffffffffffff7fffp1023-0x1.fffffffffffff7fffp1023i", -1.7976931348623157e+308 - 1.7976931348623157e+308i, nil},
 		// borderline - too large
-		{"1.797693134862315808e308+1.797693134862315808e308i", infpp, ErrRange},
-		{"-1.797693134862315808e308-1.797693134862315808e308i", infmm, ErrRange},
-		{"0x1.fffffffffffff8p1023+0x1.fffffffffffff8p1023i", infpp, ErrRange},
-		{"-0x1.fffffffffffff8p1023-0x1.fffffffffffff8p1023i", infmm, ErrRange},
-		{"0x1fffffffffffff.8p+971+0x1fffffffffffff.8p+971i", infpp, ErrRange},
+		{"+0x1.fffffffffffff8p1023", infp0, ErrRange},
+		{"-0x1fffffffffffff.8p+971", infm0, ErrRange},
+		{"+0x1.fffffffffffff8p1023i", inf0p, ErrRange},
+		{"-0x1fffffffffffff.8p+971i", inf0m, ErrRange},
+		{"+0x1.fffffffffffff8p1023+0x1.fffffffffffff8p1023i", infpp, ErrRange},
+		{"+0x1.fffffffffffff8p1023-0x1.fffffffffffff8p1023i", infpm, ErrRange},
+		{"-0x1fffffffffffff.8p+971+0x1fffffffffffff.8p+971i", infmp, ErrRange},
 		{"-0x1fffffffffffff8p+967-0x1fffffffffffff8p+967i", infmm, ErrRange},
-		{"0x.1fffffffffffff8p1027+0x.1fffffffffffff8p1027i", infpp, ErrRange},
-		{"-0x.1fffffffffffff9p1027-0x.1fffffffffffff9p1027i", infmm, ErrRange},
-
 		// a little too large
 		{"1e308+1e308i", 1e+308 + 1e+308i, nil},
 		{"2e308+2e308i", infpp, ErrRange},
 		{"1e309+1e309i", infpp, ErrRange},
 		{"0x1p1025+0x1p1025i", infpp, ErrRange},
-		{"2e308", complex(math.Inf(1), 0), ErrRange},
-		{"1e309", complex(math.Inf(1), 0), ErrRange},
-		{"0x1p1025", complex(math.Inf(1), 0), ErrRange},
-		{"2e308i", complex(0, math.Inf(1)), ErrRange},
-		{"1e309i", complex(0, math.Inf(1)), ErrRange},
-		{"0x1p1025i", complex(0, math.Inf(1)), ErrRange},
-
+		{"2e308", infp0, ErrRange},
+		{"1e309", infp0, ErrRange},
+		{"0x1p1025", infp0, ErrRange},
+		{"2e308i", inf0p, ErrRange},
+		{"1e309i", inf0p, ErrRange},
+		{"0x1p1025i", inf0p, ErrRange},
 		// way too large
-		{"1e310+1e310i", infpp, ErrRange},
+		{"+1e310+1e310i", infpp, ErrRange},
+		{"+1e310-1e310i", infpm, ErrRange},
+		{"-1e310+1e310i", infmp, ErrRange},
 		{"-1e310-1e310i", infmm, ErrRange},
-		{"1e400+1e400i", infpp, ErrRange},
-		{"-1e400-1e400i", infmm, ErrRange},
-		{"1e400000+1e400000i", infpp, ErrRange},
-		{"-1e400000-1e400000i", infmm, ErrRange},
-		{"0x1p1030+0x1p1030i", infpp, ErrRange},
-		{"0x1p2000+0x1p2000i", infpp, ErrRange},
-		{"0x1p2000000000+0x1p2000000000i", infpp, ErrRange},
-		{"-0x1p1030-0x1p1030i", infmm, ErrRange},
-		{"-0x1p2000-0x1p2000i", infmm, ErrRange},
-		{"-0x1p2000000000-0x1p2000000000i", infmm, ErrRange},
-
-		// try to overflow exponent
+		// under/overflow exponent
+		{"1e-4294967296", 0, nil},
+		{"1e-4294967296i", 0, nil},
+		{"1e-4294967296+1i", 1i, nil},
+		{"1+1e-4294967296i", 1, nil},
 		{"1e-4294967296+1e-4294967296i", 0, nil},
+		{"1e+4294967296", infp0, ErrRange},
+		{"1e+4294967296i", inf0p, ErrRange},
 		{"1e+4294967296+1e+4294967296i", infpp, ErrRange},
-		{"1e-18446744073709551616+1e-18446744073709551616i", 0, nil},
-		{"1e+18446744073709551616+1e+18446744073709551616i", infpp, ErrRange},
-		{"0x1p-4294967296+0x1p-4294967296i", 0, nil},
-		{"0x1p+4294967296+0x1p+4294967296i", infpp, ErrRange},
-		{"0x1p-18446744073709551616+0x1p-18446744073709551616i", 0, nil},
-		{"0x1p+18446744073709551616+0x1p+18446744073709551616i", infpp, ErrRange},
+		{"1e+4294967296-1e+4294967296i", infpm, ErrRange},
 	}
-
 	for _, tt := range tests {
 		tt := tt // for capture in Run closures below
 		if tt.err != nil {
 			tt.err = &NumError{Func: "ParseComplex", Num: tt.in, Err: tt.err}
 		}
-
 		t.Run(tt.in, func(t *testing.T) {
 			got, err := ParseComplex(tt.in, 128)
-			if g, w := err, tt.err; !reflect.DeepEqual(g, w) {
-				t.Fatalf("ParseComplex(%q, 128) = %v, %v want %v %v", tt.in, got, err, tt.out, tt.err)
+			if !reflect.DeepEqual(err, tt.err) {
+				t.Fatalf("ParseComplex(%q, 128) = %v, %v want %v, %v", tt.in, got, err, tt.out, tt.err)
 			}
-
 			if !(cmplx.IsNaN(tt.out) && cmplx.IsNaN(got)) && got != tt.out {
-				t.Fatalf("ParseComplex(%q, 128) = %v, %v want %v %v", tt.in, got, err, tt.out, tt.err)
+				t.Fatalf("ParseComplex(%q, 128) = %v, %v want %v, %v", tt.in, got, err, tt.out, tt.err)
 			}
 		})
 	}
