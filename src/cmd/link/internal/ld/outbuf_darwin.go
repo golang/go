39ea0ea05dcfa281dc5977410b60458f2d2adb99
Cherry Zhang <cherryyz@google.com>
2020-05-18 15:23:46 -0400

    cmd/link: fix size calculation for file space preallocation on darwin
    On darwin, we preallocate file storage space with fcntl
    F_ALLOCATEALL in F_PEOFPOSMODE mode. This is specified as
    allocating from the physical end of the file. So the size we give
    it should be the increment, instead of the total size.
    
    Fixes #39044.
    
    Change-Id: I10c7ee8d51f237b4a7604233ac7abc6f91dcd602
    Reviewed-on: https://go-review.googlesource.com/c/go/+/234481
    Run-TryBot: Cherry Zhang <cherryyz@google.com>
    TryBot-Result: Gobot Gobot <gobot@golang.org>
    Reviewed-by: Austin Clements <austin@google.com>
    Reviewed-by: Jeremy Faller <jeremy@golang.org>

--- src/cmd/link/internal/ld/outbuf_darwin.go
+++ src/cmd/link/internal/ld/outbuf_darwin.go
@@ -10,16 +10,25 @@ import (
 )
 
 func (out *OutBuf) fallocate(size uint64) error {
+	stat, err := out.f.Stat()
+	if err != nil {
+		return err
+	}
+	cursize := uint64(stat.Size())
+	if size <= cursize {
+		return nil
+	}
+
 	store := &syscall.Fstore_t{
 		Flags:   syscall.F_ALLOCATEALL,
 		Posmode: syscall.F_PEOFPOSMODE,
 		Offset:  0,
-		Length:  int64(size),
+		Length:  int64(size - cursize), // F_PEOFPOSMODE allocates from the end of the file, so we want the size difference here
 	}
 
-	_, _, err := syscall.Syscall(syscall.SYS_FCNTL, uintptr(out.f.Fd()), syscall.F_PREALLOCATE, uintptr(unsafe.Pointer(store)))
-	if err != 0 {
-		return err
+	_, _, errno := syscall.Syscall(syscall.SYS_FCNTL, uintptr(out.f.Fd()), syscall.F_PREALLOCATE, uintptr(unsafe.Pointer(store)))
+	if errno != 0 {
+		return errno
 	}
 
 	return nil
