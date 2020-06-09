55784e52d700fbd880733c709f4c9a750f365a22
prospero78su <prospero.78.su@gmail.com>
2020-06-09 12:02:24 +0300

    Exchange with recommend linter

--- src/fmt/doc.go
+++ src/fmt/doc.go
@@ -1,7 +1,8 @@
+package fmt
+
 // Copyright 2009 The Go Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
-
 /*
 	Package fmt implements formatted I/O with functions analogous
 	to C's printf and scanf.  The format 'verbs' are derived from C's but
@@ -339,4 +340,3 @@
 	methods to a reader without that capability, use
 	bufio.NewReader.
 */
-package fmt
