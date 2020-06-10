a0dee5b16a0daa68404a5949a7e9039957b455cd
Aaron Bieber <deftly@gmail.com>
2020-06-09 22:01:58 -0600

    cmd/dist: Add OpenBSD to raceDetectorSupported
    Change-Id: I0eb115d2d86a8eb90a798ead58c2d071d784a8c4

--- src/cmd/dist/test.go
+++ src/cmd/dist/test.go
@@ -1620,7 +1620,7 @@ func raceDetectorSupported(goos, goarch string) bool {
 		return goarch == "amd64" || goarch == "ppc64le" || goarch == "arm64"
 	case "darwin":
 		return goarch == "amd64" || goarch == "arm64"
-	case "freebsd", "netbsd", "windows":
+	case "freebsd", "netbsd", "windows", "openbsd":
 		return goarch == "amd64"
 	default:
 		return false
