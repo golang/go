c7475b9d7fb2f9abb649dc991bb2292381363659
Rebecca Stambler <rstambler@golang.org>
2020-06-16 23:49:25 -0400

    internal/lsp/source: refactor highlighting code
    The current implementation has a number of helper functions that have to
    thread through context, errors, converters, etc. Simplify the code by
    factoring out the conversion to a protocol range into a final step.
    There are probably further improvements that can be made too.
    
    As part of debugging, I noticed that the cmdtest server does not get
    shutdown once tests finish, which leads to excessive logging. Is closing
    the test server connection enough to trigger shutdowns? It doesn't seem
    to be.
    
    Change-Id: Ia67666f6b8debccd120795bb24bd089620c0a87b
    Reviewed-on: https://go-review.googlesource.com/c/tools/+/238377
    Run-TryBot: Rebecca Stambler <rstambler@golang.org>
    TryBot-Result: Gobot Gobot <gobot@golang.org>
    Reviewed-by: Robert Findley <rfindley@google.com>

--- internal/lsp/source/highlight.go
+++ internal/lsp/source/highlight.go
@@ -42,9 +42,10 @@ func Highlight(ctx context.Context, snapshot Snapshot, fh FileHandle, pos protoc
 	if len(path) == 0 {
 		return nil, fmt.Errorf("no enclosing position found for %v:%v", int(pos.Line), int(pos.Character))
 	}
-	// If start==end for astutil.PathEnclosingInterval, the 1-char interval following start is used instead.
-	// As a result, we might not get an exact match so we should check the 1-char interval to the left of the
-	// passed in position to see if that is an exact match.
+	// If start == end for astutil.PathEnclosingInterval, the 1-char interval
+	// following start is used instead. As a result, we might not get an exact
+	// match so we should check the 1-char interval to the left of the passed
+	// in position to see if that is an exact match.
 	if _, ok := path[0].(*ast.Ident); !ok {
 		if p, _ := astutil.PathEnclosingInterval(file, rng.Start-1, rng.Start-1); p != nil {
 			switch p[0].(type) {
@@ -53,42 +54,69 @@ func Highlight(ctx context.Context, snapshot Snapshot, fh FileHandle, pos protoc
 			}
 		}
 	}
+	result, err := highlightPath(pkg, path)
+	if err != nil {
+		return nil, err
+	}
+	var ranges []protocol.Range
+	for rng := range result {
+		mRng, err := posToMappedRange(snapshot.View(), pkg, rng.start, rng.end)
+		if err != nil {
+			return nil, err
+		}
+		pRng, err := mRng.Range()
+		if err != nil {
+			return nil, err
+		}
+		ranges = append(ranges, pRng)
+	}
+	return ranges, nil
+}
 
+func highlightPath(pkg Package, path []ast.Node) (map[posRange]struct{}, error) {
+	result := make(map[posRange]struct{})
 	switch node := path[0].(type) {
 	case *ast.BasicLit:
 		if len(path) > 1 {
 			if _, ok := path[1].(*ast.ImportSpec); ok {
-				return highlightImportUses(ctx, snapshot.View(), pkg, path)
+				err := highlightImportUses(pkg, path, result)
+				return result, err
 			}
 		}
-		return highlightFuncControlFlow(ctx, snapshot.View(), pkg, path)
+		highlightFuncControlFlow(path, result)
 	case *ast.ReturnStmt, *ast.FuncDecl, *ast.FuncType:
-		return highlightFuncControlFlow(ctx, snapshot.View(), pkg, path)
+		highlightFuncControlFlow(path, result)
 	case *ast.Ident:
-		return highlightIdentifiers(ctx, snapshot.View(), pkg, path)
+		highlightIdentifiers(pkg, path, result)
 	case *ast.ForStmt, *ast.RangeStmt:
-		return highlightLoopControlFlow(ctx, snapshot.View(), pkg, path)
+		highlightLoopControlFlow(path, result)
 	case *ast.BranchStmt:
 		// BREAK can exit a loop, switch or select, while CONTINUE exit a loop so
 		// these need to be handled separately. They can also be embedded in any
 		// other loop/switch/select if they have a label. TODO: add support for
 		// GOTO and FALLTHROUGH as well.
 		if node.Label != nil {
-			return highlightLabeledFlow(ctx, snapshot.View(), pkg, node)
+			highlightLabeledFlow(node, result)
+		} else {
+			switch node.Tok {
+			case token.BREAK:
+				highlightUnlabeledBreakFlow(path, result)
+			case token.CONTINUE:
+				highlightLoopControlFlow(path, result)
+			}
 		}
-		switch node.Tok {
-		case token.BREAK:
-			return highlightUnlabeledBreakFlow(ctx, snapshot.View(), pkg, path)
-		case token.CONTINUE:
-			return highlightLoopControlFlow(ctx, snapshot.View(), pkg, path)
-		}
+	default:
+		// If the cursor is in an unidentified area, return empty results.
+		return nil, nil
 	}
-
-	// If the cursor is in an unidentified area, return empty results.
-	return nil, nil
+	return result, nil
 }
 
-func highlightFuncControlFlow(ctx context.Context, view View, pkg Package, path []ast.Node) ([]protocol.Range, error) {
+type posRange struct {
+	start, end token.Pos
+}
+
+func highlightFuncControlFlow(path []ast.Node, result map[posRange]struct{}) {
 	var enclosingFunc ast.Node
 	var returnStmt *ast.ReturnStmt
 	var resultsList *ast.FieldList
@@ -100,13 +128,13 @@ Outer:
 		switch node := n.(type) {
 		case *ast.KeyValueExpr:
 			// If cursor is in a key: value expr, we don't want control flow highlighting
-			return nil, nil
+			return
 		case *ast.CallExpr:
 			// If cusor is an arg in a callExpr, we don't want control flow highlighting.
 			if i > 0 {
 				for _, arg := range node.Args {
 					if arg == path[i-1] {
-						return nil, nil
+						return
 					}
 				}
 			}
@@ -129,7 +157,7 @@ Outer:
 	}
 	// Cursor is not in a function.
 	if enclosingFunc == nil {
-		return nil, nil
+		return
 	}
 	// If the cursor is on a "return" or "func" keyword, we should highlight all of the exit
 	// points of the function, including the "return" and "func" keywords.
@@ -138,7 +166,7 @@ Outer:
 	case *ast.Ident, *ast.BasicLit:
 		// Cursor is in an identifier and not in a return statement or in the results list.
 		if returnStmt == nil && !inReturnList {
-			return nil, nil
+			return
 		}
 	case *ast.FuncType:
 		highlightAllReturnsAndFunc = true
@@ -158,93 +186,78 @@ Outer:
 	}
 	_, index := nodeAtPos(nodes, path[0].Pos())
 
-	result := make(map[protocol.Range]bool)
 	// Highlight the correct argument in the function declaration return types.
 	if resultsList != nil && -1 < index && index < len(resultsList.List) {
-		rng, err := nodeToProtocolRange(view, pkg, resultsList.List[index])
-		if err != nil {
-			return nil, err
+		rng := posRange{
+			start: resultsList.List[index].Pos(),
+			end:   resultsList.List[index].End(),
 		}
-		result[rng] = true
+		result[rng] = struct{}{}
 	}
 	// Add the "func" part of the func declaration.
 	if highlightAllReturnsAndFunc {
-		funcStmt, err := posToMappedRange(view, pkg, enclosingFunc.Pos(), enclosingFunc.Pos()+token.Pos(len("func")))
-		if err != nil {
-			return nil, err
+		r := posRange{
+			start: enclosingFunc.Pos(),
+			end:   enclosingFunc.Pos() + token.Pos(len("func")),
 		}
-		rng, err := funcStmt.Range()
-		if err != nil {
-			return nil, err
-		}
-		result[rng] = true
+		result[r] = struct{}{}
 	}
-	// Traverse the AST to highlight the other relevant return statements in the function.
 	ast.Inspect(enclosingFunc, func(n ast.Node) bool {
 		// Don't traverse any other functions.
 		switch n.(type) {
 		case *ast.FuncDecl, *ast.FuncLit:
 			return enclosingFunc == n
 		}
-		if n, ok := n.(*ast.ReturnStmt); ok {
-			var toAdd ast.Node
-			// Add the entire return statement, applies when highlight the word "return" or "func".
-			if highlightAllReturnsAndFunc {
-				toAdd = n
-			}
-			// Add the relevant field within the entire return statement.
-			if -1 < index && index < len(n.Results) {
-				toAdd = n.Results[index]
-			}
-			if toAdd != nil {
-				rng, err := nodeToProtocolRange(view, pkg, toAdd)
-				if err != nil {
-					event.Error(ctx, "Error getting range for node", err)
-					return false
-				}
-				result[rng] = true
-				return false
-			}
+		ret, ok := n.(*ast.ReturnStmt)
+		if !ok {
+			return true
 		}
-		return true
+		var toAdd ast.Node
+		// Add the entire return statement, applies when highlight the word "return" or "func".
+		if highlightAllReturnsAndFunc {
+			toAdd = n
+		}
+		// Add the relevant field within the entire return statement.
+		if -1 < index && index < len(ret.Results) {
+			toAdd = ret.Results[index]
+		}
+		if toAdd != nil {
+			result[posRange{start: toAdd.Pos(), end: toAdd.End()}] = struct{}{}
+		}
+		return false
 	})
-	return rangeMapToSlice(result), nil
 }
 
-func highlightUnlabeledBreakFlow(ctx context.Context, view View, pkg Package, path []ast.Node) ([]protocol.Range, error) {
-	// Reverse walk the path until we find closest loop, select or switch.
+func highlightUnlabeledBreakFlow(path []ast.Node, result map[posRange]struct{}) {
+	// Reverse walk the path until we find closest loop, select, or switch.
 	for _, n := range path {
 		switch n.(type) {
 		case *ast.ForStmt, *ast.RangeStmt:
-			return highlightLoopControlFlow(ctx, view, pkg, path)
-		// TODO: add highlight when breaking a select or switch.
+			highlightLoopControlFlow(path, result)
+			return // only highlight the innermost statement
 		case *ast.SelectStmt, *ast.SwitchStmt:
-			return nil, nil
+			// TODO: add highlight when breaking a select or switch.
+			return
 		}
 	}
-	return nil, nil
 }
 
-func highlightLabeledFlow(ctx context.Context, view View, pkg Package, node *ast.BranchStmt) ([]protocol.Range, error) {
+func highlightLabeledFlow(node *ast.BranchStmt, result map[posRange]struct{}) {
 	obj := node.Label.Obj
 	if obj == nil || obj.Decl == nil {
-		return nil, nil
+		return
 	}
-
 	label, ok := obj.Decl.(*ast.LabeledStmt)
 	if !ok {
-		return nil, nil
+		return
 	}
-
 	switch label.Stmt.(type) {
 	case *ast.ForStmt, *ast.RangeStmt:
-		return highlightLoopControlFlow(ctx, view, pkg, []ast.Node{label.Stmt, label})
+		highlightLoopControlFlow([]ast.Node{label.Stmt, label}, result)
 	}
-
-	return nil, nil
 }
 
-func highlightLoopControlFlow(ctx context.Context, view View, pkg Package, path []ast.Node) ([]protocol.Range, error) {
+func highlightLoopControlFlow(path []ast.Node, result map[posRange]struct{}) {
 	labelFor := func(path []ast.Node) *ast.Ident {
 		if len(path) > 1 {
 			if n, ok := path[1].(*ast.LabeledStmt); ok {
@@ -270,22 +283,16 @@ Outer:
 			}
 		}
 	}
-	// Cursor is not in a for loop.
 	if loop == nil {
-		return nil, nil
+		return
 	}
 
-	result := make(map[protocol.Range]bool)
 	// Add the for statement.
-	forStmt, err := posToMappedRange(view, pkg, loop.Pos(), loop.Pos()+token.Pos(len("for")))
-	if err != nil {
-		return nil, err
+	rng := posRange{
+		start: loop.Pos(),
+		end:   loop.Pos() + token.Pos(len("for")),
 	}
-	rng, err := forStmt.Range()
-	if err != nil {
-		return nil, err
-	}
-	result[rng] = true
+	result[rng] = struct{}{}
 
 	// Traverse AST to find branch statements within the same for-loop.
 	ast.Inspect(loop, func(n ast.Node) bool {
@@ -295,19 +302,12 @@ Outer:
 		case *ast.SwitchStmt, *ast.SelectStmt:
 			return false
 		}
-
 		b, ok := n.(*ast.BranchStmt)
 		if !ok {
 			return true
 		}
-
 		if b.Label == nil || labelDecl(b.Label) == loopLabel {
-			rng, err := nodeToProtocolRange(view, pkg, b)
-			if err != nil {
-				event.Error(ctx, "Error getting range for node", err)
-				return false
-			}
-			result[rng] = true
+			result[posRange{start: b.Pos(), end: b.End()}] = struct{}{}
 		}
 		return true
 	})
@@ -320,19 +320,14 @@ Outer:
 		}
 
 		if n, ok := n.(*ast.BranchStmt); ok && n.Tok == token.CONTINUE {
-			rng, err := nodeToProtocolRange(view, pkg, n)
-			if err != nil {
-				event.Error(ctx, "Error getting range for node", err)
-				return false
-			}
-			result[rng] = true
+			result[posRange{start: n.Pos(), end: n.End()}] = struct{}{}
 		}
 		return true
 	})
 
 	// We don't need to check other for loops if we aren't looking for labeled statements.
 	if loopLabel == nil {
-		return rangeMapToSlice(result), nil
+		return
 	}
 
 	// Find labeled branch statements in any loop
@@ -341,21 +336,12 @@ Outer:
 		if !ok {
 			return true
 		}
-
 		// Statment with labels that matches the loop.
 		if b.Label != nil && labelDecl(b.Label) == loopLabel {
-			rng, err := nodeToProtocolRange(view, pkg, b)
-			if err != nil {
-				event.Error(ctx, "Error getting range for node", err)
-				return false
-			}
-			result[rng] = true
+			result[posRange{start: b.Pos(), end: b.End()}] = struct{}{}
 		}
-
 		return true
 	})
-
-	return rangeMapToSlice(result), nil
 }
 
 func labelDecl(n *ast.Ident) *ast.Ident {
@@ -372,23 +358,18 @@ func labelDecl(n *ast.Ident) *ast.Ident {
 	if !ok {
 		return nil
 	}
-
 	return stmt.Label
 }
 
-func highlightImportUses(ctx context.Context, view View, pkg Package, path []ast.Node) ([]protocol.Range, error) {
-	result := make(map[protocol.Range]bool)
+func highlightImportUses(pkg Package, path []ast.Node, result map[posRange]struct{}) error {
 	basicLit, ok := path[0].(*ast.BasicLit)
 	if !ok {
-		return nil, errors.Errorf("highlightImportUses called with an ast.Node of type %T", basicLit)
+		return errors.Errorf("highlightImportUses called with an ast.Node of type %T", basicLit)
 	}
-
 	ast.Inspect(path[len(path)-1], func(node ast.Node) bool {
 		if imp, ok := node.(*ast.ImportSpec); ok && imp.Path == basicLit {
-			if rng, err := nodeToProtocolRange(view, pkg, node); err == nil {
-				result[rng] = true
-				return false
-			}
+			result[posRange{start: node.Pos(), end: node.End()}] = struct{}{}
+			return false
 		}
 		n, ok := node.(*ast.Ident)
 		if !ok {
@@ -401,29 +382,19 @@ func highlightImportUses(ctx context.Context, view View, pkg Package, path []ast
 		if !strings.Contains(basicLit.Value, obj.Name()) {
 			return true
 		}
-		rng, err := nodeToProtocolRange(view, pkg, n)
-		if err != nil {
-			event.Error(ctx, "Error getting range for node", err)
-			return false
-		}
-		result[rng] = true
+		result[posRange{start: n.Pos(), end: n.End()}] = struct{}{}
 		return false
 	})
-	return rangeMapToSlice(result), nil
+	return nil
 }
 
-func highlightIdentifiers(ctx context.Context, view View, pkg Package, path []ast.Node) ([]protocol.Range, error) {
-	result := make(map[protocol.Range]bool)
+func highlightIdentifiers(pkg Package, path []ast.Node, result map[posRange]struct{}) error {
 	id, ok := path[0].(*ast.Ident)
 	if !ok {
-		return nil, errors.Errorf("highlightIdentifiers called with an ast.Node of type %T", id)
+		return errors.Errorf("highlightIdentifiers called with an ast.Node of type %T", id)
 	}
 	// Check if ident is inside return or func decl.
-	if toAdd, err := highlightFuncControlFlow(ctx, view, pkg, path); toAdd != nil && err == nil {
-		for _, r := range toAdd {
-			result[r] = true
-		}
-	}
+	highlightFuncControlFlow(path, result)
 
 	// TODO: maybe check if ident is a reserved word, if true then don't continue and return results.
 
@@ -431,9 +402,7 @@ func highlightIdentifiers(ctx context.Context, view View, pkg Package, path []as
 	pkgObj, isImported := idObj.(*types.PkgName)
 	ast.Inspect(path[len(path)-1], func(node ast.Node) bool {
 		if imp, ok := node.(*ast.ImportSpec); ok && isImported {
-			if rng, err := highlightImport(view, pkg, pkgObj, imp); rng != nil && err == nil {
-				result[*rng] = true
-			}
+			highlightImport(pkgObj, imp, result)
 		}
 		n, ok := node.(*ast.Ident)
 		if !ok {
@@ -442,38 +411,20 @@ func highlightIdentifiers(ctx context.Context, view View, pkg Package, path []as
 		if n.Name != id.Name {
 			return false
 		}
-		if nObj := pkg.GetTypesInfo().ObjectOf(n); nObj != idObj {
-			return false
+		if nObj := pkg.GetTypesInfo().ObjectOf(n); nObj == idObj {
+			result[posRange{start: n.Pos(), end: n.End()}] = struct{}{}
 		}
-		rng, err := nodeToProtocolRange(view, pkg, n)
-		if err != nil {
-			event.Error(ctx, "Error getting range for node", err)
-			return false
-		}
-		result[rng] = true
 		return false
 	})
-	return rangeMapToSlice(result), nil
+	return nil
 }
 
-func highlightImport(view View, pkg Package, obj *types.PkgName, imp *ast.ImportSpec) (*protocol.Range, error) {
+func highlightImport(obj *types.PkgName, imp *ast.ImportSpec, result map[posRange]struct{}) {
 	if imp.Name != nil || imp.Path == nil {
-		return nil, nil
+		return
 	}
 	if !strings.Contains(imp.Path.Value, obj.Name()) {
-		return nil, nil
+		return
 	}
-	rng, err := nodeToProtocolRange(view, pkg, imp.Path)
-	if err != nil {
-		return nil, err
-	}
-	return &rng, nil
-}
-
-func rangeMapToSlice(rangeMap map[protocol.Range]bool) []protocol.Range {
-	var list []protocol.Range
-	for i := range rangeMap {
-		list = append(list, i)
-	}
-	return list
+	result[posRange{start: imp.Path.Pos(), end: imp.Path.End()}] = struct{}{}
 }
