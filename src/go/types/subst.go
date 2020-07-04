005397e36df53cbccce00e3854e6abe3638d84be
Robert Griesemer <gri@golang.org>
2020-07-03 22:09:57 -0700

    [dev.go2go] go/types: don't crash instantiating arrays/slices that are not fully set up
    Addresses crash #18 of #39634.
    
    Updates #39634.
    
    Change-Id: I4b1a3d81ce9dc2b59ae9af3146d26f79d8c05973
    Reviewed-on: https://go-review.googlesource.com/c/go/+/240904
    Reviewed-by: Robert Griesemer <gri@golang.org>

--- src/go/types/subst.go
+++ src/go/types/subst.go
@@ -240,19 +240,20 @@ type subster struct {
 func (subst *subster) typ(typ Type) Type {
 	switch t := typ.(type) {
 	case nil:
+		// Call typOrNil if it's possible that typ is nil.
 		panic("nil typ")
 
 	case *Basic, *bottom, *top:
 		// nothing to do
 
 	case *Array:
-		elem := subst.typ(t.elem)
+		elem := subst.typOrNil(t.elem)
 		if elem != t.elem {
 			return &Array{len: t.len, elem: elem}
 		}
 
 	case *Slice:
-		elem := subst.typ(t.elem)
+		elem := subst.typOrNil(t.elem)
 		if elem != t.elem {
 			return &Slice{elem: elem}
 		}
@@ -446,6 +447,16 @@ func typeListString(list []Type) string {
 	var buf bytes.Buffer
 	writeTypeList(&buf, list, nil, nil)
 	return buf.String()
+}
+
+// typOrNil is like typ but if the argument is nil it is replaced with Typ[Invalid].
+// A nil type may appear in pathological cases such as type T(type P) []func(_ T([]_))
+// where an array/slice element is accessed before it is set up.
+func (subst *subster) typOrNil(typ Type) Type {
+	if typ == nil {
+		return Typ[Invalid]
+	}
+	return subst.typ(typ)
 }
 
 func (subst *subster) var_(v *Var) *Var {
