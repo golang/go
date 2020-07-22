e4a3e57f47b498bf81b56a6d2f1589e28dd122c9
Cherry Zhang <cherryyz@google.com>
2020-07-22 12:56:49 -0400

    [dev.link] cmd/link: stream external relocations on ARM and on Windows
    Do them in the same CL so ARM's archreloc doesn't need to support
    both streaming and non-streaming.
    
    TODO: we haven't switched to using mmap to emit external
    relocations on Windows.
    
    Change-Id: Ica2ee89c03fc74839efd6b9e26c80585fcdce45c
    Reviewed-on: https://go-review.googlesource.com/c/go/+/244357
    Run-TryBot: Cherry Zhang <cherryyz@google.com>
    Reviewed-by: Jeremy Faller <jeremy@golang.org>

--- src/cmd/link/internal/ld/target.go
+++ src/cmd/link/internal/ld/target.go
@@ -184,6 +184,6 @@ func (t *Target) IsBigEndian() bool {
 
 // Temporary helper.
 func (t *Target) StreamExtRelocs() bool {
-	return (t.IsELF || t.IsDarwin()) &&
-		(t.IsAMD64() || t.Is386() || t.IsARM64() || t.IsMIPS() || t.IsMIPS64() || t.IsS390X())
+	return (t.IsELF || t.IsDarwin() || t.IsWindows()) &&
+		(t.IsAMD64() || t.Is386() || t.IsARM() || t.IsARM64() || t.IsMIPS() || t.IsMIPS64() || t.IsS390X())
 }
