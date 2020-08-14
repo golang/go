32c06e20e31cc330d0ed2a00169433926241986a
Ian Lance Taylor <iant@golang.org>
2020-08-14 14:39:19 -0700

    test: add test case that caused gccgo undefined symbol reference
    For #40252
    
    Change-Id: Ie23d2789ca9b4b9081adb39ab64c80c412ad58ce
    Reviewed-on: https://go-review.googlesource.com/c/go/+/248637
    Run-TryBot: Ian Lance Taylor <iant@golang.org>
    Reviewed-by: Cherry Zhang <cherryyz@google.com>
    TryBot-Result: Gobot Gobot <gobot@golang.org>

---
+++ test/fixedbugs/issue40252.dir/a.go
@@ -0,0 +1,14 @@
+// Copyright 2020 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package a
+
+type I interface {
+	Func()
+}
+
+func Call() {
+	f := I.Func
+	f(nil)
+}
