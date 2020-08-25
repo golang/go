d75b7f9648bb4968a5d980c4a4b7aa6ca22c4de5
Joel Sing <joel@sing.id.au>
2020-08-25 17:52:46 +1000

    cmd/dist: add openbsd/mips64 as incomplete port
    Update #40995
    
    Change-Id: Id497f7688b00658b50feb7338157e0411b861910
    Reviewed-on: https://go-review.googlesource.com/c/go/+/250578
    Trust: Joel Sing <joel@sing.id.au>
    Reviewed-by: Tobias Klauser <tobias.klauser@gmail.com>

--- src/cmd/dist/build.go
+++ src/cmd/dist/build.go
@@ -1567,6 +1567,7 @@ var cgoEnabled = map[string]bool{
 	"openbsd/amd64":   true,
 	"openbsd/arm":     true,
 	"openbsd/arm64":   true,
+	"openbsd/mips64":  false,
 	"plan9/386":       false,
 	"plan9/amd64":     false,
 	"plan9/arm":       false,
@@ -1579,7 +1580,8 @@ var cgoEnabled = map[string]bool{
 // List of platforms which are supported but not complete yet. These get
 // filtered out of cgoEnabled for 'dist list'. See golang.org/issue/28944
 var incomplete = map[string]bool{
-	"linux/sparc64": true,
+	"linux/sparc64":  true,
+	"openbsd/mips64": true,
 }
 
 func needCC() bool {
