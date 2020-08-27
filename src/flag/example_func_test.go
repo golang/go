16328513bfb12d96e8f33fc37f816e1441027135
Carl Johnson <me@carlmjohnson.net>
2020-08-27 13:08:44 +0000

    flag: add Func
    Fixes #39557
    
    Change-Id: Ida578f7484335e8c6bf927255f75377eda63b563
    GitHub-Last-Rev: b97294f7669c24011e5b093179d65636512a84cd
    GitHub-Pull-Request: golang/go#39880
    Reviewed-on: https://go-review.googlesource.com/c/go/+/240014
    Reviewed-by: Russ Cox <rsc@golang.org>
    Trust: Ian Lance Taylor <iant@golang.org>

---
+++ src/flag/example_func_test.go
@@ -0,0 +1,41 @@
+// Copyright 2020 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package flag_test
+
+import (
+	"errors"
+	"flag"
+	"fmt"
+	"net"
+	"os"
+)
+
+func ExampleFunc() {
+	fs := flag.NewFlagSet("ExampleFunc", flag.ContinueOnError)
+	fs.SetOutput(os.Stdout)
+	var ip net.IP
+	fs.Func("ip", "`IP address` to parse", func(s string) error {
+		ip = net.ParseIP(s)
+		if ip == nil {
+			return errors.New("could not parse IP")
+		}
+		return nil
+	})
+	fs.Parse([]string{"-ip", "127.0.0.1"})
+	fmt.Printf("{ip: %v, loopback: %t}\n\n", ip, ip.IsLoopback())
+
+	// 256 is not a valid IPv4 component
+	fs.Parse([]string{"-ip", "256.0.0.1"})
+	fmt.Printf("{ip: %v, loopback: %t}\n\n", ip, ip.IsLoopback())
+
+	// Output:
+	// {ip: 127.0.0.1, loopback: true}
+	//
+	// invalid value "256.0.0.1" for flag -ip: could not parse IP
+	// Usage of ExampleFunc:
+	//   -ip IP address
+	//     	IP address to parse
+	// {ip: <nil>, loopback: false}
+}
