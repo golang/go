4bbd801c7539e8e4eef7637bfc54ccf15472b7ee
Kassian Sun <kassiansun@gmail.com>
2020-08-31 22:07:28 +0800

    sync/atomic: remove duplicated comment for atomic.Value

--- src/sync/atomic/value.go
+++ src/sync/atomic/value.go
@@ -11,8 +11,6 @@ import (
 // A Value provides an atomic load and store of a consistently typed value.
 // The zero value for a Value returns nil from Load.
 // Once Store has been called, a Value must not be copied.
-//
-// A Value must not be copied after first use.
 type Value struct {
 	v interface{}
 }
