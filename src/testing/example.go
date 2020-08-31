afa150c2ea1b121c7727c12ab3615fcc173d0d15
Changkun Ou <hi@changkun.us>
2020-08-31 20:54:17 +0200

    testing: fail Example tests that invoke runtime.Goexit
    Previously, if an example test invoked runtime.Goexit, it would
    pass yet hang until a timeout, while regular tests that invoke
    runtime.Goexit do fail. This change removes that inconsistent
    behavior and makes such example tests fail, and panic with an
    indication of having invoked runtime.Goexit.
    
    Fixes #41084
    
    Change-Id: I0ffa152204f2b1580f4d5d6961ba1ce6b13fc022
    Reviewed-on: https://go-review.googlesource.com/c/go/+/251857
    Reviewed-by: Emmanuel Odeke <emm.odeke@gmail.com>
    Reviewed-by: Bryan C. Mills <bcmills@google.com>
    Run-TryBot: Bryan C. Mills <bcmills@google.com>
    TryBot-Result: Gobot Gobot <gobot@golang.org>

--- src/testing/example.go
+++ src/testing/example.go
@@ -62,9 +62,10 @@ func sortLines(output string) string {
 // If stdout doesn't match the expected output or if recovered is non-nil, it'll print the cause of failure to stdout.
 // If the test is chatty/verbose, it'll print a success message to stdout.
 // If recovered is non-nil, it'll panic with that value.
-func (eg *InternalExample) processRunResult(stdout string, timeSpent time.Duration, recovered interface{}) (passed bool) {
+// If the test panicked with nil, or invoked runtime.Goexit, it'll be
+// made to fail and panic with errNilPanicOrGoexit
+func (eg *InternalExample) processRunResult(stdout string, timeSpent time.Duration, finished bool, recovered interface{}) (passed bool) {
 	passed = true
-
 	dstr := fmtDuration(timeSpent)
 	var fail string
 	got := strings.TrimSpace(stdout)
@@ -78,15 +79,19 @@ func (eg *InternalExample) processRunResult(stdout string, timeSpent time.Durati
 			fail = fmt.Sprintf("got:\n%s\nwant:\n%s\n", got, want)
 		}
 	}
-	if fail != "" || recovered != nil {
+	if fail != "" || !finished || recovered != nil {
 		fmt.Printf("--- FAIL: %s (%s)\n%s", eg.Name, dstr, fail)
 		passed = false
 	} else if *chatty {
 		fmt.Printf("--- PASS: %s (%s)\n", eg.Name, dstr)
 	}
+
 	if recovered != nil {
 		// Propagate the previously recovered result, by panicking.
 		panic(recovered)
+	}
+	if !finished && recovered == nil {
+		panic(errNilPanicOrGoexit)
 	}
 
 	return
