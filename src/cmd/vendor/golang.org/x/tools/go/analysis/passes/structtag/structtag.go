e2f4483227dbf13b59eedf38f527a7e880225e59
Tzu-Chiao Yeh <su3g4284zo6y7@gmail.com>
2020-09-06 09:43:34 +0800

    cmd: update golang.org/x/tools to v0.0.0-20200901153117-6e59e24738da
    Includes the latest fix on vet to warn unused context.WithValue result.
    
    Fixes #41149
    
    Change-Id: I06c204f40ef12b0f62f59b1bbdf1fe06ccd6565d
    Reviewed-on: https://go-review.googlesource.com/c/go/+/252941
    Run-TryBot: Emmanuel Odeke <emm.odeke@gmail.com>
    TryBot-Result: Gobot Gobot <gobot@golang.org>
    Reviewed-by: Bryan C. Mills <bcmills@google.com>

--- src/cmd/vendor/golang.org/x/tools/go/analysis/passes/structtag/structtag.go
+++ src/cmd/vendor/golang.org/x/tools/go/analysis/passes/structtag/structtag.go
@@ -116,7 +116,11 @@ func checkCanonicalFieldTag(pass *analysis.Pass, field *types.Var, tag string, s
 	}
 
 	for _, enc := range [...]string{"json", "xml"} {
-		if reflect.StructTag(tag).Get(enc) != "" {
+		switch reflect.StructTag(tag).Get(enc) {
+		// Ignore warning if the field not exported and the tag is marked as
+		// ignored.
+		case "", "-":
+		default:
 			pass.Reportf(field.Pos(), "struct field %s has %s tag but is not exported", field.Name(), enc)
 			return
 		}
