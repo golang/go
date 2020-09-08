625fd4c333c8e5fa4c4016aab3385dd42c0374d8
ipriver <g1ran1q@gmail.com>
2020-09-08 17:35:15 +0300

    runtime: updated GOMAXPROCS doc comment

--- src/runtime/debug.go
+++ src/runtime/debug.go
@@ -10,10 +10,8 @@ import (
 )
 
 // GOMAXPROCS sets the maximum number of CPUs that can be executing
-// simultaneously and returns the previous setting. If n < 1, it does not
-// change the current setting.
-// The number of logical CPUs on the local machine can be queried with NumCPU.
-// This call will go away when the scheduler improves.
+// simultaneously and returns the previous setting. It defaults to
+// the value of runtime.NumCPU. If n < 1, it does not change the current setting.
 func GOMAXPROCS(n int) int {
 	if GOARCH == "wasm" && n > 1 {
 		n = 1 // WebAssembly has no threads yet, so only one CPU is possible.
