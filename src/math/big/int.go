83cfbbc4fe774577c239dfd4869c80a8e60cadbd
Stephen Zerfas <36237793+szerfas1@users.noreply.github.com>
2020-09-12 12:24:33 -0700

    Correct type in SetBit documentation
    I believe there's a mistake in the documentation.
    
    `f b is 0 SetBit sets z = x &^ (1 << i). If b is not 0 or 1,` should be `f b is 0 SetBit sets z = x & ~(1 << i). If b is not 0 or 1,`

--- src/math/big/int.go
+++ src/math/big/int.go
@@ -1050,7 +1050,7 @@ func (x *Int) Bit(i int) uint {
 
 // SetBit sets z to x, with x's i'th bit set to b (0 or 1).
 // That is, if b is 1 SetBit sets z = x | (1 << i);
-// if b is 0 SetBit sets z = x &^ (1 << i). If b is not 0 or 1,
+// if b is 0 SetBit sets z = x & ~(1 << i). If b is not 0 or 1,
 // SetBit will panic.
 func (z *Int) SetBit(x *Int, i int, b uint) *Int {
 	if i < 0 {
