3b64e6b010775839f2daef4ac3fb607bf1519e05
Tobias Klauser <tklauser@distanz.ch>
2020-09-14 11:06:39 +0200

    internal/poll, internal/syscall/unix, net: enable writev on illumos
    Illumos supports iovec read/write. Add the writev wrapper to
    internal/syscall/unix and use it to implement internal/poll.writev for
    net.(*netFD).writeBuffers.
    
    Change-Id: Ie256c2f96aba8e61fb21991788789a049425f792
    Reviewed-on: https://go-review.googlesource.com/c/go/+/254638
    Run-TryBot: Tobias Klauser <tobias.klauser@gmail.com>
    TryBot-Result: Go Bot <gobot@golang.org>
    Reviewed-by: Ian Lance Taylor <iant@golang.org>
    Trust: Tobias Klauser <tobias.klauser@gmail.com>

--- src/internal/poll/writev.go
+++ src/internal/poll/writev.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// +build darwin dragonfly freebsd linux netbsd openbsd
+// +build darwin dragonfly freebsd illumos linux netbsd openbsd
 
 package poll
 
@@ -38,7 +38,7 @@ func (fd *FD) Writev(v *[][]byte) (int64, error) {
 			if len(chunk) == 0 {
 				continue
 			}
-			iovecs = append(iovecs, syscall.Iovec{Base: &chunk[0]})
+			iovecs = append(iovecs, newIovecWithBase(&chunk[0]))
 			if fd.IsStream && len(chunk) > 1<<30 {
 				iovecs[len(iovecs)-1].SetLen(1 << 30)
 				break // continue chunk on next writev
