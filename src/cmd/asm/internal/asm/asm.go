fa04d488bd54b8fdd78cc9bcc6d90de4bf5f8efb
fanzha02 <fannie.zhang@arm.com>
2020-09-16 14:05:18 +0800

    cmd/asm: fix the issue of moving 128-bit integers to vector registers on arm64
    The CL 249758 added `FMOVQ $vcon, Vd` instruction and assembler used
    128-bit simd literal-loading to load `$vcon` from pool into 128-bit vector
    register `Vd`. Because Go does not have 128-bit integers for now, the
    assembler will report an error of `immediate out of range` when
    assembleing `FMOVQ $0x123456789abcdef0123456789abcdef, V0` instruction.
    
    This patch lets 128-bit integers take two 64-bit operands, for the high
    and low parts separately and adds `VMOVQ $hi, $lo, Vd` instruction to
    move `$hi<<64+$lo' into 128-bit register `Vd`.
    
    In addition, this patch renames `FMOVQ/FMOVD/FMOVS` ops to 'VMOVQ/VMOVD/VMOVS'
    and uses them to move 128-bit, 64-bit and 32-bit constants into vector
    registers, respectively
    
    Update the go doc.
    
    Fixes #40725
    
    Change-Id: Ia3c83bb6463f104d2bee960905053a97299e0a3a
    Reviewed-on: https://go-review.googlesource.com/c/go/+/255900
    Trust: fannie zhang <Fannie.Zhang@arm.com>
    Reviewed-by: Cherry Zhang <cherryyz@google.com>

--- src/cmd/asm/internal/asm/asm.go
+++ src/cmd/asm/internal/asm/asm.go
@@ -622,8 +622,9 @@ func (p *Parser) asmInstruction(op obj.As, cond string, a []obj.Addr) {
 			prog.SetFrom3(a[1])
 			prog.To = a[2]
 		case sys.ARM64:
-			// ARM64 instructions with one input and two outputs.
-			if arch.IsARM64STLXR(op) {
+			switch {
+			case arch.IsARM64STLXR(op):
+				// ARM64 instructions with one input and two outputs.
 				prog.From = a[0]
 				prog.To = a[1]
 				if a[2].Type != obj.TYPE_REG {
@@ -631,20 +632,16 @@ func (p *Parser) asmInstruction(op obj.As, cond string, a []obj.Addr) {
 					return
 				}
 				prog.RegTo2 = a[2].Reg
-				break
-			}
-			if arch.IsARM64TBL(op) {
+			case arch.IsARM64TBL(op):
+				// one of its inputs does not fit into prog.Reg.
 				prog.From = a[0]
-				if a[1].Type != obj.TYPE_REGLIST {
-					p.errorf("%s: expected list; found %s", op, obj.Dconv(prog, &a[1]))
-				}
 				prog.SetFrom3(a[1])
 				prog.To = a[2]
-				break
+			default:
+				prog.From = a[0]
+				prog.Reg = p.getRegister(prog, op, &a[1])
+				prog.To = a[2]
 			}
-			prog.From = a[0]
-			prog.Reg = p.getRegister(prog, op, &a[1])
-			prog.To = a[2]
 		case sys.I386:
 			prog.From = a[0]
 			prog.SetFrom3(a[1])
