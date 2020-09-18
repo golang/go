01189f83fa4ac853ce3d43bf6558f8fa58b65c5a
Jay Lee <BusyJayLee@gmail.com>
2020-09-18 05:49:09 +0000

    time: support colon at start of TZ value
    According to POSIX, there are three formats for TZ variable. When
    it refers to timezone file, it should starts with a colon. This commit
    removes the colon if it exists, so that it keeps compatible with both
    the spec and the old behavior.
    
    Change-Id: I30cfeaea530d24e174de309952338cb1146694a5
    GitHub-Last-Rev: 11d83d11ca2eca9d542036cf5e23559388fd323e
    GitHub-Pull-Request: golang/go#27570
    Reviewed-on: https://go-review.googlesource.com/c/go/+/134217
    Run-TryBot: Ian Lance Taylor <iant@golang.org>
    TryBot-Result: Go Bot <gobot@golang.org>
    Reviewed-by: Ian Lance Taylor <iant@golang.org>
    Trust: Dmitri Shuralyov <dmitshur@golang.org>

--- src/time/zoneinfo_unix.go
+++ src/time/zoneinfo_unix.go
@@ -29,7 +29,9 @@ func initLocal() {
 	// consult $TZ to find the time zone to use.
 	// no $TZ means use the system default /etc/localtime.
 	// $TZ="" means use UTC.
-	// $TZ="foo" means use /usr/share/zoneinfo/foo.
+	// $TZ="foo" or $TZ=":foo" if foo is an absolute path, then the file pointed
+	// by foo will be used to initialize timezone; otherwise, file
+	// /usr/share/zoneinfo/foo will be used.
 
 	tz, ok := syscall.Getenv("TZ")
 	switch {
@@ -40,10 +42,25 @@ func initLocal() {
 			localLoc.name = "Local"
 			return
 		}
-	case tz != "" && tz != "UTC":
-		if z, err := loadLocation(tz, zoneSources); err == nil {
-			localLoc = *z
-			return
+	case tz != "":
+		if tz[0] == ':' {
+			tz = tz[1:]
+		}
+		if tz != "" && tz[0] == '/' {
+			if z, err := loadLocation(tz, []string{""}); err == nil {
+				localLoc = *z
+				if tz == "/etc/localtime" {
+					localLoc.name = "Local"
+				} else {
+					localLoc.name = tz
+				}
+				return
+			}
+		} else if tz != "" && tz != "UTC" {
+			if z, err := loadLocation(tz, zoneSources); err == nil {
+				localLoc = *z
+				return
+			}
 		}
 	}
 
