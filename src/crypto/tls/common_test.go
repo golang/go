bc108a04a3327f811686d6b53979ed0732e9e64a
Chen.Zhidong <njutczd@gmail.com>
2020-09-27 13:55:49 +0800

    add Go copyright header, simplify the test

--- src/crypto/tls/common_test.go
+++ src/crypto/tls/common_test.go
@@ -1,17 +1,17 @@
+// Copyright 2020 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 package tls_test
 
 import (
+	"crypto/tls"
 	"testing"
 )
 
 func TestCloneNilConfig(t *testing.T) {
-	defer func() {
-		if r := recover(); r != nil {
-			t.Fatal("Clone a nil Config should not produce panic")
-		}
-	}()
-
-	if cc := c.Clone(); cc != nil {
+	var config *tls.Config
+	if cc := config.Clone(); cc != nil {
 		t.Fatalf("Clone with nil should return nil, got: %+v", cc)
 	}
 }
