01aeb5b684a871c31371228bed6d48d138851e31
Russ Cox <rsc@golang.org>
2020-09-30 15:48:14 -0400

    io: make clear that EOF should not be wrapped
    For #40827.
    
    Change-Id: Ifd108421abd8d0988dd7b985e4f9e2bd5356964a
    Reviewed-on: https://go-review.googlesource.com/c/go/+/258524
    Trust: Russ Cox <rsc@golang.org>
    Reviewed-by: Rob Pike <r@golang.org>

--- src/io/io.go
+++ src/io/io.go
@@ -31,6 +31,8 @@ var ErrShortWrite = errors.New("short write")
 var ErrShortBuffer = errors.New("short buffer")
 
 // EOF is the error returned by Read when no more input is available.
+// (Read must return EOF itself, not an error wrapping EOF,
+// because callers will test for EOF using ==.)
 // Functions should return EOF only to signal a graceful end of input.
 // If the EOF occurs unexpectedly in a structured data stream,
 // the appropriate error is either ErrUnexpectedEOF or some other error
