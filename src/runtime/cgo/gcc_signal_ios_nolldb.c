1c1db628705e7ec53d799340d383794a20338728
Cherry Zhang <cherryyz@google.com>
2020-10-14 19:26:20 -0400

    runtime/cgo: build iOS-specific code only on iOS
    Don't build them on macOS/ARM64.
    
    Updates #38485.
    
    Change-Id: I9fbea838fdce52db22742487926879761dea0d6a
    Reviewed-on: https://go-review.googlesource.com/c/go/+/262559
    Trust: Cherry Zhang <cherryyz@google.com>
    Run-TryBot: Cherry Zhang <cherryyz@google.com>
    Reviewed-by: Ian Lance Taylor <iant@golang.org>

--- src/runtime/cgo/gcc_signal_ios_nolldb.c
+++ src/runtime/cgo/gcc_signal_ios_nolldb.c
@@ -2,8 +2,8 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// +build !lldb !ios
-// +build darwin
+// +build !lldb
+// +build ios
 // +build arm64
 
 #include <stdint.h>
