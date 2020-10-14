8de197ae20b47ace914b3b5a932af849ab4b0c81
Michael Pratt <mpratt@google.com>
2020-10-14 17:18:27 -0400

    runtime: remove debugCachedWork
    debugCachedWork and all of its dependent fields and code were added to
    aid in debugging issue #27993. Now that the source of the problem is
    known and mitigated (via the extra work check after STW in gcMarkDone),
    these extra checks are no longer required and simply make the code more
    difficult to follow.
    
    Remove it all.
    
    Updates #27993
    
    Change-Id: I594beedd5ca61733ba9cc9eaad8f80ea92df1a0d
    Reviewed-on: https://go-review.googlesource.com/c/go/+/262350
    Trust: Michael Pratt <mpratt@google.com>
    Run-TryBot: Michael Pratt <mpratt@google.com>
    TryBot-Result: Go Bot <gobot@golang.org>
    Reviewed-by: Austin Clements <austin@google.com>

--- src/cmd/compile/internal/gc/inl_test.go
+++ src/cmd/compile/internal/gc/inl_test.go
@@ -83,7 +83,7 @@ func TestIntendedInlining(t *testing.T) {
 			"puintptr.ptr",
 			"spanOf",
 			"spanOfUnchecked",
-			//"(*gcWork).putFast", // TODO(austin): For debugging #27993
+			"(*gcWork).putFast",
 			"(*gcWork).tryGetFast",
 			"(*guintptr).set",
 			"(*markBits).advance",
