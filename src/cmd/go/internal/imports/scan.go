ed2010e676042b387d72daaa4c39b54c4c57c031
Keyuan <keyuanli123@gmail.com>
2020-10-20 19:47:29 -0700

    imports: make ScanDir ignore go files start with dot
    Adding "." Prefix Check for go files.
    
    Fixes #42047
    Change-Id: Ifc42bf562f52fdd304f9828b06fc57888fcd8049
    Reviewed-on: https://go-review.googlesource.com/c/go/+/264078
    Reviewed-by: Bryan C. Mills <bcmills@google.com>
    Run-TryBot: Bryan C. Mills <bcmills@google.com>
    TryBot-Result: Go Bot <gobot@golang.org>
    Trust: Jay Conrod <jayconrod@google.com>
    Trust: Bryan C. Mills <bcmills@google.com>

--- src/cmd/go/internal/imports/scan.go
+++ src/cmd/go/internal/imports/scan.go
@@ -34,7 +34,7 @@ func ScanDir(dir string, tags map[string]bool) ([]string, []string, error) {
 			}
 		}
 
-		if info.Mode().IsRegular() && !strings.HasPrefix(name, "_") && strings.HasSuffix(name, ".go") && MatchFile(name, tags) {
+		if info.Mode().IsRegular() && !strings.HasPrefix(name, "_") && !strings.HasPrefix(name, ".") && strings.HasSuffix(name, ".go") && MatchFile(name, tags) {
 			files = append(files, filepath.Join(dir, name))
 		}
 	}
