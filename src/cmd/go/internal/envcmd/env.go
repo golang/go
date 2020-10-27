b2ef159db237ba09278704ec2970f6ae41f130b1
Daniel Martí <mvdan@mvdan.cc>
2020-10-27 18:12:03 +0000

    cmd/go: introduce the GOVERSION env variable
    This is an extra variable available via 'go env', but not read from the
    user's environment. It corresponds to the same string that
    runtime.Version returns, assuming a program is built by the same version
    of Go.
    
    It's similar to the output of 'go version', but without the "go version"
    prefix nor the "$GOOS/$GOARCH" suffix.
    
    The main use case here is tools, which often use 'go env' to query basic
    information about the installed Go tree. Its version was one missing
    piece of information, which required an extra call to 'go version'
    before this change.
    
    Fixes #41116.
    
    Change-Id: I5c9d8c2ba856c816c9f4c462ba73c907b3441445
    Reviewed-on: https://go-review.googlesource.com/c/go/+/265637
    Run-TryBot: Daniel Martí <mvdan@mvdan.cc>
    TryBot-Result: Go Bot <gobot@golang.org>
    Reviewed-by: Jay Conrod <jayconrod@google.com>
    Reviewed-by: Russ Cox <rsc@golang.org>
    Trust: Jay Conrod <jayconrod@google.com>
    Trust: Daniel Martí <mvdan@mvdan.cc>

--- src/cmd/go/internal/envcmd/env.go
+++ src/cmd/go/internal/envcmd/env.go
@@ -88,6 +88,7 @@ func MkEnv() []cfg.EnvVar {
 		{Name: "GOTMPDIR", Value: cfg.Getenv("GOTMPDIR")},
 		{Name: "GOTOOLDIR", Value: base.ToolDir},
 		{Name: "GOVCS", Value: cfg.GOVCS},
+		{Name: "GOVERSION", Value: runtime.Version()},
 	}
 
 	if work.GccgoBin != "" {
@@ -399,7 +400,7 @@ func getOrigEnv(key string) string {
 
 func checkEnvWrite(key, val string) error {
 	switch key {
-	case "GOEXE", "GOGCCFLAGS", "GOHOSTARCH", "GOHOSTOS", "GOMOD", "GOTOOLDIR":
+	case "GOEXE", "GOGCCFLAGS", "GOHOSTARCH", "GOHOSTOS", "GOMOD", "GOTOOLDIR", "GOVERSION":
 		return fmt.Errorf("%s cannot be modified", key)
 	case "GOENV":
 		return fmt.Errorf("%s can only be set using the OS environment", key)
