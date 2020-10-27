76661d12e89971e3bf4caaae2c37b969ab654194
Joel Sing <joel@sing.id.au>
2020-10-27 23:10:13 +1100

    runtime: remove new g register (X27) from preempt save/restore
    The g register is now in X27 (previously X4, which collided with TP usage). Remove
    X27 from preempt save/restore.
    
    Change-Id: I9dd38ec3a8222fa0710757463769dbfac8ae7d20
    Reviewed-on: https://go-review.googlesource.com/c/go/+/265517
    Trust: Joel Sing <joel@sing.id.au>
    Run-TryBot: Cherry Zhang <cherryyz@google.com>
    Reviewed-by: Cherry Zhang <cherryyz@google.com>
    TryBot-Result: Go Bot <gobot@golang.org>

--- src/runtime/mkpreempt.go
+++ src/runtime/mkpreempt.go
@@ -495,12 +495,12 @@ func genPPC64() {
 }
 
 func genRISCV64() {
-	// X0 (zero), X1 (LR), X2 (SP), X4 (g), X31 (TMP) are special.
+	// X0 (zero), X1 (LR), X2 (SP), X4 (TP), X27 (g), X31 (TMP) are special.
 	var l = layout{sp: "X2", stack: 8}
 
-	// Add integer registers (X3, X5-X30).
+	// Add integer registers (X3, X5-X26, X28-30).
 	for i := 3; i < 31; i++ {
-		if i == 4 {
+		if i == 4 || i == 27 {
 			continue
 		}
 		reg := fmt.Sprintf("X%d", i)
