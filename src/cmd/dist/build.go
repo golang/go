b7e0adfee28b516a0dcceb5ecf0e5a695b38c61f
Joel Sing <joel@sing.id.au>
2020-11-05 16:41:32 +1100

    cmd/dist: remove openbsd/mips64 from incomplete ports
    Remove openbsd/mips64 from incomplete ports lists - all of the necessary code
    has landed and we want to run tests so we can see/deal with remaining failures.
    
    Update #40995
    
    Change-Id: I5d4f89af82ff3abe57570a9a8abf889498093d32
    Reviewed-on: https://go-review.googlesource.com/c/go/+/267606
    Trust: Joel Sing <joel@sing.id.au>
    Reviewed-by: Cherry Zhang <cherryyz@google.com>

--- src/cmd/dist/build.go
+++ src/cmd/dist/build.go
@@ -1580,8 +1580,7 @@ var cgoEnabled = map[string]bool{
 // List of platforms which are supported but not complete yet. These get
 // filtered out of cgoEnabled for 'dist list'. See golang.org/issue/28944
 var incomplete = map[string]bool{
-	"linux/sparc64":  true,
-	"openbsd/mips64": true,
+	"linux/sparc64": true,
 }
 
 func needCC() bool {
