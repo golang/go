d5c545cf98fd052b21887c0d65cbfc030dbb905b
Matthew Dempsky <mdempsky@google.com>
2020-11-29 14:06:17 -0800

    [dev.regabi] cmd/compile: process //go:linknames after declarations
    Allows emitting errors about ineffectual //go:linkname directives.
    
    In particular, this exposed: a typo in os2_aix.go; redundant (but
    harmless) directives for libc_pipe in both os3_solaris.go and
    syscall2_solaris.go; and a bunch of useless //go:linkname directives
    in macOS wrapper code.
    
    However, because there's also ineffectual directives in the vendored
    macOS code from x/sys, that can't be an error just yet. So instead we
    print a warning (including a heads up that it will be promoted to an
    error in Go 1.17) to prevent backsliding while we fix and re-vendor
    that code.
    
    Passes toolstash-check.
    
    Change-Id: I59badeab5df0d8b3abfd14c6066e9bb00e840f73
    Reviewed-on: https://go-review.googlesource.com/c/go/+/273986
    Run-TryBot: Matthew Dempsky <mdempsky@google.com>
    TryBot-Result: Go Bot <gobot@golang.org>
    Reviewed-by: Russ Cox <rsc@golang.org>
    Trust: Matthew Dempsky <mdempsky@google.com>

--- src/go/types/stdlib_test.go
+++ src/go/types/stdlib_test.go
@@ -156,6 +156,7 @@ func TestStdTest(t *testing.T) {
 	testTestDir(t, filepath.Join(runtime.GOROOT(), "test"),
 		"cmplxdivide.go", // also needs file cmplxdivide1.go - ignore
 		"directive.go",   // tests compiler rejection of bad directive placement - ignore
+		"linkname2.go",   // go/types doesn't check validity of //go:xxx directives
 	)
 }
 
