d2911d76127deaa08644979cec7d990559f0aa54
Keith Randall <khr@golang.org>
2020-12-09 14:59:40 -0800

    cmd/compile: fold MOV*nop and MOV*const
    MOV*nop and MOV*reg seem superfluous. They are there to keep type
    information around that would otherwise get thrown away. Not sure
    what we need it for. I think our compiler needs a normalization of
    how types are represented in SSA, especially after lowering.
    
    MOV*nop gets in the way of some optimization rules firing, like for
    load combining.
    
    For now, just fold MOV*nop and MOV*const. It's certainly safe to
    do that, as the type info on the MOV*const isn't ever useful.
    
    R=go1.17
    
    Change-Id: I3630a80afc2455a8e9cd9fde10c7abe05ddc3767
    Reviewed-on: https://go-review.googlesource.com/c/go/+/276792
    Trust: Keith Randall <khr@golang.org>
    Run-TryBot: Keith Randall <khr@golang.org>
    TryBot-Result: Go Bot <gobot@golang.org>
    Reviewed-by: David Chase <drchase@google.com>

--- src/cmd/compile/internal/ssa/rewriteARM.go
+++ src/cmd/compile/internal/ssa/rewriteARM.go
@@ -202,6 +202,8 @@ func rewriteValueARM(v *Value) bool {
 		return rewriteValueARM_OpARMMOVWloadshiftRA(v)
 	case OpARMMOVWloadshiftRL:
 		return rewriteValueARM_OpARMMOVWloadshiftRL(v)
+	case OpARMMOVWnop:
+		return rewriteValueARM_OpARMMOVWnop(v)
 	case OpARMMOVWreg:
 		return rewriteValueARM_OpARMMOVWreg(v)
 	case OpARMMOVWstore:
@@ -6497,6 +6499,21 @@ func rewriteValueARM_OpARMMOVWloadshiftRL(v *Value) bool {
 		v.reset(OpARMMOVWload)
 		v.AuxInt = int32ToAuxInt(int32(uint32(c) >> uint64(d)))
 		v.AddArg2(ptr, mem)
+		return true
+	}
+	return false
+}
+func rewriteValueARM_OpARMMOVWnop(v *Value) bool {
+	v_0 := v.Args[0]
+	// match: (MOVWnop (MOVWconst [c]))
+	// result: (MOVWconst [c])
+	for {
+		if v_0.Op != OpARMMOVWconst {
+			break
+		}
+		c := auxIntToInt32(v_0.AuxInt)
+		v.reset(OpARMMOVWconst)
+		v.AuxInt = int32ToAuxInt(c)
 		return true
 	}
 	return false
