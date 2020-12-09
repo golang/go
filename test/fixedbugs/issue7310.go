810957b1555358fd22e6dfd75cdceb2117362f91
Robert Griesemer <gri@golang.org>
2020-12-08 17:48:39 -0800

    [dev.typeparams] cmd/compile/internal/types2: adjusted array error message for compiler
    Also: Triaged/adjusted some more test/fixedbugs tests.
    Change-Id: Idaba1875273d6da6ef82dd8de8edd8daa885d32c
    Reviewed-on: https://go-review.googlesource.com/c/go/+/276472
    Trust: Robert Griesemer <gri@golang.org>
    Reviewed-by: Robert Findley <rfindley@google.com>

--- test/fixedbugs/issue7310.go
+++ test/fixedbugs/issue7310.go
@@ -9,7 +9,7 @@
 package main
 
 func main() {
-	_ = copy(nil, []int{}) // ERROR "use of untyped nil"
-	_ = copy([]int{}, nil) // ERROR "use of untyped nil"
-	_ = 1 + true           // ERROR "mismatched types untyped int and untyped bool"
+	_ = copy(nil, []int{}) // ERROR "use of untyped nil|untyped nil"
+	_ = copy([]int{}, nil) // ERROR "use of untyped nil|untyped nil"
+	_ = 1 + true           // ERROR "mismatched types untyped int and untyped bool|untyped int .* untyped bool"
 }
