6d8084c57dac019ba8b97b38064ac909c4ff8040
Ignacio Hagopian <jsign.uy@gmail.com>
2021-01-03 01:33:33 -0300

    reflect: add benchmark for method call
    Signed-off-by: Ignacio Hagopian <jsign.uy@gmail.com>

--- src/reflect/all_test.go
+++ src/reflect/all_test.go
@@ -1942,6 +1942,26 @@ func BenchmarkCall(b *testing.B) {
 	})
 }
 
+type myint int64
+
+type inccer interface {
+	inc()
+}
+
+func (i *myint) inc() {
+	*i = *i + 1
+}
+
+func BenchmarkCallMethod(b *testing.B) {
+	b.ReportAllocs()
+	z := inccer(new(myint))
+
+	v := ValueOf(z.inc)
+	for i := 0; i < b.N; i++ {
+		v.Call(nil)
+	}
+}
+
 func BenchmarkCallArgCopy(b *testing.B) {
 	byteArray := func(n int) Value {
 		return Zero(ArrayOf(n, TypeOf(byte(0))))
