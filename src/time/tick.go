c590f9f58220b04a5ffd3ad09389ed0964f72520
pschou <pschou@users.noreply.github.com>
2021-01-04 19:12:14 -0500

    Switch to NewTickerStartingAt

--- src/time/tick.go
+++ src/time/tick.go
@@ -40,33 +40,38 @@ func NewTicker(d Duration) *Ticker {
 	return t
 }
 
-// NewTickerRuntimeOffset returns a new Ticker containing a channel
+// NewTickerStartingAt returns a new Ticker containing a channel
 // that will send the time on the channel after each tick. The period
 // of the ticks is specified by the duration argument. The ticker
 // will adjust the time interval or drop ticks to make up for slow
 // receivers.  The duration d must be greater than zero; if not,
-// NewTicker will panic. The offset duration is the time to wait from 
-// the process start. Stop the ticker to release associated
-// resources.
-func NewTickerRuntimeOffset(d Duration, offset Duration) *Ticker {
-	if d <= 0 {
-		panic(errors.New("non-positive interval for NewTicker"))
-	}
-	// Give the channel a 1-element time buffer.
-	// If the client falls behind while reading, we drop ticks
-	// on the floor until the client catches up.
-	c := make(chan Time, 1)
-	t := &Ticker{
-		C: c,
-		r: runtimeTimer{
-			when:   startNano + int64(offset),
-			period: int64(d),
-			f:      sendTime,
-			arg:    c,
-		},
-	}
-	startTimer(&t.r)
-	return t
+// NewTicker will panic, and start is the time for the first tick
+// to fire.
+//
+// Note: Since this method uses time.Sub to derive the first tick,
+// if the system is put to sleep or crosses a leap second this time
+// may be off by a second or more.
+//
+// Stop the ticker to release associated resources.
+func NewTickerStartingAt(d Duration, start Time) *Ticker {
+        if d <= 0 {
+                panic(errors.New("non-positive interval for NewTicker"))
+        }
+        // Give the channel a 1-element time buffer.
+        // If the client falls behind while reading, we drop ticks
+        // on the floor until the client catches up.
+        c := make(chan Time, 1)
+        t := &Ticker{
+                C: c,
+                r: runtimeTimer{
+                        when:   startNano + int64(start.Sub(RuntimeStarted())),
+                        period: int64(d),
+                        f:      sendTime,
+                        arg:    c,
+                },
+        }
+        startTimer(&t.r)
+        return t
 }
 
 // Stop turns off a ticker. After Stop, no more ticks will be sent.
