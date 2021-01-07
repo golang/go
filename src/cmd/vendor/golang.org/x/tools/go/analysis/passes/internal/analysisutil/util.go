81a8b4f729c362188ed8c52c8c5589547d658b44
Ian Lance Taylor <iant@golang.org>
2021-01-07 12:35:01 -0800

    cmd: update to latest golang.org/x/tools
    In particular bring in CL 201973, which reverts support for multiple
    keys in a struct tag.
    
    For #40281
    For #43083
    For #43226
    
    Change-Id: I66e76639cbbca55bdbff6956acdb0a97650fdd31
    Reviewed-on: https://go-review.googlesource.com/c/go/+/282412
    Trust: Ian Lance Taylor <iant@golang.org>
    Run-TryBot: Ian Lance Taylor <iant@golang.org>
    TryBot-Result: Go Bot <gobot@golang.org>
    Reviewed-by: Emmanuel Odeke <emmanuel@orijtech.com>

--- src/cmd/vendor/golang.org/x/tools/go/analysis/passes/internal/analysisutil/util.go
+++ src/cmd/vendor/golang.org/x/tools/go/analysis/passes/internal/analysisutil/util.go
@@ -1,3 +1,7 @@
+// Copyright 2018 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 // Package analysisutil defines various helper functions
 // used by two or more packages beneath go/analysis.
 package analysisutil
