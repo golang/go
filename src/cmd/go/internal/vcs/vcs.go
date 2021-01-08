59bfc18e3441d9cd0b1b2f302935403bbf52ac8b
Jay Conrod <jayconrod@google.com>
2021-01-08 16:12:46 -0500

    cmd/go: add hint to read 'go help vcs' to GOVCS errors
    Fixes #43596
    
    Change-Id: Iff925d077b5de64161e88c9471402bc7e8885fcd
    Reviewed-on: https://go-review.googlesource.com/c/go/+/282713
    Trust: Jay Conrod <jayconrod@google.com>
    Run-TryBot: Jay Conrod <jayconrod@google.com>
    TryBot-Result: Go Bot <gobot@golang.org>
    Reviewed-by: Bryan C. Mills <bcmills@google.com>

--- src/cmd/go/internal/vcs/vcs.go
+++ src/cmd/go/internal/vcs/vcs.go
@@ -729,7 +729,7 @@ func checkGOVCS(vcs *Cmd, root string) error {
 		if private {
 			what = "private"
 		}
-		return fmt.Errorf("GOVCS disallows using %s for %s %s", vcs.Cmd, what, root)
+		return fmt.Errorf("GOVCS disallows using %s for %s %s; see 'go help vcs'", vcs.Cmd, what, root)
 	}
 
 	return nil
