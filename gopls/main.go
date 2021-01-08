e1c06e4683d281553e101157fba544737ca7f517
Russ Cox <rsc@golang.org>
2021-01-08 12:59:44 -0500

    gopls: use standard command doc comment format
    "Cmd" not "The cmd command".
    While we're here, add pronounciation
    and link directly to the docs (README.md)
    instead of a commit directory listing.
    
    Change-Id: I1ea8d9bfa371a4d8422dfd0586de40fdba26999b
    Reviewed-on: https://go-review.googlesource.com/c/tools/+/282652
    Trust: Russ Cox <rsc@golang.org>
    Run-TryBot: Russ Cox <rsc@golang.org>
    Reviewed-by: Rebecca Stambler <rstambler@golang.org>
    gopls-CI: kokoro <noreply+kokoro@google.com>

--- gopls/main.go
+++ gopls/main.go
@@ -2,13 +2,13 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// The gopls command is an LSP server for Go.
+// Gopls (pronounced “go please”) is an LSP server for Go.
 // The Language Server Protocol allows any text editor
 // to be extended with IDE-like features;
 // see https://langserver.org/ for details.
 //
-// See https://github.com/golang/tools/tree/master/gopls
-// for the most up-to-date information on the gopls status.
+// See https://github.com/golang/tools/blob/master/gopls/README.md
+// for the most up-to-date documentation.
 package main // import "golang.org/x/tools/gopls"
 
 import (
