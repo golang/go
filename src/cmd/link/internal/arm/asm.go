9c7463ca903863a0c67b3f76454b37da0a400c13
Cherry Zhang <cherryyz@google.com>
2021-02-16 10:20:58 -0500

    [release-branch.go1.16] cmd/link: generate trampoline for inter-dependent packages
    Currently, in the trampoline generation pass we expect packages
    are laid out in dependency order, so a cross-package jump always
    has a known target address so we can check if a trampoline is
    needed. With linknames, there can be cycles in the package
    dependency graph, making this algorithm no longer work. For them,
    as the target address is unkown we conservatively generate a
    trampoline. This may generate unnecessary trampolines (if the
    packages turn out laid together), but package cycles are extremely
    rare so this is fine.
    
    Updates #44639.
    Fixes #44640.
    
    Change-Id: I2dc2998edacbda27d726fc79452313a21d07787a
    Reviewed-on: https://go-review.googlesource.com/c/go/+/292490
    Trust: Cherry Zhang <cherryyz@google.com>
    Reviewed-by: Than McIntosh <thanm@google.com>
    (cherry picked from commit 098504c73ff6ece19566a1ac811ceed73be7c81d)
    Reviewed-on: https://go-review.googlesource.com/c/go/+/296909
    Run-TryBot: Cherry Zhang <cherryyz@google.com>
    TryBot-Result: Go Bot <gobot@golang.org>

--- src/cmd/link/internal/arm/asm.go
+++ src/cmd/link/internal/arm/asm.go
@@ -370,10 +370,16 @@ func trampoline(ctxt *ld.Link, ldr *loader.Loader, ri int, rs, s loader.Sym) {
 	r := relocs.At(ri)
 	switch r.Type() {
 	case objabi.R_CALLARM:
-		// r.Add is the instruction
-		// low 24-bit encodes the target address
-		t := (ldr.SymValue(rs) + int64(signext24(r.Add()&0xffffff)*4) - (ldr.SymValue(s) + int64(r.Off()))) / 4
-		if t > 0x7fffff || t < -0x800000 || (*ld.FlagDebugTramp > 1 && ldr.SymPkg(s) != ldr.SymPkg(rs)) {
+		var t int64
+		// ldr.SymValue(rs) == 0 indicates a cross-package jump to a function that is not yet
+		// laid out. Conservatively use a trampoline. This should be rare, as we lay out packages
+		// in dependency order.
+		if ldr.SymValue(rs) != 0 {
+			// r.Add is the instruction
+			// low 24-bit encodes the target address
+			t = (ldr.SymValue(rs) + int64(signext24(r.Add()&0xffffff)*4) - (ldr.SymValue(s) + int64(r.Off()))) / 4
+		}
+		if t > 0x7fffff || t < -0x800000 || ldr.SymValue(rs) == 0 || (*ld.FlagDebugTramp > 1 && ldr.SymPkg(s) != ldr.SymPkg(rs)) {
 			// direct call too far, need to insert trampoline.
 			// look up existing trampolines first. if we found one within the range
 			// of direct call, we can reuse it. otherwise create a new one.
@@ -445,7 +451,7 @@ func gentramp(arch *sys.Arch, linkmode ld.LinkMode, ldr *loader.Loader, tramp *l
 	arch.ByteOrder.PutUint32(P[8:], o3)
 	tramp.SetData(P)
 
-	if linkmode == ld.LinkExternal {
+	if linkmode == ld.LinkExternal || ldr.SymValue(target) == 0 {
 		r, _ := tramp.AddRel(objabi.R_ADDR)
 		r.SetOff(8)
 		r.SetSiz(4)
