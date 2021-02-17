49dccf141f5e315739c5517b24572fff7cb13734
Conrad Irwin <conrad.irwin@gmail.com>
2021-02-17 14:13:01 -0800

    time: add Time.Unix{Milli,Micro} and to-Time helpers UnixMicro, UnixMilli
    Adds helper functions for users working with other systems which
    represent time in milliseconds or microseconds since the Unix epoch.
    
    Fixes #44196
    
    Change-Id: Ibc4490b52ddec94ebd0c692cb7b52a33e4536759
    Reviewed-on: https://go-review.googlesource.com/c/go/+/293349
    Reviewed-by: Emmanuel Odeke <emmanuel@orijtech.com>
    Reviewed-by: Ian Lance Taylor <iant@golang.org>
    Run-TryBot: Emmanuel Odeke <emmanuel@orijtech.com>
    TryBot-Result: Go Bot <gobot@golang.org>

--- src/time/time.go
+++ src/time/time.go
@@ -1135,6 +1135,24 @@ func (t Time) Unix() int64 {
 	return t.unixSec()
 }
 
+// UnixMilli returns t as a Unix time, the number of milliseconds elapsed since
+// January 1, 1970 UTC. The result is undefined if the Unix time in
+// milliseconds cannot be represented by an int64 (a date more than 292 million
+// years before or after 1970). The result does not depend on the
+// location associated with t.
+func (t Time) UnixMilli() int64 {
+	return t.unixSec()*1e3 + int64(t.nsec())/1e6
+}
+
+// UnixMicro returns t as a Unix time, the number of microseconds elapsed since
+// January 1, 1970 UTC. The result is undefined if the Unix time in
+// microseconds cannot be represented by an int64 (a date before year -290307 or
+// after year 294246). The result does not depend on the location associated
+// with t.
+func (t Time) UnixMicro() int64 {
+	return t.unixSec()*1e6 + int64(t.nsec())/1e3
+}
+
 // UnixNano returns t as a Unix time, the number of nanoseconds elapsed
 // since January 1, 1970 UTC. The result is undefined if the Unix time
 // in nanoseconds cannot be represented by an int64 (a date before the year
@@ -1307,6 +1325,18 @@ func Unix(sec int64, nsec int64) Time {
 		}
 	}
 	return unixTime(sec, int32(nsec))
+}
+
+// UnixMilli returns the local Time corresponding to the given Unix time,
+// msec milliseconds since January 1, 1970 UTC.
+func UnixMilli(msec int64) Time {
+	return Unix(msec/1e3, (msec%1e3)*1e6)
+}
+
+// UnixMicro returns the local Time corresponding to the given Unix time,
+// usec milliseconds since January 1, 1970 UTC.
+func UnixMicro(usec int64) Time {
+	return Unix(usec/1e6, (usec%1e6)*1e3)
 }
 
 // IsDST reports whether the time in the configured location is in Daylight Savings Time.
