545a862926fdba467bd6812e66685b1a3173f1c0
Michael Anthony Knyszek <mknyszek@google.com>
2021-02-17 19:28:33 +0000

    reflect: use global variables for register count
    This change switches reflect to use global variables for ABI-related
    register counts instead of internal/abi constants. The advantage of
    doing so is that we can make the internal/abi constants non-zero and
    enable the runtime register argument spiller/unspiller even if they're
    not used. It's basically turning two things we need to flip when we
    switch to the register ABI into one.
    
    It also paves the way for testing the reflect register ABI path
    independently, because now we can switch the global variables at will
    and run the register-assignment algorithm in tests without having the
    rest of the runtime be broken.
    
    Change-Id: Ie23629a37a5c80aeb24909d4bd9eacbd3f0c06d9
    Reviewed-on: https://go-review.googlesource.com/c/go/+/293149
    Trust: Michael Knyszek <mknyszek@google.com>
    Run-TryBot: Michael Knyszek <mknyszek@google.com>
    TryBot-Result: Go Bot <gobot@golang.org>
    Reviewed-by: Cherry Zhang <cherryyz@google.com>

--- src/internal/abi/abi_amd64.go
+++ src/internal/abi/abi_amd64.go
@@ -10,16 +10,12 @@ package abi
 const (
 	// See abi_generic.go.
 
-	// Currently these values are zero because whatever uses
-	// them will expect the register ABI, which isn't ready
-	// yet.
-
 	// RAX, RBX, RCX, RDI, RSI, R8, R9, R10, R11.
-	IntArgRegs = 0 // 9
+	IntArgRegs = 9
 
 	// X0 -> X14.
-	FloatArgRegs = 0 // 15
+	FloatArgRegs = 15
 
 	// We use SSE2 registers which support 64-bit float operations.
-	EffectiveFloatRegSize = 0 // 8
+	EffectiveFloatRegSize = 8
 )
