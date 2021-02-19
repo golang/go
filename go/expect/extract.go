a43f69b1f5e22e6a9892effdfa50d1f867db4c89
Rob Findley <rfindley@google.com>
2021-02-18 19:44:30 -0500

    go/expect: use parser.AllErrors when extracting Notes
    Without this parser mode, go/parser will bail out and return no comments
    from a parsed file if there are more than 10 errors. This can lead to
    tests silently not running, as noted in the TODO right above the parse.
    
    This can still happen: poor parser recovery could result in comments not
    being parsed. However, the parser is generally pretty good about
    preserving comments, and anyway passing parser.AllErrors means that
    adding a single new error to a testdata file will not suddenly cause all
    errors in that file to be ignored.
    
    Change-Id: Ib3c22944999651018d38fa6ce5c5a11ac6853aa0
    Reviewed-on: https://go-review.googlesource.com/c/tools/+/293932
    Trust: Robert Findley <rfindley@google.com>
    Run-TryBot: Robert Findley <rfindley@google.com>
    gopls-CI: kokoro <noreply+kokoro@google.com>
    TryBot-Result: Go Bot <gobot@golang.org>
    Reviewed-by: Heschi Kreinick <heschi@google.com>

--- go/expect/extract.go
+++ go/expect/extract.go
@@ -42,7 +42,7 @@ func Parse(fset *token.FileSet, filename string, content []byte) ([]*Note, error
 		// there are ways you can break the parser such that it will not add all the
 		// comments to the ast, which may result in files where the tests are silently
 		// not run.
-		file, err := parser.ParseFile(fset, filename, src, parser.ParseComments)
+		file, err := parser.ParseFile(fset, filename, src, parser.ParseComments|parser.AllErrors)
 		if file == nil {
 			return nil, err
 		}
