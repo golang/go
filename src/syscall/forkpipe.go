76c0003cd5645078e342c1d24c98b3ce5ae42eb4
Tobias Klauser <tklauser@distanz.ch>
2021-02-23 14:58:32 +0100

    syscall, os: use pipe2 syscall on DragonflyBSD instead of pipe
    Follow the implementation used by the other BSDs and account for the
    intricacy of having to pass the fds array even though the file
    descriptors are returned.
    
    Re-submit of CL 130996 with corrected pipe2 wrapper.
    
    Change-Id: Ie36d8214cba60c4fdb579f18bfc1c1ab3ead3ddc
    Reviewed-on: https://go-review.googlesource.com/c/go/+/295372
    Trust: Tobias Klauser <tobias.klauser@gmail.com>
    Run-TryBot: Tobias Klauser <tobias.klauser@gmail.com>
    TryBot-Result: Go Bot <gobot@golang.org>
    Reviewed-by: Ian Lance Taylor <iant@golang.org>

--- src/syscall/forkpipe.go
+++ src/syscall/forkpipe.go
@@ -2,8 +2,8 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-//go:build aix || darwin || dragonfly || solaris
-// +build aix darwin dragonfly solaris
+//go:build aix || darwin || solaris
+// +build aix darwin solaris
 
 package syscall
 
