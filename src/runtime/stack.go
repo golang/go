2fbbad51430e15601e46d10521140ac71f367dbd
David Chase <drchase@google.com>
2021-02-23 14:02:33 -0500

    runtime: reset stack poison flag accidentally set
    See if this clears failure on openbsd-amd64-68 (ahem).
    
    Change-Id: Ifa60ef711a95e5de8ad91433ffa425f75b36c76f
    Reviewed-on: https://go-review.googlesource.com/c/go/+/295629
    Trust: David Chase <drchase@google.com>
    Run-TryBot: David Chase <drchase@google.com>
    Reviewed-by: Cherry Zhang <cherryyz@google.com>

--- src/runtime/stack.go
+++ src/runtime/stack.go
@@ -112,7 +112,7 @@ const (
 	stackDebug       = 0
 	stackFromSystem  = 0 // allocate stacks from system memory instead of the heap
 	stackFaultOnFree = 0 // old stacks are mapped noaccess to detect use after free
-	stackPoisonCopy  = 1 // fill stack that should not be accessed with garbage, to detect bad dereferences during copy
+	stackPoisonCopy  = 0 // fill stack that should not be accessed with garbage, to detect bad dereferences during copy
 	stackNoCache     = 0 // disable per-P small stack caches
 
 	// check the BP links during traceback.
