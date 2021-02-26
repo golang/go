f2ccac8f43f3b6c3ed88453533dbdcbf92bec768
Jay Conrod <jayconrod@google.com>
2021-02-26 10:54:11 -0500

    [dev.fuzz] testing, internal/fuzz: multiple small fixes
    * Run gofmt with go1.17 build constraint changes.
    * Tighten regular expressions used in tests. "ok" got some false
      positives with verbose output, so make sure it appears at the start
      of a line.
    * Return err in deps.RunFuzzWorker instead of nil.
    * Call common.Helper from F methods. This prevents F methods from
      appearing in stack traces.
    
    Change-Id: I839c70ec8a9f313c1a4ea68e6bb34a4d801dd36f
    Reviewed-on: https://go-review.googlesource.com/c/go/+/297032
    Trust: Jay Conrod <jayconrod@google.com>
    Trust: Katie Hockman <katie@golang.org>
    Run-TryBot: Jay Conrod <jayconrod@google.com>
    TryBot-Result: Go Bot <gobot@golang.org>
    Reviewed-by: Katie Hockman <katie@golang.org>

--- src/internal/fuzz/sys_unimplemented.go
+++ src/internal/fuzz/sys_unimplemented.go
@@ -3,6 +3,7 @@
 // license that can be found in the LICENSE file.
 
 // TODO(jayconrod): support more platforms.
+//go:build !darwin && !linux && !windows
 // +build !darwin,!linux,!windows
 
 package fuzz
