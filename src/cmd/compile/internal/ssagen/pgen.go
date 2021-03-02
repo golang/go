c4e3f6c4c78f52060d409a549b83b72644069137
eric fang <eric.fang@arm.com>
2021-03-02 06:10:58 +0000

    cmd/compile: remove 8-byte alignment requirement of stack slot on s390x
    This CL applies CL 267999 to s390x.
    
    Updates #42385
    
    Change-Id: Ie8e69ad1b3f7ddc2c8f05125f4af617aeac035ec
    Reviewed-on: https://go-review.googlesource.com/c/go/+/297769
    Reviewed-by: Keith Randall <khr@golang.org>
    Trust: eric fang <eric.fang@arm.com>

--- src/cmd/compile/internal/ssagen/pgen.go
+++ src/cmd/compile/internal/ssagen/pgen.go
@@ -138,7 +138,7 @@ func (s *ssafn) AllocFrame(f *ssa.Func) {
 		} else {
 			lastHasPtr = false
 		}
-		if Arch.LinkArch.InFamily(sys.ARM, sys.ARM64, sys.PPC64, sys.S390X) {
+		if Arch.LinkArch.InFamily(sys.ARM, sys.ARM64, sys.PPC64) {
 			s.stksize = types.Rnd(s.stksize, int64(types.PtrSize))
 		}
 		n.SetFrameOffset(-s.stksize)
