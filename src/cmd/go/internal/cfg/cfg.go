06ca809410649e24c1a3a079a0a859bae2d4613a
Austin Clements <austin@google.com>
2021-03-15 15:43:45 -0400

    cmd/internal/objabi: centralize GOEXPERIMENT parsing
    objabi parses GOEXPERIMENT, but most of the consumers look at the raw
    GOEXPERIMENT string that objabi gets from the environment. Centralize
    this logic by only exposing the parsed GOEXPERIMENT value from objabi.
    This sets us up for the next few changes. It also has the nice but
    mostly useless property that the order of experiment names will be
    canonicalized in build cache hashes.
    
    After this, the only remaining place that looks at raw GOEXPERIMENT is
    cmd/dist, which we'll fix in the next CL.
    
    For #40724.
    
    Change-Id: Idb150f848e17c184fae91372ca8b361591472f51
    Reviewed-on: https://go-review.googlesource.com/c/go/+/302049
    Trust: Austin Clements <austin@google.com>
    Run-TryBot: Austin Clements <austin@google.com>
    TryBot-Result: Go Bot <gobot@golang.org>
    Reviewed-by: Matthew Dempsky <mdempsky@google.com>
    Reviewed-by: David Chase <drchase@google.com>
    Reviewed-by: Cherry Zhang <cherryyz@google.com>

--- src/cmd/go/internal/cfg/cfg.go
+++ src/cmd/go/internal/cfg/cfg.go
@@ -252,13 +252,12 @@ var (
 	GOMODCACHE   = envOr("GOMODCACHE", gopathDir("pkg/mod"))
 
 	// Used in envcmd.MkEnv and build ID computations.
-	GOARM        = envOr("GOARM", fmt.Sprint(objabi.GOARM))
-	GO386        = envOr("GO386", objabi.GO386)
-	GOMIPS       = envOr("GOMIPS", objabi.GOMIPS)
-	GOMIPS64     = envOr("GOMIPS64", objabi.GOMIPS64)
-	GOPPC64      = envOr("GOPPC64", fmt.Sprintf("%s%d", "power", objabi.GOPPC64))
-	GOWASM       = envOr("GOWASM", fmt.Sprint(objabi.GOWASM))
-	GOEXPERIMENT = envOr("GOEXPERIMENT", objabi.GOEXPERIMENT)
+	GOARM    = envOr("GOARM", fmt.Sprint(objabi.GOARM))
+	GO386    = envOr("GO386", objabi.GO386)
+	GOMIPS   = envOr("GOMIPS", objabi.GOMIPS)
+	GOMIPS64 = envOr("GOMIPS64", objabi.GOMIPS64)
+	GOPPC64  = envOr("GOPPC64", fmt.Sprintf("%s%d", "power", objabi.GOPPC64))
+	GOWASM   = envOr("GOWASM", fmt.Sprint(objabi.GOWASM))
 
 	GOPROXY    = envOr("GOPROXY", "https://proxy.golang.org,direct")
 	GOSUMDB    = envOr("GOSUMDB", "sum.golang.org")
