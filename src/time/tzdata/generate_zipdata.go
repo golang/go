1ca06bf7dd5b6837fc70f9135b161b3897d392d3
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-03-15 22:16:57 -0400

    Update generate_zipdata.go

--- src/time/tzdata/generate_zipdata.go
+++ src/time/tzdata/generate_zipdata.go
@@ -40,7 +40,7 @@ const zipdata = `
 
 func main() {
 	// We should be run in the $GOROOT/src/time/tzdata directory.
-	data, err := os.ReadFile("../../../lib/time/zoneinfo.zip")
+	data, err := os.ReadFile("zoneinfo.zip")
 	if err != nil {
 		die("cannot find zoneinfo.zip file: %v", err)
 	}
