f38b6428a2d1beae784a9402368e4d7f9c7cd8c5
Tobias Klauser <tklauser@distanz.ch>
2021-03-17 11:17:02 +0100

    crypto/rand, internal/syscall/unix: add support for getentropy syscall on darwin
    The getentropy syscall is available on macOS since version 10.12, which
    is the minimum required version since Go 1.15.
    
    Change-Id: I294259af0b11df9669e4dc5fa891d2f2f039d91a
    Reviewed-on: https://go-review.googlesource.com/c/go/+/302489
    Trust: Tobias Klauser <tobias.klauser@gmail.com>
    Reviewed-by: Ian Lance Taylor <iant@golang.org>
    Run-TryBot: Tobias Klauser <tobias.klauser@gmail.com>
    TryBot-Result: Go Bot <gobot@golang.org>

--- src/crypto/rand/rand_getentropy.go
+++ src/crypto/rand/rand_getentropy.go
@@ -2,6 +2,9 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
+//go:build darwin || openbsd
+// +build darwin openbsd
+
 package rand
 
 import (
@@ -9,10 +12,10 @@ import (
 )
 
 func init() {
-	altGetRandom = getRandomOpenBSD
+	altGetRandom = getEntropy
 }
 
-func getRandomOpenBSD(p []byte) (ok bool) {
+func getEntropy(p []byte) (ok bool) {
 	// getentropy(2) returns a maximum of 256 bytes per call
 	for i := 0; i < len(p); i += 256 {
 		end := i + 256
