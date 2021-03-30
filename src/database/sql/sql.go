6339a31a9e6c980d1c25d7eafa3d21322378fe74
Alexander Melentyev <alexander.melentyev@icloud.com>
2021-03-30 19:12:43 +0300

    feat(database/sql): Added NullUint64

--- src/database/sql/sql.go
+++ src/database/sql/sql.go
@@ -286,6 +286,32 @@ func (n NullFloat64) Value() (driver.Value, error) {
 	return n.Float64, nil
 }
 
+// NullUint64 represents an uint64 that may be null.
+// NullUint64 implements the Scanner interface so
+// it can be used as a scan destination, similar to NullString.
+type NullUint64 struct {
+	Uint64 uint64
+	Valid  bool // Valid is true if Int64 is not NULL
+}
+
+// Scan implements the Scanner interface.
+func (n *NullUint64) Scan(value interface{}) error {
+	if value == nil {
+		n.Uint64, n.Valid = 0, false
+		return nil
+	}
+	n.Valid = true
+	return convertAssign(&n.Uint64, value)
+}
+
+// Value implements the driver Valuer interface.
+func (n NullUint64) Value() (driver.Value, error) {
+	if !n.Valid {
+		return nil, nil
+	}
+	return n.Uint64, nil
+}
+
 // NullBool represents a bool that may be null.
 // NullBool implements the Scanner interface so
 // it can be used as a scan destination, similar to NullString.
