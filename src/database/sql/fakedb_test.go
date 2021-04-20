10d625d5b41f17c118da18a592c683e60fcdcb3b
Ariel Mashraki <ariel@mashraki.co.il>
2021-04-20 16:15:29 +0300

    database/sql: add NullInt16 and NullByte
    Fixes #40082
    
    Change-Id: I01cd4d0e23c0376a6ee6e0b196c9f840cd662325
    Reviewed-on: https://go-review.googlesource.com/c/go/+/311572
    Reviewed-by: Emmanuel Odeke <emmanuel@orijtech.com>
    Reviewed-by: Daniel Theophanes <kardianos@gmail.com>
    Run-TryBot: Emmanuel Odeke <emmanuel@orijtech.com>
    TryBot-Result: Go Bot <gobot@golang.org>

--- src/database/sql/fakedb_test.go
+++ src/database/sql/fakedb_test.go
@@ -1186,9 +1186,11 @@ func converterForType(typ string) driver.ValueConverter {
 		return driver.Bool
 	case "nullbool":
 		return driver.Null{Converter: driver.Bool}
+	case "byte", "int16":
+		return driver.NotNull{Converter: driver.DefaultParameterConverter}
 	case "int32":
 		return driver.Int32
-	case "nullint32":
+	case "nullbyte", "nullint32", "nullint16":
 		return driver.Null{Converter: driver.DefaultParameterConverter}
 	case "string":
 		return driver.NotNull{Converter: fakeDriverString{}}
@@ -1222,6 +1224,10 @@ func colTypeToReflectType(typ string) reflect.Type {
 		return reflect.TypeOf(false)
 	case "nullbool":
 		return reflect.TypeOf(NullBool{})
+	case "int16":
+		return reflect.TypeOf(int16(0))
+	case "nullint16":
+		return reflect.TypeOf(NullInt16{})
 	case "int32":
 		return reflect.TypeOf(int32(0))
 	case "nullint32":
