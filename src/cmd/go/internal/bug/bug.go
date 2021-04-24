a65a095ca423c21bdd53a6a8300b501b88d60137
Manlio Perillo <manlio.perillo@gmail.com>
2021-04-24 16:19:07 +0200

    cmd/go/internal/bug: remove duplicate code
    Change-Id: I8a14b2fbb44f7ed1ea126cf27adc447f33fdf6f2
    Reviewed-on: https://go-review.googlesource.com/c/go/+/313170
    Reviewed-by: Bryan C. Mills <bcmills@google.com>
    Run-TryBot: Bryan C. Mills <bcmills@google.com>
    TryBot-Result: Go Bot <gobot@golang.org>
    Trust: Cherry Mui <cherryyz@google.com>

--- src/cmd/go/internal/bug/bug.go
+++ src/cmd/go/internal/bug/bug.go
@@ -106,8 +106,9 @@ func printGoEnv(w io.Writer) {
 }
 
 func printGoDetails(w io.Writer) {
-	printCmdOut(w, "GOROOT/bin/go version: ", filepath.Join(runtime.GOROOT(), "bin/go"), "version")
-	printCmdOut(w, "GOROOT/bin/go tool compile -V: ", filepath.Join(runtime.GOROOT(), "bin/go"), "tool", "compile", "-V")
+	gocmd := filepath.Join(runtime.GOROOT(), "bin/go")
+	printCmdOut(w, "GOROOT/bin/go version: ", gocmd, "version")
+	printCmdOut(w, "GOROOT/bin/go tool compile -V: ", gocmd, "tool", "compile", "-V")
 }
 
 func printOSDetails(w io.Writer) {
