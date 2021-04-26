903b25178e51fa96d37a91b020747ce236ba04f0
Tobias Klauser <tklauser@distanz.ch>
2021-04-27 00:11:00 +0200

    syscall: on linux use accept4 in Accept, fall back to accept
    Android seems to block the accept syscall in newer versions. Use accept4
    instead on kernel versions that support it (Linux 2.6.28 and newer) and
    fall back to accept on ENOSYS.
    
    Fixes #45767
    
    Change-Id: If190ace0e0213207fdaf6eeb79a5543ef18456de
    Reviewed-on: https://go-review.googlesource.com/c/go/+/313769
    Trust: Tobias Klauser <tobias.klauser@gmail.com>
    Run-TryBot: Tobias Klauser <tobias.klauser@gmail.com>
    Reviewed-by: Ian Lance Taylor <iant@golang.org>
    TryBot-Result: Go Bot <gobot@golang.org>

--- src/syscall/syscall_linux.go
+++ src/syscall/syscall_linux.go
@@ -553,7 +553,11 @@ func anyToSockaddr(rsa *RawSockaddrAny) (Sockaddr, error) {
 func Accept(fd int) (nfd int, sa Sockaddr, err error) {
 	var rsa RawSockaddrAny
 	var len _Socklen = SizeofSockaddrAny
-	nfd, err = accept(fd, &rsa, &len)
+	// Try accept4 first for Android, then try accept for kernel older than 2.6.28
+	nfd, err = accept4(fd, &rsa, &len, 0)
+	if err == ENOSYS {
+		nfd, err = accept(fd, &rsa, &len)
+	}
 	if err != nil {
 		return
 	}
