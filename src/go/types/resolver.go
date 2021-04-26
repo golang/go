e91cc6a6f89424632eed4b5628789cc469139249
Rob Findley <rfindley@google.com>
2021-04-26 19:23:05 -0400

    go/types: walk all imports when determining package name ambiguity
    CL 209578 disambiguated paths among imported packages, but as
    demonstrated in #43119, formatted types may reference packages that are
    not directly imported.
    
    Fix this by recursively walking all imports to determine whether there
    is any ambiguity in the import graph. This might result in
    over-qualification of names, but it is straightforward and should
    eliminate any ambiguity.
    
    In general this should be fine, but might introduce risk of infinite
    recursion in the case of an importer bug, or performance problems for
    very large import graphs. Mitigate the former by tracking seen packages,
    and the latter by only walking the import graph once an error has been
    produced.
    
    Fixes #43119
    
    Change-Id: If874f050ad0e808db8e354c2ffc88bc6d64fd277
    Reviewed-on: https://go-review.googlesource.com/c/go/+/313035
    Trust: Robert Findley <rfindley@google.com>
    Trust: Robert Griesemer <gri@golang.org>
    Run-TryBot: Robert Findley <rfindley@google.com>
    TryBot-Result: Go Bot <gobot@golang.org>
    Reviewed-by: Robert Griesemer <gri@golang.org>

--- src/go/types/resolver.go
+++ src/go/types/resolver.go
@@ -192,7 +192,11 @@ func (check *Checker) importPackage(at positioner, path, dir string) *Package {
 	// package should be complete or marked fake, but be cautious
 	if imp.complete || imp.fake {
 		check.impMap[key] = imp
-		check.pkgCnt[imp.name]++
+		// Once we've formatted an error message once, keep the pkgPathMap
+		// up-to-date on subsequent imports.
+		if check.pkgPathMap != nil {
+			check.markImports(imp)
+		}
 		return imp
 	}
 
