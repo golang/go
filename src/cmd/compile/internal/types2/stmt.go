be28caf0aa1649c4422f64e687a388a027686e6e
Robert Griesemer <gri@golang.org>
2021-04-26 12:50:44 -0700

    cmd/compile/internal/types2: respect IgnoreFuncBodies for function literals
    Updates #45783.
    
    Change-Id: Id552a60f262e2da62125acd6aec0901a82f5a29a
    Reviewed-on: https://go-review.googlesource.com/c/go/+/313650
    Trust: Robert Griesemer <gri@golang.org>
    Run-TryBot: Robert Griesemer <gri@golang.org>
    Reviewed-by: Robert Findley <rfindley@google.com>
    TryBot-Result: Go Bot <gobot@golang.org>

--- src/cmd/compile/internal/types2/stmt.go
+++ src/cmd/compile/internal/types2/stmt.go
@@ -13,6 +13,10 @@ import (
 )
 
 func (check *Checker) funcBody(decl *declInfo, name string, sig *Signature, body *syntax.BlockStmt, iota constant.Value) {
+	if check.conf.IgnoreFuncBodies {
+		panic("internal error: function body not ignored")
+	}
+
 	if check.conf.Trace {
 		check.trace(body.Pos(), "--- %s: %s", name, sig)
 		defer func() {
