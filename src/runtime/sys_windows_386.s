4b7850afd9e96a84ff87aeb0c0d2429970ba1c3c
Ian Lance Taylor <iant@golang.org>
2021-04-27 10:58:54 -0700

    runtime: consolidate Windows time constants into single copy
    Change-Id: I1a583d3da9cca4ac51f3fec9b508b7638b452d60
    Reviewed-on: https://go-review.googlesource.com/c/go/+/314270
    Trust: Ian Lance Taylor <iant@golang.org>
    Run-TryBot: Ian Lance Taylor <iant@golang.org>
    Reviewed-by: Michael Knyszek <mknyszek@google.com>

--- src/runtime/sys_windows_386.s
+++ src/runtime/sys_windows_386.s
@@ -5,6 +5,7 @@
 #include "go_asm.h"
 #include "go_tls.h"
 #include "textflag.h"
+#include "time_windows.h"
 
 // void runtime·asmstdcall(void *c);
 TEXT runtime·asmstdcall<ABIInternal>(SB),NOSPLIT,$0
@@ -330,16 +331,6 @@ TEXT runtime·switchtothread(SB),NOSPLIT,$0
 	CALL	AX
 	MOVL	BP, SP
 	RET
-
-// See https://wrkhpi.wordpress.com/2007/08/09/getting-os-information-the-kuser_shared_data-structure/
-// Archived copy at:
-// http://web.archive.org/web/20210411000829/https://wrkhpi.wordpress.com/2007/08/09/getting-os-information-the-kuser_shared_data-structure/
-// Must read hi1, then lo, then hi2. The snapshot is valid if hi1 == hi2.
-#define _INTERRUPT_TIME 0x7ffe0008
-#define _SYSTEM_TIME 0x7ffe0014
-#define time_lo 0
-#define time_hi1 4
-#define time_hi2 8
 
 TEXT runtime·nanotime1(SB),NOSPLIT,$0-8
 	CMPB	runtime·useQPCTime(SB), $0
