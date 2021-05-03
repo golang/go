9b3961f307d09ed3a1d2dc52a391b2a009acd483
Bryan C. Mills <bcmills@google.com>
2021-05-03 02:10:46 -0400

    cmd: update x/term to pull in CL 316112
    Even though x/term is now lazy, this has no overall effect (yet) on
    the contents of cmd/go.sum, because the dependency that would be
    pruned out (an old version of x/sys) is still transitively required
    through x/crypto, x/sys, and/or x/tools.
    
    Once those modules are also lazy (CL 316109, CL 316111, and CL 315570
    respectively), the extra go.sum entries for x/sys will drop out.
    
    For #36460
    Updates #36905
    
    Change-Id: I79e715328f7c417ea20ae8fe4f8e0e3eb71ee6c7
    Reviewed-on: https://go-review.googlesource.com/c/go/+/316250
    Trust: Bryan C. Mills <bcmills@google.com>
    Run-TryBot: Bryan C. Mills <bcmills@google.com>
    TryBot-Result: Go Bot <gobot@golang.org>
    Reviewed-by: Dmitri Shuralyov <dmitshur@golang.org>

--- src/cmd/go.mod
+++ src/cmd/go.mod
@@ -9,7 +9,7 @@ require (
 	golang.org/x/crypto v0.0.0-20210220033148-5ea612d1eb83 // indirect
 	golang.org/x/mod v0.4.3-0.20210409134425-858fdbee9c24
 	golang.org/x/sys v0.0.0-20210403161142-5e06dd20ab57 // indirect
-	golang.org/x/term v0.0.0-20210220032956-6a3ed077a48d
+	golang.org/x/term v0.0.0-20210503060354-a79de5458b56
 	golang.org/x/tools v0.1.1-0.20210422170518-f946a157eefe
 	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
 )
--- src/cmd/vendor/golang.org/x/term/term_unix.go
+++ src/cmd/vendor/golang.org/x/term/term_unix.go
@@ -2,8 +2,8 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-//go:build aix || darwin || dragonfly || freebsd || linux || netbsd || openbsd || zos
-// +build aix darwin dragonfly freebsd linux netbsd openbsd zos
+//go:build aix || darwin || dragonfly || freebsd || linux || netbsd || openbsd || solaris || zos
+// +build aix darwin dragonfly freebsd linux netbsd openbsd solaris zos
 
 package term
 
