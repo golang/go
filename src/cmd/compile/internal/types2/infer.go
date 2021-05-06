b5868570cb6b7b47f773f1c9b1065c6ff98545d4
Robert Griesemer <gri@golang.org>
2021-05-06 14:46:40 -0700

    cmd/compile/internal/types2: add test case for issue 45985
    This is a port of https://golang.org/cl/317471.
    
    Updates #45985.
    
    Change-Id: I539d0fa6f9a77eb2fc169a21c5db9921f8eceb03
    Reviewed-on: https://go-review.googlesource.com/c/go/+/317732
    Trust: Robert Griesemer <gri@golang.org>
    Reviewed-by: Robert Findley <rfindley@google.com>

--- src/cmd/compile/internal/types2/infer.go
+++ src/cmd/compile/internal/types2/infer.go
@@ -417,7 +417,7 @@ func (check *Checker) inferB(tparams []*TypeName, targs []Type, report bool) (ty
 	// was given, unification produced the type list [int, []C, *A]. We eliminate the
 	// remaining type parameters by substituting the type parameters in this type list
 	// until nothing changes anymore.
-	types, index = u.x.types()
+	types, _ = u.x.types()
 	if debug {
 		for i, targ := range targs {
 			assert(targ == nil || types[i] == targ)
