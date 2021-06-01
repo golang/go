b1f48e8addb640b6cbfad56d790b7702ff9fd30d
Keith Randall <khr@golang.org>
2021-06-01 16:57:59 -0700

    [dev.typeparams] cmd/compile: fix formatting
    Looks like CL 322850 didn't have the change to ARM64Ops.go
    properly gofmt'ed.
    
    Change-Id: I1a080bc13ea27b897fbb91f18ded754ce440994b
    Reviewed-on: https://go-review.googlesource.com/c/go/+/324109
    Trust: Keith Randall <khr@golang.org>
    Run-TryBot: Keith Randall <khr@golang.org>
    TryBot-Result: Go Bot <gobot@golang.org>
    Reviewed-by: Cherry Mui <cherryyz@google.com>

--- src/cmd/compile/internal/ssa/gen/ARM64Ops.go
+++ src/cmd/compile/internal/ssa/gen/ARM64Ops.go
@@ -759,17 +759,17 @@ func init() {
 	}
 
 	archs = append(archs, arch{
-		name:            "ARM64",
-		pkg:             "cmd/internal/obj/arm64",
-		genfile:         "../../arm64/ssa.go",
-		ops:             ops,
-		blocks:          blocks,
-		regnames:        regNamesARM64,
+		name:               "ARM64",
+		pkg:                "cmd/internal/obj/arm64",
+		genfile:            "../../arm64/ssa.go",
+		ops:                ops,
+		blocks:             blocks,
+		regnames:           regNamesARM64,
 		ParamIntRegNames:   "R0 R1 R2 R3 R4 R5 R6 R7 R8 R9 R10 R11 R12 R13 R14 R15",
 		ParamFloatRegNames: "F0 F1 F2 F3 F4 F5 F6 F7 F8 F9 F10 F11 F12 F13 F14 F15",
-		gpregmask:       gp,
-		fpregmask:       fp,
-		framepointerreg: -1, // not used
-		linkreg:         int8(num["R30"]),
+		gpregmask:          gp,
+		fpregmask:          fp,
+		framepointerreg:    -1, // not used
+		linkreg:            int8(num["R30"]),
 	})
 }
