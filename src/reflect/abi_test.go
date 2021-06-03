6776bc3d8c9eb2db1bf1bf0fb76ef81ee4cd1a6f
Cherry Mui <cherryyz@google.com>
2021-06-03 15:23:22 -0400

    [dev.typeparams] reflect: guard abi_test.go with regabiargs build tag
    The test in abi_test.go relies on the compiler to generate
    register-ABI calls using a magic name. As of CL 300150 the name
    loses its magic. Guard it with regabiargs for the use of
    register-ABI calls.
    
    Change-Id: Ib8b3c24f71ea5161d607c9becfb3027ceee40ac1
    Reviewed-on: https://go-review.googlesource.com/c/go/+/324767
    Trust: Cherry Mui <cherryyz@google.com>
    Run-TryBot: Cherry Mui <cherryyz@google.com>
    Reviewed-by: Michael Knyszek <mknyszek@google.com>
    TryBot-Result: Go Bot <gobot@golang.org>

--- src/reflect/abi_test.go
+++ src/reflect/abi_test.go
@@ -2,8 +2,8 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-//go:build goexperiment.regabireflect
-// +build goexperiment.regabireflect
+//go:build goexperiment.regabireflect && goexperiment.regabiargs
+// +build goexperiment.regabireflect,goexperiment.regabiargs
 
 package reflect_test
 
