8177466ece242cd1ce83ae33b6637c84a05ba20d
Katie Hockman <katie@golang.org>
2021-06-07 14:29:43 -0400

    net/url: reject query values with semicolons
    Semicolons are no longer valid separators, so
    net/url.ParseQuery will now return an error
    if any part of the query contains a semicolon.
    
    net/http.(*Request).ParseMultipartForm has been
    changed to fall through and continue parsing
    even if the call to (*Request).ParseForm fails.
    
    This change also includes a few minor refactors
    to existing tests.
    
    Fixes #25192
    
    Change-Id: Iba3f108950fb99b9288e402c41fe71ca3a2ababd
    Reviewed-on: https://go-review.googlesource.com/c/go/+/325697
    Trust: Katie Hockman <katie@golang.org>
    Run-TryBot: Katie Hockman <katie@golang.org>
    TryBot-Result: Go Bot <gobot@golang.org>
    Reviewed-by: Filippo Valsorda <filippo@golang.org>

--- src/net/http/server.go
+++ src/net/http/server.go
@@ -2863,6 +2863,11 @@ func (sh serverHandler) ServeHTTP(rw ResponseWriter, req *Request) {
 		handler = globalOptionsHandler{}
 	}
 	handler.ServeHTTP(rw, req)
+	if req.URL != nil && strings.Contains(req.URL.RawQuery, ";") {
+		// TODO(filippo): update this not to log if the special
+		// semicolon handler was called.
+		sh.srv.logf("http: URL query contains semicolon, which is no longer a supported separator; parts of the query may be stripped when parsed; see golang.org/issue/25192")
+	}
 }
 
 // ListenAndServe listens on the TCP network address srv.Addr and then
