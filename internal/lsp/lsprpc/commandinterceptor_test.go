e3040f2721b947b2c37bb385b92ee8ef3cb9113d
Rob Findley <rfindley@google.com>
2021-06-28 11:32:20 -0400

    internal/lsp/lsprpc: add a CommandInterceptor middleware
    This will be used for starting the debug server along the serving path.
    
    Change-Id: I19d13753ad70c179a53f348ed574aaea19c0d301
    Reviewed-on: https://go-review.googlesource.com/c/tools/+/331770
    Trust: Robert Findley <rfindley@google.com>
    Run-TryBot: Robert Findley <rfindley@google.com>
    gopls-CI: kokoro <noreply+kokoro@google.com>
    TryBot-Result: Go Bot <gobot@golang.org>
    Reviewed-by: Ian Cottrell <iancottrell@google.com>

---
+++ internal/lsp/lsprpc/commandinterceptor_test.go
@@ -0,0 +1,42 @@
+// Copyright 2021 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package lsprpc_test
+
+import (
+	"context"
+	"testing"
+
+	"golang.org/x/tools/internal/lsp/protocol"
+
+	. "golang.org/x/tools/internal/lsp/lsprpc"
+)
+
+func TestCommandInterceptor(t *testing.T) {
+	const command = "foo"
+	caught := false
+	intercept := func(_ *protocol.ExecuteCommandParams) (interface{}, error) {
+		caught = true
+		return map[string]interface{}{}, nil
+	}
+
+	ctx := context.Background()
+	env := new(TestEnv)
+	defer env.Shutdown(t)
+	mw := CommandInterceptor(command, intercept)
+	l, _ := env.serve(ctx, t, mw(noopBinder))
+	conn := env.dial(ctx, t, l.Dialer(), noopBinder, false)
+
+	params := &protocol.ExecuteCommandParams{
+		Command: command,
+	}
+	var res interface{}
+	err := conn.Call(ctx, "workspace/executeCommand", params).Await(ctx, &res)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if !caught {
+		t.Errorf("workspace/executeCommand was not intercepted")
+	}
+}
