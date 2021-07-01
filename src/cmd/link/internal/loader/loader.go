a80f36ae60af3e1f478da68c9a732367fbeacd12
Tao Qingyun <qingyunha@gmail.com>
2021-07-01 00:22:51 +0000

    cmd/link: make npkgsyms meaningful
    npkgsyms always equals 1 currently.
    
    Change-Id: Ie88bc273beaace12ec7432054ca7c151396e8e35
    GitHub-Last-Rev: c5d3b12b88ddcdc6011f97787fd7c0e94c7df20e
    GitHub-Pull-Request: golang/go#46988
    Reviewed-on: https://go-review.googlesource.com/c/go/+/331929
    Reviewed-by: Cherry Mui <cherryyz@google.com>
    Run-TryBot: Cherry Mui <cherryyz@google.com>
    TryBot-Result: Go Bot <gobot@golang.org>
    Trust: Dmitri Shuralyov <dmitshur@golang.org>

--- src/cmd/link/internal/loader/loader.go
+++ src/cmd/link/internal/loader/loader.go
@@ -2209,7 +2209,6 @@ func (l *Loader) LoadSyms(arch *sys.Arch) {
 	// Index 0 is invalid for symbols.
 	l.objSyms = make([]objSym, 1, symSize)
 
-	l.npkgsyms = l.NSym()
 	st := loadState{
 		l:            l,
 		hashed64Syms: make(map[uint64]symAndSize, hashed64Size),
@@ -2219,6 +2218,7 @@ func (l *Loader) LoadSyms(arch *sys.Arch) {
 	for _, o := range l.objs[goObjStart:] {
 		st.preloadSyms(o.r, pkgDef)
 	}
+	l.npkgsyms = l.NSym()
 	for _, o := range l.objs[goObjStart:] {
 		st.preloadSyms(o.r, hashed64Def)
 		st.preloadSyms(o.r, hashedDef)
