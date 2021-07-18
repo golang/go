1fd8a9c8d3dcdbfacd4a326fb5143df795d6b596
Dan Scales <danscales@google.com>
2021-07-18 11:09:12 -0700

    [dev.typeparams] Get dictionaryCapture.go working.
    METHVALUE in a generic function (that is not called) was not causing
    buildClosure() to be called and therefore not using dictionaries. Also,
    had to add an extra check to make sure that if we have a FUNCINST
    node above a METHVALUE, we only call buildClosure once.
    
    Change-Id: I49756152fc343e5ac1c449e697960fc2a0f482ae
    Reviewed-on: https://go-review.googlesource.com/c/go/+/336429
    Reviewed-by: Keith Randall <khr@golang.org>
    Trust: Dan Scales <danscales@google.com>
    Run-TryBot: Dan Scales <danscales@google.com>
    TryBot-Result: Go Bot <gobot@golang.org>

--- test/run.go
+++ test/run.go
@@ -2174,10 +2174,9 @@ var g3Failures = setOf(
 
 	"typeparam/mdempsky/4.go", // -G=3 can't export functions with labeled breaks in loops
 
-	"typeparam/cons.go",              // causes an unreachable method
-	"typeparam/dictionaryCapture.go", // segv, dictionary access failure?
-	"typeparam/issue44688.go",        // interface conversion fails due to missing method
-	"typeparam/mdempsky/14.go",       // interface comparison failure
+	"typeparam/cons.go",        // causes an unreachable method
+	"typeparam/issue44688.go",  // interface conversion fails due to missing method
+	"typeparam/mdempsky/14.go", // interface comparison failure
 )
 
 var unifiedFailures = setOf(
