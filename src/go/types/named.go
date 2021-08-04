e79449dde4d9fe3ff757e28be03f3849c426a8d5
Rob Findley <rfindley@google.com>
2021-08-03 21:43:39 -0400

    [dev.typeparams] go/types: move instance.go contents into named.go (cleanup)
    This is a port of CL 338469 to go/types.
    
    Change-Id: I3ee655fa2dc7e789f210c8dec171b3358c4ff132
    Reviewed-on: https://go-review.googlesource.com/c/go/+/339677
    Trust: Robert Findley <rfindley@google.com>
    Run-TryBot: Robert Findley <rfindley@google.com>
    TryBot-Result: Go Bot <gobot@golang.org>
    Reviewed-by: Robert Griesemer <gri@golang.org>

--- src/go/types/named.go
+++ src/go/types/named.go
@@ -4,7 +4,10 @@
 
 package types
 
-import "sync"
+import (
+	"go/token"
+	"sync"
+)
 
 // TODO(rfindley) Clean up Named struct below; specifically the fromRHS field (can we use underlying?).
 
@@ -251,4 +254,39 @@ func (n *Named) setUnderlying(typ Type) {
 	if n != nil {
 		n.underlying = typ
 	}
+}
+
+// instance holds position information for use in lazy instantiation.
+//
+// TODO(rfindley): come up with a better name for this type, now that its usage
+// has changed.
+type instance struct {
+	pos     token.Pos   // position of type instantiation; for error reporting only
+	posList []token.Pos // position of each targ; for error reporting only
+}
+
+// expand ensures that the underlying type of n is instantiated.
+// The underlying type will be Typ[Invalid] if there was an error.
+// TODO(rfindley): expand would be a better name for this method, but conflicts
+// with the existing concept of lazy expansion. Need to reconcile this.
+func (n *Named) expand() {
+	if n.instance != nil {
+		// n must be loaded before instantiation, in order to have accurate
+		// tparams. This is done implicitly by the call to n.TParams, but making it
+		// explicit is harmless: load is idempotent.
+		n.load()
+		inst := n.check.instantiate(n.instance.pos, n.orig.underlying, n.TParams().list(), n.targs, n.instance.posList)
+		n.underlying = inst
+		n.fromRHS = inst
+		n.instance = nil
+	}
+}
+
+// expand expands uninstantiated named types and leaves all other types alone.
+// expand does not recurse.
+func expand(typ Type) Type {
+	if t, _ := typ.(*Named); t != nil {
+		t.expand()
+	}
+	return typ
 }
