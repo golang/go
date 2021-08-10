d22cc3f989bf6a5f75a1baca6913eaa914e7ef6e
Dmitri Shuralyov <dmitshur@golang.org>
2021-08-09 20:29:14 -0400

    all: gofmt more (but vendor, testdata, and top-level test directories)
    CL 294430 made packages in std and cmd modules use Go 1.17 gofmt format,
    adding //go:build lines. This change applies the same formatting to some
    more packages that 'go fmt' missed (e.g., syscall/js, runtime/msan), and
    everything else that is easy and safe to modify in bulk.
    
    Consider the top-level test directory, testdata, and vendor directories
    out of scope, since there are many files that don't follow strict gofmt
    formatting, often for intentional and legitimate reasons (testing gofmt
    itself, invalid Go programs that shouldn't crash the compiler, etc.).
    
    That makes it easy and safe to gofmt -w the .go files that are found
    with gofmt -l with aforementioned directories filtered out:
    
    	$ gofmt -l . 2>/dev/null | \
    		grep -v '^test/' | \
    		grep -v '/testdata/' | \
    		grep -v '/vendor/' | wc -l
    	      51
    
    None of the 51 files are generated. After this change, the same command
    prints 0.
    
    For #41184.
    
    Change-Id: Ia96ee2a0f998d6a167d4473bcad17ad09bc1d86e
    Reviewed-on: https://go-review.googlesource.com/c/go/+/341009
    Run-TryBot: Dmitri Shuralyov <dmitshur@golang.org>
    TryBot-Result: Go Bot <gobot@golang.org>
    Reviewed-by: Ian Lance Taylor <iant@golang.org>
    Trust: Dmitri Shuralyov <dmitshur@golang.org>

--- misc/android/go_android_exec.go
+++ misc/android/go_android_exec.go
@@ -2,6 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
+//go:build ignore
 // +build ignore
 
 // This program can be used as go_android_GOARCH_exec by the Go tool.
