ace475b28e04cae546bfc222e1eac3607cf87e54
Dan Scales <danscales@google.com>
2021-08-12 09:58:54 -0700

    [dev.typeparams] cmd/compile:  remove some shape checks in type substituter, other cleanups
    The type substituter (typecheck.Typ()) was temporarily substituting from
    shapes back to concrete types, but doesn't need to anymore. So, remove
    two shape checks, so the type substituter is now only for substituting
    type params again.
    
    Several other cleanups:
    
     - renamed makeGenericName() to makeInstName1(), since that function is
       a helper to MakeInstName() and MakeDictName() that definitely makes
       instantiated names, not generic names.
    
     - removed the logic in makeInstName1() that adds the ".inst." prefix
       for concrete type args. We are only specifying concrete type args (as
       opposed to shape args) when we are calling from MakeDictName, and
       then we immediately strip of the .inst prefix anyway.
    
     - Added a comment on types.Identical that a shape type is considered
       identicall to another type if their underlying types are the same, or
       they are both pointers.
    
    Change-Id: I3e0206dbd403897797ae7bec3c527ae16b0b930a
    Reviewed-on: https://go-review.googlesource.com/c/go/+/341729
    Run-TryBot: Dan Scales <danscales@google.com>
    TryBot-Result: Go Bot <gobot@golang.org>
    Reviewed-by: Keith Randall <khr@golang.org>
    Trust: Dan Scales <danscales@google.com>

--- src/cmd/compile/internal/types/identity.go
+++ src/cmd/compile/internal/types/identity.go
@@ -4,8 +4,11 @@
 
 package types
 
-// Identical reports whether t1 and t2 are identical types, following
-// the spec rules. Receiver parameter types are ignored.
+// Identical reports whether t1 and t2 are identical types, following the spec rules.
+// Receiver parameter types are ignored. Named (defined) types are only equal if they
+// are pointer-equal - i.e. there must be a unique types.Type for each specific named
+// type. Also, a type containing a shape type is considered identical to another type
+// (shape or not) if their underlying types are the same, or they are both pointers.
 func Identical(t1, t2 *Type) bool {
 	return identical(t1, t2, true, nil)
 }
