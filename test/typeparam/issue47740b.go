687f2acf6ae00226e8304e15f156f36e19ac971f
Keith Randall <khr@golang.org>
2021-08-17 10:40:44 -0700

    cmd/compile: only use dictionaries for conversions to type parameters
    Conversions to regular concrete types should not be rewritten during
    stenciling.
    
    Fixes #47740
    
    Change-Id: I2b45e22f962dcd2e18bd6cc876ebc0f850860822
    Reviewed-on: https://go-review.googlesource.com/c/go/+/342989
    Trust: Keith Randall <khr@golang.org>
    Trust: Dan Scales <danscales@google.com>
    Trust: Cuong Manh Le <cuong.manhle.vn@gmail.com>
    Run-TryBot: Keith Randall <khr@golang.org>
    TryBot-Result: Go Bot <gobot@golang.org>
    Reviewed-by: Dan Scales <danscales@google.com>

---
+++ test/typeparam/issue47740b.go
@@ -0,0 +1,23 @@
+// run -gcflags=-G=3
+
+// Copyright 2021 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+import "reflect"
+
+type S[T any] struct {
+	a interface{}
+}
+
+func (e S[T]) M() {
+	v := reflect.ValueOf(e.a)
+	_, _ = v.Interface().(int)
+}
+
+func main() {
+	e := S[int]{0}
+	e.M()
+}
