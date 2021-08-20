3081f817da8c194982596ddddf5d3ec321c859af
Keith Randall <khr@golang.org>
2021-08-20 16:15:53 -0700

    cmd/compile: always remove receiver type from instantiated method values
    If a type T has a method foo, then
    
    var t T
    var i interface{} = t.foo
    
    The type of foo is a method type, but the type of t.foo should be a
    standard function type. Make sure we always do that conversion.
    
    Fixes #47775
    
    Change-Id: I464ec792196b050aba1914e070a4ede34bfd0bfa
    Reviewed-on: https://go-review.googlesource.com/c/go/+/343881
    Trust: Keith Randall <khr@golang.org>
    Trust: Dan Scales <danscales@google.com>
    Reviewed-by: Matthew Dempsky <mdempsky@google.com>
    Reviewed-by: Dan Scales <danscales@google.com>

---
+++ test/typeparam/issue47775b.go
@@ -0,0 +1,28 @@
+// run -gcflags=-G=3
+
+// Copyright 2021 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+type C[T any] struct {
+}
+
+func (c *C[T]) reset() {
+}
+
+func New[T any]() {
+	c := &C[T]{}
+	i = c.reset
+	z(c.reset)
+}
+
+var i interface{}
+
+func z(interface{}) {
+}
+
+func main() {
+	New[int]()
+}
