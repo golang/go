5a687eeaf186dd109e53860db15f8465b0456fc0
Joel Sing <joel@sing.id.au>
2021-08-21 18:40:20 +1000

    cmd/asm,cmd/internal/obj/riscv: add more error tests for riscv64 assembly
    Add more error tests for riscv64 assembly. Also avoid a panic when one of
    these error conditions is hit.
    
    Change-Id: If5d913894facbd67f7d014eab745da77c8c66ab0
    Reviewed-on: https://go-review.googlesource.com/c/go/+/344228
    Trust: Joel Sing <joel@sing.id.au>
    Run-TryBot: Joel Sing <joel@sing.id.au>
    TryBot-Result: Go Bot <gobot@golang.org>
    Reviewed-by: Meng Zhuo <mzh@golangcn.org>

--- src/cmd/asm/internal/asm/testdata/riscv64error.s
+++ src/cmd/asm/internal/asm/testdata/riscv64error.s
@@ -3,6 +3,14 @@
 // license that can be found in the LICENSE file.
 
 TEXT errors(SB),$0
+	MOV	$errors(SB), (X5)		// ERROR "unsupported addr MOV"
+	MOV	$8(SP), (X5)			// ERROR "unsupported addr MOV"
+	MOVB	$8(SP), X5			// ERROR "unsupported addr MOV"
+	MOVH	$8(SP), X5			// ERROR "unsupported addr MOV"
+	MOVW	$8(SP), X5			// ERROR "unsupported addr MOV"
+	MOVF	$8(SP), X5			// ERROR "unsupported addr MOV"
+	MOV	$1234, 0(SP)			// ERROR "constant load must target register"
+	MOV	$1234, 8(SP)			// ERROR "constant load must target register"
 	MOV	$0, 0(SP)			// ERROR "constant load must target register"
 	MOV	$0, 8(SP)			// ERROR "constant load must target register"
 	MOV	$1234, 0(SP)			// ERROR "constant load must target register"
@@ -11,4 +19,8 @@ TEXT errors(SB),$0
 	MOVH	$1, X5				// ERROR "unsupported constant load"
 	MOVW	$1, X5				// ERROR "unsupported constant load"
 	MOVF	$1, X5				// ERROR "unsupported constant load"
+	MOVBU	X5, (X6)			// ERROR "unsupported unsigned store"
+	MOVHU	X5, (X6)			// ERROR "unsupported unsigned store"
+	MOVWU	X5, (X6)			// ERROR "unsupported unsigned store"
+
 	RET
