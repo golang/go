c32dd46c0050e1e18abd2054a9f9a607c2fadfaa
smasher164 <aindurti@gmail.com>
2021-08-24 01:25:55 -0400

    internal/imports: update stdlib index for 1.17
        $ go run mkstdlib.go
    
    Updates golang/go#38706.
    
    Change-Id: I0ad28f7d5e1fec2d1ccc3bc0f01f43a7d2f0d817
    Reviewed-on: https://go-review.googlesource.com/c/tools/+/344629
    Run-TryBot: Akhil Indurti <aindurti@gmail.com>
    gopls-CI: kokoro <noreply+kokoro@google.com>
    TryBot-Result: Go Bot <gobot@golang.org>
    Reviewed-by: Dmitri Shuralyov <dmitshur@golang.org>
    Reviewed-by: Heschi Kreinick <heschi@google.com>
    Trust: Dmitri Shuralyov <dmitshur@golang.org>

--- internal/imports/mkstdlib.go
+++ internal/imports/mkstdlib.go
@@ -69,6 +69,7 @@ func main() {
 		mustOpen(api("go1.14.txt")),
 		mustOpen(api("go1.15.txt")),
 		mustOpen(api("go1.16.txt")),
+		mustOpen(api("go1.17.txt")),
 
 		// The API of the syscall/js package needs to be computed explicitly,
 		// because it's not included in the GOROOT/api/go1.*.txt files at this time.
