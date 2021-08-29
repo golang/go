31f611fdbb2357671a1efe450d409679517624c1
Dirkjan Bussink <d.bussink@gmail.com>
2021-08-29 20:01:35 +0200

    internal/cpu: Add SHA CPU extension flag
    This adds the SHA extension flag so it can be used to detect if SHA
    extensions are available to speed up SHA1 and SHA256 computations.

--- src/internal/cpu/cpu.go
+++ src/internal/cpu/cpu.go
@@ -41,6 +41,7 @@ var X86 struct {
 	HasSSSE3     bool
 	HasSSE41     bool
 	HasSSE42     bool
+	HasSHA       bool
 	_            CacheLinePad
 }
 
--- src/internal/cpu/cpu_x86.go
+++ src/internal/cpu/cpu_x86.go
@@ -36,6 +36,7 @@ const (
 	cpuid_BMI2 = 1 << 8
 	cpuid_ERMS = 1 << 9
 	cpuid_ADX  = 1 << 19
+	cpuid_SHA  = 1 << 29
 
 	// edx bits for CPUID 0x80000001
 	cpuid_RDTSCP = 1 << 27
@@ -56,6 +57,7 @@ func doinit() {
 		{Name: "pclmulqdq", Feature: &X86.HasPCLMULQDQ},
 		{Name: "popcnt", Feature: &X86.HasPOPCNT},
 		{Name: "rdtscp", Feature: &X86.HasRDTSCP},
+		{Name: "sha", Feature: &X86.HasSHA},
 		{Name: "sse3", Feature: &X86.HasSSE3},
 		{Name: "sse41", Feature: &X86.HasSSE41},
 		{Name: "sse42", Feature: &X86.HasSSE42},
@@ -111,6 +113,7 @@ func doinit() {
 	X86.HasBMI2 = isSet(ebx7, cpuid_BMI2)
 	X86.HasERMS = isSet(ebx7, cpuid_ERMS)
 	X86.HasADX = isSet(ebx7, cpuid_ADX)
+	X86.HasSHA = isSet(ebx7, cpuid_SHA)
 
 	var maxExtendedInformation uint32
 	maxExtendedInformation, _, _, _ = cpuid(0x80000000, 0)
