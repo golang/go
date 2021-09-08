466b542a2ea5a74cffadee34e4764d77028c0cf1
Robert Griesemer <gri@golang.org>
2021-09-08 13:30:36 -0700

    cmd/compile/internal/types2: implement deduplication of instances using the Environment
    This is a port of CL 344390 with adjustments to names to make it
    work for types2.
    
    Change-Id: I05c33d9858f973adfbf48d8a1faaf377280f6985
    Reviewed-on: https://go-review.googlesource.com/c/go/+/348572
    Trust: Robert Griesemer <gri@golang.org>
    Reviewed-by: Robert Findley <rfindley@google.com>

--- src/cmd/compile/internal/types2/named.go
+++ src/cmd/compile/internal/types2/named.go
@@ -242,7 +242,7 @@ func (n *Named) setUnderlying(typ Type) {
 
 // expand ensures that the underlying type of n is instantiated.
 // The underlying type will be Typ[Invalid] if there was an error.
-func (n *Named) expand(typMap map[string]*Named) *Named {
+func (n *Named) expand(env *Environment) *Named {
 	if n.instPos != nil {
 		// n must be loaded before instantiation, in order to have accurate
 		// tparams. This is done implicitly by the call to n.TParams, but making it
@@ -250,19 +250,25 @@ func (n *Named) expand(typMap map[string]*Named) *Named {
 		n.load()
 		var u Type
 		if n.check.validateTArgLen(*n.instPos, n.tparams.Len(), n.targs.Len()) {
-			if typMap == nil {
+			// TODO(rfindley): handling an optional Checker and Environment here (and
+			// in subst) feels overly complicated. Can we simplify?
+			if env == nil {
 				if n.check != nil {
-					typMap = n.check.typMap
+					env = n.check.env
 				} else {
 					// If we're instantiating lazily, we might be outside the scope of a
 					// type-checking pass. In that case we won't have a pre-existing
-					// typMap, but don't want to create a duplicate of the current instance
-					// in the process of expansion.
-					h := typeHash(n.orig, n.targs.list())
-					typMap = map[string]*Named{h: n}
+					// environment, but don't want to create a duplicate of the current
+					// instance in the process of expansion.
+					env = NewEnvironment()
 				}
+				h := env.typeHash(n.orig, n.targs.list())
+				// add the instance to the environment to avoid infinite recursion.
+				// addInstance may return a different, existing instance, but we
+				// shouldn't return that instance from expand.
+				env.typeForHash(h, n)
 			}
-			u = n.check.subst(*n.instPos, n.orig.underlying, makeSubstMap(n.TParams().list(), n.targs.list()), typMap)
+			u = n.check.subst(*n.instPos, n.orig.underlying, makeSubstMap(n.TParams().list(), n.targs.list()), env)
 		} else {
 			u = Typ[Invalid]
 		}
