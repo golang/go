5492d01d98d3be4e014a66fc31953c04cc38c8c8
Robert Findley <rfindley@google.com>
2021-09-14 19:16:13 -0400

    internal/lsp/testdata: update inferred.go to use hoverdef
    Change-Id: I0245a594680c8aaf6e2664eafd97ed8ad5ce0ac3
    Reviewed-on: https://go-review.googlesource.com/c/tools/+/350029
    Trust: Robert Findley <rfindley@google.com>
    Run-TryBot: Robert Findley <rfindley@google.com>
    gopls-CI: kokoro <noreply+kokoro@google.com>
    TryBot-Result: Go Bot <gobot@golang.org>
    Reviewed-by: Robert Griesemer <gri@golang.org>

--- internal/lsp/testdata/godef/infer_generics/inferred.go
+++ internal/lsp/testdata/godef/infer_generics/inferred.go
@@ -5,8 +5,8 @@ func app[S interface{ ~[]E }, E any](s S, e E) S {
 }
 
 func _() {
-	_ = app[[]int]             //@mark(constrInfer, "app"),hover("app", constrInfer)
-	_ = app[[]int, int]        //@mark(instance, "app"),hover("app", instance)
-	_ = app[[]int]([]int{}, 0) //@mark(partialInfer, "app"),hover("app", partialInfer)
-	_ = app([]int{}, 0)        //@mark(argInfer, "app"),hover("app", argInfer)
+	_ = app[[]int]             //@mark(constrInfer, "app"),hoverdef("app", constrInfer)
+	_ = app[[]int, int]        //@mark(instance, "app"),hoverdef("app", instance)
+	_ = app[[]int]([]int{}, 0) //@mark(partialInfer, "app"),hoverdef("app", partialInfer)
+	_ = app([]int{}, 0)        //@mark(argInfer, "app"),hoverdef("app", argInfer)
 }
