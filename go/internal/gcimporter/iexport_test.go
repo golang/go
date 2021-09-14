92077074d40106c4de535e3d6f3716dd54bedf5c
Robert Findley <rfindley@google.com>
2021-09-14 18:08:45 -0400

    go/internal/gcimporter: skip TestIExportData_stdlib on go1.18
    This test will fail on Go 1.18 once standard library changes are
    introduced that use generics. Skip it until iexport.go has been updated
    to support generics.
    
    Updates golang/go#48392
    
    Change-Id: I0f779689daaf2bc3127affff633c883ed9d7289b
    Reviewed-on: https://go-review.googlesource.com/c/tools/+/349954
    Trust: Robert Findley <rfindley@google.com>
    Run-TryBot: Robert Findley <rfindley@google.com>
    Reviewed-by: Robert Griesemer <gri@golang.org>
    gopls-CI: kokoro <noreply+kokoro@google.com>
    TryBot-Result: Go Bot <gobot@golang.org>

--- go/internal/gcimporter/iexport_test.go
+++ go/internal/gcimporter/iexport_test.go
@@ -31,6 +31,7 @@ import (
 	"golang.org/x/tools/go/buildutil"
 	"golang.org/x/tools/go/internal/gcimporter"
 	"golang.org/x/tools/go/loader"
+	"golang.org/x/tools/internal/testenv"
 )
 
 func readExportFile(filename string) ([]byte, error) {
@@ -63,6 +64,7 @@ func iexport(fset *token.FileSet, pkg *types.Package) ([]byte, error) {
 }
 
 func TestIExportData_stdlib(t *testing.T) {
+	testenv.SkipAfterGo1Point(t, 17)
 	if runtime.Compiler == "gccgo" {
 		t.Skip("gccgo standard library is inaccessible")
 	}
