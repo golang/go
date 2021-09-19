ff8a7e513bda9e89dfbde8ecefaa4514c11ec596
Leonard Wang <wangdeyu0907@gmail.com>
2021-09-20 00:23:40 +0800

    cmd/compile: print expression for invalid operation errors
    For #48472
    
    Change-Id: I5072ebcf53e03fb5515c51a2ad01f02d72b30719
    Reviewed-on: https://go-review.googlesource.com/c/go/+/350929
    Reviewed-by: Robert Griesemer <gri@golang.org>
    Trust: David Chase <drchase@google.com>

--- src/go/types/expr.go
+++ src/go/types/expr.go
@@ -994,7 +994,7 @@ func (check *Checker) binary(x *operand, e ast.Expr, lhs, rhs ast.Expr, op token
 			if e != nil {
 				posn = e
 			}
-			check.invalidOp(posn, _MismatchedTypes, "mismatched types %s and %s", x.typ, y.typ)
+			check.invalidOp(posn, _MismatchedTypes, "%s (mismatched types %s and %s)", e, x.typ, y.typ)
 		}
 		x.mode = invalid
 		return
