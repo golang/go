8e36ab055162efa6f67f3b9ee62f625ac8874901
Russ Cox <rsc@golang.org>
2021-09-21 10:59:16 -0400

    bytes, strings: add Cut
    Using Cut is a clearer way to write the vast majority (>70%)
    of existing code that calls Index, IndexByte, IndexRune, and SplitN.
    There is more discussion on https://golang.org/issue/46336.
    
    Fixes #46336.
    
    Change-Id: Ia418ed7c3706c65bf61e1b2c5baf534cb783e4d3
    Reviewed-on: https://go-review.googlesource.com/c/go/+/351710
    Trust: Russ Cox <rsc@golang.org>
    Run-TryBot: Russ Cox <rsc@golang.org>
    TryBot-Result: Go Bot <gobot@golang.org>
    Reviewed-by: Ian Lance Taylor <iant@golang.org>

--- src/strings/strings.go
+++ src/strings/strings.go
@@ -1118,3 +1118,14 @@ func Index(s, substr string) int {
 	}
 	return -1
 }
+
+// Cut slices s around the first instance of sep,
+// returning the text before and after sep.
+// The found result reports whether sep appears in s.
+// If sep does not appear in s, cut returns s, "", false.
+func Cut(s, sep string) (before, after string, found bool) {
+	if i := Index(s, sep); i >= 0 {
+		return s[:i], s[i+len(sep):], true
+	}
+	return s, "", false
+}
