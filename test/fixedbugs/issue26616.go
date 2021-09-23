0626ac064d9300d16e1e5878777efc0430088569
Robert Griesemer <gri@golang.org>
2021-09-22 17:08:00 -0700

    cmd/compile: restore original assignment error messages
    When used with the compiler, types2 will report assignment error
    messages that closely match what the compiler type checker (types1)
    produces.
    
    Also, mark lhs variables as used in invalid variable initializations
    to avoid a class of follow-on errors.
    
    Fixes #48558.
    
    Change-Id: I92d1de006c66b3a2364bb1bea773a312963afe75
    Reviewed-on: https://go-review.googlesource.com/c/go/+/351669
    Trust: Robert Griesemer <gri@golang.org>
    Run-TryBot: Robert Griesemer <gri@golang.org>
    TryBot-Result: Go Bot <gobot@golang.org>
    Reviewed-by: Robert Findley <rfindley@google.com>

--- test/fixedbugs/issue26616.go
+++ test/fixedbugs/issue26616.go
@@ -6,11 +6,11 @@
 
 package p
 
-var x int = three() // ERROR "assignment mismatch: 1 variable but three returns 3 values|multiple-value function call in single-value context|3\-valued"
+var x int = three() // ERROR "assignment mismatch: 1 variable but three returns 3 values|multiple-value function call in single-value context|multiple-value "
 
 func f() {
-	var _ int = three() // ERROR "assignment mismatch: 1 variable but three returns 3 values|multiple-value function call in single-value context|3\-valued"
-	var a int = three() // ERROR "assignment mismatch: 1 variable but three returns 3 values|multiple-value function call in single-value context|3\-valued"
+	var _ int = three() // ERROR "assignment mismatch: 1 variable but three returns 3 values|multiple-value function call in single-value context|multiple-value "
+	var a int = three() // ERROR "assignment mismatch: 1 variable but three returns 3 values|multiple-value function call in single-value context|multiple-value "
 	a = three()         // ERROR "assignment mismatch: 1 variable but three returns 3 values|multiple-value function call in single-value context|cannot assign"
 	b := three()        // ERROR "assignment mismatch: 1 variable but three returns 3 values|single variable set to multiple-value|multiple-value function call in single-value context|cannot initialize"
 	_, _ = a, b
