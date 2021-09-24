d624e3ba81143d1d7899e92c047ccb9287f27c6e
jiahua wang <wjh180909@gmail.com>
2021-09-24 10:07:44 +0800

    bytes: add example for (*Buffer).Next
    Change-Id: Ic0a97fd7bb89865448e436e5c092415a29d8badf
    Reviewed-on: https://go-review.googlesource.com/c/go/+/352009
    Run-TryBot: Ian Lance Taylor <iant@golang.org>
    TryBot-Result: Go Bot <gobot@golang.org>
    Reviewed-by: Ian Lance Taylor <iant@golang.org>
    Trust: David Chase <drchase@google.com>

--- src/bytes/example_test.go
+++ src/bytes/example_test.go
@@ -54,6 +54,19 @@ func ExampleBuffer_Len() {
 	// Output: 5
 }
 
+func ExampleBuffer_Next() {
+	var b bytes.Buffer
+	b.Grow(64)
+	b.Write([]byte("abcde"))
+	fmt.Printf("%s\n", string(b.Next(2)))
+	fmt.Printf("%s\n", string(b.Next(2)))
+	fmt.Printf("%s", string(b.Next(2)))
+	// Output:
+	// ab
+	// cd
+	// e
+}
+
 func ExampleCompare() {
 	// Interpret Compare's result by comparing it to zero.
 	var a, b []byte
