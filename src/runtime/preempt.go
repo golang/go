5aa4503c264e1b8621ad164fdd82dec819c964ba
Josh Bleecher Snyder <josharian@gmail.com>
2021-10-01 16:25:32 -0700

    cmd, runtime: eliminate runtime.no_pointers_stackmap
    runtime.no_pointers_stackmap is an odd beast.
    It is defined in a Go file, populated by assembly,
    used by the GC, and its address is magic used
    by async pre-emption to ascertain whether a
    routine was implemented in assembly.
    
    A subsequent change will force all GC data into the go.func.* linker symbol.
    runtime.no_pointers_stackmap is GC data, so it must go there.
    Yet it also needs to go into rodata, for the runtime address trick.
    
    This change eliminates it entirely.
    
    Replace the runtime address check with the newly introduced asm funcflag.
    
    Handle the assembly macro as magic, similarly to our handling of go_args_stackmap.
    This allows the no_pointers_stackmap to be identical in all ways
    to other gclocals stackmaps, including content-addressability.
    
    Change-Id: Id2f20a262cfab0719beb88e6342984ec4b196268
    Reviewed-on: https://go-review.googlesource.com/c/go/+/353672
    Trust: Josh Bleecher Snyder <josharian@gmail.com>
    Reviewed-by: Cherry Mui <cherryyz@google.com>

--- src/runtime/preempt.go
+++ src/runtime/preempt.go
@@ -56,7 +56,6 @@ import (
 	"internal/abi"
 	"internal/goarch"
 	"runtime/internal/atomic"
-	"unsafe"
 )
 
 type suspendGState struct {
@@ -405,12 +404,9 @@ func isAsyncSafePoint(gp *g, pc, sp, lr uintptr) (bool, uintptr) {
 		// functions (except at calls).
 		return false, 0
 	}
-	if fd := funcdata(f, _FUNCDATA_LocalsPointerMaps); fd == nil || fd == unsafe.Pointer(&no_pointers_stackmap) {
-		// This is assembly code. Don't assume it's
-		// well-formed. We identify assembly code by
-		// checking that it has either no stack map, or
-		// no_pointers_stackmap, which is the stack map
-		// for ones marked as NO_LOCAL_POINTERS.
+	if fd := funcdata(f, _FUNCDATA_LocalsPointerMaps); fd == nil || f.flag&funcFlag_ASM != 0 {
+		// This is assembly code. Don't assume it's well-formed.
+		// TODO: Empirically we still need the fd == nil check. Why?
 		//
 		// TODO: Are there cases that are safe but don't have a
 		// locals pointer map, like empty frame functions?
@@ -455,5 +451,3 @@ func isAsyncSafePoint(gp *g, pc, sp, lr uintptr) (bool, uintptr) {
 	}
 	return true, pc
 }
-
-var no_pointers_stackmap uint64 // defined in assembly, for NO_LOCAL_POINTERS macro
