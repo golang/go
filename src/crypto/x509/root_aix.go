fd4b391849dcd3ac0e90d3a86b0ee7df00b9b269
Shang Jian Ding <sding3@ncsu.edu>
2021-10-05 19:52:20 -0500

    crypto/x509: fix comments on certDirectories
    CL 205237 allowed SSL_CERT_DIR to be a colon delimited list of
    directories. In the case that SSL_CERT_DIR is unset, the change
    also made certDirectories to all be loaded rather than stopping
    after successfully reading at least one file from a directory.
    This update fixes code comments on the certDirectories package
    level variables to reflect current behavior.
    
    Fixes #48808
    
    Change-Id: Id92f875545272fc6205d9955d03ea7bf844f15eb
    Reviewed-on: https://go-review.googlesource.com/c/go/+/354140
    Reviewed-by: Emmanuel Odeke <emmanuel@orijtech.com>
    Reviewed-by: Katie Hockman <katie@golang.org>
    Run-TryBot: Emmanuel Odeke <emmanuel@orijtech.com>
    TryBot-Result: Gopher Robot <gobot@golang.org>
    Trust: Katie Hockman <katie@golang.org>

--- src/crypto/x509/root_aix.go
+++ src/crypto/x509/root_aix.go
@@ -9,8 +9,7 @@ var certFiles = []string{
 	"/var/ssl/certs/ca-bundle.crt",
 }
 
-// Possible directories with certificate files; stop after successfully
-// reading at least one file from a directory.
+// Possible directories with certificate files; all will be read.
 var certDirectories = []string{
 	"/var/ssl/certs",
 }
