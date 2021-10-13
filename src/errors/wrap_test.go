8e59b0ba7f2ca0092f23a7dcabb850b307d088cc
jiahua wang <wjh180909@gmail.com>
2021-10-13 22:16:37 +0800

    errors: add errors.Unwrap example
    Change-Id: Id2336a6059f7a8d627e6c0661a4d4c05485b65f3
    Reviewed-on: https://go-review.googlesource.com/c/go/+/355589
    Reviewed-by: Ian Lance Taylor <iant@golang.org>
    Run-TryBot: Ian Lance Taylor <iant@golang.org>
    TryBot-Result: Go Bot <gobot@golang.org>
    Trust: Cherry Mui <cherryyz@google.com>
    Trust: Robert Findley <rfindley@google.com>

--- src/errors/wrap_test.go
+++ src/errors/wrap_test.go
@@ -265,3 +265,13 @@ func ExampleAs() {
 	// Output:
 	// Failed at path: non-existing
 }
+
+func ExampleUnwrap() {
+	err1 := errors.New("error1")
+	err2 := fmt.Errorf("error2: [%w]", err1)
+	fmt.Println(err2)
+	fmt.Println(errors.Unwrap(err2))
+	// Output
+	// error2: [error1]
+	// error1
+}
