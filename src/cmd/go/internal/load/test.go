4cbc0a555474f59b8600e7e8ecba5c70d619ba0b
Robert Findley <rfindley@google.com>
2021-10-14 11:34:00 -0400

    cmd/go: produce a better error for generic test functions
    Test functions with type parameters cannot be invoked without type
    arguments, so cmd/go would previously fail with a type-checking error in
    the test harness.
    
    Avoid this by producing an error explaining that test functions cannot
    have type parameters.
    
    Fixes #48953
    
    Change-Id: I4e39c9b7a06c964fad5f12d74d262aa090c25d79
    Reviewed-on: https://go-review.googlesource.com/c/go/+/355889
    Trust: Robert Findley <rfindley@google.com>
    Run-TryBot: Bryan C. Mills <bcmills@google.com>
    Reviewed-by: Bryan C. Mills <bcmills@google.com>
    TryBot-Result: Go Bot <gobot@golang.org>

--- src/cmd/go/internal/load/test.go
+++ src/cmd/go/internal/load/test.go
@@ -678,10 +678,16 @@ func (t *testFuncs) load(filename, pkg string, doImport, seen *bool) error {
 }
 
 func checkTestFunc(fn *ast.FuncDecl, arg string) error {
+	var why string
 	if !isTestFunc(fn, arg) {
-		name := fn.Name.String()
+		why = fmt.Sprintf("must be: func %s(%s *testing.%s)", fn.Name.String(), strings.ToLower(arg), arg)
+	}
+	if fn.Type.TypeParams.NumFields() > 0 {
+		why = "test functions cannot have type parameters"
+	}
+	if why != "" {
 		pos := testFileSet.Position(fn.Pos())
-		return fmt.Errorf("%s: wrong signature for %s, must be: func %s(%s *testing.%s)", pos, name, name, strings.ToLower(arg), arg)
+		return fmt.Errorf("%s: wrong signature for %s, %s", pos, fn.Name.String(), why)
 	}
 	return nil
 }
