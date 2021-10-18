f063e0da28d441065d36f7d676f86d478f67db1f
Michael Anthony Knyszek <mknyszek@google.com>
2021-10-18 19:01:33 +0000

    runtime: add harddecommit GODEBUG flag
    This change adds a new debug flag that makes the runtime map pages
    PROT_NONE in sysUnused on Linux, in addition to the usual madvise calls.
    This behavior mimics the behavior of decommit on Windows, and is helpful
    in debugging the scavenger. sysUsed is also updated to re-map the pages
    as PROT_READ|PROT_WRITE, mimicing Windows' explicit commit behavior.
    
    Change-Id: Iaac5fcd0e6920bd1d0e753dd4e7f0c0b128fe842
    Reviewed-on: https://go-review.googlesource.com/c/go/+/356612
    Trust: Michael Knyszek <mknyszek@google.com>
    Reviewed-by: Michael Pratt <mpratt@google.com>

--- src/runtime/mem_linux.go
+++ src/runtime/mem_linux.go
@@ -114,9 +114,29 @@ func sysUnused(v unsafe.Pointer, n uintptr) {
 		atomic.Store(&adviseUnused, _MADV_DONTNEED)
 		madvise(v, n, _MADV_DONTNEED)
 	}
+
+	if debug.harddecommit > 0 {
+		p, err := mmap(v, n, _PROT_NONE, _MAP_ANON|_MAP_FIXED|_MAP_PRIVATE, -1, 0)
+		if p != v || err != 0 {
+			throw("runtime: cannot disable permissions in address space")
+		}
+	}
 }
 
 func sysUsed(v unsafe.Pointer, n uintptr) {
+	if debug.harddecommit > 0 {
+		p, err := mmap(v, n, _PROT_READ|_PROT_WRITE, _MAP_ANON|_MAP_FIXED|_MAP_PRIVATE, -1, 0)
+		if err == _ENOMEM {
+			throw("runtime: out of memory")
+		}
+		if p != v || err != 0 {
+			throw("runtime: cannot remap pages in address space")
+		}
+		return
+
+		// Don't do the sysHugePage optimization in hard decommit mode.
+		// We're breaking up pages everywhere, there's no point.
+	}
 	// Partially undo the NOHUGEPAGE marks from sysUnused
 	// for whole huge pages between v and v+n. This may
 	// leave huge pages off at the end points v and v+n
