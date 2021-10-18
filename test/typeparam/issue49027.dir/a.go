2646e6121aa608b69a1448e7ed97a43f2d4f976d
Keith Randall <khr@golang.org>
2021-10-18 10:59:29 -0700

    cmd/compile: allow importing and exporting of ODYNAMICTYPE
    Change-Id: I2fca7a801c85ed93c002c23bfcb0cf9593f1bdf4
    Reviewed-on: https://go-review.googlesource.com/c/go/+/356571
    Trust: Keith Randall <khr@golang.org>
    Trust: Dan Scales <danscales@google.com>
    Run-TryBot: Keith Randall <khr@golang.org>
    TryBot-Result: Go Bot <gobot@golang.org>
    Reviewed-by: Dan Scales <danscales@google.com>

--- test/typeparam/issue49027.dir/a.go
+++ test/typeparam/issue49027.dir/a.go
@@ -15,7 +15,41 @@ func conv[T any](v interface{}) T {
 func Conv2(v interface{}) (string, bool) {
 	return conv2[string](v)
 }
+
 func conv2[T any](v interface{}) (T, bool) {
 	x, ok := v.(T)
 	return x, ok
+}
+
+func Conv3(v interface{}) string {
+	return conv3[string](v)
+}
+
+func conv3[T any](v interface{}) T {
+	switch v := v.(type) {
+	case T:
+		return v
+	default:
+		var z T
+		return z
+	}
+}
+
+type Mystring string
+
+func (Mystring) Foo() {
+}
+
+func Conv4(v interface{Foo()}) Mystring {
+	return conv4[Mystring](v)
+}
+
+func conv4[T interface{Foo()}](v interface{Foo()}) T {
+	switch v := v.(type) {
+	case T:
+		return v
+	default:
+		var z T
+		return z
+	}
 }
