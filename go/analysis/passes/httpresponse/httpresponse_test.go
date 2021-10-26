c4ead46007cd1af7f5f93bd721c949e2732ac785
Guodong Li <guodongli@google.com>
2021-10-26 00:25:41 -0700

    go/analysis/passes/httpresponse: fix a testing issue
    Fixes golang/go#49160
    
    Change-Id: Ie22f45626df37b4d701feedaf0ba4516505bfbfe
    Reviewed-on: https://go-review.googlesource.com/c/tools/+/358698
    Reviewed-by: Dmitri Shuralyov <dmitshur@golang.org>
    Reviewed-by: Robert Findley <rfindley@google.com>

--- go/analysis/passes/httpresponse/httpresponse_test.go
+++ go/analysis/passes/httpresponse/httpresponse_test.go
@@ -14,7 +14,7 @@ import (
 func Test(t *testing.T) {
 	testdata := analysistest.TestData()
 	tests := []string{"a"}
-	if true || typeparams.Enabled {
+	if typeparams.Enabled {
 		tests = append(tests, "typeparams")
 	}
 	analysistest.Run(t, testdata, httpresponse.Analyzer, tests...)
