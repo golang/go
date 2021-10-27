87d59f52111e92537195d78e48c684fdccc4c470
Robert Griesemer <gri@golang.org>
2021-10-27 13:55:15 -0700

    cmd/compile/internal/types2: disallow lone type parameter on RHS of type declaration
    We may revisit this decision in a future release. By disallowing this
    for Go 1.18 we are ensuring that we don't lock in the generics design
    in a place that may need to change later. (Type declarations are the
    primary construct where it crucially matters what the underlying type
    of a type parameter is.)
    
    Comment out all tests that rely on this feature; add comments referring
    to issue so we can find all places easily should we change our minds.
    
    Fixes #45639.
    
    Change-Id: I730510e4da66d3716d455a9071c7778a1e4a1152
    Reviewed-on: https://go-review.googlesource.com/c/go/+/359177
    Trust: Robert Griesemer <gri@golang.org>
    Trust: Dan Scales <danscales@google.com>
    Reviewed-by: Dan Scales <danscales@google.com>

--- test/typeparam/issue47708.go
+++ test/typeparam/issue47708.go
@@ -6,35 +6,32 @@
 
 package main
 
-import (
-	"fmt"
-)
-
 type FooType[T any] interface {
-	Foo(BarType[T])string
+	Foo(BarType[T]) string
 }
 type BarType[T any] interface {
-	Bar(FooType[T])string
+	Bar(FooType[T]) string
 }
 
-type Baz[T any] T
-func (l Baz[T]) Foo(v BarType[T]) string {
-	return v.Bar(l)
-}
-type Bob[T any] T
-func (l Bob[T]) Bar(v FooType[T]) string {
-	if v,ok := v.(Baz[T]);ok{
-		return fmt.Sprintf("%v%v",v,l)
-	}
-	return ""
-}
-
+// For now, a lone type parameter is not permitted as RHS in a type declaration (issue #45639).
+// type Baz[T any] T
+// func (l Baz[T]) Foo(v BarType[T]) string {
+// 	return v.Bar(l)
+// }
+// type Bob[T any] T
+// func (l Bob[T]) Bar(v FooType[T]) string {
+// 	if v,ok := v.(Baz[T]);ok{
+// 		return fmt.Sprintf("%v%v",v,l)
+// 	}
+// 	return ""
+// }
 
 func main() {
-	var baz Baz[int] = 123
-	var bob Bob[int] = 456
-
-	if got, want := baz.Foo(bob), "123456"; got != want {
-		panic(fmt.Sprintf("got %s want %s", got, want))
-	}
+	// For now, a lone type parameter is not permitted as RHS in a type declaration (issue #45639).
+	// var baz Baz[int] = 123
+	// var bob Bob[int] = 456
+	//
+	// if got, want := baz.Foo(bob), "123456"; got != want {
+	// 	panic(fmt.Sprintf("got %s want %s", got, want))
+	// }
 }
--- test/typeparam/issue47740.go
+++ test/typeparam/issue47740.go
@@ -12,10 +12,11 @@ type Exp[Ty any] interface {
 	Eval() Ty
 }
 
-type Lit[Ty any] Ty
-
-func (lit Lit[Ty]) Eval() Ty       { return Ty(lit) }
-func (lit Lit[Ty]) String() string { return fmt.Sprintf("(lit %v)", Ty(lit)) }
+// For now, a lone type parameter is not permitted as RHS in a type declaration (issue #45639).
+// type Lit[Ty any] Ty
+//
+// func (lit Lit[Ty]) Eval() Ty       { return Ty(lit) }
+// func (lit Lit[Ty]) String() string { return fmt.Sprintf("(lit %v)", Ty(lit)) }
 
 type Eq[Ty any] struct {
 	a Exp[Ty]
@@ -26,12 +27,14 @@ func (e Eq[Ty]) String() string {
 	return fmt.Sprintf("(eq %v %v)", e.a, e.b)
 }
 
-var (
-	e0 = Eq[int]{Lit[int](128), Lit[int](64)}
-	e1 = Eq[bool]{Lit[bool](true), Lit[bool](true)}
-)
+// For now, a lone type parameter is not permitted as RHS in a type declaration (issue #45639).
+// var (
+// 	e0 = Eq[int]{Lit[int](128), Lit[int](64)}
+// 	e1 = Eq[bool]{Lit[bool](true), Lit[bool](true)}
+// )
 
 func main() {
-	fmt.Printf("%v\n", e0)
-	fmt.Printf("%v\n", e1)
+	// For now, a lone type parameter is not permitted as RHS in a type declaration (issue #45639).
+	// fmt.Printf("%v\n", e0)
+	// fmt.Printf("%v\n", e1)
 }
