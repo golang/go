d75868bef2ecddbdf0c41094dd84ab5dc6f24d70
Bryan C. Mills <bcmills@google.com>
2021-10-28 20:54:45 -0400

    cmd/go: document that tests must not write to their source modules
    Fixes #28386
    Updates #48495
    
    Change-Id: I76186077c7bbe3f8f608026ee1865de83fe169b4
    Reviewed-on: https://go-review.googlesource.com/c/go/+/359574
    Trust: Bryan C. Mills <bcmills@google.com>
    Reviewed-by: Ian Lance Taylor <iant@golang.org>
    Run-TryBot: Ian Lance Taylor <iant@golang.org>
    TryBot-Result: Go Bot <gobot@golang.org>

--- src/cmd/go/internal/test/test.go
+++ src/cmd/go/internal/test/test.go
@@ -407,7 +407,11 @@ leave the test binary in pkg.test for use when analyzing the profiles.
 When 'go test' runs a test binary, it does so from within the
 corresponding package's source code directory. Depending on the test,
 it may be necessary to do the same when invoking a generated test
-binary directly.
+binary directly. Because that directory may be located within the
+module cache, which may be read-only and is verified by checksums, the
+test must not write to it or any other directory within the module
+unless explicitly requested by the user (such as with the -fuzz flag,
+which writes failures to testdata/fuzz).
 
 The command-line package list, if present, must appear before any
 flag not known to the go test command. Continuing the example above,
