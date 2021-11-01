60e2dde9338b811a177932a12cf7307197ad2830
quinn <2904951429@qq.com>
2021-11-01 22:37:43 +0800

    update zoneinfo compile script
    modify zoneinfo shell script (compilation result is consistent with zoneinfo in Unix system).

--- lib/time/update.bash
+++ lib/time/update.bash
@@ -8,30 +8,45 @@
 # Consult https://www.iana.org/time-zones for the latest versions.
 
 # Versions to use.
-CODE=2021a
-DATA=2021a
+VER=2021e
 
 set -e
-rm -rf work
-mkdir work
-cd work
-mkdir zoneinfo
-curl -L -O https://www.iana.org/time-zones/repository/releases/tzcode$CODE.tar.gz
-curl -L -O https://www.iana.org/time-zones/repository/releases/tzdata$DATA.tar.gz
-tar xzf tzcode$CODE.tar.gz
-tar xzf tzdata$DATA.tar.gz
 
-make CFLAGS=-DSTD_INSPIRED AWK=awk TZDIR=zoneinfo posix_only
+rm -rf work && mkdir work
+cd work && mkdir -p tzdata
 
-cd zoneinfo
-rm -f ../../zoneinfo.zip
-zip -0 -r ../../zoneinfo.zip *
+# download
+curl -L -O "https://www.iana.org/time-zones/repository/releases/tzcode$VER.tar.gz"
+curl -L -O "https://www.iana.org/time-zones/repository/releases/tzdata$VER.tar.gz"
+tar xf "tzcode$VER.tar.gz" -C tzdata
+tar xf "tzdata$VER.tar.gz" -C tzdata
+chmod -Rf a+rX,u+w,g-w,o-w tzdata
+
+# build
+cd tzdata
+make VERSION="$VER" AWK=awk CFLAGS=-DSTD_INSPIRED "tzdata$VER-rearguard.tar.gz"
+tar xvf "tzdata$VER-rearguard.tar.gz"
+rm tzdata.zi
+make VERSION="$VER" AWK=awk CFLAGS=-DSTD_INSPIRED DATAFORM=rearguard tzdata.zi
+
+files="africa antarctica asia australasia europe northamerica southamerica pacificnew etcetera backward"
+mkdir -p zoneinfo/posix && mkdir -p zoneinfo/right
+zic -y ./yearistype -d zoneinfo -L /dev/null -p America/New_York ${files}
+zic -y ./yearistype -d zoneinfo/posix -L /dev/null  ${files}
+zic -y ./yearistype -d zoneinfo/right -L leapseconds  ${files}
+
+# install
+cp -prd zoneinfo ..
+install -p -m 644 zone.tab zone1970.tab iso3166.tab leapseconds tzdata.zi ../zoneinfo
+
+cd ../zoneinfo
+rm -f ../../zoneinfo.zip && zip -0 -r ../../zoneinfo.zip *
 cd ../..
 
 go generate time/tzdata
 
 echo
-if [ "$1" = "-work" ]; then
+if [[ "$1" = "-work" ]]; then
 	echo Left workspace behind in work/.
 else
 	rm -rf work
