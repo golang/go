519c0a2323700934cbec97b75df92917108548be
Matthew Dempsky <mdempsky@google.com>
2021-11-02 12:50:05 -0700

    cmd/compile/internal/types2: port nomono check from go/types
    Same logic as CL 357449 (including CL 360815), just ported to types2.
    
    Updates #48098.
    
    Change-Id: I4578f7329bb4ffc42410025bb6cb97e24697ebfd
    Reviewed-on: https://go-review.googlesource.com/c/go/+/360857
    Trust: Matthew Dempsky <mdempsky@google.com>
    Reviewed-by: Robert Griesemer <gri@golang.org>

--- src/cmd/compile/internal/types2/check.go
+++ src/cmd/compile/internal/types2/check.go
@@ -126,6 +126,7 @@ type Checker struct {
 	imports       []*PkgName                  // list of imported packages
 	dotImportMap  map[dotImportKey]*PkgName   // maps dot-imported objects to the package they were dot-imported through
 	recvTParamMap map[*syntax.Name]*TypeParam // maps blank receiver type parameters to their type
+	mono          monoGraph                   // graph for detecting non-monomorphizable instantiation loops
 
 	firstErr error                    // first error encountered
 	methods  map[*TypeName][]*Func    // maps package scope type names to associated non-blank (non-interface) methods
@@ -316,6 +317,11 @@ func (check *Checker) checkFiles(files []*syntax.File) (err error) {
 
 	print("== recordUntyped ==")
 	check.recordUntyped()
+
+	if check.firstErr == nil {
+		// TODO(mdempsky): Ensure monomorph is safe when errors exist.
+		check.monomorph()
+	}
 
 	check.pkg.complete = true
 
