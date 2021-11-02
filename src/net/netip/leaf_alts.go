eeb5e8ee33c8d62de293ae5ba637949a92f3f980
Jason A. Donenfeld <Jason@zx2c4.com>
2021-11-02 21:33:23 +0100

    net/netip: add missing encoding.BinaryUnmarshaler to AddrPort and Prefix
    The Addr type got an encoding.BinaryUnmarshaler implementation, but not
    AddrPort and Prefix. This commit adds the missing implementation of that
    interface to these types. It also adds two round trip tests that follow
    the template of the existing one for Addr.
    
    Updates #49298.
    
    Change-Id: Iac633aed8aac579960815bb64d06ff3181214841
    Reviewed-on: https://go-review.googlesource.com/c/go/+/360875
    Trust: Jason A. Donenfeld <Jason@zx2c4.com>
    Run-TryBot: Jason A. Donenfeld <Jason@zx2c4.com>
    TryBot-Result: Go Bot <gobot@golang.org>
    Reviewed-by: Brad Fitzpatrick <bradfitz@golang.org>

--- src/net/netip/leaf_alts.go
+++ src/net/netip/leaf_alts.go
@@ -41,3 +41,14 @@ func bePutUint32(b []byte, v uint32) {
 	b[2] = byte(v >> 8)
 	b[3] = byte(v)
 }
+
+func leUint16(b []byte) uint16 {
+	_ = b[1] // bounds check hint to compiler; see golang.org/issue/14808
+	return uint16(b[0]) | uint16(b[1])<<8
+}
+
+func lePutUint16(b []byte, v uint16) {
+	_ = b[1] // early bounds check to guarantee safety of writes below
+	b[0] = byte(v)
+	b[1] = byte(v >> 8)
+}
