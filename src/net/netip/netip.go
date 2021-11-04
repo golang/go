b0f1989cde25641cfc9d70dba662145bd967ded6
jiahua wang <wjh180909@gmail.com>
2021-11-04 10:13:11 +0800

    net/netip: fix various typos in comment
    Change-Id: I02724dadacd9b3f23ca7e6bda581cba62ceff828
    Reviewed-on: https://go-review.googlesource.com/c/go/+/361274
    Reviewed-by: Ian Lance Taylor <iant@golang.org>
    Reviewed-by: Damien Neil <dneil@google.com>
    Trust: Damien Neil <dneil@google.com>
    Run-TryBot: Damien Neil <dneil@google.com>
    TryBot-Result: Go Bot <gobot@golang.org>

--- src/net/netip/netip.go
+++ src/net/netip/netip.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// Package netip defines a IP address type that's a small value type.
+// Package netip defines an IP address type that's a small value type.
 // Building on that Addr type, the package also defines AddrPort (an
 // IP address and a port), and Prefix (an IP address and a bit length
 // prefix).
