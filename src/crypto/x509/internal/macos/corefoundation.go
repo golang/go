0d674c72eac868017ca32942bfd57c6fcb4b5abb
Roland Shoemaker <roland@golang.org>
2021-11-08 08:05:24 -0800

    crypto/x509: remove ios build tag restriction
    Fixes #49435
    
    Change-Id: I77ce12f447e727e7dc3b23de947357c27a268bd2
    Reviewed-on: https://go-review.googlesource.com/c/go/+/362294
    Trust: Roland Shoemaker <roland@golang.org>
    Run-TryBot: Roland Shoemaker <roland@golang.org>
    Reviewed-by: Bryan C. Mills <bcmills@google.com>

--- src/crypto/x509/internal/macos/corefoundation.go
+++ src/crypto/x509/internal/macos/corefoundation.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-//go:build darwin && !ios
+//go:build darwin
 
 // Package macOS provides cgo-less wrappers for Core Foundation and
 // Security.framework, similarly to how package syscall provides access to
