8d5a51ec92f74964de2df7b4239818173f01b470
Than McIntosh <thanm@google.com>
2021-11-16 18:44:08 -0500

    cmd/compile: emit definition of 'any' when compiling runtime
    Include the predefined type 'any' in the list of other important
    predefined types that are emitted when compiling the runtime package
    (uintptr, string, etc).
    
    Fixes #49619.
    
    Change-Id: I4a851ba2f302fbc3a425e65daa325c6bf83659da
    Reviewed-on: https://go-review.googlesource.com/c/go/+/364377
    Trust: Than McIntosh <thanm@google.com>
    Trust: Dan Scales <danscales@google.com>
    Run-TryBot: Than McIntosh <thanm@google.com>
    TryBot-Result: Go Bot <gobot@golang.org>
    Reviewed-by: Dan Scales <danscales@google.com>

---
+++ test/fixedbugs/issue49619.go
@@ -0,0 +1,19 @@
+// build
+
+// Copyright 2021 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// This testcase caused a linker crash in DWARF generation.
+
+package main
+
+//go:noinline
+func f() any {
+	var a []any
+	return a[0]
+}
+
+func main() {
+	f()
+}
