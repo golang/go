8fc1f4f31db2b7d7298a7ec8875f2e93d65553b1
Robert Findley <rfindley@google.com>
2021-11-17 19:45:49 -0500

    go/types: better position for invalid slice indices error
    This is a port of CL 363671 from types2 to go/types. Also adjust the
    error message to match types2 ("invalid" vs "swapped").
    
    Change-Id: I662a73c915814fea14bfcb1ebde0fbf39589f022
    Reviewed-on: https://go-review.googlesource.com/c/go/+/364902
    Trust: Robert Findley <rfindley@google.com>
    Run-TryBot: Robert Findley <rfindley@google.com>
    TryBot-Result: Go Bot <gobot@golang.org>
    Reviewed-by: Robert Griesemer <gri@golang.org>

--- src/go/types/index.go
+++ src/go/types/index.go
@@ -310,9 +310,13 @@ func (check *Checker) sliceExpr(x *operand, e *ast.SliceExpr) {
 L:
 	for i, x := range ind[:len(ind)-1] {
 		if x > 0 {
-			for _, y := range ind[i+1:] {
-				if y >= 0 && x > y {
-					check.errorf(inNode(e, e.Rbrack), _SwappedSliceIndices, "swapped slice indices: %d > %d", x, y)
+			for j, y := range ind[i+1:] {
+				if y >= 0 && y < x {
+					// The value y corresponds to the expression e.Index[i+1+j].
+					// Because y >= 0, it must have been set from the expression
+					// when checking indices and thus e.Index[i+1+j] is not nil.
+					at := []ast.Expr{e.Low, e.High, e.Max}[i+1+j]
+					check.errorf(at, _SwappedSliceIndices, "invalid slice indices: %d < %d", y, x)
 					break L // only report one error, ok to continue
 				}
 			}
