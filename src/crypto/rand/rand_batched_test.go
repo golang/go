d68a8d0f27bb3599b49cacd119d7ac3202248050
Jason A. Donenfeld <Jason@zx2c4.com>
2021-12-10 17:23:08 +0100

    crypto/rand: batch and buffer calls to getrandom/getentropy
    We're using bufio to batch reads of /dev/urandom to 4k, but we weren't
    doing the same on newer platforms with getrandom/getentropy. Since the
    overhead is the same for these -- one syscall -- we should batch reads
    of these into the same 4k buffer. While we're at it, we can simplify a
    lot of the constant dispersal.
    
    This also adds a new test case to make sure the buffering works as
    desired.
    
    Change-Id: I7297d4aa795c00712e6484b841cef8650c2be4ef
    Reviewed-on: https://go-review.googlesource.com/c/go/+/370894
    Reviewed-by: Filippo Valsorda <valsorda@google.com>
    Run-TryBot: Jason Donenfeld <Jason@zx2c4.com>
    Auto-Submit: Jason Donenfeld <Jason@zx2c4.com>
    TryBot-Result: Gopher Robot <gobot@golang.org>
    Reviewed-by: Ian Lance Taylor <iant@google.com>

--- src/crypto/rand/rand_batched_test.go
+++ src/crypto/rand/rand_batched_test.go
@@ -2,21 +2,24 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-//go:build linux || freebsd || dragonfly || solaris
+//go:build aix || darwin || dragonfly || freebsd || linux || netbsd || openbsd || solaris
 
 package rand
 
 import (
 	"bytes"
+	"encoding/binary"
+	"errors"
+	prand "math/rand"
 	"testing"
 )
 
 func TestBatched(t *testing.T) {
-	fillBatched := batched(func(p []byte) bool {
+	fillBatched := batched(func(p []byte) error {
 		for i := range p {
 			p[i] = byte(i)
 		}
-		return true
+		return nil
 	}, 5)
 
 	p := make([]byte, 13)
@@ -29,16 +32,49 @@ func TestBatched(t *testing.T) {
 	}
 }
 
+func TestBatchedBuffering(t *testing.T) {
+	var prandSeed [8]byte
+	Read(prandSeed[:])
+	prand.Seed(int64(binary.LittleEndian.Uint64(prandSeed[:])))
+
+	backingStore := make([]byte, 1<<23)
+	prand.Read(backingStore)
+	backingMarker := backingStore[:]
+	output := make([]byte, len(backingStore))
+	outputMarker := output[:]
+
+	fillBatched := batched(func(p []byte) error {
+		n := copy(p, backingMarker)
+		backingMarker = backingMarker[n:]
+		return nil
+	}, 731)
+
+	for len(outputMarker) > 0 {
+		max := 9200
+		if max > len(outputMarker) {
+			max = len(outputMarker)
+		}
+		howMuch := prand.Intn(max + 1)
+		if !fillBatched(outputMarker[:howMuch]) {
+			t.Fatal("batched function returned false")
+		}
+		outputMarker = outputMarker[howMuch:]
+	}
+	if !bytes.Equal(backingStore, output) {
+		t.Error("incorrect batch result")
+	}
+}
+
 func TestBatchedError(t *testing.T) {
-	b := batched(func(p []byte) bool { return false }, 5)
+	b := batched(func(p []byte) error { return errors.New("failure") }, 5)
 	if b(make([]byte, 13)) {
-		t.Fatal("batched function should have returned false")
+		t.Fatal("batched function should have returned an error")
 	}
 }
 
 func TestBatchedEmpty(t *testing.T) {
-	b := batched(func(p []byte) bool { return false }, 5)
+	b := batched(func(p []byte) error { return errors.New("failure") }, 5)
 	if !b(make([]byte, 0)) {
-		t.Fatal("empty slice should always return true")
+		t.Fatal("empty slice should always return successful")
 	}
 }
