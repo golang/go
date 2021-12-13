dbbe4cca5d7069482983316694334bdf2fe6a7ec
Michael Pratt <mpratt@google.com>
2021-12-13 17:34:16 -0500

    cmd/dist: log OS version when testing
    As a follow-up to https://golang.org/cl/371474, add the OS version to
    the metadata printed for each test.
    
    This is a redo of CL 371475. This version updates go.mod and conforms to
    the changes made in the parent commit.
    
    Fixes #50146.
    
    Change-Id: Iba5541cc8dd2c85c1fa3a215e30c8c3f9b6aaaab
    Reviewed-on: https://go-review.googlesource.com/c/go/+/378590
    Reviewed-by: Austin Clements <austin@google.com>
    Trust: Michael Pratt <mpratt@google.com>
    Run-TryBot: Michael Pratt <mpratt@google.com>
    TryBot-Result: Gopher Robot <gobot@golang.org>

--- src/cmd/internal/metadata/main.go
+++ src/cmd/internal/metadata/main.go
@@ -8,6 +8,7 @@
 package main
 
 import (
+	"cmd/internal/osinfo"
 	"fmt"
 	"internal/sysinfo"
 	"runtime"
@@ -16,4 +17,11 @@ import (
 func main() {
 	fmt.Printf("# GOARCH: %s\n", runtime.GOARCH)
 	fmt.Printf("# CPU: %s\n", sysinfo.CPU.Name())
+
+	fmt.Printf("# GOOS: %s\n", runtime.GOOS)
+	ver, err := osinfo.Version()
+	if err != nil {
+		ver = fmt.Sprintf("UNKNOWN: error determining OS version: %v", err)
+	}
+	fmt.Printf("# OS Version: %s\n", ver)
 }
