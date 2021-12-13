c88d4b5649f1a6d4e129d81a3006c8e8cc8db9b2
Dan Scales <danscales@google.com>
2021-12-13 15:10:31 -0800

    test: add simpler test for issue 50109
    Thanks to the simpler test case for issue 50109. I'm keeping the old
    test case in place, since it's not too complex, and may be useful for
    testing other things as well.
    
    Updates #50109
    
    Change-Id: I80cdbd1da473d0cc4dcbd68e45bab7ddb6f9263e
    Reviewed-on: https://go-review.googlesource.com/c/go/+/371515
    Trust: Dan Scales <danscales@google.com>
    Run-TryBot: Dan Scales <danscales@google.com>
    TryBot-Result: Gopher Robot <gobot@golang.org>
    Reviewed-by: roger peppe <rogpeppe@gmail.com>

---
+++ test/typeparam/issue50109b.go
@@ -0,0 +1,29 @@
+// run -gcflags=-G=3
+
+// Copyright 2021 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+func main() {
+	F[any]()
+}
+
+func F[T any]() I[T] {
+	return (*S1[T])(nil)
+}
+
+type I[T any] interface{}
+
+type S1[T any] struct {
+	*S2[T]
+}
+
+type S2[T any] struct {
+	S3 *S3[T]
+}
+
+type S3[T any] struct {
+	x int
+}
