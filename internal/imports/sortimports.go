d7a4bb4f6a592485d1b96c0852435f18b2464728
pjw <pjw@google.com>
2022-01-07 07:05:37 -0500

    internal/imports: avoid rare panic
    (*token.File)MergeLine() can panic. Add code to avoid the panic
    and request information from users.
    
    Fixes golang/go#50329
    
    Change-Id: I9c10a5c6cbee4ff8a34321e77e32971f7e588cb5
    Reviewed-on: https://go-review.googlesource.com/c/tools/+/376274
    Run-TryBot: Peter Weinberger <pjw@google.com>
    Trust: Peter Weinberger <pjw@google.com>
    gopls-CI: kokoro <noreply+kokoro@google.com>
    TryBot-Result: Gopher Robot <gobot@golang.org>
    Reviewed-by: Robert Findley <rfindley@google.com>

--- internal/imports/sortimports.go
+++ internal/imports/sortimports.go
@@ -9,6 +9,7 @@ package imports
 import (
 	"go/ast"
 	"go/token"
+	"log"
 	"sort"
 	"strconv"
 )
@@ -60,6 +61,7 @@ func sortImports(localPrefix string, fset *token.FileSet, f *ast.File) {
 
 // mergeImports merges all the import declarations into the first one.
 // Taken from golang.org/x/tools/ast/astutil.
+// This does not adjust line numbers properly
 func mergeImports(fset *token.FileSet, f *ast.File) {
 	if len(f.Decls) <= 1 {
 		return
@@ -237,8 +239,17 @@ func sortSpecs(localPrefix string, fset *token.FileSet, f *ast.File, specs []ast
 		p := s.Pos()
 		line := fset.File(p).Line(p)
 		for previousLine := line - 1; previousLine >= firstSpecLine; {
-			fset.File(p).MergeLine(previousLine)
-			previousLine--
+			// MergeLine can panic. Avoid the panic at the cost of not removing the blank line
+			// golang/go#50329
+			if previousLine > 0 && previousLine < fset.File(p).LineCount() {
+				fset.File(p).MergeLine(previousLine)
+				previousLine--
+			} else {
+				// try to gather some data to diagnose how this could happen
+				req := "Please report what the imports section of your go file looked like."
+				log.Printf("panic avoided: first:%d line:%d previous:%d max:%d. %s",
+					firstSpecLine, line, previousLine, fset.File(p).LineCount(), req)
+			}
 		}
 	}
 	return specs
