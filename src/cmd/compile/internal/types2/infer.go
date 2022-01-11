bda442b1a5663aa72f930802c17f0451bfb73f83
Robert Griesemer <gri@golang.org>
2022-01-11 15:36:38 -0800

    go/types, types2: make function type inference argument-order independent
    If we have more than 2 arguments, we may have arguments with named and
    unnamed types. If that is the case, permutate params and args such that
    the arguments with named types are first in the list. This doesn't affect
    type inference if all types are taken as is. But when we have inexact
    unification enabled (as is the case for function type inference), when
    a named type is unified with an unnamed type, unification proceeds with
    the underlying type of the named type because otherwise unification would
    fail right away. This leads to an asymmetry in type inference: in cases
    where arguments of named and unnamed types are passed to parameters with
    identical type, different types (named vs underlying) may be inferred
    depending on the order of the arguments.
    By ensuring that named types are seen first, order dependence is avoided
    and unification succeeds where it can.
    
    This CL implements the respectice code but keeps it disabled for now,
    pending decision whether we want to address this issue in the first
    place.
    
    For #43056.
    
    Change-Id: Ibe3b08ec2afe90a24a8c30cd1875d504bcc2ef39
    Reviewed-on: https://go-review.googlesource.com/c/go/+/377894
    Trust: Robert Griesemer <gri@golang.org>
    Run-TryBot: Robert Griesemer <gri@golang.org>
    Reviewed-by: Robert Findley <rfindley@google.com>

--- src/cmd/compile/internal/types2/infer.go
+++ src/cmd/compile/internal/types2/infer.go
@@ -54,6 +54,54 @@ func (check *Checker) infer(pos syntax.Pos, tparams []*TypeParam, targs []Type, 
 	}
 	// len(targs) < n
 
+	// If we have more than 2 arguments, we may have arguments with named and unnamed types.
+	// If that is the case, permutate params and args such that the arguments with named
+	// types are first in the list. This doesn't affect type inference if all types are taken
+	// as is. But when we have inexact unification enabled (as is the case for function type
+	// inference), when a named type is unified with an unnamed type, unification proceeds
+	// with the underlying type of the named type because otherwise unification would fail
+	// right away. This leads to an asymmetry in type inference: in cases where arguments of
+	// named and unnamed types are passed to parameters with identical type, different types
+	// (named vs underlying) may be inferred depending on the order of the arguments.
+	// By ensuring that named types are seen first, order dependence is avoided and unification
+	// succeeds where it can.
+	//
+	// This code is disabled for now pending decision whether we want to address cases like
+	// these and make the spec on type inference more complicated (see issue #43056).
+	const enableArgSorting = false
+	if m := len(args); m >= 2 && enableArgSorting {
+		// Determine indices of arguments with named and unnamed types.
+		var named, unnamed []int
+		for i, arg := range args {
+			if hasName(arg.typ) {
+				named = append(named, i)
+			} else {
+				unnamed = append(unnamed, i)
+			}
+		}
+
+		// If we have named and unnamed types, move the arguments with
+		// named types first. Update the parameter list accordingly.
+		// Make copies so as not to clobber the incoming slices.
+		if len(named) != 0 && len(unnamed) != 0 {
+			params2 := make([]*Var, m)
+			args2 := make([]*operand, m)
+			i := 0
+			for _, j := range named {
+				params2[i] = params.At(j)
+				args2[i] = args[j]
+				i++
+			}
+			for _, j := range unnamed {
+				params2[i] = params.At(j)
+				args2[i] = args[j]
+				i++
+			}
+			params = NewTuple(params2...)
+			args = args2
+		}
+	}
+
 	// --- 1 ---
 	// Continue with the type arguments we have. Avoid matching generic
 	// parameters that already have type arguments against function arguments:
@@ -62,7 +110,7 @@ func (check *Checker) infer(pos syntax.Pos, tparams []*TypeParam, targs []Type, 
 	// arguments we have, and continue with that parameter list.
 
 	// First, make sure we have a "full" list of type arguments, some of which
-	// may be nil (unknown).
+	// may be nil (unknown). Make a copy so as to not clobber the incoming slice.
 	if len(targs) < n {
 		targs2 := make([]Type, n)
 		copy(targs2, targs)
