607057ffd9162c8e00581c20f7989d8d570e44c3
Patrik Nyblom <pnyb@google.com>
2022-01-12 17:50:55 -0800

    testing: skip flaky TestRaiseException on windows-amd64-2012
    This is in relation to #49681
    
    Change-Id: I32ad8b506cf8fb0a94b15c3cc8b1eaf5af728c59
    Reviewed-on: https://go-review.googlesource.com/c/go/+/378254
    Run-TryBot: Patrik Nyblom <pnyb@google.com>
    TryBot-Result: Gopher Robot <gobot@golang.org>
    Reviewed-by: Bryan Mills <bcmills@google.com>
    Trust: Patrik Nyblom <pnyb@google.com>

--- src/runtime/syscall_windows_test.go
+++ src/runtime/syscall_windows_test.go
@@ -628,6 +628,9 @@ func TestOutputDebugString(t *testing.T) {
 }
 
 func TestRaiseException(t *testing.T) {
+	if testenv.Builder() == "windows-amd64-2012" {
+		testenv.SkipFlaky(t, 49681)
+	}
 	o := runTestProg(t, "testprog", "RaiseException")
 	if strings.Contains(o, "RaiseException should not return") {
 		t.Fatalf("RaiseException did not crash program: %v", o)
