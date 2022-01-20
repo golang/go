3c751cd15c4c991829b13a128387223a5b0efa84
Tim King <taking@google.com>
2022-01-20 15:04:44 -0800

    passes/ifaceassert: supress typeparams reports
    Supresses reporting when either interface is type parameterized.
    
    In principle, we should be able to report when we know two interfaces
    cannot be unified. This is more complicated with type parameters.
    Waiting on go/types to provide this complex functionality.
    
    Updates #50658
    
    Change-Id: Ib767585c785aea12dbb9e337cc339881a63be57e
    Reviewed-on: https://go-review.googlesource.com/c/tools/+/380014
    Run-TryBot: Tim King <taking@google.com>
    Reviewed-by: Robert Findley <rfindley@google.com>
    gopls-CI: kokoro <noreply+kokoro@google.com>
    TryBot-Result: Gopher Robot <gobot@golang.org>
    Trust: Tim King <taking@google.com>

--- go/analysis/passes/ifaceassert/testdata/src/typeparams/typeparams.go
+++ go/analysis/passes/ifaceassert/testdata/src/typeparams/typeparams.go
@@ -34,3 +34,69 @@ func GenericInterfaceAssertionTest[T io.Reader]() {
 	default:
 	}
 }
+
+// Issue 50658: Check for type parameters in type switches.
+type Float interface {
+	float32 | float64
+}
+
+type Doer[F Float] interface {
+	Do() F
+}
+
+func Underlying[F Float](v Doer[F]) string {
+	switch v.(type) {
+	case Doer[float32]:
+		return "float32!"
+	case Doer[float64]:
+		return "float64!"
+	default:
+		return "<unknown>"
+	}
+}
+
+func DoIf[F Float]() {
+	// This is a synthetic function to create a non-generic to generic assignment.
+	// This function does not make much sense.
+	var v Doer[float32]
+	if t, ok := v.(Doer[F]); ok {
+		t.Do()
+	}
+}
+
+func IsASwitch[F Float, U Float](v Doer[F]) bool {
+	switch v.(type) {
+	case Doer[U]:
+		return true
+	}
+	return false
+}
+
+func IsA[F Float, U Float](v Doer[F]) bool {
+	_, is := v.(Doer[U])
+	return is
+}
+
+func LayeredTypes[F Float]() {
+	// This is a synthetic function cover more isParameterized cases.
+	type T interface {
+		foo() struct{ _ map[T][2]chan *F }
+	}
+	type V interface {
+		foo() struct{ _ map[T][2]chan *float32 }
+	}
+	var t T
+	var v V
+	t, _ = v.(T)
+	_ = t
+}
+
+type X[T any] struct{}
+
+func (x X[T]) m(T) {}
+
+func InstancesOfGenericMethods() {
+	var x interface{ m(string) }
+	// _ = x.(X[int])    // BAD. Not enabled as it does not type check.
+	_ = x.(X[string]) // OK
+}
