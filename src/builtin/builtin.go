1178255f8596ea503daf30c84c7c1039f755e7f0
Russ Cox <rsc@golang.org>
2022-02-03 11:50:45 -0500

    all: untab /* */ doc comments
    A long time ago, gofmt insisted on inserting tabs in /* */ comments
    at the top level of the file, like this:
    
    	/*
    		Package doc comment.
    	*/
    	package p
    
    Gofmt still insists on the tab for comments not at top level,
    but it has relaxed the rules about top-level comments.
    A few very old doc comments are indented, left over from the old rule.
    
    We are considering formatting doc comments, and so to make
    everything consistent, standardize on unindented doc comments
    by removing tabs in the few doc comments that are still indented this way.
    
    Also update some cmd/gofmt testdata to match.
    
    Change-Id: I293742e39b52f8a48ec41f72ca4acdafa7ce43bc
    Reviewed-on: https://go-review.googlesource.com/c/go/+/384261
    Trust: Russ Cox <rsc@golang.org>
    Run-TryBot: Russ Cox <rsc@golang.org>
    TryBot-Result: Gopher Robot <gobot@golang.org>
    Reviewed-by: Ian Lance Taylor <iant@golang.org>

--- src/builtin/builtin.go
+++ src/builtin/builtin.go
@@ -3,10 +3,10 @@
 // license that can be found in the LICENSE file.
 
 /*
-	Package builtin provides documentation for Go's predeclared identifiers.
-	The items documented here are not actually in package builtin
-	but their descriptions here allow godoc to present documentation
-	for the language's special identifiers.
+Package builtin provides documentation for Go's predeclared identifiers.
+The items documented here are not actually in package builtin
+but their descriptions here allow godoc to present documentation
+for the language's special identifiers.
 */
 package builtin
 
