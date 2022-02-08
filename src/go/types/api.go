c5bce7445e1792f134413ad312fd1f2211c0a55d
Robert Griesemer <gri@golang.org>
2022-02-07 18:37:02 -0800

    go/types, types2: AssertableTo is undefined for generalized interfaces
    Document that AssertableTo is undefined (at least for 1.18) if
    the first argument is a generalized interface; i.e., an interface
    that may only be used as a constraint in Go code.
    
    Still, implement it as we might expect it to be defined in the
    future, to prevent problems down the road due to Hyrum's Law.
    
    While at it, also removed the internal flag forceStrict and its
    one use in Checker.assertableTo; forceStrict was never enabled
    and if it would have been enabled, the behavior would not have
    been correct.
    
    Change-Id: Ie4dc9345c88d04c9640f881132154a002db22643
    Reviewed-on: https://go-review.googlesource.com/c/go/+/383917
    Trust: Robert Griesemer <gri@golang.org>
    Run-TryBot: Robert Griesemer <gri@golang.org>
    Reviewed-by: Robert Findley <rfindley@google.com>

--- src/go/types/api.go
+++ src/go/types/api.go
@@ -417,9 +417,15 @@ func (conf *Config) Check(path string, fset *token.FileSet, files []*ast.File, i
 }
 
 // AssertableTo reports whether a value of type V can be asserted to have type T.
+// The behavior of AssertableTo is undefined if V is a generalized interface; i.e.,
+// an interface that may only be used as a type constraint in Go code.
 func AssertableTo(V *Interface, T Type) bool {
-	m, _ := (*Checker)(nil).assertableTo(V, T)
-	return m == nil
+	// Checker.newAssertableTo suppresses errors for invalid types, so we need special
+	// handling here.
+	if T.Underlying() == Typ[Invalid] {
+		return false
+	}
+	return (*Checker)(nil).newAssertableTo(V, T) == nil
 }
 
 // AssignableTo reports whether a value of type V is assignable to a variable of type T.
