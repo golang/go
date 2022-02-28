c105ee93836aaa5b9d89cb6aad3f8da6f4412906
Michael Pratt <mpratt@google.com>
2022-02-28 13:41:33 -0500

    syscall: move Syscall declarations to OS files
    Future CLs will be changing the provenance of these functions. Move the
    declarations to the individual OS files now so that future CLs can
    change only 1 OS at a time rather than changing all at once.
    
    For #51087
    
    Change-Id: I5e1bca71e670263d8c0faa586c1b6b4de1a114b6
    Reviewed-on: https://go-review.googlesource.com/c/go/+/388474
    Run-TryBot: Michael Pratt <mpratt@google.com>
    Reviewed-by: Cherry Mui <cherryyz@google.com>
    TryBot-Result: Gopher Robot <gobot@golang.org>

--- src/syscall/syscall_dragonfly.go
+++ src/syscall/syscall_dragonfly.go
@@ -17,6 +17,11 @@ import (
 	"unsafe"
 )
 
+func Syscall(trap, a1, a2, a3 uintptr) (r1, r2 uintptr, err Errno)
+func Syscall6(trap, a1, a2, a3, a4, a5, a6 uintptr) (r1, r2 uintptr, err Errno)
+func RawSyscall(trap, a1, a2, a3 uintptr) (r1, r2 uintptr, err Errno)
+func RawSyscall6(trap, a1, a2, a3, a4, a5, a6 uintptr) (r1, r2 uintptr, err Errno)
+
 const _SYS_DUP3 = 0
 
 // See version list in https://github.com/DragonFlyBSD/DragonFlyBSD/blob/master/sys/sys/param.h
