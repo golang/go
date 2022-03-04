6c221e47f76c9a48b2d631a43e975b5aea0c87b6
Ian Lance Taylor <iant@golang.org>
2022-03-03 18:23:35 -0800

    all: use new "unix" build tag where appropriate
    For #20322
    For #51572
    
    Change-Id: Id0b4799d097d01128e98ba4cc0092298357bca45
    Reviewed-on: https://go-review.googlesource.com/c/go/+/389935
    Trust: Ian Lance Taylor <iant@golang.org>
    Reviewed-by: Tobias Klauser <tobias.klauser@gmail.com>

--- src/time/zoneinfo_unix.go
+++ src/time/zoneinfo_unix.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-//go:build aix || (darwin && !ios) || dragonfly || freebsd || (linux && !android) || netbsd || openbsd || solaris
+//go:build unix && !ios && !android
 
 // Parse "zoneinfo" time zone file.
 // This is a fairly standard file format used on OS X, Linux, BSD, Sun, and others.
