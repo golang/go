085ef537c4a2c57d373e72f4a110d9fae9a287be
Tobias Klauser <tklauser@distanz.ch>
2022-03-08 14:54:53 +0100

    os, internal/syscall/unix: consolidate Pipe implementations
    All platforms with the pipe2 syscall now provide syscall.Pipe2.  Use it
    to implement os.Pipe.
    
    This also allows to drop the illumos-specific wrapper in
    internal/sys/unix.
    
    Change-Id: Ieb712a1498e86a389bad261e4e97c61c11d4bdd0
    Reviewed-on: https://go-review.googlesource.com/c/go/+/390715
    Trust: Tobias Klauser <tobias.klauser@gmail.com>
    Run-TryBot: Tobias Klauser <tobias.klauser@gmail.com>
    Trust: Matt Layher <mdlayher@gmail.com>
    TryBot-Result: Gopher Robot <gobot@golang.org>
    Reviewed-by: Ian Lance Taylor <iant@golang.org>

--- src/os/pipe2_unix.go
+++ src/os/pipe2_unix.go
@@ -1,24 +1,21 @@
-// Copyright 2020 The Go Authors. All rights reserved.
+// Copyright 2017 The Go Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-//go:build illumos
+//go:build dragonfly || freebsd || linux || netbsd || openbsd || solaris
 
 package os
 
-import (
-	"internal/syscall/unix"
-	"syscall"
-)
+import "syscall"
 
 // Pipe returns a connected pair of Files; reads from r return bytes written to w.
 // It returns the files and an error, if any.
 func Pipe() (r *File, w *File, err error) {
 	var p [2]int
 
-	e := unix.Pipe2(p[0:], syscall.O_CLOEXEC)
+	e := syscall.Pipe2(p[0:], syscall.O_CLOEXEC)
 	if e != nil {
-		return nil, nil, NewSyscallError("pipe", e)
+		return nil, nil, NewSyscallError("pipe2", e)
 	}
 
 	return newFile(uintptr(p[0]), "|0", kindPipe), newFile(uintptr(p[1]), "|1", kindPipe), nil
