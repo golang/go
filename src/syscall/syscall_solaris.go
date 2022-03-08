de8ddd97accb417450db014d1f45723515cb5d80
Tobias Klauser <tklauser@distanz.ch>
2022-03-08 11:13:37 +0100

    syscall: add Pipe2 on solaris and use it for forkExecPipe
    Other platforms already define Pipe2, so add it for solaris as well.
    
    Change-Id: If0d2dfc9a3613479fb4611a673a20a4aa0af0b2b
    Reviewed-on: https://go-review.googlesource.com/c/go/+/390714
    Trust: Tobias Klauser <tobias.klauser@gmail.com>
    Run-TryBot: Tobias Klauser <tobias.klauser@gmail.com>
    Trust: Matt Layher <mdlayher@gmail.com>
    TryBot-Result: Gopher Robot <gobot@golang.org>
    Reviewed-by: Ian Lance Taylor <iant@golang.org>

--- src/syscall/syscall_solaris.go
+++ src/syscall/syscall_solaris.go
@@ -63,6 +63,21 @@ func Pipe(p []int) (err error) {
 	return
 }
 
+//sysnb	pipe2(p *[2]_C_int, flags int) (err error)
+
+func Pipe2(p []int, flags int) error {
+	if len(p) != 2 {
+		return EINVAL
+	}
+	var pp [2]_C_int
+	err := pipe2(&pp, flags)
+	if err == nil {
+		p[0] = int(pp[0])
+		p[1] = int(pp[1])
+	}
+	return err
+}
+
 func (sa *SockaddrInet4) sockaddr() (unsafe.Pointer, _Socklen, error) {
 	if sa.Port < 0 || sa.Port > 0xFFFF {
 		return nil, 0, EINVAL
