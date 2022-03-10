29f0189dcddb37853c933de63458e2eca5b7b7a7
Tobias Klauser <tklauser@distanz.ch>
2022-03-10 09:28:09 +0100

    runtime/pprof, syscall: report MaxRSS on all unix platforms
    All unix platforms currently supported by Go provide the getrusage
    syscall. On aix and solaris the Getrusage syscall wrapper is not
    available yet, so add and use it to report MaxRSS in memory profiles.
    
    Change-Id: Ie880a3058171031fd2e12ccf9adfb85ce18858b1
    Reviewed-on: https://go-review.googlesource.com/c/go/+/391434
    Trust: Tobias Klauser <tobias.klauser@gmail.com>
    Run-TryBot: Tobias Klauser <tobias.klauser@gmail.com>
    TryBot-Result: Gopher Robot <gobot@golang.org>
    Reviewed-by: Michael Pratt <mpratt@google.com>
    Trust: Michael Pratt <mpratt@google.com>
    Reviewed-by: Ian Lance Taylor <iant@golang.org>

--- src/runtime/pprof/pprof_rusage.go
+++ src/runtime/pprof/pprof_rusage.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-//go:build darwin || linux
+//go:build aix || darwin || dragonfly || freebsd || linux || netbsd || openbsd || solaris
 
 package pprof
 
@@ -17,10 +17,12 @@ import (
 func addMaxRSS(w io.Writer) {
 	var rssToBytes uintptr
 	switch runtime.GOOS {
-	case "linux", "android":
+	case "aix", "android", "dragonfly", "freebsd", "linux", "netbsd", "openbsd":
 		rssToBytes = 1024
 	case "darwin", "ios":
 		rssToBytes = 1
+	case "illumos", "solaris":
+		rssToBytes = uintptr(syscall.Getpagesize())
 	default:
 		panic("unsupported OS")
 	}
