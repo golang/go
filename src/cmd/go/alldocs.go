712fed7bee1563adfa786137372d04324b482db8
qmuntal <quimmuntal@gmail.com>
2022-03-11 16:55:19 +0100

    cmd/go: support shared libraries in 'go version' on Windows
    This change modifies 'go version' to support shared windows libraries.
    
    Updates #48187
    
    Change-Id: I2e8436b8df84fe76677106fa9ca02dcd1fb90e77
    Reviewed-on: https://go-review.googlesource.com/c/go/+/391855
    Reviewed-by: Bryan Mills <bcmills@google.com>
    TryBot-Result: Gopher Robot <gobot@golang.org>
    Run-TryBot: Quim Muntal <quimmuntal@gmail.com>
    Run-TryBot: Bryan Mills <bcmills@google.com>
    Reviewed-by: Joedian Reid <joedian@golang.org>
    Auto-Submit: Bryan Mills <bcmills@google.com>

--- src/cmd/go/alldocs.go
+++ src/cmd/go/alldocs.go
@@ -1774,10 +1774,9 @@
 //
 //	go version [-m] [-v] [file ...]
 //
-// Version prints the build information for Go executables.
+// Version prints the build information for Go binary files.
 //
-// Go version reports the Go version used to build each of the named
-// executable files.
+// Go version reports the Go version used to build each of the named files.
 //
 // If no files are named on the command line, go version prints its own
 // version information.
@@ -1787,7 +1786,7 @@
 // By default, go version does not report unrecognized files found
 // during a directory scan. The -v flag causes it to report unrecognized files.
 //
-// The -m flag causes go version to print each executable's embedded
+// The -m flag causes go version to print each file's embedded
 // module version information, when available. In the output, the module
 // information consists of multiple lines following the version line, each
 // indented by a leading tab character.
