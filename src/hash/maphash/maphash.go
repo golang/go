9e16cc1541d42cb081d359339e3f45b4b9b2a372
Russ Cox <rsc@golang.org>
2022-03-14 12:31:33 -0400

    hash/maphash: add Bytes and String
    For very small inputs, h.Reset+h.Write+h.Sum64 is fundamentally
    slower than a single operation, by about a factor of two, because
    Write must copy the data into h's buffer, just in case there is another
    Write before the Sum64.
    
    A single function doing the whole sequence knows there is no extra
    write that will happen, so it doesn't need the buffer, so it avoids the copy.
    
    Fixes #42710.
    
    Change-Id: Icc79c68ccb10827f6640071d026df86b4940fcc1
    Reviewed-on: https://go-review.googlesource.com/c/go/+/392494
    Reviewed-by: Ian Lance Taylor <iant@golang.org>
    Trust: Russ Cox <rsc@golang.org>
    Run-TryBot: Russ Cox <rsc@golang.org>
    TryBot-Result: Gopher Robot <gobot@golang.org>

--- src/hash/maphash/maphash.go
+++ src/hash/maphash/maphash.go
@@ -33,6 +33,54 @@ type Seed struct {
 	s uint64
 }
 
+// Bytes returns the hash of b with the given seed.
+//
+// Bytes is equivalent to, but more convenient and efficient than:
+//
+//	var h Hash
+//	h.SetSeed(seed)
+//	h.Write(b)
+//	return h.Sum()
+func Bytes(seed Seed, b []byte) uint64 {
+	state := seed.s
+	if state == 0 {
+		panic("maphash: use of uninitialized Seed")
+	}
+	if len(b) == 0 {
+		return rthash(nil, 0, state) // avoid &b[0] index panic below
+	}
+	if len(b) > bufSize {
+		b = b[:len(b):len(b)] // merge len and cap calculations when reslicing
+		for len(b) > bufSize {
+			state = rthash(&b[0], bufSize, state)
+			b = b[bufSize:]
+		}
+	}
+	return rthash(&b[0], len(b), state)
+}
+
+// String returns the hash of s with the given seed.
+//
+// String is equivalent to, but more convenient and efficient than:
+//
+//	var h Hash
+//	h.SetSeed(seed)
+//	h.WriteString(s)
+//	return h.Sum()
+func String(seed Seed, s string) uint64 {
+	state := seed.s
+	if state == 0 {
+		panic("maphash: use of uninitialized Seed")
+	}
+	for len(s) > bufSize {
+		p := (*byte)((*unsafeheader.String)(unsafe.Pointer(&s)).Data)
+		state = rthash(p, bufSize, state)
+		s = s[bufSize:]
+	}
+	p := (*byte)((*unsafeheader.String)(unsafe.Pointer(&s)).Data)
+	return rthash(p, len(s), state)
+}
+
 // A Hash computes a seeded hash of a byte sequence.
 //
 // The zero Hash is a valid Hash ready to use.
