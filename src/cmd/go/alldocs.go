bdbfbefe2a5c15aedc2a7f4896c4769c6cbcdb28
Ian Lance Taylor <iant@golang.org>
2022-03-14 10:29:51 -0700

    cmd/go: document that 'go run' strips debug info
    Change-Id: Ie7293a33862853ac56ee0a9017b201d8ff0ba1f0
    Reviewed-on: https://go-review.googlesource.com/c/go/+/392574
    Trust: Ian Lance Taylor <iant@golang.org>
    Run-TryBot: Ian Lance Taylor <iant@golang.org>
    TryBot-Result: Gopher Robot <gobot@golang.org>
    Reviewed-by: Bryan Mills <bcmills@google.com>

--- src/cmd/go/alldocs.go
+++ src/cmd/go/alldocs.go
@@ -1599,6 +1599,10 @@
 // cross-compiled programs when a simulator or other execution method is
 // available.
 //
+// By default, 'go run' compiles the binary without generating the information
+// used by debuggers, to reduce build time. To include debugger information in
+// the binary, use 'go build'.
+//
 // The exit status of Run is not the exit status of the compiled binary.
 //
 // For more about build flags, see 'go help build'.
