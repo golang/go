c75befeec2a8ef2fea3c41da589ca0ffddda506f
Michael Matloob <matloob@golang.org>
2022-03-14 13:00:03 -0400

    cmd/go: don't compute Deps fields if they're not needed
    If the user provides the -json flag to explicitly specify fields, but
    doesn't specify the Deps or DepsErrors fields, skip computing the deps
    fields.
    
    For #29666
    
    Change-Id: I15596c374aba1af13bdf5808d11d54abdc838667
    Reviewed-on: https://go-review.googlesource.com/c/go/+/392495
    Reviewed-by: Bryan Mills <bcmills@google.com>
    Run-TryBot: Michael Matloob <matloob@golang.org>
    Auto-Submit: Michael Matloob <matloob@golang.org>
    Reviewed-by: Michael Matloob <matloob@golang.org>
    TryBot-Result: Gopher Robot <gobot@golang.org>

--- src/cmd/go/internal/list/list.go
+++ src/cmd/go/internal/list/list.go
@@ -568,6 +568,13 @@ func runList(ctx context.Context, cmd *base.Command, args []string) {
 		IgnoreImports:   *listFind,
 		ModResolveTests: *listTest,
 		LoadVCS:         true,
+		// SuppressDeps is set if the user opts to explicitly ask for the json fields they
+		// need, don't ask for Deps or DepsErrors. It's not set when using a template string,
+		// even if *listFmt doesn't contain .Deps because Deps are used to find import cycles
+		// for test variants of packages and users who have been providing format strings
+		// might not expect those errors to stop showing up.
+		// See issue #52443.
+		SuppressDeps: !listJsonFields.needAny("Deps", "DepsErrors"),
 	}
 	pkgs := load.PackagesAndErrors(ctx, pkgOpts, args)
 	if !*listE {
