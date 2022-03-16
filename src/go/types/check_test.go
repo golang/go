6be52abfa1aea30b33c1e2db27ea92e954661aa4
Robert Griesemer <gri@golang.org>
2022-03-16 15:17:36 -0700

    go/types, types2: remove mechanism to exclude tests for unified build
    The unified build will become the norm and the excluded tests run now.
    
    Change-Id: I0f0873eb73483a4f04736d167d2eb796ee8a857b
    Reviewed-on: https://go-review.googlesource.com/c/go/+/393438
    Trust: Robert Griesemer <gri@golang.org>
    Reviewed-by: Matthew Dempsky <mdempsky@google.com>

--- src/go/types/check_test.go
+++ src/go/types/check_test.go
@@ -31,7 +31,6 @@ import (
 	"go/parser"
 	"go/scanner"
 	"go/token"
-	"internal/buildcfg"
 	"internal/testenv"
 	"os"
 	"path/filepath"
@@ -200,26 +199,9 @@ func asGoVersion(s string) string {
 	return ""
 }
 
-// excludedForUnifiedBuild lists files that cannot be tested
-// when using the unified build's export data.
-// TODO(gri) enable as soon as the unified build supports this.
-var excludedForUnifiedBuild = map[string]bool{
-	"issue47818.go2": true,
-	"issue49705.go2": true,
-}
-
 func testFiles(t *testing.T, sizes Sizes, filenames []string, srcs [][]byte, manual bool, imp Importer) {
 	if len(filenames) == 0 {
 		t.Fatal("no source files")
-	}
-
-	if buildcfg.Experiment.Unified {
-		for _, f := range filenames {
-			if excludedForUnifiedBuild[filepath.Base(f)] {
-				t.Logf("%s cannot be tested with unified build - skipped", f)
-				return
-			}
-		}
 	}
 
 	if strings.HasSuffix(filenames[0], ".go1") {
