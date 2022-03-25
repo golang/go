5226d35cc85d45e210d77e6298fae5227c06776f
Paul E. Murphy <murp@ibm.com>
2022-03-25 10:37:11 -0500

    cmd/compile: reallocate FPR31 to XER for PPC64 regalloc
    We are limited to 64 registers in the allocator today, and PPC64
    has not been allocating F0,F26-F31 for quite some time without
    performance impact.
    
    Change-Id: If9d60be5037c94991fdd90a44461c3a6b96315cf
    Reviewed-on: https://go-review.googlesource.com/c/go/+/395835
    Reviewed-by: Keith Randall <khr@golang.org>
    Reviewed-by: Cherry Mui <cherryyz@google.com>
    Run-TryBot: Paul Murphy <murp@ibm.com>
    TryBot-Result: Gopher Robot <gobot@golang.org>

--- src/cmd/compile/internal/ssa/gen/PPC64Ops.go
+++ src/cmd/compile/internal/ssa/gen/PPC64Ops.go
@@ -82,7 +82,9 @@ var regNamesPPC64 = []string{
 	"F28",
 	"F29",
 	"F30",
-	"F31",
+	// "F31", the allocator is limited to 64 entries. We sacrifice this FPR to support XER.
+
+	"XER",
 
 	// If you add registers, update asyncPreempt in runtime.
 
@@ -124,7 +126,7 @@ func init() {
 
 	var (
 		gp = buildReg("R3 R4 R5 R6 R7 R8 R9 R10 R11 R12 R14 R15 R16 R17 R18 R19 R20 R21 R22 R23 R24 R25 R26 R27 R28 R29")
-		fp = buildReg("F0 F1 F2 F3 F4 F5 F6 F7 F8 F9 F10 F11 F12 F13 F14 F15 F16 F17 F18 F19 F20 F21 F22 F23 F24 F25 F26 F27 F28 F29 F30 F31")
+		fp = buildReg("F0 F1 F2 F3 F4 F5 F6 F7 F8 F9 F10 F11 F12 F13 F14 F15 F16 F17 F18 F19 F20 F21 F22 F23 F24 F25 F26 F27 F28 F29 F30")
 		sp = buildReg("SP")
 		sb = buildReg("SB")
 		gr = buildReg("g")
