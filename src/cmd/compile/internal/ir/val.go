b743c893ded74161f0b43eb46b7cad04fef6e9e7
Cuong Manh Le <cuong.manhle.vn@gmail.com>
2022-03-28 21:41:19 +0700

    cmd/compile/internal/ir: remove un-used code for const
    CL 390474 removed last usages of ConstValue, it can now be removed, and
    also Float64Val, since when it's only used by ConstValue.
    
    CanInt64 is un-used for a long time, its original form last usage was
    removed in CL 221802.
    
    Change-Id: Id142b0da49c319faca73ef1b2090325f81431321
    Reviewed-on: https://go-review.googlesource.com/c/go/+/396078
    Trust: Cuong Manh Le <cuong.manhle.vn@gmail.com>
    Run-TryBot: Cuong Manh Le <cuong.manhle.vn@gmail.com>
    TryBot-Result: Gopher Robot <gobot@golang.org>
    Reviewed-by: Matthew Dempsky <mdempsky@google.com>

--- src/cmd/compile/internal/ir/val.go
+++ src/cmd/compile/internal/ir/val.go
@@ -6,7 +6,6 @@ package ir
 
 import (
 	"go/constant"
-	"math"
 
 	"cmd/compile/internal/base"
 	"cmd/compile/internal/types"
@@ -17,27 +16,6 @@ func ConstType(n Node) constant.Kind {
 		return constant.Unknown
 	}
 	return n.Val().Kind()
-}
-
-// ConstValue returns the constant value stored in n as an interface{}.
-// It returns int64s for ints and runes, float64s for floats,
-// and complex128s for complex values.
-func ConstValue(n Node) interface{} {
-	switch v := n.Val(); v.Kind() {
-	default:
-		base.Fatalf("unexpected constant: %v", v)
-		panic("unreachable")
-	case constant.Bool:
-		return constant.BoolVal(v)
-	case constant.String:
-		return constant.StringVal(v)
-	case constant.Int:
-		return IntVal(n.Type(), v)
-	case constant.Float:
-		return Float64Val(v)
-	case constant.Complex:
-		return complex(Float64Val(constant.Real(v)), Float64Val(constant.Imag(v)))
-	}
 }
 
 // IntVal returns v converted to int64.
@@ -53,14 +31,6 @@ func IntVal(t *types.Type, v constant.Value) int64 {
 		}
 	}
 	base.Fatalf("%v out of range for %v", v, t)
-	panic("unreachable")
-}
-
-func Float64Val(v constant.Value) float64 {
-	if x, _ := constant.Float64Val(v); !math.IsInf(x, 0) {
-		return x + 0 // avoid -0 (should not be needed, but be conservative)
-	}
-	base.Fatalf("bad float64 value: %v", v)
 	panic("unreachable")
 }
 
@@ -113,18 +83,6 @@ func idealType(ct constant.Kind) *types.Type {
 }
 
 var OKForConst [types.NTYPE]bool
-
-// CanInt64 reports whether it is safe to call Int64Val() on n.
-func CanInt64(n Node) bool {
-	if !IsConst(n, constant.Int) {
-		return false
-	}
-
-	// if the value inside n cannot be represented as an int64, the
-	// return value of Int64 is undefined
-	_, ok := constant.Int64Val(n.Val())
-	return ok
-}
 
 // Int64Val returns n as an int64.
 // n must be an integer or rune constant.
