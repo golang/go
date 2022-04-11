4fd27ebb44f8ea8aa4039a72f4653db756cde040
Russ Cox <rsc@golang.org>
2022-04-11 17:19:09 -0400

    go/ast, go/printer: recognize export and extern line directives
    Now that gofmt is reformatting these, we can't get away with
    not knowing about directives such as //export and //extern (for gccgo).
    Otherwise "//export foo" and "//extern foo" turn into "// export foo",
    and "// extern foo", which are completely different meanings.
    
    For #51082.
    
    Change-Id: Id0970331fa0b52ab5fa621631b5fa460767068bb
    Reviewed-on: https://go-review.googlesource.com/c/go/+/399734
    Run-TryBot: Russ Cox <rsc@golang.org>
    TryBot-Result: Gopher Robot <gobot@golang.org>
    Auto-Submit: Russ Cox <rsc@golang.org>
    Reviewed-by: Ian Lance Taylor <iant@google.com>

--- src/go/ast/ast.go
+++ src/go/ast/ast.go
@@ -161,8 +161,10 @@ func (g *CommentGroup) Text() string {
 // This code is also in go/printer.
 func isDirective(c string) bool {
 	// "//line " is a line directive.
+	// "//extern " is for gccgo.
+	// "//export " is for cgo.
 	// (The // has been removed.)
-	if strings.HasPrefix(c, "line ") {
+	if strings.HasPrefix(c, "line ") || strings.HasPrefix(c, "extern ") || strings.HasPrefix(c, "export ") {
 		return true
 	}
 
