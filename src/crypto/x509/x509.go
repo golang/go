f6860725f326d625e5ea5610aa1e42311d8540c9
Roland Shoemaker <roland@golang.org>
2022-04-14 17:57:22 -0700

    crypto/x509: don't create certs with negative serials
    Refuse to create certificates with negative serial numbers, as they
    are explicitly disallowed by RFC 5280.
    
    We still allow parsing certificates with negative serial numbers,
    because in the past there were buggy CA implementations which would
    produce them (although there are currently *no* trusted certificates
    that have this issue). We may want to revisit this decision if we can
    find metrics about the prevalence of this issue in enterprise settings.
    
    Change-Id: I131262008db99b6354f542f335abc68775a2d6d0
    Reviewed-on: https://go-review.googlesource.com/c/go/+/400494
    Reviewed-by: Damien Neil <dneil@google.com>
    Reviewed-by: Roland Shoemaker <roland@golang.org>
    Run-TryBot: Roland Shoemaker <roland@golang.org>
    Auto-Submit: Roland Shoemaker <roland@golang.org>
    TryBot-Result: Gopher Robot <gobot@golang.org>

--- src/crypto/x509/x509.go
+++ src/crypto/x509/x509.go
@@ -1478,13 +1478,17 @@ func CreateCertificate(rand io.Reader, template, parent *Certificate, pub, priv 
 		return nil, errors.New("x509: no SerialNumber given")
 	}
 
-	// RFC 5280 Section 4.1.2.2: serial number must not be longer than 20 octets
+	// RFC 5280 Section 4.1.2.2: serial number must positive and should not be longer
+	// than 20 octets.
 	//
 	// We cannot simply check for len(serialBytes) > 20, because encoding/asn1 may
 	// pad the slice in order to prevent the integer being mistaken for a negative
 	// number (DER uses the high bit of the left-most byte to indicate the sign.),
 	// so we need to double check the composition of the serial if it is exactly
 	// 20 bytes.
+	if template.SerialNumber.Sign() == -1 {
+		return nil, errors.New("x509: serial number must be positive")
+	}
 	serialBytes := template.SerialNumber.Bytes()
 	if len(serialBytes) > 20 || (len(serialBytes) == 20 && serialBytes[0]&0x80 != 0) {
 		return nil, errors.New("x509: serial number exceeds 20 octets")
