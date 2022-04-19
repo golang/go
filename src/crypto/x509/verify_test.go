8473ed9fd074f7c040a579622c88558951929ee4
Roland Shoemaker <roland@golang.org>
2022-04-19 12:05:10 -0700

    crypto/x509: use SAN when comparing certs during path building
    Per RFC 4158 Section 2.4.2, when we are discarding candidate
    certificates during path building, use the SANs as well as subject and
    public key when checking whether a certificate is already present in
    the built path. This supports the case where a certificate in the chain
    (typically a leaf) has the exact same subject and public key as another
    certificate in the chain (typically its parent) but has SANs which don't
    match.
    
    Change-Id: I212c234e94a1f6afbe9691e4a3ba257461db3a7e
    Reviewed-on: https://go-review.googlesource.com/c/go/+/401115
    Reviewed-by: Roland Shoemaker <roland@golang.org>
    Run-TryBot: Roland Shoemaker <roland@golang.org>
    TryBot-Result: Gopher Robot <gobot@golang.org>
    Reviewed-by: Damien Neil <dneil@google.com>

--- src/crypto/x509/verify_test.go
+++ src/crypto/x509/verify_test.go
@@ -2340,6 +2340,29 @@ func TestPathBuilding(t *testing.T) {
 				"CN=leaf -> CN=inter b -> CN=inter c -> CN=root",
 			},
 		},
+		{
+			// Build a simple two node graph, where the leaf is directly issued from
+			// the root and both certificates have matching subject and public key, but
+			// the leaf has SANs.
+			name: "leaf with same subject, key, as parent but with SAN",
+			graph: trustGraphDescription{
+				Roots: []string{"root"},
+				Leaf:  "root",
+				Graph: []trustGraphEdge{
+					{
+						Issuer:  "root",
+						Subject: "root",
+						Type:    leafCertificate,
+						MutateTemplate: func(c *Certificate) {
+							c.DNSNames = []string{"localhost"}
+						},
+					},
+				},
+			},
+			expectedChains: []string{
+				"CN=root -> CN=root",
+			},
+		},
 	}
 
 	for _, tc := range tests {
