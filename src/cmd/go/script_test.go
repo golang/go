06e8022ed43ea83d0d7d76366f4930903220bc25
Than McIntosh <thanm@google.com>
2022-04-25 10:09:47 -0400

    cmd/go: add hook to check for GOEXPERIMENT in script tests
    Add a new hook to allow script tests to check whether a specific
    GOEXPERIMENT is enabled.
    
    Updates #51430.
    
    Change-Id: Icdf39f845ff2c8b10c634d49e9c27bc90e7984f8
    Reviewed-on: https://go-review.googlesource.com/c/go/+/402174
    Reviewed-by: Bryan Mills <bcmills@google.com>
    Run-TryBot: Than McIntosh <thanm@google.com>
    TryBot-Result: Gopher Robot <gobot@golang.org>

--- src/cmd/go/script_test.go
+++ src/cmd/go/script_test.go
@@ -14,6 +14,7 @@ import (
 	"flag"
 	"fmt"
 	"go/build"
+	"internal/buildcfg"
 	"internal/testenv"
 	"internal/txtar"
 	"io/fs"
@@ -246,6 +247,24 @@ func goVersion() (string, error) {
 
 var execCache par.Cache
 
+func goExperimentIsValid(expname string) bool {
+	for _, exp := range buildcfg.Experiment.All() {
+		if expname == exp || expname == "no"+exp || "no"+expname == exp {
+			return true
+		}
+	}
+	return false
+}
+
+func goExperimentIsEnabled(expname string) bool {
+	for _, exp := range buildcfg.Experiment.Enabled() {
+		if exp == expname {
+			return true
+		}
+	}
+	return false
+}
+
 // run runs the test script.
 func (ts *testScript) run() {
 	// Truncate log at end of last phase marker,
@@ -442,6 +461,15 @@ Script:
 				if strings.HasPrefix(cond.tag, "buildmode:") {
 					value := strings.TrimPrefix(cond.tag, "buildmode:")
 					ok = sys.BuildModeSupported(runtime.Compiler, value, runtime.GOOS, runtime.GOARCH)
+					break
+				}
+				if strings.HasPrefix(cond.tag, "GOEXPERIMENT:") {
+					rawval := strings.TrimPrefix(cond.tag, "GOEXPERIMENT:")
+					value := strings.TrimSpace(rawval)
+					if !goExperimentIsValid(value) {
+						ts.fatalf("unknown/unrecognized GOEXPERIMENT %q", value)
+					}
+					ok = goExperimentIsEnabled(value)
 					break
 				}
 				if !imports.KnownArch[cond.tag] && !imports.KnownOS[cond.tag] && cond.tag != "gc" && cond.tag != "gccgo" {
