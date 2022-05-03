43bba587b0768f3525662ab11196ad430f2f6209
Robert Griesemer <gri@golang.org>
2022-05-03 10:42:22 -0700

    go/parser: accept all valid type parameter lists
    This is a port of CL 402256 from the syntax package to go/parser
    with adjustments because of the different AST structure, and
    excluding any necessary go/printer changes (separate CL).
    
    Type parameter lists starting with the form [name *T|...] or
    [name (X)|...] may look like an array length expression [x].
    Only after parsing the entire initial expression and checking
    whether the expression contains type elements or is followed
    by a comma can we make the final decision.
    
    This change simplifies the existing parsing strategy: instead
    of trying to make an upfront decision with limited information
    (which is insufficient), the parser now parses the start of a
    type parameter list or array length specification as expression.
    In a second step, if the expression can be split into a name
    followed by a type element, or a name followed by an ordinary
    expression which is succeeded by a comma, we assume a type
    parameter list (because it can't be an array length).
    In all other cases we assume an array length specification.
    
    Fixes #52559.
    
    Change-Id: I11ab6e62b073b78b2331bb6063cf74d2a9eaa236
    Reviewed-on: https://go-review.googlesource.com/c/go/+/403937
    Run-TryBot: Robert Griesemer <gri@google.com>
    Reviewed-by: Robert Findley <rfindley@google.com>
    Reviewed-by: Robert Griesemer <gri@google.com>

--- src/go/types/testdata/fixedbugs/issue49482.go
+++ src/go/types/testdata/fixedbugs/issue49482.go
@@ -2,9 +2,6 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// This file is tested when running "go test -run Manual"
-// without source arguments. Use for one-off debugging.
-
 package p
 
 // The following is OK, per the special handling for type literals discussed in issue #49482.
@@ -14,12 +11,16 @@ type _[P (*int),] int
 
 const P = 2 // declare P to avoid noisy 'undeclared name' errors below.
 
-// The following parse as invalid array types.
-type _[P *int /* ERROR "int \(type\) is not an expression" */ ] int
-type _[P /* ERROR non-function P */ (*int)] int
+// The following parse as invalid array types due to parsing ambiguitiues.
+type _ [P *int /* ERROR "int \(type\) is not an expression" */ ]int
+type _ [P /* ERROR non-function P */ (*int)]int
 
-// The following should be parsed as a generic type, but is instead parsed as an array type.
-type _[P *struct /* ERROR "expected expression" */ {}| int /* ERROR "not an expression" */ ] struct{}
+// Adding a trailing comma or an enclosing interface resolves the ambiguity.
+type _[P *int,] int
+type _[P (*int),] int
+type _[P interface{*int}] int
+type _[P interface{(*int)}] int
 
-// The following fails to parse, due to the '~'
-type _[P *struct /* ERROR "expected expression" */ {}|~int /* ERROR "not an expression" */ ] struct{}
+// The following parse correctly as valid generic types.
+type _[P *struct{} | int] struct{}
+type _[P *struct{} | ~int] struct{}
