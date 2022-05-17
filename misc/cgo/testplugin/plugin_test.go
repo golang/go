4ab5dae7caa9165146795073aeb808fdd5c95eac
Cuong Manh Le <cuong.manhle.vn@gmail.com>
2022-05-17 15:38:35 +0700

    test,misc: fix builders that do not support plugin
    CL 406358 added test that use -buildmode=plugin. But plugin mode only
    supports on some os/arch pairs, so this CL moving the test to
    misc/cgo/testplugin directory instead.
    
    Updates #52937
    
    Change-Id: Iad049443c1f6539f6af1988bebd4dff56c6e1bf9
    Reviewed-on: https://go-review.googlesource.com/c/go/+/406774
    Reviewed-by: Michael Knyszek <mknyszek@google.com>
    Run-TryBot: Cuong Manh Le <cuong.manhle.vn@gmail.com>
    Reviewed-by: Cherry Mui <cherryyz@google.com>
    Auto-Submit: Cuong Manh Le <cuong.manhle.vn@gmail.com>
    TryBot-Result: Gopher Robot <gobot@golang.org>

--- misc/cgo/testplugin/plugin_test.go
+++ misc/cgo/testplugin/plugin_test.go
@@ -296,6 +296,10 @@ func TestIssue44956(t *testing.T) {
 	run(t, "./issue44956.exe")
 }
 
+func TestIssue52937(t *testing.T) {
+	goCmd(t, "build", "-buildmode=plugin", "-o", "issue52937.so", "./issue52937/main.go")
+}
+
 func TestForkExec(t *testing.T) {
 	// Issue 38824: importing the plugin package causes it hang in forkExec on darwin.
 
