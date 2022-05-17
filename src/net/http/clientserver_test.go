ea521b02ae1e873f9b8be6a2a3e81699d8eb5584
Kévin Dunglas <kevin@dunglas.fr>
2022-05-17 21:21:09 +0200

    net/http: unskip TestEarlyHintsRequest_h2
    golang/net#134 and golang/net#96 have been merged.
    This patch updates h2_bundle.go and enables TestEarlyHintsRequest_h2.

--- src/net/http/clientserver_test.go
+++ src/net/http/clientserver_test.go
@@ -1624,9 +1624,6 @@ func TestEarlyHintsRequest_h1(t *testing.T) { testEarlyHintsRequest(t, h1Mode) }
 func TestEarlyHintsRequest_h2(t *testing.T) { testEarlyHintsRequest(t, h2Mode) }
 func testEarlyHintsRequest(t *testing.T, h2 bool) {
 	defer afterTest(t)
-	if h2 {
-		t.Skip("Waiting for H2 support to be merged: https://go-review.googlesource.com/c/net/+/406494")
-	}
 
 	var wg sync.WaitGroup
 	wg.Add(1)
