ecc70d722406f0c7d0c1930c872db392e80e7cf5
Motiejus Jakštys <motiejus@jakstys.lt>
2022-05-19 09:41:46 +0300

    cmd/dist: support spaces and quotes in CC
    As of CL 334732 `go build` can accept `$CC` with spaces and quotes,
    which lets us easily use `zig cc` as the C compiler, or easily pass
    extra compiler parameters:
    
    ```
    CC="zig cc" go build <...>
    CC="clang-13 -v" go build <...>
    CC="zig cc -Wl,--print-gc-sections" go build <...>
    ```
    
    However, the same does not apply for building go itself:
    
    ```
    $ CC="zig cc" ./make.bash
    Building Go cmd/dist using /usr/local/go. (go1.18.2 linux/amd64)
    go tool dist: cannot invoke C compiler "zig cc": exec: "zig cc": executable file not found in $PATH
    
    Go needs a system C compiler for use with cgo.
    To set a C compiler, set CC=the-compiler.
    To disable cgo, set CGO_ENABLED=0.
    ```
    
    With this change Go can be built directly with `zig cc` (the linker arg
    will disappear with CL 407216):
    
    ```
    $ CC="zig cc -Wl,--no-gc-sections" ./make.bash
    Building Go cmd/dist using /usr/local/go. (go1.18.2 linux/amd64)
    Building Go toolchain1 using /usr/local/go.
    Building Go bootstrap cmd/go (go_bootstrap) using Go toolchain1.
    Building Go toolchain2 using go_bootstrap and Go toolchain1.
    Building Go toolchain3 using go_bootstrap and Go toolchain2.
    Building packages and commands for linux/amd64.
    ---
    Installed Go for linux/amd64 in /home/motiejus/code/go
    Installed commands in /home/motiejus/code/go/bin
    $ ../bin/go version
    go version devel go1.19-811f1913a8 Thu May 19 09:44:49 2022 +0300 linux/amd64
    ```
    
    Fixes #52990

--- src/cmd/dist/build.go
+++ src/cmd/dist/build.go
@@ -1631,7 +1631,13 @@ func checkCC() {
 	if !needCC() {
 		return
 	}
-	if output, err := exec.Command(defaultcc[""], "--help").CombinedOutput(); err != nil {
+	cc, err := quotedSplit(defaultcc[""])
+	if err != nil {
+		fatalf("split CC: %v", err)
+	}
+	var ccHelp = append(cc, "--help")
+
+	if output, err := exec.Command(ccHelp[0], ccHelp[1:]...).CombinedOutput(); err != nil {
 		outputHdr := ""
 		if len(output) > 0 {
 			outputHdr = "\nCommand output:\n\n"
@@ -1639,7 +1645,7 @@ func checkCC() {
 		fatalf("cannot invoke C compiler %q: %v\n\n"+
 			"Go needs a system C compiler for use with cgo.\n"+
 			"To set a C compiler, set CC=the-compiler.\n"+
-			"To disable cgo, set CGO_ENABLED=0.\n%s%s", defaultcc[""], err, outputHdr, output)
+			"To disable cgo, set CGO_ENABLED=0.\n%s%s", cc, err, outputHdr, output)
 	}
 }
 
