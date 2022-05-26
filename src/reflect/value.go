e6d8b05a61e3977f5769e8e896d24d2d26dcb23c
Ian Lance Taylor <iant@golang.org>
2022-05-26 17:52:28 +0000

    Revert "reflect: deprecate (Slice|String)Header"
    This reverts CL 401434.
    
    Reason for revert: We should't deprecate these without a replacement.
    
    Fixes #53079
    
    Change-Id: I5515b03e39787d4e528a98c733cdb643b86b17ce
    Reviewed-on: https://go-review.googlesource.com/c/go/+/408815
    Reviewed-by: Ian Lance Taylor <iant@google.com>
    Auto-Submit: Ian Lance Taylor <iant@google.com>
    Reviewed-by: Dmitri Shuralyov <dmitshur@google.com>
    Run-TryBot: Ian Lance Taylor <iant@golang.org>
    TryBot-Result: Gopher Robot <gobot@golang.org>
    Reviewed-by: Dmitri Shuralyov <dmitshur@golang.org>
    Run-TryBot: Ian Lance Taylor <iant@google.com>

--- src/reflect/value.go
+++ src/reflect/value.go
@@ -2667,9 +2667,6 @@ func (v Value) UnsafePointer() unsafe.Pointer {
 // Moreover, the Data field is not sufficient to guarantee the data
 // it references will not be garbage collected, so programs must keep
 // a separate, correctly typed pointer to the underlying data.
-//
-// Deprecated: this type should not be used, it exists only for
-// backward compatibility.
 type StringHeader struct {
 	Data uintptr
 	Len  int
@@ -2681,9 +2678,6 @@ type StringHeader struct {
 // Moreover, the Data field is not sufficient to guarantee the data
 // it references will not be garbage collected, so programs must keep
 // a separate, correctly typed pointer to the underlying data.
-//
-// Deprecated: this type should not be used, it exists only for
-// backward compatibility.
 type SliceHeader struct {
 	Data uintptr
 	Len  int
