62359d572ea3e149d78fbb216e9dce035c219371
Pouya Kary ✨ <pouya@kary.us>
2022-05-28 13:46:14 +0000

    modify wasm_exec to also load in node

--- misc/wasm/wasm_exec_node.js
+++ misc/wasm/wasm_exec_node.js
@@ -9,25 +9,6 @@ if (process.argv.length < 3) {
 	process.exit(1);
 }
 
-globalThis.require = require;
-globalThis.fs = require("fs");
-globalThis.TextEncoder = require("util").TextEncoder;
-globalThis.TextDecoder = require("util").TextDecoder;
-
-globalThis.performance = {
-	now() {
-		const [sec, nsec] = process.hrtime();
-		return sec * 1000 + nsec / 1000000;
-	},
-};
-
-const crypto = require("crypto");
-globalThis.crypto = {
-	getRandomValues(b) {
-		crypto.randomFillSync(b);
-	},
-};
-
 require("./wasm_exec");
 
 const go = new Go();
