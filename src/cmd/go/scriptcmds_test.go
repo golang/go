151668c694a15029db236976f06f18482859db61
Bryan C. Mills <bcmills@google.com>
2022-07-20 17:02:35 -0400

    cmd/go: extract the TestScript engine into a standalone package
    This change decouples the script engine from both cmd/go and the
    testing package; I intend to reuse it in the replacement for the
    vcs-test.golang.org server.
    
    This change also adds a few new script commands:
    
    	- 'echo' echoes its arguments, useful for verifying argument expansion.
    
    	- 'cat' prints the contents of files, useful for debugging failing script tests.
    
    	- 'help' displays information about script commands and conditions,
    	  reducing the toil of maintaining lists in the README file.
    
    The 'cmp' and 'cmpenv' commands now use internal/diff instead of their
    own separate diff implementation.
    
    The 'env' command now writes to the script log instead of the stdout
    buffer. (This makes it more consistent with the behavior of other
    synchronous builtins.)
    
    The 'stale' command no longer logs output when a target is
    unexpectedly non-stale. (However, the ouput of the 'stale' command is
    not usually very useful anyway.)
    
    The 'grep', 'stdout', and 'stderr' commands now display matching lines
    (like Unix 'grep'), making their negation behavior more consistent
    with running real commands on a command-line.
    
    Likewise, the 'cmp' command now always displays differences. That
    makes it useful with the '?' prefix to produce diffs for informational
    purposes while debugging.
    
    For #27494.
    
    Change-Id: If49fd81d9b922d07c20618a8e2cef908191f9ef6
    Reviewed-on: https://go-review.googlesource.com/c/go/+/419875
    Reviewed-by: Russ Cox <rsc@golang.org>
    Auto-Submit: Bryan Mills <bcmills@google.com>
    Run-TryBot: Bryan Mills <bcmills@google.com>
    TryBot-Result: Gopher Robot <gobot@golang.org>

---
+++ src/cmd/go/scriptcmds_test.go
@@ -0,0 +1,131 @@
+// Copyright 2018 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main_test
+
+import (
+	"bytes"
+	"cmd/go/internal/script"
+	"cmd/go/internal/script/scripttest"
+	"cmd/go/internal/work"
+	"errors"
+	"fmt"
+	"os"
+	"strings"
+	"time"
+)
+
+func scriptCommands(interrupt os.Signal, gracePeriod time.Duration) map[string]script.Cmd {
+	cmds := scripttest.DefaultCmds()
+
+	// Customize the "exec" interrupt signal and grace period.
+	cmdExec := script.Exec(quitSignal(), gracePeriod)
+	cmds["exec"] = cmdExec
+
+	add := func(name string, cmd script.Cmd) {
+		if _, ok := cmds[name]; ok {
+			panic(fmt.Sprintf("command %q is already registered", name))
+		}
+		cmds[name] = cmd
+	}
+
+	add("addcrlf", scriptAddCRLF())
+	add("cc", scriptCC(cmdExec))
+	cmdGo := scriptGo(interrupt, gracePeriod)
+	add("go", cmdGo)
+	add("stale", scriptStale(cmdGo))
+
+	return cmds
+}
+
+// scriptAddCRLF adds CRLF line endings to the named files.
+func scriptAddCRLF() script.Cmd {
+	return script.Command(
+		script.CmdUsage{
+			Summary: "convert line endings to CRLF",
+			Args:    "file...",
+		},
+		func(s *script.State, args ...string) (script.WaitFunc, error) {
+			if len(args) == 0 {
+				return nil, script.ErrUsage
+			}
+
+			for _, file := range args {
+				file = s.Path(file)
+				data, err := os.ReadFile(file)
+				if err != nil {
+					return nil, err
+				}
+				err = os.WriteFile(file, bytes.ReplaceAll(data, []byte("\n"), []byte("\r\n")), 0666)
+				if err != nil {
+					return nil, err
+				}
+			}
+
+			return nil, nil
+		})
+}
+
+// scriptCC runs the C compiler along with platform specific options.
+func scriptCC(cmdExec script.Cmd) script.Cmd {
+	return script.Command(
+		script.CmdUsage{
+			Summary: "run the platform C compiler",
+			Args:    "args...",
+		},
+		func(s *script.State, args ...string) (script.WaitFunc, error) {
+			b := work.NewBuilder(s.Getwd())
+			wait, err := cmdExec.Run(s, append(b.GccCmd(".", ""), args...)...)
+			if err != nil {
+				return wait, err
+			}
+			waitAndClean := func(s *script.State) (stdout, stderr string, err error) {
+				stdout, stderr, err = wait(s)
+				if closeErr := b.Close(); err == nil {
+					err = closeErr
+				}
+				return stdout, stderr, err
+			}
+			return waitAndClean, nil
+		})
+}
+
+// scriptGo runs the go command.
+func scriptGo(interrupt os.Signal, gracePeriod time.Duration) script.Cmd {
+	return script.Program(testGo, interrupt, gracePeriod)
+}
+
+// scriptStale checks that the named build targets are stale.
+func scriptStale(cmdGo script.Cmd) script.Cmd {
+	return script.Command(
+		script.CmdUsage{
+			Summary: "check that build targets are stale",
+			Args:    "target...",
+		},
+		func(s *script.State, args ...string) (script.WaitFunc, error) {
+			if len(args) == 0 {
+				return nil, script.ErrUsage
+			}
+			tmpl := "{{if .Error}}{{.ImportPath}}: {{.Error.Err}}" +
+				"{{else}}{{if not .Stale}}{{.ImportPath}} ({{.Target}}) is not stale{{end}}" +
+				"{{end}}"
+
+			wait, err := cmdGo.Run(s, append([]string{"list", "-e", "-f=" + tmpl}, args...)...)
+			if err != nil {
+				return nil, err
+			}
+
+			stdout, stderr, err := wait(s)
+			if len(stderr) != 0 {
+				s.Logf("%s", stderr)
+			}
+			if err != nil {
+				return nil, err
+			}
+			if out := strings.TrimSpace(stdout); out != "" {
+				return nil, errors.New(out)
+			}
+			return nil, nil
+		})
+}
---
+++ src/cmd/go/scriptconds_test.go
@@ -0,0 +1,165 @@
+// Copyright 2018 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main_test
+
+import (
+	"cmd/go/internal/script"
+	"cmd/go/internal/script/scripttest"
+	"errors"
+	"fmt"
+	"internal/buildcfg"
+	"internal/platform"
+	"internal/testenv"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"runtime"
+	"runtime/debug"
+	"strings"
+)
+
+func scriptConditions() map[string]script.Cond {
+	conds := scripttest.DefaultConds()
+
+	// Our "exec" has a special case for plan9 git, which does not
+	// behave like git on other platforms.
+	//
+	// TODO(bcmills): replace this special-case "exec" with a more tailored "git"
+	// condition.
+	conds["exec"] = script.CachedCondition(
+		conds["exec"].Usage().Summary,
+		func(name string) (bool, error) {
+			if runtime.GOOS == "plan9" && name == "git" {
+				// The Git command is usually not the real Git on Plan 9.
+				// See https://golang.org/issues/29640.
+				return false, nil
+			}
+			_, err := exec.LookPath(name)
+			return err == nil, nil
+		})
+
+	add := func(name string, cond script.Cond) {
+		if _, ok := conds[name]; ok {
+			panic(fmt.Sprintf("condition %q is already registered", name))
+		}
+		conds[name] = cond
+	}
+
+	lazyBool := func(summary string, f func() bool) script.Cond {
+		return script.OnceCondition(summary, func() (bool, error) { return f(), nil })
+	}
+
+	add("asan", sysCondition("-asan", platform.ASanSupported))
+	add("buildmode", script.PrefixCondition("go supports -buildmode=<suffix>", hasBuildmode))
+	add("case-sensitive", script.OnceCondition("$WORK filesystem is case-sensitive", isCaseSensitive))
+	add("cgo", script.BoolCondition("host CGO_ENABLED", canCgo))
+	add("cross", script.BoolCondition("cmd/go GOOS/GOARCH != GOHOSTOS/GOHOSTARCH", goHostOS != runtime.GOOS || goHostArch != runtime.GOARCH))
+	add("fuzz", sysCondition("-fuzz", platform.FuzzSupported))
+	add("fuzz-instrumented", sysCondition("-fuzz with instrumentation", platform.FuzzInstrumented))
+	add("gc", script.BoolCondition(`runtime.Compiler == "gc"`, runtime.Compiler == "gc"))
+	add("gccgo", script.BoolCondition(`runtime.Compiler == "gccgo"`, runtime.Compiler == "gccgo"))
+	add("GODEBUG", script.PrefixCondition("GODEBUG contains <suffix>", hasGodebug))
+	add("GOEXPERIMENT", script.PrefixCondition("GOEXPERIMENT <suffix> is enabled", hasGoexperiment))
+	add("link", lazyBool("testenv.HasLink()", testenv.HasLink))
+	add("mismatched-goroot", script.Condition("test's GOROOT_FINAL does not match the real GOROOT", isMismatchedGoroot))
+	add("msan", sysCondition("-msan", platform.MSanSupported))
+	add("net", lazyBool("testenv.HasExternalNetwork()", testenv.HasExternalNetwork))
+	add("race", sysCondition("-race", platform.RaceDetectorSupported))
+	add("symlink", lazyBool("testenv.HasSymlink()", testenv.HasSymlink))
+	add("trimpath", script.OnceCondition("test binary was built with -trimpath", isTrimpath))
+
+	return conds
+}
+
+func isMismatchedGoroot(s *script.State) (bool, error) {
+	gorootFinal, _ := s.LookupEnv("GOROOT_FINAL")
+	if gorootFinal == "" {
+		gorootFinal, _ = s.LookupEnv("GOROOT")
+	}
+	return gorootFinal != testGOROOT, nil
+}
+
+func sysCondition(flag string, f func(goos, goarch string) bool) script.Cond {
+	return script.Condition(
+		"GOOS/GOARCH supports "+flag,
+		func(s *script.State) (bool, error) {
+			GOOS, _ := s.LookupEnv("GOOS")
+			GOARCH, _ := s.LookupEnv("GOARCH")
+			return f(GOOS, GOARCH), nil
+		})
+}
+
+func hasBuildmode(s *script.State, mode string) (bool, error) {
+	GOOS, _ := s.LookupEnv("GOOS")
+	GOARCH, _ := s.LookupEnv("GOARCH")
+	return platform.BuildModeSupported(runtime.Compiler, mode, GOOS, GOARCH), nil
+}
+
+func hasGodebug(s *script.State, value string) (bool, error) {
+	godebug, _ := s.LookupEnv("GODEBUG")
+	for _, p := range strings.Split(godebug, ",") {
+		if strings.TrimSpace(p) == value {
+			return true, nil
+		}
+	}
+	return false, nil
+}
+
+func hasGoexperiment(s *script.State, value string) (bool, error) {
+	GOOS, _ := s.LookupEnv("GOOS")
+	GOARCH, _ := s.LookupEnv("GOARCH")
+	goexp, _ := s.LookupEnv("GOEXPERIMENT")
+	flags, err := buildcfg.ParseGOEXPERIMENT(GOOS, GOARCH, goexp)
+	if err != nil {
+		return false, err
+	}
+	for _, exp := range flags.All() {
+		if value == exp {
+			return true, nil
+		}
+		if strings.TrimPrefix(value, "no") == strings.TrimPrefix(exp, "no") {
+			return false, nil
+		}
+	}
+	return false, fmt.Errorf("unrecognized GOEXPERIMENT %q", value)
+}
+
+func isCaseSensitive() (bool, error) {
+	tmpdir, err := os.MkdirTemp(testTmpDir, "case-sensitive")
+	if err != nil {
+		return false, fmt.Errorf("failed to create directory to determine case-sensitivity: %w", err)
+	}
+	defer os.RemoveAll(tmpdir)
+
+	fcap := filepath.Join(tmpdir, "FILE")
+	if err := os.WriteFile(fcap, []byte{}, 0644); err != nil {
+		return false, fmt.Errorf("error writing file to determine case-sensitivity: %w", err)
+	}
+
+	flow := filepath.Join(tmpdir, "file")
+	_, err = os.ReadFile(flow)
+	switch {
+	case err == nil:
+		return false, nil
+	case os.IsNotExist(err):
+		return true, nil
+	default:
+		return false, fmt.Errorf("unexpected error reading file when determining case-sensitivity: %w", err)
+	}
+}
+
+func isTrimpath() (bool, error) {
+	info, _ := debug.ReadBuildInfo()
+	if info == nil {
+		return false, errors.New("missing build info")
+	}
+
+	for _, s := range info.Settings {
+		if s.Key == "-trimpath" && s.Value == "true" {
+			return true, nil
+		}
+	}
+	return false, nil
+}
