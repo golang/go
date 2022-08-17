dc032abffb530f52695f7f27b13e462db8ce5d71
doujiang24 <doujiang24@gmail.com>
2022-08-17 10:41:26 +0800

    wip: test: added cgo trace parse test, should be passed in on platforms.
    just a test for #54458

---
+++ src/runtime/testdata/testprogcgo/cgotrace.go
@@ -0,0 +1,53 @@
+// Copyright 2011 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// This is for issue #29707
+
+package main
+
+/*
+   #include <pthread.h>
+
+   extern void* callback(void*);
+   typedef void* (*cb)(void*);
+
+   static void testCallback(cb cb) {
+	cb(NULL);
+   }
+*/
+import "C"
+
+import (
+	"bytes"
+	"fmt"
+	traceparser "internal/trace"
+	"runtime/trace"
+	"time"
+	"unsafe"
+)
+
+func init() {
+	register("CgoTraceParser", CgoTraceParser)
+}
+
+//export callback
+func callback(unsafe.Pointer) unsafe.Pointer {
+	time.Sleep(time.Millisecond)
+	return nil
+}
+
+func CgoTraceParser() {
+	buf := new(bytes.Buffer)
+
+	trace.Start(buf)
+	C.testCallback(C.cb(C.callback))
+	trace.Stop()
+
+	_, err := traceparser.Parse(buf, "")
+	if err != nil {
+		fmt.Println("Parse error: ", err)
+	} else {
+		fmt.Println("OK")
+	}
+}
