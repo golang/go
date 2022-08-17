35026f373233de30dbdb6752822d3eabf2220c11
Austin Clements <austin@google.com>
2022-08-17 09:06:14 -0400

    runtime: consolidate stkframe and its methods into stkframe.go
    The stkframe struct and its methods are strewn across different source
    files. Since they actually have a pretty coherent theme at this point,
    migrate it all into a new file, stkframe.go. There are no code changes
    in this CL.
    
    For #54466, albeit rather indirectly.
    
    Change-Id: Ibe53fc4b1106d131005e1c9d491be838a8f14211
    Reviewed-on: https://go-review.googlesource.com/c/go/+/424516
    Reviewed-by: Michael Pratt <mpratt@google.com>
    Reviewed-by: Cherry Mui <cherryyz@google.com>
    TryBot-Result: Gopher Robot <gobot@golang.org>
    Run-TryBot: Austin Clements <austin@google.com>
    Auto-Submit: Austin Clements <austin@google.com>

--- src/runtime/runtime2.go
+++ src/runtime/runtime2.go
@@ -985,55 +985,6 @@ type _panic struct {
 	goexit    bool
 }
 
-// A stkframe holds information about a single physical stack frame.
-type stkframe struct {
-	// fn is the function being run in this frame. If there is
-	// inlining, this is the outermost function.
-	fn funcInfo
-
-	// pc is the program counter within fn.
-	//
-	// The meaning of this is subtle:
-	//
-	// - Typically, this frame performed a regular function call
-	//   and this is the return PC (just after the CALL
-	//   instruction). In this case, pc-1 reflects the CALL
-	//   instruction itself and is the correct source of symbolic
-	//   information.
-	//
-	// - If this frame "called" sigpanic, then pc is the
-	//   instruction that panicked, and pc is the correct address
-	//   to use for symbolic information.
-	//
-	// - If this is the innermost frame, then PC is where
-	//   execution will continue, but it may not be the
-	//   instruction following a CALL. This may be from
-	//   cooperative preemption, in which case this is the
-	//   instruction after the call to morestack. Or this may be
-	//   from a signal or an un-started goroutine, in which case
-	//   PC could be any instruction, including the first
-	//   instruction in a function. Conventionally, we use pc-1
-	//   for symbolic information, unless pc == fn.entry(), in
-	//   which case we use pc.
-	pc uintptr
-
-	// continpc is the PC where execution will continue in fn, or
-	// 0 if execution will not continue in this frame.
-	//
-	// This is usually the same as pc, unless this frame "called"
-	// sigpanic, in which case it's either the address of
-	// deferreturn or 0 if this frame will never execute again.
-	//
-	// This is the PC to use to look up GC liveness for this frame.
-	continpc uintptr
-
-	lr   uintptr // program counter at caller aka link register
-	sp   uintptr // stack pointer at pc
-	fp   uintptr // stack pointer at caller aka frame pointer
-	varp uintptr // top of local variables
-	argp uintptr // pointer to function arguments
-}
-
 // ancestorInfo records details of where a goroutine was started.
 type ancestorInfo struct {
 	pcs  []uintptr // pcs from the stack of this goroutine
