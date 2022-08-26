0d6a7f9d2eb2e5e9b96cd1b144d122f6eb5aac81
Tobias Klauser <tklauser@distanz.ch>
2022-08-26 12:09:34 +0200

    internal/poll, internal/syscall/unix, net, runtime: convert openbsd (except mips64) to direct libc calls
    Call libc wrappers directly rather than calling using syscall(2).
    
    Updates golang/go#36435
    
    Change-Id: I40be410c7472f7d89cbec2ebdc7c841c7726ca4a
    Reviewed-on: https://go-review.googlesource.com/c/go/+/425637
    Reviewed-by: David Chase <drchase@google.com>
    Reviewed-by: Heschi Kreinick <heschi@google.com>
    TryBot-Result: Gopher Robot <gobot@golang.org>
    Run-TryBot: Tobias Klauser <tobias.klauser@gmail.com>
    Reviewed-by: Joel Sing <joel@sing.id.au>

--- src/internal/syscall/unix/getentropy_openbsd.go
+++ src/internal/syscall/unix/getentropy_openbsd.go
@@ -1,25 +1,17 @@
-// Copyright 2016 The Go Authors. All rights reserved.
+// Copyright 2022 The Go Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
+//go:build openbsd && !mips64
+
 package unix
 
-import (
-	"syscall"
-	"unsafe"
-)
-
-// getentropy(2)'s syscall number, from /usr/src/sys/kern/syscalls.master
-const entropyTrap uintptr = 7
+import _ "unsafe" // for linkname
 
 // GetEntropy calls the OpenBSD getentropy system call.
 func GetEntropy(p []byte) error {
-	_, _, errno := syscall.Syscall(entropyTrap,
-		uintptr(unsafe.Pointer(&p[0])),
-		uintptr(len(p)),
-		0)
-	if errno != 0 {
-		return errno
-	}
-	return nil
+	return getentropy(p)
 }
+
+//go:linkname getentropy syscall.getentropy
+func getentropy(p []byte) error
