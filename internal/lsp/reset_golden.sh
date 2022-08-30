ddbeb754297c663e090e1b417390adebe8741cf6
Robert Findley <rfindley@google.com>
2022-08-30 15:16:04 -0400

    internal/lsp: run internal/lsp/reset_golden.sh
    Run reset_golden.sh, so that our golden files are stable. This will be
    useful later, when we migrate internal/lsp to gopls/internal/lsp, and
    golden files must be updated to account for changing offsets.
    
    For golang/go#54509
    
    Change-Id: I2e9a8d3493d64d632b9f0f0e0360d633803f9d92
    Reviewed-on: https://go-review.googlesource.com/c/tools/+/426797
    Run-TryBot: Robert Findley <rfindley@google.com>
    TryBot-Result: Gopher Robot <gobot@golang.org>
    Reviewed-by: Alan Donovan <adonovan@google.com>
    gopls-CI: kokoro <noreply+kokoro@google.com>

--- internal/lsp/reset_golden.sh
+++ internal/lsp/reset_golden.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
+#
+# Copyright 2022 The Go Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style
+# license that can be found in the LICENSE file.
+#
+# Regenerates the *.golden files in the ./internal/lsp/testdata directory.
 
-find ./internal/lsp/ -name *.golden -delete
+set -eu
+
+find ./internal/lsp/testdata -name *.golden ! -name summary*.txt.golden -delete
 go test ./internal/lsp/source  -golden
 go test ./internal/lsp/ -golden
 go test ./internal/lsp/cmd  -golden
