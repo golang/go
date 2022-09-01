4c1ca42aa295d68b6172b7f49e4fb4fd14d86475
Kir Kolyshkin <kolyshkin@gmail.com>
2022-09-01 14:50:26 -0700

    internal/syscall/unix: KernelVersion nitpicks
    A few minor improvements for KernelVersion for the sake of readability.
    
    Change-Id: I06d2df60ecee8ee0ae603952470fb73e7dcd5d74
    Reviewed-on: https://go-review.googlesource.com/c/go/+/427676
    Reviewed-by: Ian Lance Taylor <iant@google.com>
    Reviewed-by: Tobias Klauser <tobias.klauser@gmail.com>
    Reviewed-by: Heschi Kreinick <heschi@google.com>
    Auto-Submit: Tobias Klauser <tobias.klauser@gmail.com>

--- src/internal/syscall/unix/kernel_version_linux.go
+++ src/internal/syscall/unix/kernel_version_linux.go
@@ -13,17 +13,17 @@ import (
 // or parsed.
 //
 // Currently only implemented for Linux.
-func KernelVersion() (major int, minor int) {
+func KernelVersion() (major, minor int) {
 	var uname syscall.Utsname
 	if err := syscall.Uname(&uname); err != nil {
 		return
 	}
 
-	rl := uname.Release
-	var values [2]int
-	vi := 0
-	value := 0
-	for _, c := range rl {
+	var (
+		values    [2]int
+		value, vi int
+	)
+	for _, c := range uname.Release {
 		if '0' <= c && c <= '9' {
 			value = (value * 10) + int(c-'0')
 		} else {
