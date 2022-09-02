dc029f286e1860eca810b1c087ce09e0933ed746
Tobias Klauser <tklauser@distanz.ch>
2022-09-02 14:28:11 +0200

    runtime: add missing copyright header to signal_windows_test.go
    The file was added in CL 181839 which was submitted in 2019.
    
    Also delete the superfluous //go:build windows build constraint which is
    already part of the file name.
    
    Change-Id: Iecdf3d75848a7b8e2a76cd18c9fff4fb90acdac2
    Reviewed-on: https://go-review.googlesource.com/c/go/+/427854
    Run-TryBot: Tobias Klauser <tobias.klauser@gmail.com>
    Reviewed-by: Ian Lance Taylor <iant@google.com>
    Auto-Submit: Tobias Klauser <tobias.klauser@gmail.com>
    TryBot-Result: Gopher Robot <gobot@golang.org>
    Reviewed-by: Michael Knyszek <mknyszek@google.com>

--- src/runtime/signal_windows_test.go
+++ src/runtime/signal_windows_test.go
@@ -1,4 +1,6 @@
-//go:build windows
+// Copyright 2019 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
 
 package runtime_test
 
