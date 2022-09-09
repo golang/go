244127e4b40f3170317ebdb93fa8a92143a54c18
Tobias Klauser <tklauser@distanz.ch>
2022-09-09 10:34:58 +0200

    all: move //go:build lines below copyright notice
    Change-Id: Ib6196f01b1927ea8a84c095f445320f03a514dd1
    Reviewed-on: https://go-review.googlesource.com/c/go/+/429796
    Run-TryBot: Tobias Klauser <tobias.klauser@gmail.com>
    Auto-Submit: Tobias Klauser <tobias.klauser@gmail.com>
    Reviewed-by: Dmitri Shuralyov <dmitshur@google.com>
    TryBot-Result: Gopher Robot <gobot@golang.org>
    Reviewed-by: Ian Lance Taylor <iant@google.com>

--- src/cmd/link/internal/ld/elf_test.go
+++ src/cmd/link/internal/ld/elf_test.go
@@ -1,9 +1,9 @@
-//go:build cgo
-// +build cgo
-
 // Copyright 2019 The Go Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
+
+//go:build cgo
+// +build cgo
 
 package ld
 
