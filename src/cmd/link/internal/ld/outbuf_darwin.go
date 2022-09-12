8df21a78027d54faa8014ce9040bd8007d1b23e0
Tobias Klauser <tklauser@distanz.ch>
2022-09-12 17:18:49 +0200

    cmd/link/internal/ld: drop Go 1.12 compatibility on darwin
    Go 1.17 is the minimum required version for bootstrap as of Go 1.20.
    Also, the package no longer builds with Go 1.12 used for bootstrap.
    Thus, drop the Go 1.12 compatibility and used linkname unconditionally.
    
    For #44505
    
    Change-Id: Ic160aba4a33d580987b4633a3db03a025804d090
    Reviewed-on: https://go-review.googlesource.com/c/go/+/430335
    TryBot-Result: Gopher Robot <gobot@golang.org>
    Reviewed-by: Cherry Mui <cherryyz@google.com>
    Run-TryBot: Tobias Klauser <tobias.klauser@gmail.com>
    Reviewed-by: Ian Lance Taylor <iant@google.com>
    Auto-Submit: Tobias Klauser <tobias.klauser@gmail.com>

--- src/cmd/link/internal/ld/outbuf_darwin.go
+++ src/cmd/link/internal/ld/outbuf_darwin.go
@@ -2,9 +2,6 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-//go:build darwin && go1.12
-// +build darwin,go1.12
-
 package ld
 
 import (
--- src/cmd/link/internal/ld/outbuf_nofallocate.go
+++ src/cmd/link/internal/ld/outbuf_nofallocate.go
@@ -2,8 +2,8 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-//go:build (!darwin && !linux) || (darwin && !go1.12)
-// +build !darwin,!linux darwin,!go1.12
+//go:build !darwin && !linux
+// +build !darwin,!linux
 
 package ld
 
