eb25de6e2a94ebbd4401db4c7695bbb8d0501159
Robert Findley <rfindley@google.com>
2022-09-23 10:55:58 -0400

    go/analysis/passes/loopclosure: only check statements after t.Parallel
    After experimenting with the new t.Run+t.Parallel check in the
    loopclosure analyzer, we discovered that users rely on the fact that
    statements before the call to t.Parallel are executed synchronously, for
    example by declaring test := test inside the function literal, but
    before the call to t.Parallel.
    
    To avoid such false positives, only consider statements occurring after
    the first call to t.Parallel.
    
    Change-Id: I88466ea3bfd318d42d734c320677fbe5e3f6cb00
    Reviewed-on: https://go-review.googlesource.com/c/tools/+/433535
    Run-TryBot: Robert Findley <rfindley@google.com>
    TryBot-Result: Gopher Robot <gobot@golang.org>
    Reviewed-by: Alan Donovan <adonovan@google.com>
    gopls-CI: kokoro <noreply+kokoro@google.com>

--- go/analysis/passes/loopclosure/testdata/src/subtests/subtest.go
+++ go/analysis/passes/loopclosure/testdata/src/subtests/subtest.go
@@ -18,7 +18,7 @@ type T struct{}
 // Run should not match testing.T.Run. Note that the second argument is
 // intentionally a *testing.T, not a *T, so that we can check both
 // testing.T.Parallel inside a T.Run, and a T.Parallel inside a testing.T.Run.
-func (t *T) Run(string, func(*testing.T)) { // The second argument here is testing.T
+func (t *T) Run(string, func(*testing.T)) {
 }
 
 func (t *T) Parallel() {}
@@ -38,11 +38,33 @@ func _(t *testing.T) {
 			println(test)
 		})
 
-		// Check that the location of t.Parallel does not matter.
+		// Check that the location of t.Parallel matters.
 		t.Run("", func(t *testing.T) {
+			println(i)
+			println(test)
+			t.Parallel()
 			println(i)    // want "loop variable i captured by func literal"
 			println(test) // want "loop variable test captured by func literal"
+		})
+
+		// Check that shadowing the loop variables within the test literal is OK if
+		// it occurs before t.Parallel().
+		t.Run("", func(t *testing.T) {
+			i := i
+			test := test
 			t.Parallel()
+			println(i)
+			println(test)
+		})
+
+		// Check that shadowing the loop variables within the test literal is Not
+		// OK if it occurs after t.Parallel().
+		t.Run("", func(t *testing.T) {
+			t.Parallel()
+			i := i        // want "loop variable i captured by func literal"
+			test := test  // want "loop variable test captured by func literal"
+			println(i)    // OK
+			println(test) // OK
 		})
 
 		// Check uses in nested blocks.
