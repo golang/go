c5cd9430779e9d1fb77da7b0f1fe24f05f86370c
Robert Findley <rfindley@google.com>
2022-09-26 12:16:58 -0400

    gopls: fix the reset_golden.sh script and regenerate golden files
    Update the reset_golden.sh script to run the ./test and ./internal/lsp
    directories, following the deprecation of ./internal/lsp/cmd and
    ./internal/lsp/source tests. Also, fix it to generate golden data for
    tests that only run at 1.17 and earlier.
    
    Use the newly fixed script to sync the golden data.
    
    For golang/go#54845
    
    Change-Id: I2b42dac91cf1c7e2e8e25fd2aa8ab23c91e05c6c
    Reviewed-on: https://go-review.googlesource.com/c/tools/+/434635
    Reviewed-by: Alan Donovan <adonovan@google.com>
    gopls-CI: kokoro <noreply+kokoro@google.com>
    Run-TryBot: Robert Findley <rfindley@google.com>
    TryBot-Result: Gopher Robot <gobot@golang.org>

--- gopls/internal/lsp/reset_golden.sh
+++ gopls/internal/lsp/reset_golden.sh
@@ -4,11 +4,27 @@
 # Use of this source code is governed by a BSD-style
 # license that can be found in the LICENSE file.
 #
-# Regenerates the *.golden files in the ./internal/lsp/testdata directory.
+# Updates the *.golden files ... to match the tests' current behavior.
 
 set -eu
 
+GO117BIN="go1.17.9"
+
+command -v $GO117BIN >/dev/null 2>&1 || {
+  go install golang.org/dl/$GO117BIN@latest
+  $GO117BIN download
+}
+
 find ./internal/lsp/testdata -name *.golden ! -name summary*.txt.golden -delete
-go test ./internal/lsp/source  -golden
-go test ./internal/lsp/ -golden
-go test ./internal/lsp/cmd  -golden
+# Here we intentionally do not run the ./internal/lsp/source tests with
+# -golden. Eventually these tests will be deleted, and in the meantime they are
+# redundant with the ./internal/lsp tests.
+#
+# Note: go1.17.9 tests must be run *before* go tests, as by convention the
+# golden output should match the output of gopls built with the most recent
+# version of Go. If output differs at 1.17, tests must be tolerant of the 1.17
+# output.
+$GO117BIN test ./internal/lsp -golden
+go test ./internal/lsp -golden
+$GO117BIN test ./test  -golden
+go test ./test  -golden
