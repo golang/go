e7a3d87ddae6aa720e104213bd2cf98e3e743c9a
cuiweixie <cuiweixie@gmail.com>
2022-09-29 20:00:00 +0800

    cmd/compile/internal/ir: delete unused code
    Change-Id: I4f7581d1f4cd8a305acc02454e032c0788d39283
    Reviewed-on: https://go-review.googlesource.com/c/go/+/436646
    Reviewed-by: Robert Griesemer <gri@google.com>
    Auto-Submit: Ian Lance Taylor <iant@google.com>
    Run-TryBot: Robert Griesemer <gri@google.com>
    Reviewed-by: Ian Lance Taylor <iant@google.com>
    TryBot-Result: Gopher Robot <gobot@golang.org>
    Run-TryBot: Ian Lance Taylor <iant@google.com>

--- src/cmd/compile/internal/ir/bitset.go
+++ src/cmd/compile/internal/ir/bitset.go
@@ -35,37 +35,3 @@ func (f *bitset16) set(mask uint16, b bool) {
 		*(*uint16)(f) &^= mask
 	}
 }
-
-type bitset32 uint32
-
-func (f *bitset32) set(mask uint32, b bool) {
-	if b {
-		*(*uint32)(f) |= mask
-	} else {
-		*(*uint32)(f) &^= mask
-	}
-}
-
-func (f bitset32) get2(shift uint8) uint8 {
-	return uint8(f>>shift) & 3
-}
-
-// set2 sets two bits in f using the bottom two bits of b.
-func (f *bitset32) set2(shift uint8, b uint8) {
-	// Clear old bits.
-	*(*uint32)(f) &^= 3 << shift
-	// Set new bits.
-	*(*uint32)(f) |= uint32(b&3) << shift
-}
-
-func (f bitset32) get3(shift uint8) uint8 {
-	return uint8(f>>shift) & 7
-}
-
-// set3 sets three bits in f using the bottom three bits of b.
-func (f *bitset32) set3(shift uint8, b uint8) {
-	// Clear old bits.
-	*(*uint32)(f) &^= 7 << shift
-	// Set new bits.
-	*(*uint32)(f) |= uint32(b&7) << shift
-}
