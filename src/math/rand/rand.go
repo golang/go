c30bce7f51398ef059e0232bc6c13af6f03552c1
hopehook <hopehook.com@gmail.com>
2022-09-30 09:17:11 +0800

    math/rand: deprecate Read
    For #20661.
    
    Change-Id: I1e638cb619e643eadc210d71f92bd1af7bafc912
    Reviewed-on: https://go-review.googlesource.com/c/go/+/436955
    Reviewed-by: Ian Lance Taylor <iant@google.com>
    TryBot-Result: Gopher Robot <gobot@golang.org>
    Run-TryBot: hopehook <hopehook@golangcn.org>
    Auto-Submit: Ian Lance Taylor <iant@google.com>
    Reviewed-by: Robert Griesemer <gri@google.com>
    Run-TryBot: Ian Lance Taylor <iant@google.com>

--- src/math/rand/rand.go
+++ src/math/rand/rand.go
@@ -377,6 +377,8 @@ func Shuffle(n int, swap func(i, j int)) { globalRand.Shuffle(n, swap) }
 // Read generates len(p) random bytes from the default Source and
 // writes them into p. It always returns len(p) and a nil error.
 // Read, unlike the Rand.Read method, is safe for concurrent use.
+//
+// Deprecated: For almost all use cases, crypto/rand.Read is more appropriate.
 func Read(p []byte) (n int, err error) { return globalRand.Read(p) }
 
 // NormFloat64 returns a normally distributed float64 in the range
