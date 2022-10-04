dfd8aa461be982533e01867ab002fb83ba3cc943
Andrew Pogrebnoy <absourd.noise@gmail.com>
2022-10-04 19:44:20 +0300

    cmd/compile: add "runtime/internal/syscall" back to runtimePackages
    It was accidentally removed in CL 355451.
    
    Change-Id: I818ea01b83e437c25829bf7e88c7180963e696f8
    Reviewed-on: https://go-review.googlesource.com/c/go/+/438615
    Auto-Submit: Michael Pratt <mpratt@google.com>
    TryBot-Result: Gopher Robot <gobot@golang.org>
    Reviewed-by: Michael Pratt <mpratt@google.com>
    Run-TryBot: Michael Pratt <mpratt@google.com>
    Reviewed-by: Than McIntosh <thanm@google.com>

--- src/cmd/go/internal/work/gc.go
+++ src/cmd/go/internal/work/gc.go
@@ -30,16 +30,17 @@ import (
 const trimPathGoRootFinal string = "$GOROOT"
 
 var runtimePackages = map[string]struct{}{
-	"internal/abi":            struct{}{},
-	"internal/bytealg":        struct{}{},
-	"internal/coverage/rtcov": struct{}{},
-	"internal/cpu":            struct{}{},
-	"internal/goarch":         struct{}{},
-	"internal/goos":           struct{}{},
-	"runtime":                 struct{}{},
-	"runtime/internal/atomic": struct{}{},
-	"runtime/internal/math":   struct{}{},
-	"runtime/internal/sys":    struct{}{},
+	"internal/abi":             struct{}{},
+	"internal/bytealg":         struct{}{},
+	"internal/coverage/rtcov":  struct{}{},
+	"internal/cpu":             struct{}{},
+	"internal/goarch":          struct{}{},
+	"internal/goos":            struct{}{},
+	"runtime":                  struct{}{},
+	"runtime/internal/atomic":  struct{}{},
+	"runtime/internal/math":    struct{}{},
+	"runtime/internal/sys":     struct{}{},
+	"runtime/internal/syscall": struct{}{},
 }
 
 // The Go toolchain.
