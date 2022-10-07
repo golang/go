48a8793e876381d0349f80e735cf6296ea4b168b
Wayne Zuo <wdvxdr@golangcn.org>
2022-10-07 12:19:32 +0800

    cmd/compile: move {SHL,SHR,SAR}X rules to late lower pass
    This can reduce a bunch of rules.
    
    Change-Id: Id7d644307c295a0ed16eb837b3755d1117a4fbf7
    Reviewed-on: https://go-review.googlesource.com/c/go/+/440036
    Reviewed-by: Keith Randall <khr@golang.org>
    Reviewed-by: Keith Randall <khr@google.com>
    Run-TryBot: Wayne Zuo <wdvxdr@golangcn.org>
    Reviewed-by: David Chase <drchase@google.com>
    TryBot-Result: Gopher Robot <gobot@golang.org>

--- src/cmd/compile/internal/ssa/rewriteAMD64latelower.go
+++ src/cmd/compile/internal/ssa/rewriteAMD64latelower.go
@@ -3,6 +3,8 @@
 
 package ssa
 
+import "internal/buildcfg"
+
 func rewriteValueAMD64latelower(v *Value) bool {
 	switch v.Op {
 	case OpAMD64LEAL1:
@@ -29,6 +31,18 @@ func rewriteValueAMD64latelower(v *Value) bool {
 		return rewriteValueAMD64latelower_OpAMD64LEAW4(v)
 	case OpAMD64LEAW8:
 		return rewriteValueAMD64latelower_OpAMD64LEAW8(v)
+	case OpAMD64SARL:
+		return rewriteValueAMD64latelower_OpAMD64SARL(v)
+	case OpAMD64SARQ:
+		return rewriteValueAMD64latelower_OpAMD64SARQ(v)
+	case OpAMD64SHLL:
+		return rewriteValueAMD64latelower_OpAMD64SHLL(v)
+	case OpAMD64SHLQ:
+		return rewriteValueAMD64latelower_OpAMD64SHLQ(v)
+	case OpAMD64SHRL:
+		return rewriteValueAMD64latelower_OpAMD64SHRL(v)
+	case OpAMD64SHRQ:
+		return rewriteValueAMD64latelower_OpAMD64SHRQ(v)
 	}
 	return false
 }
@@ -397,6 +411,114 @@ func rewriteValueAMD64latelower_OpAMD64LEAW8(v *Value) bool {
 		v0 := b.NewValue0(v.Pos, OpAMD64LEAW8, x.Type)
 		v0.AddArg2(x, y)
 		v.AddArg(v0)
+		return true
+	}
+	return false
+}
+func rewriteValueAMD64latelower_OpAMD64SARL(v *Value) bool {
+	v_1 := v.Args[1]
+	v_0 := v.Args[0]
+	// match: (SARL x y)
+	// cond: buildcfg.GOAMD64 >= 3
+	// result: (SARXL x y)
+	for {
+		x := v_0
+		y := v_1
+		if !(buildcfg.GOAMD64 >= 3) {
+			break
+		}
+		v.reset(OpAMD64SARXL)
+		v.AddArg2(x, y)
+		return true
+	}
+	return false
+}
+func rewriteValueAMD64latelower_OpAMD64SARQ(v *Value) bool {
+	v_1 := v.Args[1]
+	v_0 := v.Args[0]
+	// match: (SARQ x y)
+	// cond: buildcfg.GOAMD64 >= 3
+	// result: (SARXQ x y)
+	for {
+		x := v_0
+		y := v_1
+		if !(buildcfg.GOAMD64 >= 3) {
+			break
+		}
+		v.reset(OpAMD64SARXQ)
+		v.AddArg2(x, y)
+		return true
+	}
+	return false
+}
+func rewriteValueAMD64latelower_OpAMD64SHLL(v *Value) bool {
+	v_1 := v.Args[1]
+	v_0 := v.Args[0]
+	// match: (SHLL x y)
+	// cond: buildcfg.GOAMD64 >= 3
+	// result: (SHLXL x y)
+	for {
+		x := v_0
+		y := v_1
+		if !(buildcfg.GOAMD64 >= 3) {
+			break
+		}
+		v.reset(OpAMD64SHLXL)
+		v.AddArg2(x, y)
+		return true
+	}
+	return false
+}
+func rewriteValueAMD64latelower_OpAMD64SHLQ(v *Value) bool {
+	v_1 := v.Args[1]
+	v_0 := v.Args[0]
+	// match: (SHLQ x y)
+	// cond: buildcfg.GOAMD64 >= 3
+	// result: (SHLXQ x y)
+	for {
+		x := v_0
+		y := v_1
+		if !(buildcfg.GOAMD64 >= 3) {
+			break
+		}
+		v.reset(OpAMD64SHLXQ)
+		v.AddArg2(x, y)
+		return true
+	}
+	return false
+}
+func rewriteValueAMD64latelower_OpAMD64SHRL(v *Value) bool {
+	v_1 := v.Args[1]
+	v_0 := v.Args[0]
+	// match: (SHRL x y)
+	// cond: buildcfg.GOAMD64 >= 3
+	// result: (SHRXL x y)
+	for {
+		x := v_0
+		y := v_1
+		if !(buildcfg.GOAMD64 >= 3) {
+			break
+		}
+		v.reset(OpAMD64SHRXL)
+		v.AddArg2(x, y)
+		return true
+	}
+	return false
+}
+func rewriteValueAMD64latelower_OpAMD64SHRQ(v *Value) bool {
+	v_1 := v.Args[1]
+	v_0 := v.Args[0]
+	// match: (SHRQ x y)
+	// cond: buildcfg.GOAMD64 >= 3
+	// result: (SHRXQ x y)
+	for {
+		x := v_0
+		y := v_1
+		if !(buildcfg.GOAMD64 >= 3) {
+			break
+		}
+		v.reset(OpAMD64SHRXQ)
+		v.AddArg2(x, y)
 		return true
 	}
 	return false
