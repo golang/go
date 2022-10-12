fac2eb8dc7df9c12717733eee2553f6ff4dded9d
Bryan C. Mills <bcmills@google.com>
2022-10-12 14:57:47 -0400

    os: split wait6 syscall wrapper into per-platform files
    There are getting to be enough special cases in this wrapper that
    the increase in clarity from having a single file is starting to be
    outweighed by the complexity from chained conditionals.
    
    Updates #50138.
    Updates #13987.
    
    Change-Id: If4f1be19c0344e249aa6092507c28363ca6c8438
    Reviewed-on: https://go-review.googlesource.com/c/go/+/442575
    Run-TryBot: Bryan Mills <bcmills@google.com>
    TryBot-Result: Gopher Robot <gobot@golang.org>
    Auto-Submit: Bryan Mills <bcmills@google.com>
    Reviewed-by: Ian Lance Taylor <iant@google.com>

--- src/os/wait_wait6.go
+++ src/os/wait_wait6.go
@@ -11,29 +11,13 @@ import (
 	"syscall"
 )
 
-const (
-	_P_PID        = 0 // everywhere except for NetBSD?
-	_P_PID_NETBSD = 1 // on NetBSD, 0 is P_ALL
-)
-
 // blockUntilWaitable attempts to block until a call to p.Wait will
 // succeed immediately, and reports whether it has done so.
 // It does not actually call p.Wait.
 func (p *Process) blockUntilWaitable() (bool, error) {
 	var errno syscall.Errno
 	for {
-		// The arguments on 32-bit FreeBSD look like the following:
-		// - freebsd32_wait6_args{ idtype, id1, id2, status, options, wrusage, info } or
-		// - freebsd32_wait6_args{ idtype, pad, id1, id2, status, options, wrusage, info } when PAD64_REQUIRED=1 on ARM, MIPS or PowerPC
-		if runtime.GOOS == "freebsd" && runtime.GOARCH == "386" {
-			_, _, errno = syscall.Syscall9(syscall.SYS_WAIT6, _P_PID, uintptr(p.Pid), 0, 0, syscall.WEXITED|syscall.WNOWAIT, 0, 0, 0, 0)
-		} else if runtime.GOOS == "freebsd" && runtime.GOARCH == "arm" {
-			_, _, errno = syscall.Syscall9(syscall.SYS_WAIT6, _P_PID, 0, uintptr(p.Pid), 0, 0, syscall.WEXITED|syscall.WNOWAIT, 0, 0, 0)
-		} else if runtime.GOOS == "netbsd" {
-			_, _, errno = syscall.Syscall6(syscall.SYS_WAIT6, _P_PID_NETBSD, uintptr(p.Pid), 0, syscall.WEXITED|syscall.WNOWAIT, 0, 0)
-		} else {
-			_, _, errno = syscall.Syscall6(syscall.SYS_WAIT6, _P_PID, uintptr(p.Pid), 0, syscall.WEXITED|syscall.WNOWAIT, 0, 0)
-		}
+		_, errno = wait6(_P_PID, p.Pid, syscall.WEXITED|syscall.WNOWAIT)
 		if errno != syscall.EINTR {
 			break
 		}
