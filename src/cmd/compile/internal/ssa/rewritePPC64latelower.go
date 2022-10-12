8e459924c0dec96d429b608700ce818516b2f4a1
Paul E. Murphy <murp@ibm.com>
2022-10-12 11:04:50 -0500

    cmd/compile: enable lateLower pass on PPC64
    This allows new rules to be added which would otherwise
    greatly overcomplicate the generic rules, like CC opcode
    conversion or zero register simplification.
    
    Change-Id: I1533f0fa07815aff99ed8ab890077bd22a3bfbf5
    Reviewed-on: https://go-review.googlesource.com/c/go/+/442595
    Reviewed-by: Lynn Boger <laboger@linux.vnet.ibm.com>
    Run-TryBot: Paul Murphy <murp@ibm.com>
    Reviewed-by: David Chase <drchase@google.com>
    TryBot-Result: Gopher Robot <gobot@golang.org>
    Reviewed-by: Bryan Mills <bcmills@google.com>

---
+++ src/cmd/compile/internal/ssa/rewritePPC64latelower.go
@@ -0,0 +1,11 @@
+// Code generated from gen/PPC64latelower.rules; DO NOT EDIT.
+// generated with: cd gen; go run *.go
+
+package ssa
+
+func rewriteValuePPC64latelower(v *Value) bool {
+	return false
+}
+func rewriteBlockPPC64latelower(b *Block) bool {
+	return false
+}
