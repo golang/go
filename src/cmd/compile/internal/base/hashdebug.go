ecb8a8d526119bcde6a3d8755d03f3f2d4f16429
David Chase <drchase@google.com>
2022-10-14 12:04:52 -0400

    cmd/compile: renovate GOSSAHASH
    Randomized feature enable/disable might be something we use to
    help users debug any problems with changed loop variable capture,
    and there's another CL that would like to use it to help in
    locating places where "fused" multiply add instructions change
    program behavior.
    
    This CL:
    - adds the ability to include an integer parameter (e.g. line number)
    - replumbed the environment variable into a flag to simplify go build cache management
    - but added an environment variable to allow flag setting through the environment
    - which adds the possibility of switching on a different variable
      (if there's one built-in for variable capture, it shouldn't be GOSSAHASH)
    - cleaned up the checking code
    - adds tests for all the intended behavior
    - removes the case for GSHS_LOGFILE; TBD whether we'll need to put that back
      or if there is another way.
    
    Change-Id: I8503e1bb3dbc4a743aea696e04411ea7ab884787
    Reviewed-on: https://go-review.googlesource.com/c/go/+/443063
    Reviewed-by: Keith Randall <khr@google.com>
    Reviewed-by: Keith Randall <khr@golang.org>
    TryBot-Result: Gopher Robot <gobot@golang.org>
    Reviewed-by: Russ Cox <rsc@golang.org>
    Run-TryBot: David Chase <drchase@google.com>

--- src/cmd/compile/internal/base/hashdebug.go
+++ src/cmd/compile/internal/base/hashdebug.go
@@ -9,51 +9,64 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"strconv"
 	"strings"
 	"sync"
 )
-
-const GOSSAHASH = "GOSSAHASH"
 
 type writeSyncer interface {
 	io.Writer
 	Sync() error
 }
 
+type hashAndMask struct {
+	// a hash h matches if (h^hash)&mask == 0
+	hash uint64
+	mask uint64
+	name string // base name, or base name + "0", "1", etc.
+}
+
 type HashDebug struct {
-	mu sync.Mutex
+	mu   sync.Mutex
+	name string // base name of the flag/variable.
 	// what file (if any) receives the yes/no logging?
 	// default is os.Stdout
 	logfile writeSyncer
+	matches []hashAndMask // A hash matches if one of these matches.
+	yes, no bool
 }
 
-var hd HashDebug
+// The default compiler-debugging HashDebug, for "-d=gossahash=..."
+var hashDebug *HashDebug
 
-// DebugHashMatch reports whether environment variable GOSSAHASH
+// DebugHashMatch reports whether debug variable Gossahash
 //
-//  1. is empty (this is a special more-quickly implemented case of 3)
+//  1. is empty (returns true; this is a special more-quickly implemented case of 4 below)
 //
-//  2. is "y" or "Y"
+//  2. is "y" or "Y" (returns true)
 //
-//  3. is a suffix of the sha1 hash of name
+//  3. is "n" or "N" (returns false)
 //
-//  4. OR
-//     if evname(i) is a suffix of the sha1 hash of name
-//     where evname(i)=fmt.Sprintf("GOSSAHASH%d", i),
-//     for 0<=i<n such that for all i evname(i) != "" and evname(n) == ""
+//  4. is a suffix of the sha1 hash of pkgAndName (returns true)
 //
-//     That is, as long as they're not empty, try GOSSAHASH, GOSSAHASH0, GOSSAHASH1, etc,
-//     but quit trying at the first empty environment variable substitution.
+//  5. OR
+//     if the value is in the regular language "[01]+(;[01]+)+"
+//     test the [01]+ substrings after in order returning true
+//     for the first one that suffix-matches. The substrings AFTER
+//     the first semicolon are numbered 0,1, etc and are named
+//     fmt.Sprintf("%s%d", varname, number)
+//     Clause 5 is not really intended for human use and only
+//     matters for failures that require multiple triggers.
 //
 // Otherwise it returns false.
-// Clause 4 is not really intended for human use.
 //
-// Unless GOSSAHASH is empty, when DebugHashMatch returns true the message
+// Unless Flags.Gossahash is empty, when DebugHashMatch returns true the message
 //
-//	"%s triggered %s\n", evname, name
+//	"%s triggered %s\n", varname, pkgAndName
 //
 // is printed on the file named in environment variable GSHS_LOGFILE,
-// or standard out if that is empty.
+// or standard out if that is empty.  "Varname" is either the name of
+// the variable or the name of the substring, depending on which matched.
 //
 // Typical use:
 //
@@ -62,15 +75,16 @@ var hd HashDebug
 //  2. it is broken in some mystifying way, for example, make.bash builds a broken
 //     compiler that almost works, but crashes compiling a test in run.bash.
 //
-//  3. add this guard to the code, which by default leaves it broken, but
-//     does not run the broken new code if GOSSAHASH is non-empty and non-matching:
+//  3. add this guard to the code, which by default leaves it broken, but does not
+//     run the broken new code if Flags.Gossahash is non-empty and non-matching:
 //
 //     if !base.DebugHashMatch(ir.PkgFuncName(fn)) {
 //     return nil // early exit, do nothing
 //     }
 //
-//  4. rebuild w/o the bad code, GOSSAHASH=n ./all.bash to verify that you
-//     put theguard in the right place with the right sense of the test.
+//  4. rebuild w/o the bad code,
+//     GOCOMPILEDEBUG=gossahash=n ./all.bash
+//     to verify that you put the guard in the right place with the right sense of the test.
 //
 //  5. use github.com/dr2chase/gossahash to search for the error:
 //
@@ -83,50 +97,129 @@ var hd HashDebug
 //  6. gossahash should return a single function whose miscompilation
 //     causes the problem, and you can focus on that.
 func DebugHashMatch(pkgAndName string) bool {
-	return hd.DebugHashMatch(pkgAndName)
+	return hashDebug.DebugHashMatch(pkgAndName)
 }
 
-func (d *HashDebug) DebugHashMatch(pkgAndName string) bool {
-	evname := GOSSAHASH
-	evhash := os.Getenv(evname)
-	hstr := ""
+// HasDebugHash returns true if Flags.Gossahash is non-empty, which
+// results in hashDebug being not-nil.  I.e., if !HasDebugHash(),
+// there is no need to create the string for hashing and testing.
+func HasDebugHash() bool {
+	return hashDebug != nil
+}
 
-	switch evhash {
-	case "":
-		return true // default behavior with no EV is "on"
-	case "n", "N":
+func toHashAndMask(s, varname string) hashAndMask {
+	l := len(s)
+	if l > 64 {
+		s = s[l-64:]
+		l = 64
+	}
+	m := ^(^uint64(0) << l)
+	h, err := strconv.ParseUint(s, 2, 64)
+	if err != nil {
+		Fatalf("Could not parse %s (=%s) as a binary number", varname, s)
+	}
+
+	return hashAndMask{name: varname, hash: h, mask: m}
+}
+
+// NewHashDebug returns a new hash-debug tester for the
+// environment variable ev.  If ev is not set, it returns
+// nil, allowing a lightweight check for normal-case behavior.
+func NewHashDebug(ev, s string, file writeSyncer) *HashDebug {
+	if s == "" {
+		return nil
+	}
+
+	hd := &HashDebug{name: ev, logfile: file}
+	switch s[0] {
+	case 'y', 'Y':
+		hd.yes = true
+		return hd
+	case 'n', 'N':
+		hd.no = true
+		return hd
+	}
+	ss := strings.Split(s, ";")
+	hd.matches = append(hd.matches, toHashAndMask(ss[0], ev))
+	// hash searches may use additional EVs with 0, 1, 2, ... suffixes.
+	for i := 1; i < len(ss); i++ {
+		evi := fmt.Sprintf("%s%d", ev, i-1) // convention is extras begin indexing at zero
+		hd.matches = append(hd.matches, toHashAndMask(ss[i], evi))
+	}
+	return hd
+
+}
+
+func hashOf(pkgAndName string, param uint64) uint64 {
+	hbytes := notsha256.Sum256([]byte(pkgAndName))
+	hash := uint64(hbytes[7])<<56 + uint64(hbytes[6])<<48 +
+		uint64(hbytes[5])<<40 + uint64(hbytes[4])<<32 +
+		uint64(hbytes[3])<<24 + uint64(hbytes[2])<<16 +
+		uint64(hbytes[1])<<8 + uint64(hbytes[0])
+
+	if param != 0 {
+		// Because param is probably a line number, probably near zero,
+		// hash it up a little bit, but even so only the lower-order bits
+		// likely matter because search focuses on those.
+		p0 := param + uint64(hbytes[9]) + uint64(hbytes[10])<<8 +
+			uint64(hbytes[11])<<16 + uint64(hbytes[12])<<24
+
+		p1 := param + uint64(hbytes[13]) + uint64(hbytes[14])<<8 +
+			uint64(hbytes[15])<<16 + uint64(hbytes[16])<<24
+
+		param += p0 * p1
+		param ^= param>>17 ^ param<<47
+	}
+
+	return hash ^ param
+}
+
+// DebugHashMatch returns true if either the variable used to create d is
+// unset, or if its value is y, or if it is a suffix of the base-two
+// representation of the hash of pkgAndName.  If the variable is not nil,
+// then a true result is accompanied by stylized output to d.logfile, which
+// is used for automated bug search.
+func (d *HashDebug) DebugHashMatch(pkgAndName string) bool {
+	return d.DebugHashMatchParam(pkgAndName, 0)
+}
+
+// DebugHashMatchParam returns true if either the variable used to create d is
+// unset, or if its value is y, or if it is a suffix of the base-two
+// representation of the hash of pkgAndName and param. If the variable is not
+// nil, then a true result is accompanied by stylized output to d.logfile,
+// which is used for automated bug search.
+func (d *HashDebug) DebugHashMatchParam(pkgAndName string, param uint64) bool {
+	if d == nil {
+		return true
+	}
+	if d.no {
 		return false
 	}
-
-	// Check the hash of the name against a partial input hash.
-	// We use this feature to do a binary search to
-	// find a function that is incorrectly compiled.
-	for _, b := range notsha256.Sum256([]byte(pkgAndName)) {
-		hstr += fmt.Sprintf("%08b", b)
-	}
-
-	if evhash == "y" || evhash == "Y" || strings.HasSuffix(hstr, evhash) {
-		d.logDebugHashMatch(evname, pkgAndName, hstr)
+	if d.yes {
+		d.logDebugHashMatch(d.name, pkgAndName, "y", param)
 		return true
 	}
 
-	// Iteratively try additional hashes to allow tests for multi-point
-	// failure.
-	for i := 0; true; i++ {
-		ev := fmt.Sprintf("%s%d", evname, i)
-		evv := os.Getenv(ev)
-		if evv == "" {
-			break
-		}
-		if strings.HasSuffix(hstr, evv) {
-			d.logDebugHashMatch(ev, pkgAndName, hstr)
+	hash := hashOf(pkgAndName, param)
+
+	for _, m := range d.matches {
+		if (m.hash^hash)&m.mask == 0 {
+			hstr := ""
+			if hash == 0 {
+				hstr = "0"
+			} else {
+				for ; hash != 0; hash = hash >> 1 {
+					hstr = string('0'+byte(hash&1)) + hstr
+				}
+			}
+			d.logDebugHashMatch(m.name, pkgAndName, hstr, param)
 			return true
 		}
 	}
 	return false
 }
 
-func (d *HashDebug) logDebugHashMatch(evname, name, hstr string) {
+func (d *HashDebug) logDebugHashMatch(varname, name, hstr string, param uint64) {
 	d.mu.Lock()
 	defer d.mu.Unlock()
 	file := d.logfile
@@ -148,6 +241,10 @@ func (d *HashDebug) logDebugHashMatch(evname, name, hstr string) {
 		hstr = hstr[len(hstr)-24:]
 	}
 	// External tools depend on this string
-	fmt.Fprintf(file, "%s triggered %s %s\n", evname, name, hstr)
+	if param == 0 {
+		fmt.Fprintf(file, "%s triggered %s %s\n", varname, name, hstr)
+	} else {
+		fmt.Fprintf(file, "%s triggered %s:%d %s\n", varname, name, param, hstr)
+	}
 	file.Sync()
 }
