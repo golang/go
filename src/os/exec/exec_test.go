2a7adf4ccde9734becf41acf8274d1a190f2ec64
Damien Neil <dneil@google.com>
2022-10-17 17:38:29 -0700

    [release-branch.go1.19] syscall, os/exec: reject environment variables containing NULs
    Check for and reject environment variables containing NULs.
    
    The conventions for passing environment variables to subprocesses
    cause most or all systems to interpret a NUL as a separator. The
    syscall package rejects environment variables containing a NUL
    on most systems, but erroneously did not do so on Windows. This
    causes an environment variable such as "FOO=a\x00BAR=b" to be
    interpreted as "FOO=a", "BAR=b".
    
    Check for and reject NULs in environment variables passed to
    syscall.StartProcess on Windows.
    
    Add a redundant check to os/exec as extra insurance.
    
    Updates #56284
    Fixes #56328
    Fixes CVE-2022-41716
    
    Change-Id: I2950e2b0cb14ebd26e5629be1521858f66a7d4ae
    Reviewed-on: https://team-review.git.corp.google.com/c/golang/go-private/+/1609434
    Run-TryBot: Damien Neil <dneil@google.com>
    Reviewed-by: Tatiana Bradley <tatianabradley@google.com>
    Reviewed-by: Roland Shoemaker <bracewell@google.com>
    TryBot-Result: Security TryBots <security-trybots@go-security-trybots.iam.gserviceaccount.com>
    (cherry picked from commit 845accdebb2772c5344ed0c96df9910f3b02d741)
    Reviewed-on: https://team-review.git.corp.google.com/c/golang/go-private/+/1617553
    Run-TryBot: Tatiana Bradley <tatianabradley@google.com>
    Reviewed-by: Damien Neil <dneil@google.com>
    Reviewed-on: https://go-review.googlesource.com/c/go/+/446879
    Reviewed-by: Tatiana Bradley <tatiana@golang.org>
    Reviewed-by: Heschi Kreinick <heschi@google.com>
    Run-TryBot: Matthew Dempsky <mdempsky@google.com>
    TryBot-Result: Gopher Robot <gobot@golang.org>

--- src/os/exec/exec_test.go
+++ src/os/exec/exec_test.go
@@ -1053,6 +1053,15 @@ func TestDedupEnvEcho(t *testing.T) {
 	}
 }
 
+func TestEnvNULCharacter(t *testing.T) {
+	cmd := helperCommand(t, "echoenv", "FOO", "BAR")
+	cmd.Env = append(cmd.Environ(), "FOO=foo\x00BAR=bar")
+	out, err := cmd.CombinedOutput()
+	if err == nil {
+		t.Errorf("output = %q; want error", string(out))
+	}
+}
+
 func TestString(t *testing.T) {
 	echoPath, err := exec.LookPath("echo")
 	if err != nil {
