de675d547938a7540a5ea69462d4d1ebb65fefd2
pjw <pjw@google.com>
2022-10-23 15:54:03 -0400

    tools/gopls: argument in function bodies marked as parameter by semantic tokens
    In func f(x int) int {return x;} the second x used to be marked as a
    variable, but now is marked as a parameter, visually tying it to its
    definition.
    
    Fixes golang/go#56257
    
    Change-Id: I8aa506b1ddff5ed9a3d2716d48c64521bdea0fd5
    Reviewed-on: https://go-review.googlesource.com/c/tools/+/445095
    Reviewed-by: Robert Findley <rfindley@google.com>
    TryBot-Result: Gopher Robot <gobot@golang.org>
    Run-TryBot: Peter Weinberger <pjw@google.com>
    gopls-CI: kokoro <noreply+kokoro@google.com>

--- gopls/internal/lsp/semantic.go
+++ gopls/internal/lsp/semantic.go
@@ -526,9 +526,14 @@ func (e *encoded) ident(x *ast.Ident) {
 			tok(x.Pos(), len(x.Name), tokFunction, nil)
 		} else if _, ok := y.Type().(*typeparams.TypeParam); ok {
 			tok(x.Pos(), len(x.Name), tokTypeParam, nil)
+		} else if e.isParam(use.Pos()) {
+			// variable, unless use.pos is the pos of a Field in an ancestor FuncDecl
+			// or FuncLit and then it's a parameter
+			tok(x.Pos(), len(x.Name), tokParameter, nil)
 		} else {
 			tok(x.Pos(), len(x.Name), tokVariable, nil)
 		}
+
 	default:
 		// can't happen
 		if use == nil {
@@ -541,6 +546,30 @@ func (e *encoded) ident(x *ast.Ident) {
 			e.unexpected(fmt.Sprintf("%s %T", x.String(), use))
 		}
 	}
+}
+
+func (e *encoded) isParam(pos token.Pos) bool {
+	for i := len(e.stack) - 1; i >= 0; i-- {
+		switch n := e.stack[i].(type) {
+		case *ast.FuncDecl:
+			for _, f := range n.Type.Params.List {
+				for _, id := range f.Names {
+					if id.Pos() == pos {
+						return true
+					}
+				}
+			}
+		case *ast.FuncLit:
+			for _, f := range n.Type.Params.List {
+				for _, id := range f.Names {
+					if id.Pos() == pos {
+						return true
+					}
+				}
+			}
+		}
+	}
+	return false
 }
 
 func isSignature(use types.Object) bool {
@@ -640,7 +669,7 @@ func (e *encoded) unkIdent(x *ast.Ident) (tokenType, []string) {
 				if nd.Tok != token.DEFINE {
 					def = nil
 				}
-				return tokVariable, def
+				return tokVariable, def // '_' in _ = ...
 			}
 		}
 		// RHS, = x
