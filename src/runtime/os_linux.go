ed24b37fd2b0c242525eb2203d90627c4be1b149
Brad Fitzpatrick <bradfitz@golang.org>
2022-10-26 13:38:07 -0700

    runtime: add missing closing curly brace in runtime corruption error message
    (Fixing the most important part of this bug.)
    
    Updates #56426
    
    Change-Id: If657ae47a5fe7dacc31d2c487e53e9f2dd5d03bf
    Reviewed-on: https://go-review.googlesource.com/c/go/+/445695
    TryBot-Result: Gopher Robot <gobot@golang.org>
    Run-TryBot: Brad Fitzpatrick <bradfitz@golang.org>
    Auto-Submit: Brad Fitzpatrick <bradfitz@golang.org>
    Reviewed-by: Ian Lance Taylor <iant@google.com>
    Reviewed-by: Michael Pratt <mpratt@google.com>

--- src/runtime/os_linux.go
+++ src/runtime/os_linux.go
@@ -880,7 +880,7 @@ func runPerThreadSyscall() {
 	}
 	if errno != 0 || r1 != args.r1 || r2 != args.r2 {
 		print("trap:", args.trap, ", a123456=[", args.a1, ",", args.a2, ",", args.a3, ",", args.a4, ",", args.a5, ",", args.a6, "]\n")
-		print("results: got {r1=", r1, ",r2=", r2, ",errno=", errno, "}, want {r1=", args.r1, ",r2=", args.r2, ",errno=0\n")
+		print("results: got {r1=", r1, ",r2=", r2, ",errno=", errno, "}, want {r1=", args.r1, ",r2=", args.r2, ",errno=0}\n")
 		fatal("AllThreadsSyscall6 results differ between threads; runtime corrupted")
 	}
 
