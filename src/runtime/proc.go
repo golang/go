85dccbb2677bf53b02f01c3477f64b39f24ed7e0
doujiang24 <doujiang24@gmail.com>
2022-10-27 23:49:52 +0800

    add a note for dropm.

--- src/runtime/proc.go
+++ src/runtime/proc.go
@@ -2053,6 +2053,8 @@ func oneNewExtraM() {
 // So that the destructor would invoke dropm while the non-Go thread is exiting.
 // This is much faster since it avoids expensive signal-related syscalls.
 //
+// NOTE: this always runs without a P, so, nowritebarrierrec required.
+//
 //go:nowritebarrierrec
 func dropm() {
 	// Clear m and g, and return m to the extra list.
