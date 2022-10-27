2633c9af5808c9c003e08fd4e2a46665d8577faa
doujiang24 <doujiang24@gmail.com>
2022-10-27 12:31:19 +0800

    Revert "wip: commented failed tests."
    This reverts commit f8ebf13072c380e7e33766e570f55152bfa71298.

--- misc/cgo/testshared/shared_test.go
+++ misc/cgo/testshared/shared_test.go
@@ -734,7 +734,6 @@ func requireGccgo(t *testing.T) {
 	}
 }
 
-/*
 // Build a GOPATH package into a shared library with gccgo and an executable that
 // links against it.
 func TestGoPathShlibGccgo(t *testing.T) {
@@ -784,7 +783,6 @@ func TestTwoGopathShlibsGccgo(t *testing.T) {
 	// And check it runs.
 	run(t, "gccgo-built", "../../bin/exe2")
 }
-*/
 
 // Testing rebuilding of shared libraries when they are stale is a bit more
 // complicated that it seems like it should be. First, we make everything "old": but
