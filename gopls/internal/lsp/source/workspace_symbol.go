f1c8f7f8d5ac22654e879bbaa5c6887bb3782733
Alan Donovan <adonovan@google.com>
2022-10-28 12:07:01 -0400

    internal/lsp/source: optimize filter regular expression
    When the default filter glob changed to **/node_modules,
    the performance of BenchmarkWorkspaceSymbols was observed
    to degrade by an astonishing 20%.
    
    (CPU profiles of the benchmark reported that the Disallow
    functions percentage had increased only slightly, but these
    measures are misleading since the benchmark has a very
    CPU-intensive set-up step, so all the percentages are
    quotients of this figure, masking their relative importance
    to the small region during which the benchmark timer is
    running.)
    
    This change removes the unnecessary ^.* prefix from the
    generated regular expression. Really the regexp package
    ought to do this.
    
    Also, minor cleanups and tweaks to the surrounding code.
    
    Change-Id: I806aad810ce2e7bbfb2c9b04009d8db752a3b10d
    Reviewed-on: https://go-review.googlesource.com/c/tools/+/446177
    Run-TryBot: Alan Donovan <adonovan@google.com>
    gopls-CI: kokoro <noreply+kokoro@google.com>
    TryBot-Result: Gopher Robot <gobot@golang.org>
    Reviewed-by: Robert Findley <rfindley@google.com>

--- gopls/internal/lsp/source/workspace_symbol.go
+++ gopls/internal/lsp/source/workspace_symbol.go
@@ -379,6 +379,7 @@ func NewFilterer(rawFilters []string) *Filterer {
 	var f Filterer
 	for _, filter := range rawFilters {
 		filter = path.Clean(filepath.ToSlash(filter))
+		// TODO(dungtuanle): fix: validate [+-] prefix.
 		op, prefix := filter[0], filter[1:]
 		// convertFilterToRegexp adds "/" at the end of prefix to handle cases where a filter is a prefix of another filter.
 		// For example, it prevents [+foobar, -foo] from excluding "foobar".
@@ -391,20 +392,19 @@ func NewFilterer(rawFilters []string) *Filterer {
 
 // Disallow return true if the path is excluded from the filterer's filters.
 func (f *Filterer) Disallow(path string) bool {
+	// Ensure trailing but not leading slash.
 	path = strings.TrimPrefix(path, "/")
-	var excluded bool
-
-	for i, filter := range f.filters {
-		path := path
-		if !strings.HasSuffix(path, "/") {
-			path += "/"
-		}
-		if !filter.MatchString(path) {
-			continue
-		}
-		excluded = f.excluded[i]
+	if !strings.HasSuffix(path, "/") {
+		path += "/"
 	}
 
+	// TODO(adonovan): opt: iterate in reverse and break at first match.
+	excluded := false
+	for i, filter := range f.filters {
+		if filter.MatchString(path) {
+			excluded = f.excluded[i] // last match wins
+		}
+	}
 	return excluded
 }
 
@@ -419,6 +419,7 @@ func convertFilterToRegexp(filter string) *regexp.Regexp {
 	ret.WriteString("^")
 	segs := strings.Split(filter, "/")
 	for _, seg := range segs {
+		// Inv: seg != "" since path is clean.
 		if seg == "**" {
 			ret.WriteString(".*")
 		} else {
@@ -426,8 +427,15 @@ func convertFilterToRegexp(filter string) *regexp.Regexp {
 		}
 		ret.WriteString("/")
 	}
+	pattern := ret.String()
 
-	return regexp.MustCompile(ret.String())
+	// Remove unnecessary "^.*" prefix, which increased
+	// BenchmarkWorkspaceSymbols time by ~20% (even though
+	// filter CPU time increased by only by ~2.5%) when the
+	// default filter was changed to "**/node_modules".
+	pattern = strings.TrimPrefix(pattern, "^.*")
+
+	return regexp.MustCompile(pattern)
 }
 
 // symbolFile holds symbol information for a single file.
