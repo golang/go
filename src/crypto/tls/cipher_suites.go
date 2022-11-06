20a7fe0778fbfcfd789a194456e87dd4a60b655e
Sebastiaan van Stijn <github@gone.nl>
2022-11-06 13:37:03 +0100

    crypto/tls: remove unused nonAESGCMAEADCiphers variable
    It was no longer used since CL 314609

--- src/crypto/tls/cipher_suites.go
+++ src/crypto/tls/cipher_suites.go
@@ -377,14 +377,6 @@ var aesgcmCiphers = map[uint16]bool{
 	TLS_AES_256_GCM_SHA384: true,
 }
 
-var nonAESGCMAEADCiphers = map[uint16]bool{
-	// TLS 1.2
-	TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305:   true,
-	TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305: true,
-	// TLS 1.3
-	TLS_CHACHA20_POLY1305_SHA256: true,
-}
-
 // aesgcmPreferred returns whether the first known cipher in the preference list
 // is an AES-GCM cipher, implying the peer has hardware support for it.
 func aesgcmPreferred(ciphers []uint16) bool {
