37e3779253e851cfdcb1f0b7f83707b0d99f0bc8
Romain Doumenc <romain@trout.software>
2022-11-25 09:42:06 +0000

    Fixes typo in doc

--- src/syscall/js/js.go
+++ src/syscall/js/js.go
@@ -134,8 +134,8 @@ func Global() Value {
 	return valueGlobal
 }
 
-// Scope returns the Javascript object attached to the Go class.
-// If nothing has been explicitly set, this is usually "windo", or "global".
+// Scope returns the Javascript object attached to the scope field of the Go class.
+// If nothing has been explicitly set, behaves like [js.Global].
 func Scope() Value {
 	return jsGo.Get("scope")
 }
