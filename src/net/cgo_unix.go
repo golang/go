d044bbaf95ccf662307f7cb7e4aac608f43d8efd
Mateusz Poliwczak <mpoliwczak34@gmail.com>
2022-12-01 10:12:36 +0100

    retry with bigger buffer

--- src/net/cgo_unix.go
+++ src/net/cgo_unix.go
@@ -14,6 +14,8 @@ package net
 import (
 	"context"
 	"errors"
+	"fmt"
+	"math"
 	"syscall"
 	"unsafe"
 
@@ -346,17 +348,35 @@ func resSearch(ctx context.Context, hostname string, rtype, class int) ([]dnsmes
 	// giving us no way to find out how big the packet is.
 	// For now, we are willing to take res_search's word that there's nothing
 	// useful in the response, even though there *is* a response.
-	const bufSize = 1500
-	buf := (*_C_uchar)(_C_malloc(bufSize))
+	bufSize := maxDNSPacketSize
+	buf := (*_C_uchar)(_C_malloc(uintptr(bufSize)))
 	defer _C_free(unsafe.Pointer(buf))
+
 	s := _C_CString(hostname)
 	defer _C_FreeCString(s)
-	size, err := _C_res_nsearch(state, s, class, rtype, buf, bufSize)
-	if size <= 0 || size > bufSize {
+
+	for {
+		size, err := _C_res_nsearch(state, s, class, rtype, buf, bufSize)
+		if size <= 0 {
+			if err != nil {
+				return nil, fmt.Errorf("res_nsearch failure: %v", err)
+			}
+			return nil, errors.New("res_nsearch failure")
+		}
+		if size <= bufSize {
+			break
+		}
+		if size < math.MaxUint16 {
+			_C_free(unsafe.Pointer(buf))
+			bufSize = size
+			buf = (*_C_uchar)(_C_malloc(uintptr(bufSize)))
+			continue
+		}
 		return nil, errors.New("res_nsearch failure")
 	}
+
 	var p dnsmessage.Parser
-	if _, err := p.Start(unsafe.Slice((*byte)(unsafe.Pointer(buf)), size)); err != nil {
+	if _, err := p.Start(unsafe.Slice((*byte)(unsafe.Pointer(buf)), bufSize)); err != nil {
 		return nil, err
 	}
 	p.SkipAllQuestions()
