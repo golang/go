5e0d70bcb048f2b56342d0f37096cef1c7a8f594
doujiang24 <doujiang24@gmail.com>
2022-12-14 09:11:33 +0800

    remove the hack of GenABIWrappers.

--- src/cmd/compile/internal/ssagen/abi.go
+++ src/cmd/compile/internal/ssagen/abi.go
@@ -205,8 +205,8 @@ func (s *SymABIs) GenABIWrappers() {
 		}
 
 		// Double check that cgo-exported symbols don't get
-		// any wrappers when it's a function implemented in Go.
-		if len(cgoExport) > 0 && fn.ABIRefs&^obj.ABISetOf(fn.ABI) != 0 && hasBody {
+		// any wrappers.
+		if len(cgoExport) > 0 && fn.ABIRefs&^obj.ABISetOf(fn.ABI) != 0 {
 			base.Fatalf("cgo exported function %v cannot have ABI wrappers", fn)
 		}
 
