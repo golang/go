23bf862cf5b0f1c1bb2e528c9005537ddd449f2a
chowyu12 <chowyu08@gmail.com>
2022-12-30 15:54:12 +0800

    remove delete Type1

--- src/builtin/builtin.go
+++ src/builtin/builtin.go
@@ -151,7 +151,7 @@ func copy(dst, src []Type) int
 // The delete built-in function deletes the element with the specified key
 // (m[key]) from the map. If m is nil or there is no such element, delete
 // is a no-op.
-func delete(m map[Type]Type1, key Type)
+func delete(m map[Type]Type, key Type)
 
 // The len built-in function returns the length of v, according to its type:
 //
