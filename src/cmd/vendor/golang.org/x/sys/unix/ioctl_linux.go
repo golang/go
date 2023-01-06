5e06a9a232b55627b7d528e0e8c973734907f88f
Marcel Meyer <meyermarcel@posteo.de>
2023-01-06 22:35:54 +0000

    all: fix typos in go file comments
    These typos were found by executing grep, aspell, sort and uniq in
    a pipe and searching in the resulted list manually for possible typos.
    
    grep -r --include '*.go' -E '^// .*$' . | aspell list | sort | uniq

--- src/cmd/vendor/golang.org/x/sys/unix/ioctl_linux.go
+++ src/cmd/vendor/golang.org/x/sys/unix/ioctl_linux.go
@@ -193,7 +193,7 @@ func ioctlIfreqData(fd int, req uint, value *ifreqData) error {
 	return ioctlPtr(fd, req, unsafe.Pointer(value))
 }
 
-// IoctlKCMClone attaches a new file descriptor to a multiplexor by cloning an
+// IoctlKCMClone attaches a new file descriptor to a multiplexer by cloning an
 // existing KCM socket, returning a structure containing the file descriptor of
 // the new socket.
 func IoctlKCMClone(fd int) (*KCMClone, error) {
@@ -206,12 +206,12 @@ func IoctlKCMClone(fd int) (*KCMClone, error) {
 }
 
 // IoctlKCMAttach attaches a TCP socket and associated BPF program file
-// descriptor to a multiplexor.
+// descriptor to a multiplexer.
 func IoctlKCMAttach(fd int, info KCMAttach) error {
 	return ioctlPtr(fd, SIOCKCMATTACH, unsafe.Pointer(&info))
 }
 
-// IoctlKCMUnattach unattaches a TCP socket file descriptor from a multiplexor.
+// IoctlKCMUnattach unattaches a TCP socket file descriptor from a multiplexer.
 func IoctlKCMUnattach(fd int, info KCMUnattach) error {
 	return ioctlPtr(fd, SIOCKCMUNATTACH, unsafe.Pointer(&info))
 }
