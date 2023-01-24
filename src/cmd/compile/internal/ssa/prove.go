10124c2631893fc944a98880796eadd7df048c37
Keith Randall <khr@golang.org>
2023-01-24 16:11:06 +0000

    [release-branch.go1.20] Revert "cmd/compile: teach prove about bitwise OR operation"
    This reverts commit 3680b5e9c4f42fcf9155aa42b3b344d1fbe19571.
    
    Reason for revert: causes long compile times on certain functions. See issue #57959
    
    Change-Id: Ie9e881ca8abbc79a46de2bfeaed0b9d6c416ed42
    Reviewed-on: https://go-review.googlesource.com/c/go/+/463295
    Run-TryBot: Keith Randall <khr@golang.org>
    Reviewed-by: David Chase <drchase@google.com>
    Reviewed-by: Cherry Mui <cherryyz@google.com>
    TryBot-Result: Gopher Robot <gobot@golang.org>
    Reviewed-by: Cuong Manh Le <cuong.manhle.vn@gmail.com>
    (cherry picked from commit a6ddb15f8f5955d93eeb2f674ec564ffd4530c18)
    Reviewed-on: https://go-review.googlesource.com/c/go/+/463415
    Run-TryBot: Matthew Dempsky <mdempsky@google.com>
    Reviewed-by: Keith Randall <khr@golang.org>
    Auto-Submit: Matthew Dempsky <mdempsky@google.com>
    Reviewed-by: Keith Randall <khr@google.com>

--- src/cmd/compile/internal/ssa/prove.go
+++ src/cmd/compile/internal/ssa/prove.go
@@ -856,9 +856,6 @@ func prove(f *Func) {
 			case OpAnd64, OpAnd32, OpAnd16, OpAnd8:
 				ft.update(b, v, v.Args[1], unsigned, lt|eq)
 				ft.update(b, v, v.Args[0], unsigned, lt|eq)
-			case OpOr64, OpOr32, OpOr16, OpOr8:
-				ft.update(b, v, v.Args[1], unsigned, gt|eq)
-				ft.update(b, v, v.Args[0], unsigned, gt|eq)
 			case OpPhi:
 				// Determine the min and max value of OpPhi composed entirely of integer constants.
 				//
