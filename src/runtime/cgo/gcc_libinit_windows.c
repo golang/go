10c283f16dde5ff52aa439a5e9f3bc14a0bd7904
doujiang24 <doujiang24@gmail.com>
2023-03-09 19:52:26 +0800

    missing x_crosscall2 in windows.
    Signed-off-by: doujiang24 <doujiang24@gmail.com>

--- src/runtime/cgo/gcc_libinit_windows.c
+++ src/runtime/cgo/gcc_libinit_windows.c
@@ -31,6 +31,7 @@ static HANDLE runtime_init_wait;
 static int runtime_init_done;
 
 uintptr_t x_cgo_pthread_key_created;
+void (*x_crosscall2)(void (*fn)(void *), void *, int, size_t);
 
 // Pre-initialize the runtime synchronization objects
 void
