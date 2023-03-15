1d06667bc40160d909897470948bb3fc6ad4da96
Tobias Klauser <tklauser@distanz.ch>
2023-03-15 15:17:11 +0100

    net: skip tests if creating a socket is disallowed
    In a container environment, creating a socket may be disallowed. Try to
    detect these cases and skip the tests instead of failing them.
    
    Fixes #58114
    
    Change-Id: I681d19107e946d2508e2d1704956360f13c7335b
    Reviewed-on: https://go-review.googlesource.com/c/go/+/476217
    Reviewed-by: Cherry Mui <cherryyz@google.com>
    TryBot-Result: Gopher Robot <gobot@golang.org>
    Run-TryBot: Tobias Klauser <tobias.klauser@gmail.com>
    Auto-Submit: Tobias Klauser <tobias.klauser@gmail.com>
    Reviewed-by: Bryan Mills <bcmills@google.com>

--- src/net/protoconn_test.go
+++ src/net/protoconn_test.go
@@ -10,6 +10,7 @@
 package net
 
 import (
+	"internal/testenv"
 	"os"
 	"runtime"
 	"testing"
@@ -161,16 +162,15 @@ func TestUDPConnSpecificMethods(t *testing.T) {
 }
 
 func TestIPConnSpecificMethods(t *testing.T) {
-	if os.Getuid() != 0 {
-		t.Skip("must be root")
-	}
-
 	la, err := ResolveIPAddr("ip4", "127.0.0.1")
 	if err != nil {
 		t.Fatal(err)
 	}
 	c, err := ListenIP("ip4:icmp", la)
-	if err != nil {
+	if testenv.SyscallIsNotSupported(err) {
+		// May be inside a container that disallows creating a socket.
+		t.Skipf("skipping: %v", err)
+	} else if err != nil {
 		t.Fatal(err)
 	}
 	defer c.Close()
