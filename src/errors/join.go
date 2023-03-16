99c3910cf9eacca4b472201030be323925266bb7
David Bendory <bendory@users.noreply.github.com>
2023-03-16 17:17:02 -0400

    Oops! Get the godoc right!

--- src/errors/join.go
+++ src/errors/join.go
@@ -6,7 +6,7 @@ package errors
 
 // Join returns an error that wraps the given errors.
 // Any nil error values are discarded.
-// Join returns nil if errs consists entirely of non-nil values.
+// Join returns nil if errs consists entirely of nil values.
 // The error formats as the concatenation of the strings obtained
 // by calling the Error method of each element of errs, with a newline
 // between each string.
