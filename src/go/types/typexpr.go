bf9d9b7dba25ecd2956f4f613ff83c1a3624a038
Robert Griesemer <gri@golang.org>
2023-03-23 13:57:47 -0700

    go/types, types2: better error message for some invalid integer array lengths
    Don't say "array length must be integer" if it is in fact an integer.
    
    Fixes #59209
    
    Change-Id: If60b93a0418f5837ac334412d3838eec25eeb855
    Reviewed-on: https://go-review.googlesource.com/c/go/+/479115
    Reviewed-by: Robert Griesemer <gri@google.com>
    Run-TryBot: Robert Griesemer <gri@google.com>
    Reviewed-by: Robert Findley <rfindley@google.com>
    Auto-Submit: Robert Griesemer <gri@google.com>
    TryBot-Result: Gopher Robot <gobot@golang.org>

--- src/go/types/typexpr.go
+++ src/go/types/typexpr.go
@@ -494,13 +494,17 @@ func (check *Checker) arrayLength(e ast.Expr) int64 {
 				if n, ok := constant.Int64Val(val); ok && n >= 0 {
 					return n
 				}
-				check.errorf(&x, InvalidArrayLen, "invalid array length %s", &x)
-				return -1
 			}
 		}
 	}
 
-	check.errorf(&x, InvalidArrayLen, "array length %s must be integer", &x)
+	var msg string
+	if isInteger(x.typ) {
+		msg = "invalid array length %s"
+	} else {
+		msg = "array length %s must be integer"
+	}
+	check.errorf(&x, InvalidArrayLen, msg, &x)
 	return -1
 }
 
