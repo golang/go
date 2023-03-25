ed9744dff5341ab692154eab1e6e0f1a3c138733
Johan Brandhorst-Satzkorn <johan.brandhorst@gmail.com>
2023-03-24 22:25:18 -0700

    cmd/compile: allow go:wasmimport in syscall
    The go:wasmimport compiler directive is used by the wasi port
    to access host APIs, some of need to implemented in the syscall
    package.
    
    Co-authored-by: Richard Musiol <neelance@gmail.com>
    Co-authored-by: Achille Roussel <achille.roussel@gmail.com>
    Co-authored-by: Julien Fabre <ju.pryz@gmail.com>
    Co-authored-by: Evan Phoenix <evan@phx.io>
    Change-Id: I3851e154c6989094effcd25bba5864faa133564e
    Reviewed-on: https://go-review.googlesource.com/c/go/+/479615
    Reviewed-by: Cherry Mui <cherryyz@google.com>
    Auto-Submit: Johan Brandhorst-Satzkorn <johan.brandhorst@gmail.com>
    Reviewed-by: Matthew Dempsky <mdempsky@google.com>
    TryBot-Result: Gopher Robot <gobot@golang.org>
    Run-TryBot: Johan Brandhorst-Satzkorn <johan.brandhorst@gmail.com>

--- src/cmd/compile/internal/noder/noder.go
+++ src/cmd/compile/internal/noder/noder.go
@@ -237,7 +237,7 @@ func (p *noder) pragma(pos syntax.Pos, blankLine bool, text string, old syntax.P
 			p.error(syntax.Error{Pos: pos, Msg: "usage: //go:wasmimport importmodule importname"})
 			break
 		}
-		if !base.Flag.CompilingRuntime && base.Ctxt.Pkgpath != "syscall/js" && base.Ctxt.Pkgpath != "syscall/js_test" {
+		if !base.Flag.CompilingRuntime && base.Ctxt.Pkgpath != "syscall/js" && base.Ctxt.Pkgpath != "syscall/js_test" && base.Ctxt.Pkgpath != "syscall" {
 			p.error(syntax.Error{Pos: pos, Msg: "//go:wasmimport directive cannot be used outside of runtime or syscall/js"})
 			break
 		}
