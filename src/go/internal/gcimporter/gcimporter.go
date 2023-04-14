750e91152bbd1cb2869660ddb80f2b3adcfd1f9b
Bryan C. Mills <bcmills@google.com>
2023-04-14 16:48:26 +0000

    cmd/compile/internal/importer,go/internal/gcimporter: use the 'go' command from build.Default.GOROOT in lookupGorootExport
    Also set GOROOT explicitly in case it is set to something else in the
    caller's environment.
    
    Fixes #59598.
    
    Change-Id: I5599ed1183b23187fc3b976786f3c320d42ef4f3
    Reviewed-on: https://go-review.googlesource.com/c/go/+/484756
    Run-TryBot: Bryan Mills <bcmills@google.com>
    TryBot-Result: Gopher Robot <gobot@golang.org>
    Auto-Submit: Bryan Mills <bcmills@google.com>
    Reviewed-by: Michael Matloob <matloob@golang.org>

--- src/go/internal/gcimporter/gcimporter.go
+++ src/go/internal/gcimporter/gcimporter.go
@@ -43,8 +43,9 @@ func lookupGorootExport(pkgDir string) (string, bool) {
 		)
 		f, _ = exportMap.LoadOrStore(pkgDir, func() (string, bool) {
 			listOnce.Do(func() {
-				cmd := exec.Command("go", "list", "-export", "-f", "{{.Export}}", pkgDir)
+				cmd := exec.Command(filepath.Join(build.Default.GOROOT, "bin", "go"), "list", "-export", "-f", "{{.Export}}", pkgDir)
 				cmd.Dir = build.Default.GOROOT
+				cmd.Env = append(cmd.Environ(), "GOROOT="+build.Default.GOROOT)
 				var output []byte
 				output, err := cmd.Output()
 				if err != nil {
