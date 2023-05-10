697c255ece18cd4772b76d62991474a7da2536d8
Shogo Hida <shogo.hida@gmail.com>
2023-05-10 23:33:14 +0900

    Add paragraph

--- src/cmd/go/alldocs.go
+++ src/cmd/go/alldocs.go
@@ -1708,6 +1708,10 @@
 // error. (The go command's standard error is reserved for printing
 // errors building the tests.)
 //
+// The go command places $GOROOT/bin at the beginning of $PATH
+// in the test's environment, so that tests that execute
+// 'go' commands use the same 'go' as the parent 'go test' command.
+//
 // Go test runs in two different modes:
 //
 // The first, called local directory mode, occurs when go test is
