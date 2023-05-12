c0ed873cd8259f16d0da67eee783fda49f45ef61
Bryan C. Mills <bcmills@google.com>
2023-05-12 14:15:16 -0400

    [release-branch.go1.20] cmd/go: disallow package directories containing newlines
    Directory or file paths containing newlines may cause tools (such as
    cmd/cgo) that emit "//line" or "#line" -directives to write part of
    the path into non-comment lines in generated source code. If those
    lines contain valid Go code, it may be injected into the resulting
    binary.
    
    (Note that Go import paths and file paths within module zip files
    already could not contain newlines.)
    
    Thanks to Juho Nurminen of Mattermost for reporting this issue.
    
    Updates #60167.
    Fixes #60516.
    Fixes CVE-2023-29402.
    
    Change-Id: Ic3c7d8d1f6460993bd93a27035d61bff7dd68832
    Reviewed-on: https://team-review.git.corp.google.com/c/golang/go-private/+/1882606
    Reviewed-by: Roland Shoemaker <bracewell@google.com>
    Run-TryBot: Roland Shoemaker <bracewell@google.com>
    Reviewed-by: Russ Cox <rsc@google.com>
    Reviewed-by: Damien Neil <dneil@google.com>
    (cherry picked from commit 41f9046495564fc728d6f98384ab7276450ac7e2)
    Reviewed-on: https://team-review.git.corp.google.com/c/golang/go-private/+/1902230
    Reviewed-on: https://team-review.git.corp.google.com/c/golang/go-private/+/1904347
    Reviewed-by: Bryan Mills <bcmills@google.com>
    Reviewed-by: Michael Knyszek <mknyszek@google.com>
    Reviewed-on: https://go-review.googlesource.com/c/go/+/501222
    Run-TryBot: David Chase <drchase@google.com>
    Auto-Submit: Michael Knyszek <mknyszek@google.com>
    TryBot-Result: Gopher Robot <gobot@golang.org>

--- src/cmd/go/script_test.go
+++ src/cmd/go/script_test.go
@@ -222,6 +222,7 @@ func scriptEnv(srv *vcstest.Server, srvCertFile string) ([]string, error) {
 		"devnull=" + os.DevNull,
 		"goversion=" + version,
 		"CMDGO_TEST_RUN_MAIN=true",
+		"newline=\n",
 	}
 
 	if testenv.Builder() != "" || os.Getenv("GIT_TRACE_CURL") == "1" {
