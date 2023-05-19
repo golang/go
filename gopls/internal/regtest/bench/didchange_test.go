b742cb9a5ed7bcd56636d0ebe4f3afe7bab0c628
Rob Findley <rfindley@google.com>
2023-05-19 10:22:58 -0400

    gopls/internal/regtest/bench: add a benchmark for diagnosing saves
    As we discovered while investigating golang/go#60089, mod tidy
    operations can significantly affect the amount of time it takes gopls to
    diagnose a saved file.
    
    Add a benchmark for this operation. For reference, this new benchmark
    takes 8s+ on google-cloud-go, vs 300ms for DiagnoseChange (without the
    save).
    
    Updates golang/go#60089
    
    Change-Id: Ie88bd63dd7d205b8629173e7f84aa1aa9858016b
    Reviewed-on: https://go-review.googlesource.com/c/tools/+/496435
    TryBot-Result: Gopher Robot <gobot@golang.org>
    gopls-CI: kokoro <noreply+kokoro@google.com>
    Run-TryBot: Robert Findley <rfindley@google.com>
    Reviewed-by: Alan Donovan <adonovan@google.com>

--- gopls/internal/regtest/bench/didchange_test.go
+++ gopls/internal/regtest/bench/didchange_test.go
@@ -19,11 +19,13 @@ import (
 // shared file cache.
 var editID int64 = time.Now().UnixNano()
 
-var didChangeTests = []struct {
+type changeTest struct {
 	repo string
 	file string
-}{
-	{"google-cloud-go", "httpreplay/httpreplay.go"},
+}
+
+var didChangeTests = []changeTest{
+	{"google-cloud-go", "internal/annotate.go"},
 	{"istio", "pkg/fuzz/util.go"},
 	{"kubernetes", "pkg/controller/lookup_cache.go"},
 	{"kuma", "api/generic/insights.go"},
@@ -64,43 +66,63 @@ func BenchmarkDidChange(b *testing.B) {
 
 func BenchmarkDiagnoseChange(b *testing.B) {
 	for _, test := range didChangeTests {
-		b.Run(test.repo, func(b *testing.B) {
-			sharedEnv := getRepo(b, test.repo).sharedEnv(b)
-			config := fake.EditorConfig{
-				Env: map[string]string{
-					"GOPATH": sharedEnv.Sandbox.GOPATH(),
-				},
-				Settings: map[string]interface{}{
-					"diagnosticsDelay": "0s",
-				},
-			}
-			// Use a new env to avoid the diagnostic delay: we want to measure how
-			// long it takes to produce the diagnostics.
-			env := getRepo(b, test.repo).newEnv(b, "diagnoseChange", config)
-			defer env.Close()
-			env.OpenFile(test.file)
-			// Insert the text we'll be modifying at the top of the file.
-			env.EditBuffer(test.file, protocol.TextEdit{NewText: "// __REGTEST_PLACEHOLDER_0__\n"})
-			env.AfterChange()
-			b.ResetTimer()
-
-			// We must use an extra subtest layer here, so that we only set up the
-			// shared env once (otherwise we pay additional overhead and the profiling
-			// flags don't work).
-			b.Run("diagnose", func(b *testing.B) {
-				for i := 0; i < b.N; i++ {
-					edits := atomic.AddInt64(&editID, 1)
-					env.EditBuffer(test.file, protocol.TextEdit{
-						Range: protocol.Range{
-							Start: protocol.Position{Line: 0, Character: 0},
-							End:   protocol.Position{Line: 1, Character: 0},
-						},
-						// Increment the placeholder text, to ensure cache misses.
-						NewText: fmt.Sprintf("// __REGTEST_PLACEHOLDER_%d__\n", edits),
-					})
-					env.AfterChange()
-				}
-			})
-		})
+		runChangeDiagnosticsBenchmark(b, test, false)
 	}
+}
+
+// TODO(rfindley): add a benchmark for with a metadata-affecting change, when
+// this matters.
+func BenchmarkDiagnoseSave(b *testing.B) {
+	for _, test := range didChangeTests {
+		runChangeDiagnosticsBenchmark(b, test, true)
+	}
+}
+
+// runChangeDiagnosticsBenchmark runs a benchmark to edit the test file and
+// await the resulting diagnostics pass. If save is set, the file is also saved.
+func runChangeDiagnosticsBenchmark(b *testing.B, test changeTest, save bool) {
+	b.Run(test.repo, func(b *testing.B) {
+		sharedEnv := getRepo(b, test.repo).sharedEnv(b)
+		config := fake.EditorConfig{
+			Env: map[string]string{
+				"GOPATH": sharedEnv.Sandbox.GOPATH(),
+			},
+			Settings: map[string]interface{}{
+				"diagnosticsDelay": "0s",
+			},
+		}
+		// Use a new env to avoid the diagnostic delay: we want to measure how
+		// long it takes to produce the diagnostics.
+		env := getRepo(b, test.repo).newEnv(b, "diagnoseSave", config)
+		defer env.Close()
+		env.OpenFile(test.file)
+		// Insert the text we'll be modifying at the top of the file.
+		env.EditBuffer(test.file, protocol.TextEdit{NewText: "// __REGTEST_PLACEHOLDER_0__\n"})
+		if save {
+			env.SaveBuffer(test.file)
+		}
+		env.AfterChange()
+		b.ResetTimer()
+
+		// We must use an extra subtest layer here, so that we only set up the
+		// shared env once (otherwise we pay additional overhead and the profiling
+		// flags don't work).
+		b.Run("diagnose", func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				edits := atomic.AddInt64(&editID, 1)
+				env.EditBuffer(test.file, protocol.TextEdit{
+					Range: protocol.Range{
+						Start: protocol.Position{Line: 0, Character: 0},
+						End:   protocol.Position{Line: 1, Character: 0},
+					},
+					// Increment the placeholder text, to ensure cache misses.
+					NewText: fmt.Sprintf("// __REGTEST_PLACEHOLDER_%d__\n", edits),
+				})
+				if save {
+					env.SaveBuffer(test.file)
+				}
+				env.AfterChange()
+			}
+		})
+	})
 }
