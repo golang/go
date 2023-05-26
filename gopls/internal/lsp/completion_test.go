726c727df97f9144045163b8df42c855b4314612
Cuong Manh Le <cuong.manhle.vn@gmail.com>
2023-05-26 11:30:54 +0700

    gopls/internal/lsp: enable min/max builtin completion test
    For golang/go#59488
    
    Change-Id: I93680138c90750454b4d94af6dc84fe942c9dd34
    Reviewed-on: https://go-review.googlesource.com/c/tools/+/498516
    Run-TryBot: Cuong Manh Le <cuong.manhle.vn@gmail.com>
    Reviewed-by: Robert Findley <rfindley@google.com>
    Reviewed-by: Matthew Dempsky <mdempsky@google.com>
    TryBot-Result: Gopher Robot <gobot@golang.org>
    Auto-Submit: Cuong Manh Le <cuong.manhle.vn@gmail.com>
    gopls-CI: kokoro <noreply+kokoro@google.com>

--- gopls/internal/lsp/completion_test.go
+++ gopls/internal/lsp/completion_test.go
@@ -25,8 +25,8 @@ func (r *runner) Completion(t *testing.T, src span.Span, test tests.Completion, 
 		opts.LiteralCompletions = strings.Contains(string(src.URI()), "literal")
 		opts.ExperimentalPostfixCompletions = strings.Contains(string(src.URI()), "postfix")
 	})
-	got = filterSkipCompletionItems(tests.FilterBuiltins(src, got))
-	want := filterSkipCompletionItems(expected(t, test, items))
+	got = tests.FilterBuiltins(src, got)
+	want := expected(t, test, items)
 	if diff := tests.DiffCompletionItems(want, got); diff != "" {
 		t.Errorf("mismatching completion items (-want +got):\n%s", diff)
 	}
@@ -174,17 +174,4 @@ func (r *runner) callCompletion(t *testing.T, src span.Span, options func(*sourc
 		t.Fatal(err)
 	}
 	return list.Items
-}
-
-func filterSkipCompletionItems(items []protocol.CompletionItem) []protocol.CompletionItem {
-	n := 0
-	for _, item := range items {
-		// TODO(cuonglm): remove once https://go-review.googlesource.com/c/go/+/498495 land.
-		if item.Label == "max" || item.Label == "min" {
-			continue
-		}
-		items[n] = item
-		n++
-	}
-	return items[:n]
 }
