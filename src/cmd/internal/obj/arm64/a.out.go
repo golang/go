8a1ff5182780af72d08c3e6b91694425a1498014
erifan01 <eric.fang@arm.com>
2023-06-06 17:55:58 +0800

    cmd/asm: add the fifth argument of the instruction to Optab on arm64
    Currently the Optab structure contains four arguments of an instruction,
    excludes the fifth argument p.RegTo2. It does not participate in
    instruction matching and is usually handled separately.
    
    Instructions with five operands are common in the newer arm instruction
    set, so this CL adds the fifth argument to Optab, so that instruction
    matching is easier. This caused the oplook function also needs to be
    updated synchronously, this CL also made some cleaning and modifications
    to this function.
    
    Change-Id: I1d95ad99e72a44dfad1e00db182cfc369a0e55c6
    Reviewed-on: https://go-review.googlesource.com/c/go/+/505975
    Reviewed-by: Cherry Mui <cherryyz@google.com>
    TryBot-Result: Gopher Robot <gobot@golang.org>
    Reviewed-by: Matthew Dempsky <mdempsky@google.com>
    Run-TryBot: Eric Fang <eric.fang@arm.com>

--- src/cmd/internal/obj/arm64/a.out.go
+++ src/cmd/internal/obj/arm64/a.out.go
@@ -341,21 +341,21 @@ const (
 	// optab is sorted based on the order of these constants
 	// and the first match is chosen.
 	// The more specific class needs to come earlier.
-	C_NONE   = iota
-	C_REG    // R0..R30
-	C_ZREG   // R0..R30, ZR
-	C_RSP    // R0..R30, RSP
-	C_FREG   // F0..F31
-	C_VREG   // V0..V31
-	C_PAIR   // (Rn, Rm)
-	C_SHIFT  // Rn<<2
-	C_EXTREG // Rn.UXTB[<<3]
-	C_SPR    // REG_NZCV
-	C_COND   // condition code, EQ, NE, etc.
-	C_SPOP   // special operand, PLDL1KEEP, VMALLE1IS, etc.
-	C_ARNG   // Vn.<T>
-	C_ELEM   // Vn.<T>[index]
-	C_LIST   // [V1, V2, V3]
+	C_NONE   = iota + 1 // starting from 1, leave unclassified Addr's class as 0
+	C_REG               // R0..R30
+	C_ZREG              // R0..R30, ZR
+	C_RSP               // R0..R30, RSP
+	C_FREG              // F0..F31
+	C_VREG              // V0..V31
+	C_PAIR              // (Rn, Rm)
+	C_SHIFT             // Rn<<2
+	C_EXTREG            // Rn.UXTB[<<3]
+	C_SPR               // REG_NZCV
+	C_COND              // condition code, EQ, NE, etc.
+	C_SPOP              // special operand, PLDL1KEEP, VMALLE1IS, etc.
+	C_ARNG              // Vn.<T>
+	C_ELEM              // Vn.<T>[index]
+	C_LIST              // [V1, V2, V3]
 
 	C_ZCON     // $0
 	C_ABCON0   // could be C_ADDCON0 or C_BITCON
