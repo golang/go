a1212b535a33bf0a03d9bd9416f0b151f14e3d34
Alexey Gorgurov <leha-bot@yandex.ru>
2023-06-13 14:37:09 +0200

    runtime/cgo: Suppress -Werror=parenthesis to avoid compile error
    Current runtime may not compile under rare circumstances if compiler
    enforces the -Werror flag for ambiguous parentheses
    (e.g., GCC 11 in C++ mode, but it may enable it in future releases).
    
    Build may fail with this error:
    
    In file included from gcc_context.c:8:
    libcgo.h:102:15: error: unnecessary parentheses in declaration of '_cgo_get_context_function' [-Werror=parentheses]
       102 | extern void (*(_cgo_get_context_function(void)))(struct context_arg*);
          |               ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
    libcgo.h:102:15: note: remove parentheses
       102 | extern void (*(_cgo_get_context_function(void)))(struct context_arg*);
          |               ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
          |               -                               -
    cc1plus: all warnings being treated as errors
    
    This commit suppresses this warning, to avoid the possible build errors
    on future static analyzer improvements, as these parentheses improves
    the code readability.

--- src/runtime/cgo/libcgo.h
+++ src/runtime/cgo/libcgo.h
@@ -99,7 +99,10 @@ void darwin_arm_init_mach_exception_handler(void);
 struct context_arg {
 	uintptr_t Context;
 };
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wparentheses"
 extern void (*(_cgo_get_context_function(void)))(struct context_arg*);
+#pragma GCC diagnostic pop
 
 /*
  * The argument for the cgo traceback callback. See runtime.SetCgoTraceback.
