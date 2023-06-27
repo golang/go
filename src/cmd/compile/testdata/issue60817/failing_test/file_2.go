4b2ec2827a0b701bd7d56d39a42075688e875e9c
Hiro <laciferin@gmail.com>
2023-06-27 10:33:49 +0530

    remove test in compile
    Took 3 minutes
    
    Change-Id: Ia41ac4d7001e1dc11342e529458fa5ca0125dfb0
    
    
    Took 2 minutes

--- src/cmd/compile/testdata/issue60817/failing_test/file_2.go
+++
@@ -1,6 +0,0 @@
-package main
-
-
-type T2[T any] struct {
-	e *innerT[T, *T2[T]]
-}
--- src/cmd/compile/testdata/issue60817/failing_test/main_test.go
+++
@@ -1,7 +0,0 @@
-package main
-
-import "testing"
-
-func TestConstraints(t *testing.T) {
-	t.Log("test passed")
-}
