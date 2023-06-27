5cdb132228b90732d57215893a9910ded694c585
Paul E. Murphy <murp@ibm.com>
2023-06-27 17:17:33 -0500

    cmd/compile/internal/ssa: improve masking codegen on PPC64
    Generate RLDIC[LR] instead of MOVD mask, Rx; AND Rx, Ry, Rz.
    This helps reduce code size, and reduces the latency caused
    by the constant load.
    
    Similarly, for smaller-than-register values, truncate constants
    which exceed the range of the value's type to avoid needing to
    load a constant.
    
    Change-Id: I6019684795eb8962d4fd6d9585d08b17c15e7d64
    Reviewed-on: https://go-review.googlesource.com/c/go/+/515576
    Reviewed-by: Lynn Boger <laboger@linux.vnet.ibm.com>
    Reviewed-by: Dmitri Shuralyov <dmitshur@google.com>
    Run-TryBot: Paul Murphy <murp@ibm.com>
    TryBot-Result: Gopher Robot <gobot@golang.org>
    Reviewed-by: Cherry Mui <cherryyz@google.com>

--- src/cmd/compile/internal/ssa/rewritePPC64latelower.go
+++ src/cmd/compile/internal/ssa/rewritePPC64latelower.go
@@ -3,15 +3,115 @@
 package ssa
 
 import "internal/buildcfg"
+import "cmd/compile/internal/types"
 
 func rewriteValuePPC64latelower(v *Value) bool {
 	switch v.Op {
+	case OpPPC64AND:
+		return rewriteValuePPC64latelower_OpPPC64AND(v)
 	case OpPPC64ISEL:
 		return rewriteValuePPC64latelower_OpPPC64ISEL(v)
+	case OpPPC64RLDICL:
+		return rewriteValuePPC64latelower_OpPPC64RLDICL(v)
 	case OpPPC64SETBC:
 		return rewriteValuePPC64latelower_OpPPC64SETBC(v)
 	case OpPPC64SETBCR:
 		return rewriteValuePPC64latelower_OpPPC64SETBCR(v)
+	}
+	return false
+}
+func rewriteValuePPC64latelower_OpPPC64AND(v *Value) bool {
+	v_1 := v.Args[1]
+	v_0 := v.Args[0]
+	b := v.Block
+	typ := &b.Func.Config.Types
+	// match: (AND <t> x:(MOVDconst [m]) n)
+	// cond: t.Size() <= 2
+	// result: (Select0 (ANDCCconst [int64(int16(m))] n))
+	for {
+		t := v.Type
+		for _i0 := 0; _i0 <= 1; _i0, v_0, v_1 = _i0+1, v_1, v_0 {
+			x := v_0
+			if x.Op != OpPPC64MOVDconst {
+				continue
+			}
+			m := auxIntToInt64(x.AuxInt)
+			n := v_1
+			if !(t.Size() <= 2) {
+				continue
+			}
+			v.reset(OpSelect0)
+			v0 := b.NewValue0(v.Pos, OpPPC64ANDCCconst, types.NewTuple(typ.Int, types.TypeFlags))
+			v0.AuxInt = int64ToAuxInt(int64(int16(m)))
+			v0.AddArg(n)
+			v.AddArg(v0)
+			return true
+		}
+		break
+	}
+	// match: (AND x:(MOVDconst [m]) n)
+	// cond: isPPC64ValidShiftMask(m)
+	// result: (RLDICL [encodePPC64RotateMask(0,m,64)] n)
+	for {
+		for _i0 := 0; _i0 <= 1; _i0, v_0, v_1 = _i0+1, v_1, v_0 {
+			x := v_0
+			if x.Op != OpPPC64MOVDconst {
+				continue
+			}
+			m := auxIntToInt64(x.AuxInt)
+			n := v_1
+			if !(isPPC64ValidShiftMask(m)) {
+				continue
+			}
+			v.reset(OpPPC64RLDICL)
+			v.AuxInt = int64ToAuxInt(encodePPC64RotateMask(0, m, 64))
+			v.AddArg(n)
+			return true
+		}
+		break
+	}
+	// match: (AND x:(MOVDconst [m]) n)
+	// cond: m != 0 && isPPC64ValidShiftMask(^m)
+	// result: (RLDICR [encodePPC64RotateMask(0,m,64)] n)
+	for {
+		for _i0 := 0; _i0 <= 1; _i0, v_0, v_1 = _i0+1, v_1, v_0 {
+			x := v_0
+			if x.Op != OpPPC64MOVDconst {
+				continue
+			}
+			m := auxIntToInt64(x.AuxInt)
+			n := v_1
+			if !(m != 0 && isPPC64ValidShiftMask(^m)) {
+				continue
+			}
+			v.reset(OpPPC64RLDICR)
+			v.AuxInt = int64ToAuxInt(encodePPC64RotateMask(0, m, 64))
+			v.AddArg(n)
+			return true
+		}
+		break
+	}
+	// match: (AND <t> x:(MOVDconst [m]) n)
+	// cond: t.Size() == 4 && isPPC64WordRotateMask(m)
+	// result: (RLWINM [encodePPC64RotateMask(0,m,32)] n)
+	for {
+		t := v.Type
+		for _i0 := 0; _i0 <= 1; _i0, v_0, v_1 = _i0+1, v_1, v_0 {
+			x := v_0
+			if x.Op != OpPPC64MOVDconst {
+				continue
+			}
+			m := auxIntToInt64(x.AuxInt)
+			n := v_1
+			if !(t.Size() == 4 && isPPC64WordRotateMask(m)) {
+				continue
+			}
+			v.reset(OpPPC64RLWINM)
+			v.AuxInt = int64ToAuxInt(encodePPC64RotateMask(0, m, 32))
+			v.AddArg(n)
+			return true
+		}
+		break
 	}
 	return false
 }
@@ -45,6 +145,29 @@ func rewriteValuePPC64latelower_OpPPC64ISEL(v *Value) bool {
 		v.reset(OpPPC64ISELZ)
 		v.AuxInt = int32ToAuxInt(a ^ 0x4)
 		v.AddArg2(y, z)
+		return true
+	}
+	return false
+}
+func rewriteValuePPC64latelower_OpPPC64RLDICL(v *Value) bool {
+	v_0 := v.Args[0]
+	// match: (RLDICL [em] x:(SRDconst [s] a))
+	// cond: (em&0xFF0000)==0
+	// result: (RLDICL [mergePPC64RLDICLandSRDconst(em, s)] a)
+	for {
+		em := auxIntToInt64(v.AuxInt)
+		x := v_0
+		if x.Op != OpPPC64SRDconst {
+			break
+		}
+		s := auxIntToInt64(x.AuxInt)
+		a := x.Args[0]
+		if !((em & 0xFF0000) == 0) {
+			break
+		}
+		v.reset(OpPPC64RLDICL)
+		v.AuxInt = int64ToAuxInt(mergePPC64RLDICLandSRDconst(em, s))
+		v.AddArg(a)
 		return true
 	}
 	return false
