aa12b8053610ab55df34a23d71f1fc7fa0d73126
Hiro <laciferin@gmail.com>
2023-06-27 09:58:02 +0530

    import-test: issue60817
    Took 16 minutes
    
    Change-Id: I32c86b6d64661f47208818611365e2efc2778379

---
+++ src/cmd/compile/testdata/issue60817/workspace/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+"workspace/with_generics"
+)
+
+func main() {
+
+	_ = with_generics.T1[any]{
+		val: 0,
+	}
+}
---
+++ src/cmd/compile/testdata/issue60817/workspace/with_generics/generics.go
@@ -0,0 +1,16 @@
+package with_generics
+
+
+// It doesn't matter if the innerT1 struct unexported or exported, the result is the same.
+//
+// Within the deadlock issue, it does matter if the type parameter R as a pointer, and the inner field that
+// referenced to type parameter R is a pointer (double reference) or not, it behaves differently. However,
+// it may behave differently based on different issues.
+type innerT1[T any, R T1[T]] struct {
+	reference *R
+}
+
+type T1[T any] struct {
+	e   *innerT1[T, T1[T]]
+	val T
+}
