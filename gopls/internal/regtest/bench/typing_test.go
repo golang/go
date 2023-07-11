ba3c31c9428b1639d107f7a118668335ef025fab
Rob Findley <rfindley@google.com>
2023-07-11 18:12:27 -0400

    gopls/internal/regtest/bench: add a test simulating typing
    Our existing benchmarks measure latency of various operations, but while
    typing most operations do not run to completion: they are canceled. Add
    a benchmark that simulates making changes at various fixed intervals,
    and records the total CPU used.
    
    For golang/go#60926
    
    Change-Id: I972187358b6c802518ac781a12d9cff32faa6670
    Reviewed-on: https://go-review.googlesource.com/c/tools/+/509559
    Run-TryBot: Robert Findley <rfindley@google.com>
    Reviewed-by: Alan Donovan <adonovan@google.com>
    TryBot-Result: Gopher Robot <gobot@golang.org>
    gopls-CI: kokoro <noreply+kokoro@google.com>

---
+++ gopls/internal/regtest/bench/typing_test.go
@@ -0,0 +1,57 @@
+package bench
+
+import (
+	"fmt"
+	"sync/atomic"
+	"testing"
+	"time"
+
+	"golang.org/x/tools/gopls/internal/lsp/protocol"
+)
+
+// BenchmarkTyping simulates typing steadily in a single file at different
+// paces.
+//
+// The key metric for this benchmark is not latency, but cpu_seconds per
+// operation.
+func BenchmarkTyping(b *testing.B) {
+	for _, test := range didChangeTests {
+		b.Run(test.repo, func(b *testing.B) {
+			env := getRepo(b, test.repo).sharedEnv(b)
+			env.OpenFile(test.file)
+			// Insert the text we'll be modifying at the top of the file.
+			env.EditBuffer(test.file, protocol.TextEdit{NewText: "// __REGTEST_PLACEHOLDER_0__\n"})
+			env.AfterChange()
+
+			delays := []time.Duration{
+				10 * time.Millisecond,  // automated changes
+				50 * time.Millisecond,  // very fast mashing, or fast key sequences
+				150 * time.Millisecond, // avg interval for 80wpm typing.
+			}
+
+			for _, delay := range delays {
+				b.Run(delay.String(), func(b *testing.B) {
+					if stopAndRecord := startProfileIfSupported(b, env, qualifiedName(test.repo, "typing")); stopAndRecord != nil {
+						defer stopAndRecord()
+					}
+					ticker := time.NewTicker(delay)
+					for i := 0; i < b.N; i++ {
+						edits := atomic.AddInt64(&editID, 1)
+						env.EditBuffer(test.file, protocol.TextEdit{
+							Range: protocol.Range{
+								Start: protocol.Position{Line: 0, Character: 0},
+								End:   protocol.Position{Line: 1, Character: 0},
+							},
+							// Increment the placeholder text, to ensure cache misses.
+							NewText: fmt.Sprintf("// __REGTEST_PLACEHOLDER_%d__\n", edits),
+						})
+						<-ticker.C
+					}
+					b.StopTimer()
+					ticker.Stop()
+					env.AfterChange() // wait for all change processing to complete
+				})
+			}
+		})
+	}
+}
