e68759e896c56a155b5cde08fe591d487a5f460d
Hiro <laciferin@gmail.com>
2023-08-02 02:21:43 +0530

    sync/map: Clear fixed
    Took 3 minutes

--- src/sync/map.go
+++ src/sync/map.go
@@ -156,16 +156,21 @@ func (m *Map) Store(key, value any) {
 }
 
 func (m *Map) Clear() {
-	defer m.mu.Unlock()
-	m.mu.Lock()
+	/*defer m.mu.Unlock()
+	m.mu.Lock()*/
 
-	read := m.loadReadOnly()
+	/*read := m.loadReadOnly()
 
 	read.amended = false
 
 	clear(read.m) // FIXME: e.delete()
 
-	clear(m.dirty)
+	clear(m.dirty)*/
+
+	m.Range(func(key, value any) bool {
+		m.Delete(key)
+		return true
+	})
 
 }
 
