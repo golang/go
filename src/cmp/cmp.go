5f1b6a7e260209d2162254bcd699915cc56ebec4
Carl Johnson <me@carlmjohnson.net>
2023-08-11 16:04:31 -0400

    Assume CL 517777

--- src/cmp/cmp.go
+++ src/cmp/cmp.go
@@ -60,8 +60,7 @@ func isNaN[T Ordered](x T) bool {
 
 // Or returns the first of its arguments that is not equal to the zero value.
 // If no argument is non-zero, it returns the zero value.
-func Or[T comparable](vals ...T) T {
-	var zero T
+func Or[T any](vals ...T) T {
 	for _, val := range vals {
 		if val != zero {
 			return val
