b4b55abef2172ac15f02d4f2b3ef940f5b3aaf0b
Hiro <laciferin@gmail.com>
2023-08-15 02:39:02 +0530

    testcases: add #62027
    Took 45 seconds

--- src/cmd/compile/internal/ssa/testdata/i61992.go
+++ src/cmd/compile/internal/ssa/testdata/i61992.go
@@ -1,10 +1,11 @@
 package main
 
-
+import "fmt"
 
 type S1 struct {
-	a, b, c []int
-	i       int
+	a, b, c []int //important removing this removes ICE
+
+	i int
 }
 
 type S2 struct {
@@ -14,13 +15,11 @@ type S2 struct {
 
 func F(i int, f func(S1, S2, int) int) int {
 	return f(
-		S1{a: []int{}},
-		S2{b: []int{}},
-		1>>i,
-	)
+		S1{},
+		S2{m: map[int]int{}},
+		1<<i)
 }
 
 func main() {
-
-	println("Hello, Hiro")
+	fmt.Println("Hello, Hiro")
 }
