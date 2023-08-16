b225aa0590e71ba82bf4bb7696430681ced04c53
Ian Lance Taylor <iant@golang.org>
2023-08-16 11:58:10 -0700

    internal/typeparams: run go generate
    Change-Id: I732afe9ff8c4451ead84cd1a429ccd47b781938e
    Reviewed-on: https://go-review.googlesource.com/c/tools/+/520176
    Auto-Submit: Ian Lance Taylor <iant@google.com>
    Reviewed-by: Robert Findley <rfindley@google.com>
    Run-TryBot: Ian Lance Taylor <iant@golang.org>
    gopls-CI: kokoro <noreply+kokoro@google.com>
    Reviewed-by: Ian Lance Taylor <iant@google.com>
    Run-TryBot: Ian Lance Taylor <iant@google.com>
    TryBot-Result: Gopher Robot <gobot@golang.org>

--- internal/typeparams/termlist.go
+++ internal/typeparams/termlist.go
@@ -30,7 +30,7 @@ func (xl termlist) String() string {
 	var buf bytes.Buffer
 	for i, x := range xl {
 		if i > 0 {
-			buf.WriteString(" ∪ ")
+			buf.WriteString(" | ")
 		}
 		buf.WriteString(x.String())
 	}
