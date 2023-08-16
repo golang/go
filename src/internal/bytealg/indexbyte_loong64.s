1052d09dd768d658bda2aa99d13ce4c032f09dc0
Guoqi Chen <chenguoqi@loongson.cn>
2023-08-16 08:55:13 +0800

    internal/bytealg: add regABI support in bytealg functions on loong64
    Update #40724
    
    Co-authored-by: Xiaolin Zhao <zhaoxiaolin@loongson.cn>
    Change-Id: I4a7392afd7238d44e7d09aaca7e0d733649926ac
    Reviewed-on: https://go-review.googlesource.com/c/go/+/521785
    TryBot-Result: Gopher Robot <gobot@golang.org>
    Reviewed-by: Cherry Mui <cherryyz@google.com>
    Run-TryBot: David Chase <drchase@google.com>
    Reviewed-by: Meidan Li <limeidan@loongson.cn>
    Reviewed-by: David Chase <drchase@google.com>
    Auto-Submit: David Chase <drchase@google.com>

--- src/internal/bytealg/indexbyte_loong64.s
+++ src/internal/bytealg/indexbyte_loong64.s
@@ -5,11 +5,18 @@
 #include "go_asm.h"
 #include "textflag.h"
 
-TEXT ·IndexByte(SB),NOSPLIT,$0-40
+TEXT ·IndexByte<ABIInternal>(SB),NOSPLIT,$0-40
+#ifndef GOEXPERIMENT_regabiargs
 	MOVV	b_base+0(FP), R4
 	MOVV	b_len+8(FP), R5
-	MOVBU	c+24(FP), R6	// byte to find
-	MOVV	R4, R7		// store base for later
+	MOVBU	c+24(FP), R7	// byte to find
+#endif
+	// R4 = b_base
+	// R5 = b_len
+	// R6 = b_cap (unused)
+	// R7 = byte to find
+	AND	$0xff, R7
+	MOVV	R4, R6		// store base for later
 	ADDV	R4, R5		// end
 	ADDV	$-1, R4
 
@@ -18,21 +25,30 @@ loop:
 	ADDV	$1, R4
 	BEQ	R4, R5, notfound
 	MOVBU	(R4), R8
-	BNE	R6, R8, loop
+	BNE	R7, R8, loop
 
-	SUBV	R7, R4		// remove base
+	SUBV	R6, R4		// remove base
+#ifndef GOEXPERIMENT_regabiargs
 	MOVV	R4, ret+32(FP)
+#endif
 	RET
 
 notfound:
 	MOVV	$-1, R4
+#ifndef GOEXPERIMENT_regabiargs
 	MOVV	R4, ret+32(FP)
+#endif
 	RET
 
-TEXT ·IndexByteString(SB),NOSPLIT,$0-32
+TEXT ·IndexByteString<ABIInternal>(SB),NOSPLIT,$0-32
+#ifndef GOEXPERIMENT_regabiargs
 	MOVV	s_base+0(FP), R4
 	MOVV	s_len+8(FP), R5
 	MOVBU	c+16(FP), R6	// byte to find
+#endif
+	// R4 = s_base
+	// R5 = s_len
+	// R6 = byte to find
 	MOVV	R4, R7		// store base for later
 	ADDV	R4, R5		// end
 	ADDV	$-1, R4
@@ -45,10 +61,14 @@ loop:
 	BNE	R6, R8, loop
 
 	SUBV	R7, R4		// remove base
+#ifndef GOEXPERIMENT_regabiargs
 	MOVV	R4, ret+24(FP)
+#endif
 	RET
 
 notfound:
 	MOVV	$-1, R4
+#ifndef GOEXPERIMENT_regabiargs
 	MOVV	R4, ret+24(FP)
+#endif
 	RET
