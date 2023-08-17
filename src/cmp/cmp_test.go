90d2c4c9d2ebaa29ec4fa0c0c187b6c29cf09cf9
Carl Johnson <me@carlmjohnson.net>
2023-08-17 12:25:32 -0400

    Example with cmp.Compare

--- src/cmp/cmp_test.go
+++ src/cmp/cmp_test.go
@@ -9,6 +9,7 @@ import (
 	"fmt"
 	"math"
 	"sort"
+	"slices"
 	"testing"
 )
 
@@ -128,4 +129,38 @@ func ExampleOr() {
 	// default
 	// some text
 	// some text
+}
+
+func ExampleOr_sort() {
+	type Order struct {
+		Product string
+		Customer string
+		Price float64
+	}
+	orders := []Order{
+		{"foo", "alice", 1.00},
+		{"bar", "bob", 3.00},
+		{"baz", "carol", 4.00},
+		{"foo", "alice", 2.00},
+		{"bar", "carol", 1.00},
+		{"foo", "bob", 4.00},
+	}
+	slices.SortFunc(orders, func(a, b Order) int {
+		return cmp.Or(
+			cmp.Compare(a.Customer, b.Customer),
+			cmp.Compare(a.Product, b.Product),
+			cmp.Compare(a.Price, b.Price),
+		)
+	})
+	for _, order := range orders {
+		fmt.Printf("%s %s %.2f\n", order.Product, order.Customer, order.Price)
+	}
+
+	// Output:
+	// foo alice 1.00
+	// foo alice 2.00
+	// bar bob 3.00
+	// foo bob 4.00
+	// bar carol 1.00
+	// baz carol 4.00
 }
