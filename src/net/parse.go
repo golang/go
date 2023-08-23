a97f71578fe326629b6cdeaabe6eb0286ccc5c74
Tobias Klauser <tklauser@distanz.ch>
2023-08-23 10:54:37 +0200

    net: remove unused func appendHex
    It's unused since CL 463987.
    
    Change-Id: Ic28fd3b4a613cd7b43f817118841d40e3005a5fc
    Reviewed-on: https://go-review.googlesource.com/c/go/+/522135
    Auto-Submit: Tobias Klauser <tobias.klauser@gmail.com>
    Run-TryBot: Tobias Klauser <tobias.klauser@gmail.com>
    TryBot-Result: Gopher Robot <gobot@golang.org>
    Reviewed-by: Ian Lance Taylor <iant@google.com>
    Reviewed-by: Damien Neil <dneil@google.com>

--- src/net/parse.go
+++ src/net/parse.go
@@ -180,20 +180,6 @@ func xtoi2(s string, e byte) (byte, bool) {
 	return byte(n), ok && ei == 2
 }
 
-// Convert i to a hexadecimal string. Leading zeros are not printed.
-func appendHex(dst []byte, i uint32) []byte {
-	if i == 0 {
-		return append(dst, '0')
-	}
-	for j := 7; j >= 0; j-- {
-		v := i >> uint(j*4)
-		if v > 0 {
-			dst = append(dst, hexDigit[v&0xf])
-		}
-	}
-	return dst
-}
-
 // Number of occurrences of b in s.
 func count(s string, b byte) int {
 	n := 0
