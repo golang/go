ec711aaaaa31b551a47b3677d05014e8aa60f9ea
Guoqi Chen <chenguoqi@loongson.cn>
2023-08-30 17:49:55 +0800

    all: delete loong64 non-register ABI fallback path
    Change-Id: If1d3eba9a922ac6f9d78301bb8f07e445c712899
    Reviewed-on: https://go-review.googlesource.com/c/go/+/525576
    Run-TryBot: Cherry Mui <cherryyz@google.com>
    Reviewed-by: David Chase <drchase@google.com>
    Reviewed-by: sophie zhao <zhaoxiaolin@loongson.cn>
    LUCI-TryBot-Result: Go LUCI <golang-scoped@luci-project-accounts.iam.gserviceaccount.com>
    Reviewed-by: Cherry Mui <cherryyz@google.com>
    TryBot-Result: Gopher Robot <gobot@golang.org>
    Reviewed-by: Meidan Li <limeidan@loongson.cn>
    Commit-Queue: abner chenc <chenguoqi@loongson.cn>
    Run-TryBot: abner chenc <chenguoqi@loongson.cn>

--- src/internal/bytealg/equal_loong64.s
+++ src/internal/bytealg/equal_loong64.s
@@ -9,20 +9,12 @@
 
 // memequal(a, b unsafe.Pointer, size uintptr) bool
 TEXT runtime·memequal<ABIInternal>(SB),NOSPLIT|NOFRAME,$0-25
-#ifndef GOEXPERIMENT_regabiargs
-	MOVV	a+0(FP), R4
-	MOVV	b+8(FP), R5
-	MOVV	size+16(FP), R6
-#endif
 	BEQ	R4, R5, eq
 	ADDV	R4, R6, R7
 	PCALIGN	$16
 loop:
 	BNE	R4, R7, test
 	MOVV	$1, R4
-#ifndef GOEXPERIMENT_regabiargs
-	MOVB	R4, ret+24(FP)
-#endif
 	RET
 test:
 	MOVBU	(R4), R9
@@ -32,23 +24,13 @@ test:
 	BEQ	R9, R10, loop
 
 	MOVB    R0, R4
-#ifndef GOEXPERIMENT_regabiargs
-	MOVB	R0, ret+24(FP)
-#endif
 	RET
 eq:
 	MOVV	$1, R4
-#ifndef GOEXPERIMENT_regabiargs
-	MOVB	R4, ret+24(FP)
-#endif
 	RET
 
 // memequal_varlen(a, b unsafe.Pointer) bool
 TEXT runtime·memequal_varlen<ABIInternal>(SB),NOSPLIT,$40-17
-#ifndef GOEXPERIMENT_regabiargs
-	MOVV	a+0(FP), R4
-	MOVV	b+8(FP), R5
-#endif
 	BEQ	R4, R5, eq
 	MOVV	8(REGCTXT), R6    // compiler stores size at offset 8 in the closure
 	MOVV	R4, 8(R3)
@@ -56,13 +38,7 @@ TEXT runtime·memequal_varlen<ABIInternal>(SB),NOSPLIT,$40-17
 	MOVV	R6, 24(R3)
 	JAL	runtime·memequal(SB)
 	MOVBU	32(R3), R4
-#ifndef GOEXPERIMENT_regabiargs
-	MOVB	R4, ret+16(FP)
-#endif
 	RET
 eq:
 	MOVV	$1, R4
-#ifndef GOEXPERIMENT_regabiargs
-	MOVB	R4, ret+16(FP)
-#endif
 	RET
