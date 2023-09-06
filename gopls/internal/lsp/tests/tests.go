b0cdf010371e2581ac99bdfc23bd68e64b09d1a8
Rob Findley <rfindley@google.com>
2023-09-06 15:53:35 -0400

    gopls/internal/lsp/tests: eliminate the go1.18 summary file
    We can now assume a Go version >= 1.18.
    
    Change-Id: I1faa4f9051f55a62fb2f4971f2006ca8f2dec494
    Reviewed-on: https://go-review.googlesource.com/c/tools/+/526416
    Reviewed-by: Alan Donovan <adonovan@google.com>
    LUCI-TryBot-Result: Go LUCI <golang-scoped@luci-project-accounts.iam.gserviceaccount.com>
    Auto-Submit: Robert Findley <rfindley@google.com>

--- gopls/internal/lsp/tests/tests.go
+++ gopls/internal/lsp/tests/tests.go
@@ -56,8 +56,6 @@ var summaryFile = "summary.txt"
 func init() {
 	if testenv.Go1Point() >= 21 {
 		summaryFile = "summary_go1.21.txt"
-	} else if testenv.Go1Point() >= 18 {
-		summaryFile = "summary_go1.18.txt"
 	}
 }
 
