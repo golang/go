633026453c3841771a740904bed4fc34dd7473e1
Jes Cok <xigua67damn@gmail.com>
2023-09-12 13:42:14 +0800

    go/doc/comment: use sort.Find in func isStdPkg
    sort.Find was added in 1.19, and currently the bootstrap toolchain
    is 1.20, use sort.Find to simplify code.

--- src/go/doc/comment/parse.go
+++ src/go/doc/comment/parse.go
@@ -260,10 +260,10 @@ func (d *parseDoc) lookupPkg(pkg string) (importPath string, ok bool) {
 }
 
 func isStdPkg(path string) bool {
-	// TODO(rsc): Use sort.Find once we don't have to worry about
-	// copying this code into older Go environments.
-	i := sort.Search(len(stdPkgs), func(i int) bool { return stdPkgs[i] >= path })
-	return i < len(stdPkgs) && stdPkgs[i] == path
+	_, ok := sort.Find(len(stdPkgs), func(i int) int {
+		return strings.Compare(path, stdPkgs[i])
+	})
+	return ok
 }
 
 // DefaultLookupPackage is the default package lookup
