da13da1fceff5439af70571826e2dd35925d86a5
Michael Pratt <mpratt@google.com>
2023-09-12 14:43:38 -0400

    cmd/compile/internal/pgo: remove stale comment
    targetDeclFuncs was removed before CL 492436 was ever merged.
    
    Change-Id: I1aafc28446ad23f63bcafc144b91f924dacd676e
    Reviewed-on: https://go-review.googlesource.com/c/go/+/528176
    Reviewed-by: Matthew Dempsky <mdempsky@google.com>
    LUCI-TryBot-Result: Go LUCI <golang-scoped@luci-project-accounts.iam.gserviceaccount.com>
    Auto-Submit: Michael Pratt <mpratt@google.com>

--- src/cmd/compile/internal/pgo/irgraph.go
+++ src/cmd/compile/internal/pgo/irgraph.go
@@ -352,12 +352,6 @@ func (p *Profile) addIREdge(callerNode *IRNode, callerName string, call ir.Node,
 // addIndirectEdges adds indirect call edges found in the profile to the graph,
 // to be used for devirtualization.
 //
-// targetDeclFuncs is the set of functions in typecheck.Target.Decls. Only
-// edges from these functions will be added.
-//
-// Devirtualization is only applied to typecheck.Target.Decls functions, so there
-// is no need to add edges from other functions.
-//
 // N.B. despite the name, addIndirectEdges will add any edges discovered via
 // the profile. We don't know for sure that they are indirect, but assume they
 // are since direct calls would already be added. (e.g., direct calls that have
