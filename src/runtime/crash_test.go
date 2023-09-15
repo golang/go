d516aa6bdceb4d0a0bf4f714ef4a432a3896c6d7
Cherry Mui <cherryyz@google.com>
2023-09-15 19:57:36 -0400

    runtime: skip TestG0StackOverflow on iOS
    Apparently it falls into infinite printing loop. Skipping for now.
    Will investigate.
    
    For #62671.
    
    Change-Id: I5be8e8c1d0f4f2eaa231d6bcdfe13e7f08c28b98
    Reviewed-on: https://go-review.googlesource.com/c/go/+/528403
    Reviewed-by: Bryan Mills <bcmills@google.com>
    LUCI-TryBot-Result: Go LUCI <golang-scoped@luci-project-accounts.iam.gserviceaccount.com>
    Run-TryBot: Bryan Mills <bcmills@google.com>

--- src/runtime/crash_test.go
+++ src/runtime/crash_test.go
@@ -792,6 +792,10 @@ func TestRuntimePanic(t *testing.T) {
 func TestG0StackOverflow(t *testing.T) {
 	testenv.MustHaveExec(t)
 
+	if runtime.GOOS == "ios" {
+		testenv.SkipFlaky(t, 62671)
+	}
+
 	if os.Getenv("TEST_G0_STACK_OVERFLOW") != "1" {
 		cmd := testenv.CleanCmdEnv(exec.Command(os.Args[0], "-test.run=^TestG0StackOverflow$", "-test.v"))
 		cmd.Env = append(cmd.Env, "TEST_G0_STACK_OVERFLOW=1")
