8267665a0348faa0a10ac63b18909a1b13f9971d
Mauri de Souza Meneguzzo <mauri870@gmail.com>
2023-09-15 23:22:23 -0300

    runtime/internal/atomic: add arm/arm64 operators for And/Or
    For #61395

--- src/runtime/internal/atomic/atomic_andor_test.go
+++ src/runtime/internal/atomic/atomic_andor_test.go
@@ -1,4 +1,4 @@
-//go:build 386 || amd64 || ppc64 || ppc64le || riscv64 || wasm
+//go:build 386 || amd64 || arm || arm64 || ppc64 || ppc64le || riscv64 || wasm
 
 //
 // Copyright 2023 The Go Authors. All rights reserved.
