2ddfc04d12da7028334ab4f8effbc3a78b92d9d2
Ian Lance Taylor <iant@golang.org>
2023-09-20 16:16:29 -0700

    [release-branch.go1.21] cmd/compile: use absolute file name in isCgo check
    For #23672
    Updates #63211
    Fixes #63214
    Fixes CVE-2023-39323
    
    Change-Id: I4586a69e1b2560036afec29d53e53cf25e6c7352
    Reviewed-on: https://team-review.git.corp.google.com/c/golang/go-private/+/2032884
    Reviewed-by: Matthew Dempsky <mdempsky@google.com>
    Reviewed-by: Roland Shoemaker <bracewell@google.com>
    (cherry picked from commit 9b19e751918dd218035811b1ef83a8c2693b864a)
    Reviewed-on: https://team-review.git.corp.google.com/c/golang/go-private/+/2037958
    Reviewed-by: Damien Neil <dneil@google.com>
    Reviewed-by: Tatiana Bradley <tatianabradley@google.com>
    Run-TryBot: Roland Shoemaker <bracewell@google.com>
    Reviewed-on: https://go-review.googlesource.com/c/go/+/533215
    Reviewed-by: Than McIntosh <thanm@google.com>
    Auto-Submit: Michael Pratt <mpratt@google.com>
    Reviewed-by: Ian Lance Taylor <iant@google.com>
    LUCI-TryBot-Result: Go LUCI <golang-scoped@luci-project-accounts.iam.gserviceaccount.com>

---
+++ src/cmd/cgo/internal/testerrors/testdata/err5.go
@@ -0,0 +1,10 @@
+// Copyright 2023 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+//line /tmp/_cgo_.go:1
+//go:cgo_dynamic_linker "/elf/interp" // ERROR HERE: only allowed in cgo-generated code
+
+func main() {}
