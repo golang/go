80e642cb7a73a7bc1169655bc299abe8629475dc
Carl Johnson <me@carlmjohnson.net>
2023-09-22 21:03:27 +0000

    cmp: add Or
    Fixes #60204
    
    Change-Id: I1234cacf0f25097d034038bcfb33f6630373a057
    GitHub-Last-Rev: e9098ed8b3dd9125661e4340ffe01d846670ba0f
    GitHub-Pull-Request: golang/go#60931
    Reviewed-on: https://go-review.googlesource.com/c/go/+/504883
    Auto-Submit: Ian Lance Taylor <iant@google.com>
    LUCI-TryBot-Result: Go LUCI <golang-scoped@luci-project-accounts.iam.gserviceaccount.com>
    Reviewed-by: Than McIntosh <thanm@google.com>
    Reviewed-by: Ian Lance Taylor <iant@google.com>
    Reviewed-by: qiulaidongfeng <2645477756@qq.com>

--- src/cmp/cmp_test.go
+++ src/cmp/cmp_test.go
@@ -6,8 +6,10 @@ package cmp_test
 
 import (
 	"cmp"
+	"fmt"
 	"math"
 	"sort"
+	"slices"
 	"testing"
 )
 
@@ -92,4 +94,74 @@ func TestSort(t *testing.T) {
 			t.Errorf("Compare sort mismatch at %d in %v", i, input)
 		}
 	}
+}
+
+func TestOr(t *testing.T) {
+	cases := []struct {
+		in   []int
+		want int
+	}{
+		{nil, 0},
+		{[]int{0}, 0},
+		{[]int{1}, 1},
+		{[]int{0, 2}, 2},
+		{[]int{3, 0}, 3},
+		{[]int{4, 5}, 4},
+		{[]int{0, 6, 7}, 6},
+	}
+	for _, tc := range cases {
+		if got := cmp.Or(tc.in...); got != tc.want {
+			t.Errorf("cmp.Or(%v) = %v; want %v", tc.in, got, tc.want)
+		}
+	}
+}
+
+func ExampleOr() {
+	// Suppose we have some user input
+	// that may or may not be an empty string
+	userInput1 := ""
+	userInput2 := "some text"
+
+	fmt.Println(cmp.Or(userInput1, "default"))
+	fmt.Println(cmp.Or(userInput2, "default"))
+	fmt.Println(cmp.Or(userInput1, userInput2, "default"))
+	// Output:
+	// default
+	// some text
+	// some text
+}
+
+func ExampleOr_sort() {
+	type Order struct {
+		Product string
+		Customer string
+		Price float64
+	}
+	orders := []Order{
+		{"foo", "alice", 1.00},
+		{"bar", "bob", 3.00},
+		{"baz", "carol", 4.00},
+		{"foo", "alice", 2.00},
+		{"bar", "carol", 1.00},
+		{"foo", "bob", 4.00},
+	}
+	// Sort by customer first, product second, and last by higher price
+	slices.SortFunc(orders, func(a, b Order) int {
+		return cmp.Or(
+			cmp.Compare(a.Customer, b.Customer),
+			cmp.Compare(a.Product, b.Product),
+			cmp.Compare(b.Price, a.Price),
+		)
+	})
+	for _, order := range orders {
+		fmt.Printf("%s %s %.2f\n", order.Product, order.Customer, order.Price)
+	}
+
+	// Output:
+	// foo alice 2.00
+	// foo alice 1.00
+	// bar bob 3.00
+	// foo bob 4.00
+	// bar carol 1.00
+	// baz carol 4.00
 }
