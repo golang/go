84ecf912b8b7e244baec3ccfa719279596c08f45
Mateusz Poliwczak <mpoliwczak34@gmail.com>
2023-09-22 12:52:41 +0200

    detect unknown port on windows properly
    Change-Id: I1a00a7ae81321994f3b98a168f50b4d0469e05fa

--- src/net/lookup_windows.go
+++ src/net/lookup_windows.go
@@ -22,6 +22,7 @@ const cgoAvailable = true
 const (
 	_WSAHOST_NOT_FOUND = syscall.Errno(11001)
 	_WSATRY_AGAIN      = syscall.Errno(11002)
+	_WSATYPE_NOT_FOUND = syscall.Errno(10109)
 )
 
 func winError(call string, err error) error {
@@ -217,12 +218,11 @@ func (r *Resolver) lookupPort(ctx context.Context, network, service string) (int
 		if port, err := lookupPortMap(network, service); err == nil {
 			return port, nil
 		}
-		err := winError("getaddrinfow", e)
-		dnsError := &DNSError{Err: err.Error(), Name: network + "/" + service}
-		if err == errNoSuchHost {
-			dnsError.IsNotFound = true
+		if e == _WSATYPE_NOT_FOUND {
+			return 0, &DNSError{Err: "unknown port", Name: network + "/" + service, IsNotFound: true}
 		}
-		return 0, dnsError
+		err := os.NewSyscallError("getaddrinfow", e)
+		return 0, &DNSError{Err: err.Error(), Name: network + "/" + service}
 	}
 	defer syscall.FreeAddrInfoW(result)
 	if result == nil {
