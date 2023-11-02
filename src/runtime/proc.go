4a28618e46902190f1809efe35ddb022c07a6ab7
Mauri de Souza Meneguzzo <mauri870@gmail.com>
2023-11-02 12:43:28 -0300

    runtime: add crash stack suport for mips64x

--- src/runtime/proc.go
+++ src/runtime/proc.go
@@ -574,10 +574,10 @@ func switchToCrashStack(fn func()) {
 	abort()
 }
 
-const crashStackImplemented = GOARCH == "amd64" || GOARCH == "arm64" || GOARCH == "riscv64"
+const crashStackImplemented = GOARCH == "amd64" || GOARCH == "arm64" || GOARCH == "mips64" || GOARCH == "mips64le" || GOARCH == "riscv64"
 
 //go:noescape
-func switchToCrashStack0(func()) // in assembly
+func switchToCrashStack0(fn func()) // in assembly
 
 func lockedOSThread() bool {
 	gp := getg()
