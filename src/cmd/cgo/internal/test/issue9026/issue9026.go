5447418de3f6aae387a347218e7f97121628abe0
Jes Cok <xigua67damn@gmail.com>
2023-11-12 20:07:02 +0800

    all: add missing copyright header

--- src/cmd/cgo/internal/test/issue9026/issue9026.go
+++ src/cmd/cgo/internal/test/issue9026/issue9026.go
@@ -1,3 +1,7 @@
+// Copyright 2014 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 package issue9026
 
 // This file appears in its own package since the assertion tests the
