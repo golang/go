d961b12be9001cf8dbf8f52847607dbf84d94f8d
Jes Cok <xigua67damn@gmail.com>
2023-11-24 10:02:34 +0000

    io/fs, os: unify PathError.Path for dirFS.{ReadFile, ReadDir}
    Fixes #64366
    
    Change-Id: Ie78ab2cb9e11c0766665cd37fd7a26d36a1c24fa
    GitHub-Last-Rev: 3cb3bb84a0ccccc88ba30258f52f699ac1364a7b
    GitHub-Pull-Request: golang/go#64372
    Reviewed-on: https://go-review.googlesource.com/c/go/+/544835
    Reviewed-by: Dmitri Shuralyov <dmitshur@google.com>
    Run-TryBot: Jes Cok <xigua67damn@gmail.com>
    LUCI-TryBot-Result: Go LUCI <golang-scoped@luci-project-accounts.iam.gserviceaccount.com>
    Auto-Submit: Bryan Mills <bcmills@google.com>
    TryBot-Result: Gopher Robot <gobot@golang.org>
    Reviewed-by: Bryan Mills <bcmills@google.com>

--- src/os/file.go
+++ src/os/file.go
@@ -690,7 +690,15 @@ func (dir dirFS) ReadFile(name string) ([]byte, error) {
 	if err != nil {
 		return nil, &PathError{Op: "readfile", Path: name, Err: err}
 	}
-	return ReadFile(fullname)
+	b, err := ReadFile(fullname)
+	if err != nil {
+		if e, ok := err.(*PathError); ok {
+			// See comment in dirFS.Open.
+			e.Path = name
+		}
+		return nil, err
+	}
+	return b, nil
 }
 
 // ReadDir reads the named directory, returning all its directory entries sorted
@@ -700,7 +708,15 @@ func (dir dirFS) ReadDir(name string) ([]DirEntry, error) {
 	if err != nil {
 		return nil, &PathError{Op: "readdir", Path: name, Err: err}
 	}
-	return ReadDir(fullname)
+	entries, err := ReadDir(fullname)
+	if err != nil {
+		if e, ok := err.(*PathError); ok {
+			// See comment in dirFS.Open.
+			e.Path = name
+		}
+		return nil, err
+	}
+	return entries, nil
 }
 
 func (dir dirFS) Stat(name string) (fs.FileInfo, error) {
