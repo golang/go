ca756506916e6cdb83bff1885c232fef52f46fc7
Miki Tebeka <miki@353solutions.com>
2023-11-30 14:44:48 +0200

    Backticks and error example.

--- src/flag/example_flagset_test.go
+++ src/flag/example_flagset_test.go
@@ -7,21 +7,21 @@ package flag_test
 import (
 	"flag"
 	"fmt"
-	"os"
 	"time"
 )
 
 func ExampleFlagSet() {
 	start := func(args []string) {
-		fs := flag.NewFlagSet("start", flag.ExitOnError)
-		addr := fs.String("address", ":8080", "address to listen on")
+		fs := flag.NewFlagSet("start", flag.ContinueOnError)
+		addr := fs.String("address", ":8080", "`address` to listen on")
 		fs.Parse(args)
 		fmt.Printf("starting server on %s\n", *addr)
 	}
 
 	stop := func(args []string) {
-		fs := flag.NewFlagSet("stop", flag.ExitOnError)
-		timeout := fs.Duration("timeout", time.Second, "stop timeout")
+		// On regular program use `flag.ExitOnError`.
+		fs := flag.NewFlagSet("stop", flag.ContinueOnError)
+		timeout := fs.Duration("timeout", time.Second, "stop timeout in `seconds`")
 		fs.Parse(args)
 		fmt.Printf("stopping server (timeout=%v)\n", *timeout)
 	}
@@ -34,15 +34,17 @@ func ExampleFlagSet() {
 		case "stop":
 			stop(subArgs)
 		default:
-			fmt.Fprintf(os.Stderr, "error: unknown command - %q\n", args[1])
-			os.Exit(1)
+			fmt.Printf("error: unknown command - %q\n", args[1])
+			// On regular main print to `os.Stderr` and exit the program with non-zero value.
 		}
 	}
 
 	main([]string{"httpd", "start", "-address", ":9999"})
 	main([]string{"httpd", "stop"})
+	main([]string{"http", "info"})
 
 	// Output:
 	// starting server on :9999
 	// stopping server (timeout=1s)
+	// error: unknown command - "info"
 }
