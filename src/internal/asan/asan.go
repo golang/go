7f7c9eb25d1a810e0f3c4b3497067cc77b73f65f
Mauri de Souza Meneguzzo <mauri870@gmail.com>
2023-12-10 12:34:48 -0300

    internal/asan: add new package
    The internal/asan package contains helper functions for manually
    instrumenting code for the address sanitizer. It reexports the asan
    routines in runtime uncoditionally, making the functions a no-op if the
    build flag "asan" is disabled.
    
    For [reserved]

---
+++ src/internal/asan/asan.go
@@ -0,0 +1,22 @@
+// Copyright 2023 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+//go:build asan
+
+package asan
+
+import (
+	"runtime"
+	"unsafe"
+)
+
+const Enabled = true
+
+func Read(addr unsafe.Pointer, len int) {
+	runtime.ASanRead(addr, len)
+}
+
+func Write(addr unsafe.Pointer, len int) {
+	runtime.ASanWrite(addr, len)
+}
