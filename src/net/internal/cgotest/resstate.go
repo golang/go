4085a4893c427a2138e28fe4f16a646e7f571adb
Russ Cox <rsc@golang.org>
2023-12-12 12:53:32 -0500

    net: move cgo-using darwin test to separate package
    It is a goal / requirement that nothing in the standard library
    on darwin require cgo, and this test-only file makes net use
    cgo on darwin. Move it elsewhere.
    
    Change-Id: I6c11a8391d3913f73ce0098ba63b29adf5864f93
    Reviewed-on: https://go-review.googlesource.com/c/go/+/549255
    Reviewed-by: Mauri de Souza Meneguzzo <mauri870@gmail.com>
    Reviewed-by: Bryan Mills <bcmills@google.com>
    LUCI-TryBot-Result: Go LUCI <golang-scoped@luci-project-accounts.iam.gserviceaccount.com>
    Auto-Submit: Russ Cox <rsc@golang.org>

--- src/net/internal/cgotest/resstate.go
+++ src/net/internal/cgotest/resstate.go
@@ -2,9 +2,9 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-//go:build !netgo && cgo && darwin
+//go:build cgo && darwin
 
-package net
+package cgotest
 
 /*
 #include <resolv.h>
