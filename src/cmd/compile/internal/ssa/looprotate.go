cc41b16a15e7cd0f0e6d5b99917efdbd766d4790
Yi Yang <qingfeng.yy@alibaba-inc.com>
2023-12-27 14:28:01 +0800

    place loop backedge after all body blocks

--- src/cmd/compile/internal/ssa/looprotate.go
+++ src/cmd/compile/internal/ssa/looprotate.go
@@ -1087,6 +1087,22 @@ func (fn *Func) RotateLoop(loop *loop) bool {
 	return true
 }
 
+func moveBlock(slice []*Block, from, to int) []*Block {
+	if from < 0 || to < 0 || from >= len(slice) || to >= len(slice) {
+		return slice
+	}
+
+	elem := slice[from]
+	if from < to {
+		copy(slice[from:], slice[from+1:to+1])
+	} else {
+		copy(slice[to+1:], slice[to:from])
+	}
+
+	slice[to] = elem
+	return slice
+}
+
 // layoutLoop converts loops with a check-loop-condition-at-beginning
 // to loops with a check-loop-condition-at-end by reordering blocks. no
 // CFG changes here. This helps loops avoid extra unnecessary jumps.
@@ -1113,84 +1129,36 @@ func layoutLoop(f *Func) {
 		return
 	}
 
-	idToIdx := f.Cache.allocIntSlice(f.NumBlocks())
-	defer f.Cache.freeIntSlice(idToIdx)
-	for i, b := range f.Blocks {
-		idToIdx[b.ID] = i
-	}
-
-	// Set of blocks we're moving, by ID.
-	move := map[ID]struct{}{}
-
-	// Map from block ID to the moving blocks that should
-	// come right after it.
-	after := map[ID][]*Block{}
-
-	// Check each loop header and decide if we want to move it.
 	for _, loop := range loopnest.loops {
-		b := loop.header
-		var p *Block // b's in-loop predecessor
-		for _, e := range b.Preds {
+		header := loop.header
+		// If loop rotation is already applied, loop latch should be right after
+		// all loop body blocks
+		if header.Kind == BlockPlain && len(header.Succs) == 1 {
+			continue
+		}
+		// Otherwise, place loop header right after all body blocks
+		var latch *Block // b's in-loop predecessor
+		for _, e := range header.Preds {
 			if e.b.Kind != BlockPlain {
 				continue
 			}
 			if loopnest.b2l[e.b.ID] != loop {
 				continue
 			}
-			p = e.b
+			latch = e.b
 		}
-		if p == nil || p == b {
+		if latch == nil || latch == header {
 			continue
 		}
-		after[p.ID] = []*Block{b}
-		for {
-			nextIdx := idToIdx[b.ID] + 1
-			if nextIdx >= len(f.Blocks) { // reached end of function (maybe impossible?)
-				break
+		iheader, ilatch := 0, 0
+		for ib, b := range f.Blocks {
+			if b == header {
+				iheader = ib
+			} else if b == latch {
+				ilatch = ib
 			}
-			nextb := f.Blocks[nextIdx]
-			if nextb == p { // original loop predecessor is next
-				break
-			}
-			if loopnest.b2l[nextb.ID] == loop {
-				after[p.ID] = append(after[p.ID], nextb)
-			}
-			b = nextb
 		}
-		// Swap b and p so that we'll handle p before b when moving blocks.
-		f.Blocks[idToIdx[loop.header.ID]] = p
-		f.Blocks[idToIdx[p.ID]] = loop.header
-		idToIdx[loop.header.ID], idToIdx[p.ID] = idToIdx[p.ID], idToIdx[loop.header.ID]
-
-		// Place b after p.
-		for _, b := range after[p.ID] {
-			move[b.ID] = struct{}{}
-		}
-	}
-
-	// Move blocks to their destinations in a single pass.
-	// We rely here on the fact that loop headers must come
-	// before the rest of the loop.  And that relies on the
-	// fact that we only identify reducible loops.
-	j := 0
-	// Some blocks that are not part of a loop may be placed
-	// between loop blocks. In order to avoid these blocks from
-	// being overwritten, use a temporary slice.
-	oldOrder := f.Cache.allocBlockSlice(len(f.Blocks))
-	defer f.Cache.freeBlockSlice(oldOrder)
-	copy(oldOrder, f.Blocks)
-	for _, b := range oldOrder {
-		if _, ok := move[b.ID]; ok {
-			continue
-		}
-		f.Blocks[j] = b
-		j++
-		for _, a := range after[b.ID] {
-			f.Blocks[j] = a
-			j++
-		}
-	}
-	if j != len(oldOrder) {
-		f.Fatalf("bad reordering in looprotate")
+		// Reordering the loop blocks from [header,body,latch] to [latch,body,header]
+		f.Blocks = moveBlock(f.Blocks, iheader, ilatch)
 	}
 }
