4a632b11d44e455f2081991f45b33d5cfdcc2b10
pgxiaolianzi <gnnu_d13@163.com>
2024-01-04 20:16:01 +0800

    cmp: add byte and rune for type Ordered
    byte and rune type supports the operators < <= >= >, so is an ordered type

--- src/cmp/cmp.go
+++ src/cmp/cmp.go
@@ -19,6 +19,7 @@ type Ordered interface {
 	~int | ~int8 | ~int16 | ~int32 | ~int64 |
 		~uint | ~uint8 | ~uint16 | ~uint32 | ~uint64 | ~uintptr |
 		~float32 | ~float64 |
+		~byte | ~rune |
 		~string
 }
 
