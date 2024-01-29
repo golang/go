84da7403372e6dca69392ff01e821c821429bcd0
Mauri de Souza Meneguzzo <mauri870@gmail.com>
2024-01-29 18:14:26 -0300

    add linkname to runtime funcs, fix package docs

--- src/internal/msan/doc.go
+++ src/internal/msan/doc.go
@@ -1,12 +1,9 @@
-// Copyright 2023 The Go Authors. All rights reserved.
+// Copyright 2024 The Go Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-/*
-Package msan contains helper functions for manually instrumenting code
-for the memory sanitizer.
-
-This package exports the private msan routines in runtime unconditionally
-but without the "msan" build tag they are no-ops.
-*/
+// Package msan contains helper functions for manually instrumenting code
+// for the memory sanitizer.
+// This package exports the private msan routines in runtime unconditionally
+// but without the "msan" build tag they are no-ops.
 package msan
