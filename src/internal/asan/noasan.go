db99b9b17c124ccd18766999d3f1302e5e878f38
Mauri de Souza Meneguzzo <mauri870@gmail.com>
2024-02-07 22:51:00 +0000

    internal/asan: add new package
    The internal/asan package contains helper functions for manually
    instrumenting code for the address sanitizer. It reexports the asan
    routines in runtime unconditionally, making the functions a no-op if the
    build flag "asan" is not present.
    
    For #64611
    
    Change-Id: Ie79e698aea7a6d969afd2a5f008c084c9545b1a5
    GitHub-Last-Rev: e658670c146adb5a5496afe4a2425dd5291fd7ac
    GitHub-Pull-Request: golang/go#64635
    Reviewed-on: https://go-review.googlesource.com/c/go/+/548695
    Reviewed-by: Austin Clements <austin@google.com>
    Reviewed-by: Cherry Mui <cherryyz@google.com>
    LUCI-TryBot-Result: Go LUCI <golang-scoped@luci-project-accounts.iam.gserviceaccount.com>

---
+++ src/internal/asan/noasan.go
@@ -0,0 +1,19 @@
+// Copyright 2024 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+//go:build !asan
+
+package asan
+
+import (
+	"unsafe"
+)
+
+const Enabled = false
+
+func Read(addr unsafe.Pointer, len int) {
+}
+
+func Write(addr unsafe.Pointer, len int) {
+}
