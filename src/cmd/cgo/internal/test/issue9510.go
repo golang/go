af3f94ce646201914a62145821630cc232c1d014
Paul E. Murphy <murp@ibm.com>
2024-02-14 16:42:27 -0600

    cmd/cgo/internal/test: disable issue8694 and issue9510 on ppc64x with internal linking
    These link in libgcc functions which require more advanced handling
    of small toc relocations. The internal linker is not capable of
    placing these functions to ensure they are always resolvable.
    
    Change-Id: Idd4a0264bfbbd7016472120dc4bee84814e8c2eb
    Reviewed-on: https://go-review.googlesource.com/c/go/+/564235
    Run-TryBot: Paul Murphy <murp@ibm.com>
    Reviewed-by: Than McIntosh <thanm@google.com>
    Reviewed-by: Lynn Boger <laboger@linux.vnet.ibm.com>
    Reviewed-by: Cherry Mui <cherryyz@google.com>
    TryBot-Result: Gopher Robot <gobot@golang.org>
    LUCI-TryBot-Result: Go LUCI <golang-scoped@luci-project-accounts.iam.gserviceaccount.com>

--- src/cmd/cgo/internal/test/issue9510.go
+++ src/cmd/cgo/internal/test/issue9510.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-//go:build cgo
+//go:build cgo && !((ppc64 || ppc64le) && internal)
 
 // Test that we can link together two different cgo packages that both
 // use the same libgcc function.
