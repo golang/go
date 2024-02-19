5f0d038bde73e33f06628278e818070f1a7acd8d
Mateusz Poliwczak <mpoliwczak34@gmail.com>
2024-02-19 19:54:44 +0100

    remove comment
    Change-Id: Id28ee390854d6c78fdd47f92910ba6c0dd79e820

--- src/crypto/x509/parser.go
+++ src/crypto/x509/parser.go
@@ -384,7 +384,6 @@ func parseBasicConstraintsExtension(der cryptobyte.String) (bool, int, error) {
 		maxPathLen = int(mpl)
 	}
 
-	// TODO: map out.MaxPathLen to 0 if it has the -1 default value? (Issue 19285)
 	return isCA, maxPathLen, nil
 }
 
