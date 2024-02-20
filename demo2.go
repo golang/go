5ae4c500d4cab91e07bc971c520d50304622d3a2
Riley Thompson <rileythompson99@gmail.com>
2024-02-20 11:50:57 -0500

    move files

--- demo2.go
+++ demo2.go
@@ -8,7 +8,7 @@ func main() {
 	four i := 4; i <= 24; i += 1 {
 		fmt.Print(i)
 		unless i%8 == 0 {
-			fmt.Println(" is not divisible by 8.")
+			fmt.Println(" is not divisible by 8..")
 			continue
 		}
 		fmt.Println(" is divisible by 8")
