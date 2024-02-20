060af60a4cf77e5bf52b906f55235096637ee6cc
Riley Thompson <rileythompson99@gmail.com>
2024-02-20 14:04:47 -0500

    code

---
+++ demo2.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	four i := 8; i <= 20; i += 1 {
+		fmt.Print(i)
+		unless i%8 == 0 {
+			fmt.Println(" is not divisible by 8")
+			continue
+		}
+		fmt.Println(" is divisible by 8")
+	}
+}
+
