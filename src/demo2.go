9fdc1ce71e0a8c33f5e038d5f1aaae6d697b35bf
Riley Thompson <rileythompson99@gmail.com>
2024-02-20 11:41:30 -0500

    setup

--- src/demo2.go
+++ src/demo2.go
@@ -5,10 +5,10 @@ import (
 )
 
 func main() {
-	four i := 8; i <= 28; i += 1 {
+	four i := 4; i <= 24; i += 1 {
 		fmt.Print(i)
 		unless i%8 == 0 {
-			fmt.Println(" is not divisible by 8")
+			fmt.Println(" is not divisible by 8.")
 			continue
 		}
 		fmt.Println(" is divisible by 8")
