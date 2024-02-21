e23707b59cc7842867cb6366e33eaf145b861f7c
Robert Griesemer <gri@golang.org>
2024-02-21 12:54:10 -0800

    [release-branch.go1.22] go/types, types2: handle Alias types in substitution
    Fixes #65858.
    For #65778. // for x/tools/cmd/gotype
    
    Change-Id: I67d4644b28e831926fc6c233098aa1755c57162f
    Reviewed-on: https://go-review.googlesource.com/c/go/+/565835
    Auto-Submit: Robert Griesemer <gri@google.com>
    LUCI-TryBot-Result: Go LUCI <golang-scoped@luci-project-accounts.iam.gserviceaccount.com>
    Reviewed-by: Robert Findley <rfindley@google.com>
    Reviewed-by: Robert Griesemer <gri@google.com>
    Reviewed-on: https://go-review.googlesource.com/c/go/+/565840
    Run-TryBot: Robert Griesemer <gri@google.com>
    TryBot-Result: Gopher Robot <gobot@golang.org>

--- src/go/types/subst.go
+++ src/go/types/subst.go
@@ -97,6 +97,18 @@ func (subst *subster) typ(typ Type) Type {
 	case *Basic:
 		// nothing to do
 
+	case *Alias:
+		rhs := subst.typ(t.fromRHS)
+		if rhs != t.fromRHS {
+			// This branch cannot be reached because the RHS of an alias
+			// may only contain type parameters of an enclosing function.
+			// Such function bodies are never "instantiated" and thus
+			// substitution is not called on locally declared alias types.
+			// TODO(gri) adjust once parameterized aliases are supported
+			panic("unreachable for unparameterized aliases")
+			// return subst.check.newAlias(t.obj, rhs)
+		}
+
 	case *Array:
 		elem := subst.typOrNil(t.elem)
 		if elem != t.elem {
