3fefec96adae7e0ea66e2b624d4bef54a3a8b7df
Andy Pan <i@andypan.me>
2024-03-26 07:13:57 +0800

    runtime: fix the failures from longtest builders
    Follow up CL 560155
    
    Change-Id: Id9230d79c296452f3741123c75b45c3d3b1be4f5
    Reviewed-on: https://go-review.googlesource.com/c/go/+/574295
    Reviewed-by: Than McIntosh <thanm@google.com>
    Auto-Submit: Than McIntosh <thanm@google.com>
    Reviewed-by: Keith Randall <khr@google.com>
    LUCI-TryBot-Result: Go LUCI <golang-scoped@luci-project-accounts.iam.gserviceaccount.com>

--- src/cmd/internal/objabi/pkgspecial.go
+++ src/cmd/internal/objabi/pkgspecial.go
@@ -73,7 +73,7 @@ var extraNoInstrumentPkgs = []string{
 	"-internal/bytealg",
 }
 
-var noRaceFuncPkgs = []string{"sync", "sync/atomic"}
+var noRaceFuncPkgs = []string{"sync", "sync/atomic", "internal/runtime/atomic"}
 
 var allowAsmABIPkgs = []string{
 	"runtime",
