45703b50a6ae557ebe9bd7b1bea8e49d66fc97ee
Tobias Klauser <tklauser@distanz.ch>
2024-04-08 11:30:12 +0200

    internal/syscall/unix: implement Eaccess on openbsd
    Like on other BSDs, use faccessat(AT_FDCWD, path, mode, AT_EACCESS)
    
    Change-Id: I80f8d327dd152576165b9206e32dfb749b41d187
    Reviewed-on: https://go-review.googlesource.com/c/go/+/538836
    Reviewed-by: Carlos Amedee <carlos@golang.org>
    TryBot-Result: Gopher Robot <gobot@golang.org>
    Auto-Submit: Tobias Klauser <tobias.klauser@gmail.com>
    Reviewed-by: Ian Lance Taylor <iant@google.com>
    LUCI-TryBot-Result: Go LUCI <golang-scoped@luci-project-accounts.iam.gserviceaccount.com>
    Run-TryBot: Tobias Klauser <tobias.klauser@gmail.com>

--- src/internal/syscall/unix/at_sysnum_openbsd.go
+++ src/internal/syscall/unix/at_sysnum_openbsd.go
@@ -10,7 +10,11 @@ const unlinkatTrap uintptr = syscall.SYS_UNLINKAT
 const openatTrap uintptr = syscall.SYS_OPENAT
 const fstatatTrap uintptr = syscall.SYS_FSTATAT
 
-const AT_REMOVEDIR = 0x08
-const AT_SYMLINK_NOFOLLOW = 0x02
+const (
+	AT_EACCESS          = 0x1
+	AT_FDCWD            = -0x64
+	AT_REMOVEDIR        = 0x08
+	AT_SYMLINK_NOFOLLOW = 0x02
 
-const UTIME_OMIT = -0x1
+	UTIME_OMIT = -0x1
+)
