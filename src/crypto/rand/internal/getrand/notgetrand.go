7ceb961d5c3367c95482ba9437ebbe5ba7261676
Mateusz Poliwczak <mpoliwczak34@gmail.com>
2024-04-12 21:17:46 +0200

    revert reverted :)
    Change-Id: I250cfc50b9d26aa6f69a82553f21ce3403d8f520

---
+++ src/crypto/rand/internal/getrand/notgetrand.go
@@ -0,0 +1,12 @@
+//go:build !(linux || dragonfly || freebsd || illumos || solaris || darwin || openbsd || netbsd || (js && wasm) || wasip1 || windows)
+
+// Copyright 2024 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+package getrand
+
+import "errors"
+
+func GetRandom(out []byte) error {
+	return errors.ErrUnsupported
+}
