0fc9c3cb9b505f2c23488ddf53139768f8617cb1
Mateusz Poliwczak <mpoliwczak34@gmail.com>
2024-04-12 21:08:03 +0200

    simplify
    Change-Id: Iaa1701b1624f6ce174d76b166b285b3e37f07a28

--- src/crypto/rand/internal/getrand/getrand.go
+++ src/crypto/rand/internal/getrand/getrand.go
@@ -2,8 +2,6 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-//go:build linux || dragonfly || freebsd || illumos || solaris || darwin || openbsd || netbsd || (js && wasm) || wasip1 || windows
-
 package getrand
 
 import "math"
