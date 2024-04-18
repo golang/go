3f400c42f939cd6c0c7749ca4f81d272f5afd726
doujiang24 <doujiang24@gmail.com>
2024-04-18 09:23:30 +0800

    Revert "cmd/cgo: disable #cgo noescape/nocallback until Go 1.23"
    This reverts commit 607e020150f1bba84beec11aceadbc42abf33986.
    
    Reason for revert: Go1.22 is released.
    
    It's aggressive to introdcue #cgo noescape/nocallback in Go1.22, as in #63739
    And it won't be a problem again while upgrading from Go1.22 to Go1.23.
    
    fix #56378
    
    Signed-off-by: doujiang24 <doujiang24@gmail.com>

--- src/runtime/testdata/testprogcgo/cgonoescape.go
+++ src/runtime/testdata/testprogcgo/cgonoescape.go
@@ -13,7 +13,7 @@ package main
 // 2. less than 100 new allocated heap objects after invoking withoutNoEscape 100 times.
 
 /*
-// TODO(#56378): #cgo noescape runCWithNoEscape
+#cgo noescape runCWithNoEscape
 
 void runCWithNoEscape(void *p) {
 }
