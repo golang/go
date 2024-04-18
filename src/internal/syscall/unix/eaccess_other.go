974b2011ca2a74ca1137558771b428bddb2e7df3
Tobias Klauser <tklauser@distanz.ch>
2024-04-18 09:39:31 +0200

    internal/syscall/unix: implement Eaccess on darwin
    Like on other Unix-like platforms, use faccessat(AT_FDCWD, path, mode,
    AT_EACCESS).
    
    Change-Id: I6e04b4244f8e90bd3b35b1b8a9ca38fe845b34ae
    Reviewed-on: https://go-review.googlesource.com/c/go/+/579976
    Auto-Submit: Tobias Klauser <tobias.klauser@gmail.com>
    LUCI-TryBot-Result: Go LUCI <golang-scoped@luci-project-accounts.iam.gserviceaccount.com>
    Reviewed-by: Cherry Mui <cherryyz@google.com>
    Reviewed-by: Ian Lance Taylor <iant@google.com>

--- src/internal/syscall/unix/eaccess_other.go
+++ src/internal/syscall/unix/eaccess_other.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-//go:build unix && !dragonfly && !freebsd && !linux && !openbsd && !netbsd
+//go:build unix && !darwin && !dragonfly && !freebsd && !linux && !openbsd && !netbsd
 
 package unix
 
