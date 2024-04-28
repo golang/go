e7b06aaa35812204ccd646847bda201086782553
qiulaidongfeng <2645477756@qq.com>
2024-04-28 10:42:26 +0800

    n
    Change-Id: I47cfa3453a897d3fb387c7f0897c6312ff88032a

--- src/reflect/iter_test.go
+++ src/reflect/iter_test.go
@@ -4,11 +4,12 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-package reflect
+package reflect_test
 
 import (
+	"iter"
 	"maps"
-	"std/iter"
+	. "reflect"
 	"testing"
 )
 
