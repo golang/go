d82fe3043379ca9794d5cb9196f08246be2314e8
qiulaidongfeng <2645477756@qq.com>
2024-04-29 06:37:12 +0800

    n
    Change-Id: I3d1f59eb50c754715d333526b84f7cebee65476a

--- src/os/exec/exec.go
+++ src/os/exec/exec.go
@@ -333,9 +333,9 @@ type Cmd struct {
 	// and https://go.dev/issue/43724 for more context.
 	lookPathErr error
 
-	// callLookExtensions indicate whether
+	// calledLookExtensions indicates whether
 	// lookExtensions has been called in Cmd.Command.
-	callLookExtensions bool
+	calledLookExtensions bool
 }
 
 // A ctxResult reports the result of watching the Context associated with a
@@ -440,7 +440,7 @@ func Command(name string, arg ...string) *Cmd {
 		// Note that we cannot add an extension here for relative paths, because
 		// cmd.Dir may be set after we return from this function and that may cause
 		// the command to resolve to a different extension.
-		cmd.callLookExtensions = true
+		cmd.calledLookExtensions = true
 		lp, err := lookExtensions(name, "")
 		if lp != "" {
 			cmd.Path = lp
@@ -646,7 +646,7 @@ func (c *Cmd) Start() error {
 		return c.Err
 	}
 	lp := c.Path
-	if runtime.GOOS == "windows" && !c.callLookExtensions {
+	if runtime.GOOS == "windows" && !c.calledLookExtensions {
 		// If c.Path is relative, we had to wait until now
 		// to resolve it in case c.Dir was changed.
 		// (If it is absolute, we already resolved its extension in Command
