44df840589e7b88cd56e7ac45d939c238cf55aef
qiulaidongfeng <2645477756@qq.com>
2024-04-29 06:39:22 +0800

    n
    Change-Id: I06043a5b2141aa392c2993f85161d35b7c6fe11e

--- src/reflect/iter_test.go
+++ src/reflect/iter_test.go
@@ -1,8 +1,8 @@
-//go:build goexperiment.rangefunc
-
 // Copyright 2024 The Go Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
+
+//go:build goexperiment.rangefunc
 
 package reflect_test
 
