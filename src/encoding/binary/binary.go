df97b16c3e9b2ed80b5fc67501fb036e3f21704e
Mateusz Poliwczak <mpoliwczak34@gmail.com>
2024-05-06 18:14:44 +0200

    update
    Change-Id: Ia4e68fac84656613380ccff29ad962b0a8aa0769

--- src/encoding/binary/binary.go
+++ src/encoding/binary/binary.go
@@ -67,9 +67,9 @@ var (
 )
 
 type (
-	littleEndian = binarylite.LittleEndianOrder
-	bigEndian    = binarylite.BigEndianOrder
-	nativeEndian = binarylite.NativeEndianOrder
+	littleEndian struct{ binarylite.LittleEndianOrder }
+	bigEndian    struct{ binarylite.BigEndianOrder }
+	nativeEndian = struct{ binarylite.NativeEndianOrder }
 )
 
 // Read reads structured binary data from r into data.
