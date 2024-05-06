947e43e3711948aa3f4d2bbb55ae51ccf54e7638
Cherry Mui <cherryyz@google.com>
2024-05-06 20:16:53 +0000

    [release-branch.go1.22] Revert "cmd/compile: don't combine loads in generated equality functions"
    This reverts CL 583417.
    
    Reason for revert: release branch is currently frozen.
    
    Change-Id: Ia6223911e0e124f14509c4bf978d7e37688e3d4c
    Reviewed-on: https://go-review.googlesource.com/c/go/+/582960
    Reviewed-by: Dmitri Shuralyov <dmitshur@google.com>
    LUCI-TryBot-Result: Go LUCI <golang-scoped@luci-project-accounts.iam.gserviceaccount.com>

--- src/cmd/compile/internal/compare/compare.go
+++ src/cmd/compile/internal/compare/compare.go
@@ -148,7 +148,7 @@ func calculateCostForType(t *types.Type) int64 {
 		return EqStructCost(t)
 	case types.TSLICE:
 		// Slices are not comparable.
-		base.Fatalf("calculateCostForType: unexpected slice type")
+		base.Fatalf("eqStructFieldCost: unexpected slice type")
 	case types.TARRAY:
 		elemCost := calculateCostForType(t.Elem())
 		cost = t.NumElem() * elemCost
@@ -371,11 +371,6 @@ func eqmem(p, q ir.Node, field int, size int64) ir.Node {
 }
 
 func eqmemfunc(size int64, t *types.Type) (fn *ir.Name, needsize bool) {
-	if !base.Ctxt.Arch.CanMergeLoads && t.Alignment() < int64(base.Ctxt.Arch.Alignment) && t.Alignment() < t.Size() {
-		// We can't use larger comparisons if the value might not be aligned
-		// enough for the larger comparison. See issues 46283 and 67160.
-		size = 0
-	}
 	switch size {
 	case 1, 2, 4, 8, 16:
 		buf := fmt.Sprintf("memequal%d", int(size)*8)
