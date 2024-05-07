292ec6d6ac8e155bb22d68e2672a50f06e69f15c
qiulaidongfeng <2645477756@qq.com>
2024-05-07 21:26:21 +0800

    n
    Change-Id: I61fada81d49e225ccf45b954b33002620d0c5dba

--- src/reflect/iter.go
+++ src/reflect/iter.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-//go:build goexperiment.rangefunc
+//go:build go1.23 || goexperiment.rangefunc
 
 package reflect
 
