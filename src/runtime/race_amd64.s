97cd4bb60e8af45cf88ff8b7f43d1b4981530e4b
Mauri de Souza Meneguzzo <mauri870@gmail.com>
2024-05-13 14:44:35 -0300

    trying to figure out the calling convention

--- src/runtime/race_amd64.s
+++ src/runtime/race_amd64.s
@@ -306,12 +306,13 @@ TEXT	sync∕atomic·AddUintptr(SB), NOSPLIT, $0-24
 // Implementation for platforms that do not have tsan v3 apis.
 #ifdef GOOS_openbsd
 
-// TODO(mauri870): the stack frame size for all of these is wrong!
-TEXT	sync∕atomic·AndInt64(SB), NOSPLIT, $0-24
+TEXT	sync∕atomic·AndInt64(SB), NOSPLIT, $48-24
 	GO_ARGS
 
 	MOVQ 	addr+0(FP), R12
 	MOVQ 	mask+8(FP), R13
+
+loop:
 	MOVQ 	(R12), R15
 	ANDQ 	R15, R13
 
@@ -321,11 +322,13 @@ TEXT	sync∕atomic·AndInt64(SB), NOSPLIT, $0-24
 	MOVQ 	R13, 32(SP)	// new
 
 	MOVQ	g_racectx(R14), RARG0	// goroutine context
-	MOVQ	8(SP), RARG1			// caller pc
-	MOVQ	(SP), RARG2				// pc
-	LEAQ	16(SP), RARG3			// arguments
+	MOVQ	8(SP), RARG1	// caller pc
+	MOVQ	(SP), RARG2	// pc
+	LEAQ	16(SP), RARG3	// arguments
 	MOVQ	$__tsan_go_atomic64_compare_exchange(SB), AX
 	CALL	racecall<>(SB)
+	TESTB 	$0, 40(SP)
+	JEQ 	loop
 
 	MOVQ	R15, ret+16(FP)
 
@@ -334,70 +337,20 @@ TEXT	sync∕atomic·AndInt64(SB), NOSPLIT, $0-24
 TEXT	sync∕atomic·AndInt32(SB), NOSPLIT, $0-20
 	GO_ARGS
 
-	MOVQ 	addr+0(FP), R12
-	MOVL 	mask+8(FP), R13
-	MOVL 	(R12), R15
-	ANDL 	R15, R13
-
-	// args for tsan cas
-	MOVQ 	R12, 16(SP)	// addr
-	MOVL 	R15, 24(SP)	// old
-	MOVL 	R13, 28(SP)	// new
-
-	MOVQ	g_racectx(R14), RARG0	// goroutine context
-	MOVQ	8(SP), RARG1	// caller pc
-	MOVQ	(SP), RARG2	// pc
-	LEAQ	16(SP), RARG3	// arguments
-	MOVQ	$__tsan_go_atomic32_compare_exchange(SB), AX
-	CALL	racecall<>(SB)
-
-	MOVQ	R15, ret+16(FP)
+	UNDEF
 	RET
 
 // Or
 TEXT	sync∕atomic·OrInt32(SB), NOSPLIT, $0-20
 	GO_ARGS
 
-	MOVQ 	addr+0(FP), R12
-	MOVL 	mask+8(FP), R13
-	MOVL 	(R12), R15
-	ORL 	R15, R13
-
-	MOVQ 	R12, 16(SP)	// addr
-	MOVL 	R15, 24(SP)	// old
-	MOVL 	R13, 28(SP)	// new
-
-	MOVQ	g_racectx(R14), RARG0	// goroutine context
-	MOVQ	8(SP), RARG1	// caller pc
-	MOVQ	(SP), RARG2	// pc
-	LEAQ	16(SP), RARG3	// arguments
-	MOVQ	$__tsan_go_atomic32_compare_exchange(SB), AX
-	CALL	racecall<>(SB)
-
-	MOVQ	R15, ret+16(FP)
+	UNDEF
 	RET
 
 TEXT	sync∕atomic·OrInt64(SB), NOSPLIT, $0-24
 	GO_ARGS
 
-	MOVQ 	addr+0(FP), R12
-	MOVQ 	mask+8(FP), R13
-	MOVQ 	(R12), R15
-	ORQ 	R15, R13
-
-	// args for tsan cas
-	MOVQ 	R12, 16(SP)	// addr
-	MOVQ 	R15, 24(SP)	// old
-	MOVQ 	R13, 32(SP)	// new
-
-	MOVQ	g_racectx(R14), RARG0	// goroutine context
-	MOVQ	8(SP), RARG1	// caller pc
-	MOVQ	(SP), RARG2	// pc
-	LEAQ	16(SP), RARG3	// arguments
-	MOVQ	$__tsan_go_atomic64_compare_exchange(SB), AX
-	CALL	racecall<>(SB)
-
-	MOVQ	R15, ret+16(FP)
+	UNDEF
 	RET
 
 #else
