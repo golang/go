fba90c90964703e84bcb43a3567884960043b756
Hiroaki Nakamura <hnakamur@gmail.com>
2024-05-15 17:57:05 +0900

    cmd/go/internal/script: Fix build tags of isETXTBSY
    syscall.ETXTBSY is only meaningful on Unix platforms, not on Windows.
    
    Fixes #67356
    
    Change-Id: I1b13292d821d61d820b396e08be9a4dd1683e843
    Reviewed-on: https://go-review.googlesource.com/c/go/+/585695
    Reviewed-by: Ian Lance Taylor <iant@google.com>
    Auto-Submit: Ian Lance Taylor <iant@google.com>
    LUCI-TryBot-Result: Go LUCI <golang-scoped@luci-project-accounts.iam.gserviceaccount.com>
    Reviewed-by: Michael Matloob <matloob@golang.org>

--- src/cmd/go/internal/script/cmds_unix.go
+++ src/cmd/go/internal/script/cmds_unix.go
@@ -2,10 +2,15 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-//go:build !(unix || windows)
+//go:build unix
 
 package script
 
+import (
+	"errors"
+	"syscall"
+)
+
 func isETXTBSY(err error) bool {
-	return false
+	return errors.Is(err, syscall.ETXTBSY)
 }
