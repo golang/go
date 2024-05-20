d68d485405d8e604921e6d63ed32ae344fd3049e
Robert Griesemer <gri@golang.org>
2024-05-20 15:53:48 -0700

    go/types, types2: use GOEXPERIMENT to enable alias type parameters
    For #46477.
    
    Change-Id: Ia3558f9d2bf43fdd9e3618bd9f800d268e13b367
    Reviewed-on: https://go-review.googlesource.com/c/go/+/586956
    Reviewed-by: Carlos Amedee <carlos@golang.org>
    Reviewed-by: Robert Findley <rfindley@google.com>
    Reviewed-by: Robert Griesemer <gri@google.com>
    LUCI-TryBot-Result: Go LUCI <golang-scoped@luci-project-accounts.iam.gserviceaccount.com>
    Auto-Submit: Robert Griesemer <gri@google.com>

--- src/go/types/decl.go
+++ src/go/types/decl.go
@@ -9,6 +9,7 @@ import (
 	"go/ast"
 	"go/constant"
 	"go/token"
+	"internal/buildcfg"
 	. "internal/types/errors"
 )
 
@@ -597,6 +598,10 @@ func (check *Checker) typeDecl(obj *TypeName, tdecl *ast.TypeSpec, def *TypeName
 
 			// handle type parameters even if not allowed (Alias type is supported)
 			if tparam0 != nil {
+				if !versionErr && !buildcfg.Experiment.AliasTypeParams {
+					check.error(tdecl, UnsupportedFeature, "generic type alias requires GOEXPERIMENT=aliastypeparams")
+					versionErr = true
+				}
 				check.openScope(tdecl, "type parameters")
 				defer check.closeScope()
 				check.collectTypeParams(&alias.tparams, tdecl.TypeParams)
