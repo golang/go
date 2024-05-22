85115da5d3351c4300337579eafe726700f3b587
kenshi kamata <kenshi.kamata@gmail.com>
2024-05-22 17:19:56 +0900

    errors: change interface{} to any in comment
    Change-Id: Ibf28c4b3026019427a2485230e14929ed80a1727
    Reviewed-on: https://go-review.googlesource.com/c/go/+/587255
    Reviewed-by: Ian Lance Taylor <iant@google.com>
    LUCI-TryBot-Result: Go LUCI <golang-scoped@luci-project-accounts.iam.gserviceaccount.com>
    Auto-Submit: Ian Lance Taylor <iant@google.com>
    Reviewed-by: Carlos Amedee <carlos@golang.org>

--- src/errors/wrap.go
+++ src/errors/wrap.go
@@ -85,7 +85,7 @@ func is(err, target error, targetComparable bool) bool {
 // errors, As examines err followed by a depth-first traversal of its children.
 //
 // An error matches target if the error's concrete value is assignable to the value
-// pointed to by target, or if the error has a method As(interface{}) bool such that
+// pointed to by target, or if the error has a method As(any) bool such that
 // As(target) returns true. In the latter case, the As method is responsible for
 // setting target.
 //
