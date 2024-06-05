55424acc646d0f05268f09064e79d62086dac42b
xzhang39 <xzhang39@tesla.com>
2024-06-05 01:15:27 -0700

    Init N of PublicKey to avoid NPE panic

--- src/crypto/rsa/pkcs1v15_test.go
+++ src/crypto/rsa/pkcs1v15_test.go
@@ -16,6 +16,7 @@ import (
 	"encoding/hex"
 	"encoding/pem"
 	"io"
+	"math/big"
 	"testing"
 	"testing/quick"
 )
@@ -217,7 +218,12 @@ func TestSignPKCS1v15WithPrivateKeySizeZero(t *testing.T) {
 	h := sha1.New()
 	h.Write([]byte("key"))
 	digest := h.Sum(nil)
-	_, err := SignPKCS1v15(nil, &PrivateKey{PublicKey: PublicKey{}}, crypto.SHA1, digest)
+	_, err := SignPKCS1v15(nil,
+		&PrivateKey{
+			PublicKey: PublicKey{
+				N: big.NewInt(0),
+			},
+		}, crypto.SHA1, digest)
 	if err == nil {
 		t.Error("expected error but got nil")
 	}
