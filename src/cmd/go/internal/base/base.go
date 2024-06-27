935bf1395c47088d3455722452b28f18084f1450
Mauri de Souza Meneguzzo <mauri870@gmail.com>
2024-06-27 23:59:38 +0000

    cmd/go: place GOROOT/bin at the beginning of PATH in 'go run'
    This causes programs that use 'go' as a subprocess to use the same go
    command as the parent 'go run' command.
    
    Fixes #68005
    
    Change-Id: I937cef474bf038a925bb74fc73e5f377b03e27b7
    GitHub-Last-Rev: 9986537cadb0921763cc1496a63e3806b4aac1ac
    GitHub-Pull-Request: golang/go#68040
    Reviewed-on: https://go-review.googlesource.com/c/go/+/593255
    LUCI-TryBot-Result: Go LUCI <golang-scoped@luci-project-accounts.iam.gserviceaccount.com>
    Reviewed-by: Michael Knyszek <mknyszek@google.com>
    Reviewed-by: Michael Matloob <matloob@golang.org>

--- src/cmd/go/internal/base/base.go
+++ src/cmd/go/internal/base/base.go
@@ -14,6 +14,7 @@ import (
 	"os"
 	"os/exec"
 	"reflect"
+	"slices"
 	"strings"
 	"sync"
 
@@ -211,7 +212,9 @@ func RunStdin(cmdline []string) {
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
-	cmd.Env = cfg.OrigEnv
+	env := slices.Clip(cfg.OrigEnv)
+	env = AppendPATH(env)
+	cmd.Env = env
 	StartSigHandlers()
 	if err := cmd.Run(); err != nil {
 		Errorf("%v", err)
