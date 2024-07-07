409823499288a0b12ae70e71b6e6639f66ba07f9
doujiang24 <doujiang24@gmail.com>
2024-07-07 18:19:43 +0800

    fix windows test
    Signed-off-by: doujiang24 <doujiang24@gmail.com>

--- src/runtime/testdata/testprogcgo/issue63739.go
+++ src/runtime/testdata/testprogcgo/issue63739.go
@@ -2,9 +2,6 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-//go:build !plan9 && !windows
-// +build !plan9,!windows
-
 package main
 
 // This is for issue #56378.
