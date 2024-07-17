90bdd83ac0635baae7ede54e9a89662793d609b8
Zxilly <zxilly@outlook.com>
2024-07-17 11:02:19 +0800

    add test

--- src/bytes/bytes_test.go
+++ src/bytes/bytes_test.go
@@ -2278,3 +2278,12 @@ func TestClone(t *testing.T) {
 		}
 	}
 }
+
+func TestIssue65571(t *testing.T) {
+	b := make([]byte, 1<<31+1)
+	b[1<<31] = 1
+	i := IndexByte(b, 1)
+	if i != 1<<31 {
+		t.Errorf("IndexByte(b, 1) = %d; want %d", i, 1<<31)
+	}
+}
