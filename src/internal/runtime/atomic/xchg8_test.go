841bb62f19b782277b9ce3ce80dab010fcc583c6
Rhys Hiltner <rhys.hiltner@gmail.com>
2024-08-19 13:51:59 -0700

    internal/runtime/atomic: add Xchg8 for amd64
    For #68578
    
    Change-Id: Idecfdbb793f46560dd69287af9170c07cf4ee973
    Reviewed-on: https://go-review.googlesource.com/c/go/+/606900
    Reviewed-by: Keith Randall <khr@google.com>
    Reviewed-by: Mauri de Souza Meneguzzo <mauri870@gmail.com>
    Auto-Submit: Rhys Hiltner <rhys.hiltner@gmail.com>
    LUCI-TryBot-Result: Go LUCI <golang-scoped@luci-project-accounts.iam.gserviceaccount.com>
    Reviewed-by: Keith Randall <khr@golang.org>
    Reviewed-by: Michael Knyszek <mknyszek@google.com>

---
+++ src/internal/runtime/atomic/xchg8_test.go
@@ -0,0 +1,39 @@
+// Copyright 2024 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+//go:build amd64
+
+package atomic_test
+
+import (
+	"internal/runtime/atomic"
+	"testing"
+)
+
+func TestXchg8(t *testing.T) {
+	var a [16]uint8
+	for i := range a {
+		next := uint8(i + 50)
+		a[i] = next
+	}
+	b := a
+
+	// Compare behavior against non-atomic implementation. Expect the operation
+	// to work at any byte offset and to not clobber neighboring values.
+	for i := range a {
+		next := uint8(i + 100)
+		pa := atomic.Xchg8(&a[i], next)
+		pb := b[i]
+		b[i] = next
+		if pa != pb {
+			t.Errorf("atomic.Xchg8(a[%d]); %d != %d", i, pa, pb)
+		}
+		if a != b {
+			t.Errorf("after atomic.Xchg8(a[%d]); %d != %d", i, a, b)
+		}
+		if t.Failed() {
+			break
+		}
+	}
+}
