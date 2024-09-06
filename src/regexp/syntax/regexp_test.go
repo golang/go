a5d106abecec935db0e97c04887518437560f5c7
func25 <phuongle0205@gmail.com>
2024-09-06 10:24:35 +0700

    add benchmark

---
+++ src/regexp/syntax/regexp_test.go
@@ -0,0 +1,30 @@
+// Copyright 2011 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package syntax
+
+import "testing"
+
+var benchmarkTests = []string{
+	`^(.*);$|^;(.*)`,
+	`(foo|bar$)x*`,
+	`[^=,]`,
+	`([^=,]+)=([^=,]+)`,
+	`([^=,]+)=([^=,]+),.*`,
+}
+
+func BenchmarkString(b *testing.B) {
+	for _, tt := range benchmarkTests {
+		re, err := Parse(tt, Perl|DotNL)
+		if err != nil {
+			b.Fatalf("Parse(%#q) = error %v", tt, err)
+		}
+
+		b.Run(tt, func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				_ = re.String()
+			}
+		})
+	}
+}
