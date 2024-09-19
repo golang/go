1596a6c8ec6d4ccb4a219b0c467bed192720de12
Michael Anthony Knyszek <mknyszek@google.com>
2024-09-19 15:43:14 +0000

    unique,internal/concurrent: add some more tests
    One is a test of unsafe.String usage, which was broken before CL 610738
    was merged.
    
    The other is trying to improve coverage of "near collision" scenarios in
    the HashTrieMap where only the last few bits differ. This is intended to
    catch off-by-one errors in iterating down the tree.
    
    For #69534.
    
    Change-Id: I3f302e148e81269a50e93b5edf83cafc2d291098
    Reviewed-on: https://go-review.googlesource.com/c/go/+/614475
    Auto-Submit: Michael Knyszek <mknyszek@google.com>
    LUCI-TryBot-Result: Go LUCI <golang-scoped@luci-project-accounts.iam.gserviceaccount.com>
    Reviewed-by: Ian Lance Taylor <iant@google.com>
    Reviewed-by: Cuong Manh Le <cuong.manhle.vn@gmail.com>

--- src/unique/handle_test.go
+++ src/unique/handle_test.go
@@ -9,6 +9,7 @@ import (
 	"internal/abi"
 	"reflect"
 	"runtime"
+	"strconv"
 	"strings"
 	"testing"
 	"time"
@@ -137,4 +138,27 @@ func TestMakeClonesStrings(t *testing.T) {
 	case <-ran:
 	}
 	runtime.KeepAlive(h)
+}
+
+func TestHandleUnsafeString(t *testing.T) {
+	var testData []string
+	for i := range 1024 {
+		testData = append(testData, strconv.Itoa(i))
+	}
+	var buf []byte
+	var handles []Handle[string]
+	for _, s := range testData {
+		if len(buf) < len(s) {
+			buf = make([]byte, len(s)*2)
+		}
+		copy(buf, s)
+		sbuf := unsafe.String(&buf[0], len(s))
+		handles = append(handles, Make(sbuf))
+	}
+	for i, s := range testData {
+		h := Make(s)
+		if handles[i].Value() != h.Value() {
+			t.Fatal("unsafe string improperly retained internally")
+		}
+	}
 }
