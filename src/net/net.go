de542f1b51e44a71e4258be5d5a5442c58da78cb
Mateusz Poliwczak <mpoliwczak34@gmail.com>
2024-09-27 20:43:58 +0200

    update
    Change-Id: Idef626b47e7213a199c3e74ac43d61e9d5a9f8f5

--- src/net/net.go
+++ src/net/net.go
@@ -68,11 +68,11 @@ GODEBUG environment variable (see package runtime) to go or cgo, as in:
 
 The decision can also be forced while building the Go source tree
 by setting the netgo or netcgo build tag.
-The netgo build flag disables entirely the use of the native (CGO) resolver, so the
-Go resolver is the only one that is supported, whereas with the netcgo build tag
-the native and the pure Go resolver is compiled into the binary, but the native (CGO)
-resolver is preferred over the Go resolver, no matter the system configuration.
-The Go resolver can still be forced with GODEBUG=netdns=go or with the [Resolver.PreferGo] setting.
+The netgo build flag disables entirely the use of the native (CGO) resolver,
+meaning the Go resolver is the only one that can be used.
+With the netcgo build tag the native and the pure Go resolver are compiled into the binary,
+but the native (CGO) resolver is preferred over the Go resolver, no matter the system configuration.
+With netcgo, the Go resolver can still be forced at runtime with GODEBUG=netdns=go.
 
 A numeric netdns setting, as in GODEBUG=netdns=1, causes the resolver
 to print debugging information about its decisions.
