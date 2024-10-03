2a301b12df2573b1f85eece69175c109b462ab95
Mateusz Poliwczak <mpoliwczak34@gmail.com>
2024-10-03 17:43:10 +0200

    go/ast/internal/tests: add missing copyright header
    I forgot to add it in CL 616340
    
    Change-Id: I6c906fa79f2e53ce8bf506d836472b037cb46f85

--- src/go/ast/internal/tests/sortimports_test.go
+++ src/go/ast/internal/tests/sortimports_test.go
@@ -1,3 +1,7 @@
+// Copyright 2024 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 // Tests is a helper package to avoid cyclic dependency between go/ast and go/parser.
 package tests
 
