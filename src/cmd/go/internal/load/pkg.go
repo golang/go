7e906e5b715d183263a05751aaa8c77a997b3a8d
xzhang39 <xzhang39@tesla.com>
2024-10-07 18:10:27 -0700

    Use extractFirstImport and inline code

--- src/cmd/go/internal/load/pkg.go
+++ src/cmd/go/internal/load/pkg.go
@@ -327,11 +327,7 @@ func (p *Package) setLoadPackageDataError(err error, path string, stk *ImportSta
 	// move the modload errors into this package to avoid a package import cycle,
 	// and from having to export an error type for the errors produced in build.
 	if !isMatchErr && (nogoErr != nil || isScanErr) {
-		var pos *token.Position
-		if len(importPos) > 0 {
-			pos = &importPos[0]
-		}
-		stk.Push(ImportInfo{Pkg: path, Pos: pos})
+		stk.Push(ImportInfo{Pkg: path, Pos: extractFirstImport(importPos)})
 		defer stk.Pop()
 	}
 
