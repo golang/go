bfc8c5b85a8b749b6bf3c8004b30acb598cdc962
Robert Griesemer <gri@golang.org>
2024-10-23 09:19:57 -0700

    go/types, types2: simplify cycle reporting code (minor cleanup)
    Change-Id: I49c7107d9624c9a8aa97dcc87e2d4722b9ba0a20
    Reviewed-on: https://go-review.googlesource.com/c/go/+/622055
    LUCI-TryBot-Result: Go LUCI <golang-scoped@luci-project-accounts.iam.gserviceaccount.com>
    Reviewed-by: Tim King <taking@google.com>
    Reviewed-by: Robert Griesemer <gri@google.com>
    Auto-Submit: Robert Griesemer <gri@google.com>

--- src/go/types/initorder.go
+++ src/go/types/initorder.go
@@ -166,10 +166,11 @@ func (check *Checker) reportCycle(cycle []Object) {
 
 	err := check.newError(InvalidInitCycle)
 	err.addf(obj, "initialization cycle for %s", obj.Name())
-	// "cycle[i] refers to cycle[j]" for (i,j) = (0, n-1), (n-1, n-2), ..., (1,0) for len(cycle) = n.
+	// "cycle[i] refers to cycle[j]" for (i,j) = (0,n-1), (n-1,n-2), ..., (1,0) for len(cycle) = n.
 	for j := len(cycle) - 1; j >= 0; j-- {
-		err.addf(obj, "%s refers to %s", obj.Name(), cycle[j].Name())
-		obj = cycle[j]
+		next := cycle[j]
+		err.addf(obj, "%s refers to %s", obj.Name(), next.Name())
+		obj = next
 	}
 	err.report()
 }
