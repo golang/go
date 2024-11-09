4351d946cd9b44e83857f74c2983f59b1da171aa
Jack Christensen <jack@jackchristensen.com>
2024-11-09 09:36:27 -0600

    Rename rowsColumnScanner to rcs

--- src/database/sql/sql.go
+++ src/database/sql/sql.go
@@ -3399,8 +3399,8 @@ func (rs *Rows) Scan(dest ...any) error {
 	for i, sv := range rs.lastcols {
 		err := driver.ErrSkip
 
-		if rowsColumnScanner, ok := rs.rowsi.(driver.RowsColumnScanner); ok {
-			err = rowsColumnScanner.ScanColumn(dest[i], i)
+		if rcs, ok := rs.rowsi.(driver.RowsColumnScanner); ok {
+			err = rcs.ScanColumn(dest[i], i)
 		}
 
 		if err == driver.ErrSkip {
