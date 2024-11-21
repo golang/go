844426104a901b68f3a8c7a7ca96d910b03fb054
Russ Cox <rsc@golang.org>
2024-11-21 08:46:57 -0500

    internal/copyright: add test that copyright notices exist
    We shouldn't spend human code review time checking this.
    Let the computer check.
    
    Change-Id: I6de9d733c128d833b958b0e43a52b564e8f82dd3
    Reviewed-on: https://go-review.googlesource.com/c/go/+/630417
    LUCI-TryBot-Result: Go LUCI <golang-scoped@luci-project-accounts.iam.gserviceaccount.com>
    Reviewed-by: Sam Thanawalla <samthanawalla@google.com>

--- src/internal/runtime/sys/dit_arm64.s
+++ src/internal/runtime/sys/dit_arm64.s
@@ -1,3 +1,7 @@
+// Copyright 2024 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 #include "textflag.h"
 
 TEXT ·EnableDIT(SB),$0-1
