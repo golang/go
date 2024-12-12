957fd30cd0f806eeaf4aaf153d0de5c3316935bc
Max Neverov <neverov.max@gmail.com>
2024-12-12 07:12:06 +0100

    Fix GOCACHE doc comment again.

--- src/cmd/go/alldocs.go
+++ src/cmd/go/alldocs.go
@@ -2331,7 +2331,7 @@
 //	GOBIN
 //		The directory where 'go install' will install a command.
 //	GOCACHE
-//		The absolute path to the directory where the go command will store cached
+//		The directory where the go command will store cached
 //		information for reuse in future builds. Must be an absolute path.
 //	GOCACHEPROG
 //		A command (with optional space-separated flags) that implements an
