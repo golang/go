7b43f98b99007166dee048ea83c5bbb8bca25211
Kroum Tzanev <kpym.tzanev@gmail.com>
2024-12-20 14:08:25 +0000

    fix indentation : spaces to tabs

--- src/text/template/funcs.go
+++ src/text/template/funcs.go
@@ -130,10 +130,10 @@ func goodName(name string) bool {
 	}
 	for i, r := range name {
 		switch {
-        case unicode.IsLetter(r):
+		case unicode.IsLetter(r):
 		case r == '_':
 		case i > 0 && unicode.IsDigit(r):
-        default:
+		default:
 			return false
 		}
 	}
