e5e59bcc69a9cd146002b05afb5956de0c401a6a
Mateusz Poliwczak <mpoliwczak34@gmail.com>
2024-12-23 20:20:56 +0100

    go/types: propagate lstmt in blockBranches properly
    Change-Id: Ibf108a0a7c533260d3264695c2429d6f4b6a75e1

---
+++ src/internal/types/testdata/check/issue70974.go
@@ -0,0 +1,27 @@
+// Copyright 2014 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package p
+
+func _() {
+outer:
+	for {
+		break outer
+	}
+
+	for {
+		continue outer /* ERROR "invalid continue label outer" */
+	}
+}
+
+func _() {
+outer:
+	for {
+		continue outer
+	}
+
+	for {
+		continue outer /* ERROR "invalid continue label outer" */
+	}
+}
