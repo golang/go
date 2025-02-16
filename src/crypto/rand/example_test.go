ef2057888465be3cff7fb60bc6ffe3d7573ec348
Alan Yee <alanyee@users.noreply.github.com>
2025-02-15 18:59:10 -0800

    crypto/rand: add example for Int

--- src/crypto/rand/example_test.go
+++ src/crypto/rand/example_test.go
@@ -4,7 +4,21 @@
 
 package rand_test
 
-import "crypto/rand"
+import (
+	"crypto/rand"
+	"fmt"
+	"math/big"
+)
+
+// ExampleInt prints a single cryptographically secure pseudorandom number between 0 and 99 inclusive.
+func ExampleInt() {
+	a, err := rand.Int(rand.Reader, big.NewInt(100))
+	if err != nil {
+		fmt.Println("error:", err)
+		return
+	}
+	fmt.Println(a.Int64())
+}
 
 func ExampleRead() {
 	// Note that no error handling is necessary, as Read always succeeds.
