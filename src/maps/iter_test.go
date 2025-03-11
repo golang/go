ac846f336e5cf32bf67d9e657940d296bed6a59b
Jes Cok <xigua67damn@gmail.com>
2025-03-12 07:43:21 +0800

    maps test package
    Change-Id: I56b7c2055999b13b791458ffa7e7f581ae8ab41b

--- src/maps/iter_test.go
+++ src/maps/iter_test.go
@@ -2,9 +2,10 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-package maps
+package maps_test
 
 import (
+	. "maps"
 	"slices"
 	"testing"
 )
