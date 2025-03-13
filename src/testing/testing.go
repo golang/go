4a5b4a7063bf6dafc2d1acf76ee2b195d624b46c
suntala <artichaut2023@gmail.com>
2025-03-13 18:31:49 +0100

    Convert indent to string

--- src/testing/testing.go
+++ src/testing/testing.go
@@ -843,7 +843,7 @@ type indenter struct {
 	c *common
 }
 
-var indent = []byte("    ")
+const indent = "    "
 
 func (w indenter) Write(b []byte) (n int, err error) {
 	n = len(b)
@@ -1013,7 +1013,7 @@ func (c *common) log(s string) {
 
 	// Second and subsequent lines are indented 4 spaces. This is in addition to
 	// the indentation provided by outputWriter.
-	s = strings.ReplaceAll(s, "\n", fmt.Sprintf("\n%s", indent))
+	s = strings.ReplaceAll(s, "\n", "\n"+indent)
 	s += "\n"
 
 	// Prefix with the call site. It is located by skipping 3 functions:
@@ -1072,7 +1072,7 @@ func (o *outputWriter) Write(p []byte) (int, error) {
 	}
 
 	for _, line := range lines {
-		o.writeLine(append(indent, line...), p)
+		o.writeLine(append([]byte(indent), line...), p)
 	}
 	return len(p), nil
 }
