1383d63cc788784e13423df9ccbb22a6ff18f23b
suntala <artichaut2023@gmail.com>
2025-03-14 19:31:01 +0100

    Add common method setOutputWriter

--- src/testing/benchmark.go
+++ src/testing/benchmark.go
@@ -675,7 +675,7 @@ func (s *benchState) processBench(b *B) {
 					benchFunc: b.benchFunc,
 					benchTime: b.benchTime,
 				}
-				b.o = b.newOutputWriter()
+				b.setOutputWriter()
 				b.run1()
 			}
 			r := b.doBench()
@@ -752,7 +752,7 @@ func (b *B) Run(name string, f func(b *B)) bool {
 		benchTime:  b.benchTime,
 		bstate:     b.bstate,
 	}
-	sub.o = sub.newOutputWriter()
+	sub.setOutputWriter()
 	if partial {
 		// Partial name match, like -bench=X/Y matching BenchmarkX.
 		// Only process sub-benchmarks, if any.
@@ -929,7 +929,7 @@ func Benchmark(f func(b *B)) BenchmarkResult {
 		benchFunc: f,
 		benchTime: benchTime,
 	}
-	b.o = b.newOutputWriter()
+	b.setOutputWriter()
 	if b.run1() {
 		b.run()
 	}
