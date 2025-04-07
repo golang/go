ffe47d978990c5bfc0089b94a5bea0f61c6219b7
Mateusz Poliwczak <mpoliwczak34@gmail.com>
2025-04-07 20:38:54 +0200

    go/ast: move sortimports test directly to go/ast
    For some reason i have created a separate package instead
    of using _test package. Let's move this test where it belongs.
    
    Change-Id: Ib569ca433de1ef4e161b9d334125648e00b7d3c4
    Reviewed-on: https://go-review.googlesource.com/c/go/+/663555
    Auto-Submit: Alan Donovan <adonovan@google.com>
    Reviewed-by: Dmitri Shuralyov <dmitshur@google.com>
    LUCI-TryBot-Result: Go LUCI <golang-scoped@luci-project-accounts.iam.gserviceaccount.com>
    Reviewed-by: Alan Donovan <adonovan@google.com>

--- src/go/ast/import_test.go
+++ src/go/ast/import_test.go
@@ -2,8 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// Tests is a helper package to avoid cyclic dependency between go/ast and go/parser.
-package tests
+package ast_test
 
 import (
 	"go/ast"
