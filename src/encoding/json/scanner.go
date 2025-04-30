23bf5c9a9342baaec1471552b760676c2363cec5
1911860538 <alxps1911@gmail.com>
2025-04-30 22:06:21 +0800

    encoding/json: remove redundant c <= ' ' check in isSpace

--- src/encoding/json/scanner.go
+++ src/encoding/json/scanner.go
@@ -199,7 +199,7 @@ func (s *scanner) popParseState() {
 }
 
 func isSpace(c byte) bool {
-	return c <= ' ' && (c == ' ' || c == '\t' || c == '\r' || c == '\n')
+	return c == ' ' || c == '\t' || c == '\r' || c == '\n'
 }
 
 // stateBeginValueOrEmpty is the state after reading `[`.
