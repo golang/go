8ec555931dc65f1cd0e7cf28d771615d8064a8ad
Russ Cox <rsc@golang.org>
2025-05-03 17:06:41 -0400

    go/types: fix typo in Info.Types doc comment
    Change-Id: Ib95b77ab3dc6e48158e25e70fabb2579f3706b5f
    Reviewed-on: https://go-review.googlesource.com/c/go/+/669755
    Auto-Submit: Russ Cox <rsc@golang.org>
    LUCI-TryBot-Result: Go LUCI <golang-scoped@luci-project-accounts.iam.gserviceaccount.com>
    Reviewed-by: Robert Griesemer <gri@google.com>

--- src/go/types/api.go
+++ src/go/types/api.go
@@ -228,7 +228,7 @@ type Info struct {
 	// Similarly, no type is recorded for the (synthetic) FuncType
 	// node in a FuncDecl.Type field, since there is no corresponding
 	// syntactic function type expression in the source in this case
-	// Instead, the function type is found in the Defs.map entry for
+	// Instead, the function type is found in the Defs map entry for
 	// the corresponding function declaration.
 	Types map[ast.Expr]TypeAndValue
 
