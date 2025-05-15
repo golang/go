6449f2973d28c3b4a5c9e289c38dfcc38f83b3d9
Zxilly <zxilly@outlook.com>
2025-05-15 18:43:19 +0800

    cmd/link: fix outdated output mmap check

--- src/cmd/link/internal/ld/asmb.go
+++ src/cmd/link/internal/ld/asmb.go
@@ -195,10 +195,7 @@ func relocSectFn(ctxt *Link, relocSect func(*Link, *OutBuf, *sym.Section, []load
 		fn = func(ctxt *Link, sect *sym.Section, syms []loader.Sym) {
 			wg.Add(1)
 			sem <- 1
-			out, err := ctxt.Out.View(sect.Reloff)
-			if err != nil {
-				panic(err)
-			}
+			out := ctxt.Out.View(sect.Reloff)
 			go func() {
 				relocSect(ctxt, out, sect, syms)
 				wg.Done()
