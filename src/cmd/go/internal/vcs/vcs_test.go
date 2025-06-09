54c9d776302d53ab1907645cb67fa4a948e1500c
Roland Shoemaker <bracewell@google.com>
2025-06-09 11:23:46 -0700

    cmd/go: disable support for multiple vcs in one module
    Removes the somewhat redundant vcs.FromDir, "allowNesting" argument,
    which was always enabled, and disallow multiple VCS metadata folders
    being present in a single directory. This makes VCS injection attacks
    much more difficult.
    
    Also adds a GODEBUG, allowmultiplevcs, which re-enables this behavior.
    
    Thanks to RyotaK (https://ryotak.net) of GMO Flatt Security Inc for reporting this issue.
    
    Fixes #74380
    Fixes CVE-2025-4674
    
    Change-Id: I5787d90cdca8deb3aca6f154efb627df1e7d2789
    Reviewed-on: https://go-review.googlesource.com/c/go/+/686515
    LUCI-TryBot-Result: Go LUCI <golang-scoped@luci-project-accounts.iam.gserviceaccount.com>
    Reviewed-by: David Chase <drchase@google.com>
    Commit-Queue: Carlos Amedee <carlos@golang.org>
    Reviewed-by: Carlos Amedee <carlos@golang.org>

--- src/cmd/go/internal/vcs/vcs_test.go
+++ src/cmd/go/internal/vcs/vcs_test.go
@@ -239,7 +239,7 @@ func TestFromDir(t *testing.T) {
 			}
 
 			wantRepoDir := filepath.Dir(dir)
-			gotRepoDir, gotVCS, err := FromDir(dir, tempDir, false)
+			gotRepoDir, gotVCS, err := FromDir(dir, tempDir)
 			if err != nil {
 				t.Errorf("FromDir(%q, %q): %v", dir, tempDir, err)
 				continue
