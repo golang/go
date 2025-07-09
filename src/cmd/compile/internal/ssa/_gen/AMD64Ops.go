08cd62e9f50b10a19f96b94c1e75f868b958d113
David Chase <drchase@google.com>
2025-07-09 14:43:30 -0400

    [dev.simd] cmd/compile: remove X15 from register mask
    mistakes were made. X15 is reserved zero and
    cannot be allocated normally.
    
    Change-Id: I70b24aa07dc31f9b40e306a9aae1d53dfea794f9
    Reviewed-on: https://go-review.googlesource.com/c/go/+/686996
    LUCI-TryBot-Result: Go LUCI <golang-scoped@luci-project-accounts.iam.gserviceaccount.com>
    Reviewed-by: Junyang Shao <shaojunyang@google.com>

--- src/cmd/compile/internal/ssa/_gen/AMD64Ops.go
+++ src/cmd/compile/internal/ssa/_gen/AMD64Ops.go
@@ -126,7 +126,7 @@ func init() {
 		g          = buildReg("g")
 		fp         = buildReg("X0 X1 X2 X3 X4 X5 X6 X7 X8 X9 X10 X11 X12 X13 X14")
 		v          = buildReg("X0 X1 X2 X3 X4 X5 X6 X7 X8 X9 X10 X11 X12 X13 X14")
-		w          = buildReg("X0 X1 X2 X3 X4 X5 X6 X7 X8 X9 X10 X11 X12 X13 X14 X15 X16 X17 X18 X19 X20 X21 X22 X23 X24 X25 X26 X27 X28 X29 X30 X31")
+		w          = buildReg("X0 X1 X2 X3 X4 X5 X6 X7 X8 X9 X10 X11 X12 X13 X14 X16 X17 X18 X19 X20 X21 X22 X23 X24 X25 X26 X27 X28 X29 X30 X31")
 		x15        = buildReg("X15")
 		mask       = buildReg("K1 K2 K3 K4 K5 K6 K7")
 		gpsp       = gp | buildReg("SP")
