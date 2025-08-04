dd96aeb7b594ed8d8bd26e3c9f613c7ec41902a2
Jes Cok <xigua67damn@gmail.com>
2025-08-04 21:38:47 +0800

    2
    Change-Id: I2effccfd296366065ee9626a9674cd637b702218

---
+++ src/log/slog/multi_handler.go
@@ -0,0 +1,58 @@
+// Copyright 2025 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package slog
+
+import (
+	"context"
+	"errors"
+)
+
+// MultiHandler returns a handler that invokes all the given Handlers.
+// Its Enable method reports whether any of the handlers' Enabled methods return true.
+// Its Handle, WithAttr and WithGroup methods call the corresponding method on each of the enabled handlers.
+func MultiHandler(handlers ...Handler) Handler {
+	return &multiHandler{multi: handlers}
+}
+
+type multiHandler struct {
+	multi []Handler
+}
+
+func (h *multiHandler) Enabled(ctx context.Context, l Level) bool {
+	for i := range h.multi {
+		if h.multi[i].Enabled(ctx, l) {
+			return true
+		}
+	}
+	return false
+}
+
+func (h *multiHandler) Handle(ctx context.Context, r Record) error {
+	var errs []error
+	for i := range h.multi {
+		if h.multi[i].Enabled(ctx, r.Level) {
+			if err := h.multi[i].Handle(ctx, r.Clone()); err != nil {
+				errs = append(errs, err)
+			}
+		}
+	}
+	return errors.Join(errs...)
+}
+
+func (h *multiHandler) WithAttrs(attrs []Attr) Handler {
+	handlers := make([]Handler, 0, len(h.multi))
+	for i := range h.multi {
+		handlers = append(handlers, h.multi[i].WithAttrs(attrs))
+	}
+	return &multiHandler{multi: handlers}
+}
+
+func (h *multiHandler) WithGroup(name string) Handler {
+	handlers := make([]Handler, 0, len(h.multi))
+	for i := range h.multi {
+		handlers = append(handlers, h.multi[i].WithGroup(name))
+	}
+	return &multiHandler{multi: handlers}
+}
