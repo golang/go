f3606b0825c20e3f3ccdae00bb8d5d7d8dfd745e
Xiaolin Zhao <zhaoxiaolin@loongson.cn>
2025-08-06 10:31:59 +0800

    cmd/compile/internal/ssa: fix typo in LOONG64Ops.go comment
    Change-Id: I680bae7fc1a26c1f249ab833fa8d41e9387b2d50
    Reviewed-on: https://go-review.googlesource.com/c/go/+/693456
    Reviewed-by: Dmitri Shuralyov <dmitshur@google.com>
    LUCI-TryBot-Result: Go LUCI <golang-scoped@luci-project-accounts.iam.gserviceaccount.com>
    Reviewed-by: Mark Freeman <markfreeman@google.com>
    Reviewed-by: abner chenc <chenguoqi@loongson.cn>

--- src/cmd/compile/internal/ssa/_gen/LOONG64Ops.go
+++ src/cmd/compile/internal/ssa/_gen/LOONG64Ops.go
@@ -589,7 +589,7 @@ func init() {
 		{name: "FPT", controls: 1},  // FP flag is true
 		{name: "FPF", controls: 1},  // FP flag is false
 		{name: "BEQ", controls: 2},  // controls[0] == controls[1]
-		{name: "BNE", controls: 2},  // controls[0] == controls[1]
+		{name: "BNE", controls: 2},  // controls[0] != controls[1]
 		{name: "BGE", controls: 2},  // controls[0] >= controls[1]
 		{name: "BLT", controls: 2},  // controls[0] < controls[1]
 		{name: "BGEU", controls: 2}, // controls[0] >= controls[1], unsigned
