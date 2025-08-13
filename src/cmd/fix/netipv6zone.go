459b85ccaa30bbce4c22b2779672dfe402a2b2da
qiulaidongfeng <2645477756@qq.com>
2025-08-13 22:33:14 +0800

    cmd/fix: remove all functionality except for buildtag
    For #73605
    
    Change-Id: I4b46b5eb72471c215f2cc208c1b0cdd1fbdbf81a
    Reviewed-on: https://go-review.googlesource.com/c/go/+/695855
    Reviewed-by: Michael Matloob <matloob@golang.org>
    Reviewed-by: Michael Pratt <mpratt@google.com>
    Reviewed-by: Alan Donovan <adonovan@google.com>
    LUCI-TryBot-Result: Go LUCI <golang-scoped@luci-project-accounts.iam.gserviceaccount.com>
    Auto-Submit: Alan Donovan <adonovan@google.com>

--- src/cmd/fix/netipv6zone.go
+++ src/cmd/fix/netipv6zone.go
@@ -4,11 +4,6 @@
 
 package main
 
-import (
-	"go/ast"
-	"slices"
-)
-
 func init() {
 	register(netipv6zoneFix)
 }
@@ -16,56 +11,9 @@ func init() {
 var netipv6zoneFix = fix{
 	name: "netipv6zone",
 	date: "2012-11-26",
-	f:    netipv6zone,
-	desc: `Adapt element key to IPAddr, UDPAddr or TCPAddr composite literals.
+	f:    noop,
+	desc: `Adapt element key to IPAddr, UDPAddr or TCPAddr composite literals (removed).
 
 https://codereview.appspot.com/6849045/
 `,
-}
-
-func netipv6zone(f *ast.File) bool {
-	if !imports(f, "net") {
-		return false
-	}
-
-	fixed := false
-	walk(f, func(n any) {
-		cl, ok := n.(*ast.CompositeLit)
-		if !ok {
-			return
-		}
-		se, ok := cl.Type.(*ast.SelectorExpr)
-		if !ok {
-			return
-		}
-		if !isTopName(se.X, "net") || se.Sel == nil {
-			return
-		}
-		switch ss := se.Sel.String(); ss {
-		case "IPAddr", "UDPAddr", "TCPAddr":
-			for i, e := range cl.Elts {
-				if _, ok := e.(*ast.KeyValueExpr); ok {
-					break
-				}
-				switch i {
-				case 0:
-					cl.Elts[i] = &ast.KeyValueExpr{
-						Key:   ast.NewIdent("IP"),
-						Value: e,
-					}
-				case 1:
-					if elit, ok := e.(*ast.BasicLit); ok && elit.Value == "0" {
-						cl.Elts = slices.Delete(cl.Elts, i, i+1)
-					} else {
-						cl.Elts[i] = &ast.KeyValueExpr{
-							Key:   ast.NewIdent("Port"),
-							Value: e,
-						}
-					}
-				}
-				fixed = true
-			}
-		}
-	})
-	return fixed
 }
