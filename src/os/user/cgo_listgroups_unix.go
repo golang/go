63cd9120836fe935bf39dc9b3a19ad44eab7f063
Kir Kolyshkin <kolyshkin@gmail.com>
2025-09-11 15:45:37 -0700

    os/user: simplify go:build
    The go:build constraint for these files is way too complicated,
    and can be simplified by using unix tag.
    
    Change-Id: Id8278db0ba799a4e951d3c976f77c7402bebb332
    Reviewed-on: https://go-review.googlesource.com/c/go/+/703155
    Reviewed-by: Ian Lance Taylor <iant@golang.org>
    Reviewed-by: Mark Freeman <markfreeman@google.com>
    Reviewed-by: Michael Knyszek <mknyszek@google.com>
    LUCI-TryBot-Result: Go LUCI <golang-scoped@luci-project-accounts.iam.gserviceaccount.com>

--- src/os/user/cgo_listgroups_unix.go
+++ src/os/user/cgo_listgroups_unix.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-//go:build (cgo || darwin) && !osusergo && (darwin || dragonfly || freebsd || (linux && !android) || netbsd || openbsd || solaris)
+//go:build (cgo || darwin) && !osusergo && unix && !android && !aix
 
 package user
 
--- src/os/user/getgrouplist_unix.go
+++ src/os/user/getgrouplist_unix.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-//go:build cgo && !osusergo && (dragonfly || freebsd || (!android && linux) || netbsd || openbsd || solaris)
+//go:build cgo && !osusergo && unix && !android && !aix && !darwin
 
 package user
 
