374779b85f6ae7f5ad5f87f7a78811612c74c953
Klaus Post <klauspost@gmail.com>
2025-09-27 16:27:38 +0200

    [klauspost/deflate-improve-comp] don't use internal/byteorder
    Change-Id: I0ac5571da9585daba9491b360c9a6b4e0cecbcee

--- src/compress/flate/unsafe_disabled.go
+++ src/compress/flate/unsafe_disabled.go
@@ -4,10 +4,6 @@
 
 package flate
 
-import (
-	"internal/byteorder"
-)
-
 type indexer interface {
 	int | int8 | int16 | int32 | int64 | uint | uint8 | uint16 | uint32 | uint64
 }
@@ -19,15 +15,26 @@ func loadLE8[I indexer](b []byte, i I) byte {
 
 // loadLE32 will load from b at index i.
 func loadLE32[I indexer](b []byte, i I) uint32 {
-	return byteorder.LEUint32(b[i:])
+	b = b[i : i+4]
+	return uint32(b[0]) | uint32(b[1])<<8 | uint32(b[2])<<16 | uint32(b[3])<<24
 }
 
 // loadLE64 will load from b at index i.
 func loadLE64[I indexer](b []byte, i I) uint64 {
-	return byteorder.LEUint64(b[i:])
+	b = b[i : i+8]
+	return uint64(b[0]) | uint64(b[1])<<8 | uint64(b[2])<<16 | uint64(b[3])<<24 |
+		uint64(b[4])<<32 | uint64(b[5])<<40 | uint64(b[6])<<48 | uint64(b[7])<<56
 }
 
 // storeLE64 will store v at start of b.
 func storeLE64(b []byte, v uint64) {
-	byteorder.LEPutUint64(b, v)
+	_ = b[7] // early bounds check to guarantee safety of writes below
+	b[0] = byte(v)
+	b[1] = byte(v >> 8)
+	b[2] = byte(v >> 16)
+	b[3] = byte(v >> 24)
+	b[4] = byte(v >> 32)
+	b[5] = byte(v >> 40)
+	b[6] = byte(v >> 48)
+	b[7] = byte(v >> 56)
 }
