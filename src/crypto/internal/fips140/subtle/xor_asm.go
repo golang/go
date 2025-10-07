cb81270113968408d7cc41c0b1530adb51dd8496
Keith Randall <khr@golang.org>
2025-10-07 10:12:43 -0700

    Revert "crypto/internal/fips140/subtle: add assembly implementation of xorBytes for mipsx"
    This reverts commit 343e486bfdbf9ca614d3e197afd79ad7ed5fef3e.
    
    Reason for revert: doesn't handle unaligned accesses correctly.
    
    Update #74998
    
    Change-Id: I1d6210eeca9336f2ce311e99944cb270565563aa
    Reviewed-on: https://go-review.googlesource.com/c/go/+/709795
    Reviewed-by: Cherry Mui <cherryyz@google.com>
    Reviewed-by: Michael Knyszek <mknyszek@google.com>
    LUCI-TryBot-Result: Go LUCI <golang-scoped@luci-project-accounts.iam.gserviceaccount.com>
    Reviewed-by: Keith Randall <khr@google.com>

--- src/crypto/internal/fips140/subtle/xor_asm.go
+++ src/crypto/internal/fips140/subtle/xor_asm.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-//go:build (amd64 || arm64 || mips || mipsle || mips64 || mips64le || ppc64 || ppc64le || riscv64) && !purego
+//go:build (amd64 || arm64 || mips64 || mips64le || ppc64 || ppc64le || riscv64) && !purego
 
 package subtle
 
