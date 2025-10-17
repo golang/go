80876f4b42c807e0f90eab20a3e8a98ef95f2cb0
Alan Donovan <adonovan@google.com>
2025-10-17 16:11:56 -0400

    cmd/go/internal/vet: tweak help doc
    For #71859
    
    Change-Id: I3cea3375bd5adff9486b849e472d29ad8324dd54
    Reviewed-on: https://go-review.googlesource.com/c/go/+/712665
    Reviewed-by: Michael Matloob <matloob@golang.org>
    LUCI-TryBot-Result: Go LUCI <golang-scoped@luci-project-accounts.iam.gserviceaccount.com>
    Auto-Submit: Alan Donovan <adonovan@google.com>
    Reviewed-by: Michael Matloob <matloob@google.com>

--- src/cmd/go/internal/vet/vet.go
+++ src/cmd/go/internal/vet/vet.go
@@ -54,9 +54,11 @@ can be built and run using these commands:
 Alternative vet tools should be built atop golang.org/x/tools/go/analysis/unitchecker,
 which handles the interaction with go vet.
 
-For more about specifying packages, see 'go help packages'.
-For a list of checkers and their flags, see 'go tool vet help'.
+The default vet tool is 'go tool vet' or cmd/vet.
+For help on its checkers and their flags, run 'go tool vet help'.
 For details of a specific checker such as 'printf', see 'go tool vet help printf'.
+
+For more about specifying packages, see 'go help packages'.
 
 The build flags supported by go vet are those that control package resolution
 and execution, such as -C, -n, -x, -v, -tags, and -toolexec.
@@ -71,7 +73,7 @@ var CmdFix = &base.Command{
 	UsageLine:   "go fix [build flags] [-fixtool prog] [fix flags] [packages]",
 	Short:       "apply fixes suggested by static checkers",
 	Long: `
-Fix runs the Go fix tool (cmd/vet) on the named packages
+Fix runs the Go fix tool (cmd/fix) on the named packages
 and applies suggested fixes.
 
 It supports these flags:
@@ -80,15 +82,14 @@ It supports these flags:
 	instead of applying each fix, print the patch as a unified diff
 
 The -fixtool=prog flag selects a different analysis tool with
-alternative or additional fixes; see the documentation for go vet's
+alternative or additional fixers; see the documentation for go vet's
 -vettool flag for details.
 
+The default fix tool is 'go tool fix' or cmd/fix.
+For help on its fixers and their flags, run 'go tool fix help'.
+For details of a specific fixer such as 'hostport', see 'go tool fix help hostport'.
+
 For more about specifying packages, see 'go help packages'.
-
-For a list of fixers and their flags, see 'go tool fix help'.
-
-For details of a specific fixer such as 'hostport',
-see 'go tool fix help hostport'.
 
 The build flags supported by go fix are those that control package resolution
 and execution, such as -C, -n, -x, -v, -tags, and -toolexec.
