03e2ca887302dbeb4e231dd13ebf171e39e52cd0
Sjors Gielen <sjors@sjorsgielen.nl>
2025-10-24 14:01:34 +0200

    Remove redundant build tag.

--- src/net/http/errors_plan9.go
+++ src/net/http/errors_plan9.go
@@ -2,8 +2,6 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-//go:build plan9
-
 package http
 
 func isClientDisconnected(err error) bool {
