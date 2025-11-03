c37e118cd26df69c16a7555f86230ce36f11ec80
Daniel Beneker <d.beneker@mittwald.de>
2025-11-03 15:59:12 +0100

    just return r.Attrs

--- src/log/slog/record.go
+++ src/log/slog/record.go
@@ -95,9 +95,7 @@ func (r Record) Attrs(f func(Attr) bool) {
 
 // AllAttrs returns an iterator over every attribute in the [Record].
 func (r Record) AllAttrs() iter.Seq[Attr] {
-	return func(yield func(Attr) bool) {
-		r.Attrs(yield)
-	}
+	return r.Attrs
 }
 
 // AddAttrs appends the given Attrs to the [Record]'s list of Attrs.
