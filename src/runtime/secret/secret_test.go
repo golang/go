de456450e7a49789de63c515cb78a1e38e88440e
Daniel Morsing <daniel.morsing@gmail.com>
2025-11-28 09:40:00 +0000

    runtime/secret: disable tests under memory validating modes
    These tests rely on reading memory that has been freed, so any of the
    modes that validate memory accesses are going to fail. Disable them for
    now.
    
    Fixes #76586.
    
    Cq-Include-Trybots: luci.golang.try:gotip-linux-amd64-msan-clang15,gotip-linux-amd64-asan-clang15,gotip-linux-amd64-race
    Change-Id: I14ee5dfccbafa0e4da684a95ee42acf54499b013
    Reviewed-on: https://go-review.googlesource.com/c/go/+/725140
    Auto-Submit: Keith Randall <khr@golang.org>
    LUCI-TryBot-Result: Go LUCI <golang-scoped@luci-project-accounts.iam.gserviceaccount.com>
    Reviewed-by: Keith Randall <khr@golang.org>
    Reviewed-by: Keith Randall <khr@google.com>
    Reviewed-by: Carlos Amedee <carlos@golang.org>

--- src/runtime/secret/secret_test.go
+++ src/runtime/secret/secret_test.go
@@ -2,10 +2,14 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// the race detector does not like our pointer shenanigans
-// while checking the stack.
+// these tests rely on inspecting freed memory, so they
+// can't be run under any of the memory validating modes.
+// TODO: figure out just which test violate which condition
+// and split this file out by individual test cases.
+// There could be some value to running some of these
+// under validation
 
-//go:build goexperiment.runtimesecret && (arm64 || amd64) && linux && !race
+//go:build goexperiment.runtimesecret && (arm64 || amd64) && linux && !race && !asan && !msan
 
 package secret
 
