44a39c9dacdb3378197b9515add22d292dd71af9
Michael Anthony Knyszek <mknyszek@google.com>
2025-12-08 16:17:15 +0000

    runtime: only run TestNotInGoMetricCallback when building with cgo
    It seems like some platforms don't build with cgo at all, like
    linux/ppc64.
    
    Change-Id: Ibe5306e79899822e77d42cb3018f9f0c9ca2604c
    Reviewed-on: https://go-review.googlesource.com/c/go/+/728140
    LUCI-TryBot-Result: Go LUCI <golang-scoped@luci-project-accounts.iam.gserviceaccount.com>
    Reviewed-by: Cherry Mui <cherryyz@google.com>
    Auto-Submit: Michael Knyszek <mknyszek@google.com>

--- src/runtime/metrics_test.go
+++ src/runtime/metrics_test.go
@@ -9,7 +9,6 @@ import (
 	"internal/abi"
 	"internal/goexperiment"
 	"internal/profile"
-	"internal/race"
 	"internal/testenv"
 	"os"
 	"reflect"
@@ -1580,25 +1579,6 @@ func TestReadMetricsFinalizers(t *testing.T) {
 func TestReadMetricsSched(t *testing.T) {
 	// This test is run in a subprocess to prevent other tests from polluting the metrics.
 	output := runTestProg(t, "testprog", "SchedMetrics")
-	want := "OK\n"
-	if output != want {
-		t.Fatalf("output:\n%s\n\nwanted:\n%s", output, want)
-	}
-}
-
-func TestNotInGoMetricCallback(t *testing.T) {
-	switch runtime.GOOS {
-	case "windows", "plan9":
-		t.Skip("unsupported on Windows and Plan9")
-	case "freebsd":
-		if race.Enabled {
-			t.Skipf("race + cgo freebsd not supported. See https://go.dev/issue/73788.")
-		}
-	}
-
-	// This test is run in a subprocess to prevent other tests from polluting the metrics
-	// and because we need to make some cgo callbacks.
-	output := runTestProg(t, "testprogcgo", "NotInGoMetricCallback")
 	want := "OK\n"
 	if output != want {
 		t.Fatalf("output:\n%s\n\nwanted:\n%s", output, want)
