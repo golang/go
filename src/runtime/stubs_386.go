d4972f6295aede2ddc35bcb1da5f6351623e9e4d
khr@golang.org <khr@golang.org>
2025-12-08 09:21:39 -0500

    runtime: mark getfp as nosplit
    When compiling with -l, we can't take a stack split here.
    
    Fixes #76702
    
    Change-Id: Ieab1225c6259c7f16bb5188aa84bff615d9db2e5
    Reviewed-on: https://go-review.googlesource.com/c/go/+/728060
    LUCI-TryBot-Result: Go LUCI <golang-scoped@luci-project-accounts.iam.gserviceaccount.com>
    Auto-Submit: Keith Randall <khr@golang.org>
    Reviewed-by: Cherry Mui <cherryyz@google.com>
    Reviewed-by: Keith Randall <khr@google.com>

--- src/runtime/stubs_386.go
+++ src/runtime/stubs_386.go
@@ -21,4 +21,6 @@ func asmcgocall_no_g(fn, arg unsafe.Pointer)
 
 // getfp returns the frame pointer register of its caller or 0 if not implemented.
 // TODO: Make this a compiler intrinsic
+//
+//go:nosplit
 func getfp() uintptr { return 0 }
