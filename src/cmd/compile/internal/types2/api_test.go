2e6b7c8c7f6d2bf4dd128ced7dc18d4375162ae7
goto1134 <1134togo@gmail.com>
2025-12-10 18:15:08 +0100

    cmd/compile: fix typos in TestGenericMethodInfo

--- src/cmd/compile/internal/types2/api_test.go
+++ src/cmd/compile/internal/types2/api_test.go
@@ -809,13 +809,13 @@ func (r *N[C]) n() {  }
 		t.Errorf(`N.Method(...) returns %v for "m", but Info.Defs has %v`, gm, dm)
 	}
 	if gn != dn {
-		t.Errorf(`N.Method(...) returns %v for "m", but Info.Defs has %v`, gm, dm)
+		t.Errorf(`N.Method(...) returns %v for "n", but Info.Defs has %v`, gn, dn)
 	}
 	if dmm != dm {
 		t.Errorf(`Inside "m", r.m uses %v, want the defined func %v`, dmm, dm)
 	}
 	if dmn == dn {
-		t.Errorf(`Inside "m", r.n uses %v, want a func distinct from %v`, dmm, dm)
+		t.Errorf(`Inside "m", r.n uses %v, want a func distinct from %v`, dmn, dn)
 	}
 }
 
