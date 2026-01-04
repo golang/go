e4f2b9e820c1fe1fb5d0abfea5bc0353373a9106
xuri <xuri.me@gmail.com>
2026-01-04 20:22:07 +0800

    Remove spaces in the tail for the doc blocks

--- src/archive/zip/writer_test.go
+++ src/archive/zip/writer_test.go
@@ -672,7 +672,7 @@ func TestIssue61875(t *testing.T) {
 	}
 }
 
-// TestZip64LFHVersion tests that the Local File Header version is written 
+// TestZip64LFHVersion tests that the Local File Header version is written
 // to 45 for ZIP64 entries to match the Central Directory version.
 func TestZip64LFHVersion(t *testing.T) {
 	buf := new(bytes.Buffer)
