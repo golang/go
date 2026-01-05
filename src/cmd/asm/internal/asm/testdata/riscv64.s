cd875d93145ee3eb34d326e6b6da82acaa45d62a
wenchangping <987706386@qq.com>
2026-01-05 17:32:01 +0800

    cmd/internal/obj: add asm support for the Zcb extension of riscv64 (#80)
    * cmd/internal/obj: add asm support for the Zcb extension of riscv64
    
    * remove ternary flag for ‘cu’ encode format
    
    ---------
    
    Co-authored-by: 文昌平10295505 <wen.changping@zte.com.cn>

--- src/cmd/asm/internal/asm/testdata/riscv64.s
+++ src/cmd/asm/internal/asm/testdata/riscv64.s
@@ -512,6 +512,21 @@ start:
 
 	// 26.5.6: Compressed Breakpoint Instruction
 	CEBREAK						// 0290
+
+	// 27.8: Simple code-size saving Instructions (Zcb)
+	CLBU	3(X10), X11		// 6c81
+	CLHU	2(X10), X11		// 2c85
+	CLH		2(X10), X11		// 6c85
+	CSB		X11, 2(X10)		// 2c89
+	CSH		X11, 2(X10)		// 2c8d
+	CZEXTB	X10				// 619d
+	CSEXTB	X10				// 659d
+	CZEXTH	X10				// 699d
+	CSEXTH	X10				// 6d9d
+	CZEXTW	X10				// 719d
+	CNOT	X10				// 759d
+	CMUL	X11, X10		// 4d9d
+
 	// 24: "Zfa" Extension for Additional Floating-Point Instructions
 	// 24.1: Load-Immediate Instructions
 	FLIS $(NaN), F1                // d3801ff0
