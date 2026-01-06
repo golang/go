c16402d15b42cf494774796e606aba66c90d3d6b
Damien Neil <dneil@google.com>
2026-01-06 15:57:00 -0800

    os/exec: move platform-specific LookPath docs to a common comment
    We have four different LookPath variations (unix, windows, plan9, wasm),
    each with slightly different doc comments. Unify the documentation and
    move it to a single, common LookPath.
    
    Change-Id: I56bae57e80887a73ef0f6933258ee0a48dbccdcf
    Reviewed-on: https://go-review.googlesource.com/c/go/+/734320
    Reviewed-by: Roland Shoemaker <roland@golang.org>
    Reviewed-by: Brad Fitzpatrick <bradfitz@golang.org>
    LUCI-TryBot-Result: Go LUCI <golang-scoped@luci-project-accounts.iam.gserviceaccount.com>

--- src/os/exec/lp_plan9.go
+++ src/os/exec/lp_plan9.go
@@ -26,16 +26,7 @@ func findExecutable(file string) error {
 	return fs.ErrPermission
 }
 
-// LookPath searches for an executable named file in the
-// directories named by the path environment variable.
-// If file begins with "/", "#", "./", or "../", it is tried
-// directly and the path is not consulted.
-// On success, the result is an absolute path.
-//
-// In older versions of Go, LookPath could return a path relative to the current directory.
-// As of Go 1.19, LookPath will instead return that path along with an error satisfying
-// [errors.Is](err, [ErrDot]). See the package documentation for more details.
-func LookPath(file string) (string, error) {
+func lookPath(file string) (string, error) {
 	if err := validateLookPath(filepath.Clean(file)); err != nil {
 		return "", &Error{file, err}
 	}
