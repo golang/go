b2abaab3fc24262d6161a03f0e958a8f8c699053
Roland Shoemaker <roland@golang.org>
2026-01-26 10:49:30 -0800

    [release-branch.go1.24] Revert "crypto/tls: don't copy auto-rotated session ticket keys in Config.Clone"
    This reverts CL 736709 (commit bba24719a4cad5cc8d771fc9cfff5a38019d554a).
    
    Updates #77113
    Updates #77355
    Updates CVE-2025-68121
    
    Change-Id: I0261cb75e9adf9d0ac9890dc91ae8476b8988ba0
    Reviewed-on: https://go-review.googlesource.com/c/go/+/739320
    Reviewed-by: Coia Prant <coiaprant@gmail.com>
    Reviewed-by: Filippo Valsorda <filippo@golang.org>
    LUCI-TryBot-Result: Go LUCI <golang-scoped@luci-project-accounts.iam.gserviceaccount.com>
    Reviewed-by: Dmitri Shuralyov <dmitshur@google.com>
    Reviewed-on: https://go-review.googlesource.com/c/go/+/740060
    Reviewed-by: Damien Neil <dneil@google.com>
    Auto-Submit: Dmitri Shuralyov <dmitshur@google.com>
    Reviewed-by: Nicholas Husin <nsh@golang.org>
    Reviewed-by: Nicholas Husin <husin@google.com>

--- src/crypto/tls/tls_test.go
+++ src/crypto/tls/tls_test.go
@@ -930,8 +930,8 @@ func TestCloneNonFuncFields(t *testing.T) {
 		}
 	}
 	// Set the unexported fields related to session ticket keys, which are copied with Clone().
+	c1.autoSessionTicketKeys = []ticketKey{c1.ticketKeyFromBytes(c1.SessionTicketKey)}
 	c1.sessionTicketKeys = []ticketKey{c1.ticketKeyFromBytes(c1.SessionTicketKey)}
-	// We explicitly don't copy autoSessionTicketKeys in Clone, so don't set it.
 
 	c2 := c1.Clone()
 	if !reflect.DeepEqual(&c1, c2) {
@@ -2247,14 +2247,5 @@ func TestECH(t *testing.T) {
 	}
 	if !cs.VerifiedChains[0][0].Equal(secretCert) {
 		t.Fatal("unexpected certificate")
-	}
-}
-
-func TestConfigCloneAutoSessionTicketKeys(t *testing.T) {
-	orig := &Config{}
-	orig.ticketKeys(nil)
-	clone := orig.Clone()
-	if slices.Equal(orig.autoSessionTicketKeys, clone.autoSessionTicketKeys) {
-		t.Fatal("autoSessionTicketKeys slice copied in Clone")
 	}
 }
