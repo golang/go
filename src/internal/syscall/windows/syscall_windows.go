db5882ea9bf63849d554910f486c4c5cfea08852
Michal Pristas <michal.pristas@gmail.com>
2026-02-04 09:36:59 +0000

    os: support deleting inaccessible files in RemoveAll
    windows: retry file open with DELETE access after access denied
    
    Additional access rights when opening files, including SYNCHRONIZE,
    break deletion when the caller has FILE_DELETE_CHILD on the parent
    directory but not the file. Retry with DELETE only restores correct
    Windows semantics.
    
    Fixes #77402
    
    Change-Id: Ie53bc6f1673de1a8af4dcfb7496daf99e71098cb
    GitHub-Last-Rev: 0ad635cf1a13c0242e3b1922cf47a8c594dd7215
    GitHub-Pull-Request: golang/go#77403
    Reviewed-on: https://go-review.googlesource.com/c/go/+/741040
    Reviewed-by: Quim Muntal <quimmuntal@gmail.com>
    LUCI-TryBot-Result: Go LUCI <golang-scoped@luci-project-accounts.iam.gserviceaccount.com>
    Reviewed-by: Michael Pratt <mpratt@google.com>
    Auto-Submit: Michael Pratt <mpratt@google.com>
    Reviewed-by: Michael Knyszek <mknyszek@google.com>

--- src/internal/syscall/windows/syscall_windows.go
+++ src/internal/syscall/windows/syscall_windows.go
@@ -561,6 +561,7 @@ const (
 	STATUS_NOT_SUPPORTED             NTStatus = 0xC00000BB
 	STATUS_INVALID_PARAMETER         NTStatus = 0xC000000D
 	STATUS_INVALID_INFO_CLASS        NTStatus = 0xC0000003
+	STATUS_ACCESS_DENIED             NTStatus = 0xC0000022
 )
 
 const (
@@ -579,3 +580,6 @@ type FILE_MODE_INFORMATION struct {
 //sys   NtSetInformationFile(handle syscall.Handle, iosb *IO_STATUS_BLOCK, inBuffer unsafe.Pointer, inBufferLen uint32, class uint32) (ntstatus error) = ntdll.NtSetInformationFile
 //sys	RtlIsDosDeviceName_U(name *uint16) (ret uint32) = ntdll.RtlIsDosDeviceName_U
 //sys   NtQueryInformationFile(handle syscall.Handle, iosb *IO_STATUS_BLOCK, inBuffer unsafe.Pointer, inBufferLen uint32, class uint32) (ntstatus error) = ntdll.NtQueryInformationFile
+
+//sys	SetEntriesInAcl(countExplicitEntries uint32, explicitEntries *EXPLICIT_ACCESS, oldACL syscall.Handle, newACL *syscall.Handle) (ret error) =  advapi32.SetEntriesInAclW
+//sys	SetNamedSecurityInfo(objectName string, objectType int32, securityInformation uint32, owner *syscall.SID, group *syscall.SID, dacl syscall.Handle, sacl syscall.Handle) (ret error) = advapi32.SetNamedSecurityInfoW
