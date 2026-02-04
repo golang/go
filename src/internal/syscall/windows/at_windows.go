c283d1f762ea21472812eac2451d02eb8f354fac
Michal Pristas <michal.pristas@gmail.com>
2026-02-04 09:36:59 +0000

    [release-branch.go1.26] os: support deleting inaccessible files in RemoveAll
    windows: retry file open with DELETE access after access denied
    
    Additional access rights when opening files, including SYNCHRONIZE,
    break deletion when the caller has FILE_DELETE_CHILD on the parent
    directory but not the file. Retry with DELETE only restores correct
    Windows semantics.
    
    For #77407
    
    Change-Id: Ie53bc6f1673de1a8af4dcfb7496daf99e71098cb
    GitHub-Last-Rev: 0ad635cf1a13c0242e3b1922cf47a8c594dd7215
    GitHub-Pull-Request: golang/go#77403
    Reviewed-on: https://go-review.googlesource.com/c/go/+/741040
    Reviewed-by: Quim Muntal <quimmuntal@gmail.com>
    LUCI-TryBot-Result: Go LUCI <golang-scoped@luci-project-accounts.iam.gserviceaccount.com>
    Reviewed-by: Michael Pratt <mpratt@google.com>
    Auto-Submit: Michael Pratt <mpratt@google.com>
    Reviewed-by: Michael Knyszek <mknyszek@google.com>
    (cherry picked from commit db5882ea9bf63849d554910f486c4c5cfea08852)
    Reviewed-on: https://go-review.googlesource.com/c/go/+/746362
    Reviewed-by: Junyang Shao <shaojunyang@google.com>
    Auto-Submit: Mark Freeman <markfreeman@google.com>

--- src/internal/syscall/windows/at_windows.go
+++ src/internal/syscall/windows/at_windows.go
@@ -246,14 +246,31 @@ func Deleteat(dirfd syscall.Handle, name string, options uint32) error {
 	var h syscall.Handle
 	err := NtOpenFile(
 		&h,
-		SYNCHRONIZE|FILE_READ_ATTRIBUTES|DELETE,
+		FILE_READ_ATTRIBUTES|DELETE,
 		objAttrs,
 		&IO_STATUS_BLOCK{},
 		FILE_SHARE_DELETE|FILE_SHARE_READ|FILE_SHARE_WRITE,
-		FILE_OPEN_REPARSE_POINT|FILE_OPEN_FOR_BACKUP_INTENT|FILE_SYNCHRONOUS_IO_NONALERT|options,
+		FILE_OPEN_REPARSE_POINT|FILE_OPEN_FOR_BACKUP_INTENT|options,
 	)
 	if err != nil {
-		return ntCreateFileError(err, 0)
+		if ntStatus, ok := err.(NTStatus); !ok || ntStatus != STATUS_ACCESS_DENIED {
+			return ntCreateFileError(err, 0)
+		}
+
+		// Access denied, try opening with DELETE only.
+		// This may succeed if the file has restrictive permissions
+		// but the caller has delete child permission on the parent directory.
+		err = NtOpenFile(
+			&h,
+			DELETE,
+			objAttrs,
+			&IO_STATUS_BLOCK{},
+			FILE_SHARE_DELETE|FILE_SHARE_READ|FILE_SHARE_WRITE,
+			FILE_OPEN_REPARSE_POINT|FILE_OPEN_FOR_BACKUP_INTENT|options,
+		)
+		if err != nil {
+			return ntCreateFileError(err, 0)
+		}
 	}
 	defer syscall.CloseHandle(h)
 
