0b395557234c0013b1795fe5d90ddadd1d6302a3
qmuntal <quimmuntal@gmail.com>
2026-02-17 14:18:02 +0100

    runtime/cgo: use pthreads to get stack bound on Solaris
    x_cgo_getstackbound already supports Solaris using pthread_getattr_np,
    which is more robust than getcontext.
    
    Cq-Include-Trybots: luci.golang.try:gotip-solaris-amd64
    Change-Id: Ib96d9c30df3413b4c5044a1b2c392b25835c8004
    Reviewed-on: https://go-review.googlesource.com/c/go/+/745602
    LUCI-TryBot-Result: Go LUCI <golang-scoped@luci-project-accounts.iam.gserviceaccount.com>
    Auto-Submit: Quim Muntal <quimmuntal@gmail.com>
    Reviewed-by: Cherry Mui <cherryyz@google.com>
    Reviewed-by: Michael Pratt <mpratt@google.com>

--- src/runtime/cgo/gcc_unix.c
+++ src/runtime/cgo/gcc_unix.c
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-//go:build unix && !solaris
+//go:build unix
 
 #include "libcgo.h"
 #include "libcgo_unix.h"
