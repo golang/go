09c3cfbc208fbd3ee8a06885e7efa53cdd56be7a
Emmanuel T Odeke <emmanuel@orijtech.com>
2026-02-18 17:03:37 -0500

    net/url: add (*URL).Clone
    This change adds URL.Clone which creates a deep copy
    of the URL's fields including the .User and tests these.
    In a separate CL I shall send Values.Clone too.
    
    Updates #73450
    
    Change-Id: Ifea4bfc4ddd0640247544ec111ec83bd9bbe9104
    Reviewed-on: https://go-review.googlesource.com/c/go/+/746800
    Reviewed-by: Nicholas Husin <husin@google.com>
    Auto-Submit: Emmanuel Odeke <emmanuel@orijtech.com>
    Reviewed-by: Damien Neil <dneil@google.com>
    Reviewed-by: Nicholas Husin <nsh@golang.org>
    LUCI-TryBot-Result: Go LUCI <golang-scoped@luci-project-accounts.iam.gserviceaccount.com>
    Auto-Submit: Damien Neil <dneil@google.com>

--- src/net/http/clone.go
+++ src/net/http/clone.go
@@ -39,16 +39,7 @@ func cloneURLValues(v url.Values) url.Values {
 //
 //go:linkname cloneURL
 func cloneURL(u *url.URL) *url.URL {
-	if u == nil {
-		return nil
-	}
-	u2 := new(url.URL)
-	*u2 = *u
-	if u.User != nil {
-		u2.User = new(url.Userinfo)
-		*u2.User = *u.User
-	}
-	return u2
+	return u.Clone()
 }
 
 // cloneMultipartForm should be an internal detail,
