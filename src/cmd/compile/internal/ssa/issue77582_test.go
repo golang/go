8411ab9d3d947fc842dd99eadf0ee01e3f6b9477
Cherry Mui <cherryyz@google.com>
2026-02-25 13:49:08 -0800

    Revert "test/simd: add test for issue 77582"
    This reverts CL 748760.
    
    Reason for revert:
    1. the fix in CL 745460 is not really correct.
    2. this test doesn't actually reproduce the failure. (It needs to
       actually use the result, instead of assigning to _.)
    
    Change-Id: Ifb3da8954c5e8774ac000ee3bbf92e5813705dd6
    Reviewed-on: https://go-review.googlesource.com/c/go/+/749240
    Reviewed-by: David Chase <drchase@google.com>
    LUCI-TryBot-Result: Go LUCI <golang-scoped@luci-project-accounts.iam.gserviceaccount.com>
    Auto-Submit: Cherry Mui <cherryyz@google.com>

--- src/cmd/compile/internal/ssa/issue77582_test.go
+++ src/cmd/compile/internal/ssa/issue77582_test.go
@@ -1,25 +1,29 @@
-// compile
-
-//go:build goexperiment.simd && amd64
-
 // Copyright 2026 The Go Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-package p
+//go:build goexperiment.simd && amd64
+
+package ssa
 
 import (
-	"simd/archsimd"
+	"testing"
+
+	"cmd/compile/internal/archsimd"
 )
 
-func vpternlogdPanic() {
+func TestVPTERNLOGDPanic(t *testing.T) {
+	if !archsimd.X86.AVX512() {
+		t.Skip("Test only applies to AVX512")
+	}
+
 	resultsMask := archsimd.Mask64x8{}
 	a := archsimd.Mask64x8FromBits(0xFF)
 	b := archsimd.Float64x8{}.Less(archsimd.Float64x8{})
 
 	for i := 0; i < 1; i++ {
 		resultsMask = a.Or(b).Or(resultsMask)
-		// This nested logic triggered the panic in CL 745460
+		// This nested logic triggered the panic
 		_ = resultsMask.And(resultsMask.And(archsimd.Mask64x8{}))
 	}
 }
