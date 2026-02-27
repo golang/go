ad168c5131c83981daefdb0db8a45d1d3315c388
George Adams <georgeadams1995@gmail.com>
2026-02-27 15:45:55 +0000

    runtime/cgo: add abi_riscv64.h for callee-saved register macros
    Add abi_riscv64.h with SAVE_GPR/SAVE_FPR macros for saving and
    restoring the RISC-V callee-saved registers, following the pattern
    established by the other abi_*.h headers.
    
    Refactor asm_riscv64.s (cgo crosscall2), asm_riscv64.s (runtime
    _rt0_riscv64_lib), and race_riscv64.s (racecallbackthunk) to use
    the new macros, replacing inline register save/restore sequences.
    
    Change-Id: I83cef17a35c57fc8b5e045e7181460283ae5b423
    Reviewed-on: https://go-review.googlesource.com/c/go/+/749900
    Reviewed-by: David Chase <drchase@google.com>
    LUCI-TryBot-Result: Go LUCI <golang-scoped@luci-project-accounts.iam.gserviceaccount.com>
    Reviewed-by: Quim Muntal <quimmuntal@gmail.com>
    Reviewed-by: Russ Cox <rsc@golang.org>

--- src/runtime/asm_riscv64.s
+++ src/runtime/asm_riscv64.s
@@ -5,6 +5,7 @@
 #include "go_asm.h"
 #include "funcdata.h"
 #include "textflag.h"
+#include "cgo/abi_riscv64.h"
 
 
 // When building with -buildmode=c-shared, this symbol is called when the shared
@@ -12,30 +13,8 @@
 TEXT _rt0_riscv64_lib(SB),NOSPLIT,$224
 	// Preserve callee-save registers, along with X1 (LR).
 	MOV	X1, (8*3)(X2)
-	MOV	X8, (8*4)(X2)
-	MOV	X9, (8*5)(X2)
-	MOV	X18, (8*6)(X2)
-	MOV	X19, (8*7)(X2)
-	MOV	X20, (8*8)(X2)
-	MOV	X21, (8*9)(X2)
-	MOV	X22, (8*10)(X2)
-	MOV	X23, (8*11)(X2)
-	MOV	X24, (8*12)(X2)
-	MOV	X25, (8*13)(X2)
-	MOV	X26, (8*14)(X2)
-	MOV	g, (8*15)(X2)
-	MOVD	F8, (8*16)(X2)
-	MOVD	F9, (8*17)(X2)
-	MOVD	F18, (8*18)(X2)
-	MOVD	F19, (8*19)(X2)
-	MOVD	F20, (8*20)(X2)
-	MOVD	F21, (8*21)(X2)
-	MOVD	F22, (8*22)(X2)
-	MOVD	F23, (8*23)(X2)
-	MOVD	F24, (8*24)(X2)
-	MOVD	F25, (8*25)(X2)
-	MOVD	F26, (8*26)(X2)
-	MOVD	F27, (8*27)(X2)
+	SAVE_GPR((8*4))
+	SAVE_FPR((8*16))
 
 	// Initialize g as nil in case of using g later e.g. sigaction in cgo_sigaction.go
 	MOV	X0, g
@@ -66,30 +45,8 @@ nocgo:
 restore:
 	// Restore callee-save registers, along with X1 (LR).
 	MOV	(8*3)(X2), X1
-	MOV	(8*4)(X2), X8
-	MOV	(8*5)(X2), X9
-	MOV	(8*6)(X2), X18
-	MOV	(8*7)(X2), X19
-	MOV	(8*8)(X2), X20
-	MOV	(8*9)(X2), X21
-	MOV	(8*10)(X2), X22
-	MOV	(8*11)(X2), X23
-	MOV	(8*12)(X2), X24
-	MOV	(8*13)(X2), X25
-	MOV	(8*14)(X2), X26
-	MOV	(8*15)(X2), g
-	MOVD	(8*16)(X2), F8
-	MOVD	(8*17)(X2), F9
-	MOVD	(8*18)(X2), F18
-	MOVD	(8*19)(X2), F19
-	MOVD	(8*20)(X2), F20
-	MOVD	(8*21)(X2), F21
-	MOVD	(8*22)(X2), F22
-	MOVD	(8*23)(X2), F23
-	MOVD	(8*24)(X2), F24
-	MOVD	(8*25)(X2), F25
-	MOVD	(8*26)(X2), F26
-	MOVD	(8*27)(X2), F27
+	RESTORE_GPR((8*4))
+	RESTORE_FPR((8*16))
 
 	RET
 
