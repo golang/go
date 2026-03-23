e32ec4742e3a9b5a28635e20806331afd587b429
Cherry Mui <cherryyz@google.com>
2026-03-23 17:23:04 -0400

    crypto/internal/fips140/drbg: build tag out entropy generation on Wasm
    In FIPS-140 mode we currently use a scratch buffer in the BSS
    section to generate entropy by measuring jittering for memory
    touches. The BSS variable usually doesn't cost much, except on
    Wasm, due to the way the linear memory works. FIPS-140 mode is
    not supported on Wasm, so this code is not actually needed there.
    This CL uses a build tag to exclude it, so we don't need to
    include an (unused) 32 MB BSS variable in Wasm binaries.
    
    Fixes #78321.
    
    Change-Id: I5139029fa98c302e8769be3e3034967d777f1f16
    Reviewed-on: https://go-review.googlesource.com/c/go/+/758361
    LUCI-TryBot-Result: golang-scoped@luci-project-accounts.iam.gserviceaccount.com <golang-scoped@luci-project-accounts.iam.gserviceaccount.com>
    Reviewed-by: Roland Shoemaker <roland@golang.org>

---
+++ src/crypto/internal/fips140/drbg/entropy_wasm.go
@@ -0,0 +1,11 @@
+// Copyright 2026 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+//go:build wasm
+
+package drbg
+
+func readFromEntropy(b []byte) {
+	panic("FIPS-140 entropy generation is not supported on Wasm")
+}
