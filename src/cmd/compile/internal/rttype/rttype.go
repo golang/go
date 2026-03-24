golang/go

commit 55600733988b0d3bb708be22b5cbecd8edd83380

internal/runtime/maps: add GOEXPERIMENT=mapsplitgroup for KKKKVVVV slot order
Map groups are currently:

type group struct {
    ctrl  uint64
    slots [8]slot
}

type slot struct {
    key  K
    elem E
}

If the element type is struct{}, the slot will be padded so that the
address of the elem is unique rather than pointing outside the alloc.
This has the effect of map[K]struct{} wasting space due to the extra
byte and padding, making it no better than map[K]bool.

This CL changes the group layout to instead place keys and elems
together, as they used to be before swiss maps:

type group struct {
    ctrl  uint64
    keys  [8]K
    elems [8]V
}

This is an alternative to CL 701976, which I suspect will have better
performance. Keys placed together should lead to better cache behavior,
at the cost of more expensive elem lookups, since the elems are not a
fixed offset from their keys.

This change is locked behind GOEXPERIMENT=mapsplitgroup.

Updates #70835
Updates #71368

Change-Id: Ide8d1406ae4ab636f86edc40e0640cc80653197c
Reviewed-on: https://go-review.googlesource.com/c/go/+/711560
Reviewed-by: Michael Pratt <mpratt@google.com>
Auto-Submit: Michael Pratt <mpratt@google.com>
LUCI-TryBot-Result: Go LUCI <golang-scoped@luci-project-accounts.iam.gserviceaccount.com>
Reviewed-by: Dmitri Shuralyov <dmitshur@google.com>

src/cmd/link/internal/ld/deadcode.go
@@ -560,7 +560,7 @@ func (d *deadcodePass) decodetypeMethods(ldr *loader.Loader, arch *sys.Arch, sym
 	case abi.Chan: // reflect.chanType
 		off += 2 * arch.PtrSize
 	case abi.Map:
-		off += 7*arch.PtrSize + 4 // internal/abi.MapType
+		off += 10*arch.PtrSize + 4 // internal/abi.MapType
 		if arch.PtrSize == 8 {
 			off += 4 // padding for final uint32 field (Flags).
 		}

src/cmd/compile/internal/reflectdata/map.go
@@ -13,6 +13,7 @@ import (
 	"cmd/internal/objabi"
 	"cmd/internal/src"
 	"internal/abi"
+	"internal/buildcfg"
 )
 
 // MapGroupType makes the map slot group type given the type of the map.
@@ -26,14 +27,6 @@ func MapGroupType(t *types.Type) *types.Type {
 	// a correct GC program for it.
 	//
 	// Make sure this stays in sync with internal/runtime/maps/group.go.
-	//
-	// type group struct {
-	//     ctrl uint64
-	//     slots [abi.MapGroupSlots]struct {
-	//         key  keyType
-	//         elem elemType
-	//     }
-	// }
 
 	keytype := t.Key()
 	elemtype := t.Elem()
@@ -46,19 +39,48 @@ func MapGroupType(t *types.Type) *types.Type {
 		elemtype = types.NewPtr(elemtype)
 	}
 
-	slotFields := []*types.Field{
-		makefield("key", keytype),
-		makefield("elem", elemtype),
-	}
-	slot := types.NewStruct(slotFields)
-	slot.SetNoalg(true)
+	var fields []*types.Field
+	if buildcfg.Experiment.MapSplitGroup {
+		// Split layout (KKKKVVVV):
+		// type group struct {
+		//     ctrl  uint64
+		//     keys  [abi.MapGroupSlots]keyType
+		//     elems [abi.MapGroupSlots]elemType
+		// }
+		keyArr := types.NewArray(keytype, abi.MapGroupSlots)
+		keyArr.SetNoalg(true)
 
-	slotArr := types.NewArray(slot, abi.MapGroupSlots)
-	slotArr.SetNoalg(true)
+		elemArr := types.NewArray(elemtype, abi.MapGroupSlots)
+		elemArr.SetNoalg(true)
 
-	fields := []*types.Field{
-		makefield("ctrl", types.Types[types.TUINT64]),
-		makefield("slots", slotArr),
+		fields = []*types.Field{
+			makefield("ctrl", types.Types[types.TUINT64]),
+			makefield("keys", keyArr),
+			makefield("elems", elemArr),
+		}
+	} else {
+		// Interleaved slot layout (KVKVKVKV):
+		// type group struct {
+		//     ctrl  uint64
+		//     slots [abi.MapGroupSlots]struct {
+		//         key  keyType
+		//         elem elemType
+		//     }
+		// }
+		slotFields := []*types.Field{
+			makefield("key", keytype),
+			makefield("elem", elemtype),
+		}
+		slot := types.NewStruct(slotFields)
+		slot.SetNoalg(true)
+
+		slotArr := types.NewArray(slot, abi.MapGroupSlots)
+		slotArr.SetNoalg(true)
+
+		fields = []*types.Field{
+			makefield("ctrl", types.Types[types.TUINT64]),
+			makefield("slots", slotArr),
+		}
 	}
 
 	group := types.NewStruct(fields)
@@ -269,13 +291,30 @@ func writeMapType(t *types.Type, lsym *obj.LSym, c rttype.Cursor) {
 	s3 := writeType(gtyp)
 	hasher := genhash(t.Key())
 
-	slotTyp := gtyp.Field(1).Type.Elem()
-	elemOff := slotTyp.Field(1).Offset
-	if types.AlgType(t.Key()) == types.AMEM && t.Key().Size() == 8 && elemOff != 8 {
-		base.Fatalf("runtime assumes elemOff for 8-byte keys is 8, got %d", elemOff)
-	}
-	if types.AlgType(t.Key()) == types.ASTRING && elemOff != int64(2*types.PtrSize) {
-		base.Fatalf("runtime assumes elemOff for string keys is %d, got %d", 2*types.PtrSize, elemOff)
+	var keysOff int64
+	var keyStride int64
+	var elemsOff int64
+	var elemStride int64
+	var elemOff int64
+	if buildcfg.Experiment.MapSplitGroup {
+		// Split layout: field 1 is keys array, field 2 is elems array.
+		keysOff = gtyp.Field(1).Offset
+		keyStride = gtyp.Field(1).Type.Elem().Size()
+		elemsOff = gtyp.Field(2).Offset
+		elemStride = gtyp.Field(2).Type.Elem().Size()
+	} else {
+		// Interleaved layout: field 1 is slots array.
+		// KeysOff = offset of slots array (first key).
+		// KeyStride = ElemStride = slot stride.
+		// ElemsOff = offset of slots + offset of elem within slot.
+		keysOff = gtyp.Field(1).Offset
+		slotTyp := gtyp.Field(1).Type.Elem()
+		slotSize := slotTyp.Size()
+		elemOffInSlot := slotTyp.Field(1).Offset
+		keyStride = slotSize
+		elemsOff = keysOff + elemOffInSlot
+		elemStride = slotSize
+		elemOff = slotTyp.Field(1).Offset
 	}
 
 	c.Field("Key").WritePtr(s1)
@@ -283,7 +322,10 @@ func writeMapType(t *types.Type, lsym *obj.LSym, c rttype.Cursor) {
 	c.Field("Group").WritePtr(s3)
 	c.Field("Hasher").WritePtr(hasher)
 	c.Field("GroupSize").WriteUintptr(uint64(gtyp.Size()))
-	c.Field("SlotSize").WriteUintptr(uint64(slotTyp.Size()))
+	c.Field("KeysOff").WriteUintptr(uint64(keysOff))
+	c.Field("KeyStride").WriteUintptr(uint64(keyStride))
+	c.Field("ElemsOff").WriteUintptr(uint64(elemsOff))
+	c.Field("ElemStride").WriteUintptr(uint64(elemStride))
 	c.Field("ElemOff").WriteUintptr(uint64(elemOff))
 	var flags uint32
 	if needkeyupdate(t.Key()) {

src/internal/abi/map.go
@@ -37,9 +37,27 @@ type MapType struct {
 	// function for hashing keys (ptr to key, seed) -> hash
 	Hasher    func(unsafe.Pointer, uintptr) uintptr
 	GroupSize uintptr // == Group.Size_
-	SlotSize  uintptr // size of key/elem slot
-	ElemOff   uintptr // offset of elem in key/elem slot
-	Flags     uint32
+	// These fields describe how to access keys and elems within a group.
+	// The formulas key(i) = KeysOff + i*KeyStride and
+	// elem(i) = ElemsOff + i*ElemStride work for both group layouts:
+	//
+	// With GOEXPERIMENT=mapsplitgroup (split arrays KKKKVVVV):
+	//   KeysOff    = offset of keys array in group
+	//   KeyStride  = size of a single key
+	//   ElemsOff   = offset of elems array in group
+	//   ElemStride = size of a single elem
+	//
+	// Without (interleaved slots KVKVKVKV):
+	//   KeysOff    = offset of slots array in group
+	//   KeyStride  = size of a key/elem slot (stride between keys)
+	//   ElemsOff   = offset of first elem (slots offset + elem offset within slot)
+	//   ElemStride = size of a key/elem slot (stride between elems)
+	KeysOff    uintptr
+	KeyStride  uintptr
+	ElemsOff   uintptr
+	ElemStride uintptr
+	ElemOff    uintptr // GOEXPERIMENT=nomapsplitgroup only
+	Flags      uint32
 }
 
 // Flag values

src/internal/goexperiment/exp_mapsplitgroup_off.go
@@ -1,0 +1,8 @@
+// Code generated by mkconsts.go. DO NOT EDIT.
+
+//go:build !goexperiment.mapsplitgroup
+
+package goexperiment
+
+const MapSplitGroup = false
+const MapSplitGroupInt = 0

src/internal/goexperiment/exp_mapsplitgroup_on.go
@@ -1,0 +1,8 @@
+// Code generated by mkconsts.go. DO NOT EDIT.
+
+//go:build goexperiment.mapsplitgroup
+
+package goexperiment
+
+const MapSplitGroup = true
+const MapSplitGroupInt = 1

src/internal/goexperiment/flags.go
@@ -128,4 +128,9 @@ type Flags struct {
 
 	// RuntimeSecret enables the runtime/secret package.
 	RuntimeSecret bool
+
+	// MapSplitGroup changes the internal representation of map groups
+	// from interleaved key/elem slots (KVKVKVKV) to split key and elem
+	// arrays (KKKKVVVV).
+	MapSplitGroup bool
 }

src/internal/runtime/maps/group.go
@@ -242,24 +242,28 @@ func ctrlGroupMatchFull(g ctrlGroup) bitset {
 // control word.
 type groupReference struct {
 	// data points to the group, which is described by typ.Group and has
-	// layout:
+	// layout depending on GOEXPERIMENT=mapsplitgroup:
 	//
+	// With mapsplitgroup (split arrays):
 	// type group struct {
 	// 	ctrls ctrlGroup
-	// 	slots [abi.MapGroupSlots]slot
+	// 	keys  [abi.MapGroupSlots]typ.Key
+	// 	elems [abi.MapGroupSlots]typ.Elem
 	// }
 	//
-	// type slot struct {
-	// 	key  typ.Key
-	// 	elem typ.Elem
+	// Without (interleaved slots):
+	// type group struct {
+	// 	ctrls ctrlGroup
+	// 	slots [abi.MapGroupSlots]struct {
+	// 		key  typ.Key
+	// 		elem typ.Elem
+	// 	}
 	// }
+	//
+	// In both cases, key(i) and elem(i) use the same formula via
+	// typ.KeysOff/KeyStride and typ.ElemsOff/ElemStride.
 	data unsafe.Pointer // data *typ.Group
 }
-
-const (
-	ctrlGroupsSize   = unsafe.Sizeof(ctrlGroup(0))
-	groupSlotsOffset = ctrlGroupsSize
-)
 
 // alignUp rounds n up to a multiple of a. a must be a power of 2.
 func alignUp(n, a uintptr) uintptr {
@@ -287,14 +291,14 @@ func (g *groupReference) ctrls() *ctrlGroup {
 
 // key returns a pointer to the key at index i.
 func (g *groupReference) key(typ *abi.MapType, i uintptr) unsafe.Pointer {
-	offset := groupSlotsOffset + i*typ.SlotSize
+	offset := typ.KeysOff + i*typ.KeyStride
 
 	return unsafe.Pointer(uintptr(g.data) + offset)
 }
 
 // elem returns a pointer to the element at index i.
 func (g *groupReference) elem(typ *abi.MapType, i uintptr) unsafe.Pointer {
-	offset := groupSlotsOffset + i*typ.SlotSize + typ.ElemOff
+	offset := typ.ElemsOff + i*typ.ElemStride
 
 	return unsafe.Pointer(uintptr(g.data) + offset)
 }

src/internal/runtime/maps/runtime.go
@@ -7,6 +7,7 @@ package maps
 import (
 	"internal/abi"
 	"internal/asan"
+	"internal/goexperiment"
 	"internal/msan"
 	"internal/race"
 	"internal/runtime/sys"
@@ -124,7 +125,12 @@ func runtime_mapaccess2(typ *abi.MapType, m *Map, key unsafe.Pointer) (unsafe.Po
 				slotKey = *((*unsafe.Pointer)(slotKey))
 			}
 			if typ.Key.Equal(key, slotKey) {
-				slotElem := unsafe.Pointer(uintptr(slotKeyOrig) + typ.ElemOff)
+				var slotElem unsafe.Pointer
+				if goexperiment.MapSplitGroup {
+					slotElem = g.elem(typ, i)
+				} else {
+					slotElem = unsafe.Pointer(uintptr(slotKeyOrig) + typ.ElemOff)
+				}
 				if typ.IndirectElem() {
 					slotElem = *((*unsafe.Pointer)(slotElem))
 				}
@@ -223,7 +229,11 @@ outer:
 						typedmemmove(typ.Key, slotKey, key)
 					}
 
-					slotElem = unsafe.Pointer(uintptr(slotKeyOrig) + typ.ElemOff)
+					if goexperiment.MapSplitGroup {
+						slotElem = g.elem(typ, i)
+					} else {
+						slotElem = unsafe.Pointer(uintptr(slotKeyOrig) + typ.ElemOff)
+					}
 					if typ.IndirectElem() {
 						slotElem = *((*unsafe.Pointer)(slotElem))
 					}
@@ -265,7 +275,11 @@ outer:
 					}
 					typedmemmove(typ.Key, slotKey, key)
 
-					slotElem = unsafe.Pointer(uintptr(slotKeyOrig) + typ.ElemOff)
+					if goexperiment.MapSplitGroup {
+						slotElem = g.elem(typ, i)
+					} else {
+						slotElem = unsafe.Pointer(uintptr(slotKeyOrig) + typ.ElemOff)
+					}
 					if typ.IndirectElem() {
 						emem := newobject(typ.Elem)
 						*(*unsafe.Pointer)(slotElem) = emem

commit 3390ec59af33d3c9ab431ff88a245b710b7c9e40

internal/abi, cmd/link: centralize type descriptor size calculation
The linker hardcoded the calculation for some types, which caused me
some annoyance in CL 711560 because I didn't know it needed to be
updated (and it took me a long time to find the hardcoded value).

Move this calculation over to the abi package, similar to other funcs.
Then update rttype.Init to also check that the function works, like
again like other funcs in this package.

This actually caught a latent bug; decodetypeMethods was incorrectly
calculating the size for the Interface case!

Change-Id: Iaa66055061b68ec93c9912ae6aa6e605260f52a8
Reviewed-on: https://go-review.googlesource.com/c/go/+/749961
LUCI-TryBot-Result: Go LUCI <golang-scoped@luci-project-accounts.iam.gserviceaccount.com>
Reviewed-by: David Chase <drchase@google.com>
Reviewed-by: Cherry Mui <cherryyz@google.com>

src/cmd/compile/internal/rttype/rttype.go
@@ -88,6 +88,24 @@ func Init() {
 	if got, want := int64(abi.ITabTypeOff(ptrSize)), ITab.OffsetOf("Type"); got != want {
 		base.Fatalf("abi.ITabTypeOff() == %d, want %d", got, want)
 	}
+	for _, test := range []struct {
+		kind abi.Kind
+		typ  *types.Type
+		name string
+	}{
+		{abi.Struct, StructType, "Struct"},
+		{abi.Pointer, PtrType, "Pointer"},
+		{abi.Func, FuncType, "Func"},
+		{abi.Slice, SliceType, "Slice"},
+		{abi.Array, ArrayType, "Array"},
+		{abi.Chan, ChanType, "Chan"},
+		{abi.Map, MapType, "Map"},
+		{abi.Interface, InterfaceType, "Interface"},
+	} {
+		if got, want := int64(abi.RTypeSize(test.kind, ptrSize)), test.typ.Size(); got != want {
+			base.Fatalf("abi.RTypeSize(%s) == %d, want %d", test.name, got, want)
+		}
+	}
 }
 
 // FromReflect translates from a host type to the equivalent target type.

src/cmd/link/internal/ld/deadcode.go
@@ -545,30 +545,7 @@ func (d *deadcodePass) decodetypeMethods(ldr *loader.Loader, arch *sys.Arch, sym
 	if !decodetypeHasUncommon(arch, p) {
 		panic(fmt.Sprintf("no methods on %q", ldr.SymName(symIdx)))
 	}
-	off := commonsize(arch) // reflect.rtype
-	switch decodetypeKind(arch, p) {
-	case abi.Struct: // reflect.structType
-		off += 4 * arch.PtrSize
-	case abi.Pointer: // reflect.ptrType
-		off += arch.PtrSize
-	case abi.Func: // reflect.funcType
-		off += arch.PtrSize // 4 bytes, pointer aligned
-	case abi.Slice: // reflect.sliceType
-		off += arch.PtrSize
-	case abi.Array: // reflect.arrayType
-		off += 3 * arch.PtrSize
-	case abi.Chan: // reflect.chanType
-		off += 2 * arch.PtrSize
-	case abi.Map:
-		off += 10*arch.PtrSize + 4 // internal/abi.MapType
-		if arch.PtrSize == 8 {
-			off += 4 // padding for final uint32 field (Flags).
-		}
-	case abi.Interface: // reflect.interfaceType
-		off += 3 * arch.PtrSize
-	default:
-		// just Sizeof(rtype)
-	}
+	off := abi.RTypeSize(decodetypeKind(arch, p), arch.PtrSize)
 
 	mcount := int(decodeInuxi(arch, p[off+4:], 2))
 	moff := int(decodeInuxi(arch, p[off+4+2+2:], 4))

src/cmd/link/internal/ld/decodesym.go
@@ -120,10 +120,7 @@ func decodetypeNameEmbedded(ldr *loader.Loader, symIdx loader.Sym, relocs *loade
 }
 
 func decodetypeFuncInType(ldr *loader.Loader, arch *sys.Arch, symIdx loader.Sym, relocs *loader.Relocs, i int) loader.Sym {
-	uadd := commonsize(arch) + 4
-	if arch.PtrSize == 8 {
-		uadd += 4
-	}
+	uadd := abi.RTypeSize(abi.Func, arch.PtrSize)
 	if decodetypeHasUncommon(arch, ldr.Data(symIdx)) {
 		uadd += uncommonSize(arch)
 	}
@@ -175,7 +172,7 @@ func decodetypeStructFieldCount(ldr *loader.Loader, arch *sys.Arch, symIdx loade
 
 func decodetypeStructFieldArrayOff(ldr *loader.Loader, arch *sys.Arch, symIdx loader.Sym, i int) int {
 	data := ldr.Data(symIdx)
-	off := commonsize(arch) + 4*arch.PtrSize
+	off := abi.RTypeSize(abi.Struct, arch.PtrSize)
 	if decodetypeHasUncommon(arch, data) {
 		off += uncommonSize(arch)
 	}

src/internal/abi/compiletype.go
@@ -26,3 +26,33 @@ func TFlagOff(ptrSize int) int { return 2*ptrSize + 4 }
 
 // ITabTypeOff returns the offset of ITab.Type for a compilation target with a given ptrSize
 func ITabTypeOff(ptrSize int) int { return ptrSize }
+
+// RTypeSize returns sizeof(kindType) for a compilation target with a given ptrSize.
+func RTypeSize(kind Kind, ptrSize int) int {
+	cs := CommonSize(ptrSize)
+	switch kind {
+	case Struct: // reflect.structType
+		return cs + 4*ptrSize
+	case Pointer: // reflect.ptrType
+		return cs + ptrSize
+	case Func: // reflect.funcType
+		return cs + ptrSize // 4 bytes, pointer aligned
+	case Slice: // reflect.sliceType
+		return cs + ptrSize
+	case Array: // reflect.arrayType
+		return cs + 3*ptrSize
+	case Chan: // reflect.chanType
+		return cs + 2*ptrSize
+	case Map: // internal/abi.MapType
+		sz := cs + 10*ptrSize + 4
+		if ptrSize == 8 {
+			sz += 4 // padding for final uint32 field (Flags).
+		}
+		return sz
+	case Interface: // reflect.interfaceType
+		return cs + 4*ptrSize
+	default:
+		// just Sizeof(rtype)
+		return cs
+	}
+}