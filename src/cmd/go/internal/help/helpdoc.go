068f27a8e06e58361bcc2d5e7d17deafab4b52bc
Michael Matloob <matloob@golang.org>
2026-03-24 12:22:25 -0400

    cmd/go: add information about GODEBUGs to go help gopath
    And make it clear that standard library tests won't run properly with
    GO111MODULE=off.
    
    Fixes #73973
    Fixes #69202
    
    Change-Id: I8c71c739e1da80fbf2e13f84ecaf9c346a6a6964
    Reviewed-on: https://go-review.googlesource.com/c/go/+/758720
    Reviewed-by: Dmitri Shuralyov <dmitshur@google.com>
    Reviewed-by: Dmitri Shuralyov <dmitshur@golang.org>
    Reviewed-by: Michael Matloob <matloob@google.com>
    LUCI-TryBot-Result: Go LUCI <golang-scoped@luci-project-accounts.iam.gserviceaccount.com>
    Auto-Submit: Michael Matloob <matloob@google.com>

--- src/cmd/go/internal/help/helpdoc.go
+++ src/cmd/go/internal/help/helpdoc.go
@@ -524,6 +524,13 @@ top-level "crash/bang".
 Code in GOPATH mode vendor directories is not subject to
 GOPATH mode import path checking (see 'go help importpath').
 
+In GOPATH mode, the default GODEBUG values built into a binary
+will be those used in Go 1.20, setting the same GODEBUG values
+as when a module specifies "godebug default=go1.20". To use
+different GODEBUG settings, the GODEBUG environment variable must
+be set to override those values. This also means that the standard
+library tests will not run properly with GO111MODULE=off.
+
 See https://go.dev/s/go15vendor for details.
 
 See https://go.dev/ref/mod#vendoring for details about vendoring in
