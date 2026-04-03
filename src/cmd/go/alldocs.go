9956aca06ab9d3083fa0afaebb640a1a25dde77f
Sean Liao <sean@liao.dev>
2026-04-04 00:55:05 +0100

    cmd/go/internal/workcmd: correct go.work file name
    Fixes #51714
    
    Change-Id: I0619b1ca6715bed85ccc066a83a449f66a6a6964
    Reviewed-on: https://go-review.googlesource.com/c/go/+/762660
    Reviewed-by: David Chase <drchase@google.com>
    LUCI-TryBot-Result: Go LUCI <golang-scoped@luci-project-accounts.iam.gserviceaccount.com>
    Reviewed-by: Michael Matloob <matloob@google.com>
    Reviewed-by: Michael Matloob <matloob@golang.org>

--- src/cmd/go/alldocs.go
+++ src/cmd/go/alldocs.go
@@ -1634,10 +1634,10 @@
 // The -toolchain=name flag sets the Go toolchain to use.
 //
 // The -print flag prints the final go.work in its text format instead of
-// writing it back to go.mod.
+// writing it back to go.work.
 //
 // The -json flag prints the final go.work file in JSON format instead of
-// writing it back to go.mod. The JSON output corresponds to these Go types:
+// writing it back to go.work. The JSON output corresponds to these Go types:
 //
 //	type GoWork struct {
 //		Go        string
