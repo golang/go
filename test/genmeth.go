6f662188c2e2640712af74a01f589ee31ba0282f
Mark Freeman <mark@golang.org>
2026-04-06 15:23:04 -0400

    test: add a simple test using generic methods
    Change-Id: Ia22aff4d7b6d6f9250c3915fba947fcc3bd800d5
    Reviewed-on: https://go-review.googlesource.com/c/go/+/763180
    Reviewed-by: Robert Griesemer <gri@google.com>
    LUCI-TryBot-Result: Go LUCI <golang-scoped@luci-project-accounts.iam.gserviceaccount.com>

---
+++ test/genmeth.go
@@ -0,0 +1,47 @@
+//run
+
+// Copyright 2026 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Verify that generic methods are assembled correctly.
+
+package main
+
+import "fmt"
+
+type List[E any] []E
+
+func (l List[E]) apply[P any](f func(E) P) List[P] {
+	r := make(List[P], len(l))
+	for i, x := range l {
+		r[i] = f(x)
+	}
+	return r
+}
+
+func (l List[E]) print() string {
+	s := ""
+	for i, x := range l {
+		if i > 0 {
+			s += "->"
+		}
+		s += fmt.Sprint(x)
+	}
+	return s
+}
+
+func main() {
+	l := make(List[int], 3)
+	for i := range l {
+		l[i] = i
+	}
+
+	f := func(i int) string {
+		return string("abc"[i])
+	}
+
+	if r := l.apply(f).print(); r != "a->b->c" {
+		panic(fmt.Sprintf("got %s, want a->b->c", r))
+	}
+}
