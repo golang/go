d7d918d4ede9d7a374a0a51f569c6890d74c8f91
Weixie Cui <cuiweixie@gmail.com>
2026-04-09 21:28:03 +0800

    os/exec: doc typo

--- src/os/exec/exec.go
+++ src/os/exec/exec.go
@@ -1268,7 +1268,7 @@ func dedupEnv(env []string) ([]string, error) {
 
 // dedupEnvCase is dedupEnv with a case option for testing.
 // If caseInsensitive is true, the case of keys is ignored.
-// If nulOK is false, items containing NUL characters are allowed.
+// If nulOK is false, items containing NUL characters are rejected.
 func dedupEnvCase(caseInsensitive, nulOK bool, env []string) ([]string, error) {
 	// Construct the output in reverse order, to preserve the
 	// last occurrence of each key.
