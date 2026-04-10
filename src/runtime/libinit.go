golang/go

commit 6bdfdb5f51951584732e6a95a7ee8a5ba5e8cb93

runtime: implement part of library initialization in Go
All architectures supporting c-shared and c-archive share the same
initialization code in assembly, and most of it can be implemented in
pure Go.

Cq-Include-Trybots: luci.golang.try:gotip-darwin-arm64-longtest,gotip-linux-ppc64le_power10,gotip-linux-riscv64,gotip-linux-loong64,gotip-linux-s390x
Change-Id: Iaa9fb7d6f9ca8785f1098461646d607ef6b00d47
Reviewed-on: https://go-review.googlesource.com/c/go/+/706417
Auto-Submit: Quim Muntal <quimmuntal@gmail.com>
Reviewed-by: Cherry Mui <cherryyz@google.com>
LUCI-TryBot-Result: Go LUCI <golang-scoped@luci-project-accounts.iam.gserviceaccount.com>
Reviewed-by: Carlos Amedee <carlos@golang.org>

src/runtime/os_aix.go
@@ -112,7 +112,7 @@ func getCPUCount() int32 {
 // This function is not safe to use after initialization as it does not pass an M as fnarg.
 //
 //go:nosplit
-func newosproc0(stacksize uintptr, fn *funcDescriptor) {
+func newosproc0(stacksize uintptr, fn unsafe.Pointer) {
 	var (
 		attr pthread_attr
 		oset sigset
@@ -226,7 +226,7 @@ func newosproc(mp *m) {
 	// with signals disabled. It will enable them in minit.
 	sigprocmask(_SIG_SETMASK, &sigset_all, &oset)
 	ret := retryOnEAGAIN(func() int32 {
-		return pthread_create(&tid, &attr, &tstart, unsafe.Pointer(mp))
+		return pthread_create(&tid, &attr, unsafe.Pointer(&tstart), unsafe.Pointer(mp))
 	})
 	sigprocmask(_SIG_SETMASK, &oset, nil)
 	if ret != 0 {

src/runtime/asm_386.s
@@ -36,48 +36,17 @@ TEXT _rt0_386_lib(SB),NOSPLIT,$0
 	MOVL	12(BP), AX
 	MOVL	AX, _rt0_386_lib_argv<>(SB)
 
-	// Synchronous initialization.
-	CALL	runtime·libpreinit(SB)
+	CALL	runtime·libInit(SB)
 
-	SUBL	$8, SP
-
-	// Create a new thread to do the runtime initialization.
-	MOVL	_cgo_sys_thread_create(SB), AX
-	TESTL	AX, AX
-	JZ	nocgo
-
-	// Align stack to call C function.
-	// We moved SP to BP above, but BP was clobbered by the libpreinit call.
-	MOVL	SP, BP
-	ANDL	$~15, SP
-
-	MOVL	$_rt0_386_lib_go(SB), BX
-	MOVL	BX, 0(SP)
-	MOVL	$0, 4(SP)
-
-	CALL	AX
-
-	MOVL	BP, SP
-
-	JMP	restore
-
-nocgo:
-	MOVL	$0x800000, 0(SP)                    // stacksize = 8192KB
-	MOVL	$_rt0_386_lib_go(SB), AX
-	MOVL	AX, 4(SP)                           // fn
-	CALL	runtime·newosproc0(SB)
-
-restore:
-	ADDL	$8, SP
 	POPL	DI
 	POPL	SI
 	POPL	BX
 	POPL	BP
 	RET
 
-// _rt0_386_lib_go initializes the Go runtime.
+// rt0_lib_go initializes the Go runtime.
 // This is started in a separate thread by _rt0_386_lib.
-TEXT _rt0_386_lib_go(SB),NOSPLIT,$8
+TEXT runtime·rt0_lib_go<ABIInternal>(SB),NOSPLIT,$8
 	MOVL	_rt0_386_lib_argc<>(SB), AX
 	MOVL	AX, 0(SP)
 	MOVL	_rt0_386_lib_argv<>(SB), AX

src/runtime/asm_amd64.s
@@ -36,55 +36,22 @@ TEXT _rt0_amd64_lib(SB),NOSPLIT|NOFRAME,$0
 	MOVQ	DI, _rt0_amd64_lib_argc<>(SB)
 	MOVQ	SI, _rt0_amd64_lib_argv<>(SB)
 
-	// Synchronous initialization.
-#ifndef GOOS_windows
-	// Avoid calling it on Windows because it is not used
-	// and it would crash the application due to the autogenerated
-	// ABI wrapper trying to access a non-existent TLS slot.
-	CALL	runtime·libpreinit(SB)
-#endif
-
-	// Create a new thread to finish Go runtime initialization.
-	MOVQ	_cgo_sys_thread_create(SB), AX
-	TESTQ	AX, AX
-	JZ	nocgo
-
-	// We're calling back to C.
-	// Align stack per C ABI requirements.
-	MOVQ	SP, BX  // Callee-save in C ABI
-	ANDQ	$~15, SP
-	MOVQ	$_rt0_amd64_lib_go(SB), DI
-	MOVQ	$0, SI
 #ifdef GOOS_windows
-	// For Windows ABI
-	MOVQ	DI, CX
-	MOVQ	SI, DX
-	// Leave space for four words on the stack as required
-	// by the Windows amd64 calling convention.
-	ADJSP	$32
+	// Set up a dummy TLS value on Windows so that the autogenerated
+	// ABI wrappers don't crash when trying to load G from TLS before
+	// wintls has set up the real TLS slot in rt0_go.
+	MOVQ	$zeroTLS<>(SB), DI
+	CALL	runtime·settls(SB)
 #endif
-	CALL	AX
-#ifdef GOOS_windows
-	ADJSP	$-32 // just to make the assembler not complain about unbalanced stack
-#endif
-	MOVQ	BX, SP
-	JMP	restore
 
-nocgo:
-	ADJSP	$16
-	MOVQ	$0x800000, 0(SP)		// stacksize
-	MOVQ	$_rt0_amd64_lib_go(SB), AX
-	MOVQ	AX, 8(SP)			// fn
-	CALL	runtime·newosproc0(SB)
-	ADJSP	$-16
+	CALL	runtime·libInit(SB)
 
-restore:
 	POP_REGS_HOST_TO_ABI0()
 	RET
 
-// _rt0_amd64_lib_go initializes the Go runtime.
+// rt0_lib_go initializes the Go runtime.
 // This is started in a separate thread by _rt0_amd64_lib.
-TEXT _rt0_amd64_lib_go(SB),NOSPLIT,$0
+TEXT runtime·rt0_lib_go<ABIInternal>(SB),NOSPLIT,$0
 	MOVQ	_rt0_amd64_lib_argc<>(SB), DI
 	MOVQ	_rt0_amd64_lib_argv<>(SB), SI
 	JMP	runtime·rt0_go(SB)

src/runtime/asm_arm.s
@@ -58,24 +58,8 @@ skipfpsave:
 
 	MOVW	$0, g // Initialize g.
 
-	// Synchronous initialization.
-	CALL	runtime·libpreinit(SB)
+	CALL	runtime·libInit(SB)
 
-	// Create a new thread to do the runtime initialization.
-	MOVW	_cgo_sys_thread_create(SB), R2
-	CMP	$0, R2
-	BEQ	nocgo
-	MOVW	$_rt0_arm_lib_go<>(SB), R0
-	MOVW	$0, R1
-	BL	(R2)
-	B	rr
-nocgo:
-	MOVW	$0x800000, R0                     // stacksize = 8192KB
-	MOVW	$_rt0_arm_lib_go<>(SB), R1  // fn
-	MOVW	R0, 4(R13)
-	MOVW	R1, 8(R13)
-	BL	runtime·newosproc0(SB)
-rr:
 	// Restore callee-save registers and return.
 	MOVB    runtime·goarmsoftfp(SB), R11
 	CMP     $0, R11
@@ -98,9 +82,9 @@ skipfprest:
 	MOVW	36(R13), R11
 	RET
 
-// _rt0_arm_lib_go initializes the Go runtime.
+// rt0_lib_go initializes the Go runtime.
 // This is started in a separate thread by _rt0_arm_lib.
-TEXT _rt0_arm_lib_go<>(SB),NOSPLIT,$8
+TEXT runtime·rt0_lib_go<ABIInternal>(SB),NOSPLIT,$8
 	MOVW	_rt0_arm_lib_argc<>(SB), R0
 	MOVW	_rt0_arm_lib_argv<>(SB), R1
 	B	runtime·rt0_go(SB)

src/runtime/asm_arm64.s
@@ -41,35 +41,15 @@ TEXT _rt0_arm64_lib(SB),NOSPLIT,$184
 	MOVD	R0, _rt0_arm64_lib_argc<>(SB)
 	MOVD	R1, _rt0_arm64_lib_argv<>(SB)
 
-	// Synchronous initialization.
-	MOVD	$runtime·libpreinit(SB), R4
+	MOVD	$runtime·libInit(SB), R4
 	BL	(R4)
 
-	// Create a new thread to do the runtime initialization and return.
-	MOVD	_cgo_sys_thread_create(SB), R4
-	CBZ	R4, nocgo
-	MOVD	$_rt0_arm64_lib_go(SB), R0
-	MOVD	$0, R1
-	SUB	$16, RSP		// reserve 16 bytes for sp-8 where fp may be saved.
-	BL	(R4)
-	ADD	$16, RSP
-	B	restore
-
-nocgo:
-	MOVD	$0x800000, R0                     // stacksize = 8192KB
-	MOVD	$_rt0_arm64_lib_go(SB), R1
-	MOVD	R0, 8(RSP)
-	MOVD	R1, 16(RSP)
-	MOVD	$runtime·newosproc0(SB),R4
-	BL	(R4)
-
-restore:
 	// Restore callee-save registers.
 	RESTORE_R19_TO_R28(24)
 	RESTORE_F8_TO_F15(104)
 	RET
 
-TEXT _rt0_arm64_lib_go(SB),NOSPLIT,$0
+TEXT runtime·rt0_lib_go<ABIInternal>(SB),NOSPLIT,$0
 	MOVD	_rt0_arm64_lib_argc<>(SB), R0
 	MOVD	_rt0_arm64_lib_argv<>(SB), R1
 	MOVD	$runtime·rt0_go(SB),R4

src/runtime/asm_loong64.s
@@ -21,33 +21,15 @@ TEXT _rt0_loong64_lib(SB),NOSPLIT,$168
 	MOVV	R4, _rt0_loong64_lib_argc<>(SB)
 	MOVV	R5, _rt0_loong64_lib_argv<>(SB)
 
-	// Synchronous initialization.
-	MOVV	$runtime·libpreinit(SB), R19
+	MOVV	$runtime·libInit(SB), R19
 	JAL	(R19)
 
-	// Create a new thread to do the runtime initialization and return.
-	MOVV	_cgo_sys_thread_create(SB), R19
-	BEQ	R19, nocgo
-	MOVV	$_rt0_loong64_lib_go(SB), R4
-	MOVV	$0, R5
-	JAL	(R19)
-	JMP	restore
-
-nocgo:
-	MOVV	$0x800000, R4                     // stacksize = 8192KB
-	MOVV	$_rt0_loong64_lib_go(SB), R5
-	MOVV	R4, 8(R3)
-	MOVV	R5, 16(R3)
-	MOVV	$runtime·newosproc0(SB), R19
-	JAL	(R19)
-
-restore:
 	// Restore callee-save registers.
 	RESTORE_R22_TO_R31(3*8)
 	RESTORE_F24_TO_F31(13*8)
 	RET
 
-TEXT _rt0_loong64_lib_go(SB),NOSPLIT,$0
+TEXT runtime·rt0_lib_go<ABIInternal>(SB),NOSPLIT,$0
 	MOVV	_rt0_loong64_lib_argc<>(SB), R4
 	MOVV	_rt0_loong64_lib_argv<>(SB), R5
 	MOVV	$runtime·rt0_go(SB),R19

src/runtime/asm_ppc64x.s
@@ -31,53 +31,26 @@ TEXT _rt0_ppc64x_lib(SB),NOSPLIT|NOFRAME,$0
 	MOVD	R12, CTR
 	BL	(CTR)
 
+	// Initialize g as nil in case of using g later e.g. sigaction in cgo_sigaction.go
+	MOVD	R0, g
+
 #ifdef GOOS_aix
 	// See runtime/cgo/gcc_aix_ppc64.c
 	MOVBZ	runtime·isarchive(SB), R3	// Check buildmode = c-archive
 	CMP		$0, R3
-	BEQ		done
+	BEQ		skipInit
 #endif
 
-	MOVD	$runtime·libpreinit(SB), R12
+	MOVD	$runtime·libInit(SB), R12
 	MOVD	R12, CTR
 	BL	(CTR)
 
-	// Create a new thread to do the runtime initialization and return.
-	// _cgo_sys_thread_create is a C function.
-	MOVD	_cgo_sys_thread_create(SB), R12
-	CMP	$0, R12
-	BEQ	nocgo
-	MOVD	$_rt0_ppc64x_lib_go(SB), R3
-	MOVD	$0, R4
-#ifdef GO_PPC64X_HAS_FUNCDESC
-	// Load the real entry address from the first slot of the function descriptor.
-	MOVD	8(R12), R2
-	MOVD	(R12), R12
-#endif
-	MOVD	R12, CTR
-	BL	(CTR)
-	MOVD	24(R1), R2 // Restore the old frame, and R2.
-	BR	done
-
-nocgo:
-	MOVD	$0x800000, R12                     // stacksize = 8192KB
-	MOVD	R12, 8+FIXED_FRAME(R1)
-	MOVD	$_rt0_ppc64x_lib_go(SB), R12
-	MOVD	R12, 16+FIXED_FRAME(R1)
-	MOVD	$runtime·newosproc0(SB),R12
-	MOVD	R12, CTR
-	BL	(CTR)
-
-done:
+skipInit:
+	// Restore and return to ELFv2 caller.
 	UNSTACK_AND_RESTORE_GO_TO_HOST_ABI(32)
 	RET
 
-#ifdef GO_PPC64X_HAS_FUNCDESC
-DEFINE_PPC64X_FUNCDESC(_rt0_ppc64x_lib_go, __rt0_ppc64x_lib_go)
-TEXT __rt0_ppc64x_lib_go(SB),NOSPLIT,$0
-#else
-TEXT _rt0_ppc64x_lib_go(SB),NOSPLIT,$0
-#endif
+TEXT runtime·rt0_lib_go<ABIInternal>(SB),NOSPLIT,$0
 	MOVD	_rt0_ppc64x_lib_argc<>(SB), R3
 	MOVD	_rt0_ppc64x_lib_argv<>(SB), R4
 	MOVD	$runtime·rt0_go(SB), R12

src/runtime/asm_riscv64.s
@@ -22,27 +22,9 @@ TEXT _rt0_riscv64_lib(SB),NOSPLIT,$224
 	MOV	A0, _rt0_riscv64_lib_argc<>(SB)
 	MOV	A1, _rt0_riscv64_lib_argv<>(SB)
 
-	// Synchronous initialization.
-	MOV	$runtime·libpreinit(SB), T1
+	MOV	$runtime·libInit(SB), T1
 	JALR	RA, T1
 
-	// Create a new thread to do the runtime initialization and return.
-	MOV	_cgo_sys_thread_create(SB), T1
-	BEQZ	T1, nocgo
-	MOV	$_rt0_riscv64_lib_go(SB), A0
-	MOV	$0, A1
-	JALR	RA, T1
-	JMP	restore
-
-nocgo:
-	MOV	$0x800000, A0                     // stacksize = 8192KB
-	MOV	$_rt0_riscv64_lib_go(SB), A1
-	MOV	A0, 8(X2)
-	MOV	A1, 16(X2)
-	MOV	$runtime·newosproc0(SB), T1
-	JALR	RA, T1
-
-restore:
 	// Restore callee-save registers, along with X1 (LR).
 	MOV	(8*3)(X2), X1
 	RESTORE_GPR((8*4))
@@ -50,7 +32,7 @@ restore:
 
 	RET
 
-TEXT _rt0_riscv64_lib_go(SB),NOSPLIT,$0
+TEXT runtime·rt0_lib_go<ABIInternal>(SB),NOSPLIT,$0
 	MOV	_rt0_riscv64_lib_argc<>(SB), A0
 	MOV	_rt0_riscv64_lib_argv<>(SB), A1
 	MOV	$runtime·rt0_go(SB), T0

commit 0e31741044d519065f62a5e96499909d6cd230dc

runtime: fix AIX library initialization
On AIX, libInit passes a function descriptor instead of the function
pointer to pthread_create. This is a regression from CL 706417.

Change-Id: I660175eb992a41ef61b1927c51392887a724cd76
Reviewed-on: https://go-review.googlesource.com/c/go/+/761780
Reviewed-by: Ian Lance Taylor <iant@golang.org>
LUCI-TryBot-Result: Go LUCI <golang-scoped@luci-project-accounts.iam.gserviceaccount.com>
Reviewed-by: Dmitri Shuralyov <dmitshur@google.com>
Reviewed-by: David Chase <drchase@google.com>

src/runtime/libinit.go
@@ -11,6 +11,13 @@ import (
 	"unsafe"
 )
 
+// rt0LibGoFn holds the function pointer to rt0_lib_go suitable for thread
+// creation. On most platforms it is zero, meaning the raw code address should
+// be used. On AIX it is set by libpreinit to a function descriptor pointer,
+// because pthread_create on AIX expects a function descriptor, not a raw
+// code address.
+var rt0LibGoFn uintptr
+
 // libInit is common startup code for most architectures when
 // using -buildmode=c-archive or -buildmode=c-shared.
 //
@@ -22,13 +29,20 @@ func libInit() {
 	// Synchronous initialization.
 	libpreinit()
 
+	// Use the platform-specific function pointer if set (e.g. AIX
+	// function descriptor), otherwise fall back to the raw code address.
+	fn := unsafe.Pointer(rt0LibGoFn)
+	if fn == nil {
+		fn = unsafe.Pointer(abi.FuncPCABIInternal(rt0_lib_go))
+	}
+
 	// Asynchronous initialization.
 	// Prefer creating a thread via cgo if it is available.
 	if _cgo_sys_thread_create != nil {
 		// No g because the TLS is not set up until later in rt0_go.
-		asmcgocall_no_g(_cgo_sys_thread_create, unsafe.Pointer(abi.FuncPCABIInternal(rt0_lib_go)))
+		asmcgocall_no_g(_cgo_sys_thread_create, fn)
 	} else {
 		const stackSize = 0x800000 // 8192KB
-		newosproc0(stackSize, unsafe.Pointer(abi.FuncPCABIInternal(rt0_lib_go)))
+		newosproc0(stackSize, fn)
 	}
 }

src/runtime/os_aix.go
@@ -162,6 +162,10 @@ func newosproc0(stacksize uintptr, fn unsafe.Pointer) {
 //go:nosplit
 //go:nowritebarrierrec
 func libpreinit() {
+	// On AIX, pthread_create expects a function descriptor pointer,
+	// not a raw code address. Set rt0LibGoFn to the descriptor
+	// so that libInit passes the right value.
+	rt0LibGoFn = uintptr(unsafe.Pointer(&rt0LibGoDesc))
 	initsig(true)
 }
 
@@ -202,6 +206,9 @@ func mdestroy(mp *m) {
 
 // tstart is a function descriptor to _tstart defined in assembly.
 var tstart funcDescriptor
+
+// rt0LibGoDesc is a function descriptor to rt0_lib_go defined in assembly.
+var rt0LibGoDesc funcDescriptor
 
 func newosproc(mp *m) {
 	var (

src/runtime/sys_aix_ppc64.s
@@ -197,6 +197,15 @@ DATA	runtime·tstart+8(SB)/8, $TOC(SB)
 DATA	runtime·tstart+16(SB)/8, $0
 GLOBL	runtime·tstart(SB), NOPTR, $24
 
+// runtime.rt0LibGoDesc is a function descriptor to rt0_lib_go,
+// used by libInit to create a new thread on AIX where
+// pthread_create expects a function descriptor, not a raw
+// code address.
+DATA	runtime·rt0LibGoDesc+0(SB)/8, $runtime·rt0_lib_go(SB)
+DATA	runtime·rt0LibGoDesc+8(SB)/8, $TOC(SB)
+DATA	runtime·rt0LibGoDesc+16(SB)/8, $0
+GLOBL	runtime·rt0LibGoDesc(SB), NOPTR, $24
+
 TEXT tstart<>(SB),NOSPLIT,$0
 	XOR	 R0, R0 // reset R0
 