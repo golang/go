c1d5ab371cc1d9183772826608bf5099cd61252d
Mateusz Poliwczak <mpoliwczak34@gmail.com>
2026-05-06 18:28:51 +0200

    encoding/json/v2: make NotForPublicUse non-zero-sized
    Go spec allows two different pointers to zero-sized values
    to be equal or not.
    
    Currently this only triggers when Export does not inline.
    
    To avoid accidental exposure, make that type 1-byte.
    
    Change-Id: I6b23f72ce1d58e9f0619566fa903cc9c6a6a6964
    Reviewed-on: https://go-review.googlesource.com/c/go/+/774840
    Reviewed-by: Dmitri Shuralyov <dmitshur@google.com>
    Reviewed-by: Michael Pratt <mpratt@google.com>
    Auto-Submit: Mateusz Poliwczak <mpoliwczak34@gmail.com>
    LUCI-TryBot-Result: golang-scoped@luci-project-accounts.iam.gserviceaccount.com <golang-scoped@luci-project-accounts.iam.gserviceaccount.com>

--- src/encoding/json/internal/internal.go
+++ src/encoding/json/internal/internal.go
@@ -11,7 +11,8 @@ import "errors"
 // NotForPublicUse is a marker type that an API is for internal use only.
 // It does not perfectly prevent usage of that API, but helps to restrict usage.
 // Anything with this marker is not covered by the Go compatibility agreement.
-type NotForPublicUse struct{}
+// Not zero-sized type, since it is used as pointer-identity.
+type NotForPublicUse struct{ _ [1]byte }
 
 // AllowInternalUse is passed from "json" to "jsontext" to authenticate
 // that the caller can have access to internal functionality.
