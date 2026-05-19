37bce6617fb957b4d0c16cc10b269cb5b5975892
Robert Griesemer <gri@golang.org>
2026-05-19 14:34:18 -0700

    types2, go/types: add missing alias test to types2, simplify go/types test
    Change-Id: I4d3377e704d718a1914135a444b9df59f84af179
    Reviewed-on: https://go-review.googlesource.com/c/go/+/780120
    Reviewed-by: Robert Griesemer <gri@google.com>
    Auto-Submit: Robert Griesemer <gri@google.com>
    Reviewed-by: David Chase <drchase@google.com>
    LUCI-TryBot-Result: golang-scoped@luci-project-accounts.iam.gserviceaccount.com <golang-scoped@luci-project-accounts.iam.gserviceaccount.com>

--- src/cmd/compile/internal/types2/alias_test.go
+++ src/cmd/compile/internal/types2/alias_test.go
@@ -1,11 +1,11 @@
-// Copyright 2025 The Go Authors. All rights reserved.
+// Copyright 2026 The Go Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-package types_test
+package types2_test
 
 import (
-	"go/types"
+	"cmd/compile/internal/types2"
 	"testing"
 )
 
@@ -27,11 +27,11 @@ type A[T any] struct{}
 
 	pkg := mustTypecheck(src, nil, nil)
 	b := pkg.Scope().Lookup("B").Type()
-	if n, ok := b.(*types.Named); ok {
-		if s, ok := n.Underlying().(*types.Struct); ok {
+	if n, ok := b.(*types2.Named); ok {
+		if s, ok := n.Underlying().(*types2.Struct); ok {
 			got := s.Field(0).Type()
-			want := types.NewPointer(pkg.Scope().Lookup("AB").Type())
-			if !types.Identical(got, want) {
+			want := types2.NewPointer(pkg.Scope().Lookup("AB").Type())
+			if !types2.Identical(got, want) {
 				t.Errorf("wrong type for f: got %v, want %v", got, want)
 			}
 			return
@@ -48,11 +48,11 @@ type A = B // undeclared
 
 	pkg, _ := typecheck(src, nil, nil) // don't panic on error
 	a := pkg.Scope().Lookup("A").Type()
-	if alias, ok := a.(*types.Alias); ok {
+	if alias, ok := a.(*types2.Alias); ok {
 		got := alias.Rhs()
-		want := types.Typ[types.Invalid]
+		want := types2.Typ[types2.Invalid]
 
-		if !types.Identical(got, want) {
+		if !types2.Identical(got, want) {
 			t.Errorf("wrong type for B: got %v, want %v", got, want)
 		}
 		return
