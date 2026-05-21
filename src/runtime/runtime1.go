a5bc606a68917caf9bc21d2f090b3bdfef92aa42
Robert Griesemer <gri@golang.org>
2026-05-21 11:26:25 -0700

    runtime: remove asynctimerchan GODEBUG setting
    For #76472.
    
    Change-Id: Id6628c749f9794d32f86a4fca564deb0c71f4504
    Reviewed-on: https://go-review.googlesource.com/c/go/+/781580
    TryBot-Bypass: Dmitri Shuralyov <dmitshur@golang.org>
    Reviewed-by: Damien Neil <dneil@google.com>
    Reviewed-by: Robert Griesemer <gri@google.com>

--- src/runtime/runtime1.go
+++ src/runtime/runtime1.go
@@ -348,16 +348,6 @@ var debug struct {
 
 	panicnil atomic.Int32
 
-	// asynctimerchan controls whether timer channels
-	// behave asynchronously (as in Go 1.22 and earlier)
-	// instead of their Go 1.23+ synchronous behavior.
-	// The value can change at any time (in response to os.Setenv("GODEBUG"))
-	// and affects all extant timer channels immediately.
-	// Programs wouldn't normally change over an execution,
-	// but allowing it is convenient for testing and for programs
-	// that do an os.Setenv in main.init or main.main.
-	asynctimerchan atomic.Int32
-
 	// tracebacklabels controls the inclusion of goroutine labels in the
 	// goroutine status header line.
 	tracebacklabels atomic.Int32
@@ -366,7 +356,6 @@ var debug struct {
 var dbgvars = []*dbgVar{
 	{name: "adaptivestackstart", value: &debug.adaptivestackstart},
 	{name: "asyncpreemptoff", value: &debug.asyncpreemptoff},
-	{name: "asynctimerchan", atomic: &debug.asynctimerchan},
 	{name: "cgocheck", value: &debug.cgocheck},
 	{name: "clobberfree", value: &debug.clobberfree},
 	{name: "containermaxprocs", value: &debug.containermaxprocs, def: 1},
