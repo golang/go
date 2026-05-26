6366476e7f2680d6bea9913c6a17a53aa7e3b519
Keith Randall <khr@golang.org>
2026-05-26 13:13:11 -0700

    runtime: make concurrent map test less flaky
    Pre-populate the map, so no actual allocations occur during
    map updates. That lessens the chance of heap corruption.
    
    Also get rid of the want2 cases in the test. Those cases are no
    longer possible after CL 770303.
    
    Update #79671
    Maybe fixes, if it avoids an infinite loop situation. Not sure
    how that would happen, but maybe?
    
    Change-Id: Id764184584f2312fe59b2091c4addd90d55c56a9
    Reviewed-on: https://go-review.googlesource.com/c/go/+/783360
    Reviewed-by: Michael Pratt <mpratt@google.com>
    LUCI-TryBot-Result: golang-scoped@luci-project-accounts.iam.gserviceaccount.com <golang-scoped@luci-project-accounts.iam.gserviceaccount.com>
    Reviewed-by: Keith Randall <khr@google.com>

--- src/runtime/crash_test.go
+++ src/runtime/crash_test.go
@@ -686,10 +686,7 @@ func TestConcurrentMapWrites(t *testing.T) {
 	testenv.MustHaveGoRun(t)
 	output := runTestProg(t, "testprog", "concurrentMapWrites")
 	want := "fatal error: concurrent map writes\n"
-	// Concurrent writes can corrupt the map in a way that we
-	// detect with a separate throw.
-	want2 := "fatal error: small map with no empty slot (concurrent map writes?)\n"
-	if !strings.HasPrefix(output, want) && !strings.HasPrefix(output, want2) {
+	if !strings.HasPrefix(output, want) {
 		t.Fatalf("output does not start with %q:\n%s", want, output)
 	}
 }
@@ -703,10 +700,7 @@ func TestConcurrentMapReadWrite(t *testing.T) {
 	testenv.MustHaveGoRun(t)
 	output := runTestProg(t, "testprog", "concurrentMapReadWrite")
 	want := "fatal error: concurrent map read and map write\n"
-	// Concurrent writes can corrupt the map in a way that we
-	// detect with a separate throw.
-	want2 := "fatal error: small map with no empty slot (concurrent map writes?)\n"
-	if !strings.HasPrefix(output, want) && !strings.HasPrefix(output, want2) {
+	if !strings.HasPrefix(output, want) {
 		t.Fatalf("output does not start with %q:\n%s", want, output)
 	}
 }
@@ -720,10 +714,7 @@ func TestConcurrentMapIterateWrite(t *testing.T) {
 	testenv.MustHaveGoRun(t)
 	output := runTestProg(t, "testprog", "concurrentMapIterateWrite")
 	want := "fatal error: concurrent map iteration and map write\n"
-	// Concurrent writes can corrupt the map in a way that we
-	// detect with a separate throw.
-	want2 := "fatal error: small map with no empty slot (concurrent map writes?)\n"
-	if !strings.HasPrefix(output, want) && !strings.HasPrefix(output, want2) {
+	if !strings.HasPrefix(output, want) {
 		t.Fatalf("output does not start with %q:\n%s", want, output)
 	}
 }
@@ -749,10 +740,7 @@ func TestConcurrentMapWritesIssue69447(t *testing.T) {
 			continue
 		}
 		want := "fatal error: concurrent map writes\n"
-		// Concurrent writes can corrupt the map in a way that we
-		// detect with a separate throw.
-		want2 := "fatal error: small map with no empty slot (concurrent map writes?)\n"
-		if !strings.HasPrefix(output, want) && !strings.HasPrefix(output, want2) {
+		if !strings.HasPrefix(output, want) {
 			t.Fatalf("output does not start with %q:\n%s", want, output)
 		}
 	}
