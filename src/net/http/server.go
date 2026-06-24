5bbd22ff78daf010c5bd19c466a0c45ac78503d4
Damien Neil <dneil@google.com>
2026-06-24 15:24:36 -0700

    [release-branch.go1.26] net/http: apply header timeout to server's unencrypted HTTP/2 check
    When a server is configured to support unencrypted HTTP/2,
    it reads a few bytes from each new connection to see if they
    contain the HTTP/2 client preface. This read was being done
    with no timeout applied. Apply the header timeout (since this
    is essentially reading the first headers from the connection).
    
    Hoist the header timeout into the server serve loop so we can
    use a single timeout to cover both the HTTP/2 preface and
    the first HTTP/1 headers.
    
    Thanks to Vsevolod Naumov and Ainar Garipov from AdGuard for reporting this issue.
    
    For #80205
    Fixes #80224
    For CVE-2026-56853
    
    Change-Id: I4bbb917e11ccb9616594379ef556cee66a6a6964
    Reviewed-on: https://go-review.googlesource.com/c/go/+/797520
    LUCI-TryBot-Result: golang-scoped@luci-project-accounts.iam.gserviceaccount.com <golang-scoped@luci-project-accounts.iam.gserviceaccount.com>
    Reviewed-by: Nicholas Husin <husin@google.com>
    Reviewed-by: Nicholas Husin <nsh@golang.org>
    Reviewed-on: https://go-review.googlesource.com/c/go/+/797940
    Reviewed-by: David Chase <drchase@google.com>

--- src/net/http/server.go
+++ src/net/http/server.go
@@ -978,18 +978,11 @@ func (c *conn) readRequest(ctx context.Context) (w *response, err error) {
 		return nil, ErrHijacked
 	}
 
-	var (
-		wholeReqDeadline time.Time // or zero if none
-		hdrDeadline      time.Time // or zero if none
-	)
 	t0 := time.Now()
-	if d := c.server.readHeaderTimeout(); d > 0 {
-		hdrDeadline = t0.Add(d)
-	}
+	var wholeReqDeadline time.Time // or zero if none
 	if d := c.server.ReadTimeout; d > 0 {
 		wholeReqDeadline = t0.Add(d)
 	}
-	c.rwc.SetReadDeadline(hdrDeadline)
 	if d := c.server.WriteTimeout; d > 0 {
 		defer func() {
 			c.rwc.SetWriteDeadline(time.Now().Add(d))
@@ -1045,10 +1038,7 @@ func (c *conn) readRequest(ctx context.Context) (w *response, err error) {
 		body.doEarlyClose = true
 	}
 
-	// Adjust the read deadline if necessary.
-	if !hdrDeadline.Equal(wholeReqDeadline) {
-		c.rwc.SetReadDeadline(wholeReqDeadline)
-	}
+	c.rwc.SetReadDeadline(wholeReqDeadline)
 
 	w = &response{
 		conn:          c,
@@ -1981,6 +1971,10 @@ func (c *conn) serve(ctx context.Context) {
 	c.bufr = newBufioReader(c.r)
 	c.bufw = newBufioWriterSize(checkConnErrorWriter{c}, 4<<10)
 
+	if d := c.server.readHeaderTimeout(); d > 0 {
+		c.rwc.SetReadDeadline(time.Now().Add(d))
+	}
+
 	protos := c.server.protocols()
 	if c.tlsState == nil && protos.UnencryptedHTTP2() {
 		if c.maybeServeUnencryptedHTTP2(ctx) {
@@ -2110,7 +2104,11 @@ func (c *conn) serve(ctx context.Context) {
 			return
 		}
 
-		c.rwc.SetReadDeadline(time.Time{})
+		if d := c.server.readHeaderTimeout(); d > 0 {
+			c.rwc.SetReadDeadline(time.Now().Add(d))
+		} else {
+			c.rwc.SetReadDeadline(time.Time{})
+		}
 	}
 }
 
