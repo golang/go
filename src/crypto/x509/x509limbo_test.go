5e5ffd0c831d7d67f0f37dcf8f4c31c3e8af2a22
Daniel McCarney <daniel@binaryparadox.net>
2026-07-31 09:06:18 -0400

    crypto/x509: skip x509-limbo ML-DSA cases when required
    This commit updates the x509-limbo path-building tests so that when
    using GOFIPS140=v1.0.0 and the frozen 1.0.0 FIPS module, the ML-DSA test
    cases are skipped. This version of the module doesn't support ML-DSA and
    so the ML-DSA test-cases that expect success will fail.
    
    Fixes #80647
    
    Cq-Include-Trybots: luci.golang.try:gotip-linux-amd64-longtest
    Change-Id: I02c482345db46ad7e2780a92f75a23dd4fbfe6ee
    Reviewed-on: https://go-review.googlesource.com/c/go/+/808460
    Auto-Submit: Daniel McCarney <daniel@binaryparadox.net>
    Reviewed-by: David Chase <drchase@google.com>
    LUCI-TryBot-Result: golang-scoped@luci-project-accounts.iam.gserviceaccount.com <golang-scoped@luci-project-accounts.iam.gserviceaccount.com>
    Reviewed-by: Roland Shoemaker <roland@golang.org>

--- src/crypto/x509/x509limbo_test.go
+++ src/crypto/x509/x509limbo_test.go
@@ -5,6 +5,7 @@
 package x509
 
 import (
+	"crypto/fips140"
 	"crypto/internal/cryptotest"
 	"crypto/internal/cryptotest/x509limbo"
 	"encoding/json"
@@ -203,6 +204,10 @@ func TestX509Limbo(t *testing.T) {
 
 			if slices.Contains(tc.Features, x509limbo.FeatureNameConstraintDn) {
 				t.Skipf("name constraints for DirectoryNames are not supported")
+			}
+
+			if slices.Contains(tc.Features, x509limbo.FeatureHasMldsa) && fips140.Version() == "v1.0.0" {
+				t.Skipf("ML-DSA is not available in FIPS 140-3 module v1.0.0")
 			}
 
 			if len(tc.SignatureAlgorithms) != 0 {
