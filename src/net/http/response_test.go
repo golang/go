d1c32e1e30bb67a6274a50029da2d8a2c9de0260
Damien Neil <dneil@google.com>
2026-08-18 18:32:26 -0700

    net/http: make response test failures more descriptive
    Change-Id: I5f7be88128ac0fcb908848b7bd1cdbad6a6a6964
    Reviewed-on: https://go-review.googlesource.com/c/go/+/818660
    Auto-Submit: Damien Neil <dneil@google.com>
    Reviewed-by: Nicholas Husin <nsh@golang.org>
    LUCI-TryBot-Result: golang-scoped@luci-project-accounts.iam.gserviceaccount.com <golang-scoped@luci-project-accounts.iam.gserviceaccount.com>
    Reviewed-by: Nicholas Husin <husin@google.com>

--- src/net/http/response_test.go
+++ src/net/http/response_test.go
@@ -21,6 +21,7 @@ import (
 )
 
 type respTest struct {
+	Name   string
 	Raw    string
 	RawOut string
 	Resp   Response
@@ -36,8 +37,9 @@ func dummyReq11(method string) *Request {
 }
 
 var respTests = []respTest{
-	// Unchunked response without Content-Length.
 	{
+		"Unchunked response without Content-Length",
+
 		"HTTP/1.0 200 OK\r\n" +
 			"Connection: close\r\n" +
 			"\r\n" +
@@ -65,9 +67,9 @@ var respTests = []respTest{
 		"Body here\n",
 	},
 
-	// Unchunked HTTP/1.1 response without Content-Length or
-	// Connection headers.
 	{
+		"Unchunked HTTP/1.1 response without Content-Length or Connection",
+
 		"HTTP/1.1 200 OK\r\n" +
 			"\r\n" +
 			"Body here\n",
@@ -92,8 +94,9 @@ var respTests = []respTest{
 		"Body here\n",
 	},
 
-	// Unchunked HTTP/1.1 204 response without Content-Length.
 	{
+		"Unchunked HTTP/1.1 204 response without Content-Length",
+
 		"HTTP/1.1 204 No Content\r\n" +
 			"\r\n" +
 			"Body should not be read!\n",
@@ -116,8 +119,9 @@ var respTests = []respTest{
 		"",
 	},
 
-	// Unchunked response with Content-Length.
 	{
+		"Unchunked response with Content-Length",
+
 		"HTTP/1.0 200 OK\r\n" +
 			"Content-Length: 10\r\n" +
 			"Connection: close\r\n" +
@@ -148,8 +152,9 @@ var respTests = []respTest{
 		"Body here\n",
 	},
 
-	// Chunked response without Content-Length.
 	{
+		"Chunked response without Content-Length",
+
 		"HTTP/1.1 200 OK\r\n" +
 			"Transfer-Encoding: chunked\r\n" +
 			"\r\n" +
@@ -184,8 +189,9 @@ var respTests = []respTest{
 		"Body here\ncontinued",
 	},
 
-	// Trailer header but no TransferEncoding
 	{
+		"Trailer header but no TransferEncoding",
+
 		"HTTP/1.0 200 OK\r\n" +
 			"Trailer: Content-MD5, Content-Sources\r\n" +
 			"Content-Length: 10\r\n" +
@@ -218,8 +224,9 @@ var respTests = []respTest{
 		"Body here\n",
 	},
 
-	// Chunked response with Content-Length.
 	{
+		"Chunked response with Content-Length",
+
 		"HTTP/1.1 200 OK\r\n" +
 			"Transfer-Encoding: chunked\r\n" +
 			"Content-Length: 10\r\n" +
@@ -253,8 +260,9 @@ var respTests = []respTest{
 		"Body here\n",
 	},
 
-	// Chunked response in response to a HEAD request
 	{
+		"Chunked response in response to a HEAD request",
+
 		"HTTP/1.1 200 OK\r\n" +
 			"Transfer-Encoding: chunked\r\n" +
 			"\r\n",
@@ -279,8 +287,9 @@ var respTests = []respTest{
 		"",
 	},
 
-	// Content-Length in response to a HEAD request
 	{
+		"Content-Length in response to a HEAD request",
+
 		"HTTP/1.0 200 OK\r\n" +
 			"Content-Length: 256\r\n" +
 			"\r\n",
@@ -306,8 +315,9 @@ var respTests = []respTest{
 		"",
 	},
 
-	// Content-Length in response to a HEAD request with HTTP/1.1
 	{
+		"Content-Length in response to a HEAD request with HTTP/1.1",
+
 		"HTTP/1.1 200 OK\r\n" +
 			"Content-Length: 256\r\n" +
 			"\r\n",
@@ -332,8 +342,9 @@ var respTests = []respTest{
 		"",
 	},
 
-	// No Content-Length or Chunked in response to a HEAD request
 	{
+		"No Content-Length or Chunked in response to a HEAD request",
+
 		"HTTP/1.0 200 OK\r\n" +
 			"\r\n",
 
@@ -357,8 +368,9 @@ var respTests = []respTest{
 		"",
 	},
 
-	// explicit Content-Length of 0.
 	{
+		"explicit Content-Length of 0",
+
 		"HTTP/1.1 200 OK\r\n" +
 			"Content-Length: 0\r\n" +
 			"\r\n",
@@ -384,9 +396,10 @@ var respTests = []respTest{
 		"",
 	},
 
-	// Status line without a Reason-Phrase, but trailing space.
-	// (permitted by RFC 7230, section 3.1.2)
 	{
+		"Status line without a Reason-Phrase, but trailing space",
+		// (permitted by RFC 7230, section 3.1.2)
+
 		"HTTP/1.0 303 \r\n\r\n",
 
 		"HTTP/1.0 303 \r\n" +
@@ -408,9 +421,10 @@ var respTests = []respTest{
 		"",
 	},
 
-	// Status line without a Reason-Phrase, and no trailing space.
-	// (not permitted by RFC 7230, but we'll accept it anyway)
 	{
+		"Status line without a Reason-Phrase, and no trailing space",
+		// (not permitted by RFC 7230, but we'll accept it anyway)
+
 		"HTTP/1.0 303\r\n\r\n",
 
 		"HTTP/1.0 303 303\r\n" +
@@ -432,8 +446,10 @@ var respTests = []respTest{
 		"",
 	},
 
-	// golang.org/issue/4767: don't special-case multipart/byteranges responses
 	{
+		// don't special-case multipart/byteranges responses
+		"Issue 4767",
+
 		`HTTP/1.1 206 Partial Content
 Connection: close
 Content-Type: multipart/byteranges; boundary=18a75608c8f47cef
@@ -463,8 +479,9 @@ some body`,
 		"some body",
 	},
 
-	// Unchunked response without Content-Length, Request is nil
 	{
+		"Unchunked response without Content-Length, Request is nil",
+
 		"HTTP/1.0 200 OK\r\n" +
 			"Connection: close\r\n" +
 			"\r\n" +
@@ -491,8 +508,10 @@ some body`,
 		"Body here\n",
 	},
 
-	// 206 Partial Content. golang.org/issue/8923
 	{
+		"206 Partial Content",
+		// golang.org/issue/8923
+
 		"HTTP/1.1 206 Partial Content\r\n" +
 			"Content-Type: text/plain; charset=utf-8\r\n" +
 			"Accept-Ranges: bytes\r\n" +
@@ -527,8 +546,10 @@ some body`,
 		"foobar",
 	},
 
-	// Both keep-alive and close, on the same Connection line. (Issue 8840)
 	{
+		"Both keep-alive and close, on the same Connection line",
+		// (Issue 8840)
+
 		"HTTP/1.1 200 OK\r\n" +
 			"Content-Length: 256\r\n" +
 			"Connection: keep-alive, close\r\n" +
@@ -557,8 +578,10 @@ some body`,
 		"",
 	},
 
-	// Both keep-alive and close, on different Connection lines. (Issue 8840)
 	{
+		"Both keep-alive and close, on different Connection lines",
+		// (Issue 8840)
+
 		"HTTP/1.1 200 OK\r\n" +
 			"Content-Length: 256\r\n" +
 			"Connection: keep-alive\r\n" +
@@ -588,9 +611,11 @@ some body`,
 		"",
 	},
 
-	// Issue 12785: HTTP/1.0 response with bogus (to be ignored) Transfer-Encoding.
-	// Without a Content-Length.
 	{
+		// Issue 12785: HTTP/1.0 response with bogus (to be ignored) Transfer-Encoding.
+		// Without a Content-Length.
+		"HTTP1.0 bogus T-E no C-L",
+
 		"HTTP/1.0 200 OK\r\n" +
 			"Transfer-Encoding: bogus\r\n" +
 			"\r\n" +
@@ -616,9 +641,11 @@ some body`,
 		"Body here\n",
 	},
 
-	// Issue 12785: HTTP/1.0 response with bogus (to be ignored) Transfer-Encoding.
-	// With a Content-Length.
 	{
+		// Issue 12785: HTTP/1.0 response with bogus (to be ignored) Transfer-Encoding.
+		// With a Content-Length.
+		"HTTP1.0 bogus T-E with C-L",
+
 		"HTTP/1.0 200 OK\r\n" +
 			"Transfer-Encoding: bogus\r\n" +
 			"Content-Length: 10\r\n" +
@@ -649,6 +676,8 @@ some body`,
 	},
 
 	{
+		"keep-alive timeout",
+
 		"HTTP/1.1 200 OK\r\n" +
 			"Content-Encoding: gzip\r\n" +
 			"Content-Length: 23\r\n" +
@@ -682,8 +711,10 @@ some body`,
 		"\x1f\x8b\b\x00\x00\x00\x00\x00\x00\x00s\xf3\xf7\a\x00\xab'\xd4\x1a\x03\x00\x00\x00",
 	},
 
-	// Issue 19989: two spaces between HTTP version and status.
 	{
+		// Issue 19989
+		"Two spaces between HTTP version and status",
+
 		"HTTP/1.0  401 Unauthorized\r\n" +
 			"Content-type: text/html\r\n" +
 			"WWW-Authenticate: Basic realm=\"\"\r\n\r\n" +
@@ -720,12 +751,12 @@ func TestReadResponse(t *testing.T) {
 	for i, tt := range respTests {
 		resp, err := ReadResponse(bufio.NewReader(strings.NewReader(tt.Raw)), tt.Resp.Request)
 		if err != nil {
-			t.Errorf("#%d: %v", i, err)
+			t.Errorf("#%d %v: %v", i, tt.Name, err)
 			continue
 		}
 		rbody := resp.Body
 		resp.Body = nil
-		diff(t, fmt.Sprintf("#%d Response", i), resp, &tt.Resp)
+		diff(t, fmt.Sprintf("#%d %v: Response", i, tt.Name), resp, &tt.Resp)
 		var bout strings.Builder
 		if rbody != nil {
 			_, err = io.Copy(&bout, rbody)
@@ -737,7 +768,7 @@ func TestReadResponse(t *testing.T) {
 		}
 		body := bout.String()
 		if body != tt.Body {
-			t.Errorf("#%d: Body = %q want %q", i, body, tt.Body)
+			t.Errorf("#%d %v: Body = %q want %q", i, tt.Name, body, tt.Body)
 		}
 	}
 }
@@ -746,18 +777,18 @@ func TestWriteResponse(t *testing.T) {
 	for i, tt := range respTests {
 		resp, err := ReadResponse(bufio.NewReader(strings.NewReader(tt.Raw)), tt.Resp.Request)
 		if err != nil {
-			t.Errorf("#%d: %v", i, err)
+			t.Errorf("#%d %v: %v", i, tt.Name, err)
 			continue
 		}
 		var buf bytes.Buffer
 		err = resp.Write(&buf)
 		if err != nil {
-			t.Errorf("#%d: %v", i, err)
+			t.Errorf("#%d %v: %v", i, tt.Name, err)
 			continue
 		}
 		if got, want := buf.String(), tt.RawOut; got != want {
-			t.Errorf("#%d: response differs; got:\n----\n%v\n----\nwant:\n----\n%v\n----\n",
-				i,
+			t.Errorf("#%d %v: response differs; got:\n----\n%v\n----\nwant:\n----\n%v\n----\n",
+				i, tt.Name,
 				strings.ReplaceAll(got, "\r", "\\r"),
 				strings.ReplaceAll(want, "\r", "\\r"))
 		}
