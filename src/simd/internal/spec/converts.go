73afac4dad38d17bbd18963eaff596b30de5065a
Austin Clements <austin@google.com>
2026-09-04 21:59:39 -0400

    simd/internal/spec: fix ConvertToZ width rounding
    The current "zL=xL" constraint doesn't apply width rounding, so we
    don't get
    
        (Float64x2).ConverToFloat32() Float32x4
    
    This changes the constraint to z={zB}{zN}x{xL}, which does apply width
    rounding, so we do get the width-rounded method.
    
    Change-Id: I9248f858459aa83ec516f016e682506771ad7385
    Reviewed-on: https://go-review.googlesource.com/c/go/+/828144
    LUCI-TryBot-Result: golang-scoped@luci-project-accounts.iam.gserviceaccount.com <golang-scoped@luci-project-accounts.iam.gserviceaccount.com>
    Reviewed-by: Junyang Shao <shaojunyang@google.com>
    Auto-Submit: Austin Clements <austin@google.com>

--- src/simd/internal/spec/converts.go
+++ src/simd/internal/spec/converts.go
@@ -9,7 +9,7 @@ package spec
 // ConvertToZ converts element values to {zE}. The result has the same number of lanes.
 //
 //specgen:name ConvertTo{zE}
-//specgen:require zL=xL zE!=xE
+//specgen:require z={zB}{zN}x{xL} zE!=xE
 func ConvertToZ[xE Nums, xW Width, zE Nums, zW Width](x Vec[xE, xW]) (z Vec[zE, zW]) {
 	// Architectures are generally significantly more constrained in what they
 	// will convert between, but this operation describes the full universe of
