c50265b586ee5ceac8d180f07acd86a6e5a0e8bf
Michael Matloob <matloob@golang.org>
2026-09-08 08:30:05 -0400

    cmd/dist: don't set purego or math_big_pure_go when building toolchain1
    The setting of math_big_pure_go dates to when we required Go 1.4 to
    bootstrap the compiler in CL 31142, and its considerations don't
    apply anymore. The setting of purego was added in CL 415934 so that
    we could run bootstrap using a gccgo compiler, but we can't currently
    use gccgo to bootstrap the go command because we need a version newer
    than what gccgo supports. gccgo would also need math_big_pure_go for
    the same reason. Remove the setting. If gccgo starts supporting newer
    versions of go we can easily add these back but just for the
    case where the bootstrap toolchain is gccgo.
    
    The main reason to do this is to enable using the assembly sha256
    implementation when building the go command using the bootstrap
    toolchain. The go command hashes things left and right so it's important
    it's fast.
    
    For #81386
    
    Change-Id: I881448ca33fda78fe391ed014e915d9a6a6a6964
    Reviewed-on: https://go-review.googlesource.com/c/go/+/829084
    Reviewed-by: Dmitri Shuralyov <dmitshur@google.com>
    Reviewed-by: Michael Matloob <matloob@google.com>
    LUCI-TryBot-Result: golang-scoped@luci-project-accounts.iam.gserviceaccount.com <golang-scoped@luci-project-accounts.iam.gserviceaccount.com>
    Reviewed-by: Dmitri Shuralyov <dmitshur@golang.org>

--- src/cmd/dist/buildtool.go
+++ src/cmd/dist/buildtool.go
@@ -240,13 +240,10 @@ func bootstrapBuildTools() {
 	os.Setenv("GOHOSTARCH", "")
 
 	// Run Go bootstrap to build binaries.
-	// Use the math_big_pure_go build tag to disable the assembly in math/big
-	// which may contain unsupported instructions.
-	// Use the purego build tag to disable other assembly code.
 	cmd := []string{
 		pathf("%s/bin/go", goroot_bootstrap),
 		"install",
-		"-tags=math_big_pure_go compiler_bootstrap purego",
+		"-tags=compiler_bootstrap",
 	}
 	if vflag > 0 {
 		cmd = append(cmd, "-v")
