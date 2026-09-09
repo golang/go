9983c66d394a53a67eb4a4e43e1808d4db75a4a9
Kailash Nelson <37966146+KingLizard1020@users.noreply.github.com>
2026-09-09 15:27:14 -0600

    cmd/link: drop empty-linker guards in flag probes
    extld() always returns at least gcc or clang, matching findLibPathCmd.

--- src/cmd/link/internal/ld/lib.go
+++ src/cmd/link/internal/ld/lib.go
@@ -1,2 +1,5 @@
 // Inferno utils/8l/asm.c
 // https://bitbucket.org/inferno-os/inferno-os/src/master/utils/8l/asm.c
+//
+//	Copyright © 1994-1999 Lucent Technologies Inc.  All rights reserved.
+package ld
