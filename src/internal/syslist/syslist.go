5ef4dd3a8ea8ba536993013ae68ba5333421936c
Michael Matloob <matloob@golang.org>
2026-09-11 17:21:58 -0700

    internal/syslist: do cleanups post deleting cmd/dist custom build system
    This addresses comments from CL 829205.
    
    For #81386
    
    Change-Id: Ifb61f0f75385bd85610511aac2c1fb406a6a6964
    Reviewed-on: https://go-review.googlesource.com/c/go/+/831424
    Auto-Submit: Michael Matloob <matloob@golang.org>
    Reviewed-by: Michael Matloob <matloob@google.com>
    LUCI-TryBot-Result: golang-scoped@luci-project-accounts.iam.gserviceaccount.com <golang-scoped@luci-project-accounts.iam.gserviceaccount.com>
    Reviewed-by: Dmitri Shuralyov <dmitshur@golang.org>
    Reviewed-by: Dmitri Shuralyov <dmitshur@google.com>

--- src/internal/syslist/syslist.go
+++ src/internal/syslist/syslist.go
@@ -4,7 +4,6 @@
 
 // Package syslist stores tables of OS and ARCH names that are
 // (or at one point were) acceptable build targets.
-
 package syslist
 
 // Note that this file is read by internal/goarch/gengoarch.go and by
@@ -37,7 +36,6 @@ var KnownOS = map[string]bool{
 
 // UnixOS is the set of GOOS values matched by the "unix" build tag.
 // This is not used for filename matching.
-// This list also appears in cmd/dist/build.go.
 var UnixOS = map[string]bool{
 	"aix":       true,
 	"android":   true,
