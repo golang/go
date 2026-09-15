7a90186a75cc8558f1ffe36c55f5ebdd1ffede07
limeidan <limeidan@loongson.cn>
2026-09-15 10:54:18 +0800

    cmd/internal/obj/loong64: fix the 'param' register of the [X]VMOVQ when its operand is C_SAUTO
    Change-Id: I05de5e49f369df61a9729f8bd1e73dcc069cd34a
    Reviewed-on: https://go-review.googlesource.com/c/go/+/832644
    LUCI-TryBot-Result: golang-scoped@luci-project-accounts.iam.gserviceaccount.com <golang-scoped@luci-project-accounts.iam.gserviceaccount.com>
    Reviewed-by: abner chenc <chenguoqi@loongson.cn>
    Reviewed-by: Cherry Mui <cherryyz@google.com>
    Reviewed-by: Junyang Shao <shaojunyang@google.com>

--- src/cmd/asm/internal/asm/testdata/loong64enc1.s
+++ src/cmd/asm/internal/asm/testdata/loong64enc1.s
@@ -495,26 +495,26 @@ lable2:
 	VMOVQ		V2, 3(R4)       // 820c402c
 	VMOVQ		V2, 2040(R4)    // 82e05f2c
 	VMOVQ		V2, -2040(R4)   // 8220602c
-	VMOVQ		V2, y+16(FP)    // 0260402c
-	VMOVQ		V2, x+2030(FP)  // 02d85f2c
+	VMOVQ		V2, y+16(FP)    // 6260402c
+	VMOVQ		V2, x+2030(FP)  // 62d85f2c
 	VMOVQ		(R4), V2        // 8200002c
 	VMOVQ		3(R4), V2       // 820c002c
 	VMOVQ		2044(R4), V2    // 82f01f2c
 	VMOVQ		-2044(R4), V2   // 8210202c
-	VMOVQ		y+16(FP), V2    // 0260002c
-	VMOVQ		x+2030(FP), V2  // 02d81f2c
+	VMOVQ		y+16(FP), V2    // 6260002c
+	VMOVQ		x+2030(FP), V2  // 62d81f2c
 	XVMOVQ		X2, (R4)        // 8200c02c
 	XVMOVQ		X3, 3(R4)       // 830cc02c
 	XVMOVQ		X4, 2040(R4)    // 84e0df2c
 	XVMOVQ		X5, -2040(R4)   // 8520e02c
-	XVMOVQ		X6, y+16(FP)    // 0660c02c
-	XVMOVQ		X7, x+2030(FP)  // 07d8df2c
+	XVMOVQ		X6, y+16(FP)    // 6660c02c
+	XVMOVQ		X7, x+2030(FP)  // 67d8df2c
 	XVMOVQ		(R4), X2        // 8200802c
 	XVMOVQ		3(R4), X3       // 830c802c
 	XVMOVQ		2044(R4), X4    // 84f09f2c
 	XVMOVQ		-2044(R4), X5   // 8510a02c
-	XVMOVQ		y+16(FP), X6    // 0660802c
-	XVMOVQ		x+2030(FP), X7  // 07d89f2c
+	XVMOVQ		y+16(FP), X6    // 6660802c
+	XVMOVQ		x+2030(FP), X7  // 67d89f2c
 
 	// Move vector element to general-purpose register: VMOVQ  <Vn>.<T>[index], Rd
 	VMOVQ		V0.B[0], R4     // 0480ef72
